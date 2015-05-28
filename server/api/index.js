9068c1c1aa5db6a5652be41d9683dbce4b2f4a52
Tivs <tivoli@nurfed.com>
2015-05-27 17:23:30 -0700

    Add request module and update with base api request

--- server/api/index.js
+++ server/api/index.js
@@ -1,15 +1,26 @@
 'use strict';
 
 const
-  Router = require('koa-router');
+  Router  = require('koa-router'),
+  t       = require('thunkify-wrap'),
+  request = require('request');
 
 module.exports = function(app) {
+  const
+    baseRequest = request.defaults({
+      baseUrl: app.config.api.uri,
+      _json: true
+    });
+
   let router = new Router({
     prefix: '/api/v1'
   });
 
   router.get('/:path*', function *() {
-    this.body = this.params.path;
+    let
+      res   = yield t(baseRequest.get)(this.params.path),
+      data  = res[1];
+    this.body = data;
   });
 
   app
