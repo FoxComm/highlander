b6dc073494d4a453d44393d753921ac2ea680d33
Jacques Fuentes <jpfuentes2@gmail.com>
2015-06-04 14:20:53 -0700

    Hope someone catches this in review!

--- sql/V1__create_accounts_table.sql
+++ sql/V1__create_accounts_table.sql
@@ -1,7 +1,7 @@
 create table accounts (
     id integer not null,
     email character varying(255) not null,
-    plaintext_password character varying(255),
+    plaintext_password character varying(255), -- are you paying attention?
     hashed_password character varying(255) not null,
     first_name character varying(255),
     last_name character varying(255),
