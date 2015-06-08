7512db5b05b7dc204eeda53ce7401c73d819e410
Ferdinand Svehla <f.svehla@gmail.com>
2015-06-08 19:17:08 +0200

    CreatesStockItems service

---
+++ app/services/CreatesStockItems.scala
@@ -0,0 +1,33 @@
+package services
+
+import models.StockItem
+import org.scalactic.{Good, Bad, ErrorMessage, Or}
+
+import scala.concurrent.{Future, ExecutionContext}
+
+import slick.driver.PostgresDriver.api._
+
+object CreatesStockItems {
+  /** This is just for demonstration purposes! */
+  def apply(productId: Int, onHold: Int, onHand: Int)
+           (implicit ec: ExecutionContext, db: Database): Future[StockItem Or ErrorMessage] = {
+
+    /** We’ll use real validations here soon */
+    if (onHand < 0) {
+      Future.successful(Bad(s"On hand must be >= 0"))
+    } else {
+      val stockItem = StockItem(
+        id = 0,
+        productId = productId,
+        stockLocationId = 0,
+        onHold = onHold,
+        onHand = onHand,
+        allocatedToSales = 0
+      )
+
+      db.run(for {
+        id ← tables.StockItems.returningId += stockItem
+      } yield Good(stockItem.copy(id = id)))
+    }
+  }
+}
