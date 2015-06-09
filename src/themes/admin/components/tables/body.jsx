8d214198eeb398b6692ddcc68f646b6a579fa4e6
Tivs <tivoli@nurfed.com>
2015-06-09 12:18:17 -0400

    Use arrow functions for binding in table body conversion

--- src/themes/admin/components/tables/body.jsx
+++ src/themes/admin/components/tables/body.jsx
@@ -3,32 +3,32 @@
 import React from 'react';
 import moment from 'moment';
 
-function formatCurrency(num) {
-  num = num.toString();
-  let
-    dollars = num.slice(0, -2),
-    cents   = num.slice(-2);
-  dollars = dollars.replace(/\B(?=(\d{3})+(?!\d))/g, ',');
-  return `$${dollars}.${cents}`;
-}
-
 class TableBody extends React.Component {
+  formatCurrency(num) {
+    num = num.toString();
+    let
+      dollars = num.slice(0, -2),
+      cents   = num.slice(-2);
+    dollars = dollars.replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+    return `$${dollars}.${cents}`;
+  }
+
+  convert(field, type) {
+    switch(type) {
+      case 'currency': return this.formatCurrency(field);
+      case 'date': return moment(field).format('DD/MM/YYYY');
+      default: return field;
+    }
+  }
+
   render() {
     let columns = this.props.columns;
 
-    function convert(field, type) {
-      switch(type) {
-        case 'currency': return formatCurrency(field);
-        case 'date': return moment(field).format('DD/MM/YYYY');
-        default: return field;
-      }
-    }
-
-    let createRow = function(row, idx) {
+    let createRow = (row, idx) => {
       return (
         <tr key={idx}>
-          {columns.map(function(column) {
-            let data = convert(row[column.field], column.type);
+          {columns.map((column) => {
+            let data = this.convert(row[column.field], column.type);
             return <td key={`${idx}-${column.field}`}>{data}</td>;
           })}
         </tr>
