8e5e298305e92bff4f72a166b7a8ffad06310e18
Ferdinand Svehla <f.svehla@gmail.com>
2015-06-10 22:06:41 +0200

    Don’t use before/after since it is non-obvious with our traits

--- test/services/CreatesStockItemsTest.scala
+++ test/services/CreatesStockItemsTest.scala
@@ -14,25 +14,23 @@ class CreatesStockItemsTest extends FreeSpec
   import concurrent.ExecutionContext.Implicits.global
   import api._
 
-  before {
-    db.run(StockItems.query.schema.create).futureValue
-  }
-
-   after {
-    db.run(models.StockItems.query.schema.drop).futureValue
-  }
-
   "CreatesStockItems" - {
-    "creates a new stock item" in {
+    "creates a new stock item" in withStockItemSchema {
       val item = CreatesStockItems(productId = 1, onHand = 1, onHold = 0).futureValue
       item mustBe 'good
     }
 
-    "returns error messages on invalid input parameters" in {
+    "returns error messages on invalid input parameters" in withStockItemSchema {
       val result = CreatesStockItems(productId = 1, onHand = -1, onHold = 0).futureValue
       val error  = result.swap.get
 
       error mustBe "On hand must be >= 0"
     }
+  }
+
+  def withStockItemSchema(testCode: ⇒ Any) = {
+    db.run(StockItems.query.schema.create).futureValue
+    testCode
+    db.run(models.StockItems.query.schema.drop).futureValue
   }
 }
