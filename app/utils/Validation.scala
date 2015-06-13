b284511f27033f01dbcfbf3bd51dd836275f8eeb
Jacques Fuentes <jpfuentes2@gmail.com>
2015-06-13 10:47:47 -0400

    Bubble up asInstanceOf in Validation

--- app/utils/Validation.scala
+++ app/utils/Validation.scala
@@ -1,12 +1,15 @@
 package utils
 
+import com.wix.accord.transform.ValidationTransform
 import org.scalactic._
 import com.wix.accord.{validate => runValidation, Success}
 import com.wix.accord._
 
-trait Validation {
-  def validator[T]: Validator[T]
-  def validate: Result = { runValidation(this)(validator) }
+trait Validation[T] {
+  def validator: ValidationTransform.TransformedValidator[T]
+
+  def validate: Result = { runValidation(this.asInstanceOf[T])(validator.asInstanceOf[Validator[T]]) }
+
   def isValid: Boolean = { validate == Success }
 }
 
@@ -15,5 +18,5 @@ object Validation {
     failure.violations.map(formatViolation)
   }
 
-  def formatViolation(v: Violation): String = v.description.getOrElse("") ++ " " ++ v.constraint
+  def formatViolation(v: Violation): String = { v.description.getOrElse("") ++ " " ++ v.constraint }
 }
