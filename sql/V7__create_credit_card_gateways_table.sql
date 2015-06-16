8c1c3aa661ce5f52a6cb869da4b64a5b1fa2fc0a
Jacques Fuentes <jpfuentes2@gmail.com>
2015-06-16 12:13:07 -0400

    Fix gateway_customer_id column type

--- sql/V7__create_credit_card_gateways_table.sql
+++ sql/V7__create_credit_card_gateways_table.sql
@@ -1,7 +1,7 @@
 create table credit_card_gateways (
     id serial primary key,
     customer_id integer,
-    gateway_customer_id string not null,
+    gateway_customer_id character varying(255) not null,
     -- gateway_id integer not null, TODO: add lookup table
     last_four character(4),
     exp_month integer,
