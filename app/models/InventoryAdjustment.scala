7e4b630e808862a27b6b676f69d82b53b9b3bac9
Adil Wali <adil@adil.io>
2015-06-18 11:32:02 -0700

    Update inventoryAdjustment to relate to the supertable.

--- app/models/InventoryAdjustment.scala
+++ app/models/InventoryAdjustment.scala
@@ -6,7 +6,7 @@ import slick.driver.PostgresDriver.api._
 import slick.driver.PostgresDriver.backend.{DatabaseDef => Database}
 
 
-case class InventoryAdjustment(id: Int, skuId: Int, orderId: Option[Int], purchaseOrderReceiptId: Option[Int], rmaReceiptId: Option[Int], cycleCountId: Option[Int], physicalInventoryEventId: Option[Int], onHandAdjustment: Int, availablePreOrder: Int, availableBackOrder: Int, outstandingPreOrders: Int, outstandingBackOrders: Int, description: Option[String], sourceNotes: Option[String]) extends ModelWithIdParameter {
+case class InventoryAdjustment(id: Int, skuId: Int, inventoryEventId: Option[Int], availablePreOrder: Int, availableBackOrder: Int, outstandingPreOrders: Int, outstandingBackOrders: Int, description: Option[String], sourceNotes: Option[String]) extends ModelWithIdParameter {
 
 }
 
@@ -15,14 +15,8 @@ class InventoryAdjustments(tag: Tag) extends GenericTable.TableWithId[InventoryA
   def id = column[Int]("id", O.PrimaryKey, O.AutoInc)
   def inventoryEventId = column[Int]("inventory_event_id")
   def skuId = column[Int]("sku_id")
-  def orderId = column[Option[Int]]("order_id") // this happens after order is placed
-  def shipmentId = column[Option[Int]]("shipment_id") // this happens after shipment.
-  def purchaseOrderReceiptId = column[Option[Int]]("purchase_order_receipt_id")
-  def rmaReceiptId = column[Option[Int]]("rma_receipt_id")
-  def cycleCountId = column[Option[Int]]("cycle_count_id")
-  def physicalInventoryEventId = column[Option[Int]]("order_id")
   def reservedForFulfillment = column[Int]("reserved_for_fulfillment")
-  def onHandAdjustment = column[Int]("available_on_hand")
+  def availableOnHand = column[Int]("available_on_hand")
   def availablePreOrder = column[Int]("available_pre_order")
   def availableBackOrder = column[Int]("available_back_order")
   def outstandingPreOrders =  column[Int]("outstanding_pre_orders") // How many have been preordered but not yet fulfilled
@@ -31,7 +25,7 @@ class InventoryAdjustments(tag: Tag) extends GenericTable.TableWithId[InventoryA
   def sourceNotes = column[Option[String]]("source_notes") //Notes about a third party source
 
 
-  def * = (id, skuId, orderId, purchaseOrderReceiptId, rmaReceiptId, cycleCountId, physicalInventoryEventId, onHandAdjustment, availablePreOrder, availableBackOrder, outstandingPreOrders, outstandingBackOrders, description, sourceNotes) <> ((InventoryAdjustment.apply _).tupled, InventoryAdjustment.unapply)
+  def * = (id, skuId, inventoryEventId, availableBackOrder, availablePreOrder, availableBackOrder, outstandingPreOrders, outstandingBackOrders, description, sourceNotes) <> ((InventoryAdjustment.apply _).tupled, InventoryAdjustment.unapply)
 }
 
 object InventoryAdjustments extends TableQueryWithId[InventoryAdjustment, InventoryAdjustments](
--- sql/V23__create_inventory_adjustments.sql
+++ sql/V23__create_inventory_adjustments.sql
@@ -1,38 +1,13 @@
 create table inventory_adjustments (
-    id integer not null,
-    inventory_event_id integer not null,
-    sku_id integer not null,
-    rma_receipt_id integer,
-    cycle_count_id integer,
-    physical_inventory_event_id integer,
-    on_hand_adjustment_id integer,
+    id serial primary key,
+    sku_id bigint not null,
+    inventory_event_id bigint not null,
     available_pre_order integer not null,
     available_back_order integer not null,
     outstanding_pre_orders integer not null,
     outstanding_back_orders integer not null,
     description character varying(255),
-    source_notes text 
-);
-
-create sequence inventory_adjustments_id_seq
-     start with 1
-     increment by 1
-     no minvalue
-     no maxvalue
-     cache 1;
-
-
-alter table only inventory_adjustments
-    add constraint inventory_adjustments_pkey primary key (id);
-
-alter table only inventory_adjustments
-    alter column id set default nextval('inventory_adjustments_id_seq'::regclass);
-
-alter table only inventory_adjustments
-    add constraint inventory_adjustments_sku_id_fk foreign key (sku_id) references skus(id) on update restrict on delete restrict;
-
-alter table only inventory_adjustments
-    add constraint inventory_adjustments_inventory_event_id_fk foreign key (inventory_event_id) references inventory_events(id) on update restrict on delete restrict;
-
-
---TODO: Create foreign key associations with other tables as they become created
+    source_notes text,
+    foreign key (sku_id) references skus(id) on update restrict on delete restrict,
+    foreign key (inventory_event_id) references inventory_events(id) on update restrict on delete restrict
+ );
