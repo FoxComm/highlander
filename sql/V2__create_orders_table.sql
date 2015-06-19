9872e398ccb8f89fb3465aaf5b5db9764d8c8b52
Jacques Fuentes <jpfuentes2@gmail.com>
2015-06-18 23:58:12 -0400

    Use a bool column for orders.locked

--- sql/V2__create_orders_table.sql
+++ sql/V2__create_orders_table.sql
@@ -2,7 +2,7 @@ create table orders (
     id serial primary key,
     customer_id integer,
     status character varying(255) not null,
-    locked int,
+    locked boolean default false,
     created_at timestamp without time zone default (now() at time zone 'utc'),
     updated_at timestamp without time zone default (now() at time zone 'utc'),
     deleted_at timestamp without time zone null
