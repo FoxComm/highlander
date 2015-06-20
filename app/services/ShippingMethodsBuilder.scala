6688fac91f73c3c38f84e0c82ef59cdf8ccfface
Adil Wali <adil@adil.io>
2015-06-20 14:23:26 -0700

    Base placeholder for shippingMethodsBuilder.

---
+++ app/services/ShippingMethodsBuilder.scala
@@ -0,0 +1,11 @@
+package services
+
+class ShippingMethodsBuilder {
+
+  def availableShippingMethods
+  // Which shipping methods are active right now?
+
+  def calculateShippingMethodPrice
+  // What is the price of a certain shipping method based on the current order details?
+
+}
