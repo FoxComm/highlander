eb5d2ef4cf05b5c163c55345bd6e7c254013de25
Adil Wali <adil@adil.io>
2015-06-20 15:39:53 -0700

    WIP: Changing locations.

--- app/services/ShippingMethodsBuilder.scala
+++ app/services/ShippingMethodsBuilder.scala
@@ -1,11 +1,36 @@
 package services
 
-abstract class ShippingMethodsBuilder {
+import models._
 
-  def availableShippingMethods(): Unit
-  // Which shipping methods are active right now?
+import org.scalactic._
+import scala.concurrent.{Future, ExecutionContext}
+import slick.driver.PostgresDriver.api._
+import slick.driver.PostgresDriver.backend.{DatabaseDef => Database}
 
-  def calculateShippingMethodPrice(): Unit
+import slick.driver.PostgresDriver.api._
+
+
+object ShippingMethodsBuilder {
+  case class ShippingMethodWithPrice(displayName: String, estimatedTime: String, price: Int)
+
+  // Which shipping methods are active for this order?
+  // 1) Do restriction check
+  def availableShippingMethods(order: Order)
+                              (implicit ec: ExecutionContext,
+                               db: Database): Future[Seq[ShippingMethod]] = {
+    val availMethods = ShippingMethods.filter(_.isActive).result
+    db.run(availMethods).map(methods => methods)
+  }
+
+
+  def fullShippingMethodsForOrder(order: Order)
+                                 (implicit ec: ExecutionContext,
+                                   db: Database): Future[Seq[ShippingMethodWithPrice]] = {
+    val baseMethods = availableShippingMethods(order)
+    baseMethods.map{ methods =>
+
+    }
+  }
   // What is the price of a certain shipping method based on the current order details?
 
 }
