01ea0f78fe58a81685e4d1b6cb133e0c3e9862a6
Jonathan 'Tivs' Rainey <tivoli@nurfed.com>
2015-06-22 18:42:06 -0700

    [#95930732] Add note validation in tests

--- test/specs/orders/notes.js
+++ test/specs/orders/notes.js
@@ -19,5 +19,9 @@ describe('Order Notes #POST', function() {
       res     = yield this.api.post('/orders/1/notes', data),
       note    = res.response;
     expect(res.status).to.equal(201);
+    expect(note.id).to.match(/^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i);
+    expect(note.createdAt).to.match(/\d{4}-[01]\d-[0-3]\dT[0-2]\d:[0-5]\d:[0-5]\d\.\d+([+-][0-2]\d:[0-5]\d|Z)/);
+    expect(note.body).to.equal('this is a note');
+    expect(note.author).to.be.an('object');
   });
 });
