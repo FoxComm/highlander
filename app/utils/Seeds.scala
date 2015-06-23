fc3759b41e514a7b3c6421182ffdd5bcd235c254
Adil Wali <adil@adil.io>
2015-06-22 18:39:59 -0700

    Updating seeder to seed ship rules and criteria.

--- app/utils/Seeds.scala
+++ app/utils/Seeds.scala
@@ -12,7 +12,10 @@ object Seeds {
   val today = new DateTime
 
   case class TheWorld(customer: Customer,order: Order, address: Address,
-                      storeAdmin: StoreAdmin)
+                      storeAdmin: StoreAdmin, shippingMethod: ShippingMethod,
+                       shippingPriceRule: ShippingPriceRule, shippingMethodRuleMapping: ShippingMethodPriceRule,
+                       orderCriterion: OrderCriterion, orderPriceCriterion: OrderPriceCriterion,
+                       priceRuleCriteriaMapping: ShippingPriceRuleOrderCriterion)
 
   def run(): Unit = {
     import scala.concurrent.ExecutionContext.Implicits.global
@@ -22,7 +25,13 @@ object Seeds {
       customer = Factories.customer,
       storeAdmin = Factories.storeAdmin,
       order = Factories.order,
-      address = Factories.address
+      address = Factories.address,
+      shippingMethod = Factories.shippingMethod,
+      shippingPriceRule = Factories.shippingPriceRule,
+      shippingMethodRuleMapping = Factories.shippingMethodRuleMapping,
+      orderCriterion = Factories.orderCriterion,
+      orderPriceCriterion = Factories.orderPriceCriterion,
+      priceRuleCriteriaMapping = Factories.priceRuleCriteriaMapping
     )
 
     val failures = List(s.customer.validate, s.storeAdmin.validate, s.order.validate, s.address.validate).
@@ -36,6 +45,9 @@ object Seeds {
       storeAdmin ← (StoreAdmins.returningId += s.storeAdmin).map(id => s.storeAdmin.copy(id = id))
       order ← Orders.save(s.order.copy(customerId = customer.id))
       address ← Addresses.save(s.address.copy(customerId = customer.id))
+      shippingMethod ← ShippingMethods.save(s.shippingMethod)
+      shippingPriceRule ← ShippingPriceRules.save(s.shippingPriceRule)
+      shippingMethodRuleMapping ← ShippingMethodsPriceRules.save(s.shippingMethodRuleMapping)
     } yield (customer, order, address)
 
     Await.result(actions.run(), 1.second)
@@ -46,11 +58,25 @@ object Seeds {
 
     def storeAdmin = StoreAdmin(email = "admin@admin.com", password = "password", firstName = "Frankly", lastName = "Admin")
 
+
     def order = Order(customerId = 0)
 
     def address =
       Address(customerId = 0, stateId = 1, name = "Home", street1 = "555 E Lake Union St.",
         street2 = None, city = "Seattle", zip = "12345")
+
+
+    def shippingMethod = ShippingMethod(adminDisplayName = "UPS Ground", storefrontDisplayName = "UPS Ground", defaultPrice = 10, isActive = true)
+
+    def shippingPriceRule = ShippingPriceRule(name = "Flat Shipping Over 20", ruleType = ShippingPriceRule.Flat, flatPrice = 99938, flatMarkup = 0)
+
+    def shippingMethodRuleMapping = ShippingMethodPriceRule(shippingMethodId = 1, shippingPriceRuleId = 1, ruleRank = 1)
+
+    def orderCriterion = OrderCriterion(name = "doesn'tmater")
+
+    def orderPriceCriterion = OrderPriceCriterion(id = 1, priceType = OrderPriceCriterion.SubTotal, greaterThan = Some(20), currency = "USD", exclude = false)
+
+    def priceRuleCriteriaMapping = ShippingPriceRuleOrderCriterion(orderCriterionId = 1, shippingPricingRuleId = 1)
 
   }
 }
