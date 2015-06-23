104117aa47305a206c27b62b7408bb4f1eb99640
Adil Wali <adil@adil.io>
2015-06-23 14:57:31 -0700

    Foldleft to save the day?

--- app/services/ShippingMethodsBuilder.scala
+++ app/services/ShippingMethodsBuilder.scala
@@ -27,52 +27,67 @@ object ShippingMethodsBuilder {
                                  (implicit ec: ExecutionContext,
                                    db: Database): Future[Seq[ShippingMethodWithPrice]] = {
     val baseMethods = availableShippingMethods(order)
-    baseMethods.flatMap{ _.map { shippingMethod =>
-      ShippingPriceRules.shippingPriceRulesForShippingMethod(shippingMethod.id).map{
-        _.map { sRule =>
+    baseMethods.flatMap { shippingMethods =>
+      val blah = shippingMethods.map { shippingMethod =>
+
+      ShippingPriceRules.shippingPriceRulesForShippingMethod(shippingMethod.id).flatMap { shippingRules =>
+        val x = shippingRules.map { sRule =>
           // TODO: AW: Come back and deal with SKU-specific criteria later.
-          ShippingPriceRulesOrderCriteria.criteriaForPricingRule(sRule.id).map {
-            _.map { oCriterion =>
-              oCriterion match {
-                case t: OrderPriceCriterion =>
-                  t.priceType match {
-                    case OrderPriceCriterion.GrandTotal =>
-                      val exactApplies = oCriterion.exactMatch.contains(order.grandTotal)
-                      val greaterApplies = oCriterion.greaterThan.exists(gThan => order.grandTotal >= gThan)
-                      val lessApplies = oCriterion.lessThan.exists(lThan => order.grandTotal >= lThan)
-                      if (exactApplies || greaterApplies || lessApplies) {
-                        ShippingMethodWithPrice(displayName = shippingMethod.storefrontDisplayName, estimatedTime = "Long Time", price = sRule.flatPrice)
-                      } else {
-                        ShippingMethodWithPrice(displayName = "donkey", estimatedTime = "FOREVER", price = 3333)
-                      }
-                    case OrderPriceCriterion.SubTotal =>
-                      val exactApplies = oCriterion.exactMatch.contains(order.subTotal)
-                      val greaterApplies = oCriterion.greaterThan.exists(gThan => order.subTotal >= gThan)
-                      val lessApplies = oCriterion.lessThan.exists(lThan => order.subTotal >= lThan)
-                      if (exactApplies || greaterApplies || lessApplies) {
-                        ShippingMethodWithPrice(displayName = shippingMethod.storefrontDisplayName, estimatedTime = "Long Time", price = sRule.flatPrice)
-                      } else {
-                        ShippingMethodWithPrice(displayName = "donkey", estimatedTime = "FOREVER", price = 3333)
-                      }
-                    case OrderPriceCriterion.GrandTotalLessShipping =>
-                      ShippingMethodWithPrice(displayName = "donkey", estimatedTime = "FOREVER", price = 3333)
-                    case OrderPriceCriterion.GrandTotalLessTax =>
-                      ShippingMethodWithPrice(displayName = "donkey", estimatedTime = "FOREVER", price = 3333)
-                    case _ => ShippingMethodWithPrice(displayName = "donkey", estimatedTime = "FOREVER", price = 3333)
-                  }
-                case _ =>
-                  ShippingMethodWithPrice(displayName = "donkey", estimatedTime = "FOREVER", price = 3333)//could not find inherited objects or case classes
-              }
+          ShippingPriceRulesOrderCriteria.criteriaForPricingRule(sRule.id).map { oCriteria =>
+            val matches = oCriteria.filter { oCriterion =>
+              criteriaMatchForShippingRule(oCriterion, order)
             }
+            val shippingPrice = if (matches.nonEmpty) {
+              sRule.flatPrice
+            } else {
+              shippingMethod.defaultPrice
+            }
+            ShippingMethodWithPrice(displayName = "donkey", estimatedTime = "FOREVER", price = shippingPrice)
           }
         }
 
+        Future.sequence(x)
       }
-      // This is where I want to assemble the actual price.
-      //ShippingMethodWithPrice(displayName = "donkey", estimatedTime = "FOREVER", price = 3333)
     }
+      val accum: Future[Seq[ShippingMethodWithPrice]] = Future.successful(Seq())
+      blah.foldLeft(accum) { case (ac, futureSeq) =>
+        futureSeq.flatMap { seq =>
+          ac.map { finalSeq =>
+            finalSeq ++ seq
+          }
+        }
+      }
+
+      // This is where I want to assemble the actual price.
+
     }
   }
   // What is the price of a certain shipping method based on the current order details?
 
+
+  def criteriaMatchForShippingRule(oCriterion: OrderPriceCriterion, order: Order): Boolean = {
+    oCriterion match {
+      case t: OrderPriceCriterion =>
+        t.priceType match {
+          case OrderPriceCriterion.GrandTotal =>
+            val exactApplies = oCriterion.exactMatch.contains(order.grandTotal)
+            val greaterApplies = oCriterion.greaterThan.exists(gThan => order.grandTotal >= gThan)
+            val lessApplies = oCriterion.lessThan.exists(lThan => order.grandTotal >= lThan)
+            (exactApplies || greaterApplies || lessApplies)
+          case OrderPriceCriterion.SubTotal =>
+            val exactApplies = oCriterion.exactMatch.contains(order.subTotal)
+            val greaterApplies = oCriterion.greaterThan.exists(gThan => order.subTotal >= gThan)
+            val lessApplies = oCriterion.lessThan.exists(lThan => order.subTotal >= lThan)
+            (exactApplies || greaterApplies || lessApplies)
+          case OrderPriceCriterion.GrandTotalLessShipping =>
+            false
+          case OrderPriceCriterion.GrandTotalLessTax =>
+            false
+          case _ =>
+            false
+        }
+      case _ =>
+        false //could not find inherited objects or case classes
+    }
+  }
 }
