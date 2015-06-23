849a929bc920735a5c72ce8427687a84df113992
Cameron Stitt <cameron@cam.st>
2015-06-22 22:27:16 -0700

    #95930672 Better innercontent and form setup.

--- src/themes/admin/components/addresses/new_address.jsx
+++ src/themes/admin/components/addresses/new_address.jsx
@@ -8,8 +8,9 @@ export default class NewAddress extends React.Component {
     super(props);
     this.state = {
       isActive: true
-    }
+    };
   }
+
   handleChanges(e) {
     let field = {},
       target = e.target;
@@ -27,41 +28,41 @@ export default class NewAddress extends React.Component {
 
   render() {
     return (
-      <form method='POST' onSubmit={this.onSubmitForm.bind(this)}>
+      <form className='vertical gutter' method='POST' onSubmit={this.onSubmitForm.bind(this)}>
         <div>
           <label htmlFor="name">Name</label>
-          <input type="text" name="name" onChange={this.handleChanges.bind(this)} required />
+          <input type="text" name="name" className='control' onChange={this.handleChanges.bind(this)} required />
         </div>
         <div>
           <label htmlFor="street1">Address 1</label>
-          <input type="text" name="street1" onChange={this.handleChanges.bind(this)} required />
+          <input type="text" name="street1" className='control' onChange={this.handleChanges.bind(this)} required />
         </div>
         <div>
           <label htmlFor="street2">Address 2 (option)</label>
-          <input type="text" name="street2" onChange={this.handleChanges.bind(this)} />
+          <input type="text" name="street2" className='control' onChange={this.handleChanges.bind(this)} />
         </div>
         <div>
           <label htmlFor="country">Country</label>
-          <input type="text" name="country" onChange={this.handleChanges.bind(this)} required />
+          <input type="text" name="country" className='control' onChange={this.handleChanges.bind(this)} required />
         </div>
         <div>
           <label htmlFor="state">State</label>
-          <input type="text" name="state" onChange={this.handleChanges.bind(this)} required />
+          <input type="text" name="state" className='control' onChange={this.handleChanges.bind(this)} required />
         </div>
         <div>
           <label htmlFor="city">City</label>
-          <input type="text" name="city" onChange={this.handleChanges.bind(this)} required />
+          <input type="text" name="city" className='control' onChange={this.handleChanges.bind(this)} required />
         </div>
         <div>
           <label htmlFor="zip">Zip</label>
-          <input type="number" name="zip" onChange={this.handleChanges.bind(this)} required />
+          <input type="number" name="zip" className='control' onChange={this.handleChanges.bind(this)} required />
         </div>
         <div>
           <label htmlFor="phone">Phone</label>
-          <input type="tel" name="phone" onChange={this.handleChanges.bind(this)} required />
+          <input type="tel" name="phone" className='control' onChange={this.handleChanges.bind(this)} required />
         </div>
         <div>
-          <button type='submit'>Submit</button>
+          <button type='submit' className='btn'>Submit</button>
         </div>
       </form>
     );
