a0531df30bcb609ac221522be159b709f47c25f8
Ferdinand Svehla <f.svehla@gmail.com>
2015-06-24 17:29:41 +0200

    WIP

--- test/util/DbTestSupport.scala
+++ test/util/DbTestSupport.scala
@@ -1,23 +1,45 @@
 package util
 
+import scala.annotation.tailrec
+
 import com.typesafe.config.ConfigFactory
 import org.flywaydb.core.Flyway
 import org.postgresql.ds.PGSimpleDataSource
-import org.scalatest.{Outcome, BeforeAndAfterAll, Suite, SuiteMixin}
+import org.scalatest.{BeforeAndAfterAll, Outcome, Suite, SuiteMixin}
 
-trait DbTestSupport extends SuiteMixin { this: Suite ⇒
+trait DbTestSupport extends SuiteMixin with BeforeAndAfterAll { this: Suite ⇒
   val api = slick.driver.PostgresDriver.api
   import api._
 
   implicit lazy val db = Database.forConfig("db.test")
 
-  override abstract protected def withFixture(test: NoArgTest): Outcome = {
+  override protected def beforeAll(): Unit = {
     val flyway = new Flyway
     flyway.setDataSource(jdbcDataSourceFromConfig("db.test"))
     flyway.setLocations("filesystem:./sql")
 
     flyway.clean()
     flyway.migrate()
+  }
+
+  override abstract protected def withFixture(test: NoArgTest): Outcome = {
+    val config  = jdbcDataSourceFromConfig("db.test")
+
+    val conn     = config.getConnection
+    val metaData = conn.getMetaData
+    val catalog  = conn.getCatalog
+    val tables   = metaData.getTables(catalog, "public", "%", Array("TABLE"))
+
+    @tailrec
+    def iterate(in: Seq[String]): Seq[String] = {
+      if (tables.next()) {
+        iterate(in :+ tables.getString(3))
+      } else {
+        in
+      }
+    }
+
+    println(iterate(Seq.empty[String]).filterNot(_.startsWith("pg_")))
 
     super.withFixture(test)
   }
@@ -32,4 +54,8 @@ trait DbTestSupport extends SuiteMixin { this: Suite ⇒
 
     source
   }
+}
+
+object DbTestSupport {
+
 }
