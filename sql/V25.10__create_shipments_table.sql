17add973daf13ce6f7e0f5c5306cb95ca4af4fea
Adil Wali <adil@adil.io>
2015-06-25 14:13:36 -0700

    [#97844796] Correcting camelcase mistake in migrations.  #4HrsSlp

--- sql/V25.10__create_shipments_table.sql
+++ sql/V25.10__create_shipments_table.sql
@@ -1,8 +1,8 @@
 create table shipments (
     id bigint primary key,
     order_id bigint not null,
-    shippingMethodId integer,
-    shippingAddressId integer,
+    shipping_method_id integer,
+    shipping_address_id integer,
     status character varying(255) not null,
     shipping_price integer,
     created_at timestamp without time zone default (now() at time zone 'utc'),
@@ -10,8 +10,8 @@ create table shipments (
     deleted_at timestamp without time zone null,
     foreign key (id) references inventory_events(id) on update restrict on delete restrict,
     foreign key (order_id) references orders(id) on update restrict on delete restrict,
-    foreign key (shippingMethodId) references shipping_methods(id) on update restrict on delete restrict,
-    foreign key (shippingAddressId) references addresses(id) on update restrict on delete restrict
+    foreign key (shipping_method_id) references shipping_methods(id) on update restrict on delete restrict,
+    foreign key (shipping_address_id) references addresses(id) on update restrict on delete restrict
 );
 
 create trigger set_inventory_id_trigger
