0f8a14c219bb30ec8b7b6c0ae7c99ae8aded5b21
Cameron Stitt <cameron@cam.st>
2015-06-25 10:38:44 -0700

    Progress [#96835078]. Edit deal details. Freeze remorse period.

--- src/themes/admin/components/countdown/countdown.jsx
+++ src/themes/admin/components/countdown/countdown.jsx
@@ -2,15 +2,19 @@
 
 import React from 'react';
 import moment from 'moment';
+import { listenTo, stopListeningTo } from '../../lib/dispatcher';
 
 const zeroTime = '00:00:00';
+const editEvent = 'toggle-order-edit';
 
-class Countdown extends React.Component {
+export default class Countdown extends React.Component {
   constructor(props) {
     super(props);
+    this.onToggleOrderEdit = this.onToggleOrderEdit.bind(this);
     this.state = {
       difference: zeroTime,
-      endDate: props.endDate
+      endDate: props.endDate,
+      frozen: false
     };
   }
 
@@ -28,6 +32,7 @@ class Countdown extends React.Component {
   stopInterval() {
     if (this.interval) {
       clearInterval(this.interval);
+      this.interval = null;
     }
   }
 
@@ -46,18 +51,37 @@ class Countdown extends React.Component {
   }
 
   componentDidMount() {
+    listenTo(editEvent, this);
+    this.tick();
     this.startInterval();
   }
 
   componentWillUnmount() {
+    stopListeningTo(editEvent, this);
     this.stopInterval();
   }
 
+  onToggleOrderEdit() {
+    this.setState({
+      frozen: !this.state.frozen
+    });
+  }
+
   render() {
+    let
+      rightContent = null;
+
+    if (this.state.frozen) {
+      this.stopInterval();
+      rightContent = <span>Frozen while editing.</span>;
+    } else {
+      this.startInterval();
+      rightContent = <button className='btn' onClick={this.addTime.bind(this, 15, 'm')}>+15</button>;
+    }
     return (
       <div className='countdown'>
         <span>{this.state.difference}</span>
-        <button className='btn' onClick={this.addTime.bind(this, 15, 'm')}>+15</button>
+        {rightContent}
       </div>
     );
   }
@@ -70,5 +94,3 @@ Countdown.propTypes = {
 Countdown.defaultProps = {
   endDate: moment.utc().add(3, 'h').format()
 };
-
-export default Countdown;
