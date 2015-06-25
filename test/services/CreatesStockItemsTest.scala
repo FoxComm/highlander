fe367b8ce8cd429850a37c8e5dd6e1ee81dcd76f
Ferdinand Svehla <f.svehla@gmail.com>
2015-06-25 14:28:15 +0200

    Fix CreatesStockItemsTest

--- test/services/CreatesStockItemsTest.scala
+++ test/services/CreatesStockItemsTest.scala
@@ -17,7 +17,7 @@ class CreatesStockItemsTest extends IntegrationTestBase {
       val result = CreatesStockItems(productId = 1, onHand = -1, onHold = 0).futureValue
       val error  = result.swap.get
 
-      error mustBe "On hand must be >= 0"
+      error mustBe List(GeneralFailure("On hand must be >= 0"))
     }
   }
 
