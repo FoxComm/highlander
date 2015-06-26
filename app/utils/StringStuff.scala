22172b292fd40c23ceadce6853708f62f9927b54
Jacques Fuentes <jpfuentes2@gmail.com>
2015-06-25 23:34:16 -0400

    StringsStuff -> Strings

--- app/utils/StringStuff.scala
+++
@@ -1,9 +0,0 @@
-package utils
-
-// Yax --> :trollface: for you.  TODO: Name this better
-object StringStuff {
-
-  def lowerCaseFirstLetter(s:String) = s(0).toLower + s.substring(1)
-
-  def upperCaseFirstLetter(s:String) = s(0).toUpper + s.substring(1)
-}
