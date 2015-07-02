6642b7ddba4d9984baea05095714f9fbd42102fe
Jacques Fuentes <jpfuentes2@gmail.com>
2015-07-02 16:24:24 -0400

    Add created_at default timestamp to GC adjustments

--- sql/V28__create_gift_card_adjustments.sql
+++ sql/V28__create_gift_card_adjustments.sql
@@ -5,6 +5,7 @@ create table gift_card_adjustments (
     credit integer not null default 0,
     debit integer not null default 0,
     capture boolean not null default false,
+    created_at timestamp without time zone default (now() at time zone 'utc'),
     foreign key (gift_card_id) references gift_cards(id) on update restrict on delete restrict,
     -- both credit/debit are unsigned (never negative) and only one can be > 0
     constraint valid_entry check ((credit >= 0 and debit >= 0) and (credit > 0 or debit > 0) and
