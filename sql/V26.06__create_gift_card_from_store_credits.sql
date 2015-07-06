59859f2e3f0d99a5fc3546fcd349368125663d63
Jacques Fuentes <jpfuentes2@gmail.com>
2015-07-06 16:26:06 -0400

    Add GC/SC conversion tables

---
+++ sql/V26.06__create_gift_card_from_store_credits.sql
@@ -0,0 +1,14 @@
+create table gift_cards_from_store_credits (
+    id integer primary key,
+    store_credit_id integer not null,
+    created_at timestamp without time zone default (now() at time zone 'utc'),
+    foreign key (id) references gift_card_origins(id) on update restrict on delete restrict,
+    foreign key (store_credit_id) references store_credits(id) on update restrict on delete restrict
+);
+
+create trigger set_gift_cards_from_store_credits_id
+    before insert
+    on gift_cards_from_store_credits
+    for each row
+    execute procedure set_gift_card_origin_id();
+
