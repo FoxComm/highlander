fbff831074703c1a8eb646b2eb4cf06d997b2c91
Jacques Fuentes <jpfuentes2@gmail.com>
2015-07-06 16:47:34 -0400

    rm defunct models and extract all from app/Address

--- app/Address.scala
+++ app/Address.scala
@@ -1,95 +1,24 @@
-import java.util.Date
-import java.util.concurrent.TimeoutException
-import akka.http.scaladsl.Http.ServerBinding
-import akka.http.scaladsl.model.headers.BasicHttpCredentials
-import akka.stream.ActorMaterializer
-import com.stripe.model.Token
-import com.stripe.net.{RequestOptions => StripeRequestOptions}
-import com.stripe.model.{Charge => StripeCharge}
-import com.stripe.Stripe
-import de.heikoseeberger.akkahttpjson4s.Json4sSupport
-import models.Order.{FulfillmentStarted, PartiallyShipped}
-
-import org.scalactic._
-import slick.driver.PostgresDriver
-import slick.driver.PostgresDriver.api._
+import scala.concurrent.{ExecutionContext, Future}
 import akka.actor.ActorSystem
-import akka.http.scaladsl.Http
-import akka.http.scaladsl.marshalling.ToResponseMarshallable
-import akka.http.scaladsl.model.{StatusCodes, HttpResponse}
-import com.typesafe.config.{ConfigFactory, Config}
-import com.wix.accord._
-import com.wix.accord.{validate => runValidation, Success => ValidationSuccess, Failure => ValidationFailure}
-import dsl.{validator => createValidator}
-import dsl._
 import akka.event.Logging
-import slick.lifted.ProvenShape
+import akka.http.scaladsl.Http
+import akka.http.scaladsl.Http.ServerBinding
 import akka.http.scaladsl.model.StatusCodes._
+import akka.http.scaladsl.model.{HttpResponse, StatusCodes}
 import akka.http.scaladsl.server.Directives._
-import slick.lifted.Tag
-import de.heikoseeberger.akkahttpjson4s.Json4sSupport._
+import akka.stream.ActorMaterializer
 
-import org.json4s.JsonAST.JString
-import org.json4s.{jackson, JValue, CustomSerializer, DefaultFormats}
-import org.json4s.jackson.Serialization.{write => render}
-import org.json4s.jackson.JsonMethods._
-import scala.concurrent.{ExecutionContext, Future, Await}
-import scala.concurrent.duration._
-import scala.util.{Try, Failure, Success}
-import collection.JavaConversions.mapAsJavaMap
-import akka.http.scaladsl.server.directives._
-
-import utils.{RichTable, Validation, Strings}
-import utils.RunOnDbIO
+import com.typesafe.config.{Config, ConfigFactory}
+import de.heikoseeberger.akkahttpjson4s.Json4sSupport
 import models._
+import org.json4s.jackson
+import org.json4s.jackson.Serialization.{write ⇒ render}
+import org.scalactic._
 import payloads._
 import responses.{FullOrder, PublicSku}
 import services._
-
-final case class Store(id: Int, name: String, Configuration: StoreConfiguration)
-
-//  This is a super quick placeholder for store configuration.  We will want to blow this out later.
-// TODO: Create full configuration data model
-final case class StoreConfiguration(id: Int, storeId: Int, PaymentGateway: PaymentGateway)
-
-final case class StockLocation(id: Int, name: String)
-
-// TODO: money/currency abstraction. Use joda-money, most likely
-final case class Money(currency: String, amount: Int)
-
-final case class Coupon(id: Int, orderId: Int, code: String, adjustment: List[Adjustment])
-
-final case class Promotion(id: Int, orderId: Int, adjustments: List[Adjustment])
-
-sealed trait Destination
-final case class EmailDestination(email: String) extends Destination
-final case class ResidenceDestination(address: Address) extends Destination
-final case class StockLocationDestination(stockLocation: StockLocation) extends Destination
-
-final case class Fulfillment(id: Int, destination: Destination)
-
-final case class Archetype(id: Int, name: String) extends Validation[Archetype] {
-  override def validator = createValidator[Archetype] { a =>
-    a.name is notEmpty
-  }
-}
-
-final case class Catalog(id: Int, name: String,
-                   archetypes: Seq[Archetype], products: Seq[Product]) extends Validation[Catalog] {
-  override def validator = createValidator[Catalog] { c =>
-    c.name is notEmpty
-  }
-}
-
-final case class Product(id: Int, name: String, sku: String, archetypes: Seq[Archetype])
-
-final case class Asset(id: Int, url: String, rank: Int)
-
-final case class Collection(id: Int, name: String, isActive: Boolean) extends Validation[Collection] {
-  override def validator = createValidator[Collection] { c =>
-    c.name is notEmpty
-  }
-}
+import slick.driver.PostgresDriver.api._
+import utils.RunOnDbIO
 
 object Main {
   def main(args: Array[String]): Unit = {
