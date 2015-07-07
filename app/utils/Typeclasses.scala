c7957bd4086aaa456ae572217e3f5cd4d41b4058
Ferdinand Svehla <f.svehla@gmail.com>
2015-07-07 19:35:05 +0200

    Suppress warnings for warts.Any
    This is in general very useful

--- app/utils/Typeclasses.scala
+++ app/utils/Typeclasses.scala
@@ -26,6 +26,10 @@ trait ADT[F] extends Read[F] with Show[F] { self ⇒
 
   override def shows(f: F): String = f.toString.lowerCaseFirstLetter
 
+  /**
+   * Json4s works by matching types against Any at runtime so we need to support these features.
+   */
+  @SuppressWarnings(Array("org.brianmckenna.wartremover.warts.Any", "org.brianmckenna.wartremover.warts.IsInstanceOf"))
   def jsonFormat(implicit m: Manifest[F]): CustomSerializer[F] = new CustomSerializer[F](format => ({
     case JString(str) ⇒ read(str).get // if we cannot deserialize then we throw. Yes, I know it's not *pure*.
   }, {
