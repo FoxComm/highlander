8301e40ac1db0868cc921ba4617982bdcb401bc6
Jacques Fuentes <jpfuentes2@gmail.com>
2015-07-15 16:51:54 -0400

    Add timestamps to notes

--- sql/V18__create_order_notes_table.sql
+++ sql/V18__create_order_notes_table.sql
@@ -4,6 +4,8 @@ create table notes (
     reference_id integer not null,
     reference_type character varying(255) not null,
     text character varying(1000) not null,
+    created_at timestamp without time zone default (now() at time zone 'utc'),
+    deleted_at timestamp without time zone null,
     foreign key (store_admin_id) references store_admins(id) on update restrict on delete restrict
 );
 
