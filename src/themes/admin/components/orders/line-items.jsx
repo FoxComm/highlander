8b74830598be6ae0a74892805b60311f793866a3
Cameron Stitt <cameron@cam.st>
2015-07-22 09:50:59 +1000

    Remove uncessary changes for line items.

--- src/themes/admin/components/orders/line-items.jsx
+++ src/themes/admin/components/orders/line-items.jsx
@@ -3,78 +3,10 @@
 import React from 'react';
 import TableHead from '../tables/head';
 import TableBody from '../tables/body';
-import LineItemManager from './line-item-manager';
-import DeleteLineItem from './delete-line-item';
-import Typeahead from '../typeahead/typeahead';
-import SkuResult from './sku-result';
-import SkuStore from './sku-store';
-import { dispatch, listenTo, stopListeningTo } from '../../lib/dispatcher';
-
-const defaultColumns = [
-  {field: 'image', text: 'Image', type: 'image'},
-  {field: 'name', text: 'Name'},
-  {field: 'skuId', text: 'SKU'},
-  {field: 'price', text: 'Price', type: 'currency'},
-  {field: 'quantity', text: 'Quantity'},
-  {field: 'total', text: 'Total', type: 'currency'},
-  {field: 'status', text: 'Shipping Status'}
-];
-
-const editColumns = [
-  {field: 'id', text: 'Select', type: 'checkbox'},
-  {field: 'image', text: 'Image', type: 'image'},
-  {field: 'name', text: 'Name'},
-  {field: 'skuId', text: 'SKU'},
-  {field: 'price', text: 'Price', type: 'currency'},
-  {field: 'lineItem', text: 'Quantity', component: 'LineItemManager'},
-  {field: 'total', text: 'Total', type: 'currency'},
-  {field: 'status', text: 'Shipping Status'},
-  {field: 'delete', text: 'Delete', component: 'DeleteLineItem'}
-];
 
 export default class OrderLineItems extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-
-  onAddLineItem(sku) {
-    dispatch('updateLineItem', [{'skuId': sku.skuId, 'quantity': 1}]);
-  }
-
-  componentDidMount() {
-    listenTo('add-line-item', this);
-  }
-
-  componentWillUnmount() {
-    stopListeningTo('add-line-item', this);
-  }
-
   render() {
-    let
-      order     = this.props.order,
-      isEditing = this.props.isEditing,
-      columns   = this.props.tableColumns,
-      body      = null,
-      addItem   = null;
-
-    if (isEditing) {
-      columns = editColumns;
-      body = (
-        <TableBody columns={columns} rows={order.lineItems} model='order'>
-          <LineItemManager />
-          <DeleteLineItem />
-        </TableBody>
-      );
-      addItem = (
-        <div className="add-item">
-          <strong>Add Item</strong>
-          <Typeahead component={SkuResult} store={SkuStore} selectEvent="addLineItem" />
-        </div>
-      );
-    } else {
-      columns = defaultColumns;
-      body = <TableBody columns={columns} rows={order.lineItems} model='order'/>;
-    }
+    let order = this.props.order;
 
     return (
       <section id="order-line-items">
@@ -83,7 +15,6 @@ export default class OrderLineItems extends React.Component {
           <TableHead columns={this.props.tableColumns}/>
           <TableBody columns={this.props.tableColumns} rows={order.lineItems} model='order'/>
         </table>
-        {addItem}
       </section>
     );
   }
