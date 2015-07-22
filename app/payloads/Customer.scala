bd611ec957060041a0b74c25c0ca524e8db6fe02
Jacques Fuentes <jpfuentes2@gmail.com>
2015-07-22 10:54:34 -0400

    Remove unused case class

--- app/payloads/Customer.scala
+++ app/payloads/Customer.scala
@@ -2,8 +2,5 @@ package payloads
 
 final case class CreateCustomer(email: String, password: String, firstName: String, lastName: String) {}
 
-final case class UpdateCustomer(email: Option[String], password: Option[String], firstName: Option[String],
-  lastName: Option[String], phoneNumber: Option[String])
-
 final case class ToggleCustomerDisabled(disabled: Boolean)
 
