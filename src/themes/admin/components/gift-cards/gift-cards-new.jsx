ea27664fc512eb4a0c949e5dc63b3c55b987b956
Cameron Stitt <cameron@cam.st>
2015-07-24 13:17:32 +1000

    WIP of sub type display.

--- src/themes/admin/components/gift-cards/gift-cards-new.jsx
+++ src/themes/admin/components/gift-cards/gift-cards-new.jsx
@@ -2,20 +2,60 @@
 
 import React from 'react';
 
+const
+  types = ['Appeasement', 'Marketing'],
+  subTypes = [
+    [],
+    ['One', 'Two']
+  ];
+
 export default class NewGiftCard extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      amount: '0.00',
+      type: types[0]
+    };
+  }
+
   onChangeValue(event) {
     this.setState({
-      value: event.target.value
+      amount: event.target.value
     });
   }
 
   setValue(value) {
     this.setState({
-      value: value
+      amount: value
+    });
+  }
+
+  setType(event) {
+    this.setState({
+      type: types[+event.target.value]
     });
   }
 
   render() {
+    let
+      subTypeContent = null,
+      typeIdx        = types.indexOf(this.state.type),
+      typesSubTypes  = null;
+
+    if (typeIdx > -1) {
+      typeSubTypes = subTypes[typeIdx];
+      subTypeContent = (
+        <div>
+          <label htmlFor="cardSubType">Subtype</label>
+          <select name="cardSubType">
+            {typeSubTypes.map((subType, idx) => {
+              return <option val={subType}>{subType}</option>;
+             })};
+          </select>
+        </div>
+      );
+    }
+
     return (
       <div id="new-gift-card" className="gutter">
         <h2>New Gift Cards</h2>
@@ -23,17 +63,18 @@ export default class NewGiftCard extends React.Component {
           <fieldset>
             <fieldset>
               <label htmlFor="cardType">Gift Card Type</label>
-              <select name="cardType">
+              <select name="cardType" onChange={this.setType.bind(this)}>
+                {types.map((type, idx) => {
+                  return <option val={idx} key={`${idx}-${type}`}>{type}</option>;
+                })}
               </select>
-              <label htmlFor="cardSubType">Subtype</label>
-              <select name="cardSubType">
-              </select>
+              {subTypeContent}
             </fieldset>
             <fieldset>
               <label htmlFor="value">Value</label>
               <div className="form-icon">
                 <i className="icon-dollar"></i>
-                <input type="number" name="value" value={this.state.value} onChange={this.onChangeValue.bind(this)} />
+                <input type="number" name="value" value={this.state.amount} onChange={this.onChangeValue.bind(this)} />
               </div>
               <div>
                 <a className="btn" onClick={this.setValue.bind(this, '10.00')}>$10</a>
