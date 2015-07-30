e342ab68b31fdc9b11c75a1abc2d75e3e6a32c9e
Jacques Fuentes <jpfuentes2@gmail.com>
2015-07-30 18:11:11 -0400

    Refactor creating shipping addresses

--- app/services/OrderUpdater.scala
+++ app/services/OrderUpdater.scala
@@ -3,7 +3,6 @@ package services
 import models._
 import payloads.{CreateShippingAddress, UpdateOrderPayload}
 import slick.dbio.Effect.All
-import slick.driver.PostgresDriver
 
 import utils.Validation.Result.{Failure ⇒ Invalid, Success}
 import org.scalactic._
@@ -11,8 +10,6 @@ import scala.concurrent.{Future, ExecutionContext}
 import slick.driver.PostgresDriver.api._
 import slick.driver.PostgresDriver.backend.{DatabaseDef => Database}
 import responses.{Addresses ⇒ Response}
-import slick.driver.PostgresDriver.api._
-
 
 object OrderUpdater {
 
@@ -23,14 +20,14 @@ object OrderUpdater {
       _ <- Orders.insertOrUpdate(newOrder)
       updatedOrder <- Orders.findById(order.id)
     } yield (updatedOrder)
-    
+
     db.run(insertedQuery).map { optOrder =>
       optOrder match {
         case Some(orderExists) => Good(orderExists)
         case None => Bad(List("Not able to update order"))
       }
     }
-    
+
   }
 
   def createNote = "Note"
@@ -39,9 +36,12 @@ object OrderUpdater {
     (implicit db: Database, ec: ExecutionContext): Future[responses.Addresses.Root Or Failure] = {
 
     (payload.addressId, payload.address) match {
-      case (Some(addressId), _) ⇒ createShippingAddressFromAddressId(addressId, order.id)
-      case (None, Some(payloadAddress)) ⇒ createShippingAddressFromPayload(Address.fromPayload(payloadAddress), order)
-      case (None, None) ⇒ Future.successful(Bad(GeneralFailure("Not able to create the shipping address")))
+      case (Some(addressId), _) ⇒
+        createShippingAddressFromAddressId(addressId, order.id)
+      case (None, Some(payloadAddress)) ⇒
+        createShippingAddressFromPayload(Address.fromPayload(payloadAddress), order)
+      case (None, None) ⇒
+        Future.successful(Bad(GeneralFailure("Not able to create the shipping address")))
     }
   }
 
@@ -65,17 +65,16 @@ object OrderUpdater {
   private def createShippingAddressFromAddressId(addressId: Int, orderId: Int)
     (implicit db: Database, ec: ExecutionContext): Future[responses.Addresses.Root Or Failure] = {
 
-    val noState: DBIOAction[Option[State], NoStream, All] = DBIO.successful((None))
-    val noAddress: DBIOAction[Option[OrderShippingAddress], NoStream, All] = DBIO.successful((None))
-
     db.run(for {
-      newAddress: Option[Address] ← Addresses.findById(addressId)
-      state ← newAddress.fold(noState)((a: Address) ⇒ States.findById(a.stateId))
-      _ ← newAddress.fold(noAddress)(OrderShippingAddresses.copyFromAddress(_, orderId).map(Some(_)))
-    } yield (newAddress, state)).map {
-      case (Some(address: Address), Some(state: State)) ⇒ Good(Response.build(address, state))
-      case (None, _) ⇒ Bad(NotFoundFailure(Address, addressId))
-      case (Some(address: Address), None) ⇒ Bad(NotFoundFailure(State, address.stateId))
+      address ← Addresses.findById(addressId)
+      state ← address.map { a ⇒ States.findById(a.stateId) }.getOrElse(DBIO.successful(None))
+      newAddress ← address.map(OrderShippingAddresses.copyFromAddress(_, orderId).map(Some(_))).
+        getOrElse(DBIO.successful(None))
+    } yield (address, state, newAddress)).map {
+      case (Some(address), Some(state), Some(_)) ⇒
+        Good(Response.build(address, state))
+      case _ ⇒
+        Bad(NotFoundFailure(Address, addressId))
     }
   }
 }
