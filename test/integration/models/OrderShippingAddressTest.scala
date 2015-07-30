8851a6b0db087c27e27b4eeec5f13674507a221c
Jeff Mataya <jeff@foxcommerce.com>
2015-07-29 18:44:47 -0700

    Actually testing OrderShippingAddress uniqueness
    - Add in a models integration test to verify that an order may have at
      most one shipping address

--- test/integration/models/OrderShippingAddressTest.scala
+++ test/integration/models/OrderShippingAddressTest.scala
@@ -1,23 +1,29 @@
 package models
 
+import com.wix.accord.{Failure ⇒ ValidationFailure, Success ⇒ ValidationSuccess}
+import services.GeneralFailure
 import util.IntegrationTestBase
 import utils.Seeds.Factories
+import utils.jdbc.withUniqueConstraint
 
 class OrderShippingAddressTest extends IntegrationTestBase {
   import concurrent.ExecutionContext.Implicits.global
 
   "OrderShippingAddress" - {
-//    "has only one default per customer" in new Fixture {
-//      val anotherDefault = OrderShippingAddresses.createFromAddress(address, isDefault = true).run().failed.futureValue
-//      anotherDefault.getMessage must include ("violates unique constraint \"shipping_addresses_default_idx\"")
-//    }
+    "has only one shipping address per order" in new Fixture {
+      val result = withUniqueConstraint {
+        OrderShippingAddresses.save(shippingAddress.copy(name = "Yax2")).run()
+      } { notUnique ⇒ GeneralFailure("There was already a shipping address") }
+
+      result.futureValue mustBe 'bad
+    }
   }
 
   trait Fixture {
-//    val (customer, address, shippingAddress) = (for {
-//      customer ← Customers.save(Factories.customer)
-//      address ← Addresses.save(Factories.address.copy(customerId = customer.id))
-//      shippingAddress ← OrderShippingAddresses.createFromAddress(address, isDefault = true)
-//    } yield (customer, address, shippingAddress)).run().futureValue
+    val (order, shippingAddress) = (for {
+      customer ← Customers.save(Factories.customer)
+      order ← Orders.save(Factories.order.copy(customerId = customer.id))
+      shippingAddress ← OrderShippingAddresses.save(Factories.shippingAddress.copy(orderId = order.id))
+    } yield (order, shippingAddress)).run().futureValue
   }
 }
