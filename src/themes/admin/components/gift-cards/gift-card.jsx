59252cf75d122fc3fa3cd1874d08be9b3e2d46d0
Cameron Stitt <cameron@cam.st>
2015-07-30 11:59:54 +1000

    Displaying of more details. Progress [#98832274].

--- src/themes/admin/components/gift-cards/gift-card.jsx
+++ src/themes/admin/components/gift-cards/gift-card.jsx
@@ -5,6 +5,7 @@ import { RouteHandler } from 'react-router';
 import Api from '../../lib/api';
 import { Link } from 'react-router';
 import { formatCurrency } from '../../lib/format';
+import moment from 'moment';
 
 export default class GiftCard extends React.Component {
   constructor(props) {
@@ -53,14 +54,46 @@ export default class GiftCard extends React.Component {
         </div>
         <div className="gutter grid-row grid-gutter grid-match">
           <div className="grid-unit col-1">
-            <article className="panel featured">
-              <span>Available Balance</span>
-              <strong>{ formatCurrency(card.availableBalance) }</strong>
+            <article className="panel featured available-balance">
+              <header>Available Balance</header>
+              <p>{ formatCurrency(card.availableBalance) }</p>
             </article>
           </div>
           <div className="grid-unit col-3">
             <article className="panel">
               <p>Blah</p>
+            </article>
+          </div>
+        </div>
+        <div className="gutter grid-row grid-gutter grid-match row-5">
+          <div className="grid-unit col-1">
+            <article className="panel featured">
+              <header>Original Balance</header>
+              <p>{ formatCurrency(card.balance) }</p>
+            </article>
+          </div>
+          <div className="grid-unit col-1">
+            <article className="panel featured">
+              <header>Current Balance</header>
+              <p>{ formatCurrency(card.currentBalance) }</p>
+            </article>
+          </div>
+          <div className="grid-unit col-1">
+            <article className="panel featured">
+              <header>Date/Time Issued</header>
+              <p>{ moment(card.date).format('MM/DD/YYYY HH:mm:ss') }</p>
+            </article>
+          </div>
+          <div className="grid-unit col-1">
+            <article className="panel featured">
+              <header>Gift Card Type</header>
+              <p>{ card.type }</p>
+            </article>
+          </div>
+          <div className="grid-unit col-1">
+            <article className="panel featured">
+              <header>Current State</header>
+              <p>{ card.state }</p>
             </article>
           </div>
         </div>
