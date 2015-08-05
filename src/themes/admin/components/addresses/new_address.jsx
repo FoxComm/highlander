0e80b573760f749afd47c977673006c35af65458
Cameron Stitt <cameron@cam.st>
2015-08-05 17:00:14 +1000

    Add state model. Update new address.

--- src/themes/admin/components/addresses/new_address.jsx
+++ src/themes/admin/components/addresses/new_address.jsx
@@ -3,30 +3,34 @@
 import React from 'react';
 import AddressStore from './store';
 import { dispatch } from '../../lib/dispatcher';
+import Api from '../../lib/api';
 
 export default class NewAddress extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      isActive: true,
-      country: 'US',
-      state: 'CA'
+      states: []
     };
   }
 
-  handleChanges(e) {
-    let
-      field = {},
-      target = e.target;
-    field[target.name] = target.value;
-    this.setState(field);
+  componentDidMount() {
+    Api.get('/states')
+       .then((res) => {
+         this.setState({
+           states: res
+         });
+       })
+       .catch((err) => { console.log(err); });
   }
 
-  onSubmitForm(e) {
-    e.preventDefault();
+  onSubmitForm(event) {
+    event.preventDefault();
 
-    // Todo (cam-stitt): Alter this.state to be json of form.
-    AddressStore.create(this.state);
+    Api.submitForm(event.target)
+       .then((res) => {
+         AddressStore.update(res);
+       })
+       .catch((err) => { console.log(err); });
   }
 
   cancelAddress() {
@@ -35,46 +39,42 @@ export default class NewAddress extends React.Component {
 
   render() {
     return (
-      <form className='vertical' method='POST' onSubmit={this.onSubmitForm.bind(this)}>
+      <form action={`/customers/addresses`} className='vertical' method='POST' onSubmit={this.onSubmitForm.bind(this)}>
         <div>
           <label htmlFor="name">Name</label>
-          <input type="text" name="name" className='control' onChange={this.handleChanges.bind(this)} required />
+          <input type="text" name="name" className='control' required />
         </div>
         <div>
           <label htmlFor="street1">Address 1</label>
-          <input type="text" name="street1" className='control' onChange={this.handleChanges.bind(this)} required />
+          <input type="text" name="street1" className='control' required />
         </div>
         <div>
           <label htmlFor="street2">Address 2 (option)</label>
-          <input type="text" name="street2" className='control' onChange={this.handleChanges.bind(this)} />
-        </div>
-        <div>
-          <label htmlFor="country">Country</label>
-          <select name='country' onChange={this.handleChanges.bind(this)}>
-            <option val='US'>United States</option>
-          </select>
+          <input type="text" name="street2" className='control' />
         </div>
         <div>
           <label htmlFor="state">State</label>
-          <select name='state' onChange={this.handleChanges.bind(this)}>
-            <option val='CA'>California</option>
+          <select name='stateId'>
+            {this.state.states.map((state) => {
+              return <option value={state.id}>{state.name}</option>;
+             })}
           </select>
         </div>
         <div>
           <label htmlFor="city">City</label>
-          <input type="text" name="city" className='control' onChange={this.handleChanges.bind(this)} required />
+          <input type="text" name="city" className='control' required />
         </div>
         <div>
           <label htmlFor="zip">Zip</label>
-          <input type="number" name="zip" className='control' onChange={this.handleChanges.bind(this)} required />
+          <input type="number" name="zip" className='control' required />
         </div>
         <div>
           <label htmlFor="phone">Phone</label>
-          <input type="tel" name="phone" className='control' onChange={this.handleChanges.bind(this)} required />
+          <input type="tel" name="phone" className='control' required />
         </div>
         <div>
           <a onClick={this.cancelAddress}>Cancel</a>
-          <button type='submit' className='btn'>Submit</button>
+          <input type='submit' className='btn' value="Submit" />
         </div>
       </form>
     );
