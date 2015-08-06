62ba9ccc7f49fff34bf175755cf13075c91f7bf2
Cameron Stitt <cameron@cam.st>
2015-08-06 11:05:05 +1000

    Create addresses via stitch. Progress [#100358480].

--- src/themes/admin/components/addresses/new_address.jsx
+++ src/themes/admin/components/addresses/new_address.jsx
@@ -4,20 +4,26 @@ import React from 'react';
 import AddressStore from './store';
 import { dispatch } from '../../lib/dispatcher';
 import Api from '../../lib/api';
+import _ from 'underscore';
 
 export default class NewAddress extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      states: []
+      states: [],
+      formData: {}
     };
   }
 
   componentDidMount() {
+    let formData = _.extend({}, this.state.formData);
+
     Api.get('/states')
        .then((res) => {
+         formData.stateId = res[0].id;
          this.setState({
-           states: res
+           states: res,
+           formData: formData
          });
        })
        .catch((err) => { console.log(err); });
@@ -26,11 +32,27 @@ export default class NewAddress extends React.Component {
   onSubmitForm(event) {
     event.preventDefault();
 
-    Api.submitForm(event.target)
+    Api.post(event.target.getAttribute('action'), this.state.formData)
        .then((res) => {
          AddressStore.update(res);
        })
        .catch((err) => { console.log(err); });
+  }
+
+  onChangeValue(event) {
+    let
+      formData = _.extend({}, this.state.formData),
+      target = event.target;
+
+    if (target.name === 'stateId') {
+      formData[target.name] = +target.value;
+    } else {
+      formData[target.name] = target.value;
+    }
+
+    this.setState({
+      formData: formData
+    });
   }
 
   cancelAddress() {
@@ -39,7 +61,7 @@ export default class NewAddress extends React.Component {
 
   render() {
     return (
-      <form action={`/customers/addresses`} className='vertical' method='POST' onSubmit={this.onSubmitForm.bind(this)}>
+      <form action={`/customers/${this.props.customerId}/addresses`} className='vertical' method='POST' onSubmit={this.onSubmitForm.bind(this)} onChange={this.onChangeValue.bind(this)}>
         <div>
           <label htmlFor="name">Name</label>
           <input type="text" name="name" className='control' required />
@@ -53,8 +75,8 @@ export default class NewAddress extends React.Component {
           <input type="text" name="street2" className='control' />
         </div>
         <div>
-          <label htmlFor="state">State</label>
-          <select name='stateId'>
+          <label htmlFor="stateId">State</label>
+          <select name="stateId">
             {this.state.states.map((state) => {
               return <option value={state.id}>{state.name}</option>;
              })}
@@ -66,11 +88,11 @@ export default class NewAddress extends React.Component {
         </div>
         <div>
           <label htmlFor="zip">Zip</label>
-          <input type="number" name="zip" className='control' required />
+          <input type="text" name="zip" className='control' required />
         </div>
         <div>
-          <label htmlFor="phone">Phone</label>
-          <input type="tel" name="phone" className='control' required />
+          <label htmlFor="phoneNumber">Phone</label>
+          <input type="tel" name="phoneNumber" className='control' required />
         </div>
         <div>
           <a onClick={this.cancelAddress}>Cancel</a>
@@ -80,3 +102,7 @@ export default class NewAddress extends React.Component {
     );
   }
 }
+
+NewAddress.propTypes = {
+  customerId: React.PropTypes.number
+};
