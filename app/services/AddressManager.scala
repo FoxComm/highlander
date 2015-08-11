71da9a232d02c567d75ad9b834ea32fa85ee3bae
Jacques Fuentes <jpfuentes2@gmail.com>
2015-08-10 17:15:16 -0700

    Edit address in customer's address book
    [Finish #100054132]

--- app/services/AddressManager.scala
+++ app/services/AddressManager.scala
@@ -28,6 +28,28 @@ object AddressManager {
     }
   }
 
+  def edit(addressId: Int, customerId: Int, payload: CreateAddressPayload)
+    (implicit ec: ExecutionContext, db: Database): Future[Root Or Failures] = {
+    val address = Address.fromPayload(payload).copy(customerId = customerId, id = addressId)
+    address.validate match {
+      case Success ⇒
+        db.run((for {
+          rowsAffected ← Addresses.insertOrUpdate(address)
+          state ← States.findById(address.stateId)
+        } yield (rowsAffected, address, state)).transactionally).map {
+          case (n, address, Some(state)) ⇒
+            if (n == 1) {
+              Good(Response.build(address, state))
+            } else {
+              Bad(NotFoundFailure(address).single)
+            }
+          case (_, _, None)              ⇒
+            Bad(NotFoundFailure(State, address.stateId).single)
+        }
+      case f: Invalid ⇒ Future.successful(Bad(ValidationFailure(f).single))
+    }
+  }
+
   def setDefaultShippingAddress(customerId: Int, addressId: Int)
     (implicit ec: ExecutionContext, db: Database): Future[Option[Failure]] = {
     db.run((for {
