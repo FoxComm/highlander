465162a7249b6cb6db03ecfe1ab0225ca192ec9e
Denys Mikhalenko <prontiol@gmail.com>
2015-08-12 12:06:27 +0300

    Mock for order reference in returns list

--- server/seeds/models/return.js
+++ server/seeds/models/return.js
@@ -2,11 +2,13 @@
 
 const
   BaseModel = require('../lib/base-model'),
+  Order = require('./order'),
   errors    = require('../../errors');
 
 const seed = [
   {field: 'referenceNumber', method: 'string', opts: {length: 8, pool: 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'}},
-  {field: 'orderNumber', method: 'string', opts: {length: 8, pool: 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'}},
+  {field: 'email', method: 'pick', opts: ['bree@foxcommerce.com']},
+  {field: 'orderNumber', method: 'pick', opts: [Order.findRandom().referenceNumber]},
   {field: 'returnStatus', method: 'pick', opts: ['Pending', 'Processing', 'Complete']},
   {field: 'asignee', method: 'pick', opts: ['Unassigned']},
   {field: 'returnTotal', method: 'integer', opts: {min: 100, max: 10000}}
