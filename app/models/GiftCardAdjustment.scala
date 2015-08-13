d8c61e4d8bcee886837390b78a8c455faff6c0d1
Jacques Fuentes <jpfuentes2@gmail.com>
2015-08-13 16:00:47 -0700

    Add orderPaymentId ref to GC/SC

--- app/models/GiftCardAdjustment.scala
+++ app/models/GiftCardAdjustment.scala
@@ -7,7 +7,8 @@ import slick.driver.PostgresDriver.api._
 import slick.driver.PostgresDriver.backend.{DatabaseDef => Database}
 import scala.concurrent.{ExecutionContext, Future}
 
-final case class GiftCardAdjustment(id: Int = 0, giftCardId: Int, credit: Int, debit: Int, capture: Boolean)
+final case class GiftCardAdjustment(id: Int = 0, giftCardId: Int, orderPaymentId: Int,
+  credit: Int, debit: Int, capture: Boolean)
   extends ModelWithIdParameter {
 }
 
@@ -17,11 +18,15 @@ class GiftCardAdjustments(tag: Tag) extends GenericTable.TableWithId[GiftCardAdj
 with RichTable {
   def id = column[Int]("id", O.PrimaryKey, O.AutoInc)
   def giftCardId = column[Int]("gift_card_id")
+  def orderPaymentId = column[Int]("order_payment_id")
   def credit = column[Int]("credit")
   def debit = column[Int]("debit")
   def capture = column[Boolean]("capture")
 
-  def * = (id, giftCardId, credit, debit, capture) <> ((GiftCardAdjustment.apply _).tupled, GiftCardAdjustment.unapply)
+  def * = (id, giftCardId, orderPaymentId,
+    credit, debit, capture) <> ((GiftCardAdjustment.apply _).tupled, GiftCardAdjustment.unapply)
+
+  def payment = foreignKey(OrderPayments.tableName, orderPaymentId, OrderPayments)(_.id)
 }
 
 object GiftCardAdjustments extends TableQueryWithId[GiftCardAdjustment, GiftCardAdjustments](
