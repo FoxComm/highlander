41cb2206fcb3635853631e67f3fcbf71c7ebdc05
Jacques Fuentes <jpfuentes2@gmail.com>
2015-08-13 16:00:32 -0700

    Put is_default and billing address back on CCs

--- sql/V7__create_credit_cards.sql
+++ sql/V7__create_credit_cards.sql
@@ -1,13 +1,16 @@
 create table credit_cards (
     id integer primary key,
     customer_id integer,
+    billing_address_id integer not null,
     gateway_customer_id character varying(255) not null,
     -- gateway_id integer not null, TODO: add lookup table
     last_four character(4) not null,
     exp_month integer not null,
     exp_year integer not null,
+    is_default boolean default false not null,
     foreign key (id) references payment_methods(id) on update restrict on delete restrict,
     foreign key (customer_id) references customers(id) on update restrict on delete restrict,
+    foreign key (billing_address_id) references addresses(id) on update restrict on delete restrict,
     constraint valid_last_four check (last_four ~ '[0-9]{4}'),
     constraint valid_exp_year check (exp_year between 2015 and 3000),
     constraint valid_exp_month check (exp_month between 1 and 12)
