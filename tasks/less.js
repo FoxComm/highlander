4fe434d5369ee146b2f688b503753e052f546bd0
Denys Mikhalenko <prontiol@gmail.com>
2015-08-17 11:00:01 +0300

    Prevent pipe breaking caused by errors in less files

--- tasks/less.js
+++ tasks/less.js
@@ -3,6 +3,7 @@
 const
   path              = require('path'),
   merge             = require('merge-stream'),
+  plumber           = require('gulp-plumber'),
   PluginCleanCSS    = require("less-plugin-clean-css"),
   PluginAutoPrefix  = require('less-plugin-autoprefix'),
   PluginNpmImport   = require('less-plugin-npm-import'),
@@ -14,11 +15,14 @@ const
   autoPrefix  = new PluginAutoPrefix({browsers: ["last 2 versions"]});
 
 module.exports = function(gulp, opts, $) {
+  let production = (process.env.NODE_ENV === 'production');
+
   gulp.task('less', function() {
     let themes = opts.getThemes(opts.themeDir);
     let tasks = themes.map(function(theme) {
       let src = path.join(opts.themeDir, theme, '**/*.less');
       return gulp.src(src)
+        .pipe($.if(!production, plumber()))
         .pipe($.concat(`${theme}.less`))
         .pipe($.less({
           plugins: [npmImport, groupMediaQueries, autoPrefix, cleanCSS]
