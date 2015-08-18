88bc1bc57c321fad5923410813e1f8a288cc69bc
Ferdinand Svehla <f.svehla@gmail.com>
2015-08-18 13:48:27 +0200

    Simplify pattern match branches

--- app/utils/FSM.scala
+++ app/utils/FSM.scala
@@ -11,14 +11,13 @@ trait FSM[A, B] { self: B ⇒
   private def currentState = stateLens.get(this)
 
   /** Returns the new state, but does not change the model */
-  def transitionState(newState: A): Xor[String, A] = fsm.get(currentState) match {
-    case Some(states) if states.contains(newState) || currentState == newState ⇒
-      right(newState)
-    case None if currentState == newState ⇒
-      right(newState)
-    case _ ⇒
-      left(s"could not transition from '${currentState}' to '${newState}'")
-  }
+  def transitionState(newState: A): Xor[String, A] =
+    if (newState == currentState) right(newState)
+    else
+      fsm.get(currentState) match {
+        case Some(states) if states.contains(newState) ⇒ right(newState)
+        case _ ⇒ left(s"could not transition from '${currentState}' to '${newState}'")
+      }
 
   /** Returns a Right of a copy of the model in the correct state, or a Left if the state
     * can’t be changed. */
