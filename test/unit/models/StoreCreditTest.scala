a4855898b8c48cfa6c28676de089bbff02fdccf4
Jacques Fuentes <jpfuentes2@gmail.com>
2015-08-20 16:09:08 -0700

    Move NewModel for validations to utils

--- test/unit/models/StoreCreditTest.scala
+++ test/unit/models/StoreCreditTest.scala
@@ -6,45 +6,34 @@ import StoreCredit._
 import cats.data.{NonEmptyList ⇒ NEL}
 
 class StoreCreditTest extends TestBase {
-  "GiftCard" - {
-    ".validate" - {
-      "returns errors when canceled with no corresponding reason" in {
-        val sc = Factories.storeCredit.copy(status = StoreCredit.Canceled)
-        val result = sc.validate
-
-        result.messages must have size 1
-        result.messages.head mustBe "canceledReason must not be empty"
-      }
-    }
-
+  "StoreCredit" - {
     ".validateNew" - {
-      "returns all errors when everything is wrong!!!!" in {
-        val sc = Factories.storeCredit.copy(originalBalance = -1, availableBalance = 100, currentBalance = 100,
-          status = Canceled, canceledReason = None)
+      "fails when originalBalance is less than zero" in {
+        val sc = Factories.storeCredit.copy(originalBalance = -1, availableBalance = 0, currentBalance = 0)
         val result = sc.validateNew
 
         result mustBe 'invalid
         result.fold(identity, m ⇒ NEL(m.modelName)) must ===(NEL(
-          "canceledReason must be present when canceled",
           "originalBalance cannot be less than currentBalance",
           "originalBalance cannot be less than availableBalance",
           "originalBalance must be greater than zero"
         ))
       }
 
-      "returns an errs when something is wrong!!!!" in {
-        val sc = Factories.storeCredit.copy(originalBalance = 50, availableBalance = 100, currentBalance = 50)
+      "fails when canceled with no corresponding reason" in {
+        val sc = Factories.storeCredit.copy(status = StoreCredit.Canceled)
         val result = sc.validateNew
 
         result mustBe 'invalid
         result.fold(identity, m ⇒ NEL(m.modelName)) must ===(NEL(
-          "originalBalance cannot be less than availableBalance"
+          "canceledReason must be present when canceled"
         ))
       }
 
-      "is right when everything is okay!!!" in {
+      "succeeds when valid" in {
         val sc = Factories.storeCredit
         val result = sc.validateNew
+
         result mustBe 'valid
         result.toOption.get === (sc)
       }
