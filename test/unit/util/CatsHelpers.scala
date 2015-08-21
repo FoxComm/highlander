38a5432d1aa5fc2675e1a6a2657a140d74916239
Ferdinand Svehla <f.svehla@gmail.com>
2015-08-21 20:19:20 +0200

    pattern match → fold

--- test/unit/util/CatsHelpers.scala
+++ test/unit/util/CatsHelpers.scala
@@ -8,33 +8,29 @@ import org.scalatest.Suite
 trait CatsHelpers { this: Suite ⇒ /** For fail */
   def rightValue[A, B](xor: A Xor B)
                       (implicit ttA: TypeTag[A], ttB: TypeTag[B]): B =
-    xor match {
-      case Xor.Right(r) ⇒ r
-      case Xor.Left(l)  ⇒
-          fail(s"Expected Right[${ ttB.tpe.dealias }], got Left[${ ttA.tpe.dealias }]: $l")
-    }
+    xor.fold(
+      l ⇒ fail(s"Expected Right[${ ttB.tpe.dealias }], got Left[${ ttA.tpe.dealias }]: $l"),
+      r ⇒ r
+    )
 
   def leftValue[A, B](xor: A Xor B)
                      (implicit ttA: TypeTag[A], ttB: TypeTag[B]): A =
-    xor match {
-      case Xor.Right(r) ⇒
-        fail(s"Expected Left[${ ttA.tpe.dealias }], got Right[${ ttB.tpe.dealias }]: $r")
-      case Xor.Left(l)  ⇒ l
-    }
+    xor.fold(
+      l ⇒ l,
+      r ⇒ fail(s"Expected Left[${ ttA.tpe.dealias }], got Right[${ ttB.tpe.dealias }]: $r")
+    )
 
   def validValue[E, A](validated: Validated[E, A])
                       (implicit ttA: TypeTag[A], ttE: TypeTag[E]): A =
-    validated match {
-      case Validated.Valid(v)   ⇒ v
-      case Validated.Invalid(e) ⇒
-        fail(s"Expected Valid[${ ttA.tpe.dealias }], got Invalid[${ ttE.tpe.dealias }]: $e")
-    }
+    validated.fold(
+      e ⇒ fail(s"Expected Valid[${ ttA.tpe.dealias }], got Invalid[${ ttE.tpe.dealias }]: $e"),
+      a ⇒ a
+    )
 
   def invalidValue[E, A](validated: Validated[E, A])
                         (implicit ttA: TypeTag[A], ttE: TypeTag[E]): E =
-    validated match {
-      case Validated.Valid(v)   ⇒
-        fail(s"Expected Invalid[${ ttA.tpe.dealias }], got Valid[${ ttE.tpe.dealias }]: $v")
-      case Validated.Invalid(e) ⇒ e
-    }
+    validated.fold(
+      e ⇒ e,
+      a ⇒ fail(s"Expected Invalid[${ ttA.tpe.dealias }], got Valid[${ ttE.tpe.dealias }]: $a")
+    )
 }
