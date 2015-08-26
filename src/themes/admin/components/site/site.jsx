e8f2839f7ac896f4f728a1cb20ab11bba292f75f
Denys Mikhalenko <prontiol@gmail.com>
2015-08-26 13:53:33 +0300

    Renamed menu -> sidebar for consistency

--- src/themes/admin/components/site/site.jsx
+++ src/themes/admin/components/site/site.jsx
@@ -3,14 +3,14 @@
 import React from 'react';
 import { RouteHandler } from 'react-router';
 import Header from '../header/header';
-import Menu from '../menu/menu';
+import Sidebar from '../sidebar/sidebar';
 import Modal from '../modal/modal';
 
 export default class Site extends React.Component {
   render() {
     return (
       <div>
-        <Menu/>
+        <Sidebar/>
         <Header/>
         <main role='main'>
           <RouteHandler/>
