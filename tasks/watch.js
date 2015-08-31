ccf680fd5d2f18eb51d8d16771200b059476b2b2
Cameron Stitt <cameron@cam.st>
2015-09-01 08:15:14 +1000

    Revert "Add USE_HOOKS check and new npm task."
    This reverts commit 988b4432ed768e136b012cfcd27d033c6e86b41e.

--- tasks/watch.js
+++ tasks/watch.js
@@ -5,9 +5,6 @@ const _ = require('lodash');
 module.exports = function(gulp, opts, $) {
   let watchTasks = _.reduce(gulp.tasks, function(acc, task, name) {
     if (name.indexOf('watch') != -1) {
-      if (name.indexOf('test') != -1 && process.env.USE_HOOKS == true) {
-        return acc;
-      }
       acc.push(name);
     }
     return acc;
