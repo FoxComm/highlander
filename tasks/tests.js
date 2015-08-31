f2de7238a563d80520d68c31b1d9ff4d1444d617
Cameron Stitt <cameron@cam.st>
2015-09-01 07:36:57 +1000

    RunSequence test tasks.

--- tasks/tests.js
+++ tasks/tests.js
@@ -1,7 +1,11 @@
 'use strict';
 
+const runSequence = require('run-sequence');
+
 module.exports = function (gulp, opts, $) {
-  gulp.task('test', ['lint', 'mocha']);
+  gulp.task('test', function(cb) {
+    runSequence('lint', 'mocha', cb);
+  });
 
   gulp.task('test.watch', function() {
     gulp.watch([opts.configSrc, opts.jsSrc, opts.serverSrc, opts.testSrc], ['test']);
