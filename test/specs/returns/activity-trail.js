a125fa91ae90871cb0a79140a90350407123f5ba
Denys Mikhalenko <prontiol@gmail.com>
2015-09-01 08:54:49 +0300

    Context in tests & code style

--- test/specs/returns/activity-trail.js
+++ test/specs/returns/activity-trail.js
@@ -1,12 +1,15 @@
 'use strict';
 
-describe('Return Activities #GET', function() {
+describe('Return Activities', function() {
 
-  it('should get a list of activities', function *() {
-    let
-      res         = yield this.api.get('/returns/1/activity-trail'),
-      activities  = res.response;
-    expect(res.status).to.equal(200);
-    expect(activities).to.be.instanceof(Array);
+  context('#GET', function() {
+    it('should get a list of activities', function *() {
+      let res = yield this.api.get('/returns/1/activity-trail');
+      let activities = res.response;
+
+      expect(res.status).to.equal(200);
+      expect(activities).to.be.instanceof(Array);
+    });
   });
+
 });
--- test/specs/returns/viewers.js
+++ test/specs/returns/viewers.js
@@ -1,12 +1,15 @@
 'use strict';
 
-describe('Return Viewers #GET', function() {
+describe('Return Viewers', function() {
 
-  it('should get a list of viewers', function *() {
-    let
-      res     = yield this.api.get('/returns/1/viewers'),
-      viewers = res.response;
-    expect(res.status).to.equal(200);
-    expect(viewers).to.be.instanceof(Array);
+  context('#GET', function() {
+    it('should get a list of viewers', function *() {
+      let res = yield this.api.get('/returns/1/viewers');
+      let viewers = res.response;
+
+      expect(res.status).to.equal(200);
+      expect(viewers).to.be.instanceof(Array);
+    });
   });
+
 });
