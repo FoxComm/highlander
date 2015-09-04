a0ba52903cd9d24798084d99fe48d28e33c24bb3
Jacques Fuentes <jpfuentes2@gmail.com>
2015-09-04 11:45:01 -0700

    Fix zip validation tests

--- test/unit/models/OrderBillingAddressTest.scala
+++ test/unit/models/OrderBillingAddressTest.scala
@@ -10,17 +10,20 @@ import utils.Slick.implicits._
 class OrderBillingAddressTest extends TestBase {
   "OrderBillingAddress" - {
     ".validateNew" - {
+      def zipFailure(pattern: String): NonEmptyList[Failure] =
+        NonEmptyList(GeneralFailure(s"zip must fully match regular expression $pattern"))
+
       "returns errors when zip is invalid" in {
         val badZip = Factories.billingAddress.copy(regionId = 1, zip = "AB+123")
         val wrongLengthZip = Factories.billingAddress.copy(regionId = 1, zip = "1")
 
         val addresses = Table(
           ("address", "errors"),
-          (badZip, NonEmptyList(GeneralFailure("zip must fully match regular expression '%s'".format(Address.zipPattern)))),
-          (wrongLengthZip, NonEmptyList(GeneralFailure("zip must fully match regular expression '%s'".format(Address.zipPattern))))
+          (badZip, zipFailure(Address.zipPattern)),
+          (wrongLengthZip, zipFailure(Address.zipPattern))
         )
 
-        forAll(addresses) { (address: OrderBillingAddress, errors: NonEmptyList[GeneralFailure]) =>
+        forAll(addresses) { (address, errors) =>
           invalidValue(address.validateNew) mustBe (errors)
         }
       }
--- test/unit/models/OrderShippingAddressTest.scala
+++ test/unit/models/OrderShippingAddressTest.scala
@@ -10,17 +10,20 @@ class OrderShippingAddressTest extends TestBase {
 
   "OrderShippingAddress" - {
     ".validateNew" - {
+      def zipFailure(pattern: String): NonEmptyList[Failure] =
+        NonEmptyList(GeneralFailure(s"zip must fully match regular expression $pattern"))
+
       "returns errors when zip is invalid" in {
         val badZip = Factories.shippingAddress.copy(regionId = 1, zip = "AB+123")
         val wrongLengthZip = Factories.shippingAddress.copy(regionId = 1, zip = "1")
 
         val addresses = Table(
           ("address", "errors"),
-          (badZip, NonEmptyList(GeneralFailure("zip must fully match regular expression '%s'".format(Address.zipPattern)))),
-          (wrongLengthZip, NonEmptyList(GeneralFailure("zip must fully match regular expression '%s'".format(Address.zipPattern))))
+          (badZip, zipFailure(Address.zipPattern)),
+          (wrongLengthZip, zipFailure(Address.zipPattern))
         )
 
-        forAll(addresses) { (address: OrderShippingAddress, errors: NonEmptyList[GeneralFailure]) =>
+        forAll(addresses) { (address, errors) =>
           invalidValue(address.validateNew) mustBe (errors)
         }
       }
