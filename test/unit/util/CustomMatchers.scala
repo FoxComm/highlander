2644d990a1c853b86e60f82d0415544ce3650ae7
Jacques Fuentes <jpfuentes2@gmail.com>
2015-09-08 13:14:50 -0700

    Improve failure matcher and some validations

--- test/unit/util/CustomMatchers.scala
+++ test/unit/util/CustomMatchers.scala
@@ -8,15 +8,17 @@ import services._
 
 object CustomMatchers {
   // Same as `include`, used for Failure inner Strings
-  class IncludeFailureMatcher(expectedSubstring: String) extends Matcher[NonEmptyList[Failure]] {
+  class IncludeFailureMatcher(f: Failure) extends Matcher[NonEmptyList[Failure]] {
     def apply(left: NonEmptyList[Failure]) = {
       MatchResult(
-        left.exists(_.description.exists(_.contains(expectedSubstring))),
-        s"""NonEmptyList of Failures does not contain "$expectedSubstring"""",
-        s"""NonEmptyList of Failure contains "$expectedSubstring""""
+        left.exists(_.description.toSet.subsetOf(f.description.toSet)),
+        s"""$left does not contain "$f"""",
+        s"""$left contains "$f""""
       )
     }
   }
 
-  def includeFailure(expectedSubstring: String) = new IncludeFailureMatcher(expectedSubstring)
+  def includeFailure(expectedSubstring: String) = new IncludeFailureMatcher(GeneralFailure(expectedSubstring))
+
+  def includeFailure(f: Failure) = new IncludeFailureMatcher(f)
 }
