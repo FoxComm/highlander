5bc5ed74c68c6b8a5d345c650ffff7dd1849298a
Cameron Stitt <cameron@cam.st>
2015-09-09 22:43:14 +1000

    Move edit button to line items.

--- src/themes/admin/components/orders/details.jsx
+++ src/themes/admin/components/orders/details.jsx
@@ -6,55 +6,20 @@ import OrderLineItems from './line-items';
 import OrderShippingAddress from './shipping-address';
 import OrderShippingMethod from './shipping-method';
 import OrderPayment from './payment';
-import OrderStore from './store';
-import Api from '../../lib/api';
-import { dispatch } from '../../lib/dispatcher';
 
 export default class OrderDetails extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      isEditing: false
-    };
-  }
-
-  toggleEdit() {
-    Api.post(`${OrderStore.uri(this.props.order.id)}/edit`)
-      .then(() => {
-        this.setState({
-          isEditing: !this.state.isEditing
-        });
-        dispatch('toggleOrderEdit');
-      })
-      .catch((err) => { console.log(err); });
-  }
-
   render() {
     let
-      order     = this.props.order,
-      isEditing = this.state.isEditing,
-      actions   = null;
-
-    if (isEditing) {
-      actions = (
-        <span>
-          <button onClick={this.toggleEdit.bind(this)}>Cancel</button>
-          <button>Save Edits</button>
-        </span>
-      );
-    } else if (OrderStore.holdStatusList.indexOf(order.orderStatus) !== -1) {
-      actions = <button className='btn' onClick={this.toggleEdit.bind(this)}>Edit Order Details</button>;
-    }
+      order     = this.props.order;
 
     return (
       <div id="order-details">
-        {actions}
-        <OrderSummary order={order} isEditing={isEditing}/>
+        <OrderSummary order={order} />
         <article>
-          <OrderLineItems order={order} isEditing={isEditing}/>
-          <OrderShippingAddress order={order} isEditing={isEditing}/>
-          <OrderShippingMethod order={order} isEditing={isEditing} />
-          <OrderPayment order={order} isEditing={isEditing}/>
+          <OrderLineItems order={order} />
+          <OrderShippingAddress order={order}/>
+          <OrderShippingMethod order={order} />
+          <OrderPayment order={order} />
         </article>
       </div>
     );
--- src/themes/admin/components/orders/line-items.jsx
+++ src/themes/admin/components/orders/line-items.jsx
@@ -3,14 +3,39 @@
 import React from 'react';
 import TableHead from '../tables/head';
 import TableBody from '../tables/body';
+import SkuStore from './sku-store';
+import SkuResult from './sku-result';
 
 export default class OrderLineItems extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isEditing: false
+    };
+  }
+
+  toggleEdit() {
+    this.setState({
+      isEditing: !this.state.isEditing
+    });
+  }
+
   render() {
     let order = this.props.order;
+    let editing = this.state.isEditing;
+    let typeahead = null;
 
+    if (editing) {
+      typeahead = <Typeahead component={SkuResult} store={SkuStore} selectEvent="addLineItem" />
+    }
     return (
       <section id="order-line-items">
-        <header>Items</header>
+        <header>
+          <div className='fc-grid'>
+            <div className="fc-col-2-3">Items</div>
+            <div className="fc-col-1-3"><button className="fc-button" onClick={this.toggleEdit.bind(this)}><i className="fa fa-pencil"></i></button></div>
+          </div>
+        </header>
         <table className="fc-table">
           <TableHead columns={this.props.tableColumns}/>
           <TableBody columns={this.props.tableColumns} rows={order.lineItems} model='order'/>
