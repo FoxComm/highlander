3bd4f44e4effd2638551414ae33f9bacc288e313
Jacques Fuentes <jpfuentes2@gmail.com>
2015-09-11 13:38:50 -0700

    Use Xor to distinguish b/t customer vs storeadmin

--- app/services/GiftCardService.scala
+++ app/services/GiftCardService.scala
@@ -2,21 +2,27 @@ package services
 
 import scala.concurrent.{ExecutionContext, Future}
 
+import cats.data.Xor
+import shapeless._
 import models.{GiftCard, Customer, Customers, GiftCards, StoreAdmin, StoreAdmins}
 import responses.{GiftCardResponse, CustomerResponse, StoreAdminResponse}
 import responses.GiftCardResponse.Root
+import slick.dbio
+import slick.dbio.Effect.All
 import slick.driver.PostgresDriver.api._
 import utils.Slick.implicits._
 
 object GiftCardService {
   val mockCustomerId = 1
 
+  type Account = Customer :+: StoreAdmin :+: CNil
+
   def getByCode(code: String)(implicit db: Database, ec: ExecutionContext): Result[Root] = {
     fetchDetails(code).run().flatMap {
-      case (Some(giftCard), Some(customer), _) ⇒
+      case (Some(giftCard), Xor.Left(Some(customer))) ⇒
         val customerResponse = Some(CustomerResponse.build(customer))
         Result.right(GiftCardResponse.build(giftCard, customerResponse))
-      case (Some(giftCard), _, Some(storeAdmin)) ⇒
+      case (Some(giftCard), Xor.Right(Some(storeAdmin))) ⇒
         val storeAdminResponse = Some(StoreAdminResponse.build(storeAdmin))
         Result.right(GiftCardResponse.build(giftCard, None, storeAdminResponse))
       case _ ⇒
@@ -24,32 +30,20 @@ object GiftCardService {
     }
   }
 
-  private def fetchDetails(code: String)(implicit db: Database, ec: ExecutionContext) = {
-    for {
-      giftCard ← GiftCards.findByCode(code).one
-      customer ← fetchCustomer(giftCard)
-      storeAdmin ← fetchStoreAdmin(giftCard)
-    } yield (giftCard, customer, storeAdmin)
-  }
+  private def fetchDetails(code: String)(implicit db: Database, ec: ExecutionContext) = for {
+    giftCard  ← GiftCards.findByCode(code).one
+    account   ← getAccount(giftCard)
+  } yield (giftCard, account)
 
-  private def fetchCustomer(gc: Option[GiftCard])(implicit db: Database, ec: ExecutionContext): DBIO[Option[Customer]]
-  = {
-    val default = DBIO.successful(None)
-    gc.map {
-      _.originType match {
-        case GiftCard.CustomerPurchase ⇒ Customers._findById(mockCustomerId).extract.one
-        case _                         ⇒ default
-      }
-    }.getOrElse(default)
-  }
-
-  private def fetchStoreAdmin(gc: Option[GiftCard])(implicit db: Database, ec: ExecutionContext): DBIO[Option[StoreAdmin]] = {
-    val default = DBIO.successful(None)
-    gc.map { giftCard ⇒
-      giftCard.originType match {
-        case GiftCard.CsrAppeasement  ⇒ StoreAdmins._findById(giftCard.originId).extract.one
-        case _                        ⇒ default
-      }
-    }.getOrElse(default)
-  }
+  private def getAccount(giftCard: Option[GiftCard])
+    (implicit db: Database, ec: ExecutionContext): DBIO[Option[Customer] Xor Option[StoreAdmin]] = giftCard.map { gc ⇒
+    gc.originType match {
+      case GiftCard.CustomerPurchase ⇒
+        Customers._findById(mockCustomerId).extract.one.map(Xor.left)
+      case GiftCard.CsrAppeasement ⇒
+        StoreAdmins._findById(gc.originId).extract.one.map(Xor.right)
+      case _ ⇒
+        DBIO.successful(Xor.left(None))
+    }
+  }.getOrElse(DBIO.successful(Xor.left(None)))
 }
