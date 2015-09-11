80afa8ccb85cf200d3319a249623598b8544e2bf
Denys Mikhalenko <prontiol@gmail.com>
2015-09-11 23:54:48 +0300

    Return Card refactor

--- src/themes/admin/components/returns/details.jsx
+++ src/themes/admin/components/returns/details.jsx
@@ -7,21 +7,11 @@ import ReturnLineItems from './line-items';
 import ReturnShippingMethod from './shipping-method';
 import ReturnPayment from './payment';
 import ReturnStore from './store';
-import { dispatch } from '../../lib/dispatcher';
 
 export default class ReturnDetails extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-      isEditing: false
-    };
-  }
-
-  toggleEdit() {
-    this.setState({
-      isEditing: !this.state.isEditing
-    });
-    dispatch('toggleReturnEdit');
+    this.state = {};
   }
 
   render() {
@@ -29,25 +19,11 @@ export default class ReturnDetails extends React.Component {
     let isEditing = this.state.isEditing;
     let actions = null;
 
-    //if (isEditing) {
-    //  actions = (
-    //    <span>
-    //      <button onClick={this.toggleEdit.bind(this)}>Cancel</button>
-    //      <button className='primary'>Save Edits</button>
-    //    </span>
-    //  );
-    //} else if (ReturnStore.holdStatusList.indexOf(order.returnStatus) !== -1 || 1) {
-    //  actions = (
-    //    <button className="return-details-edit-order" onClick={this.toggleEdit.bind(this)}>Edit Return Details</button>
-    //  );
-    //}
-
     return (
       <div className="return-details">
         <div className="return-details-controls">
           {actions}
         </div>
-
         <div className="return-details-body fc-grid fc-grid-match">
           <div className="fc-col-3-10">
             <ReturnSummary return={retrn} isEditing={isEditing}/>
