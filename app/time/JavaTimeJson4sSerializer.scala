eee5c03512f7a22647324c2ee935b3ac7ddede55
Ferdinand Svehla <f.svehla@gmail.com>
2015-09-14 18:31:13 +0200

    Json4s mapper

---
+++ app/time/JavaTimeJson4sSerializer.scala
@@ -0,0 +1,22 @@
+package time
+
+import java.time.Instant
+import java.time.format.DateTimeFormatter
+
+import org.joda.time.DateTime
+import org.json4s._
+import org.json4s.JsonAST.JString
+import org.json4s.ext.DateParser
+
+/** Since this works on instants, the time zone information is honored, but thrown away. */
+object JavaTimeJson4sSerializer {
+  val formatter = DateTimeFormatter.ISO_INSTANT
+
+  object jsonFormat extends CustomSerializer[Instant](format => ({
+    case JString(s) ⇒ Instant.from(formatter.parse(s))
+    case JNull      ⇒ null
+  },
+  {
+    case d: Instant ⇒ JString(formatter.format(d))
+  }))
+}
--- test/unit/utils/JsonFormattersTest.scala
+++ test/unit/utils/JsonFormattersTest.scala
@@ -1,16 +1,20 @@
 package utils
 
+import java.time.{Instant, ZonedDateTime}
+
 import scalaz.Show
 
 import com.pellucid.sealerate
+import org.joda.time.{DateTimeZone, DateTime}
 import org.json4s.Formats
+import org.json4s.JsonAST.JObject
 import util.TestBase
 
 import models.{GiftCard, CreditCardCharge, Order}
 import utils.Money.Currency
 
 class JsonFormattersTest extends TestBase {
-  import org.json4s.jackson.JsonMethods.parse
+  import org.json4s.jackson.JsonMethods.{ parse, pretty }
   import org.json4s.jackson.Serialization.write
 
   import utils.JsonFormatters._
@@ -33,5 +37,12 @@ class JsonFormattersTest extends TestBase {
     val ast = parse(write(Product(price = 50, currency = Currency.USD)))
     (ast \ "price").extract[Int] === (50)
     (ast \ "currency").extract[Currency] === (Currency.USD)
+  }
+
+  "(de)serializes java.time.Instant" in {
+    val instant = ZonedDateTime.of(2015, 9, 14, 15, 38, 46, 0, time.UTC).toInstant
+
+    write("hello" → instant) must === ("""{"hello":"2015-09-14T15:38:46Z"}""")
+    (parse("""{"hello":"2015-09-14T15:38:46Z"}""") \ "hello").extract[Instant] must === (instant)
   }
 }
