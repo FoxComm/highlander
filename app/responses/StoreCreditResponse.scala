640beb043600773b8a18321b8e214d2a68fcfa9f
Pavel Kiselev <kpashka@gmail.com>
2015-09-15 02:40:25 +0600

    Minor response changes & fixes [Progress [#101168190]]

--- app/responses/StoreCreditResponse.scala
+++ app/responses/StoreCreditResponse.scala
@@ -1,6 +1,6 @@
 package responses
 
-import org.joda.time.DateTime
+import java.time.Instant
 import slick.driver.PostgresDriver.api._
 import utils.Money.Currency
 
@@ -8,20 +8,20 @@ object StoreCreditResponse {
   final case class Root(
     id: Int,
     originId: Int,
-    originType: models.StoreCredit.OriginType,
+    `type`: models.StoreCredit.OriginType,
     currency: Currency,
     customerId: Int,
     originalBalance: Int,
     currentBalance: Int,
     availableBalance: Int,
     status: models.StoreCredit.Status,
-    createdAt: DateTime,
+    createdAt: Instant,
     canceledReason: Option[String] = None)
 
   def build(storeCredit: models.StoreCredit): Root = {
     Root(id = storeCredit.id,
       originId = storeCredit.originId,
-      originType = storeCredit.originType,
+      `type` = storeCredit.originType,
       currency = storeCredit.currency,
       customerId = storeCredit.customerId,
       originalBalance = storeCredit.originalBalance,
