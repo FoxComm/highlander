0f147025bbd99897a5d6cfbb9f2caf8336a002af
Jeff Mataya <jeff@foxcommerce.com>
2015-09-14 10:17:18 -0700

    Put basic auth in place only for staging

--- server/index.js
+++ server/index.js
@@ -13,21 +13,23 @@ const app = koa();
 app.init = co.wrap(function *(env) {
   if (env) { app.env = env; }
   app.config = new Config(app.env);
-  app.use(function *(next) {
-    try { 
-      yield next;
-    } catch (err) {
-      if (401 == err.status) {
-        this.status = 401;
-        this.set('WWW-Authenticate', 'Basic');
-        this.body = 'the fox says you cannot pass';
-      } else {
-        throw err;
+  if (app.env == 'staging') {
+    app.use(function *(next) {
+      try { 
+        yield next;
+      } catch (err) {
+        if (401 == err.status) {
+          this.status = 401;
+          this.set('WWW-Authenticate', 'Basic');
+          this.body = 'the fox says you cannot pass';
+        } else {
+          throw err;
+        }
       }
-    }
-  });
+    });
 
-  app.use(auth({ name: 'foxcomm', pass: 'st@ging' }));
+    app.use(auth({ name: 'foxcomm', pass: 'st@ging' }));
+  }
   app.use(serve(app.config.server.publicDir));
   app.use(favicon(app.config.layout.favicon));
   app.seeds = yield* require(`${__dirname}/seeds`)();
