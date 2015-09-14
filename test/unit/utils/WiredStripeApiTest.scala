5e02713698e04dff2f08f695ad816140aa19a3f2
Ferdinand Svehla <f.svehla@gmail.com>
2015-09-14 14:05:08 +0200

    Make sure that we don’t catch other exceptions

--- test/unit/utils/WiredStripeApiTest.scala
+++ test/unit/utils/WiredStripeApiTest.scala
@@ -1,5 +1,7 @@
 package utils
 
+import scala.concurrent.Await
+import scala.concurrent.duration.Duration.Inf
 import scala.util.control.NoStackTrace
 
 import com.stripe.exception.StripeException
@@ -7,12 +9,19 @@ import services.StripeRuntimeException
 import util.TestBase
 
 class WiredStripeApiTest extends TestBase {
+  private val api = new WiredStripeApi
+
   "Wired Stripe API" - {
     "catches StripeException and returns a Result.failure" in {
-      val api    = new WiredStripeApi
       val result = api.inBlockingPool("abc")(_ ⇒ throw someStripeException)
-
       leftValue(result.futureValue).head must === (StripeRuntimeException(someStripeException))
+    }
+
+    "does not catch other exceptions" in {
+      val result = api.inBlockingPool("abc")(_ ⇒ 42 / 0)
+
+      /** Scalatest’s futureValue wraps the exception, so we can’t use it here. */
+      an [ArithmeticException] must be thrownBy { Await.result(result, Inf) }
     }
   }
 
