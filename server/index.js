dce52db3b4e3e01a92ce1c798a55dfc374adddd7
Jeff Mataya <jeff@foxcommerce.com>
2015-09-14 10:11:21 -0700

    Configuration for the staging server
    * Run on port 80 for staging
    * Implement basic authentication

--- server/index.js
+++ server/index.js
@@ -1,24 +1,40 @@
 'use strict';
 
-const
-  koa     = require('koa'),
-  path    = require('path'),
-  co      = require('co'),
-  favicon = require('koa-favicon'),
-  serve   = require('koa-static'),
-  Config  = require(path.resolve('config'));
+const auth    = require('koa-basic-auth');
+const koa     = require('koa');
+const path    = require('path');
+const co      = require('co');
+const favicon = require('koa-favicon');
+const serve   = require('koa-static');
+const Config  = require(path.resolve('config'));
 
 const app = koa();
 
 app.init = co.wrap(function *(env) {
   if (env) { app.env = env; }
   app.config = new Config(app.env);
+  app.use(function *(next) {
+    try { 
+      yield next;
+    } catch (err) {
+      if (401 == err.status) {
+        this.status = 401;
+        this.set('WWW-Authenticate', 'Basic');
+        this.body = 'the fox says you cannot pass';
+      } else {
+        throw err;
+      }
+    }
+  });
+
+  app.use(auth({ name: 'foxcomm', pass: 'st@ging' }));
   app.use(serve(app.config.server.publicDir));
   app.use(favicon(app.config.layout.favicon));
   app.seeds = yield* require(`${__dirname}/seeds`)();
   if (app.env !== 'production') {
     app.use(require('koa-logger')());
   }
+  if 
   require(`${__dirname}/middleware`)(app);
   require(`${__dirname}/api`)(app);
   require(`${__dirname}/fauxnix`)(app);
