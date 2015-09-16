80a3e434c3e47d95629b359be750dda957ae5a7f
Denys Mikhalenko <prontiol@gmail.com>
2015-09-16 23:46:25 +0300

    Removed redundant assignment

--- src/themes/admin/components/returns/customer-info.jsx
+++ src/themes/admin/components/returns/customer-info.jsx
@@ -4,13 +4,11 @@ import React from 'react';
 
 export default class CustomerInfo extends React.Component {
   render() {
-    let retrn = this.props.return;
-
     return (
       <div className="fc-return-summary fc-content-box">
         <header className="header">Message for Customer</header>
         <article>
-          {retrn.customerMessage}
+          {this.props.return.customerMessage}
         </article>
       </div>
     );
