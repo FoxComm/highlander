e94ce1fc50588fe9e1274a163b332f2d9887ca64
Pavel Kiselev <kpashka@gmail.com>
2015-09-22 21:16:01 +0600

    Init [Progress [#101168440]]

--- app/payloads/GiftCards.scala
+++ app/payloads/GiftCards.scala
@@ -5,4 +5,6 @@ import utils.Money._
 
 final case class GiftCardCreateByCsr(balance: Int, currency: Currency = Currency.USD)
 
+final case class GiftCardBulkCreateByCsr(count: Int, balance: Int, currency: Currency = Currency.USD)
+
 final case class GiftCardUpdateStatusByCsr(status: GiftCard.Status, reason: Option[Int] = None)
