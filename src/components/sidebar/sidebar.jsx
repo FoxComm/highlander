f909b0e877d0a252164db3a8152d480173e48802
Jeff Mataya <jeff@foxcommerce.com>
2015-09-24 17:47:13 -0700

    Add the FoxComm logo to the sidebar

--- src/components/sidebar/sidebar.jsx
+++ src/components/sidebar/sidebar.jsx
@@ -2,21 +2,16 @@
 
 import React from 'react';
 import { Link } from 'react-router';
+import Navigation from './navigation';
 
 export default class Sidebar extends React.Component {
   render() {
     return (
-      <aside role='complementary' className='fc-sidebar'>
-        <nav>
-          <ul>
-            <li><Link to='home'><i className="icon-home"></i>Dashboard</Link></li>
-            <li><Link to='rmas'><i className="icon-returns"></i>Returns</Link></li>
-            <li><Link to='orders'><i className="icon-orders"></i>Orders</Link></li>
-            <li><Link to="customers"><i className="icon-customers"></i>Customers</Link></li>
-            <li><a href=""><i className="icon-items"></i>Products</a></li>
-            <li><Link to="gift-cards"><i className="icon-gift-cards"></i>Gift Cards</Link></li>
-          </ul>
-        </nav>
+      <aside role='complimentary' className='fc-sidebar'>
+        <div className='logo'>
+          <img src="https://s3-us-west-2.amazonaws.com/fc-ashes/images/fc-logo-nav.svg"></img>
+        </div>
+        <Navigation/>
       </aside>
     );
   }
