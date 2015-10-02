b388a85c369d1bdd9bbd08c67bb0d43ac44507b7
Jeff Mataya <jeff@foxcommerce.com>
2015-10-01 22:11:22 -0700

    Add the FK to SQL for order shipping methods

--- sql/V34__create_order_shipping_methods.sql
+++ sql/V34__create_order_shipping_methods.sql
@@ -4,5 +4,8 @@ create table order_shipping_methods (
     admin_display_name character varying(255) not null,
     storefront_display_name character varying(255) not null,
     shipping_carrier_id integer null,
-    price integer not null
-)
+    price integer not null,
+    foreign key (order_id) references orders(id) on update restrict on delete restrict
+);
+
+create unique index order_shipping_methods_order_id_idx on order_shipping_methods (order_id);
