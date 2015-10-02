c84c8708136db59daeb7940d47e8796c415735ef
Sergiy Prydatchenko <sergiy@foxcommerce.com>
2015-10-02 17:41:32 +0300

    Compilation fix

--- app/utils/Strings.scala
+++ app/utils/Strings.scala
@@ -4,7 +4,7 @@ import scala.language.implicitConversions
 
 object Strings {
   final implicit class EnrichedString(val s: String) extends AnyVal {
-    def lowerCaseFirstLetter = s.headOption.map(_.toLower).getOrElse("") + s.drop(1)
+    def lowerCaseFirstLetter = s.headOption.map(_.toLower.toString).getOrElse("") + s.drop(1)
     def upperCaseFirstLetter = s.capitalize
   }
 }
