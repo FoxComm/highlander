94a9a7fbad8e52fe9e7cf185fed021b7878439ff
Denys Mikhalenko <prontiol@gmail.com>
2015-10-06 01:30:28 +0300

    Default renderRow

--- src/components/table/body.jsx
+++ src/components/table/body.jsx
@@ -1,16 +1,31 @@
 'use strict';
 
 import React from 'react';
+import Wrapper from '../wrapper/wrapper';
 import TableStore from '../../lib/table-store';
 import TableRow from './row';
+import TableCell from './cell';
 
 export default class TableBody extends React.Component {
+  renderRow(row, index) {
+    return (
+      <Wrapper>
+        <TableRow>
+          {this.props.store.columns.map((column) => {
+            return <TableCell>{row[column.field]}</TableCell>
+          })}
+        </TableRow>
+      </Wrapper>
+    )
+  }
+
   render() {
+    let renderRow = this.props.renderRow || this.renderRow.bind(this);
     return (
       <tbody className="fc-table-tbody">
-        {this.props.store.rows.map((row, index) => {
-          return this.props.renderRow(row, index).props.children;
-        })}
+      {this.props.store.rows.map((row, index) => {
+        return renderRow(row, index).props.children;
+      })}
       </tbody>
     );
   }
