882196f570b82a15fab7dd7dd4bb39872b7f7600
Cameron Stitt <cameron@cam.st>
2015-10-06 15:35:37 -0700

    Progress [#105013504]. Store tests.

---
+++ test/unit/stores/gift-cards.js
@@ -0,0 +1,20 @@
+'use strict';
+
+const path = require('path');
+const assert = require('assert');
+const sinon = require('sinon');
+
+describe('GiftCard Store', function() {
+  const alt = require(path.resolve('src/alt'));
+  const giftCardStore = require(path.resolve('src/stores/gift-cards'));
+  const giftCardActions = require(path.resolve('src/actions/gift-cards'));
+
+  it('listens for update gift card action', function () {
+    alt.dispatcher.dispatch({
+      action: giftCardActions.UPDATE_GIFT_CARDS,
+      data: [1]
+    });
+
+    assert(giftCardStore.getState().giftCards.size === 1);
+  });
+});
