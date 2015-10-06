4173c6c47ea3cf52259b3f44aa0e1f446c8ef4d3
Eugene Sypachev <eugene.sypachev@gmail.com>
2015-10-06 20:20:34 +0400

    subnav

--- src/components/customers/customer.jsx
+++ src/components/customers/customer.jsx
@@ -1,6 +1,7 @@
 'use strict';
 
 import React, { PropTypes } from 'react';
+import { Link, IndexLink } from '../link';
 
 export default class Customer extends React.Component {
 
@@ -9,9 +10,45 @@ export default class Customer extends React.Component {
   };
 
   render() {
+    console.log(this.props);
     return (
-      <div id="user">
-        {this.props.children}
+      <div className="fc-user">
+        <div className="gutter">
+          <div className="fc-content-box">
+            <div className="fc-customer-info-header">
+              <div className="fc-customer-info-head">
+                <div className="fc-customer-info-rank">
+                  Top 10%
+                </div>
+              </div>
+              <div className="fc-customer-info-avatar">
+                <i className="icon-customer"></i>
+              </div>
+              <div className="fc-customer-info-name">
+                Customer Name
+              </div>
+              <div className="fc-customer-info-email">
+                customer@email.com
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="gutter">
+          <ul className="fc-tabbed-nav">
+            <li><a href="">Insights</a></li>
+            <li><IndexLink to="customer-details" params={this.props.params}>Details</IndexLink></li>
+            <li><a href="">Transaction</a></li>
+            <li><a href="">Items</a></li>
+            <li><a href="">Store Credit</a></li>
+            <li><a href="">Notifications</a></li>
+            <li><a href="">Reviews</a></li>
+            <li><a href="">Notes</a></li>
+            <li><a href="">Activity Trail</a></li>
+          </ul>
+          <div>
+            {this.props.children}
+          </div>
+        </div>
       </div>
     );
   }
