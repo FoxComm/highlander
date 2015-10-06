9b2514d030cc19c3e5e1442a6eff089c016f7608
Maxim Khailo <mempko@gmail.com>
2015-10-06 09:10:37 -0500

    [#104167932] Adding test to show that updating an address via shipping-address routes correctly updates full order query.
    Appears bug 104167932 was fixed with "[#104654866] Simplify order shipping-address API"

--- test/integration/OrderIntegrationTest.scala
+++ test/integration/OrderIntegrationTest.scala
@@ -550,6 +550,35 @@ class OrderIntegrationTest extends IntegrationTestBase
         addressBook.city must === (address.city)
       }
 
+      "full order returns updated shipping addresss" in new ShippingAddressFixture {
+        //update address
+        val name = "Even newer name"
+        val city = "Queen Max"
+        val updateAddressPayload = payloads.UpdateAddressPayload(name = Some(name), city = Some(city))
+        val response = PATCH(s"v1/orders/${order.referenceNumber}/shipping-address", updateAddressPayload)
+
+        response.status must === (StatusCodes.OK)
+
+        //get full order and compare
+        val fullResponse = GET(s"v1/orders/${order.referenceNumber}")
+        fullResponse.status must === (StatusCodes.OK)
+
+        val fullOrder = parse(response.bodyText).extract[FullOrder.Root]
+        fullOrder.shippingAddress match {
+          case Some(addr) => {
+            addr.name must === (name)
+            addr.city must === (city)
+            addr.address1 must === (address.address1)
+            addr.address2 must === (address.address2)
+            addr.regionId must === (address.regionId)
+            addr.zip must === (address.zip)
+          }
+
+          case None => {
+            fail("FullOrder should have a shipping address")
+          }
+        }
+      }
     }
 
     "deleting the shipping address from an order" - {
