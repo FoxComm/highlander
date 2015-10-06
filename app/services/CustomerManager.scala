f57ef39a6a16cc39f5e2a89bac73837722ff81a1
Sergey Rublev <narma.nsk@gmail.com>
2015-10-06 07:53:11 +0600

    fetch Regions for rendering single Customer too

--- app/services/CustomerManager.scala
+++ app/services/CustomerManager.scala
@@ -2,12 +2,15 @@ package services
 
 import scala.concurrent.ExecutionContext
 
-import models.{CreditCards, Regions, Customer, Customers, StoreAdmin}
+import models.{CreditCards, Regions, Region, Customer, Customers, StoreAdmin}
 import responses.CustomerResponse._
 import slick.driver.PostgresDriver.api._
 import utils.Slick.UpdateReturning._
 
 object CustomerManager {
+
+  type QuerySeq = Query[Customers, Customer, Seq]
+
   def toggleDisabled(customerId: Int, disabled: Boolean, admin: StoreAdmin)
     (implicit ec: ExecutionContext, db: Database): Result[Customer] = {
     db.run(for {
@@ -21,29 +24,39 @@ object CustomerManager {
   }
 
   def findAll(implicit db: Database, ec: ExecutionContext): Result[Seq[Root]] = {
-    val customersWithShipRegion = Customers
-      .joinLeft(models.Addresses).on { case(a,b) => a.id === b.customerId  && b.isDefaultShipping === true}
-      .joinLeft(Regions).on(_._2.map(_.regionId) === _.id)
-
-    val creditCardsWithRegion = CreditCards.join(Regions).on(_.regionId === _.id)
-
-    val query = customersWithShipRegion.joinLeft(creditCardsWithRegion).on(_._1._1.id === _._1.customerId)
+    val query = fetchRegions(Customers)
 
     Result.fromFuture(db.run(query.result).map { results ⇒
       results.map {
-        case (((customer, _), shipRegion), billRegion) ⇒
-          build(customer, shipRegion, billRegion.map(_._2))
+        case (customer, shipRegion, billRegion) ⇒
+          build(customer, shipRegion, billRegion)
       }
     })
   }
 
   def getById(id: Int)(implicit db: Database, ec: ExecutionContext): Result[Root] = {
-    Customers.findById(id).flatMap {
-      case Some(customer) ⇒
-        Result.right(build(customer))
+    val query = fetchRegions(Customers.filter(_.id === id))
+    db.run(query.result.headOption).flatMap {
+      case Some((customer, shipRegion: Option[Region], billRegion: Option[Region])) ⇒
+        Result.right(build(customer, shipRegion, billRegion))
       case _ ⇒
         Result.failure(NotFoundFailure(Customer, id))
     }
+  }
+
+  protected def fetchRegions(query: QuerySeq) = {
+    val customerWithShipRegion = for {
+      ((c, a), r) ← query.joinLeft(models.Addresses).on {
+        case (a, b) => a.id === b.customerId && b.isDefaultShipping === true
+      }.joinLeft(Regions).on(_._2.map(_.regionId) === _.id)
+    } yield (c, r)
+
+    val CcWithRegions = CreditCards.join(Regions).on(_.regionId === _.id)
+
+    for {
+      ((c, shipRegion), billInfo) ←
+        customerWithShipRegion.joinLeft(CcWithRegions).on(_._1.id === _._1.customerId)
+    } yield (c, shipRegion, billInfo.map(_._2))
   }
 
 }
