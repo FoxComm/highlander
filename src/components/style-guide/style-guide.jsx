531b769332ef339d97ea0cbc38a9aea5eb8125e5
Jeff Mataya <jeff@foxcommerce.com>
2015-10-06 19:32:18 -0700

    Creation of the styles for each of the breakpoints

--- src/components/style-guide/style-guide.jsx
+++ src/components/style-guide/style-guide.jsx
@@ -8,134 +8,539 @@ export default class StyleGuide extends React.Component {
 		return (
 			<div className='fc-style-guide'>
 				<div><Link to='style-guide' className='style-guide' /></div>
-				<div>This is the style guide</div>
-				<h2>Grid</h2>
+				<h2>Small Grid</h2>
+				<p>
+					The column spacing in the small grid will stay static for any browser
+					size. It will not stack even on the phone.
+				</p>
 				<div className='fc-grid'>
-					<div className='fc-col-1-12'>
+					<div className='fc-col-sm-1-12'>
 						<div className='content-box'>
-							fc-col-1-12
+							fc-col-sm-1-12
 						</div>
 					</div>
-					<div className='fc-col-1-12'>
+					<div className='fc-col-sm-1-12'>
 						<div className='content-box'>
-							fc-col-1-12
+							fc-col-sm-1-12
 						</div>
 					</div>
-					<div className='fc-col-1-12'>
+					<div className='fc-col-sm-1-12'>
 						<div className='content-box'>
-							fc-col-1-12
+							fc-col-sm-1-12
 						</div>
 					</div>
-					<div className='fc-col-1-12'>
+					<div className='fc-col-sm-1-12'>
 						<div className='content-box'>
-							fc-col-1-12
+							fc-col-sm-1-12
 						</div>
 					</div>
-					<div className='fc-col-1-12'>
+					<div className='fc-col-sm-1-12'>
 						<div className='content-box'>
-							fc-col-1-12
+							fc-col-sm-1-12
 						</div>
 					</div>
-					<div className='fc-col-1-12'>
+					<div className='fc-col-sm-1-12'>
 						<div className='content-box'>
-							fc-col-1-12
+							fc-col-sm-1-12
 						</div>
 					</div>
-					<div className='fc-col-1-12'>
+					<div className='fc-col-sm-1-12'>
 						<div className='content-box'>
-							fc-col-1-12
+							fc-col-sm-1-12
 						</div>
 					</div>
-					<div className='fc-col-1-12'>
+					<div className='fc-col-sm-1-12'>
 						<div className='content-box'>
-							fc-col-1-12
+							fc-col-sm-1-12
 						</div>
 					</div>
-					<div className='fc-col-1-12'>
+					<div className='fc-col-sm-1-12'>
 						<div className='content-box'>
-							fc-col-1-12
+							fc-col-sm-1-12
 						</div>
 					</div>
-					<div className='fc-col-1-12'>
+					<div className='fc-col-sm-1-12'>
 						<div className='content-box'>
-							fc-col-1-12
+							fc-col-sm-1-12
 						</div>
 					</div>
-					<div className='fc-col-1-12'>
+					<div className='fc-col-sm-1-12'>
 						<div className='content-box'>
-							fc-col-1-12
+							fc-col-sm-1-12
 						</div>
 					</div>
-					<div className='fc-col-1-12'>
+					<div className='fc-col-sm-1-12'>
 						<div className='content-box'>
-							fc-col-1-12
+							fc-col-sm-1-12
 						</div>
 					</div>
 				</div>
 				<div className='fc-grid'>
-					<div className='fc-col-1-6'>
+					<div className='fc-col-sm-1-6'>
 						<div className='content-box'>
-							fc-col-1-6
+							fc-col-sm-1-6
 						</div>
 					</div>
-					<div className='fc-col-5-6'>
+					<div className='fc-col-sm-5-6'>
 						<div className='content-box'>
-							fc-col-5-6
-						</div>
-					</div>
-				</div>
-				<div className='fc-grid'>
-					<div className='fc-col-1-4'>
-						<div className='content-box'>
-							fc-col-1-4
-						</div>
-					</div>
-					<div className='fc-col-3-4'>
-						<div className='content-box'>
-							fc-col-3-4
+							fc-col-sm-5-6
 						</div>
 					</div>
 				</div>
 				<div className='fc-grid'>
-					<div className='fc-col-1-3'>
+					<div className='fc-col-sm-1-4'>
 						<div className='content-box'>
-							fc-col-1-3
+							fc-col-sm-1-4
 						</div>
 					</div>
-					<div className='fc-col-2-3'>
+					<div className='fc-col-sm-3-4'>
 						<div className='content-box'>
-							fc-col-2-3
-						</div>
-					</div>
-				</div>
-				<div className='fc-grid'>
-					<div className='fc-col-5-12'>
-						<div className='content-box'>
-							fc-col-5-12
-						</div>
-					</div>
-					<div className='fc-col-7-12'>
-						<div className='content-box'>
-							fc-col-7-12
+							fc-col-sm-3-4
 						</div>
 					</div>
 				</div>
 				<div className='fc-grid'>
-					<div className='fc-col-1-2'>
+					<div className='fc-col-sm-1-3'>
 						<div className='content-box'>
-							fc-col-1-2
+							fc-col-sm-1-3
 						</div>
 					</div>
-					<div className='fc-col-1-2'>
+					<div className='fc-col-sm-2-3'>
 						<div className='content-box'>
-							fc-col-1-2
+							fc-col-sm-2-3
 						</div>
 					</div>
 				</div>
 				<div className='fc-grid'>
-					<div className='fc-col-1-1'>
+					<div className='fc-col-sm-5-12'>
 						<div className='content-box'>
-							fc-col-1-1
+							fc-col-sm-5-12
+						</div>
+					</div>
+					<div className='fc-col-sm-7-12'>
+						<div className='content-box'>
+							fc-col-sm-7-12
+						</div>
+					</div>
+				</div>
+				<div className='fc-grid'>
+					<div className='fc-col-sm-1-2'>
+						<div className='content-box'>
+							fc-col-sm-1-2
+						</div>
+					</div>
+					<div className='fc-col-sm-1-2'>
+						<div className='content-box'>
+							fc-col-sm-1-2
+						</div>
+					</div>
+				</div>
+				<div className='fc-grid'>
+					<div className='fc-col-sm-1-1'>
+						<div className='content-box'>
+							fc-col-sm-1-1
+						</div>
+					</div>
+				</div>
+				<h2>Medium Grid</h2>
+				<p>
+					The column spacing in the medium grid will stay static for any browser
+					greater than or equal to 768px. Below that, it will stack.
+				</p>
+				<div className='fc-grid'>
+					<div className='fc-col-md-1-12'>
+						<div className='content-box'>
+							fc-col-md-1-12
+						</div>
+					</div>
+					<div className='fc-col-md-1-12'>
+						<div className='content-box'>
+							fc-col-md-1-12
+						</div>
+					</div>
+					<div className='fc-col-md-1-12'>
+						<div className='content-box'>
+							fc-col-md-1-12
+						</div>
+					</div>
+					<div className='fc-col-md-1-12'>
+						<div className='content-box'>
+							fc-col-md-1-12
+						</div>
+					</div>
+					<div className='fc-col-md-1-12'>
+						<div className='content-box'>
+							fc-col-md-1-12
+						</div>
+					</div>
+					<div className='fc-col-md-1-12'>
+						<div className='content-box'>
+							fc-col-md-1-12
+						</div>
+					</div>
+					<div className='fc-col-md-1-12'>
+						<div className='content-box'>
+							fc-col-md-1-12
+						</div>
+					</div>
+					<div className='fc-col-md-1-12'>
+						<div className='content-box'>
+							fc-col-md-1-12
+						</div>
+					</div>
+					<div className='fc-col-md-1-12'>
+						<div className='content-box'>
+							fc-col-md-1-12
+						</div>
+					</div>
+					<div className='fc-col-md-1-12'>
+						<div className='content-box'>
+							fc-col-md-1-12
+						</div>
+					</div>
+					<div className='fc-col-md-1-12'>
+						<div className='content-box'>
+							fc-col-md-1-12
+						</div>
+					</div>
+					<div className='fc-col-md-1-12'>
+						<div className='content-box'>
+							fc-col-md-1-12
+						</div>
+					</div>
+				</div>
+				<div className='fc-grid'>
+					<div className='fc-col-md-1-6'>
+						<div className='content-box'>
+							fc-col-md-1-6
+						</div>
+					</div>
+					<div className='fc-col-md-5-6'>
+						<div className='content-box'>
+							fc-col-md-5-6
+						</div>
+					</div>
+				</div>
+				<div className='fc-grid'>
+					<div className='fc-col-md-1-4'>
+						<div className='content-box'>
+							fc-col-md-1-4
+						</div>
+					</div>
+					<div className='fc-col-md-3-4'>
+						<div className='content-box'>
+							fc-col-md-3-4
+						</div>
+					</div>
+				</div>
+				<div className='fc-grid'>
+					<div className='fc-col-md-1-3'>
+						<div className='content-box'>
+							fc-col-md-1-3
+						</div>
+					</div>
+					<div className='fc-col-md-2-3'>
+						<div className='content-box'>
+							fc-col-md-2-3
+						</div>
+					</div>
+				</div>
+				<div className='fc-grid'>
+					<div className='fc-col-md-5-12'>
+						<div className='content-box'>
+							fc-col-md-5-12
+						</div>
+					</div>
+					<div className='fc-col-md-7-12'>
+						<div className='content-box'>
+							fc-col-md-7-12
+						</div>
+					</div>
+				</div>
+				<div className='fc-grid'>
+					<div className='fc-col-md-1-2'>
+						<div className='content-box'>
+							fc-col-md-1-2
+						</div>
+					</div>
+					<div className='fc-col-md-1-2'>
+						<div className='content-box'>
+							fc-col-md-1-2
+						</div>
+					</div>
+				</div>
+				<div className='fc-grid'>
+					<div className='fc-col-md-1-1'>
+						<div className='content-box'>
+							fc-col-md-1-1
+						</div>
+					</div>
+				</div>
+				<h2>Large Grid</h2>
+				<p>
+					The column spacing in the large grid will stay static for any browser
+					1280px and larger. Below that, it will stack.
+				</p>
+				<div className='fc-grid'>
+					<div className='fc-col-lg-1-12'>
+						<div className='content-box'>
+							fc-col-lg-1-12
+						</div>
+					</div>
+					<div className='fc-col-lg-1-12'>
+						<div className='content-box'>
+							fc-col-lg-1-12
+						</div>
+					</div>
+					<div className='fc-col-lg-1-12'>
+						<div className='content-box'>
+							fc-col-lg-1-12
+						</div>
+					</div>
+					<div className='fc-col-lg-1-12'>
+						<div className='content-box'>
+							fc-col-lg-1-12
+						</div>
+					</div>
+					<div className='fc-col-lg-1-12'>
+						<div className='content-box'>
+							fc-col-lg-1-12
+						</div>
+					</div>
+					<div className='fc-col-lg-1-12'>
+						<div className='content-box'>
+							fc-col-lg-1-12
+						</div>
+					</div>
+					<div className='fc-col-lg-1-12'>
+						<div className='content-box'>
+							fc-col-lg-1-12
+						</div>
+					</div>
+					<div className='fc-col-lg-1-12'>
+						<div className='content-box'>
+							fc-col-lg-1-12
+						</div>
+					</div>
+					<div className='fc-col-lg-1-12'>
+						<div className='content-box'>
+							fc-col-lg-1-12
+						</div>
+					</div>
+					<div className='fc-col-lg-1-12'>
+						<div className='content-box'>
+							fc-col-lg-1-12
+						</div>
+					</div>
+					<div className='fc-col-lg-1-12'>
+						<div className='content-box'>
+							fc-col-lg-1-12
+						</div>
+					</div>
+					<div className='fc-col-lg-1-12'>
+						<div className='content-box'>
+							fc-col-lg-1-12
+						</div>
+					</div>
+				</div>
+				<div className='fc-grid'>
+					<div className='fc-col-lg-1-6'>
+						<div className='content-box'>
+							fc-col-lg-1-6
+						</div>
+					</div>
+					<div className='fc-col-lg-5-6'>
+						<div className='content-box'>
+							fc-col-lg-5-6
+						</div>
+					</div>
+				</div>
+				<div className='fc-grid'>
+					<div className='fc-col-lg-1-4'>
+						<div className='content-box'>
+							fc-col-lg-1-4
+						</div>
+					</div>
+					<div className='fc-col-lg-3-4'>
+						<div className='content-box'>
+							fc-col-lg-3-4
+						</div>
+					</div>
+				</div>
+				<div className='fc-grid'>
+					<div className='fc-col-lg-1-3'>
+						<div className='content-box'>
+							fc-col-lg-1-3
+						</div>
+					</div>
+					<div className='fc-col-lg-2-3'>
+						<div className='content-box'>
+							fc-col-lg-2-3
+						</div>
+					</div>
+				</div>
+				<div className='fc-grid'>
+					<div className='fc-col-lg-5-12'>
+						<div className='content-box'>
+							fc-col-lg-5-12
+						</div>
+					</div>
+					<div className='fc-col-lg-7-12'>
+						<div className='content-box'>
+							fc-col-lg-7-12
+						</div>
+					</div>
+				</div>
+				<div className='fc-grid'>
+					<div className='fc-col-lg-1-2'>
+						<div className='content-box'>
+							fc-col-lg-1-2
+						</div>
+					</div>
+					<div className='fc-col-lg-1-2'>
+						<div className='content-box'>
+							fc-col-lg-1-2
+						</div>
+					</div>
+				</div>
+				<div className='fc-grid'>
+					<div className='fc-col-lg-1-1'>
+						<div className='content-box'>
+							fc-col-lg-1-1
+						</div>
+					</div>
+				</div>
+				<h2>Extra Large Grid</h2>
+				<p>
+					The column spacing in the large grid will stay static for any browser
+					greater than or equal to 1441px. Below that, it will stack.
+				</p>
+				<div className='fc-grid'>
+					<div className='fc-col-xl-1-12'>
+						<div className='content-box'>
+							fc-col-xl-1-12
+						</div>
+					</div>
+					<div className='fc-col-xl-1-12'>
+						<div className='content-box'>
+							fc-col-xl-1-12
+						</div>
+					</div>
+					<div className='fc-col-xl-1-12'>
+						<div className='content-box'>
+							fc-col-xl-1-12
+						</div>
+					</div>
+					<div className='fc-col-xl-1-12'>
+						<div className='content-box'>
+							fc-col-xl-1-12
+						</div>
+					</div>
+					<div className='fc-col-xl-1-12'>
+						<div className='content-box'>
+							fc-col-xl-1-12
+						</div>
+					</div>
+					<div className='fc-col-xl-1-12'>
+						<div className='content-box'>
+							fc-col-xl-1-12
+						</div>
+					</div>
+					<div className='fc-col-xl-1-12'>
+						<div className='content-box'>
+							fc-col-xl-1-12
+						</div>
+					</div>
+					<div className='fc-col-xl-1-12'>
+						<div className='content-box'>
+							fc-col-xl-1-12
+						</div>
+					</div>
+					<div className='fc-col-xl-1-12'>
+						<div className='content-box'>
+							fc-col-xl-1-12
+						</div>
+					</div>
+					<div className='fc-col-xl-1-12'>
+						<div className='content-box'>
+							fc-col-xl-1-12
+						</div>
+					</div>
+					<div className='fc-col-xl-1-12'>
+						<div className='content-box'>
+							fc-col-xl-1-12
+						</div>
+					</div>
+					<div className='fc-col-xl-1-12'>
+						<div className='content-box'>
+							fc-col-xl-1-12
+						</div>
+					</div>
+				</div>
+				<div className='fc-grid'>
+					<div className='fc-col-xl-1-6'>
+						<div className='content-box'>
+							fc-col-xl-1-6
+						</div>
+					</div>
+					<div className='fc-col-xl-5-6'>
+						<div className='content-box'>
+							fc-col-xl-5-6
+						</div>
+					</div>
+				</div>
+				<div className='fc-grid'>
+					<div className='fc-col-xl-1-4'>
+						<div className='content-box'>
+							fc-col-xl-1-4
+						</div>
+					</div>
+					<div className='fc-col-xl-3-4'>
+						<div className='content-box'>
+							fc-col-xl-3-4
+						</div>
+					</div>
+				</div>
+				<div className='fc-grid'>
+					<div className='fc-col-xl-1-3'>
+						<div className='content-box'>
+							fc-col-xl-1-3
+						</div>
+					</div>
+					<div className='fc-col-xl-2-3'>
+						<div className='content-box'>
+							fc-col-xl-2-3
+						</div>
+					</div>
+				</div>
+				<div className='fc-grid'>
+					<div className='fc-col-xl-5-12'>
+						<div className='content-box'>
+							fc-col-xl-5-12
+						</div>
+					</div>
+					<div className='fc-col-xl-7-12'>
+						<div className='content-box'>
+							fc-col-xl-7-12
+						</div>
+					</div>
+				</div>
+				<div className='fc-grid'>
+					<div className='fc-col-xl-1-2'>
+						<div className='content-box'>
+							fc-col-xl-1-2
+						</div>
+					</div>
+					<div className='fc-col-xl-1-2'>
+						<div className='content-box'>
+							fc-col-xl-1-2
+						</div>
+					</div>
+				</div>
+				<div className='fc-grid'>
+					<div className='fc-col-xl-1-1'>
+						<div className='content-box'>
+							fc-col-xl-1-1
 						</div>
 					</div>
 				</div>
