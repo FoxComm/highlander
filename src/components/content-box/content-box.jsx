57de296633712ce391fe21d820acb9f0c307e17f
Eugene Sypachev <eugene.sypachev@gmail.com>
2015-10-07 16:37:45 +0300

    ContentBox component

---
+++ src/components/content-box/content-box.jsx
@@ -0,0 +1,24 @@
+'use strict';
+
+import {React, PropTypes} from 'react';
+
+export default class ContentBox extends React.Component {
+
+  static propTypes = {
+    title: PropTypes.string,
+    className: PropTypes.string,
+    children: PropTypes.node
+  }
+
+  render() {
+    let rootClassName = `${this.props.className} fc-content-box`;
+    return (
+      <div className={ rootClassName }>
+        <header>{ this.props.title }</header>
+        <article>
+          { this.props.children }
+        </article>
+      </div>
+    );
+  }
+}
