5cedfadc5fa808c822f70a520419c6980f3ed5b4
Denys Mikhalenko <prontiol@gmail.com>
2015-10-07 19:46:46 +0300

    Fix propType

--- src/components/section-title/title.jsx
+++ src/components/section-title/title.jsx
@@ -5,10 +5,7 @@ import React from 'react';
 export default class Title extends React.Component {
   static propTypes = {
     title: React.PropTypes.string,
-    subtitle: React.PropTypes.oneOfType([
-      React.PropTypes.string,
-      React.PropTypes.number
-    ])
+    subtitle: React.PropTypes.node
   };
 
   render() {
