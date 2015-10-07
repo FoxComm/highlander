06b2fcd6c80e5f471690d208a24edce69e2852f4
Eugene Sypachev <eugene.sypachev@gmail.com>
2015-10-07 19:09:44 +0300

    wire customer with store

--- src/components/customers/store.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-import BaseStore from '../../lib/base-store';
-
-class CustomerStore extends BaseStore {
-  get baseUri() { return '/customers'; }
-}
-
-export default new CustomerStore();
