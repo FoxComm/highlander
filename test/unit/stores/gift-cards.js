9dc37272d65ec98189d28fb0a1fc9d2135d84ca0
Cameron Stitt <cameron@cam.st>
2015-10-06 17:47:35 -0700

    Progress [#105013504]. Store tests.

--- test/unit/stores/gift-cards.js
+++ test/unit/stores/gift-cards.js
@@ -6,10 +6,11 @@ const sinon = require('sinon');
 
 describe('GiftCard Store', function() {
   const alt = require(path.resolve('src/alt'));
+  const Api = require(path.resolve('src/lib/api'));
   const giftCardStore = require(path.resolve('src/stores/gift-cards'));
   const giftCardActions = require(path.resolve('src/actions/gift-cards'));
 
-  it('listens for update gift card action', function () {
+  it('listens for UPDATE_GIFT_CARDS', function () {
     alt.dispatcher.dispatch({
       action: giftCardActions.UPDATE_GIFT_CARDS,
       data: [1]
@@ -18,9 +19,23 @@ describe('GiftCard Store', function() {
     assert(giftCardStore.getState().giftCards.size === 1);
   });
 
-  it('updates giftCards on fetchGiftCards', function () {
+  it('updates giftCards on FETCH_GIFT_CARDS', function () {
+    let stub = sinon.stub(Api, 'get').returns(Promise.resolve([1]));
     alt.dispatcher.dispatch({
       action: giftCardActions.FETCH_GIFT_CARDS
     });
+
+    assert(giftCardStore.getState().giftCards.size === 1);
+    stub.restore();
+  });
+
+  it('listens for GIFT_CARDS_FAILED', function() {
+    let spy = sinon.spy(console, 'error');
+    alt.dispatcher.dispatch({
+      action: giftCardActions.GIFT_CARDS_FAILED
+    });
+
+    assert(spy.calledOnce);
+    spy.restore();
   });
 });
