b7b39ab904c7800b462ff387199990a5de683793
Eugene Sypachev <eugene.sypachev@gmail.com>
2015-10-07 20:59:45 +0300

    title block for customer page

--- src/components/customers/title-block.jsx
+++ src/components/customers/title-block.jsx
@@ -30,6 +30,46 @@ export default class Customer extends React.Component {
             {this.customer.email}
           </div>
         </div>
+        <article className="fc-customer-info-body">
+          <div className="fc-grid">
+            <div className="fc-col-1-2">
+              <ul className="fc-customer-info-fields">
+                <li>
+                  <i className="icon-customer"></i><span>{ this.customer.id }</span>
+                </li>
+                <li>
+                  <i className="icon-phone"></i><span>{ this.customer.phoneNumber }</span>
+                </li>
+                <li>
+                  <i className="icon-location"></i><span>{ this.customer.location }</span>
+                </li>
+                <li>
+                  <i className="icon-calendar"></i>
+                  <span>{ this.customer.createdAt }</span>
+                  <span className="fc-comment">Date joined</span>
+                </li>
+              </ul>
+            </div>
+            <div className="fc-col-1-2">
+              <ul className="fc-customer-info-fields">
+                <li>
+                  <i className="icon-mobile"></i><span>{ this.customer.modality }</span>
+                </li>
+                <li>
+                  <i className="icon-usd"></i>
+                  <span>{ this.customer.totalSpent}</span>
+                  <span className="fc-comment">Total Sales</span>
+                </li>
+                <li>
+                  <i>{ this.customer.id}</i><span>Since last visit</span>
+                </li>
+                <li>
+                  <i>{ this.customer.id}</i><span>Since last visit</span>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </article>
       </div>
     );
   }
