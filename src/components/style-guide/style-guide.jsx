8065973bb8c449e906a61f12c46ced3a49255e7c
Jeff Mataya <jeff@foxcommerce.com>
2015-10-08 17:37:26 -0700

    Finish components for style guide grid

--- src/components/style-guide/style-guide.jsx
+++ src/components/style-guide/style-guide.jsx
@@ -2,91 +2,29 @@
 
 import React from 'react';
 import { Link } from '../link';
-import DescriptiveGridColumn from './descriptive-grid-column';
+import StyleGuideGrid from './style-guide-grid';
 
 export default class StyleGuide extends React.Component {
-  getGrid(size) {
-    let rows = [];
-    for (var i = 0; i < 12; i++) {
-      rows.push(<DescriptiveGridColumn size={ size } numerator="1" denominator="12" />);
-    }
-
-    return (
-      <div>
-        <div className='fc-grid'>
-          {rows}
-        </div>
-        <div className='fc-grid'>
-          <DescriptiveGridColumn size={ size } numerator="1" denominator="6" />
-          <DescriptiveGridColumn size={ size } numerator="5" denominator="6" />
-        </div>
-        <div className='fc-grid'>
-          <DescriptiveGridColumn size={ size } numerator="1" denominator="4" />
-          <DescriptiveGridColumn size={ size } numerator="3" denominator="4" />
-        </div>
-        <div className='fc-grid'>
-          <DescriptiveGridColumn size={ size } numerator="1" denominator="3" />
-          <DescriptiveGridColumn size={ size } numerator="2" denominator="3" />
-        </div>
-        <div className='fc-grid'>
-          <DescriptiveGridColumn size={ size } numerator="5" denominator="12" />
-          <DescriptiveGridColumn size={ size } numerator="7" denominator="12" />
-        </div>
-        <div className='fc-grid'>
-          <DescriptiveGridColumn size={ size } numerator="1" denominator="2" />
-          <DescriptiveGridColumn size={ size } numerator="1" denominator="2" />
-        </div>
-        <div className='fc-grid'>
-          <DescriptiveGridColumn size={ size } numerator="1" denominator="1" />
-        </div>
-      </div>
-      );
-  }
-
   render() {
     return (
       <div className='fc-style-guide'>
         <div><Link to='style-guide' className='style-guide' /></div>
-        <div className='fc-grid'>
-          <div className='fc-col-sm-1-1'>
-            <h2>Small Grid</h2>
-            <p>
-              The column spacing in the small grid will stay static for any browser
-              size. It will not stack even on the phone.
-            </p>
-          </div>
-        </div>
-        { this.getGrid('sm') }
-        <div className='fc-grid'>
-          <div className='fc-col-sm-1-1'>
-            <h2>Medium Grid</h2>
-            <p>
-              The column spacing in the medium grid will stay static for any browser
-              greater than or equal to 768px. Below that, it will stack.
-            </p>
-          </div>
-        </div>
-        { this.getGrid('md') }
-        <div className='fc-grid'>
-          <div className='fc-col-md-1-1'>
-            <h2>Large Grid</h2>
-            <p>
-              The column spacing in the large grid will stay static for any browser
-              1280px and larger. Below that, it will stack.
-            </p>
-          </div>
-        </div>
-        { this.getGrid('lg') }
-        <div className='fc-grid'>
-          <div className='fc-col-md-1-1'>
-            <h2>Extra Large Grid</h2>
-            <p>
-              The column spacing in the large grid will stay static for any browser
-              greater than or equal to 1441px. Below that, it will stack.
-            </p>
-          </div>
-        </div>
-        { this.getGrid('xl') }
+        <StyleGuideGrid
+          name="Small Grid"
+          description="The column spacing in the small grid will stay static for any browser size. It will not stack even on the phone."
+          size="sm" />
+        <StyleGuideGrid
+          name="Medium Grid"
+          description="The column spacing in the medium grid will stay static for any browser greater than or equal to 768px. Below that, it will stack."
+          size="md" />
+        <StyleGuideGrid
+          name="Large Grid"
+          description="The column spacing in the large grid will stay static for any browser 1280px and larger. Below that, it will stack."
+          size="lg" />
+        <StyleGuideGrid
+          name="Extra Large Grid"
+          description="The column spacing in the extra large grid will stay static for any browser greater than or equal to 1441px. Below that, it will stack."
+          size="xl" />
         <div className='fc-grid'>
           <div className='fc-col-sm-1-1'>
             <h2>Variable Grid</h2>
