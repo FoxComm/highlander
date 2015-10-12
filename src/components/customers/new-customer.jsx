5cb895b4744e2c0b188a61c25affe78dad3d4c0e
Eugene Sypachev <eugene.sypachev@gmail.com>
2015-10-12 20:22:41 +0400

    new customer page

---
+++ src/components/customers/new-customer.jsx
@@ -0,0 +1,16 @@
+'use strict';
+
+import React from 'react';
+import CustomerStore from '../../stores/customers';
+import CustomerActions from '../../actions/customers';
+
+export default class NewCustomer extends React.Component {
+
+  render () {
+    return (
+      <div className="customer-create">
+        Customer form will be here
+      </div>
+    );
+  }
+}
