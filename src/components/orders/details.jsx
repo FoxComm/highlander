93deaa62781b812494a8e0f96593442128abeff2
Jeff Mataya <jeff@foxcommerce.com>
2015-10-14 13:15:58 -0700

    Clean up the code conventions of OrderDetails

--- src/components/orders/details.jsx
+++ src/components/orders/details.jsx
@@ -8,48 +8,45 @@ import OrderShippingAddress from './shipping-address';
 import OrderShippingMethod from './shipping-method';
 import OrderPayment from './payment';
 import OrderStore from './../../stores/orders';
-import { dispatch } from '../../lib/dispatcher';
-import Api from '../../lib/api';
 
 export default class OrderDetails extends React.Component {
-  updateLineItems(data) {
+  static propTypes = {
+    order: React.PropTypes.object
+  };
+
+  getDefaultProps() {
+    return {
+      columns: [
+        {field: 'imagePath', text: 'Image', type: 'image'},
+        {field: 'name', text: 'Name'},
+        {field: 'skuId', text: 'SKU'},
+        {field: 'price', text: 'Price', type: 'currency'},
+        {field: 'qty', text: 'Quantity'},
+        {field: 'total', text: 'Total', type: 'currency'},
+        {field: 'status', text: 'Shipping Status'}
+      ]
+    };
   }
 
   render() {
-    let order = this.props.order;
-    let lineColumns = [
-      {field: 'imagePath', text: 'Image', type: 'image'},
-      {field: 'name', text: 'Name'},
-      {field: 'skuId', text: 'SKU'},
-      {field: 'price', text: 'Price', type: 'currency'},
-      {field: 'qty', text: 'Quantity'},
-      {field: 'total', text: 'Total', type: 'currency'},
-      {field: 'status', text: 'Shipping Status'}
-    ];
-
     return (
       <div className="fc-order-details">
         <div className="fc-order-details-body">
           <div className="fc-order-details-main">
             <LineItems
-              entity={order}
-              tableColumns={lineColumns}
-              model={'order'}
-              onChange={this.updateLineItems.bind(this)} />
-            <OrderShippingAddress order={order} />
-            <OrderShippingMethod order={order} />
-            <OrderPayment order={order} />
+              entity={this.props.order}
+              tableColumns={this.props.columns}
+              model={'order'} />
+            <OrderShippingAddress order={this.props.order} />
+            <OrderShippingMethod order={this.props.order} />
+            <OrderPayment order={this.props.order} />
           </div>
           <div className="fc-order-details-aside">
-            <OrderSummary order={order} />
-            <CustomerInfo order={order} />
+            <OrderSummary order={this.props.order} />
+            <CustomerInfo order={this.props.order} />
           </div>
         </div>
       </div>
     );
   }
 }
-
-OrderDetails.propTypes = {
-  order: React.PropTypes.object
-};
