47c6f2700d3575a80371033c3561b224232e8ff1
Sergey Rublev <narma.nsk@gmail.com>
2015-10-16 01:18:08 +0600

    Don't emit empty password when it is null

--- app/services/Authenticators.scala
+++ app/services/Authenticators.scala
@@ -15,22 +15,22 @@ object Authenticator {
 
   def customer(credentials: UserCredentials)
               (implicit ec: ExecutionContext, db: Database): Future[Option[Customer]] = {
-    auth[Customer, EmailFinder[Customer]](credentials, Customers.findByEmail, (m) ⇒ m.password.getOrElse(""))
+    auth[Customer, EmailFinder[Customer]](credentials, Customers.findByEmail, _.password)
   }
 
   def storeAdmin(credentials: UserCredentials)
                 (implicit ec: ExecutionContext, db: Database): Future[Option[StoreAdmin]] = {
-    auth[StoreAdmin, EmailFinder[StoreAdmin]](credentials, StoreAdmins.findByEmail, _.password)
+    auth[StoreAdmin, EmailFinder[StoreAdmin]](credentials, StoreAdmins.findByEmail, (m) ⇒ Some(m.password))
   }
 
-  private[this] def auth[M, F <: EmailFinder[M]](credentials: UserCredentials, finder: F, getPassword: M ⇒ String)
+  private[this] def auth[M, F <: EmailFinder[M]](credentials: UserCredentials, finder: F, getPassword: M ⇒
+    Option[String])
    (implicit ec: ExecutionContext, db: Database): Future[Option[M]] = credentials match {
 
     case p: UserCredentials.Provided ⇒
       finder(p.username).run().map { optModel ⇒
-        optModel.filter { m ⇒ p.verifySecret(getPassword(m)) }
+        optModel.filter { m ⇒ getPassword(m).map(p.verifySecret(_)).getOrElse(false) }
       }
-
     case _ ⇒
       Future.successful(None)
   }
