15dcfba2a988b0c46e59138b9e451e2e7b7d43e5
Jeff Mataya <jeff@foxcommerce.com>
2015-10-14 21:03:48 -0700

    Move the shipping method over to redux

--- src/components/orders/shipping-method.jsx
+++ src/components/orders/shipping-method.jsx
@@ -1,60 +1,35 @@
 'use strict';
 
-import _ from 'lodash';
 import React from 'react';
-import Api from '../../lib/api';
 import TableHead from '../tables/head';
 import TableBody from '../tables/body';
-import ShippingMethodItem from './shipping-method-item';
-import ShippingMethods from '../../stores/shipping-methods';
 
 export default class OrderShippingMethod extends React.Component {
+  static propTypes = {
+    order: React.PropTypes.object
+  }
+
+  static defaultProps = {
+    viewColumns: [
+      {field: 'name', text: 'Method'},
+      {field: 'price', text: 'Price', type: 'currency'}
+    ]
+  }
+
   constructor(props, context) {
     super(props, context);
-    this.state = {
-      methods: []
-    };
-  }
-
-  componentDidMount() {
-    ShippingMethods.listenToEvent('change', this);
-    ShippingMethods.fetch(this.props.order.referenceNumber);
-  }
-
-  componentWillUnmount() {
-    ShippingMethods.stopListeningToEvent('change', this);
-  }
-
-  onChangeShippingMethods(methods) {
-    this.setState({methods});
   }
 
   render() {
-    let methods = this.props.isEditing ? this.state.methods : _.filter(this.state.methods, {isActive: true});
-
     return (
       <section className="fc-content-box" id="order-shipping-method">
         <header>Shipping Method</header>
         <table className="fc-table">
-          <TableHead columns={this.props.tableColumns} />
-          <TableBody columns={this.props.tableColumns} rows={methods} model='shipping-method'>
-            <ShippingMethodItem isEditing={this.props.isEditing} />
+          <TableHead columns={this.props.viewColumns} />
+          <TableBody columns={this.props.viewColumns} rows={[this.props.order.shippingMethod]} model='shipping-method'>
           </TableBody>
         </table>
       </section>
     );
   }
 }
-
-OrderShippingMethod.propTypes = {
-  order: React.PropTypes.object,
-  isEditing: React.PropTypes.bool,
-  tableColumns: React.PropTypes.array
-};
-
-OrderShippingMethod.defaultProps = {
-  tableColumns: [
-    {field: null, text: 'Method', component: 'ShippingMethodItem'},
-    {field: 'defaultPrice', text: 'Price', type: 'currency'}
-  ]
-};
