c25d3a2cd2f2a165bf6286fa93aef5ae9a5ca507
Eugene Sypachev <eugene.sypachev@gmail.com>
2015-10-16 21:19:43 +0400

    address-details explicit height

--- src/components/addresses/address-details.jsx
+++ src/components/addresses/address-details.jsx
@@ -29,7 +29,7 @@ class AddressDetails extends React.Component {
 
     return (
       <ul className="fc-address-details">
-        <li className="name"><strong>{address.name}</strong></li>
+        <li className="name">{address.name}</li>
         <li>{address.address1}</li>
         { address.address2 ? <li>{address.address2}</li> : '' }
         <li>
