48482ce413ed6a333c24a620f2a9cb5e206a6624
Jeff Mataya <jeff@foxcommerce.com>
2015-10-15 19:09:13 -0700

    Functionify LineItemCounter

--- src/components/line-items/line-item-counter.jsx
+++ src/components/line-items/line-item-counter.jsx
@@ -12,26 +12,17 @@ const confirmOptions = {
   proceed: 'Yes, Delete'
 };
 
-export default class LineItemCounter extends React.Component {
-  static propTypes = {
-    model: React.PropTypes.object,
-    entityName: React.PropTypes.string,
-    entity: React.PropTypes.object,
-    stepUp: React.PropTypes.func,
-    stepDown: React.PropTypes.func
-  };
-
-  render() {
-    return (
-      <Counter
-        defaultValue='quantity'
-        stepAmount={1}
-        minValue={0}
-        maxValue={1000000}
-        model={this.props.model}
-        stepUp={this.props.stepUp}
-        stepDown={this.props.stepDown}
-      />
-    );
-  }
+let LineItemCounter = (props) => {
+  return (
+    <Counter
+      defaultValue='quantity'
+      stepAmount={1}
+      minValue={0}
+      maxValue={1000000}
+      model={props.model}
+      stepUp={props.stepUp}
+      stepDown={props.stepDown} />
+  );
 }
+
+export default LineItemCounter;
