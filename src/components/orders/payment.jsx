d3aa9cef90bb590191b111bb235e865e0dc18788
Jeff Mataya <jeff@foxcommerce.com>
2015-10-18 11:39:14 -0700

    Make order payment a function

--- src/components/orders/payment.jsx
+++ src/components/orders/payment.jsx
@@ -7,29 +7,27 @@ import PaymentMethod from './payment-method';
 import TableHead from '../tables/head';
 import TableBody from '../tables/body';
 
-export default class OrderPayment extends React.Component {
-  render() {
-    const order = this.props.order.currentOrder;
+const OrderPayment = (props) => {
+  const order = props.order.currentOrder;
 
-    const viewContent = (
-      <table className="fc-table">
-        <TableHead columns={this.props.tableColumns}/>
-        <TableBody columns={this.props.tableColumns} rows={_.compact([order.payment])} model='payment-method'>
-          <PaymentMethod/>
-        </TableBody>
-      </table>
-    );
+  const viewContent = (
+    <table className="fc-table">
+      <TableHead columns={props.tableColumns}/>
+      <TableBody columns={props.tableColumns} rows={_.compact([order.payment])} model='payment-method'>
+        <PaymentMethod/>
+      </TableBody>
+    </table>
+  );
 
-    return (
-      <EditableContentBox
-        className='fc-order-payment'
-        title='Payment'
-        isEditing={false}
-        editAction={() => console.log('Not implemented')}
-        viewContent={viewContent} />
-    );
-  }
-}
+  return (
+    <EditableContentBox
+      className='fc-order-payment'
+      title='Payment'
+      isEditing={false}
+      editAction={() => console.log('Not implemented')}
+      viewContent={viewContent} />
+  );
+};
 
 OrderPayment.propTypes = {
   order: React.PropTypes.object,
@@ -44,3 +42,5 @@ OrderPayment.defaultProps = {
     {field: 'createdAt', text: 'Date/Time', type: 'date'}
   ]
 };
+
+export default OrderPayment;
