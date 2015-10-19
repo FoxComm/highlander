c1ba38a04473b993d7735dce7cf95bd6bae80870
Jeff Mataya <jeff@foxcommerce.com>
2015-10-19 11:04:06 -0700

    Get rid of the reducers folder and move to module

--- src/modules/index.js
+++ src/modules/index.js
@@ -2,10 +2,10 @@
 
 import { routerStateReducer } from 'redux-router';
 import { combineReducers } from 'redux';
-import giftCards from '../modules/gift-cards';
-import customers from '../modules/customers';
-import orders from '../modules/orders';
-import order from '../modules/order'; 
+import giftCards from './gift-cards';
+import customers from './customers';
+import orders from './orders';
+import order from './order';
 
 const rootReducer = combineReducers({
   router: routerStateReducer,
--- src/store.js
+++ src/store.js
@@ -3,7 +3,7 @@
 import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import createLogger from 'redux-logger';
-import rootReducer from './reducers';
+import rootReducer from './modules';
 
 export default function configureStore(reduxReactRouter, routes, createHistory, initialState) {
   let finalCreateStore = compose(
