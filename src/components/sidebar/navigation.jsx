66d1c76e0dcf177bdbfa6859029e8ef822e00b22
Eugene Sypachev <eugene.sypachev@gmail.com>
2015-10-20 19:29:13 +0400

    links and reorder

--- src/components/sidebar/navigation.jsx
+++ src/components/sidebar/navigation.jsx
@@ -9,11 +9,14 @@ export default class Navigation extends React.Component {
       <nav>
         <ul>
           <li><IndexLink to='home'><i className="icon-home"></i>Dashboard</IndexLink></li>
-          <li><Link to='rmas'><i className="icon-returns"></i>Returns</Link></li>
-          <li><Link to='orders'><i className="icon-orders"></i>Orders</Link></li>
           <li><Link to="customers"><i className="icon-customers"></i>Customers</Link></li>
-          <li><a href=""><i className="icon-items"></i>Products</a></li>
+          <li><Link to='orders'><i className="icon-orders"></i>Orders</Link></li>
+          <li><Link to='rmas'><i className="icon-returns"></i>Returns</Link></li>
+          <li><a href=""><i className="icon-items"></i>Items</a></li>
+          <li><a href=""><i className="icon-inventory"></i>Inventory</a></li>
           <li><Link to="gift-cards"><i className="icon-gift-cards"></i>Gift Cards</Link></li>
+          <li><a href=""><i className="icon-discounts"></i>Discounts</a></li>
+          <li><a href=""><i className="icon-settings"></i>Settings</a></li>
         </ul>
       </nav>
     );
