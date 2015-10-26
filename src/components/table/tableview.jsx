ee7ead7665141a1dc8debebc40ef499c96a6e559
Denys Mikhalenko <prontiol@gmail.com>
2015-10-26 04:19:07 +0300

    Redux TableView

--- src/components/table/tableview.jsx
+++ src/components/table/tableview.jsx
@@ -2,57 +2,24 @@
 
 import React from 'react';
 import Table from './table';
-import TablePaginator from './paginator';
 
-export default class TableView extends React.Component {
-  static propTypes = {
-    children: React.PropTypes.any,
-    empty: React.PropTypes.any,
-    renderRow: React.PropTypes.func,
-    paginator: React.PropTypes.bool
-  };
+const TableView = (props) => {
+  return (
+    <div className="fc-tableview">
+      {props.data.items.length && (
+        <Table data={props.data} renderRow={props.renderRow}/>
+      )}
+    </div>
+  );
+};
 
-  static defaultProps = {
-    paginator: true
-  };
+TableView.propTypes = {
+  renderRow: React.PropTypes.func,
+  paginator: React.PropTypes.bool
+};
 
-  onLimitChange(event) {
-    event.preventDefault();
-    //this.props.store.setLimit(+event.target.value);
-  }
+TableView.defaultProps = {
+  paginator: true
+};
 
-  render() {
-    let showPaginator = this.props.paginator && this.props.store.models.length > this.props.store.limit;
-    let paginator = showPaginator && (
-        <TablePaginator store={this.props.store}/>
-      );
-
-    return (
-      <div className="fc-tableview">
-        {showPaginator && (
-          <div className="fc-table-header">
-            {paginator}
-          </div>
-        )}
-        {this.props.store.models.length && (
-          <Table store={this.props.store} renderRow={this.props.renderRow}/>
-        )}
-        {!this.props.store.models.length && (
-          <div>{this.props.empty}</div>
-        )}
-        {showPaginator && (
-          <div className="fc-table-footer">
-            <select onChange={this.onLimitChange.bind(this)}>
-              <option value="10">Show 10</option>
-              <option value="25">Show 25</option>
-              <option value="50">Show 50</option>
-              <option value="100">Show 100</option>
-              <option value="Infinity">Show all</option>
-            </select>
-            {paginator}
-          </div>
-        )}
-      </div>
-    );
-  }
-}
+export default TableView;
