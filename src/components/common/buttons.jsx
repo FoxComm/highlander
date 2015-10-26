0f532389777199c1a0a414e7cb45e11932b31fe4
Cameron Stitt <cameron@cam.st>
2015-10-27 08:37:13 +1000

    Re-useable button.

--- src/components/common/buttons.jsx
+++ src/components/common/buttons.jsx
@@ -1,48 +1,46 @@
 'use strict';
 
 import React from 'react';
+import _ from 'lodash';
 
-const DecrementButton = (props) => {
+const DefaultButton = (props) => {
+  const buttonProps = _.omit(props, 'icon', 'children', 'className');
   return (
-    <button onClick={props.onClick}>
-      <i className='icon-chevron-down'></i>
-    </button>
-  );
-};
-
-const DeleteButton = (props) => {
-  return (
-    <button className='fc-btn' onClick={props.onClick}>
-      <i className='icon-trash'></i>
-    </button>
-  );
-};
-
-const EditButton = (props) => {
-  return (
-    <button className='fc-btn' onClick={props.onClick}>
-      <i className='icon-edit'></i>
-    </button>
-  );
-};
-
-const IncrementButton = (props) => {
-  return (
-    <button onClick={props.onClick}>
-      <i className='icon-chevron-up'></i>
-    </button>
-  );
-};
-
-const PrimaryButton = (props) => {
-  return (
-    <button className='fc-btn fc-btn-primary' onClick={props.onClick}>
+    <button className={`fc-btn ${props.className}`} {...buttonProps}>
+      {props.icon &&
+        <i className={`icon-${props.icon}`}></i>
+      }
       {props.children}
     </button>
   );
 };
 
+const DecrementButton = (props) => {
+  return <DefaultButton icon='chevron-down' {...props} />;
+};
+
+const DeleteButton = (props) => {
+  return <DefaultButton icon='trash' {...props} />;
+};
+
+const EditButton = (props) => {
+  return <DefaultButton icon='edit' {...props} />;
+};
+
+const IncrementButton = (props) => {
+  return <DefaultButton icon='chevron-up' {...props} />
+};
+
+const PrimaryButton = (props) => {
+  return (
+    <DefaultButton className='fc-btn-primary'>
+      {props.children}
+    </DefaultButton>
+  );
+};
+
 export {
+  DefaultButton,
   DecrementButton,
   DeleteButton,
   EditButton,
