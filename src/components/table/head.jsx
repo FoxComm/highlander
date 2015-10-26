472eb41850ce6731205f8f263de19952e4be1024
Denys Mikhalenko <prontiol@gmail.com>
2015-10-26 05:03:46 +0300

    Redux TableHead

--- src/components/table/head.jsx
+++ src/components/table/head.jsx
@@ -5,40 +5,15 @@ import classNames from 'classnames';
 import TableRow from './row';
 
 export default class TableHead extends React.Component {
-
-  constructor(props, ...args) {
-    super(props, ...args);
-    this.state = {
-      sortingField: this.props.data.sortingField,
-      sortingOrder: this.props.data.sortingOrder
-    };
-  }
-
-  onHeaderItemClick(field, event) {
-    event.preventDefault();
-    this.setState({
-      sortingField: field,
-      sortingOrder: (field === this.state.sortingField) ? !this.state.sortingOrder : true
-    }, () => {
-      this.props.data.setSorting(this.state.sortingField, this.state.sortingOrder);
-    });
-  }
-
   render() {
-    let renderColumn = (column, index) => {
-      let classnames = classNames({
+    const renderColumn = (column, index) => {
+      const classnames = classNames({
         'fc-table-th': true,
-        'sorting': true,
-        'sorting-desc': (this.state.sortingField === column.field) && this.state.sortingOrder,
-        'sorting-asc': (this.state.sortingField === column.field) && !this.state.sortingOrder
+        'sorting': true
       });
       return (
-        <th
-          className={classnames}
-          key={`${index}-${column.field}`}
-          onClick={this.onHeaderItemClick.bind(this, column.field)}
-          >
-          {column.title}
+        <th className={classnames} key={`${column.field}`}>
+          {column.text}
         </th>
       );
     };
--- src/components/table/table.jsx
+++ src/components/table/table.jsx
@@ -7,6 +7,7 @@ import TableBody from './body';
 const Table = (props) => {
   return (
     <table className='fc-table'>
+      <TableHead data={props.data}/>
       <TableBody data={props.data} renderRow={props.renderRow}/>
     </table>
   );
