16d0fe3e6ae83c2650abb7fea547e541ea419a23
Eugene Sypachev <eugene.sypachev@gmail.com>
2015-10-29 13:58:40 +0300

    details as function

--- src/components/customers/details.jsx
+++ src/components/customers/details.jsx
@@ -10,55 +10,51 @@ import CustomerGroups from './groups';
 import CustomerNotificationSettings from './notification-settings';
 import CustomerAccountStatus from './account-status';
 
-export default class CustomerDetails extends React.Component {
-
-  static propTypes = {
-    entity: PropTypes.object
-  }
-
-  get customer() {
-    return this.props.entity;
-  }
-
-  render() {
-    return (
-      <div className="fc-customer-details">
-        <div className="gutter">
-          <h2>Details</h2>
+const CustomerDetails = (props) => {
+  const customer = props.entity;
+  return (
+    <div className="fc-customer-details">
+      <div className="gutter">
+        <h2>Details</h2>
+      </div>
+      <div className="fc-grid">
+        <div className="fc-col-md-1-2">
+          <CustomerContacts customer={ customer } />
         </div>
-        <div className="fc-grid">
-          <div className="fc-col-md-1-2">
-            <CustomerContacts customer={this.customer} />
-          </div>
-          <div className="fc-col-md-1-2">
-            <CustomerAccountPassword />
-            <CustomerRoles />
-          </div>
-        </div>
-        <div className="fc-grid">
-          <div className="fc-col-md-1-1">
-            <CustomerAddressBook customerId={this.customer.id} />
-          </div>
-        </div>
-        <div className="fc-grid">
-          <div className="fc-col-md-1-1">
-            <CustomerCreditCards customerId={this.customer.id} />
-          </div>
-        </div>
-        <div className="fc-grid">
-          <div className="fc-col-md-1-1">
-            <CustomerGroups />
-          </div>
-        </div>
-        <div className="fc-grid">
-          <div className="fc-col-md-1-2">
-            <CustomerNotificationSettings />
-          </div>
-          <div className="fc-col-md-1-2">
-            <CustomerAccountStatus customer={this.customer}/>
-          </div>
+        <div className="fc-col-md-1-2">
+          <CustomerAccountPassword />
+          <CustomerRoles />
         </div>
       </div>
-    );
-  }
-}
+      <div className="fc-grid">
+        <div className="fc-col-md-1-1">
+          <CustomerAddressBook customerId={ customer.id } />
+        </div>
+      </div>
+      <div className="fc-grid">
+        <div className="fc-col-md-1-1">
+          <CustomerCreditCards customerId={ customer.id } />
+        </div>
+      </div>
+      <div className="fc-grid">
+        <div className="fc-col-md-1-1">
+          <CustomerGroups />
+        </div>
+      </div>
+      <div className="fc-grid">
+        <div className="fc-col-md-1-2">
+          <CustomerNotificationSettings />
+        </div>
+        <div className="fc-col-md-1-2">
+          <CustomerAccountStatus customer={ customer }/>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+CustomerDetails.propTypes = {
+    entity: PropTypes.object
+};
+
+export default CustomerDetails;
