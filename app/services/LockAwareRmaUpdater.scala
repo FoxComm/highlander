771387c3f8a4595a640a2b8f0012800a51d1f644
Pavel Kiselev <kpashka@gmail.com>
2015-10-30 23:31:30 +0600

    Minor fixes

--- app/services/LockAwareRmaUpdater.scala
+++ app/services/LockAwareRmaUpdater.scala
@@ -4,6 +4,7 @@ import scala.concurrent.ExecutionContext
 import models.RmaLockEvents.scope._
 import models._
 import responses.RmaResponse
+import responses.RmaResponse.Root
 import slick.driver.PostgresDriver.api._
 import utils.Slick.UpdateReturning._
 import utils.Slick._
@@ -14,7 +15,7 @@ object LockAwareRmaUpdater {
   private val updatedRma = Rmas.map(identity)
 
   def lock(refNum: String, admin: StoreAdmin)
-    (implicit ec: ExecutionContext, db: Database): Result[RmaResponse.Root] = {
+    (implicit ec: ExecutionContext, db: Database): Result[Root] = {
     val finder = Rmas.findByRefNum(refNum)
 
     finder.selectOneForUpdate { rma ⇒
@@ -25,7 +26,7 @@ object LockAwareRmaUpdater {
     }
   }
 
-  def unlock(refNum: String)(implicit db: Database, ec: ExecutionContext): Result[RmaResponse.Root] = {
+  def unlock(refNum: String)(implicit db: Database, ec: ExecutionContext): Result[Root] = {
     Rmas.findByRefNum(refNum).selectOneForUpdate ({ rma ⇒
       if (rma.locked) {
         RmaLockEvents.findByRma(rma).mostRecentLock.one.flatMap { _ ⇒ doUnlock(rma.id) }
@@ -33,10 +34,10 @@ object LockAwareRmaUpdater {
     }, checks = Set.empty)
   }
 
-  private def doUnlock(rmaId: Int)(implicit ec: ExecutionContext, db: Database) = {
+  private def doUnlock(rmaId: Int)(implicit ec: ExecutionContext, db: Database): DbResult[Root] = {
     Rmas.findById(rmaId).extract
-      .map { rma ⇒ rma.locked }
+      .map(_.locked)
       .updateReturning(updatedRma, false).head
-      .flatMap { rma ⇒ DbResult.fromDbio(RmaResponse.fromRma(rma)) }
+      .flatMap(rma ⇒ DbResult.fromDbio(RmaResponse.fromRma(rma)))
   }
 }
