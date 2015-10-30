9a9747287e2398fe62d4d8ccea6a349cdabb71e6
Denys Mikhalenko <prontiol@gmail.com>
2015-10-30 04:36:53 +0300

    Remove redundant parentheses

--- src/components/table/tableview.jsx
+++ src/components/table/tableview.jsx
@@ -4,19 +4,17 @@ import React, { PropTypes } from 'react';
 import Table from './table';
 import TablePaginator from './paginator';
 
-const TableView = (props) => {
-  return (
-    <div className="fc-tableview">
-      <TablePaginator
-        total={props.data.total}
-        from={props.data.from}
-        size={props.data.size}
-        setFrom={props.setFrom}
-        />
-      <Table columns={props.columns} data={props.data} renderRow={props.renderRow}/>
-    </div>
-  );
-};
+const TableView = (props) => (
+  <div className="fc-tableview">
+    <TablePaginator
+      total={props.data.total}
+      from={props.data.from}
+      size={props.data.size}
+      setFrom={props.setFrom}
+      />
+    <Table columns={props.columns} data={props.data} renderRow={props.renderRow}/>
+  </div>
+);
 
 TableView.propTypes = {
   columns: PropTypes.array.isRequired,
