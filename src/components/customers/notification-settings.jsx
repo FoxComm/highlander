a43fe9cf4595475c96eedc56a08af3eb371ffb90
Eugene Sypachev <eugene.sypachev@gmail.com>
2015-11-03 21:37:24 +0400

    checkboxes in notification settings

--- src/components/customers/notification-settings.jsx
+++ src/components/customers/notification-settings.jsx
@@ -2,13 +2,45 @@
 
 import React from 'react';
 import ContentBox from '../content-box/content-box';
+import { SliderCheckbox } from '../checkbox/checkbox';
 
 export default class CustomerNotificationSettings extends React.Component {
 
   render() {
     return (
       <ContentBox title="Email & Notification Preferences" className="fc-customer-notification-settings">
-        Notification Settings
+        <div className="fc-grid fc-customer-status-row">
+            <div className="fc-col-md-2-3">
+              SMS Notifications
+            </div>
+            <div className="fc-col-md-1-3">
+              <SliderCheckbox className="fc-right" id="customerSmsNotifications" defaultChecked={ false } />
+            </div>
+        </div>
+        <div className="fc-grid fc-customer-status-row">
+            <div className="fc-col-md-2-3">
+              Email Notifications
+            </div>
+            <div className="fc-col-md-1-3">
+              <SliderCheckbox className="fc-right" id="customerEmailNotifications" defaultChecked={ true } />
+            </div>
+        </div>
+        <div className="fc-grid fc-customer-status-row">
+            <div className="fc-col-md-2-3">
+              SMS Promotional Offers
+            </div>
+            <div className="fc-col-md-1-3">
+              <SliderCheckbox className="fc-right" id="customerSmsPromoOffers" defaultChecked={ true } />
+            </div>
+        </div>
+        <div className="fc-grid fc-customer-status-row">
+            <div className="fc-col-md-2-3">
+              Email Promotional Offers
+            </div>
+            <div className="fc-col-md-1-3">
+              <SliderCheckbox className="fc-right" id="customerEmailPromoOffers" defaultChecked={ true } />
+            </div>
+        </div>
       </ContentBox>
     );
   }
