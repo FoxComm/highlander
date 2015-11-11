a575bf5dadf9062dd172da59b554eac6850a2872
Jeff Mataya <jeff@foxcommerce.com>
2015-11-11 11:53:49 -0800

    Put formatCurrency back into lib so others can use

---
+++ src/lib/format-currency.js
@@ -0,0 +1,14 @@
+const isNumber = (n) => !isNaN(parseFloat(n)) && isFinite(n);
+
+const formatCurrency = (amount, base, currency) => {
+  if (!isNumber(amount)) {
+    return null;
+  }
+  const parsed = parseFloat(amount / base).toFixed(2);
+  const decimal = parsed.slice(0, -3);
+  const fract = parsed.slice(-2);
+  const delimited = decimal.replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+  return `${currency}${delimited}.${fract}`;
+};
+
+export default formatCurrency;
