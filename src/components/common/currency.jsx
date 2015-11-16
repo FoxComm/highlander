25f3c4defe23fd9dd6eea49642653ccf744074ae
Denys Mikhalenko <prontiol@gmail.com>
2015-11-16 20:43:32 +0300

    Removed unneeded parseFloat

--- src/components/common/currency.jsx
+++ src/components/common/currency.jsx
@@ -7,7 +7,7 @@ const formatCurrency = (amount, base, currency) => {
     return null;
   }
   const sign = amount < 0 ? '-' : '';
-  const parsed = parseFloat(Math.abs(amount) / base).toFixed(2);
+  const parsed = (Math.abs(amount) / base).toFixed(2);
   const decimal = parsed.slice(0, -3);
   const fract = parsed.slice(-2);
   const delimited = decimal.replace(/\B(?=(\d{3})+(?!\d))/g, ',');
