635f8bea4f84aaf4b1fc9fe5c2cf544899a62ee7
Andrey Rublev <andrey@foxcommerce.com>
2015-11-24 20:08:36 +0600

    move isElementInViewport to dom-utils.js

--- src/components/common/auto-scroll.jsx
+++ src/components/common/auto-scroll.jsx
@@ -1,17 +1,6 @@
 
 import React from 'react';
-
-function isElementInViewport(el) {
-
-  const rect = el.getBoundingClientRect();
-
-  return (
-    rect.top >= 0 &&
-    rect.left >= 0 &&
-    rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-  );
-}
+import { isElementInViewport } from '../../lib/dom-utils';
 
 export default class AutoScroll extends React.Component {
   componentDidMount() {
