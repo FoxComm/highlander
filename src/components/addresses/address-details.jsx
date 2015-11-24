1c5a69c801a4c58c0450653f11ad70946b665bde
Andrey Rublev <andrey@foxcommerce.com>
2015-11-24 19:42:50 +0600

    Make AddressDetails stateless, add CountryInfo component

--- src/components/addresses/address-details.jsx
+++ src/components/addresses/address-details.jsx
@@ -1,54 +1,31 @@
 
 import React, { PropTypes } from 'react';
 import PhoneNumber from '../phone-number/phone-number';
-import { connect } from 'react-redux';
+import CountryInfo from './country-info';
 
-@connect(state => state)
-export default class AddressDetails extends React.Component {
+const AddressDetails = props => {
+  const address = props.address;
 
-  static propTypes = {
-    address: PropTypes.shape({
-      region: PropTypes.shape({
-        countryId: PropTypes.number
-      }).isRequired
-    }).isRequired,
-    countries: PropTypes.object
-  };
+  return (
+    <ul className="fc-address-details">
+      <li className="name">{address.name}</li>
+      <li>{address.address1}</li>
+      {address.address2 && <li>{address.address2}</li>}
+      <li>
+        {address.city}, <span>{address.region && address.region.name}</span> <span>{address.zip}</span>
+      </li>
+      <li><CountryInfo display={country => country.name} countryId={address.region.countryId}></CountryInfo></li>
+      {address.phoneNumber && <li><PhoneNumber>{address.phoneNumber}</PhoneNumber></li>}
+    </ul>
+  );
+};
 
-  get country() {
-    return this.props.countries && this.props.countries[this.props.address.region.countryId];
-  }
+AddressDetails.propTypes = {
+  address: PropTypes.shape({
+    region: PropTypes.shape({
+      countryId: PropTypes.number
+    }).isRequired
+  }).isRequired
+};
 
-  get address2() {
-    const { address } = this.props;
-
-    if (address.address2) {
-      return <li>{address.address2}</li>;
-    }
-  }
-
-  get phoneNumber() {
-    const { address } = this.props;
-
-    if (address.phoneNumber) {
-      return <li><PhoneNumber>{address.phoneNumber}</PhoneNumber></li>;
-    }
-  }
-
-  render() {
-    const address = this.props.address;
-
-    return (
-      <ul className="fc-address-details">
-        <li className="name">{address.name}</li>
-        <li>{address.address1}</li>
-        {this.address2}
-        <li>
-          {address.city}, <span>{address.region && address.region.name}</span> <span>{address.zip}</span>
-        </li>
-        <li>{this.country && this.country.name}</li>
-        {this.phoneNumber}
-      </ul>
-    );
-  }
-}
+export default AddressDetails;
