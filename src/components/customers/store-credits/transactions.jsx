8bb07afa06ce9e79765cf3682cd11432e279b5f3
Eugene Sypachev <eugene.sypachev@gmail.com>
2015-11-27 21:25:55 +0400

    transaction table columns

--- src/components/customers/store-credits/transactions.jsx
+++ src/components/customers/store-credits/transactions.jsx
@@ -1,7 +1,43 @@
 import React, { PropTypes } from 'react';
 import Summary from './summary';
+import TableView from '../../table/tableview';
+import TableRow from '../../table/row';
+import TableCell from '../../table/cell';
+import { DateTime } from '../../common/datetime';
+import Currency from '../../common/currency';
+import SearchBar from '../../search-bar/search-bar';
 
 export default class StoreCreditTransactions extends React.Component {
+
+  static defaultProps = {
+    tableColumns: [
+      {
+        field: 'createdAt',
+        text: 'Date/Time',
+        type: 'date'
+      },
+      {
+        field: 'transaction',
+        text: 'Transaction'
+      },
+      {
+        field: 'amount',
+        text: 'Amount'
+      },
+      {
+        field: 'paymentState',
+        text: 'Payment State'
+      },
+      {
+        field: 'totalAvailableBalance',
+        text: 'Total Availabale Balance'
+      }
+    ]
+  };
+
+  renderRow(row) {
+
+  }
 
   render() {
     const props = this.props;
@@ -9,8 +45,14 @@ export default class StoreCreditTransactions extends React.Component {
       <div className="fc-store-credits fc-list-page">
         <Summary {...props} />
         <div className="fc-grid fc-list-page-content">
+          <SearchBar />
           <div className="fc-col-md-1-1">
-
+            <TableView
+              columns={props.tableColumns}
+              data={props.StoreCreditTransactions}
+              renderRow={this.renderRow}
+              setState={props.setFetchParams}
+              />
           </div>
         </div>
       </div>
