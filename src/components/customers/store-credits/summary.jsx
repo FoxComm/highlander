9a84bce51c6d837a440361587197ec8822b747be
Eugene Sypachev <eugene.sypachev@gmail.com>
2015-11-27 19:43:07 +0400

    summary in color

--- src/components/customers/store-credits/summary.jsx
+++ src/components/customers/store-credits/summary.jsx
@@ -2,6 +2,8 @@ import React, { PropTypes } from 'react';
 import SectionTitle from '../../section-title/section-title';
 import TabListView from '../../tabs/tabs';
 import TabView from '../../tabs/tab';
+import Currency from '../../common/currency';
+import { PanelList, PanelListItem } from '../../panel/panel-list';
 import { Link } from '../../link';
 
 const Summary = props => {
@@ -12,8 +14,16 @@ const Summary = props => {
                     onAddClick={ () => console.log("onAddClick") }
                     isPrimary={false} />
 
-      <div className="fc-store-credits-summary">
-        Total Points here
+      <div className="fc-grid fc-grid-gutter fc-store-credits-summary">
+        <div className="fc-col-md-1-4">
+          <PanelList>
+            <PanelListItem title="Total Availabale Balance">
+              <div className="fc-store-credits-summary-balance">
+                <Currency value="106" /> {/* ToDo: Replace with real total */}
+              </div>
+            </PanelListItem>
+          </PanelList>
+        </div>
       </div>
       <TabListView>
         <TabView draggable={false}>
