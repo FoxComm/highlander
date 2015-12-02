69a551b7e94ffd7a146d32eedf17aef9b808ee58
Sergey Rublev <sergey@foxcommerce.com>
2015-12-03 02:25:23 +0600

    Base stateless component for dyn/manual group

--- src/components/customers-groups/new-dynamic-group.jsx
+++ src/components/customers-groups/new-dynamic-group.jsx
@@ -5,8 +5,8 @@ import Form from '../forms/form.jsx';
 import Dropdown from '../dropdown/dropdown';
 import { PrimaryButton, Button } from '../common/buttons';
 import { Link } from '../link';
+import NewGroupBase from './new-group-base.jsx';
 import QueryBuilder from './query-builder';
-import { transitionTo } from '../../route-helpers';
 import { connect } from 'react-redux';
 import { assoc } from 'sprout-data';
 
@@ -24,58 +24,51 @@ export default class NewDynamicGroup extends React.Component {
     customerCount: PropTypes.number
   };
 
-  render () {
+
+  render() {
     const mainMatchStatuses = {
       all: 'all',
       none: 'none'
     };
 
     return (
-      <div className='fc-group-new'>
-        <div className='fc-grid'>
-          <header className='fc-customer-form-header fc-col-md-1-1'>
-            <h1 className='fc-title'>
-              New Dynamic Customer Group
-            </h1>
-            <Link className='fc-group-new-or gc-group-new-title' to='groups-new-manual'>or create a manual group</Link>
-          </header>
-          <article className='fc-col-md-1-1'>
-            <Form>
-              <FormField label='Group Name'
-                         labelClassName='fc-group-new-title fc-group-new-name'>
-                <input id='nameField'
-                       className='fc-group-new-form-name'
-                       name='Name'
-                       maxLength='255'
-                       type='text'
-                       required
-                       onChange={ ({target}) => this.setState({name: target.value}) }
-                       value={ this.state.name } />
-              </FormField>
-              <div className='fc-group-new-match-div'>
-                <span className='fc-group-new-match-span'>Customers match</span>
-                <span className='fc-group-new-match-dropdown'>
-                  <Dropdown
-                    name='matchCriteria'
-                    items={mainMatchStatuses}
-                    value={this.state.matchCriteria}
-                    onChange={ (value) => this.setState({matchCriteria: value}) }
-                      />
-                </span>
-                <span className='fc-group-new-match-span'>of the following criteria:</span>
-              </div>
-              <QueryBuilder/>
-              <div className='fc-group-new-title fc-group-new-count-title'>Customer Count:</div>
-              <div className='fc-group-new-count'>{ this.props.customerCount }</div>
-              <div className='fc-group-new-form-submits'>
-                <Link to='customers'>Cancel</Link>
-                <Button>Make Manual Group</Button>
-                <PrimaryButton>Save Dynamic Group</PrimaryButton>
-              </div>
-            </Form>
-          </article>
-        </div>
-      </div>
+      <NewGroupBase title='New Dynamic Customer Group'
+                  alternativeId='groups-new-manual'
+                  alternativeTitle='manual group'>
+        <Form>
+          <FormField label='Group Name'
+                     labelClassName='fc-group-new-title fc-group-new-name'>
+            <input id='nameField'
+                   className='fc-group-new-form-name'
+                   name='Name'
+                   maxLength='255'
+                   type='text'
+                   required
+                   onChange={ ({target}) => this.setState({name: target.value}) }
+                   value={ this.state.name } />
+          </FormField>
+          <div className='fc-group-new-match-div'>
+            <span className='fc-group-new-match-span'>Customers match</span>
+            <span className='fc-group-new-match-dropdown'>
+              <Dropdown
+                name='matchCriteria'
+                items={mainMatchStatuses}
+                value={this.state.matchCriteria}
+                onChange={ (value) => this.setState({matchCriteria: value}) }
+                  />
+            </span>
+            <span className='fc-group-new-match-span'>of the following criteria:</span>
+          </div>
+          <QueryBuilder/>
+          <div className='fc-group-new-title fc-group-new-count-title'>Customer Count:</div>
+          <div className='fc-group-new-count'>{ this.props.customerCount }</div>
+          <div className='fc-group-new-form-submits'>
+            <Link to='customers'>Cancel</Link>
+            <Button>Make Manual Group</Button>
+            <PrimaryButton type="submit">Save Dynamic Group</PrimaryButton>
+          </div>
+        </Form>
+      </NewGroupBase>
     );
   }
 }
--- src/components/customers-groups/new-manual-group.jsx
+++ src/components/customers-groups/new-manual-group.jsx
@@ -1,34 +1,12 @@
 import React, { PropTypes } from 'react';
-import SectionTitle from '../section-title/section-title';
-import FormField from '../forms/formfield.jsx';
-import Form from '../forms/form.jsx';
-import { Link } from '../link';
-import { transitionTo } from '../../route-helpers';
-import { connect } from 'react-redux';
-import { autobind } from 'core-decorators';
+import NewGroupBase from './new-group-base.jsx';
 
-// todo: move base parts with manual groups to base component
 export default class NewManualGroup extends React.Component {
-
-  static propTypes = {
-  };
-
   render () {
     return (
-      <div className="fc-group-new">
-        <div className="fc-grid">
-          <header className="fc-customer-form-header fc-col-md-1-1">
-            <h1 className="fc-title">
-              New Manual Customer Group
-            </h1>
-            <Link to="groups-new-dynamic">or create a dynamic group</Link>
-          </header>
-          <article className="fc-col-md-1-1">
-            <div className="fc-grid fc-grid-no-gutter">
-            </div>
-          </article>
-        </div>
-      </div>
+      <NewGroupBase title='New Manual Customer Group'
+                  alternativeId='groups-new-dynamic'
+                  alternativeTitle='dynamic group'/>
     );
   }
 }
