d7214ab5e8c5014bcb2dc4f6aba786cff3e7882a
Andrey Rublev <andrey@foxcommerce.com>
2015-12-02 18:44:38 +0600

    passthrough props in Counter to input

--- src/components/forms/counter.jsx
+++ src/components/forms/counter.jsx
@@ -2,42 +2,37 @@ import React, { PropTypes } from 'react';
 import { IncrementButton, DecrementButton } from '../common/buttons';
 
 const Counter = props => {
+  const {decreaseAction, increaseAction, ...rest} = props;
+
   return (
     <div className="fc-input-group fc-counter">
       <div className="fc-input-prepend">
-        <DecrementButton onClick={props.decreaseAction} className="fc-btn-counter" />
+        <DecrementButton onClick={decreaseAction} className="fc-btn-counter" />
       </div>
       <input
         type="number"
-        id={props.inputName}
-        name={props.inputName}
-        value={props.value}
-        min={props.minValue}
-        max={props.maxValue}
-        step={props.stepAmount}
-        onChange={props.onChange} />
+        {...rest} />
       <div className="fc-input-append">
-        <IncrementButton onClick={props.increaseAction} className="fc-btn-counter" />
+        <IncrementButton onClick={increaseAction} className="fc-btn-counter" />
       </div>
     </div>
   );
 };
 
 Counter.propTypes = {
-  inputName: PropTypes.string,
   value: PropTypes.number,
-  minValue: PropTypes.number,
-  maxValue: PropTypes.number,
-  stepAmount: PropTypes.number,
+  min: PropTypes.number,
+  max: PropTypes.number,
+  step: PropTypes.number,
   decreaseAction: PropTypes.func,
   increaseAction: PropTypes.func,
 };
 
 Counter.defaultProps = {
   value: 1,
-  stepAmount: 1,
-  minValue: 1,
-  maxValue: 100
+  step: 1,
+  min: 1,
+  max: 100
 };
 
 export default Counter;
