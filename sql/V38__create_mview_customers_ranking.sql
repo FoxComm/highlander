875c3b3820446e08db252555442b34240e591353
Sergey Rublev <sergey@foxcommerce.com>
2015-12-06 03:40:39 +0600

    Fix mview ranking
    1. Filter users with zero revenue from ranking
    2. Fix typo in GiftCardAdj. status check

--- sql/V38__create_mview_customers_ranking.sql
+++ sql/V38__create_mview_customers_ranking.sql
@@ -13,13 +13,14 @@ from
     inner join order_payments as op on(op.order_id = orders.id)
     left join credit_card_charges as CCc on(CCc.order_payment_id = op.id and CCc.status = 'fullCapture')
     left join store_credit_adjustments as SCa on(SCA.order_payment_id = op.id and SCa.status = 'capture')
-    left join gift_card_adjustments as GCa on (GCa.order_payment_id = op.id and GCa.status = 'captured')
+    left join gift_card_adjustments as GCa on (GCa.order_payment_id = op.id and GCa.status = 'capture')
     left join rmas on(rmas.order_id = orders.id and rmas.status = 'complete')
     left join rma_payments as rp on (rp.rma_id = rmas.id and rp.amount is not null)
     where is_guest = false
     group by (c.id)
     order by revenue desc
   ) as ranking
+where revenue > 0
 window w as (order by ranking.revenue desc)
 order by revenue desc;
 
