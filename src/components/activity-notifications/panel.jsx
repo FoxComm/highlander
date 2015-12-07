2deadbddc37b5188e960d637a5e3bc9c44d95fe1
Eugene Sypachev <eugene.sypachev@gmail.com>
2015-12-07 21:45:29 +0300

    styles for box

--- src/components/activity-notifications/panel.jsx
+++ src/components/activity-notifications/panel.jsx
@@ -1,11 +1,23 @@
 import React, { PropTypes } from 'react';
 import ContentBox from '../content-box/content-box';
+import { PrimaryButton } from '../common/buttons';
 import { connect } from 'react-redux';
 import { autobind } from 'core-decorators';
 import * as NotificationActions from '../../modules/activity-notifications';
 
 @connect(state => ({notifications: state.activityNotifications}), NotificationActions)
 export default class NotificationPanel extends React.Component {
+
+  get footer() {
+    return (
+      <div className="fc-activity-notifications__footer">
+        <PrimaryButton onClick={ () => console.log('Not implemented yet') }
+                       className="fc-activity-notifications__footer-button">
+          View All
+        </PrimaryButton>
+      </div>
+    );
+  }
 
   get body() {
     if (this.props.notifications.displayed) {
@@ -15,7 +27,8 @@ export default class NotificationPanel extends React.Component {
                onClick={ this.props.toggleNotifiactions }>
           </div>
           <ContentBox title='Notifications'
-                      className="fc-activity-notifications__box">
+                      className="fc-activity-notifications__box"
+                      footer={ this.footer }>
             Notifications!
           </ContentBox>
         </div>
