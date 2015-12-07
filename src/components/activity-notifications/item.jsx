fd35013c2de82ff2aa77c020bb1503685d4354ad
Eugene Sypachev <eugene.sypachev@gmail.com>
2015-12-07 23:52:16 +0300

    icon for order

--- src/components/activity-notifications/item.jsx
+++ src/components/activity-notifications/item.jsx
@@ -1,5 +1,18 @@
+import _ from 'lodash';
 import React, { PropTypes } from 'react';
 import { DateTime } from '../common/datetime';
+
+const icon = (item) => {
+  console.log(item);
+  const type = _.get(item, ['body', 'reference', 'typed']);
+  console.log(type);
+  switch(type) {
+    case 'Order':
+      return (<i className='icon icon-orders'></i>);
+    default:
+      return (<i className='icon icon-bell'></i>);
+  }
+};
 
 const NotificationItem = (props) => {
   return (
@@ -10,6 +23,7 @@ const NotificationItem = (props) => {
         </div>
         <div className="fc-activity-notification-item__info">
           <div className="fc-activity-notification-item__type">
+            { icon(props.item) }
           </div>
           <div className="fc-activity-notification-item__body">
             <div className="fc-activity-notification-item__author">
@@ -21,6 +35,6 @@ const NotificationItem = (props) => {
       </div>
     </div>
   );
-}
+};
 
 export default NotificationItem;
