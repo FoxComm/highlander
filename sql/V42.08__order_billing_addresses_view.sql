90726bc11ef0d2b2b75e3a1a2314accfbfe045e2
Pavel Kiselev <kpashka@gmail.com>
2015-12-08 22:11:03 +0600

    Fix shipping address bug

--- sql/V42.08__order_billing_addresses_view.sql
+++ sql/V42.08__order_billing_addresses_view.sql
@@ -9,8 +9,7 @@ select
         json_agg((cc.address1, cc.address2, cc.city, cc.zip, r2.name, c2.name, c2.continent, c2.currency)::export_addresses)
     end as addresses 
 from orders as o
-left join order_payments as op on (o.id = op.order_id)
-left join order_payments as op_cc on (o.id = op.order_id and op.payment_method_type = 'creditCard')
+left join order_payments as op_cc on (o.id = op_cc.order_id and op_cc.payment_method_type = 'creditCard')
 left join credit_cards as cc on (cc.id = op_cc.payment_method_id)
 left join regions as r2 on (cc.region_id = r2.id)
 left join countries as c2 on (r2.country_id = c2.id)
