254acf2a694deada4eb5180eef1550fb3ae47f75
Jeff Mataya <jeff@foxcommerce.com>
2015-12-08 13:42:46 -0800

    Add PropTypes for live search to orders

--- src/components/orders/orders.jsx
+++ src/components/orders/orders.jsx
@@ -24,7 +24,10 @@ export default class Orders extends React.Component {
     orders: PropTypes.shape({
       rows: PropTypes.array.isRequired,
       total: PropTypes.number
-    })
+    }),
+    deleteSearchFilter: PropTypes.func,
+    goBack: PropTypes.func,
+    submitFilter: PropTypes.func
   };
 
   static defaultProps = {
