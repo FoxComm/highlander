a5102ac0a61cc0c45308e8a6cfad9cdfaaa3cc25
Pavel Kiselev <kpashka@gmail.com>
2015-12-08 20:48:06 +0600

    Add missing mappings and json fields

--- src/main/scala/consumer/ElasticSearchProcessor.scala
+++ src/main/scala/consumer/ElasticSearchProcessor.scala
@@ -52,55 +52,155 @@ class ElasticSearchProcessor(uri: String, cluster: String, indexName: String, to
           "name"        typed StringType analyzer "autocomplete"
         ),
         "customers_search_view" as (
+          // Customer
           "id"                    typed IntegerType,
           "name"                  typed StringType analyzer "autocomplete",
           "email"                 typed StringType analyzer "autocomplete",
           "is_disabled"           typed BooleanType,
           "is_guest"              typed BooleanType,
           "is_blacklisted"        typed BooleanType,
-          "date_joined"           typed DateType,
+          "joined_at"             typed DateType,
           "revenue"               typed IntegerType,
           "rank"                  typed IntegerType,
-          "store_credit_total"    typed IntegerType,
-          "store_credit_count"    typed IntegerType,
-          "orders"                nested (
-            "reference_number" typed StringType analyzer "autocomplete",
-            "status"           typed StringType index "not_analyzed",
-            "date_placed"      typed DateType
-          ),
+          // Orders
           "order_count"           typed IntegerType,
+          "orders"                nested (
+            "reference_number"    typed StringType analyzer "autocomplete",
+            "status"              typed StringType index "not_analyzed",
+            "created_at"          typed DateType,
+            "placed_at"           typed DateType,
+            "sub_total"           typed IntegerType,
+            "shipping_total"      typed IntegerType,
+            "adjustments_total"   typed IntegerType,
+            "taxes_total"         typed IntegerType,
+            "grand_total"         typed IntegerType         
+          ),
+          // Purchased items
+          "purchased_item_count"  typed IntegerType,
           "purchased_items"       nested (
             "sku"   typed StringType analyzer "autocomplete",
             "name"  typed StringType analyzer "autocomplete",
             "price" typed IntegerType
           ),
-          "purchased_items_count" typed IntegerType,
+          // Addresses
           "shipping_addresses"    nested (
             "address1"          typed StringType analyzer "autocomplete",
             "address2"          typed StringType analyzer "autocomplete",
             "city"              typed StringType analyzer "autocomplete",
             "zip"               typed StringType index "not_analyzed",
-            "region_name"       typed StringType analyzer "autocomplete",
-            "country_name"      typed StringType analyzer "autocomplete",
-            "country_continent" typed StringType analyzer "autocomplete",
-            "country_currency"  typed  StringType analyzer "autocomplete"
+            "region"            typed StringType analyzer "autocomplete",
+            "country"           typed StringType analyzer "autocomplete",
+            "continent"         typed StringType analyzer "autocomplete",
+            "currency"          typed  StringType analyzer "autocomplete"
           ),
           "billing_addresses"     nested (
             "address1"          typed StringType analyzer "autocomplete",
             "address2"          typed StringType analyzer "autocomplete",
             "city"              typed StringType analyzer "autocomplete",
             "zip"               typed StringType index "not_analyzed",
-            "region_name"       typed StringType analyzer "autocomplete",
-            "country_name"      typed StringType analyzer "autocomplete",
-            "country_continent" typed StringType analyzer "autocomplete",
-            "country_currency"  typed  StringType analyzer "autocomplete"
+            "region"            typed StringType analyzer "autocomplete",
+            "country"           typed StringType analyzer "autocomplete",
+            "continent"         typed StringType analyzer "autocomplete",
+            "currency"          typed  StringType analyzer "autocomplete"
           ),
+          // Store credits
+          "store_credit_total"    typed IntegerType,
+          "store_credit_count"    typed IntegerType,
+          // Saved for later
+          "saved_for_later_count" typed IntegerType,
           "saved_for_later"       nested (
-            "sku"   typed StringType analyzer "autocomplete",
-            "name"  typed StringType analyzer "autocomplete",
-            "price" typed IntegerType
+            "sku"     typed StringType analyzer "autocomplete",
+            "name"    typed StringType analyzer "autocomplete",
+            "price"   typed IntegerType
+          )
+        ),
+        "orders_search_view" as (
+          // Order
+          "id"                  typed IntegerType,
+          "reference_number"    typed StringType analyzer "autocomplete",
+          "status"              typed StringType index "not_analyzed",
+          "created_at"          typed DateType,
+          "placed_at"           typed DateType,
+          "currency"            typed StringType index "not_analyzed",
+          // Totals
+          "sub_total"           typed IntegerType,
+          "shipping_total"      typed IntegerType,
+          "adjustments_total"   typed IntegerType,
+          "taxes_total"         typed IntegerType,
+          "grand_total"         typed IntegerType,
+          // Customer
+          "customer"            nested (
+            "name"                  typed StringType analyzer "autocomplete",
+            "email"                 typed StringType analyzer "autocomplete",
+            "is_blacklisted"        typed BooleanType,
+            "joined_at"             typed DateType,
+            "revenue"               typed IntegerType,
+            "rank"                  typed IntegerType
           ),
-          "saved_for_later_count" typed IntegerType
+          // Line items
+          "line_item_cont"      typed IntegerType,
+          "line_items"          nested (
+            "status"  typed StringType index "not_analyzed",
+            "sku"     typed StringType analyzer "autocomplete",
+            "name"    typed StringType analyzer "autocomplete",
+            "price"   typed IntegerType
+          ),
+          // Payments
+          "payments"            nested (
+            "payment_method_type" typed StringType index "not_analyzed",
+            "amount"              typed IntegerType,
+            "currency"            typed StringType index "not_analyzed"
+          ),
+          "credit_card_count"   typed IntegerType,
+          "credit_card_total"   typed IntegerType,
+          "gift_card_count"     typed IntegerType,
+          "gift_card_total"     typed IntegerType,
+          "store_credit_count"  typed IntegerType,
+          "store_credit_total"  typed IntegerType,
+          // Shipments
+          "shipment_count"      typed IntegerType,
+          "shipments"           nested (
+            "status"                  typed StringType index "not_analyzed",
+            "shipping_price"          typed IntegerType,
+            "admin_display_name"      typed StringType analyzer "autocomplete",
+            "storefront_display_name" typed StringType analyzer "autocomplete"
+          ),
+          // Addresses
+          "shipping_addresses"  nested (
+            "address1"          typed StringType analyzer "autocomplete",
+            "address2"          typed StringType analyzer "autocomplete",
+            "city"              typed StringType analyzer "autocomplete",
+            "zip"               typed StringType index "not_analyzed",
+            "region"            typed StringType analyzer "autocomplete",
+            "country"           typed StringType analyzer "autocomplete",
+            "continent"         typed StringType analyzer "autocomplete",
+            "currency"          typed  StringType analyzer "autocomplete"
+          ),
+          "billing_addresses"   nested (
+            "address1"          typed StringType analyzer "autocomplete",
+            "address2"          typed StringType analyzer "autocomplete",
+            "city"              typed StringType analyzer "autocomplete",
+            "zip"               typed StringType index "not_analyzed",
+            "region"            typed StringType analyzer "autocomplete",
+            "country"           typed StringType analyzer "autocomplete",
+            "continent"         typed StringType analyzer "autocomplete",
+            "currency"          typed  StringType analyzer "autocomplete"
+          ),
+          // Assignments
+          "assignment_count"    typed IntegerType,
+          "assignees"           nested (
+            "first_name"    typed StringType analyzer "autocomplete",
+            "last_name"     typed StringType analyzer "autocomplete",
+            "assigned_at"   typed DateType
+          ),
+          // RMAs
+          "rma_count"           typed IntegerType,
+          "rmas"                nested (
+            "reference_number"  typed StringType analyzer "autocomplete",
+            "status"            typed StringType index "not_analyzed",
+            "rma_type"          typed StringType index "not_analyzed",
+            "placed_at"         typed DateType
+          )
         )
       ) analysis
         CustomAnalyzerDefinition(
@@ -140,12 +240,18 @@ class ElasticSearchProcessor(uri: String, cluster: String, indexName: String, to
 
 object ElasticSearchProcessor {
 
+  val customerJsonFields = List("orders", "purchased_items", "shipping_addresses", "billing_addresses", 
+    "save_for_later")
+
+  val orderJsonFields = List("customer", "line_items", "payments", "shipments", "shipping_addresses", 
+    "billing_addresses", "assignees", "rmas")
+
   def topicInfo(topic: String): Option[List[String]] = {
     topic match {
       case "countries"                ⇒ Some(List.empty)
       case "regions"                  ⇒ Some(List.empty)
-      case "customers_search_view"    ⇒ Some(List("orders", "purchased_items", "shipping_addresses", 
-        "billing_addresses", "save_for_later"))
+      case "customers_search_view"    ⇒ Some(customerJsonFields)
+      case "orders_search_view"       ⇒ Some(orderJsonFields)
       case _                          ⇒ None
     }    
   }
