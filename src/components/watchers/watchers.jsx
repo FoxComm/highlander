4614a71464a0fac754fe235a604857c0cba1bd4d
Eugene Sypachev <eugene.sypachev@gmail.com>
2015-12-09 23:21:20 +0400

    titles and links

--- src/components/watchers/watchers.jsx
+++ src/components/watchers/watchers.jsx
@@ -5,8 +5,29 @@ export default class Watchers extends React.Component {
 
   render() {
     return (
-      <Panel>
-        Watchers && Assignees
+      <Panel className="fc-watchers">
+        <div className="fc-watchers__container">
+          <div className="fc-watchers__title-row">
+            <div className="fc-watchers__title">
+              Assignees
+            </div>
+            <div className="fc-watchers__controls">
+              <a className="fc-watchers__link" href="#">take it</a>
+            </div>
+          </div>
+          <div className="fc-watchers__users-row">
+          </div>
+          <div className="fc-watchers__title-row">
+            <div className="fc-watchers__title">
+              Watchers
+            </div>
+            <div className="fc-watchers__controls">
+              <a className="fc-watchers__link" href="#">watch</a>
+            </div>
+          </div>
+          <div className="fc-watchers__users-row">
+          </div>
+        </div>
       </Panel>
     );
   }
