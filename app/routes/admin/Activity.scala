012ae7d17a8a64afd11c872dc589b52ec184e4f3
Anna Zubenko <anna@foxcommerce.com>
2015-12-10 12:59:51 +0200

    Fix routes

--- app/routes/admin/Activity.scala
+++ app/routes/admin/Activity.scala
@@ -1,58 +1,50 @@
 package routes.admin
 
+import scala.concurrent.ExecutionContext
 import akka.http.scaladsl.server.Directives._
 import akka.stream.Materializer
+
 import de.heikoseeberger.akkahttpjson4s.Json4sSupport._
-import slick.driver.PostgresDriver.api._
-import scala.concurrent.{ExecutionContext, Future}
-
 import models.StoreAdmin
-import models.activity.ActivityContext
-
-import services.activity.ActivityManager
-import services.activity.TrailManager
-
+import payloads.AppendActivity
+import services.activity.{ActivityManager, TrailManager}
+import slick.driver.PostgresDriver.api._
 import utils.Apis
 import utils.CustomDirectives._
 import utils.Http._
-import utils.Slick.implicits._
-
-import payloads.AppendActivity
 
 object Activity {
 
   def routes(implicit ec: ExecutionContext, db: Database, 
     mat: Materializer, storeAdminAuth: AsyncAuthenticator[StoreAdmin], apis: Apis) = {
 
-      authenticateBasicAsync(realm = "admin", storeAdminAuth) { admin ⇒
+    authenticateBasicAsync(realm = "admin", storeAdminAuth) { admin ⇒
 
-        pathPrefix("activities") { 
-          pathPrefix(IntNumber) { activityId ⇒ 
-            (get & pathEnd) { 
-              goodOrFailures {
-                ActivityManager.findById(activityId)
-              }
-            } 
-          } ~
-          pathPrefix("connections" / IntNumber) { 
-            (connectionId) ⇒ 
-              (get & pathEnd) {
-                goodOrFailures {
-                  TrailManager.findConnection(connectionId)
-                }
-              }
-          } ~
-          pathPrefix("trails" / Segment/ IntNumber) { (dimension, objectId) ⇒ 
-            (post & pathEnd & activityContext(admin)) { implicit ac ⇒ 
-              entity(as[AppendActivity]) { payload ⇒
-                goodOrFailures {
-                  TrailManager.appendActivityByObjectId(dimension, objectId, payload)
-                }
-              }
+      pathPrefix("activities") {
+        pathPrefix(IntNumber) { activityId ⇒
+          (get & pathEnd) {
+            goodOrFailures {
+              ActivityManager.findById(activityId)
+            }
+          }
+        }
+      } ~
+      pathPrefix("connections" / IntNumber) { connectionId ⇒
+        (get & pathEnd) {
+          goodOrFailures {
+            TrailManager.findConnection(connectionId)
+          }
+        }
+      } ~
+      pathPrefix("trails" / Segment / IntNumber) { (dimension, objectId) ⇒
+        (post & pathEnd & activityContext(admin)) { implicit ac ⇒
+          entity(as[AppendActivity]) { payload ⇒
+            goodOrFailures {
+              TrailManager.appendActivityByObjectId(dimension, objectId, payload)
             }
           }
         }
       }
+    }
   }
-
 }
