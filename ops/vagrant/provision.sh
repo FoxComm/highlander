8bff0e42bae61474b499ca7895f55909f2752239
Jeff Mataya <jeff@foxcommerce.com>
2015-12-12 13:32:10 -0800

    Fix up the Vagrantfile and get it running
    - Replace nvm with n
    - Fix typos that prevented nginx from installing
    - Fix typos that prevented VMWare Fusion from running
    - Give the VM an IP addresses

--- ops/vagrant/provision.sh
+++ ops/vagrant/provision.sh
@@ -1,31 +1,28 @@
 #!/bin/bash
 
 alias stop='systemctl stop'
-alias start='systemct start'
+alias start='systemctl start'
 
-#install node and npm
-apt-get update 
-apt-get install -y nodejs-legacy npm build-essential lua-cjson lua-cjson-dev
+# Setup the VM
+apt-get update
+apt-get install -y build-essential
 
-#setup vagrant user
-sudo -u vagrant /vagrant/ops/vagrant/setup_vagrant_user.sh
+# Install the latest version of node.js through n
+apt-get install -y nodejs-legacy npm
+npm install -g n
+n latest
 
+# Install nginx
+apt-get install -y lua-cjson nginx-extras
+
+# Setup custom nginx config
 cd /vagrant
-
-#install nginx
-apt-get install -y nginx-extras
-
-#setup custom nginx config
 cp ops/nginx/uuid4.lua /etc/nginx/uuid4.lua
 cp ops/nginx/default /etc/nginx/sites-available/default
 cp ops/nginx/nginx.conf /etc/nginx/nginx.conf
 
 systemctl restart nginx
 
-#install ashes as a service
+# Install ashes as a service
 cp ops/vagrant/ashes.service /etc/systemd/system/
 systemctl restart ashes
-
-
-
-
