75ed4c49037f37e634224ebdecd3e836fa4c6a60
Maxim Khailo <max@foxcommerce.com>
2015-12-15 11:32:53 -0800

    Merging the two Mains into one application.

--- ansible/roles/consumer/files/run_consumer.sh
+++ ansible/roles/consumer/files/run_consumer.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sbt -Denv=localhost clean esConsume
+sbt -Denv=localhost clean consume
---
+++ src/main/scala/consumer/Main.scala
@@ -0,0 +1,167 @@
+package consumer
+
+import com.typesafe.config.ConfigFactory
+
+import scala.collection.JavaConversions._
+import scala.concurrent.Await
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
+import scala.concurrent.duration.Duration
+
+import akka.actor.ActorSystem
+import akka.http.ConnectionPoolSettings
+import akka.stream.ActorMaterializer
+
+import consumer.activity.ActivityConnectionTransformer
+import consumer.activity.ActivityProcessor
+import consumer.activity.AdminConnector
+import consumer.activity.CustomerConnector
+import consumer.activity.PhoenixConnectionInfo
+import consumer.elastic.AvroTransformers
+import consumer.elastic.ElasticSearchProcessor
+
+/**
+ * Program which consumes several bottledwater topics and indexes them in Elastic Search
+ */
+
+
+/**
+ * I apologize for nothing!
+                                                                                                    
+                                                                     ...--------:..`                
+                                                                 `..-...........:...-.`             
+                                                               `.-...--.........:......-`           
+                                                              ...-..-..:-.---..-.........-          
+                                                             ....`  -..-`   `.-...........:         
+                                                             -..    -`.-      `-........----        
+                                                            `-`.  ``-...       .--------...-`       
+                                        `````                :`.`..-...         :...........-       
+              :``..`/```      -     `..-.......`             ````--..           -...........-       
+          .---:..----...--.`  -  ```.`..........-``          ` `````           -............-       
+        .-:--.......-.......--: ``````...............`       ``````           .:---........`-       
+        :-...........:.......:..````.......---..-....:-       ````           .-.-..--......-`       
+       /------.-.....-.......-...`...-.``` ``-`-     .-.      ````         `-.........--...-        
+      /-......----..-:-..----...:`.....`   ``....`  ``:`       `         .--............-.-`        
+      /...........:::`````..---.:`.......`........`....-`            ``.------..........--`         
+     -:...........:..---.---.---:`......................-.`````````.---......--.........-`          
+     -:.........--/:.          .-.................`  -...-...--:-.--...........-........`           
+     ./--.--..---..s:           .-.........```   .   `---.......--..-...........-....-`             
+     .+-....--.....:s           .-........   ``   .   `-..........:.-...........:....`              
+      ./...........-o.          .:....`- `    .   `.   `-..........:.-.........:.-.``.```           
+      .-/-........-.+.          ..-...`-  .    `   ..`.`---.........--....-.---:.`..`.````.``       
+         :--......--/.           .-....-.````..-...........--..-..--:::-.`..--...``.````````-       
+         .--/--.----/.            -......----.....---......-------....```````.``.```.`.``..---`     
+             /::---::.            .-..........----..........-....````` ````.```` ````.```````..-.`  
+                   `.:.           .-........-...............--...-.```````.``````. .. ```` ` .....  
+                    `o.            .-....-..................--:--.-.```````.`.```````.`.```....-.`  
+                    `::            ..-.......................------..`````---..--..``.---....-:..`  
+                    `:-.            .--........................-:....````...---.....----.---.-...-..
+                    `- :            .:..........................--....```......-----..-...-------..`
+                     - :            -............................:..``......................-.......
+                     - -.           -................................-...................--........`
+                     - -.          -`.........................``......-..-----.--------.............
+                     -  /          -`........................`...........`.....-.......``...........
+                   ``-  :..........-``````````````````..``````````````..`````````````.``````````````
+**/
+
+object Main {
+  val environmentProperty = "env"
+  val defaultEnvironment  = "default"
+
+  implicit val system = ActorSystem("system")
+  implicit val materializer = ActorMaterializer()
+  
+  implicit lazy final val connectionPoolSettings = 
+    ConnectionPoolSettings.create(implicitly[ActorSystem]).copy(
+      maxConnections  = 32,
+      maxOpenRequests = 32,
+      maxRetries      = 0)
+
+
+  def main(args: Array[String]): Unit = {
+    // Load configuration parameters & environment
+    val conf = ConfigFactory.load()
+    val env = sys.props.getOrElse(environmentProperty, defaultEnvironment)
+    println(s"Initializing consumers in $env environment...")
+
+    // Load configuration values
+    val activityTopic         = conf.getString(s"$env.activity.kafka.topic")
+    val avroSchemaRegistryUrl = conf.getString(s"$env.avro.schemaRegistryUrl")
+    val connectionTopic       = conf.getString(s"$env.activity.connection.kafka.topic")
+    val elasticSearchCluster  = conf.getString(s"$env.elastic.cluster")
+    val elasticSearchIndex    = conf.getString(s"$env.elastic.index")
+    val elasticSearchUrl      = conf.getString(s"$env.elastic.host")
+    val kafkaBroker           = conf.getString(s"$env.kafka.broker")
+    val kafkaGroupId          = conf.getString(s"$env.kafka.groupId")
+    val kafkaTopics           = conf.getStringList(s"kafka.topics").toIndexedSeq.toSeq
+    val phoenixPass           = conf.getString(s"$env.activity.phoenix.pass")
+    val phoenixUri            = conf.getString(s"$env.activity.phoenix.url")
+    val phoenixUser           = conf.getString(s"$env.activity.phoenix.user")
+
+    val phoenix = PhoenixConnectionInfo(
+      uri = phoenixUri, 
+      user = phoenixUser,
+      pass = phoenixPass)
+
+    val activityWork = Future {
+
+      val activityConnectors = Seq(CustomerConnector(), AdminConnector())
+      val activityProcessor = new ActivityProcessor(phoenix, activityConnectors)
+
+      val avroProcessor = new AvroProcessor(
+        schemaRegistryUrl = avroSchemaRegistryUrl, 
+        processor = activityProcessor)
+
+      val consumer = new MultiTopicConsumer(
+        topics = Seq(activityTopic), 
+        broker = kafkaBroker, 
+        groupId = kafkaGroupId,
+        processor = avroProcessor)
+
+      // Start consuming & processing
+      println(s"Reading from broker $kafkaBroker")
+
+      consumer.readForever()
+    }
+
+    val trailWork = Future {
+      val transformers = Map(
+        "regions" → AvroTransformers.Region(),
+        "countries"  → AvroTransformers.Country(),
+        "customers_search_view" →  AvroTransformers.CustomerSearchView(),
+        "orders_search_view" →  AvroTransformers.OrderSearchView(),
+        connectionTopic → ActivityConnectionTransformer(phoenix))
+
+      // Init processors & consumer
+      val esProcessor = new ElasticSearchProcessor(
+        uri = elasticSearchUrl, 
+        cluster = elasticSearchCluster,
+        indexName = elasticSearchIndex, 
+        topics = kafkaTopics,
+        jsonTransformers = transformers)
+
+      val avroProcessor = new AvroProcessor(
+        schemaRegistryUrl = avroSchemaRegistryUrl, 
+        processor = esProcessor)
+
+      val consumer = new MultiTopicConsumer(
+        topics = kafkaTopics, 
+        broker = kafkaBroker, 
+        groupId = kafkaGroupId,
+        processor = avroProcessor)
+
+      // Execture beforeAction
+      println("Executing pre-consuming actions...")
+      esProcessor.beforeAction()
+
+      // Start consuming & processing
+      println(s"Reading from broker $kafkaBroker")
+      consumer.readForever()
+    }
+
+    //These threads will actually never be ready. 
+    //This is a hedonist bot.
+    Await.ready(activityWork, Duration.Inf)
+    Await.ready(trailWork, Duration.Inf)
+  }
+}
