76ffee137a958e547aa59ad7b02d36a696d2f1c3
Pavel Kiselev <kpashka@gmail.com>
2015-12-22 21:31:56 +0600

    Mapping formatting

--- src/main/scala/consumer/elastic/AvroTransformers.scala
+++ src/main/scala/consumer/elastic/AvroTransformers.scala
@@ -20,23 +20,23 @@ object AvroTransformers {
 
   def address(name: String) =
     name nested (
-      "address1" typed StringType analyzer "autocomplete",
-      "address2" typed StringType analyzer "autocomplete",
-      "city" typed StringType analyzer "autocomplete",
-      "zip" typed StringType index "not_analyzed",
-      "region" typed StringType analyzer "autocomplete",
-      "country" typed StringType analyzer "autocomplete",
+      "address1"  typed StringType analyzer "autocomplete",
+      "address2"  typed StringType analyzer "autocomplete",
+      "city"      typed StringType analyzer "autocomplete",
+      "zip"       typed StringType index "not_analyzed",
+      "region"    typed StringType analyzer "autocomplete",
+      "country"   typed StringType analyzer "autocomplete",
       "continent" typed StringType analyzer "autocomplete",
-      "currency" typed StringType analyzer "autocomplete"
+      "currency"  typed StringType analyzer "autocomplete"
     )
 
   final case class Country()(implicit ec: ExecutionContext) extends AvroTransformer {
     def mapping =
       "countries" as (
-        "id" typed IntegerType,
-        "name" typed StringType analyzer "autocomplete",
+        "id"        typed IntegerType,
+        "name"      typed StringType analyzer "autocomplete",
         "continent" typed StringType index "not_analyzed",
-        "currency" typed StringType index "not_analyzed"
+        "currency"  typed StringType index "not_analyzed"
       )
 
     def fields = List.empty
@@ -46,10 +46,10 @@ object AvroTransformers {
   final case class Region()(implicit ec: ExecutionContext) extends AvroTransformer {
     def mapping =
       "regions" as (
-        "id" typed IntegerType,
-        "name" typed StringType analyzer "autocomplete",
+        "id"        typed IntegerType,
+        "name"      typed StringType analyzer "autocomplete",
         "continent" typed StringType index "not_analyzed",
-        "currency" typed StringType index "not_analyzed"
+        "currency"  typed StringType index "not_analyzed"
       )
 
     def fields = List.empty
@@ -59,49 +59,49 @@ object AvroTransformers {
     def mapping =
       "customers_search_view" as (
         // Customer
-        "id" typed IntegerType,
-        "name" typed StringType analyzer "autocomplete",
-        "email" typed StringType analyzer "autocomplete",
-        "isDisabled" typed BooleanType,
-        "isGuest" typed BooleanType,
-        "isBlacklisted" typed BooleanType,
-        "joinedAt" typed DateType format "yyyy-MM-dd HH:mm:ss",
-        "revenue" typed IntegerType,
-        "rank" typed IntegerType,
+        "id"                      typed IntegerType,
+        "name"                    typed StringType analyzer "autocomplete",
+        "email"                   typed StringType analyzer "autocomplete",
+        "isDisabled"              typed BooleanType,
+        "isGuest"                 typed BooleanType,
+        "isBlacklisted"           typed BooleanType,
+        "joinedAt"                typed DateType format "yyyy-MM-dd HH:mm:ss",
+        "revenue"                 typed IntegerType,
+        "rank"                    typed IntegerType,
         // Orders
-        "orderCount" typed IntegerType,
+        "orderCount"              typed IntegerType,
         "orders" nested(
-          "referenceNumber" typed StringType analyzer "autocomplete",
-          "status" typed StringType index "not_analyzed",
-          "createdAt" typed DateType format "yyyy-MM-dd HH:mm:ss",
-          "placedAt" typed DateType format "yyyy-MM-dd HH:mm:ss",
-          "subTotal" typed IntegerType,
-          "shippingTotal" typed IntegerType,
-          "adjustmentsTotal" typed IntegerType,
-          "taxesTotal" typed IntegerType,
-          "grandTotal" typed IntegerType
+          "referenceNumber"       typed StringType analyzer "autocomplete",
+          "status"                typed StringType index "not_analyzed",
+          "createdAt"             typed DateType format "yyyy-MM-dd HH:mm:ss",
+          "placedAt"              typed DateType format "yyyy-MM-dd HH:mm:ss",
+          "subTotal"              typed IntegerType,
+          "shippingTotal"         typed IntegerType,
+          "adjustmentsTotal"      typed IntegerType,
+          "taxesTotal"            typed IntegerType,
+          "grandTotal"            typed IntegerType
         ),
         // Purchased items
-        "purchasedItemCount" typed IntegerType,
+        "purchasedItemCount"      typed IntegerType,
         "purchasedItems" nested (
-          "sku" typed StringType analyzer "autocomplete",
-          "name" typed StringType analyzer "autocomplete",
-          "price" typed IntegerType
+          "sku"                   typed StringType analyzer "autocomplete",
+          "name"                  typed StringType analyzer "autocomplete",
+          "price"                 typed IntegerType
         ),
         // Addresses
-        "shippingAddressesCount" typed IntegerType,
+        "shippingAddressesCount"  typed IntegerType,
         address("shippingAddresses"),
-        "billingAddressesCount" typed IntegerType,
+        "billingAddressesCount"   typed IntegerType,
         address("billingAddresses"),
         // Store credits
-        "storeCreditTotal" typed IntegerType,
-        "storeCreditCount" typed IntegerType,
+        "storeCreditTotal"        typed IntegerType,
+        "storeCreditCount"        typed IntegerType,
         // Saved for later
-        "savedForLaterCount" typed IntegerType,
-        "savedForLater" nested(
-          "sku" typed StringType analyzer "autocomplete",
-          "name" typed StringType analyzer "autocomplete",
-          "price" typed IntegerType
+        "savedForLaterCount"      typed IntegerType,
+        "savedForLater" nested (
+          "sku"                   typed StringType analyzer "autocomplete",
+          "name"                  typed StringType analyzer "autocomplete",
+          "price"                 typed IntegerType
         )
       )
 
@@ -112,74 +112,74 @@ object AvroTransformers {
     def mapping = {
       "orders_search_view" as (
         // Order
-        "id" typed IntegerType,
-        "referenceNumber" typed StringType analyzer "autocomplete",
-        "status" typed StringType index "not_analyzed",
-        "createdAt" typed DateType format "yyyy-MM-dd HH:mm:ss",
-        "placedAt" typed DateType format "yyyy-MM-dd HH:mm:ss",
-        "currency" typed StringType index "not_analyzed",
+        "id"                      typed IntegerType,
+        "referenceNumber"         typed StringType analyzer "autocomplete",
+        "status"                  typed StringType index "not_analyzed",
+        "createdAt"               typed DateType format "yyyy-MM-dd HH:mm:ss",
+        "placedAt"                typed DateType format "yyyy-MM-dd HH:mm:ss",
+        "currency"                typed StringType index "not_analyzed",
         // Totals
-        "subTotal" typed IntegerType,
-        "shippingTotal" typed IntegerType,
-        "adjustmentsTotal" typed IntegerType,
-        "taxesTotal" typed IntegerType,
-        "grandTotal" typed IntegerType,
+        "subTotal"                typed IntegerType,
+        "shippingTotal"           typed IntegerType,
+        "adjustmentsTotal"        typed IntegerType,
+        "taxesTotal"              typed IntegerType,
+        "grandTotal"              typed IntegerType,
         // Customer
         "customer" nested (
-          "name" typed StringType analyzer "autocomplete",
-          "email" typed StringType analyzer "autocomplete",
-          "isBlacklisted" typed BooleanType,
-          "joinedAt" typed DateType format "yyyy-MM-dd HH:mm:ss",
-          "revenue" typed IntegerType,
-          "rank" typed IntegerType
+          "name"                  typed StringType analyzer "autocomplete",
+          "email"                 typed StringType analyzer "autocomplete",
+          "isBlacklisted"         typed BooleanType,
+          "joinedAt"              typed DateType format "yyyy-MM-dd HH:mm:ss",
+          "revenue"               typed IntegerType,
+          "rank"                  typed IntegerType
         ),
         // Line items
-        "lineItemCount" typed IntegerType,
+        "lineItemCount"           typed IntegerType,
         "lineItems" nested (
-          "status" typed StringType index "not_analyzed",
-          "sku" typed StringType analyzer "autocomplete",
-          "name" typed StringType analyzer "autocomplete",
-          "price" typed IntegerType
+          "status"                typed StringType index "not_analyzed",
+          "sku"                   typed StringType analyzer "autocomplete",
+          "name"                  typed StringType analyzer "autocomplete",
+          "price"                 typed IntegerType
         ),
         // Payments
         "payments" nested (
-          "paymentMethodType" typed StringType index "not_analyzed",
-          "amount" typed IntegerType,
-          "currency" typed StringType index "not_analyzed"
+          "paymentMethodType"     typed StringType index "not_analyzed",
+          "amount"                typed IntegerType,
+          "currency"              typed StringType index "not_analyzed"
         ),
-        "creditCardCount" typed IntegerType,
-        "creditCardTotal" typed IntegerType,
-        "giftCardCount" typed IntegerType,
-        "giftCardTotal" typed IntegerType,
-        "storeCreditCount" typed IntegerType,
-        "storeCreditTotal" typed IntegerType,
+        "creditCardCount"         typed IntegerType,
+        "creditCardTotal"         typed IntegerType,
+        "giftCardCount"           typed IntegerType,
+        "giftCardTotal"           typed IntegerType,
+        "storeCreditCount"        typed IntegerType,
+        "storeCreditTotal"        typed IntegerType,
         // Shipments
-        "shipmentCount" typed IntegerType,
+        "shipmentCount"           typed IntegerType,
         "shipments" nested (
-          "status" typed StringType index "not_analyzed",
-          "shippingPrice" typed IntegerType,
-          "adminDisplayName" typed StringType analyzer "autocomplete",
+          "status"                typed StringType index "not_analyzed",
+          "shippingPrice"         typed IntegerType,
+          "adminDisplayName"      typed StringType analyzer "autocomplete",
           "storefrontDisplayName" typed StringType analyzer "autocomplete"
         ),
         // Addresses
-        "shippingAddressesCount" typed IntegerType,
+        "shippingAddressesCount"  typed IntegerType,
         address("shippingAddresses"),
-        "billingAddressesCount" typed IntegerType,
+        "billingAddressesCount"   typed IntegerType,
         address("billingAddresses"),
         // Assignments
-        "assignmentCount" typed IntegerType,
-        "assignees" nested(
-          "firstName" typed StringType analyzer "autocomplete",
-          "lastName" typed StringType analyzer "autocomplete",
-          "assignedAt" typed DateType format "yyyy-MM-dd HH:mm:ss"
+        "assignmentCount"         typed IntegerType,
+        "assignees" nested (
+          "firstName"             typed StringType analyzer "autocomplete",
+          "lastName"              typed StringType analyzer "autocomplete",
+          "assignedAt"            typed DateType format "yyyy-MM-dd HH:mm:ss"
         ),
         // RMAs
-        "rmaCount" typed IntegerType,
+        "rmaCount"                typed IntegerType,
         "rmas" nested (
-          "referenceNumber" typed StringType analyzer "autocomplete",
-          "status" typed StringType index "not_analyzed",
-          "rmaType" typed StringType index "not_analyzed",
-          "placedAt" typed DateType format "yyyy-MM-dd HH:mm:ss"
+          "referenceNumber"       typed StringType analyzer "autocomplete",
+          "status"                typed StringType index "not_analyzed",
+          "rmaType"               typed StringType index "not_analyzed",
+          "placedAt"              typed DateType format "yyyy-MM-dd HH:mm:ss"
         )
       )
     }
@@ -193,17 +193,17 @@ object AvroTransformers {
     def mapping =
       "store_admins_search_view" as (
         // Customer
-        "id" typed IntegerType,
-        "email" typed StringType analyzer "autocomplete",
-        "firstName" typed StringType analyzer "autocomplete",
-        "lastName" typed StringType analyzer "autocomplete",
-        "department" typed StringType analyzer "autocomplete",
-        "createdAt" typed DateType format "yyyy-MM-dd HH:mm:ss",
+        "id"                typed IntegerType,
+        "email"             typed StringType analyzer "autocomplete",
+        "firstName"         typed StringType analyzer "autocomplete",
+        "lastName"          typed StringType analyzer "autocomplete",
+        "department"        typed StringType analyzer "autocomplete",
+        "createdAt"         typed DateType format "yyyy-MM-dd HH:mm:ss",
         // Assignments
-        "assignmentsCount" typed IntegerType,
-        "assignments" nested(
+        "assignmentsCount"  typed IntegerType,
+        "assignments" nested (
           "referenceNumber" typed StringType analyzer "autocomplete",
-          "assignedAt" typed DateType format "yyyy-MM-dd HH:mm:ss"
+          "assignedAt"      typed DateType format "yyyy-MM-dd HH:mm:ss"
         )
       )
 
