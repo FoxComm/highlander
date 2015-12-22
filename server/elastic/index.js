fab48345f37c57958ecef16a3c6dd92ba8795935
Andrey Rublev <andrey@foxcommerce.com>
2015-12-22 07:34:27 +0600

    format response from elastic search

--- server/elastic/index.js
+++ server/elastic/index.js
@@ -4,11 +4,29 @@ const proxy = require('koa-proxy');
 module.exports = function(app) {
   const config = app.config.elastic;
 
-  app.use(proxy({
+  const proxyHandler = proxy({
     host:  config.uri,
     match: /^\/api\/search\//,
     map: function(path) {
       return path.replace(/^\/api\/search\//, '/phoenix/');
     }
-  }));
+  });
+
+  function* formatHandler() {
+    const body = JSON.parse(this.body);
+
+    body.hits.result = body.hits.hits.map(hit => hit._source);
+    body.hits.hits = void 0;
+    body.hits.pagination = {total: body.hits.total};
+    body.hits.total = void 0;
+
+    this.body = JSON.stringify(body.hits);
+  }
+
+  function* proxyAndFormat(next) {
+    yield proxyHandler.call(this, next);
+    yield formatHandler.call(this);
+  }
+
+  app.use(proxyAndFormat);
 };
