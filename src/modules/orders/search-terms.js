aa74bd67aafe7c876b54cd55a57d66abe2b1386e
Jeff Mataya <jeff@foxcommerce.com>
2015-12-30 20:59:01 -0800

    Add the visuals for all orders terms

--- src/modules/orders/search-terms.js
+++ src/modules/orders/search-terms.js
@@ -34,17 +34,20 @@ const searchTerms = [
       {
         title: 'State',
         type: 'enum',
+        term: 'shipments.status',
         suggestions: [
-          { display: 'Shipped', value: '' },
-          { display: 'Partially Shipped', value: '' },
-          { display: 'Delivered', value: '' }
+          { display: 'Shipped', value: 'shipped' },
+          { display: 'Partially Shipped', value: 'partiallyShipped' },
+          { display: 'Delivered', value: 'delivered' }
         ]
       }, {
         title: 'Method',
-        type: 'string'
+        type: 'string',
+        term: 'shipments.admin_display_name',
       }, {
         title: 'Cost',
-        type: 'number'
+        type: 'number',
+        term: 'shipments.shipping_price'
       }, {
         title: 'Total Number',
         type: 'number'
@@ -63,7 +66,89 @@ const searchTerms = [
         title: 'Email',
         type: 'string',
         term: 'customer.email'
+      },
+      {
+        title: 'Total Sales',
+        type: 'number',
+        term: 'customer.revenue'
+      },
+      {
+        title: 'Blacklist Status',
+        type: 'bool',
+        term: 'customer.is_blacklisted'
+      },
+      {
+        title: 'Date Joined',
+        type: 'date',
+        term: 'customer.joined_at'
       }
+    ]
+  }, {
+    title: 'Shipping',
+    type: 'object',
+    options: [
+      {
+        title: 'City',
+        type: 'string',
+        term: 'shippingAddresses.city'
+      },
+      {
+        title: 'State',
+        type: 'string',
+        term: 'shippingAddresses.region'
+      },
+      {
+        title: 'Zip',
+        type: 'string',
+        term: 'shippingAddresses.zip'
+      }
+    ]
+  }, {
+    title: 'Billing',
+    type: 'object',
+    options: [
+      {
+        title: 'City',
+        type: 'string',
+        term: 'billingAddresses.city'
+      },
+      {
+        title: 'State',
+        type: 'string',
+        term: 'billingAddresses.region'
+      },
+      {
+        title: 'Zip',
+        type: 'string',
+        term: 'billingAddresses.zip'
+      }
+    ]
+  }, {
+    title: 'Items',
+    type: 'object',
+    options: [
+      {
+        title: 'Total Number',
+        type: 'number'
+      },
+      {
+        title: 'Product Name',
+        type: 'string',
+        term: 'lineItems.name'
+      },
+      {
+        title: 'Product SKU',
+        type: 'string',
+        term: 'lineItems.sku'
+      }
+    ]
+  }, {
+    title: 'Assignee',
+    type: 'enum',
+    term: 'assignees',
+    suggestions: [
+      { display: 'Has Assignee', value: 'true' },
+      { display: 'Has No Assignee', value: 'false' }
     ]
   }
 ];
