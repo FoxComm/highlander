4605c58d9b1b564955468f8ba8453cd2a7045311
Eugene Sypachev <eugene.sypachev@gmail.com>
2016-01-05 01:03:03 +0400

    general idea

--- src/components/header/breadcrumb.jsx
+++ src/components/header/breadcrumb.jsx
@@ -12,11 +12,30 @@ export default class Breadcrumb extends React.Component {
   render() {
     const pathname = this.context.location.pathname.replace(/^\/|\/$/gm, '');
 
-    const items = pathname.split('/').map((item, index) => {
+    console.log(pathname);
+
+    const pathParts = pathname.split('/');
+    const items = pathParts.map((item, index) => {
       let classname = index > 0 ? 'icon-chevron-right' : null;
       let itemName = inflect(item, 'capitalize');
+      console.log(item);
       return <span className={classname} key={`header-item-${index}`}>{` ${itemName} `}</span>;
     });
+
+    //ToDo:
+    // - create routes for each part
+    // - create details route for each `number` section
+    // - collect route parts into clickable breadcrumbs
+    // - check routes (the spec is needed, not all routes can be generated simply right now)
+
+    const acc = {routes: [], lastRoute: null};
+    const pathNames = pathParts.reduce((acc, part) => {
+      const newRoute = `${acc.lastRoute}-${part}`;
+      acc.routes.push(newRoute);
+      acc.lastRoute = newRoute;
+      return acc;
+    }, acc);
+    console.log(pathNames);
 
     return <div className="breadcrumb">{items}</div>;
   }
