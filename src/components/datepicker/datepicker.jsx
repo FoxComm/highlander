81d6ebd25743aee5fb728707e321e756bed417e4
Jeff Mataya <jeff@foxcommerce.com>
2016-01-05 15:57:49 -0800

    Programmatically show contents of the date picker

--- src/components/datepicker/datepicker.jsx
+++ src/components/datepicker/datepicker.jsx
@@ -1,132 +1,66 @@
 import React, { PropTypes } from 'react';
 
+import _ from 'lodash';
+import classNames from 'classnames';
+
 const DatePicker = props => {
+  const { month, year } = props;
+
+  const firstMonth = new Date(year, month - 1, 1);
+  const secondMonth = new Date(year, month, 1);
+  const weeks = ['S', 'M', 'T', 'W', 'T', 'F', 'S'];
+
+  const daysToDisplay = date => {
+    const startDay = 1 - date.getDay(); // Day is 1-based in JS Date.
+
+    return _.range(startDay, startDay + 35).map(day => {
+      const dt = new Date(date.getFullYear(), date.getMonth(), day);
+      const klass = classNames('fc-datepicker__day', {
+        '_last-month': date.getMonth() > dt.getMonth(),
+        '_next-month': date.getMonth() < dt.getMonth()
+      });
+
+      return <div className={klass}>{dt.getDate()}</div>;
+    });
+  };
+
+  const render = date => {
+    const monthName = date.toLocaleString('en-us', { month: 'long', year: 'numeric' });
+
+    return (
+      <div className="fc-datepicker__month">
+        <div className="fc-datepicker__month-name">
+          <span>{monthName}</span>
+        </div>
+        <div className="fc-datepicker__weeks-header">
+          {weeks.map(w => <div className="fc-datepicker__week">{w}</div>)}
+        </div>
+        <div className="fc-datepicker__days">
+          {daysToDisplay(date)}
+        </div>
+      </div>
+    );
+  };
+
   return (
     <div className="fc-datepicker">
       <input type="text" />
       <div className="fc-datepicker__dropdown-container">
-        <div className="fc-datepicker__month">
-          <div className="fc-datepicker__month-name">
-            <span>March 2015</span>
-          </div>
-          <div className="fc-datepicker__weeks-header">
-            <div className="fc-datepicker__week">S</div>
-            <div className="fc-datepicker__week">M</div>
-            <div className="fc-datepicker__week">T</div>
-            <div className="fc-datepicker__week">W</div>
-            <div className="fc-datepicker__week">T</div>
-            <div className="fc-datepicker__week">F</div>
-            <div className="fc-datepicker__week">S</div>
-          </div>
-          <div className="fc-datepicker__week-row">
-            <div className="fc-datepicker__day">1</div>
-            <div className="fc-datepicker__day">2</div>
-            <div className="fc-datepicker__day">3</div>
-            <div className="fc-datepicker__day">4</div>
-            <div className="fc-datepicker__day">5</div>
-            <div className="fc-datepicker__day">6</div>
-            <div className="fc-datepicker__day">7</div>
-          </div>
-          <div className="fc-datepicker__week-row">
-            <div className="fc-datepicker__day">8</div>
-            <div className="fc-datepicker__day">9</div>
-            <div className="fc-datepicker__day">10</div>
-            <div className="fc-datepicker__day">11</div>
-            <div className="fc-datepicker__day">12</div>
-            <div className="fc-datepicker__day">13</div>
-            <div className="fc-datepicker__day">14</div>
-          </div>
-          <div className="fc-datepicker__week-row">
-            <div className="fc-datepicker__day">15</div>
-            <div className="fc-datepicker__day">16</div>
-            <div className="fc-datepicker__day">17</div>
-            <div className="fc-datepicker__day">18</div>
-            <div className="fc-datepicker__day">19</div>
-            <div className="fc-datepicker__day">20</div>
-            <div className="fc-datepicker__day">21</div>
-          </div>
-          <div className="fc-datepicker__week-row">
-            <div className="fc-datepicker__day">22</div>
-            <div className="fc-datepicker__day">23</div>
-            <div className="fc-datepicker__day">24</div>
-            <div className="fc-datepicker__day">25</div>
-            <div className="fc-datepicker__day">26</div>
-            <div className="fc-datepicker__day">27</div>
-            <div className="fc-datepicker__day">28</div>
-          </div>
-          <div className="fc-datepicker__week-row">
-            <div className="fc-datepicker__day">29</div>
-            <div className="fc-datepicker__day">30</div>
-            <div className="fc-datepicker__day">31</div>
-            <div className="fc-datepicker__day">1</div>
-            <div className="fc-datepicker__day">2</div>
-            <div className="fc-datepicker__day">3</div>
-            <div className="fc-datepicker__day">4</div>
-          </div>
-        </div>
-        <div className="fc-datepicker__month">
-          <div className="fc-datepicker__month-name">
-            <span>April 2015</span>
-          </div>
-          <div className="fc-datepicker__weeks-header">
-            <div className="fc-datepicker__week">S</div>
-            <div className="fc-datepicker__week">M</div>
-            <div className="fc-datepicker__week">T</div>
-            <div className="fc-datepicker__week">W</div>
-            <div className="fc-datepicker__week">T</div>
-            <div className="fc-datepicker__week">F</div>
-            <div className="fc-datepicker__week">S</div>
-          </div>
-          <div className="fc-datepicker__week-row">
-            <div className="fc-datepicker__day">29</div>
-            <div className="fc-datepicker__day">30</div>
-            <div className="fc-datepicker__day">31</div>
-            <div className="fc-datepicker__day">1</div>
-            <div className="fc-datepicker__day">2</div>
-            <div className="fc-datepicker__day">3</div>
-            <div className="fc-datepicker__day">4</div>
-          </div>
-          <div className="fc-datepicker__week-row">
-            <div className="fc-datepicker__day">5</div>
-            <div className="fc-datepicker__day">6</div>
-            <div className="fc-datepicker__day">7</div>
-            <div className="fc-datepicker__day">8</div>
-            <div className="fc-datepicker__day">9</div>
-            <div className="fc-datepicker__day">10</div>
-            <div className="fc-datepicker__day">11</div>
-          </div>
-          <div className="fc-datepicker__week-row">
-            <div className="fc-datepicker__day">12</div>
-            <div className="fc-datepicker__day">13</div>
-            <div className="fc-datepicker__day">14</div>
-            <div className="fc-datepicker__day">15</div>
-            <div className="fc-datepicker__day">16</div>
-            <div className="fc-datepicker__day">17</div>
-            <div className="fc-datepicker__day">18</div>
-          </div>
-          <div className="fc-datepicker__week-row">
-            <div className="fc-datepicker__day">19</div>
-            <div className="fc-datepicker__day">20</div>
-            <div className="fc-datepicker__day">21</div>
-            <div className="fc-datepicker__day">22</div>
-            <div className="fc-datepicker__day">23</div>
-            <div className="fc-datepicker__day">24</div>
-            <div className="fc-datepicker__day">25</div>
-          </div>
-          <div className="fc-datepicker__week-row">
-            <div className="fc-datepicker__day">26</div>
-            <div className="fc-datepicker__day">27</div>
-            <div className="fc-datepicker__day">28</div>
-            <div className="fc-datepicker__day">29</div>
-            <div className="fc-datepicker__day">30</div>
-            <div className="fc-datepicker__day">1</div>
-            <div className="fc-datepicker__day">2</div>
-          </div>
-        </div>
+        {render(firstMonth)}
+        {render(secondMonth)}
       </div>
     </div>
   );
 };
 
+DatePicker.propTypes = {
+  month: PropTypes.number,
+  year: PropTypes.number
+};
+
+DatePicker.defaultProps = {
+  month: new Date().getMonth() + 1,
+  year: new Date().getFullYear()
+};
 
 export default DatePicker;
