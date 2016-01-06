6889d37c02ce8f6076b6f87369e934832d398085
Jeff Mataya <jeff@foxcommerce.com>
2016-01-05 17:08:59 -0800

    Allow for selection in the date picker

--- src/components/datepicker/datepicker.jsx
+++ src/components/datepicker/datepicker.jsx
@@ -1,66 +1,126 @@
 import React, { PropTypes } from 'react';
+import { autobind } from 'core-decorators';
 
 import _ from 'lodash';
 import classNames from 'classnames';
 
-const DatePicker = props => {
-  const { month, year } = props;
+const weeks = ['S', 'M', 'T', 'W', 'T', 'F', 'S'];
 
-  const firstMonth = new Date(year, month - 1, 1);
-  const secondMonth = new Date(year, month, 1);
-  const weeks = ['S', 'M', 'T', 'W', 'T', 'F', 'S'];
-
-  const daysToDisplay = date => {
-    const startDay = 1 - date.getDay(); // Day is 1-based in JS Date.
-
-    return _.range(startDay, startDay + 35).map(day => {
-      const dt = new Date(date.getFullYear(), date.getMonth(), day);
-      const klass = classNames('fc-datepicker__day', {
-        '_last-month': date.getMonth() > dt.getMonth(),
-        '_next-month': date.getMonth() < dt.getMonth()
-      });
-
-      return <div className={klass}>{dt.getDate()}</div>;
-    });
+export default class DatePicker extends React.Component {
+  static propTypes = {
+    month: PropTypes.number,
+    year: PropTypes.number
   };
 
-  const render = date => {
+  static defaultProps = {
+    month: new Date().getMonth() + 1,
+    year: new Date().getFullYear()
+  };
+
+  constructor(props, context) {
+    super(props, context);
+
+    // Assume that the month coming from the called is an index based on 1,
+    // because this is how it will appear in the UI. For our purposes, turn it
+    // into a 0-based index as that's how JS expects it.
+    this.state = {
+      selectedDate: null,
+      month: props.month - 1,
+      year: props.year
+    };
+  }
+
+  @autobind
+  goBackMonth() {
+    const newDate = new Date(this.state.year, this.state.month - 1, 1);
+    this.setState({
+      ...this.state,
+      month: newDate.getMonth(),
+      year: newDate.getFullYear()
+    });
+  }
+
+  @autobind
+  goForwardMonth() {
+    const newDate = new Date(this.state.year, this.state.month + 1, 1);
+    this.setState({
+      ...this.state,
+      month: newDate.getMonth(),
+      year: newDate.getFullYear()
+    });
+  }
+
+  @autobind
+  selectDate(date) {
+    this.setState({ 
+      ...this.state,
+      selectedDate: date 
+    });
+  }
+
+  @autobind
+  renderMonth(date, isFirst = false) {
     const monthName = date.toLocaleString('en-us', { month: 'long', year: 'numeric' });
 
     return (
       <div className="fc-datepicker__month">
-        <div className="fc-datepicker__month-name">
-          <span>{monthName}</span>
+        <div className="fc-datepicker__month-header">
+          <a className="fc-datepicker__month-action-back" onClick={this.goBackMonth}>
+            {isFirst && <i className="icon-chevron-left" />} 
+          </a>
+          <div className="fc-datepicker__month-name">
+            {monthName}
+          </div>
+          <a className="fc-datepicker__month-action-forward" onClick={this.goForwardMonth}>
+            {!isFirst && <i className="icon-chevron-right" />} 
+          </a>
         </div>
         <div className="fc-datepicker__weeks-header">
           {weeks.map(w => <div className="fc-datepicker__week">{w}</div>)}
         </div>
         <div className="fc-datepicker__days">
-          {daysToDisplay(date)}
+          {this.renderDays(date)}
         </div>
       </div>
     );
-  };
+  }
 
-  return (
-    <div className="fc-datepicker">
-      <input type="text" />
-      <div className="fc-datepicker__dropdown-container">
-        {render(firstMonth)}
-        {render(secondMonth)}
+  @autobind
+  renderDays(date) {
+    const startDay = 1 - date.getDay(); // Day is 1-based in JS Date.
+    const selectedTime = !_.isNull(this.state.selectedDate)
+      ? this.state.selectedDate.getTime()
+      : ''; 
+
+    return _.range(startDay, startDay + 35).map(day => {
+      const dt = new Date(date.getFullYear(), date.getMonth(), day);
+      const klass = classNames('fc-datepicker__day', {
+        '_last-month': date.getMonth() > dt.getMonth(),
+        '_next-month': date.getMonth() < dt.getMonth(),
+        '_selected': _.isEqual(dt.getTime(), selectedTime)
+      });
+
+      return (
+        <div className={klass} onClick={() => this.selectDate(dt)}>
+          {dt.getDate()}
+        </div>
+      );
+    });
+  }
+
+  render() {
+    const { month, year } = this.state;
+    const firstMonth = new Date(year, month, 1);
+    const secondMonth = new Date(year, month + 1, 1);
+
+    return (
+      <div className="fc-datepicker">
+        <input type="text" />
+        <div className="fc-datepicker__dropdown-container">
+          {this.renderMonth(firstMonth, true)}
+          {this.renderMonth(secondMonth)}
+        </div>
       </div>
-    </div>
-  );
-};
-
-DatePicker.propTypes = {
-  month: PropTypes.number,
-  year: PropTypes.number
-};
-
-DatePicker.defaultProps = {
-  month: new Date().getMonth() + 1,
-  year: new Date().getFullYear()
-};
-
-export default DatePicker;
+    );
+  }
+}
