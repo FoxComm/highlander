1c5e75899026355fecbefea424c9e8b8c07c9bc1
Eugene Sypachev <eugene.sypachev@gmail.com>
2016-01-13 00:00:01 +0300

    expand sidebar on click

--- src/components/sidebar/sidebar.jsx
+++ src/components/sidebar/sidebar.jsx
@@ -1,15 +1,46 @@
+
 import React from 'react';
-import Navigation from './navigation';
 import static_url from '../../lib/s3';
+import classNames from 'classnames';
+import { autobind } from 'core-decorators';
+
+import Navigation from './navigation';
 
 export default class Sidebar extends React.Component {
+
+  constructor(...args) {
+    super(...args);
+    this.state = {
+      open: false
+    };
+  }
+
+  @autobind
+  toggleSidebar() {
+    const status = !this.state.open;
+    this.setState({open: status});
+  }
+
   render() {
+    const indicatorClass = classNames({
+      'icon-chevron-left': this.state.open,
+      'icon-chevron-right': !this.state.open
+    });
+    const sidebarClass = classNames('fc-sidebar', {
+      '_open': this.state.open
+    });
+    const controlClass = classNames('fc-sidebar__control', {
+      '_open': this.state.open
+    });
     return (
-      <aside role='complimentary' className='fc-sidebar'>
-        <div className='logo'>
+      <aside role="complimentary" className={sidebarClass}>
+        <div className="logo">
           <img src={static_url('images/fc-logo-nav.svg')}></img>
         </div>
-        <Navigation/>
+        <div className={controlClass} onClick={this.toggleSidebar}>
+          <i className={indicatorClass}></i>
+        </div>
+        <Navigation open={this.state.open}/>
       </aside>
     );
   }
