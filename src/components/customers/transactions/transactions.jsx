595ab4f523e576238cb063b4c7a8472510d8fc35
Sergey Rublev <sergey@foxcommerce.com>
2016-01-19 07:00:11 +0600

    Limit customer transactions to current customer

--- src/components/customers/transactions/transactions.jsx
+++ src/components/customers/transactions/transactions.jsx
@@ -7,41 +7,65 @@ import _ from 'lodash';
 import ListPage from '../../list-page/list-page';
 import OrderTransactionRow from './order-txn-row';
 
-const getState = state => ({ list: state.customers.transactions });
-
 const mapDispatchToProps = dispatch => {
   return { actions: bindActionCreators(actions, dispatch) };
 };
 
-const CustomerTransactions = props => {
+const searchUrl = "orders_search_view/_search";
 
-  const renderRow = (row, index, columns) => {
-    const key = `order-${row.referenceNumber}`;
-    return <OrderTransactionRow order={row} columns={columns} key={key} />;
-  };
+@connect((state, props) => ({
+  list: state.customers.transactions,
+  customer: state.customers.details[props.params.customerId].details,
+}), mapDispatchToProps)
+export default class CustomerTransactions extends React.Component {
 
-  const tableColumns = [
-    {field: 'referenceNumber', text: 'Order', type: 'text', model: 'order'},
-    {field: 'placedAt', text: 'Date/Time', type: 'datetime'},
-    {field: 'customer.modality', text: 'Modality'},
-    {field: 'status', text: 'Order Status', type: 'status', model: 'order'},
-    {field: 'payment.status', text: 'Payment Status'},
-    {field: 'assignee', text: 'Assignee'},
-    {field: 'grandTotal', text: 'Total'},
-    //{field: 'return', text: 'Return'},
-  ];
+  componentDidMount() {
+    this.props.actions.addSearchFilter(searchUrl, [{
+      display: "Customer: " + this.props.customer.email,
+      selectedTerm: "customer.email", // FIXME: change to customer.id when ES will support this mapping
+      selectedOperator: "eq",
+      hidden: true,
+      value: {
+        type: "string",
+        value: this.props.customer.email
+      }
+    }]);
+  }
 
-  return (
-    <ListPage
-      addTitle="Order"
-      emptyResultMessage="No orders found."
-      list={props.list}
-      renderRow={renderRow}
-      tableColumns={tableColumns}
-      searchActions={props.actions}
-      title="Orders"
-      url="orders_search_view/_search" />
-  );
-};
+  render() {
+    const searchOptions = {
+      single: true,
+    };
 
-export default connect(getState, mapDispatchToProps)(CustomerTransactions);
+    const renderRow = (row, index, columns) => {
+      const key = `order-${row.referenceNumber}`;
+      return <OrderTransactionRow order={row} columns={columns} key={key} />;
+    };
+
+    const tableColumns = [
+      {field: 'referenceNumber', text: 'Order', type: 'text', model: 'order'},
+      {field: 'placedAt', text: 'Date/Time', type: 'datetime'},
+      {field: 'customer.modality', text: 'Modality'},
+      {field: 'status', text: 'Order Status', type: 'status', model: 'order'},
+      {field: 'payment.status', text: 'Payment Status'},
+      {field: 'assignee', text: 'Assignee'},
+      {field: 'grandTotal', text: 'Total'},
+      //{field: 'return', text: 'Return'},
+    ];
+
+    return (
+      <div className="fc-customer-transactions">
+        <ListPage
+        addTitle="Order"
+        emptyResultMessage="No orders found."
+        list={this.props.list}
+        renderRow={renderRow}
+        tableColumns={tableColumns}
+        searchActions={this.props.actions}
+        searchOptions={searchOptions}
+        title="Orders"
+        url={searchUrl} />
+      </div>
+    );
+  }
+}
