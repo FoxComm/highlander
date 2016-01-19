e24573eefc2ce59facce7b3ef923d9e7bb581756
Eugene Sypachev <eugene.sypachev@gmail.com>
2016-01-19 23:51:06 +0400

    failing test for collapsing manually automatically selected item

---
+++ test/acceptance/sidebar/navigation-item.jsx
@@ -0,0 +1,58 @@
+
+import _ from 'lodash';
+import React from 'react';
+import ShallowTestUtils from 'react-shallow-testutils';
+
+describe('NavigationItem', function() {
+
+  const NavigationItem = requireComponent('sidebar/navigation-item.jsx');
+  const Link = requireComponent('link/link.jsx');
+
+  let navigationItem;
+
+  const defaultProps = {
+    isExpandable: true,
+    to: 'donkeys',
+    icon: 'icon-donkey',
+    title: 'Donkeys',
+    children: [<a>Donkey 1</a>, <a>Donkey 2</a>],
+    routes: [{name: 'donkeys'}, {name: 'donkey-details'}],
+    toggleMenuItem: _.noop
+  };
+
+  afterEach(function() {
+    if (navigationItem) {
+      navigationItem.unmount();
+      navigationItem = null;
+    }
+  });
+
+  context('collapse behavior', function() {
+
+    it('should be closed if side bar is collapsed', function *() {
+      navigationItem = shallowRender(
+        <NavigationItem collapsed={true} isOpen={true} {...defaultProps}/>
+      );
+      expect(ShallowTestUtils.findAllWithClass(navigationItem, 'fc-navigation-item__children _open')).to.be.empty;
+    });
+
+    it('should be opened if side bar is not collapsed', function *() {
+      navigationItem = shallowRender(
+        <NavigationItem collapsed={false} isOpen={true} {...defaultProps}/>
+      );
+      expect(ShallowTestUtils.findAllWithClass(navigationItem, 'fc-navigation-item__children _open')).not.to.be.empty;
+    });
+
+    it('should be closed if side bar is not collapsed and open is false', function *() {
+      navigationItem = shallowRender(
+        <NavigationItem collapsed={false} isOpen={false} {...defaultProps}>
+          <Link to="donkeys">Donkeys</Link>
+          <Link to="donkeys-donkeys">Donkeys</Link>
+        </NavigationItem>
+      );
+      expect(ShallowTestUtils.findAllWithClass(navigationItem, 'fc-navigation-item__children _open')).to.be.empty;
+    });
+
+  });
+
+});
