cc07e1887e9157c5b02003993f2d1cb5d2c4057e
Eugene Sypachev <eugene.sypachev@gmail.com>
2016-01-20 21:13:22 +0400

    some defensive code

--- src/components/activity-notifications/item.jsx
+++ src/components/activity-notifications/item.jsx
@@ -11,100 +11,120 @@ import { IndexLink, Link } from '../link';
 
 const icon = (item) => {
   const type = _.get(item, 'activityType');
-  if (type.indexOf('order') >= 0) {
-    return <i className="icon icon-orders"></i>;
-  } else if (type.indexOf('cart') >= 0) {
-    return <i className="icon icon-orders"></i>;
-  } else if (type.indexOf('customer') >= 0) {
-    return <i className="icon icon-customers"></i>;
-  } else if (type.indexOf('gift_card') >= 0) {
-    return <i className="icon icon-gift-cards"></i>;
-  } else if (type.indexOf('store_credit') >= 0) {
-    return <i className="icon icon-store-credits"></i>;
+  if (!_.isEmpty(type)) {
+    if (type.indexOf('order') >= 0) {
+      return <i className="icon icon-orders"></i>;
+    } else if (type.indexOf('cart') >= 0) {
+      return <i className="icon icon-orders"></i>;
+    } else if (type.indexOf('customer') >= 0) {
+      return <i className="icon icon-customers"></i>;
+    } else if (type.indexOf('gift_card') >= 0) {
+      return <i className="icon icon-gift-cards"></i>;
+    } else if (type.indexOf('store_credit') >= 0) {
+      return <i className="icon icon-store-credits"></i>;
+    } else {
+      return <i className="icon icon-bell"></i>;
+    }
   } else {
     return <i className="icon icon-bell"></i>;
   }
 };
 
 const entity = (type) => {
-  if (type.indexOf('order') >= 0) {
-    return 'order';
-  } else if (type.indexOf('cart') >= 0) {
-    return 'cart for order';
-  } else if (type.indexOf('customer') >= 0) {
-    return 'customer';
-  } else if (type.indexOf('gift_card') >= 0) {
-    return 'gift-card';
-  } else if (type.indexOf('store_credit') >= 0) {
-    return 'store-credits';
+  if (!_.isEmpty(type)) {
+    if (type.indexOf('order') >= 0) {
+      return 'order';
+    } else if (type.indexOf('cart') >= 0) {
+      return 'cart for order';
+    } else if (type.indexOf('customer') >= 0) {
+      return 'customer';
+    } else if (type.indexOf('gift_card') >= 0) {
+      return 'gift-card';
+    } else if (type.indexOf('store_credit') >= 0) {
+      return 'store-credits';
+    } else {
+      return null;
+    }
   } else {
     return null;
   }
 };
 
 const buildDelimeter = (target) => {
-  if (target.indexOf('assigned') >= 0) {
-    return 'you to';
-  } else if (target.indexOf('unassigned') >= 0) {
-    return 'you from';
+  if (!_.isEmpty(type)) {
+    if (target.indexOf('assigned') >= 0) {
+      return 'you to';
+    } else if (target.indexOf('unassigned') >= 0) {
+      return 'you from';
+    } else {
+      return null;
+    }
   } else {
     return null;
   }
 };
 
 const buildLink = (target, id) => {
-  if (target.indexOf('order') >= 0) {
-    return (
-      <IndexLink to="order"
-                 params={{order: id}}
-                 className="fc-activity-notification-item__link">
-        {id}
-      </IndexLink>
-    );
-  } else if (target.indexOf('cart') >= 0) {
-    return (
-      <IndexLink to="order"
-                 params={{order: id}}
-                 className="fc-activity-notification-item__link">
-        {id}
-      </IndexLink>
-    );
-  } else if (target.indexOf('customer') >= 0) {
-    return (
-      <IndexLink to="customer"
-                 params={{customerId: id}}
-                 className="fc-activity-notification-item__link">
-        {id}
-      </IndexLink>
-    );
-  } else if (target.indexOf('gift_card') >= 0) {
-    return (
-      <IndexLink to="giftcard"
-                 params={{giftCard: id}}
-                 className="fc-activity-notification-item__link">
-        {id}
-      </IndexLink>
-    );
+  if (!_.isEmpty(type)) {
+    if (target.indexOf('order') >= 0) {
+      return (
+        <IndexLink to="order"
+                   params={{order: id}}
+                   className="fc-activity-notification-item__link">
+          {id}
+        </IndexLink>
+      );
+    } else if (target.indexOf('cart') >= 0) {
+      return (
+        <IndexLink to="order"
+                   params={{order: id}}
+                   className="fc-activity-notification-item__link">
+          {id}
+        </IndexLink>
+      );
+    } else if (target.indexOf('customer') >= 0) {
+      return (
+        <IndexLink to="customer"
+                   params={{customerId: id}}
+                   className="fc-activity-notification-item__link">
+          {id}
+        </IndexLink>
+      );
+    } else if (target.indexOf('gift_card') >= 0) {
+      return (
+        <IndexLink to="giftcard"
+                   params={{giftCard: id}}
+                   className="fc-activity-notification-item__link">
+          {id}
+        </IndexLink>
+      );
+    } else {
+      return (<span>{ id }</span>);
+    }
   } else {
     return (<span>{ id }</span>);
   }
 };
 
 const action = (target) => {
-  if (target.indexOf('created') >= 0) {
-    return 'created';
-  } else if (target.indexOf('updated') >= 0) {
-    return 'updated';
-  } else if (target.indexOf('deleted') >= 0) {
-    return 'deleted';
-  } else if (target.indexOf('assigned') >= 0) {
-    return 'assigned';
-  } else if (target.indexOf('unassigned') >= 0) {
-    return 'unassigned';
-  } else if (target.indexOf('removed') >= 0) {
-    return 'removed';
-  } else if (target.indexOf('changed') >= 0) {
-    return 'changed';
+  if (!_.isEmpty(type)) {
+    if (target.indexOf('created') >= 0) {
+      return 'created';
+    } else if (target.indexOf('updated') >= 0) {
+      return 'updated';
+    } else if (target.indexOf('deleted') >= 0) {
+      return 'deleted';
+    } else if (target.indexOf('assigned') >= 0) {
+      return 'assigned';
+    } else if (target.indexOf('unassigned') >= 0) {
+      return 'unassigned';
+    } else if (target.indexOf('removed') >= 0) {
+      return 'removed';
+    } else if (target.indexOf('changed') >= 0) {
+      return 'changed';
+    } else {
+      return 'made unknown action';
+    }
   } else {
     return 'made unknown action';
   }
