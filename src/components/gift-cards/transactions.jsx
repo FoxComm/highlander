27f7f23c2594255fb9ec94c4467d458e405d3509
Eugene Sypachev <eugene.sypachev@gmail.com>
2016-01-28 00:47:52 +0400

    searchable table for transactions

--- src/components/gift-cards/transactions.jsx
+++ src/components/gift-cards/transactions.jsx
@@ -1,10 +1,16 @@
+
+// libs
 import _ from 'lodash';
 import React, { PropTypes } from 'react';
 import { createSelector } from 'reselect';
-import TableView from '../table/tableview';
-import * as GiftCardsTransactionActions from '../../modules/gift-cards/transactions';
-
 import { connect } from 'react-redux';
+
+// components
+import TableView from '../table/tableview';
+import { SearchableList } from '../list-page';
+
+// redux
+import { actions as GiftCardsTransactionActions } from '../../modules/gift-cards/transactions';
 
 @connect(state => state.giftCards, GiftCardsTransactionActions)
 export default class GiftCardTransactions extends React.Component {
@@ -34,21 +40,31 @@ export default class GiftCardTransactions extends React.Component {
     return this.props.params.giftCard;
   }
 
+  get defaultSearchOptions() {
+    return {
+      singleSearch: true,
+    };
+  }
+
   componentDidMount() {
     this.props.actionReset(); // clean state from previous values
     this.props.initialFetch(this.giftCard);
   }
 
+  renderRow() {
+    return 'row';
+  }
+
   render() {
     return (
-      <div>
-        <TableView
-          columns={this.props.tableColumns}
-          data={this.props.transactions}
-          setState={fetchParams => this.props.fetch(this.giftCard, fetchParams)}
-          paginator={true}
-          />
-      </div>
+      <SearchableList
+        emptyResultMessage="No transactions found."
+        list={this.props.list}
+        renderRow={this.renderRow}
+        tableColumns={this.props.tableColumns}
+        searchActions={this.props}
+        searchOptions={this.defaultSearchOptions}
+        url="gift_card_transactions_view/_search" />
     );
   }
 }
--- src/modules/gift-cards/transactions.js
+++ src/modules/gift-cards/transactions.js
@@ -1,17 +1,20 @@
 
-import _ from 'lodash';
-import makePagination from '../pagination';
+// import _ from 'lodash';
+import makeLiveSearch from '../live-search';
 
-const namespace = 'GIFTCARD_TRANSACTIONS';
+// const namespace = 'GIFTCARD_TRANSACTIONS';
 
-const { reducer, fetch, initialFetch, actionReset } = makePagination(
-  giftCard => `/gift-cards/${giftCard}/transactions`,
-  namespace
-);
+// const { reducer, fetch, initialFetch, actionReset } = makePagination(
+//   giftCard => `/gift-cards/${giftCard}/transactions`,
+//   namespace
+// );
+const searchTerms = [];
+
+const searches = [];
+
+const { reducer, actions } = makeLiveSearch('giftCardTransactions', searchTerms, searches);
 
 export {
   reducer as default,
-  fetch,
-  initialFetch,
-  actionReset
+  actions
 };
