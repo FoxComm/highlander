5ca70976c1df9bef7d4b123e9ac80c9322bb330d
Eugene Sypachev <eugene.sypachev@gmail.com>
2016-01-29 22:43:45 +0400

    searches

--- src/modules/gift-cards/transactions.js
+++ src/modules/gift-cards/transactions.js
@@ -1,16 +1,60 @@
 
-// import _ from 'lodash';
 import makeLiveSearch from '../live-search';
 
-// const namespace = 'GIFTCARD_TRANSACTIONS';
-
-// const { reducer, fetch, initialFetch, actionReset } = makePagination(
-//   giftCard => `/gift-cards/${giftCard}/transactions`,
-//   namespace
-// );
-const searchTerms = [];
-
-// const searches = [];
+const searchTerms = [
+  {
+    title: 'Adjustment',
+    type: 'object',
+    options: [
+      {
+        title: 'ID',
+        type: 'string',
+        term: 'id'
+      },
+      {
+        title: 'Created At',
+        type: 'date',
+        term: 'createdAt'
+      },
+      {
+        title: 'Debit',
+        type: 'currency',
+        term: 'debit'
+      },
+      {
+        title: 'Credit',
+        type: 'currency',
+        term: 'credit'
+      },
+      {
+        title: 'Available Balance',
+        type: 'currency',
+        term: 'availableBalance'
+      },
+    ]
+  },
+  {
+    title: 'Order',
+    type: 'object',
+    options: [
+      {
+        title: 'Reference Number',
+        type: 'string',
+        term: 'orderReferenceNumber'
+      },
+      {
+        title: 'Created At',
+        type: 'date',
+        term: 'orderCreatedAt'
+      },
+      {
+        title: 'Payment Created At',
+        type: 'date',
+        term: 'orderPaymentCreatedAt'
+      },
+    ]
+  }
+];
 
 const { reducer, actions } = makeLiveSearch(
   'giftCards-transactions',
