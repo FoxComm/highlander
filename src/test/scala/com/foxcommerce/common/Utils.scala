c8e61f0686ce0b163584096a63d278fb30d731e9
Pavel Kiselev <kpashka@gmail.com>
2016-02-01 23:02:48 +0600

    Minor randomizer fix

--- src/test/scala/com/foxcommerce/common/Utils.scala
+++ src/test/scala/com/foxcommerce/common/Utils.scala
@@ -9,7 +9,10 @@ object Utils {
     prefix ++ "_" ++ randomString(10) ++ "@" ++ domain
   }
 
-  def randomString(length: Int) = scala.util.Random.alphanumeric.take(length).mkString
+  def randomString(length: Int) = {
+  	val rnd = new scala.util.Random()
+  	rnd.nextString(length)
+  }
 
   def addressPayloadBody(address: AddressPayload): String =
     """{"name": "%s", "regionId": %d, "address1": "%s", "address2": "%s", "city": "%s", "zip": "%s"}""".
