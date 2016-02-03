0b789a8860a17a5f11ec86bf4cc3c2301d66a5f6
Eugene Sypachev <eugene.sypachev@gmail.com>
2016-02-03 22:09:50 +0400

    store credit suggessions

--- src/modules/customers/store-credits.js
+++ src/modules/customers/store-credits.js
@@ -1,7 +1,66 @@
 
 import makeLiveSearch from '../live-search';
 
-const searchTerms = [];
+const searchTerms = [
+  {
+    title: 'Store Credit',
+    type: 'object',
+    options: [
+      {
+        title: 'ID',
+        type: 'string',
+        term: 'id'
+      },
+      {
+        title: 'Origin Type',
+        type: 'enum',
+        term: 'originType',
+        suggestions: [
+          { display: 'Appeasement', value: 'csrAppeasement' },
+          { display: 'Customer Purchase', value: 'customerPurchase' },
+          { display: 'From Store Credit', value: 'fromStoreCredit' },
+          { display: 'RMA Process', value: 'rmaProcess' },
+        ]
+      },
+      {
+        title: 'Status',
+        type: 'enum',
+        term: 'status',
+        suggestions: [
+          { display: 'Cart', value: 'cart' },
+          { display: 'Active', value: 'active' },
+          { display: 'Fully Redeemed', value: 'fullyRedeemed' },
+          { display: 'Canceled', value: 'canceled' },
+        ]
+      },
+      {
+        title: 'Original Balance',
+        type: 'currency',
+        term: 'originalBalance'
+      },
+      {
+        title: 'Current Balance',
+        type: 'currency',
+        term: 'currentBalance'
+      },
+      {
+        title: 'Available Balance',
+        type: 'currency',
+        term: 'availableBalance'
+      },
+      {
+        title: 'Canceled Amount',
+        type: 'currency',
+        term: 'canceledAmount'
+      },
+      {
+        title: 'Created Date',
+        type: 'date',
+        term: 'createdAt'
+      },
+    ]
+  },
+];
 
 const { reducer, actions } = makeLiveSearch(
   'customers-storeCredits',
