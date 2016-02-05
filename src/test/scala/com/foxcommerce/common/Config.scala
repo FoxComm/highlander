907394588c445156d1ca82e5d51f1bc8f4b9bc63
Pavel Kiselev <kpashka@gmail.com>
2016-02-05 16:13:22 +0600

    Perform requests via Nginx Proxy

--- src/test/scala/com/foxcommerce/common/Config.scala
+++ src/test/scala/com/foxcommerce/common/Config.scala
@@ -5,14 +5,14 @@ import com.typesafe.config.ConfigFactory
 import io.gatling.core.Predef._
 import io.gatling.http.Predef._
 
-final case class Config(environment: String, phoenixUrl: String, elasticUrl: String, indexName: String,
-  userAgent: String, usersCount: Int, greenRiverPause: Int) {
+final case class Config(environment: String, apiUrl: String, userAgent: String, usersCount: Int, 
+  greenRiverPause: Int) {
 
   val defaultAssertion        = global.failedRequests.count.is(0)
   val defaultInjectionProfile = atOnceUsers(usersCount)
 
   val httpConf = http
-      .baseURL(phoenixUrl)
+      .baseURL(apiUrl)
       .acceptHeader("application/json")
       .contentTypeHeader("application/json")
       .userAgentHeader(userAgent)
@@ -20,8 +20,7 @@ final case class Config(environment: String, phoenixUrl: String, elasticUrl: Str
 
   def before(): Unit = {
     Console.println(s"Starting simulation in $environment environment")
-    Console.println(s"Phoenix URL: $phoenixUrl")
-    Console.println(s"Elasticsearch URL: $elasticUrl")
+    Console.println(s"API URL: $apiUrl")
   }
 }
 
@@ -38,9 +37,7 @@ object Config {
 
     Config(
       environment     = env,
-      phoenixUrl      = conf.getString(s"$env.phoenixUrl"),
-      elasticUrl      = conf.getString(s"$env.elasticUrl"),
-      indexName       = conf.getString(s"$env.elasticIndex"),
+      apiUrl          = conf.getString(s"$env.apiUrl"),
       userAgent       = conf.getString(s"$env.userAgent"),
       usersCount      = users,
       greenRiverPause = pause
--- src/test/scala/com/foxcommerce/endpoints/SearchEndpoint.scala
+++ src/test/scala/com/foxcommerce/endpoints/SearchEndpoint.scala
@@ -11,44 +11,44 @@ object SearchEndpoint {
 
   def checkCustomer(conf: Config, customer: CustomerFixture): HttpRequestBuilder = {
     http("Check Customer Presence in Elasticsearch")
-      .get(s"${conf.elasticUrl}/${conf.indexName}" ++ "/customers_search_view/${customerId}")
+      .get("/search/customers_search_view/${customerId}")
       .check(status.is(200))
-      .check(jsonPath("$._source.name").ofType[String].is(customer.name))
-      .check(jsonPath("$._source.email").ofType[String].is("${customerEmail}"))
-      .check(jsonPath("$._source.isBlacklisted").ofType[Boolean].is(customer.isBlacklisted))
-      .check(jsonPath("$._source.isDisabled").ofType[Boolean].is(customer.isDisabled))
-      .check(jsonPath("$._source.shippingAddresses[0].address1").ofType[String].is(customer.address.address1))
-      .check(jsonPath("$._source.shippingAddresses[0].address2").ofType[String].is(customer.address.address2))
-      .check(jsonPath("$._source.shippingAddresses[0].city").ofType[String].is(customer.address.city))
-      .check(jsonPath("$._source.shippingAddresses[0].zip").ofType[String].is(customer.address.zip))
-      .check(jsonPath("$._source.storeCreditCount").ofType[Long].is(customer.storeCreditCount))
-      .check(jsonPath("$._source.storeCreditTotal").ofType[Long].is(customer.storeCreditTotal))
+      .check(jsonPath("$.name").ofType[String].is(customer.name))
+      .check(jsonPath("$.email").ofType[String].is("${customerEmail}"))
+      .check(jsonPath("$.isBlacklisted").ofType[Boolean].is(customer.isBlacklisted))
+      .check(jsonPath("$.isDisabled").ofType[Boolean].is(customer.isDisabled))
+      .check(jsonPath("$.shippingAddresses[0].address1").ofType[String].is(customer.address.address1))
+      .check(jsonPath("$.shippingAddresses[0].address2").ofType[String].is(customer.address.address2))
+      .check(jsonPath("$.shippingAddresses[0].city").ofType[String].is(customer.address.city))
+      .check(jsonPath("$.shippingAddresses[0].zip").ofType[String].is(customer.address.zip))
+      .check(jsonPath("$.storeCreditCount").ofType[Long].is(customer.storeCreditCount))
+      .check(jsonPath("$.storeCreditTotal").ofType[Long].is(customer.storeCreditTotal))
   }
 
   def checkStoreCredit(conf: Config, storeCredit: StoreCreditFixture, state: String): HttpRequestBuilder = {
     http("Check Store Credit Presence in Elasticsearch")
-      .get(s"${conf.elasticUrl}/${conf.indexName}" ++ "/store_credits_search_view/${storeCreditId}")
+      .get("/search/store_credits_search_view/${storeCreditId}")
       .check(status.is(200))
-      .check(jsonPath("$._source.state").ofType[String].is(state))
-      .check(jsonPath("$._source.originalBalance").ofType[Long].is(storeCredit.amount))
+      .check(jsonPath("$.state").ofType[String].is(state))
+      .check(jsonPath("$.originalBalance").ofType[Long].is(storeCredit.amount))
   }
 
   def checkGiftCard(conf: Config, giftCard: GiftCardFixture, state: String): HttpRequestBuilder = {
     http("Check Gift Card Presence in Elasticsearch")
-      .get(s"${conf.elasticUrl}/${conf.indexName}" ++ "/gift_cards_search_view/${giftCardId}")
+      .get("/search/gift_cards_search_view/${giftCardId}")
       .check(status.is(200))
-      .check(jsonPath("$._source.state").ofType[String].is(state))
-      .check(jsonPath("$._source.originalBalance").ofType[Long].is(giftCard.balance))
+      .check(jsonPath("$.state").ofType[String].is(state))
+      .check(jsonPath("$.originalBalance").ofType[Long].is(giftCard.balance))
   }
 
   def checkOrder(conf: Config, order: OrderFixture, state: String): HttpRequestBuilder = {
     http("Check Order Presence in Elasticsearch")
-      .get(s"${conf.elasticUrl}/${conf.indexName}" ++ "/orders_search_view/${orderId}")
+      .get("/search/orders_search_view/${orderId}")
       .check(status.is(200))
-      .check(jsonPath("$._source.state").ofType[String].is(state))
-      .check(jsonPath("$._source.shippingAddresses[0].address1").ofType[String].is(order.shippingAddress.address1))
-      .check(jsonPath("$._source.shippingAddresses[0].address2").ofType[String].is(order.shippingAddress.address2))
-      .check(jsonPath("$._source.shippingAddresses[0].city").ofType[String].is(order.shippingAddress.city))
-      .check(jsonPath("$._source.shippingAddresses[0].zip").ofType[String].is(order.shippingAddress.zip))
+      .check(jsonPath("$.state").ofType[String].is(state))
+      .check(jsonPath("$.shippingAddresses[0].address1").ofType[String].is(order.shippingAddress.address1))
+      .check(jsonPath("$.shippingAddresses[0].address2").ofType[String].is(order.shippingAddress.address2))
+      .check(jsonPath("$.shippingAddresses[0].city").ofType[String].is(order.shippingAddress.city))
+      .check(jsonPath("$.shippingAddresses[0].zip").ofType[String].is(order.shippingAddress.zip))
   }
 }
