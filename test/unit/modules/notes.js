f1de32e050485114e761845f5f98f14e76ab47db
Andrey Rublev <andrey@foxcommerce.com>
2016-02-08 16:41:08 +0600

    fix notes tests

--- test/unit/modules/notes.js
+++ test/unit/modules/notes.js
@@ -12,6 +12,12 @@ describe('Notes module', function() {
     entityType: 'orders'
   };
 
+  const initialState = {
+    notes: {
+      currentEntity: entity
+    }
+  };
+
   const notesPayload = [
     {id: 1, body: 'note 1', createdAt: new Date().toString()},
     {id: 2, body: 'leaving the house turn off the lights', createdAt: new Date().toString()}
@@ -46,7 +52,7 @@ describe('Notes module', function() {
         {type: searchActions.addEntity, payload: notePayload}
       ];
 
-      yield expect(actions.createNote(entity, notePayload), 'to dispatch actions', expectedActions);
+      yield expect(actions.createNote(notePayload), 'to dispatch actions', expectedActions, initialState);
     });
 
     it('editNote', function *() {
@@ -59,7 +65,7 @@ describe('Notes module', function() {
         .patch(notesUri(entity, 1))
         .reply(200, notePayload);
 
-      yield expect(actions.editNote(entity, 1, notePayload), 'to dispatch actions', expectedActions);
+      yield expect(actions.editNote(1, notePayload), 'to dispatch actions', expectedActions, initialState);
     });
 
     it('deleteNote', function *() {
@@ -72,7 +78,7 @@ describe('Notes module', function() {
         .delete(notesUri(entity, 1))
         .reply(204, {});
 
-      yield expect(actions.deleteNote(entity, 1), 'to dispatch actions', expectedActions);
+      yield expect(actions.deleteNote(1), 'to dispatch actions', expectedActions, initialState);
     });
   });
 
