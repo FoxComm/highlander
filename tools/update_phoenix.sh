4d5a266ea1980b0bd5051c0b5ccbf9214c6f4620
Pavel Kiselev <kpashka@gmail.com>
2016-02-09 15:57:51 +0500

    Create update_phoenix.sh

---
+++ tools/update_phoenix.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+MACHINE=${MACHINE:="appliance"}
+PROVIDER=${PROVIDER:="virtualbox"}
+
+ADDRESS="vagrant@192.168.10.111"
+KEY="../prov-shit/.vagrant/machines/$MACHINE/$PROVIDER/private_key"
+cd ../phoenix-scala
+sbt assembly && scp -i $KEY target/scala-2.11/phoenix-assembly-1.0.jar $ADDRESS:./
+
+ssh -i $KEY $ADDRESS 'sudo cp phoenix-assembly-1.0.jar /usr/local/bin/phoenix.jar && sudo systemctl restart phoenix'
