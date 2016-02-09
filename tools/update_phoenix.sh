bbcf31e3e976fc9c72ca78413b43652ecd97c550
Pavel Kiselev <kpashka@gmail.com>
2016-02-09 16:02:25 +0500

    Update update_phoenix.sh

--- tools/update_phoenix.sh
+++ tools/update_phoenix.sh
@@ -7,4 +7,4 @@ KEY="../prov-shit/.vagrant/machines/$MACHINE/$PROVIDER/private_key"
 cd ../phoenix-scala
 sbt assembly && scp -i $KEY target/scala-2.11/phoenix-scala-assembly-1.0.jar $ADDRESS:./
 
-ssh -i $KEY $ADDRESS 'sudo cp phoenix-scala-assembly-1.0.jar /usr/local/bin/phoenix.jar && sudo systemctl restart phoenix'
+ssh -i $KEY $ADDRESS 'sudo cp phoenix-scala-assembly-1.0.jar /phoenix/phoenix.jar && sudo systemctl restart phoenix'
