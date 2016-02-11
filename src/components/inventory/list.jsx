dd72d9123910d3902a1d15fec9cd1e8a6b7adc4e
Eugene Sypachev <eugene.sypachev@gmail.com>
2016-02-11 14:51:50 +0300

    WIP: searchable list

--- src/components/inventory/list.jsx
+++ src/components/inventory/list.jsx
@@ -1,8 +1,13 @@
 
+// libs
 import React, { PropTypes } from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 
+// component
+import { SearchableList } from '../list-page';
+
+// redux
 import { actions } from '../../modules/inventory/list';
 
 const getState = state => ({ list: state.inventory.list });
@@ -12,7 +17,24 @@ const mapDispatchToProps = dispatch => {
 };
 
 const InventoryList = props => {
-  return <div>Inventory</div>;
+
+  const renderRow = () => { return <div>row</div>; };
+
+  const tableColumns = [];
+
+  return (
+    <SearchableList
+      emptyResultMessage="No SKUs found."
+      list={props.list}
+      renderRow={renderRow}
+      tableColumns={tableColumns}
+      searchActions={props.actions} />
+  );
+};
+
+InventoryList.propTypes = {
+  list: PropTypes.object.isRequired,
+  actions: PropTypes.object.isRequired
 };
 
 export default connect(getState, mapDispatchToProps)(InventoryList);
