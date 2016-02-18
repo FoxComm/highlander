b9d0a3b55e8832fe5a822d0412ad16e173ca5119
Maxim Khailo <max@foxcommerce.com>
2016-02-17 16:41:26 -0800

    Fix amount displayed in gift card transactions view

--- src/components/gift-cards/gift-card-transaction-row.jsx
+++ src/components/gift-cards/gift-card-transaction-row.jsx
@@ -6,7 +6,10 @@ import React, { PropTypes } from 'react';
 // components
 import MultiSelectRow from '../table/multi-select-row';
 
-const setCellContents = (giftCard, field) => _.get(giftCard, field, null);
+const setCellContents = (giftCard, field) => {
+  const r = _.get(giftCard, field, null);
+  return field == 'debit' ?  -r : r;
+}
 
 const GiftCardTransactionRow = props => {
   const { giftCard, columns, params } = props;
