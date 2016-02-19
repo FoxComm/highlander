a40664b940c1a1dd85d7bf687fef84fb7960d17a
Sergey Rublev <narma.nsk@gmail.com>
2016-02-19 23:56:45 +0600

    Fix tests after changing authenticator at routes

--- test/integration/AutomaticAuth.scala
+++ test/integration/AutomaticAuth.scala
@@ -1,29 +1,30 @@
-
+import akka.http.scaladsl.server.directives.AuthenticationResult
 import models.StoreAdmin
 import models.customer.Customer
 import org.scalatest.concurrent.{PatienceConfiguration, ScalaFutures}
 import org.scalatest.{Suite, SuiteMixin}
-import akka.http.scaladsl.server.Directives.AsyncAuthenticator
 import util.DbTestSupport
+import utils.Passwords.hashPassword
+import services.Authenticator.AsyncAuthenticator
 import scala.concurrent.Future
 
 trait AutomaticAuth extends SuiteMixin
   with ScalaFutures
   with HttpSupport { this: Suite with PatienceConfiguration with DbTestSupport ⇒
 
-  val authedStoreAdmin = StoreAdmin(id = 1, email = "donkey@donkey.com", password = "donkeyPass",
+  val authedStoreAdmin = StoreAdmin(id = 1, email = "donkey@donkey.com", password = hashPassword("donkeyPass"),
           name = "Mister Donkey")
 
-  val authedCustomer = Customer(id = 1, email = "donkey@donkey.com", password = Some("donkeyPass"),
+  val authedCustomer = Customer(id = 1, email = "donkey@donkey.com", password = Some(hashPassword("donkeyPass")),
           name = Some("Mister Donkey"))
 
 
   override def overrideStoreAdminAuth: AsyncAuthenticator[StoreAdmin] = (UserCredentials) ⇒ {
-    Future.successful(Some(authedStoreAdmin))
+    Future.successful(AuthenticationResult.success(authedStoreAdmin))
   }
 
   override def overrideCustomerAuth: AsyncAuthenticator[Customer] = (UserCredentials) ⇒ {
-    Future.successful(Some(authedCustomer))
+    Future.successful(AuthenticationResult.success(authedCustomer))
   }
 
 }
--- test/integration/RoutesIntegrationTest.scala
+++ test/integration/RoutesIntegrationTest.scala
@@ -1,23 +1,27 @@
 import scala.concurrent.Future
 import akka.http.scaladsl.model.StatusCodes
-import akka.http.scaladsl.server.Directives._
+import akka.http.scaladsl.server.directives.AuthenticationResult
+import akka.http.scaladsl.server.directives.SecurityDirectives.challengeFor
 
 import models.StoreAdmin
 import models.customer.Customer
 import util.IntegrationTestBase
+import utils.Passwords.hashPassword
+
+import services.Authenticator.AsyncAuthenticator
 
 class RoutesAdminOnlyIntegrationTest extends IntegrationTestBase
   with HttpSupport {
 
-  val authedStoreAdmin = StoreAdmin(id = 1, email = "donkey@donkey.com", password = "donkeyPass",
+  val authedStoreAdmin = StoreAdmin(id = 1, email = "donkey@donkey.com", password = hashPassword("donkeyPass"),
     name = "Mister Donkey")
 
   override def overrideStoreAdminAuth: AsyncAuthenticator[StoreAdmin] = (UserCredentials) ⇒ {
-    Future.successful(Some(authedStoreAdmin))
+    Future.successful(AuthenticationResult.success(authedStoreAdmin))
   }
 
   override def overrideCustomerAuth: AsyncAuthenticator[Customer] = (UserCredentials) ⇒ {
-    Future.successful(None)
+    Future.successful(AuthenticationResult.failWithChallenge(challengeFor("test")))
   }
 
   "Requests with StoreAdmin only session (w/o customer)" - {
@@ -31,17 +35,17 @@ class RoutesAdminOnlyIntegrationTest extends IntegrationTestBase
 class RoutesCustomerOnlyIntegrationTest extends IntegrationTestBase
   with HttpSupport {
 
-  val authedCustomer = Customer(id = 1, email = "donkey@donkey.com", password = Some("donkeyPass"),
+  val authedCustomer = Customer(id = 1, email = "donkey@donkey.com", password = Some(hashPassword("donkeyPass")),
     name = Some("Mister Donkey"))
 
   val uriPrefix = "v1/my"
 
   override def overrideCustomerAuth: AsyncAuthenticator[Customer] = (UserCredentials) ⇒ {
-    Future.successful(Some(authedCustomer))
+    Future.successful(AuthenticationResult.success(authedCustomer))
   }
 
   override def overrideStoreAdminAuth: AsyncAuthenticator[StoreAdmin] = (UserCredentials) ⇒ {
-    Future.successful(None)
+    Future.successful(AuthenticationResult.failWithChallenge(challengeFor("test")))
   }
 
   "Requests with Customer only session (w/o StoreAdmin)" - {
