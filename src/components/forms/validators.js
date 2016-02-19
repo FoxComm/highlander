3465a0b48e6d3dd51cc6a6ae6f4c29ed0cb4bb1c
Andrey Rublev <version.ru@gmail.com>
2016-02-19 23:46:58 +0600

    add flow type checking in pre-push hook

--- src/components/forms/validators.js
+++ src/components/forms/validators.js
@@ -1,12 +1,13 @@
+/* @flow */
 
-export function ascii(value, label) {
+export function ascii(value: string, label: string): ?string {
   return /^[\x00-\x7F]+$/.test(value) ? null : `${label} must contain only ASCII characters`;
 }
 
-export function phoneNumber(value, label) {
+export function phoneNumber(value: string, label: string): ?string {
   return /^[\d#\-\(\)\+\*) ]+$/.test(value) ? null : `${label} must not contain letters or other non-valid characters`;
 }
 
-export function email(value) {
+export function email(value: string): boolean {
   return /.+@.+/.test(value);
 }
--- tasks/hooks.js
+++ tasks/hooks.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 const PRE_PUSH = '.git/hooks/pre-push';
 
 const hookScript = `#!/bin/sh
-npm run lint
+npm run lint && npm run flow
 `;
 
 function installHooks() {
@@ -12,7 +12,6 @@ function installHooks() {
     mode: 0o755,
   });
 }
-
 
 module.exports = function(gulp, $, opts) {
   if (opts.autoInstallHooks) {
