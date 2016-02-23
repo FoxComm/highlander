d14bd1a104b4ae4b0133258ba50ce7154fa42095
Pavel Kiselev <kpashka@gmail.com>
2016-02-23 20:16:28 +0600

    Flatten failures in BulkResponses

--- app/responses/GiftCardBulkResponse.scala
+++ app/responses/GiftCardBulkResponse.scala
@@ -8,10 +8,10 @@ object GiftCardBulkResponse {
     code: Option[String] = None,
     success: Boolean = false,
     giftCard: Option[GiftCardResponse.Root] = None,
-    errors: Option[Failures] = None) extends ResponseItem
+    errors: Option[List[String]] = None) extends ResponseItem
 
   def buildItemResult(result: Failures Xor GiftCardResponse.Root, code: Option[String] = None): ItemResult = {
-    result.fold(errors ⇒ ItemResult(code = code, errors = Some(errors)),
+    result.fold(errors ⇒ ItemResult(code = code, errors = Some(errors.flatten)),
       gc ⇒ ItemResult(code = code, success = true, giftCard = Some(gc)))
   }
 }
