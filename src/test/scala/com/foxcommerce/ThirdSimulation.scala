cab2638ead17427b50186c4dbbdbaa864a0f3f90
Pavel Kiselev <kpashka@gmail.com>
2016-02-26 13:02:21 +0600

    Quick fix

--- src/test/scala/com/foxcommerce/ThirdSimulation.scala
+++ src/test/scala/com/foxcommerce/ThirdSimulation.scala
@@ -13,6 +13,10 @@ class ThirdSimulation extends Simulation {
     conf.before()
   }
 
+  val baseAddress = AddressFixture(name = Utils.randomString(), regionId = 1, address1 = "Donkey street, 38",
+    address2 = "Donkey street, 39", city = "Donkeyville", zip = "55555")
+  val address = baseAddress.copy(city = "Seattle", zip = "66666")    
+
   // Prepare scenario
   val thirdScenario = scenario("Third Scenario")
     .feed(jsonFile("data/customers.json").random)
