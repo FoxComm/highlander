6f08231112ea7f25de2ab9f6eaa48fdf71a564ab
tonypizzicato <tony@foxcommerce.com>
2016-02-29 22:38:24 +0300

    added search component

--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -1,7 +1,18 @@
-import React from 'react';
+import React, { PropTypes } from 'react';
 import { Link } from '../link';
 
+import { transitionTo } from '../../route-helpers';
+
 export default class Home extends React.Component {
+
+  static contextTypes = {
+    store: PropTypes.object.isRequired
+  };
+
+  componentDidMount() {
+    transitionTo(this.context.store.dispatch, 'orders');
+  }
+
   render() {
     return (
       <div>
--- src/components/inventory/item-transactions.jsx
+++ src/components/inventory/item-transactions.jsx
@@ -1,9 +1,42 @@
+/** Libs */
 import React, { PropTypes } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
 
+/** Components */
+import { SearchableList } from '../list-page';
+import {} from './item-transactions-list-row.jsx';
+
+/** Redux */
+import { actions } from '../../modules/inventory/transactions';
+
+
+/** InventoryItemTransactions Component */
 const InventoryItemTransactions = props => {
+
+  const renderRow = (row, index, columns, params) => {
+    const key = `inventory-transaction-${row.id}`;
+    return <InventoryListRow sku={row} columns={columns} params={params}/>;
+  };
+
   return (
-    <h3>Transactions</h3>
+    <SearchableList
+      emptyResultMessage="No transaction units found."
+      list={props.list}
+      renderRow={() => <div>item</div>}
+      tableColumns={tableColumns}
+      searchActions={props.actions}/>
   );
 };
 
-export default InventoryItemTransactions;
+function mapState(state) {
+  return {
+    list: state.inventory.transactions.list
+  };
+}
+
+function mapDispatch(dispatch) {
+  return {actions: bindActionCreators(actions, dispatch)};
+}
+
+export default connect(mapState, mapDispatch)(InventoryItemTransactions);
