ee3e724a72a3f329ee22afa319f39d8faad2b57c
Eugene Sypachev <eugene.sypachev@gmail.com>
2016-02-29 21:51:02 +0300

    redirect to orders from demo

--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -1,7 +1,18 @@
-import React from 'react';
+import React, { PropTypes } from 'react';
 import { Link } from '../link';
 
+import { transitionTo } from '../../route-helpers';
+
 export default class Home extends React.Component {
+
+  static contextTypes = {
+    history: PropTypes.object.isRequired
+  };
+
+  componentDidMount() {
+    transitionTo(this.context.history, 'orders');
+  }
+
   render() {
     return (
       <div>
