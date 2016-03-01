bce29e2ec932034976673f72ba5d1d80f665eadb
Jeff Mataya <jeff@foxcommerce.com>
2016-02-29 20:07:14 -0800

    Display general properties on the PDP

--- src/components/products/details.jsx
+++ src/components/products/details.jsx
@@ -28,27 +28,54 @@ type DetailsProps = {
   params: DetailsParams,
 };
 
-export class ProductDetails extends Component<void, DetailsProps, void> {
+type ProductState = {
+  illuminatedProduct: ProductAttributes,
+  generalAttributes: ProductAttributes,
+  pricingAttributes: ProductAttributes,
+  seoAttributes: ProductAttributes,
+};
+
+const miscAttributeKeys = ['images'];
+const pricingAttributeKeys = ['price', 'retailPrice', 'salePrice'];
+const seoAttributeKeys = ['url', 'metaTitle', 'metaDescription'];
+
+function getReactStateFromProps(props: DetailsProps): ProductState {
+  const illuminatedProduct = props.details.product
+    ? getProductAttributes(1, props.details.product)
+    : {};
+
+  const generalAttributes = _.omit(illuminatedProduct, [
+    ...miscAttributeKeys,
+    ...pricingAttributeKeys,
+    ...seoAttributeKeys
+  ]);
+  const pricingAttributes = _.pick(illuminatedProduct, pricingAttributeKeys);
+  const seoAttributes = _.pick(illuminatedProduct, seoAttributeKeys);
+
+  return {
+    illuminatedProduct,
+    generalAttributes,
+    pricingAttributes,
+    seoAttributes,
+  };
+}
+
+export class ProductDetails extends Component<void, DetailsProps, ProductState> {
   static propTypes = {
     details: PropTypes.shape({
       product: PropTypes.object,
     }),
   };
 
-  get productAttributes(): ProductAttributes {
-    if (this.props.details.product) {
-      return getProductAttributes(1, this.props.details.product);
-    } else {
-      return {};
-    }
+  state: ProductState;
+
+  constructor(props: DetailsProps) {
+    super(props);
+    this.state = getReactStateFromProps(props);
   }
 
-  get renderedAttributes(): Element {
-    const attributes = _.map(this.productAttributes, attribute => {
-      return this.renderAttribute(attribute);
-    });
-
-    return <div className="fc-product-details__attributes">{attributes}</div>;
+  componentWillReceiveProps(nextProps: DetailsProps) {
+    this.setState(getReactStateFromProps(nextProps));
   }
 
   renderAttribute(attribute: ProductAttribute): Element {
@@ -64,12 +91,16 @@ export class ProductDetails extends Component<void, DetailsProps, void> {
     );
   }
 
+  get generalContentBox(): Element {
+    const attrs = _.map(this.state.generalAttributes, a => this.renderAttribute(a));
+    return <ContentBox title="General" viewContent={attrs} />;
+  }
+
   render() {
     return (
       <div className="fc-grid">
         <div className="fc-col-md-2-3">
-          <ContentBox title="General" />
-          {this.renderedAttributes}
+          {this.generalContentBox}
         </div>
       </div>
     );
@@ -78,7 +109,7 @@ export class ProductDetails extends Component<void, DetailsProps, void> {
 
 type DetailsState = { details: ProductDetailsState };
 function mapStateToProps(state: Object): DetailsState {
-  return { pdetails: state.products.details };
+  return { details: state.products.details };
 }
 
 export default connect(mapStateToProps, DetailsActions)(ProductDetails);
