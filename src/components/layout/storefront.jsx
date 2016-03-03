a22a50b14bf6e51e53a8291e4f9dfe5ce2936f5c
Eugene Sypachev <eugene.sypachev@gmail.com>
2016-03-03 18:55:42 +0300

    icon sizes in header

--- src/components/layout/storefront.jsx
+++ src/components/layout/storefront.jsx
@@ -13,15 +13,19 @@ const StoreFront = props => {
     <div styleName="storefront">
       <div styleName="head">
         <div styleName="search">
-          <Icon name="fc-magnifying-glass" />
+          <Icon name="fc-magnifying-glass" styleName="head-icon"/>
         </div>
         <div styleName="hamburger">
-          <Icon name="fc-close" />
+          <Icon name="fc-close" styleName="head-icon"/> {/* TODO: change me to hamburger */}
         </div>
         <Icon styleName="logo" name="fc-some_brand_logo" />
         <div styleName="tools">
-          <Link to="/login" styleName="login">LOG IN</Link>
-          <Icon name="fc-cart" />
+          <div styleName="login">
+            <Link to="/login" styleName="login-link">LOG IN</Link>
+          </div>
+          <div styleName="cart">
+            <Icon name="fc-cart" styleName="head-icon"/>
+          </div>
         </div>
       </div>
       <div styleName="categories">
