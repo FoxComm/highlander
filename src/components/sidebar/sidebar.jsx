7126abfaab21b9c27105e725e04acb7859953959
Eugene Sypachev <eugene.sypachev@gmail.com>
2016-03-04 19:15:00 +0300

    sidebar component

---
+++ src/components/sidebar/sidebar.jsx
@@ -0,0 +1,14 @@
+/* @flow */
+
+import React from 'react';
+import type { HTMLElement } from '../../types';
+
+// type SidebarProps = {
+
+// };
+
+const Sidebar = () : HTMLElement => {
+  return <div></div>;
+};
+
+export default Sidebar;
