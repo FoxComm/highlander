140c1ceaf5262130992a57cf633dda4e41eae7cd
Eugene Sypachev <eugene.sypachev@gmail.com>
2016-03-09 00:35:39 +0300

    product catalog on category page

---
+++ src/components/products/list-item.jsx
@@ -0,0 +1,32 @@
+/* @flow */
+
+import React from 'react';
+import type { HTMLElement } from 'types';
+import cssModules from 'react-css-modules';
+import styles from './list-item.css';
+
+// type Product {
+//   id: number;
+//   attributes: Object;
+// };
+
+const ListItem = (): HTMLElement => {
+  const price = '$50';
+  const name = 'lorem ipsum';
+  const imageUrl = 'http://lorempixel.com/75/75/fashion/';
+  return (
+    <div styleName="list-item">
+      <div styleName="preview">
+        <img src={imageUrl} />
+      </div>
+      <div styleName="name">
+        {name}
+      </div>
+      <div styleName="price">
+        {price}
+      </div>
+    </div>
+  );
+};
+
+export default cssModules(ListItem, styles);
