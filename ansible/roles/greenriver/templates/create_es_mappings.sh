22e9d0d742576db74945aceaecbc070162a074b4
Pavel Kiselev <kpashka@gmail.com>
2016-03-09 18:04:11 +0500

    Update create_es_mappings.sh
    Try with default host URL

--- ansible/roles/greenriver/templates/create_es_mappings.sh
+++ ansible/roles/greenriver/templates/create_es_mappings.sh
@@ -2,4 +2,4 @@
 
 cd /usr/share
 
-/usr/bin/java -Denv={{greenriver_env}} -D{{greenriver_env}}.kafka.broker={{kafka_server}} -D{{greenriver_env}}.elastic.host=elasticsearch://{{search_server}} -D{{greenriver_env}}.activity.phoenix.url=http://{{phoenix_server}}/v1 -D{{greenriver_env}}.avro.schemaRegistryUrl=http://{{schema_server}} -D{{greenriver_env}}.elastic.setup=true -jar {{greenriver_jar}}
+/usr/bin/java -Denv={{greenriver_env}} -D{{greenriver_env}}.kafka.broker={{kafka_server}} -D{{greenriver_env}}.activity.phoenix.url=http://{{phoenix_server}}/v1 -D{{greenriver_env}}.avro.schemaRegistryUrl=http://{{schema_server}} -D{{greenriver_env}}.elastic.setup=true -jar {{greenriver_jar}}
