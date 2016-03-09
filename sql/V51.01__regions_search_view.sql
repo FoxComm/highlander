425c2133d6a16bdec0398da900545ac372584990
Pavel Kiselev <kpashka@gmail.com>
2016-03-09 16:13:06 +0600

    Add countries search view

--- sql/V51.01__regions_search_view.sql
+++ sql/V51.01__regions_search_view.sql
@@ -3,20 +3,20 @@ select
 	r.id,
 	r.name,
 	r.abbreviation,
-    -- Country
-    to_json((
-    	c.id,
-    	c.name,
-    	c.alpha2,
-    	c.alpha3,
-    	c.code,
-    	c.continent,
-    	c.currency,
-    	c.languages,
-    	c.uses_postal_code,
-    	c.is_billable,
-    	c.is_shippable
-    )::countries) as country
+	-- Country
+	to_json((
+		c.id,
+		c.name,
+		c.alpha2,
+		c.alpha3,
+		c.code,
+		c.continent,
+		c.currency,
+		c.languages,
+		c.uses_postal_code,
+		c.is_billable,
+		c.is_shippable
+	)::countries) as country
 from regions as r
 inner join countries as c on (r.country_id = c.id);
 
---
+++ sql/V51.02__countries_search_view.sql
@@ -0,0 +1,16 @@
+create materialized view countries_search_view as
+select
+	c.id,
+	c.name,
+	c.alpha2,
+	c.alpha3,
+	c.code,
+	c.continent,
+	c.currency,
+	c.languages,
+	c.uses_postal_code,
+	c.is_billable,
+	c.is_shippable
+from countries as c;
+
+create unique index countries_search_view_idx on countries_search_view (id);
