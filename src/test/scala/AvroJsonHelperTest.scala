5f85541b48c56cb73fae7b595c96d99fc11e0133
Pavel Kiselev <kpashka@gmail.com>
2016-03-09 22:31:52 +0600

    Minor test fix

--- src/test/scala/AvroJsonHelperTest.scala
+++ src/test/scala/AvroJsonHelperTest.scala
@@ -9,7 +9,7 @@ class AvroJsonHelperTest extends FlatSpec with Matchers {
         | {
         |   "is_blacklisted": false,
         |   "orders": "[{\"reference_number\": \"BR10005\",\"grand_total\": 5040}]",
-        |   "images": "[\"BR10005\", \"BR10006\"}]"
+        |   "images": "[\"BR10005\", \"BR10006\"]"
         | }""".stripMargin
 
     val result = transformJson(inputJson, List("orders", "images"))
@@ -22,5 +22,7 @@ class AvroJsonHelperTest extends FlatSpec with Matchers {
 
     result contains "grand_total"       shouldBe false
     result contains "grandTotal"        shouldBe true
+    
+    result contains """["BR10005","BR10006"]""" shouldBe true
   } 
 }
