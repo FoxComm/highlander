32bf6a16cee6a17ee1f9476f459e47809711d406
Pavel Kiselev <kpashka@gmail.com>
2016-03-12 00:02:57 +0600

    Cleanup phase 2

--- sql/V36.10__create_rma_assignments.sql
+++
@@ -1,8 +0,0 @@
-create table rma_assignments (
-    id serial primary key,
-    created_at timestamp without time zone default (now() at time zone 'utc'),
-    rma_id integer not null references rmas(id) on update restrict on delete restrict,
-    assignee_id integer not null references store_admins(id) on update restrict on delete restrict
-);
-
-create index rma_assignments_rma_id_idx on rma_assignments (rma_id);
