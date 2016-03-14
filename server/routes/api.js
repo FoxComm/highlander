5e4dcc09eee8b91f124f3cf553218170d232d393
Jeff Mataya <jeff@foxcommerce.com>
2016-03-14 16:47:13 -0700

    Make the Phoenix configurable

--- server/routes/api.js
+++ server/routes/api.js
@@ -2,9 +2,13 @@ import proxy from 'koa-proxy';
 
 const matchUriRegexp = new RegExp(`^/api/v1/`);
 
-export function makeProxy() {
+export function makeApiProxy() {
+  const host = process.env.PHOENIX_URL
+    ? process.env.PHOENIX_URL
+    : 'http://localhost:9090';
+
   return proxy({
-    host: 'http://192.168.10.111:9090',
+    host,
     match: matchUriRegexp,
     map: path => path.replace(/^\/api\//, '/'),
   });
