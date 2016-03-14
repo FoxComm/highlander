f9557e12f486fcd08593f50cfeada137ca3138f5
Anna Zubenko <anna@foxcommerce.com>
2016-03-14 20:49:50 +0700

    Inventory transaction search view transformer

---
+++ src/main/scala/consumer/elastic/mappings/InventoryTransactionSearchView.scala
@@ -0,0 +1,22 @@
+package consumer.elastic.mappings
+
+import com.sksamuel.elastic4s.ElasticDsl.{mapping ⇒ esMapping, _}
+import com.sksamuel.elastic4s.mappings.FieldType._
+import consumer.aliases._
+import consumer.elastic.AvroTransformer
+import consumer.elastic.MappingHelpers._
+
+final case class InventoryTransactionSearchView()(implicit ec: EC) extends AvroTransformer {
+  def mapping() = esMapping("inventory_transactions_search_view").fields(
+    field("id", IntegerType),
+    field("createdAt", DateType) format dateFormat,
+    field("warehouse", StringType) analyzer "autocomplete",
+    field("event", StringType),
+    field("previousQuantity", IntegerType),
+    field("newQuantity", IntegerType),
+    field("change", IntegerType),
+    field("newAfs", IntegerType),
+    field("skuType", StringType),
+    field("state", StringType)
+  )
+}
