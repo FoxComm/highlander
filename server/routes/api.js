7ba1679b86665945f5e6dfb426d1f7d302018a59
Jeff Mataya <jeff@foxcommerce.com>
2016-03-14 21:34:16 -0700

    Wire the storefront PDP up to an API
    Okay, yeah, it's the admin API. I'll fix it, bro.

--- server/routes/api.js
+++ server/routes/api.js
@@ -2,6 +2,10 @@ import proxy from 'koa-proxy';
 
 const matchUriRegexp = new RegExp(`^/api/v1/`);
 
+function toBase64(str) {
+  return new Buffer(str).toString('base64');
+}
+
 export function makeApiProxy() {
   const host = process.env.PHOENIX_URL
     ? process.env.PHOENIX_URL
@@ -12,4 +16,20 @@ export function makeApiProxy() {
     match: matchUriRegexp,
     map: path => path.replace(/^\/api\//, '/'),
   });
+}
+
+export function makeApiHandler() {
+  // TODO: Remove once auth fully lands.
+  const user = 'admin@admin.com';
+  const password = 'password';
+  const basicAuthHeader = toBase64(`${user}:${password}`);
+
+  return function *apiHandler(next) {
+    if (this.request.url.match(matchUriRegexp)) {
+      this.request.headers.Accept = 'application/json';
+      this.request.headers.Authorization = `Basic ${basicAuthHeader}`;
+    }
+
+    yield next;
+  };
 }
