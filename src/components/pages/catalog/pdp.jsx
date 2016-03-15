dc5bc78a28dbf4707d91c7aae121131240447f48
Jeff Mataya <jeff@foxcommerce.com>
2016-03-14 21:53:07 -0700

    Add a helper function to isomorphic-fetch

--- src/components/pages/catalog/pdp.jsx
+++ src/components/pages/catalog/pdp.jsx
@@ -1,7 +1,7 @@
 /* @flow */
 
 import _ from 'lodash';
-import React, { Component, Element } from 'react';
+import React, { Component } from 'react';
 import cssModules from 'react-css-modules';
 import styles from './pdp.css';
 import { connect } from 'react-redux';
@@ -40,52 +40,52 @@ class Pdp extends Component<void, Props, void> {
     const { product } = this.props;
     if (!product) {
       return <div></div>;
-    } else {
-      const title = _.get(product, ['product', 'attributes', 'title', 'v'], '');
-      const description = _.get(product, ['product', 'attributes', 'description', 'v'], '');
-      const price = _.get(product, ['skus', 0, 'attributes', 'price', 'v', 'value'], 0);
-      const currency = _.get(product, ['skus', 0, 'attributes', 'price', 'v', 'currency'], 'USD');
-      const imageUrls = _.get(product, ['product', 'attributes', 'images', 'v'], []);
-      const images = _.map(imageUrls, (url) => <img src={url} styleName="preview-image" />);
-      return (
-        <div styleName="container">
-          <div styleName="links">
-            <div styleName="desktop-links">
-              <Link to="/" styleName="breadcrumb">SHOP</Link> / LOREM IPSUM
+    }
+
+    const title = _.get(product, ['product', 'attributes', 'title', 'v'], '');
+    const description = _.get(product, ['product', 'attributes', 'description', 'v'], '');
+    const price = _.get(product, ['skus', 0, 'attributes', 'price', 'v', 'value'], 0);
+    const currency = _.get(product, ['skus', 0, 'attributes', 'price', 'v', 'currency'], 'USD');
+    const imageUrls = _.get(product, ['product', 'attributes', 'images', 'v'], []);
+    const images = _.map(imageUrls, (url) => <img src={url} styleName="preview-image" />);
+    return (
+      <div styleName="container">
+        <div styleName="links">
+          <div styleName="desktop-links">
+            <Link to="/" styleName="breadcrumb">SHOP</Link> / LOREM IPSUM
+          </div>
+          <div styleName="mobile-links">
+            <Link to="/" styleName="breadcrumb">&lt; BACK </Link>
+          </div>
+          <div>
+            NEXT &gt;
+          </div>
+        </div>
+        <div styleName="details">
+          <div styleName="images">
+            {images}
+          </div>
+          <div styleName="info">
+            <h1 styleName="name">{title}</h1>
+            <div styleName="price">
+              <Currency value={price} currency={currency} />
             </div>
-            <div styleName="mobile-links">
-              <Link to="/" styleName="breadcrumb">&lt; BACK </Link>
+            <div styleName="description">
+              {description}
             </div>
             <div>
-              NEXT &gt;
+              <label>QUANTITY</label>
+              <div styleName="counter">
+                <Counter />
+              </div>
             </div>
-          </div>
-          <div styleName="details">
-            <div styleName="images">
-              {images}
-            </div>
-            <div styleName="info">
-              <h1 styleName="name">{title}</h1>
-              <div styleName="price">
-                <Currency value={price} currency={currency} />
-              </div>
-              <div styleName="description">
-                {description}
-              </div>
-              <div>
-                <label>QUANTITY</label>
-                <div styleName="counter">
-                  <Counter />
-                </div>
-              </div>
-              <div styleName="add-to-cart">
-                <Button>ADD TO CART</Button>
-              </div>
+            <div styleName="add-to-cart">
+              <Button>ADD TO CART</Button>
             </div>
           </div>
         </div>
-      );
-    }
+      </div>
+    );
   }
 }
 
