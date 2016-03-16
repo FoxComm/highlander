6affa3c2ea84072e39006ac427c57f76fc0861d1
Pavel Kiselev <kpashka@gmail.com>
2016-03-17 03:46:37 +0600

    Fix generalized connectors for assignments

--- src/main/scala/consumer/activity/AdminConnector.scala
+++ src/main/scala/consumer/activity/AdminConnector.scala
@@ -12,8 +12,7 @@ final case class AdminConnector()(implicit ec: EC) extends ActivityConnector {
   def process(offset: Long, activity: Activity): Future[Seq[Connection]] = Future {
     // TODO: Reduce un-necessary extractions by matching activity name
     val adminIds = byContextUserType(activity) ++: byAdminData(activity, "admin") ++:
-      byAssigneesData(activity) ++: byAdminData(activity, "assignee") ++:
-      byWatchersData(activity) ++: byAdminData(activity, "watcher")
+      byAssigneesData(activity) ++: byAdminData(activity, "assignee")
 
     adminIds.distinct.map(createConnection(_, activity.id))
   }
@@ -26,20 +25,15 @@ final case class AdminConnector()(implicit ec: EC) extends ActivityConnector {
       activityId = activityId)
   }
 
-  private def byContextUserType(activity: Activity): Seq[String] = {
-    activity.context.userType match {
-      case "admin" ⇒ Seq(activity.context.userId.toString)
-      case _       ⇒ Seq.empty
-    }
+  private def byContextUserType(activity: Activity): Seq[String] = activity.context.userType match {
+    case "admin" ⇒ Seq(activity.context.userId.toString)
+    case _       ⇒ Seq.empty
   }
 
-  private def byAdminData(activity: Activity, fieldName: String): Seq[String] = {
-    activity.data \ fieldName \ "id" match {
-      case JInt(value) ⇒ Seq(value.toString)
-      case _           ⇒ Seq.empty
-    }
+  private def byAdminData(activity: Activity, fieldName: String): Seq[String] = activity.data \ fieldName \ "id" match {
+    case JInt(value) ⇒ Seq(value.toString)
+    case _           ⇒ Seq.empty
   }
 
   private def byAssigneesData(activity: Activity): Seq[String] = extractStringSeq(activity.data, "assignees")
-  private def byWatchersData(activity: Activity): Seq[String] = extractStringSeq(activity.data, "watchers")
 }
--- src/main/scala/consumer/activity/CustomerConnector.scala
+++ src/main/scala/consumer/activity/CustomerConnector.scala
@@ -5,14 +5,15 @@ import scala.concurrent.Future
 import consumer.aliases._
 import consumer.utils.JsonTransformers.extractStringSeq
 
-import org.json4s.JsonAST.{JInt, JNothing}
+import org.json4s.JsonAST.{JInt, JString, JNothing}
 
 final case class CustomerConnector()(implicit ec: EC) extends ActivityConnector {
   val dimension = "customer"
 
   def process(offset: Long, activity: Activity): Future[Seq[Connection]] = Future {
     val customerIds = byContextUserType(activity) ++: byCustomerData(activity) ++:
-      byCustomerUpdatedActivity(activity) ++: byBulkData(activity)
+      byCustomerUpdatedActivity(activity) ++: byAssignmentBulkData(activity) ++: byAssignmentSingleData(activity)
+
     customerIds.distinct.map(createConnection(_, activity.id))
   }
 
@@ -24,26 +25,36 @@ final case class CustomerConnector()(implicit ec: EC) extends ActivityConnector 
       activityId = activityId)
   }
 
-  private def byBulkData(activity: Activity): Seq[String] = extractStringSeq(activity.data, "customerIds")
-
-  private def byContextUserType(activity: Activity): Seq[String] = {
-    activity.context.userType match {
-      case "customer" ⇒ Seq(activity.context.userId.toString)
-      case _          ⇒ Seq.empty
-    }
+  private def byContextUserType(activity: Activity): Seq[String] = activity.context.userType match {
+    case "customer" ⇒ Seq(activity.context.userId.toString)
+    case _          ⇒ Seq.empty
   }
 
-  private def byCustomerData(activity: Activity): Seq[String] = {
-    activity.data \ "customer" \ "id" match {
-      case JInt(customerId) ⇒ Seq(customerId.toString)
-      case _                ⇒ Seq.empty
-    }
+  private def byCustomerData(activity: Activity): Seq[String] = activity.data \ "customer" \ "id" match {
+    case JInt(customerId) ⇒ Seq(customerId.toString)
+    case _                ⇒ Seq.empty
   }
 
   private def byCustomerUpdatedActivity(activity: Activity): Seq[String] = {
     (activity.activityType, activity.data \ "oldInfo" \ "id") match {
       case ("customer_updated", JInt(customerId)) ⇒ Seq(customerId.toString)
       case _                                      ⇒ Seq.empty
+    }
+  }
+
+  private def byAssignmentSingleData(activity: Activity): Seq[String] = {
+    (activity.activityType, activity.data \ "entity" \ "id") match {
+      case ("assigned", JInt(customerId))   ⇒ Seq(customerId.toString)
+      case ("unassigned", JInt(customerId)) ⇒ Seq(customerId.toString)
+      case _                                ⇒ Seq.empty
+    }
+  }
+
+  private def byAssignmentBulkData(activity: Activity): Seq[String] = {
+    (activity.activityType, activity.data \ "assignmentType") match {
+      case ("bulk_assigned", JString("customer"))   ⇒ extractStringSeq(activity.data, "entityIds")
+      case ("bulk_unassigned", JString("customer")) ⇒ extractStringSeq(activity.data, "entityIds")
+      case _                                        ⇒ Seq.empty
     }
   }
 }
