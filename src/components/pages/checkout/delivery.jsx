2e8991a134030727286e75d7d52f0c645a3de537
Andrey Rublev <version.ru@gmail.com>
2016-03-16 16:36:33 +0600

    fetch shipping methods from redux

--- src/components/pages/checkout/delivery.jsx
+++ src/components/pages/checkout/delivery.jsx
@@ -1,5 +1,4 @@
 
-import _ from 'lodash';
 import React, { Component } from 'react';
 import styles from './checkout.css';
 import { autobind } from 'core-decorators';
@@ -8,39 +7,52 @@ import { connect } from 'react-redux';
 import Button from 'ui/buttons';
 import Checkbox from 'ui/checkbox';
 import EditableBlock from 'ui/editable-block';
-import { FormField, Form } from 'ui/forms';
+import { Form } from 'ui/forms';
 import Currency from 'ui/currency';
 
 import * as checkoutActions from 'modules/checkout';
 
-const ViewDelivery = props => {
+const ViewDelivery = () => {
   return (
     <div>view delivery</div>
   );
 };
 
+/* ;;`*/
+@connect(state => state.checkout, checkoutActions)
+/* ;;`*/
 class EditDelivery extends Component {
+
+  componentWillMount() {
+    this.props.fetchShippingMethods();
+  }
 
   @autobind
   handleSubmit() {
 
   }
 
+  get shippingMethods() {
+    const { shippingMethods } = this.props;
+
+    return shippingMethods.map(shippingMethod => {
+      const cost = shippingMethod.price == 0
+        ? <div styleName="delivery-cost">FREE</div>
+        : <Currency styleName="delivery-cost" value={shippingMethod.price}/>;
+
+      return (
+        <div key={shippingMethod.id} styleName="shipping-method">
+          <Checkbox name="delivery" id={shippingMethod.id}>{shippingMethod.name}</Checkbox>
+          {cost}
+        </div>
+      );
+    });
+  }
+
   render() {
     return (
       <Form onSubmit={this.handleSubmit}>
-        <div styleName="shipping-method">
-          <Checkbox name="delivery" id="d1">7-10 DAY STANDARD</Checkbox>
-          <div styleName="delivery-cost">FREE</div>
-        </div>
-        <div styleName="shipping-method">
-          <Checkbox name="delivery" id="d2">2 DAY AIR</Checkbox>
-          <Currency styleName="delivery-cost" value="1000"/>
-        </div>
-        <div styleName="shipping-method">
-          <Checkbox name="delivery" id="d3">Overnight</Checkbox>
-          <Currency styleName="delivery-cost" value="4000"/>
-        </div>
+        {this.shippingMethods}
         <Button styleName="checkout-submit" type="submit">CONTINUE</Button>
       </Form>
     );
