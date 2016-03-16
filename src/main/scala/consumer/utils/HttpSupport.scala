1d77b5869e568d4a3cb3587d0867c8c5b4078408
Pavel Kiselev <kpashka@gmail.com>
2016-03-16 18:02:42 +0600

    Still debugging

--- src/main/scala/consumer/utils/HttpSupport.scala
+++ src/main/scala/consumer/utils/HttpSupport.scala
@@ -1,6 +1,7 @@
 package consumer.utils
 
 import scala.concurrent.Future
+import scala.concurrent.duration._
 
 import akka.http.scaladsl.Http
 import akka.http.scaladsl.model.headers.RawHeader
@@ -23,7 +24,19 @@ case class Phoenix(conn: PhoenixConnectionInfo)(implicit ec: EC, ac: AS, mat: AM
 
   val authHeaderName = "Authorization"
 
-  def auth(): Future[HttpResponse] = {
+  def get(suffix: String): Future[HttpResponse] = for {
+    authResponse ← authenticate()
+    jwtToken     = extractJwtToken(authResponse)
+    response     ← getInner(suffix, jwtToken)
+  } yield response
+
+  def post(suffix: String, body: String): Future[HttpResponse] = for {
+    authResponse ← authenticate()
+    jwtToken     = extractJwtToken(authResponse)
+    response     ← postInner(suffix, body, jwtToken)
+  } yield response
+
+  private def authenticate(): Future[HttpResponse] = {
     val request = HttpRequest(
       method = HttpMethods.POST,
       uri    = authUri,
@@ -34,18 +47,6 @@ case class Phoenix(conn: PhoenixConnectionInfo)(implicit ec: EC, ac: AS, mat: AM
 
     Http().singleRequest(request, cp)
   }
-
-  def get(suffix: String): Future[HttpResponse] = for {
-    authResponse ← auth()
-    jwtToken     = extractJwtToken(authResponse)
-    response     ← getInner(suffix, jwtToken)
-  } yield response
-
-  def post(suffix: String, body: String): Future[HttpResponse] = for {
-    authResponse ← auth()
-    jwtToken     = extractJwtToken(authResponse)
-    response     ← postInner(suffix, body, jwtToken)
-  } yield response
 
   private def getInner(suffix: String, token: String): Future[HttpResponse] = {
     val request = HttpRequest(HttpMethods.GET, fullUri(suffix)).addHeader(RawHeader(authHeaderName, token))
@@ -68,8 +69,12 @@ case class Phoenix(conn: PhoenixConnectionInfo)(implicit ec: EC, ac: AS, mat: AM
 
   private def fullUri(suffix: String) = s"${conn.uri}/$suffix"
 
-  private def extractJwtToken(response: HttpResponse): String =
+  private def extractJwtToken(response: HttpResponse): String = {
+    val body = response.entity.toStrict(5 seconds).map(_.data.decodeString("UTF-8"))
+    Console.out.println(s"Auth response body: $body")
     response.headers.find(_.name() == jwtHeaderName).map(_.value()).getOrElse("")
+  }
+
 }
 
 /**
