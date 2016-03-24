8ae1a9e0d478740f7c950fe40f3d4c0a7df43410
Alex Kreskiyan <a.kreskiyan@gmail.com>
2016-03-24 15:05:57 +0300

    Added criteria empty block

--- src/components/customers-groups/dynamic/group.jsx
+++ src/components/customers-groups/dynamic/group.jsx
@@ -5,6 +5,7 @@ import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import { autobind } from 'core-decorators';
 import moment from 'moment';
+import classNames from 'classnames';
 
 //data
 import operators from '../../../paragons/customer-groups/operators';
@@ -16,6 +17,7 @@ import { transitionTo } from '../../../route-helpers';
 import { prefix } from '../../../lib/text-utils';
 
 //components
+import ContentBox from '../../content-box/content-box';
 import { PrimaryButton } from '../../common/buttons';
 
 
@@ -59,7 +61,7 @@ export default class DynamicGroup extends Component {
     const {list, group} = this.props;
 
     return (
-      <header className="fc-col-md-1-1">
+      <header className={classNames(prefixed('__header'), 'fc-col-md-1-1')}>
         <div className={prefixed('__title')}>
           <h1 className="fc-title">
             {group.name}
@@ -69,15 +71,24 @@ export default class DynamicGroup extends Component {
         </div>
         <div className={prefixed('__about')}>
           <div>
-            <span className={prefixed('__about__key')}>Type: </span>
+            <span className={prefixed('__about__key')}>Type:</span>
             <span className={prefixed('__about__value')}>{_.capitalize(group.type)}</span>
           </div>
           <div>
-            <span className={prefixed('__about__key')}>Created: </span>
+            <span className={prefixed('__about__key')}>Created:</span>
             <span className={prefixed('__about__value')}>{moment(group.createdAt).format('DD/MM/YYYY HH:mm')}</span>
           </div>
         </div>
       </header>
+    );
+  }
+
+  get criteria() {
+    return (
+      <ContentBox title="Criteria"
+                  className={prefixed('__criteria')}>
+        criterions here
+      </ContentBox>
     );
   }
 
@@ -94,6 +105,7 @@ export default class DynamicGroup extends Component {
         <div className="fc-grid">
           <article className="fc-col-md-1-1">
             {this.header}
+            {this.criteria}
           </article>
         </div>
       </div>
