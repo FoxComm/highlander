259ae7e7c0f0ba8c9afa2b90b18dde83d0bfa31b
Eugene Sypachev <eugene.sypachev@gmail.com>
2016-03-29 00:16:07 +0400

    basic request

--- src/components/pages/checkout/gift-card.jsx
+++ src/components/pages/checkout/gift-card.jsx
@@ -2,6 +2,8 @@
 import React, { Component } from 'react';
 import { autobind } from 'core-decorators';
 import styles from './checkout.css';
+import { saveGiftCard } from 'modules/checkout';
+import { connect } from 'react-redux';
 
 import EditableBlock from 'ui/editable-block';
 import { TextInput } from 'ui/inputs';
@@ -20,26 +22,36 @@ class EditGiftCard extends Component {
     });
   }
 
+  @autobind
+  onSave() {
+    this.props.saveGiftCard(this.state.code).catch(() => {
+      this.setState({code: ''});
+    });
+  }
+
   render() {
     return (
       <div styleName="gift-card-content">
         <TextInput styleName="gift-card-code" placeholder="CODE" value={this.state.code} onChange={this.changeCode} />
-        <Button styleName="gift-card-submit">REDEEM</Button>
+        <Button styleName="gift-card-submit" onClick={this.onSave}>
+          REDEEM
+        </Button>
       </div>
     );
   }
 }
 
-const GiftCard = () => {
+const GiftCard = props => {
+  const saveFun = props.saveGiftCard;
   return (
     <EditableBlock
       styleName="checkout-block"
       title="GIFT CARD"
       isEditing
       collapsed={false}
-      content={<EditGiftCard />}
+      content={<EditGiftCard saveGiftCard={saveFun} />}
     />
   );
 };
 
-export default GiftCard;
+export default connect(null, { saveGiftCard })(GiftCard);
