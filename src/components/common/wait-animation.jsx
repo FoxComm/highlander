9b4e68c4e4ea0381447f533251fc7bf06b413405
tonypizzicato <tony@foxcommerce.com>
2016-03-30 16:39:34 +0300

    replace ripple.svg with css animations - fix smil deprecation warning

--- src/components/common/wait-animation.jsx
+++ src/components/common/wait-animation.jsx
@@ -1,12 +1,27 @@
-
-import React from 'react';
+import classNames from 'classnames';
+import React, { PropTypes } from 'react';
 
 const WaitAnimation = props => {
+  const size = props.size || 'l';
+  const cls = classNames('fc-wait-animation', `fc-wait-animation_size_${size}`, props.className);
+
   return (
-    <div className="fc-wait-animation">
-      <img src="/images/ripple.svg" />
+    <div className={cls}>
+      <div className="circle1"></div>
+      <div className="circle2"></div>
+      <div className="circle3"></div>
+      <div className="circle4"></div>
     </div>
   );
+};
+
+WaitAnimation.propTypes = {
+  size: PropTypes.oneOf(['s', 'm', 'l']),
+  className: PropTypes.string,
+};
+
+WaitAnimation.defaultProps = {
+  size: 'l'
 };
 
 export default WaitAnimation;
