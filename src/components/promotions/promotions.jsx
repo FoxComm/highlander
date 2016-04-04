f3eab9f8539b3e254be4063237d4690364dc68d9
Eugene Sypachev <eugene.sypachev@gmail.com>
2016-04-04 23:54:21 +0400

    display empty table

--- src/components/promotions/promotions.jsx
+++ src/components/promotions/promotions.jsx
@@ -1,12 +1,79 @@
 
+// libs
 import React, { Component } from 'react';
+import { autobind } from 'core-decorators';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
 
+// components
+import BulkActions from '../bulk-actions/bulk-actions';
+import BulkMessages from '../bulk-actions/bulk-messages';
+import { SelectableSearchList } from '../list-page';
+
+// redux
+import { actions } from '../../modules/promotions/list';
+import { actions as bulkActions } from '../../modules/promotions/bulk';
+
+// styles
 import styles from './promotions.css';
 
+
+const mapStateToProps = (state) => {
+  return {
+    list: state.promotions.list,
+  };
+};
+
+const mapDispatchToProps = dispatch => {
+  return {
+    actions: bindActionCreators(actions, dispatch),
+    bulkActions: bindActionCreators(bulkActions, dispatch),
+  };
+};
+
+const tableColumns = [];
+
+@connect(mapStateToProps, mapDispatchToProps)
 export default class Promotions extends Component {
+
+  renderRow() {
+    return null;
+  }
+
+  bulkActions() {
+    return [];
+  }
+
+  renderDetail() {
+    return null;
+  }
+
   render() {
+    const {list, actions} = this.props;
+
+    const entity = 'promotion';
+    const module = `${entity}s`;
+
     return (
-      <div styleName="promotions">Promotions list!</div>
+      <div styleName="promotions">
+        <BulkMessages
+          storePath={`${module}.bulk`}
+          module={module}
+          entity={entity}
+          renderDetail={this.renderDetail} />
+        <BulkActions
+          module={module}
+          entity={entity}
+          actions={this.bulkActions()}>
+          <SelectableSearchList
+            emptyMessage="No promotions found."
+            list={list}
+            renderRow={this.renderRow}
+            tableColumns={tableColumns}
+            searchActions={actions}
+          />
+        </BulkActions>
+      </div>
     );
   }
 }
--- src/modules/promotions/index.js
+++ src/modules/promotions/index.js
@@ -1,9 +1,11 @@
 
 import { combineReducers } from 'redux';
 import list from './list';
+import bulk from './bulk';
 
 const promotionsReducer = combineReducers({
   list,
+  bulk,
 });
 
 export default promotionsReducer;
