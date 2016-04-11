c0955d96113e35c03102e9915a8391bba51f3a8e
Jeff Mataya <jeff@foxcommerce.com>
2016-04-11 06:00:52 -0700

    Update catalog materialized view

--- sql/V52.02__products_catalog_view.sql
+++ sql/V52.02__products_catalog_view.sql
@@ -4,19 +4,16 @@ select
     context.name as context,
     f.attributes->>(s.attributes->'title'->>'ref') as title,
     f.attributes->(s.attributes->'images'->>'ref') as images,
-    f.attributes->>(s.attributes->'description'->>'ref') as description,
-    link.skus as skus
+    f.attributes->>(s.attributes->'description'->>'ref') as description
 from
 	products as p,
 	object_forms as f,
 	object_shadows as s,
-	object_contexts as context,
-	product_sku_links_view as link
+	object_contexts as context
 where
 	p.context_id = context.id and
 	f.id = p.form_id and
 	s.id = p.shadow_id and
-	link.product_id = p.id and
     (f.attributes->>(s.attributes->'activeFrom'->>'ref'))::timestamp < CURRENT_TIMESTAMP and
     (((f.attributes->>(s.attributes->'activeTo'->>'ref')) = '') IS NOT FALSE or
     ((f.attributes->>(s.attributes->'activeTo'->>'ref'))::timestamp >= CURRENT_TIMESTAMP));
