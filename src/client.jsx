ea688b390b0a98dc7f2f803837413d87c3bf14cf
Alex Kreskiyan <a.kreskiyan@gmail.com>
2016-04-11 16:25:41 +0300

    Added some basic i18n

--- src/client.jsx
+++ src/client.jsx
@@ -1,10 +1,10 @@
-
 import React from 'react';
 import { Router, browserHistory } from 'react-router';
 import { Provider } from 'react-redux';
 import { render } from 'react-dom';
 import makeStore from './store';
 import routes from './routes';
+import I18nProvider from 'lib/i18n/provider';
 
 const DEBUG = true;
 
@@ -15,11 +15,15 @@ export function renderApp() {
     window.store = store;
   }
 
+  const {language, translation} = window.__i18n;
+
   render((
-    <Provider store={store} key="provider">
-      <Router history={browserHistory}>
-        {routes}
-      </Router>
-    </Provider>
+    <I18nProvider locale={language} translation={translation}>
+      <Provider store={store} key="provider">
+        <Router history={browserHistory}>
+          {routes}
+        </Router>
+      </Provider>
+    </I18nProvider>
   ), document.getElementById('app'));
 }
