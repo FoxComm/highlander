8acceeb36a10bf9b8fe7fb60349aaa52224b1b76
Maxim Khailo <max@foxcommerce.com>
2016-04-12 08:53:01 -0700

    Fix store credit search view.
    Was doing join against empty table.

--- sql/V43.06__store_credits_search_view.sql
+++ sql/V43.06__store_credits_search_view.sql
@@ -17,14 +17,12 @@ select distinct on (sc.id)
     to_char(sc.updated_at, 'YYYY-MM-DD"T"HH24:MI:SS.MS"Z"') as updated_at,
     -- Issued by either
     scav.store_admin,
-    scfgcv.gift_card,
-    sc_customs.metadata
+    scfgcv.gift_card
 from store_credits as sc
 inner join store_credit_subtypes_view as scsv on (scsv.id = sc.id)
 inner join store_credit_cancellation_reasons_view as sccrv on (sccrv.id = sc.id)
 inner join store_credit_admins_view as scav on (scav.id = sc.id)
 inner join store_credit_from_gift_cards_view as scfgcv on (scfgcv.id = sc.id)
-inner join store_credit_customs as sc_customs on (sc_customs.id = sc.origin_id)
 order by sc.id;
 
 create unique index store_credits_search_view_idx on store_credits_search_view (id);
