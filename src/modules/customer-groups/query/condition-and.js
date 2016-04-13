3146c9ce0b26dbfbe6265d8301e60fe9da680d88
Alex Kreskiyan <a.kreskiyan@gmail.com>
2016-04-13 15:34:45 +0300

    Simplified conditional clauses for single clause case

--- src/modules/customer-groups/query/condition-and.js
+++ src/modules/customer-groups/query/condition-and.js
@@ -9,6 +9,10 @@ export default class ConditionAnd extends Condition {
   }
 
   toRequest() {
+    if (this._conditions.length === 1) {
+      return this._conditions[0].toRequest();
+    }
+
     const result = [];
     for (const condition of this._conditions) {
       result.push({bool: condition.toRequest()});
