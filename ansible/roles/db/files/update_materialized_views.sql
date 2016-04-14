c41ca71ef8e11b585c77f61d5818ce31f596abd6
Pavel Kiselev <kpashka@gmail.com>
2016-04-14 23:16:05 +0600

    Update update_materialized_views.sql

--- ansible/roles/db/files/update_materialized_views.sql
+++ ansible/roles/db/files/update_materialized_views.sql
@@ -81,3 +81,4 @@ refresh materialized view concurrently customer_items_view;
 refresh materialized view concurrently promotion_discount_links_view;
 refresh materialized view concurrently promotions_search_view;
 refresh materialized view concurrently coupons_search_view;
+refresh materialized view concurrently coupon_codes_search_view;
