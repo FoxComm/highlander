782e87d61927afea2cac5335cb6e36b657ffc14b
Pavel Kiselev <kpashka@gmail.com>
2016-04-14 23:08:07 +0600

    Fixes

--- sql/V60.08__coupon_codes_search_view.sql
+++ sql/V60.08__coupon_codes_search_view.sql
@@ -4,18 +4,14 @@ select
     c.code,
     cp.id as coupon_id,
     cp.promotion_id,
-    context.name as context,
     0 as total_used, --this needs to be computed
-    f.created_at as created_at
+    c.created_at as created_at
 from 
     coupon_codes as c,
 	object_contexts as context,
-	coupons as cp, 
-	object_forms as f, 
-	object_shadows as s
+	coupons as cp
 where 
-	cp.context_id = context.id and
-	f.id = cp.form_id and 
-	s.id = cp.shadow_id;
+    cp.form_id = c.coupon_form_id and
+	cp.context_id = context.id;
 
 create unique index coupon_codes_search_view_idx on coupon_codes_search_view (id);
