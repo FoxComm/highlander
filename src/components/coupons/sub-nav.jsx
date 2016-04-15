f8aaa8ed4b74fcecf0945d7a2bef9d2d2b4035dd
Eugene Sypachev <eugene.sypachev@gmail.com>
2016-04-15 20:28:52 +0400

    hide tabs when new

--- src/components/coupons/sub-nav.jsx
+++ src/components/coupons/sub-nav.jsx
@@ -10,12 +10,15 @@ export default class SubNav extends Component {
 
   render() {
     const params = this.props.params;
+
+    const notNew = params.couponId != 'new';
+
     return (
       <LocalNav>
         <IndexLink to="coupon-details" params={params}>Details</IndexLink>
-        <Link to="coupon-codes" params={params}>Coupons</Link>
-        <Link to="coupon-notes" params={params}>Notes</Link>
-        <Link to="coupon-activity-trail" params={params}>Activity Trail</Link>
+        { notNew && <Link to="coupon-codes" params={params}>Coupons</Link> }
+        { notNew && <Link to="coupon-notes" params={params}>Notes</Link> }
+        { notNew && <Link to="coupon-activity-trail" params={params}>Activity Trail</Link> }
       </LocalNav>
     );
   }
