c8542886f751b38e1ceddf8a782d9f294994f1d8
Anna Zubenko <anna@foxcommerce.com>
2016-04-17 00:08:04 +0700

    Build fat jar + gatling config

--- gatling/src/main/scala/seeds/GatlingApp.scala
+++ gatling/src/main/scala/seeds/GatlingApp.scala
@@ -1,10 +1,7 @@
 package seeds
 
-import scala.collection.mutable
-
 import com.typesafe.config.Config
 import io.gatling.app.Gatling
-import io.gatling.core.ConfigKeys
 import io.gatling.core.Predef._
 import io.gatling.http.Predef._
 import io.gatling.jdbc.Predef._
@@ -24,12 +21,7 @@ object GatlingApp extends App {
 
   def dbFeeder(sql: String) = jdbcFeeder(dbUrl, dbUser, dbPassword, sql)
 
-  // Gatling config
-  val gatlingConfig = mutable.Map(
-    ConfigKeys.core.directory.Binaries → "./gatling/target/scala-2.11/gatling-classes",
-    ConfigKeys.core.Mute → "true"
-  )
-  println(Gatling.fromMap(gatlingConfig))
+  Gatling.main(Array())
 }
 
 object Conf {
