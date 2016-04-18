19a13b85a257ab947ce7bf4286550c9d2fe96c77
Maxim Khailo <max@foxcommerce.com>
2016-04-18 01:23:44 -0700

    Use image from sku instead of fake one.

--- src/components/orders/sku-result.jsx
+++ src/components/orders/sku-result.jsx
@@ -4,10 +4,10 @@ import Currency from '../common/currency';
 export default class SkuResult extends React.Component {
   render() {
     let model = this.props.model;
-    const imagePath = 'http://lorempixel.com/75/75/fashion/'; //TODO use model.imagePath when it is available
+    let imagePath = model.image || "https://s3-us-west-2.amazonaws.com/fc-firebird-public/images/product/no_image.jpg";
     return (
       <div className="fc-grid">
-        <div className="fc-col-md-2-12"><img src={imagePath} /></div>
+      <div className="fc-col-md-2-12"><img className='fc-image-column' src={imagePath} /></div>
         <div className="fc-col-md-4-12">{model.title}</div>
         <div className="fc-col-md-3-12"><strong>SKU</strong><br />{model.code}</div>
         <div className="fc-col-md-3-12"><strong>Price</strong><br /><Currency value={model.price}/></div>
