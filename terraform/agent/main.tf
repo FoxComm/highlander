1c7cef2d661e41778fc3c4cf24be55f202091e59
Maxim Khailo <max@foxcommerce.com>
2016-04-19 23:59:55 -0700

    Undo group manager for now.

--- terraform/agent/main.tf
+++ terraform/agent/main.tf
@@ -6,17 +6,17 @@ variable "queue" {}
 variable "prefix" {} 
 variable "ssh_user" {} 
 variable "ssh_private_key" {} 
-variable "group_size" {} 
 
-resource "google_compute_instance_template" "build_agent" { 
-    name = "${var.prefix}"
+resource "google_compute_instance" "agent" { 
+    name = "${var.prefix}-${var.queue}"
     machine_type = "n1-highmem-4"
     tags = ["no-ip", "${var.prefix}"]
+    zone = "us-central1-a"
 
     disk {
-        source_image = "${var.demo_image}"
-        disk_type = "pd-ssd"
-        disk_size_gb = "100"
+        image = "${var.demo_image}"
+        type = "pd-ssd"
+        size = "100"
     }   
 
     network_interface {
@@ -40,14 +40,4 @@ resource "google_compute_instance_template" "build_agent" {
           "/tmp/provision.sh ${var.queue}"
         ]
     }
-}
-
-resource "google_compute_instance_group_manager" "build_group_manager" {
-    description = "Build agent instance group"
-    name = "${var.prefix}-${var.queue}-group"
-    instance_template = "${google_compute_instance_template.build_agent.self_link}"
-    update_strategy= "NONE"
-    base_instance_name = "${var.prefix}-${var.queue}"
-    zone = "us-central1-a"
-    target_size = "${var.group_size}"
 }
