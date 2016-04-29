4213a7ea3a67eaff990aa197d26a82da73e9d696
Alex Kreskiyan <a.kreskiyan@gmail.com>
2016-04-29 17:54:36 +0300

    Fixed customers / account password layout

--- src/components/customers/account-password.jsx
+++ src/components/customers/account-password.jsx
@@ -5,18 +5,12 @@ export default class CustomerAccountPassword extends React.Component {
 
   static propTypes = {
     customer: PropTypes.object
-  }
+  };
 
   render() {
     return (
       <ContentBox title="Account Password" className="fc-customer-account-password">
-        <div className="fc-grid">
-          <div className="fc-col-md-1-2">
-          </div>
-          <div className="fc-col-md-1-2">
-            <button className="fc-btn fc-right">Reset Password</button>
-          </div>
-        </div>
+        <button className="fc-btn">Reset Password</button>
       </ContentBox>
     );
   }
