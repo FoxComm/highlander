a28f9bd508ed9f4621a0b0052da453d41722075e
Jeff Mataya <jeff@foxcommerce.com>
2016-05-02 19:43:39 -0700

    Add the image model

--- app/failures/ImageFailures.scala
+++ app/failures/ImageFailures.scala
@@ -7,19 +7,9 @@ object ImageFailures {
       NotFoundFailure404(s"Image with id=$imageId with context=$contextId cannot be found")
   }
 
-  object ImageFormNotFound {
-    def apply(formId: Int) =
-      NotFoundFailure404(s"Image form with id=$formId cannot be found")
-  }
-
-  object ImageShadowNotFound {
-    def apply(shadowId: Int) =
-      NotFoundFailure404(s"Image shadow with id=$shadowId cannot be found")
-  }
-
   object AlbumNotFoundForContext {
     def apply(albumId: Int, contextId: Int) =
       NotFoundFailure404(s"Album with id=$albumId with context=$contextId cannot be found")
   }
-
+  
 }
