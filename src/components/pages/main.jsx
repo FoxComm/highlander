6ab553f36236e312c335e86e1c380ed346e17764
Alex Kreskiyan <a.kreskiyan@gmail.com>
2016-05-06 13:07:53 +0300

    Updated to separate rendering of homepage into methods

--- src/components/pages/main.jsx
+++ src/components/pages/main.jsx
@@ -9,25 +9,55 @@ import styles from './main.css';
 
 class Main extends Component {
 
-  render(): HTMLElement {
+  get topBanner(): HTMLElement {
     return (
-      <div>
-        <div styleName="top-banner">
-          <div styleName="top-banner-wrap">
-            <div styleName="top-banner-info">
-              <div styleName="top-banner-header">Summer 2016</div>
-              <div styleName="top-banner-description">New collection is here</div>
-              <div styleName="top-banner-links">
-                <Link to="/all?shop=men" styleName="top-banner-shop-link">
-                  Shop Men
-                </Link>
-                <Link to="/all?shop=women" styleName="top-banner-shop-link">
-                  Shop Women
-                </Link>
-              </div>
+      <div styleName="top-banner">
+        <div styleName="top-banner-wrap">
+          <div styleName="top-banner-info">
+            <div styleName="top-banner-header">Summer 2016</div>
+            <div styleName="top-banner-description">New collection is here</div>
+            <div styleName="top-banner-links">
+              <Link to="/all?shop=men" styleName="top-banner-shop-link">
+                Shop Men
+              </Link>
+              <Link to="/all?shop=women" styleName="top-banner-shop-link">
+                Shop Women
+              </Link>
             </div>
           </div>
         </div>
+      </div>
+    );
+  }
+
+  get categories(): HTMLElement {
+    return (
+      <div styleName="categories">
+      </div>
+    );
+  }
+
+  get tryAtHomeBanner(): HTMLElement {
+    return (
+      <div styleName="try-at-home-banner">
+      </div>
+    );
+  }
+
+  get visitAStoreBanner(): HTMLElement {
+    return (
+      <div styleName="visit-a-store-banner">
+      </div>
+    );
+  }
+
+  render(): HTMLElement {
+    return (
+      <div>
+        {this.topBanner}
+        {this.categories}
+        {this.tryAtHomeBanner}
+        {this.visitAStoreBanner}
       </div>
     );
   }
