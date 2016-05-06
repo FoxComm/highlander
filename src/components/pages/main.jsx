5c895636edc5dbc3d86ee168c20d41b1edc63954
Alex Kreskiyan <a.kreskiyan@gmail.com>
2016-05-06 13:53:09 +0300

    Finished categories banners for now (no links)

--- src/components/pages/main.jsx
+++ src/components/pages/main.jsx
@@ -33,6 +33,24 @@ class Main extends Component {
   get categories(): HTMLElement {
     return (
       <div styleName="categories">
+        <div styleName="categories-wrap">
+          <div styleName="category-link">
+            <div styleName="category-name">
+              EYEGLASSES
+            </div>
+            <div styleName="category-image">
+              <img src="images/banners/eyeglasses.jpg" />
+            </div>
+          </div>
+          <div styleName="category-link">
+            <div styleName="category-name">
+              SUNGLASSES
+            </div>
+            <div styleName="category-image">
+              <img src="images/banners/sunglasses.jpg" />
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
