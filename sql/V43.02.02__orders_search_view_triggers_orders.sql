4185cbd0ae3366e599e568396c1e0f1c2a0922ef
Sergey Rublev <sergey@foxcommerce.com>
2016-05-11 15:53:29 +0600

    Restructure code

--- sql/V43.02.02__orders_search_view_triggers_orders.sql
+++ sql/V43.02.02__orders_search_view_triggers_orders.sql
@@ -1,40 +1,3 @@
-create table orders_search_view
-(
-    id bigint not null unique,
-    reference_number reference_number not null unique,
-    state generic_string not null,
-    created_at text,
-    placed_at text,
-    currency currency,
-    sub_total integer not null default 0,
-    shipping_total integer not null default 0,
-    adjustments_total integer not null default 0,
-    taxes_total integer not null default 0,
-    grand_total integer not null default 0,
-    customer jsonb not null,
-    line_item_count bigint,
-    line_items jsonb,
-    payments jsonb,
-    credit_card_count bigint,
-    credit_card_total bigint,
-    gift_card_count bigint,
-    gift_card_total bigint,
-    store_credit_count bigint,
-    store_credit_total bigint,
-    shipment_count bigint,
-    shipments jsonb,
-    shipping_addresses_count bigint,
-    shipping_addresses jsonb,
-    billing_addresses_count bigint,
-    billing_addresses jsonb,
-    assignment_count bigint,
-    assignees jsonb,
-    rma_count bigint,
-    rmas jsonb
-);
-
-
--- update on orders changes
 create or replace function update_orders_view_from_orders_fn() returns trigger as $$
     begin
         insert into orders_search_view select distinct on (new.id)
@@ -82,10 +45,3 @@ create trigger update_orders_view_from_orders
     after update or insert on orders
     for each row
     execute procedure update_orders_view_from_orders_fn();
-
--- update on customers changes
-create or replace function update_orders_view_from_customers_fn() returns trigger as $$
-  begin
-    return null;
-  end;
-$$ language plpgsql;
