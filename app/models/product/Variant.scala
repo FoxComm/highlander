9c4c0480e1323f50dcc2d5078745b041f5baaacd
Jeff Mataya <jeff@foxcommerce.com>
2016-05-11 14:40:29 -0700

    Create the base models for variants
    I decided to move Variants out of product and make them their own model
    because the future UI mocks call for variants (and their values) to work
    as stand-alone entities that can be searched on and modified.

---
+++ app/models/product/Variant.scala
@@ -0,0 +1,51 @@
+package models.product
+
+import java.time.Instant
+
+import models.objects._
+import shapeless._
+import slick.driver.PostgresDriver.api._
+import slick.lifted.Tag
+import utils.db._
+import utils.{JsonFormatters, Validation}
+
+object Variant {
+  val kind = "variant"
+}
+
+/**
+ * A Variant represents the attributes that are used to collect SKUs into a
+ * product. One Variant will contain an array of many values, which define the
+ * individual attributes that can be used to select a SKU.
+ */
+case class Variant(id: Int = 0, variantType: String, contextId: Int, shadowId: Int, formId: Int,
+  commitId: Int, updatedAt: Instant = Instant.now, createdAt: Instant = Instant.now)
+  extends FoxModel[Variant]
+  with Validation[Variant]
+
+class Variants(tag: Tag) extends ObjectHeads[Variant](tag, "variants") {
+  def variantType = column[String]("variant_type")
+
+  def * = (id, variantType, contextId, shadowId, formId, commitId, updatedAt,
+    createdAt) <> ((Variant.apply _).tupled, Variant.unapply)
+}
+
+object Variants extends FoxTableQuery[Variant, Variants](new Variants(_))
+  with ReturningId[Variant, Variants] {
+
+  val returningLens: Lens[Variant, Int] = lens[Variant].id
+
+  implicit val formats = JsonFormatters.phoenixFormats
+
+  def filterByContext(contextId: Int): QuerySeq = 
+    filter(_.contextId === contextId)
+
+  def filterByFormId(formId: Int): QuerySeq =
+    filter(_.formId === formId)
+
+  def filterByContextAndFormIds(contextId: Int, formIds: Seq[Int]): QuerySeq =
+    filterByContext(contextId).filter(_.id.inSet(formIds))
+
+  def filterByContextAndFormId(contextId: Int, formId: Int): QuerySeq =
+    filterByContext(contextId).filter(_.formId === formId)
+}
---
+++ sql/V20.14__create_variant_values.sql
@@ -0,0 +1,12 @@
+create table variant_values(
+  id serial primary key,
+  context_id integer not null references object_contexts(id) on update restrict on delete restrict,
+  shadow_id integer not null references object_shadows(id) on update restrict on delete restrict,
+  form_id integer not null references object_forms(id) on update restrict on delete restrict,
+  commit_id integer references object_commits(id) on update restrict on delete restrict,
+  updated_at timestamp without time zone default (now() at time zone 'utc'),
+  created_at timestamp without time zone default (now() at time zone 'utc')
+);
+
+create index variant_values_object_context_idx on variant_values (context_id);
+create index variant_values_variant_value_form_idx on variant_values (form_id);
