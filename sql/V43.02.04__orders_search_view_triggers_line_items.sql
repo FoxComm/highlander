8204a84ced8726d87017652f6b60bbb3a5001c19
Sergey Rublev <sergey@foxcommerce.com>
2016-05-17 21:58:09 +0600

    Fix orders_search_view line_items trigger [skip ci]

--- sql/V43.02.04__orders_search_view_triggers_line_items.sql
+++ sql/V43.02.04__orders_search_view_triggers_line_items.sql
@@ -31,28 +31,31 @@ begin
   end case;
 
   update orders_search_view set
-    (line_item_count, line_items) = (select
-      count(sku.id) as count,
-      case when count(sku) = 0
-      then
-        '[]'
-      else
-        json_agg((
-                oli.reference_number,
-                oli.state,
-                sku.code,
-                sku_form.attributes->>(sku_shadow.attributes->'title'->>'ref'),
-                sku_form.attributes->(sku_shadow.attributes->'salePrice'->>'ref')->>'value')::export_line_items)
-      end as items
-      from orders as o
-      left join order_line_items as oli on (o.id = oli.order_id)
-      left join order_line_item_origins as oli_origins on (oli.origin_id = oli_origins.id)
-      left join order_line_item_skus as oli_skus on (oli_origins.id = oli_skus.id)
-      left join skus as sku on (oli_skus.sku_id = sku.id)
-      left join object_forms as sku_form on (sku.form_id = sku_form.id)
-      left join object_shadows as sku_shadow on (oli_skus.sku_shadow_id = sku_shadow.id)
-      where o.id = ANY(order_ids)
-      group by o.id);
+    line_item_count = subquery.count,
+    line_items = subquery.items FROM (select
+          o.id,
+          count(sku.id) as count,
+          case when count(sku) = 0
+          then
+            '[]'
+          else
+            json_agg((
+                    oli.reference_number,
+                    oli.state,
+                    sku.code,
+                    sku_form.attributes->>(sku_shadow.attributes->'title'->>'ref'),
+                    sku_form.attributes->(sku_shadow.attributes->'salePrice'->>'ref')->>'value')::export_line_items)
+          end as items
+          from orders as o
+          left join order_line_items as oli on (o.id = oli.order_id)
+          left join order_line_item_origins as oli_origins on (oli.origin_id = oli_origins.id)
+          left join order_line_item_skus as oli_skus on (oli_origins.id = oli_skus.id)
+          left join skus as sku on (oli_skus.sku_id = sku.id)
+          left join object_forms as sku_form on (sku.form_id = sku_form.id)
+          left join object_shadows as sku_shadow on (oli_skus.sku_shadow_id = sku_shadow.id)
+          where o.id = ANY(order_ids)
+          group by o.id) AS subquery
+      WHERE orders_search_view.id = subquery.id;
 
     return null;
 end;
