4507248c6164b6750fe3aec5336ff8ff5d85836b
Sergey Rublev <sergey@foxcommerce.com>
2016-05-23 15:19:59 +0600

    Enable jmx debugging for ES, twice memory for es on stage

--- ansible/roles/base/elasticsearch/templates/run_elasticsearch.sh
+++ ansible/roles/base/elasticsearch/templates/run_elasticsearch.sh
@@ -3,5 +3,13 @@
 ELASTIC_ROOT=$1
 export ES_HEAP_SIZE={{es_heap_size}}
 
+ES_JAVA_OPTS="${ES_JAVA_OPTS} -Dcom.sun.management.jmxremote"
+ES_JAVA_OPTS="${ES_JAVA_OPTS} -Dcom.sun.management.jmxremote.port={{es_jmx_port}}"
+ES_JAVA_OPTS="${ES_JAVA_OPTS} -Dcom.sun.management.jmxremote.local.only=false"
+ES_JAVA_OPTS="${ES_JAVA_OPTS} -Dcom.sun.management.jmxremote.authenticate=false"
+ES_JAVA_OPTS="${ES_JAVA_OPTS} -Dcom.sun.management.jmxremote.ssl=false"
+export ES_JAVA_OPTS
+
+
 cd $ELASTIC_ROOT
 bin/elasticsearch
