53f16ed298d18dfebb07fb8f4edc42a912ad9707
tonypizzicato <tony@foxcommerce.com>
2016-05-25 18:12:45 +0300

    hack for skus notes

--- src/elastic/notes.js
+++ src/elastic/notes.js
@@ -2,9 +2,20 @@ import * as dsl from './dsl';
 import { addNativeFilters } from './common';
 
 export default function processQuery({ entityType, entityId }, query) {
-  return addNativeFilters(query, [
-    dsl.termFilter('referenceId', entityId),
-    dsl.termFilter('referenceType', entityType),
-    dsl.existsFilter('deletedAt', 'missing'),
-  ]);
+  switch (entityType) {
+    case 'sku':
+      return addNativeFilters(query, [
+        dsl.termFilter('referenceType', entityType),
+        dsl.nestedTermFilter('skuItem.sku', entityId),
+        dsl.existsFilter('deletedAt', 'missing'),
+      ]);
+    default:
+      return addNativeFilters(query, [
+        dsl.termFilter('referenceType', entityType),
+        dsl.termFilter('referenceId', entityId),
+        dsl.existsFilter('deletedAt', 'missing'),
+      ]);
+  }
+
+  return query;
 }
