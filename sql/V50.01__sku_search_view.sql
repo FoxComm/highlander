7ba006a525b49a9d5eddb5e9f0485976f3ab95a9
Sergey Rublev <sergey@foxcommerce.com>
2016-05-26 00:00:53 +0600

    Migrate sku_search_view to triggers approach [skip ci]

--- sql/V50.01__sku_search_view.sql
+++ sql/V50.01__sku_search_view.sql
@@ -1,4 +1,110 @@
-create materialized view sku_search_view as
+create table sku_search_view
+(
+    id integer not null,
+    code generic_string not null,
+    context generic_string not null,
+    context_id integer not null,
+    title text,
+    image text,
+    price text,
+    currency text
+);
+create unique index sku_search_view_idx on sku_search_view (id, context);
+
+create or replace function update_skus_view_from_skus_fn() returns trigger as $$
+begin
+
+  update sku_search_view set
+    code = NEW.code
+    where id = NEW.id;
+
+  return null;
+end;
+$$ language plpgsql;
+
+
+create trigger update_skus_view_from_skus_fn
+    after update or insert on skus
+    for each row
+    execute procedure update_skus_view_from_skus_fn();
+
+create or replace function update_skus_view_from_object_context_fn() returns trigger as $$
+begin
+
+  update sku_search_view set
+    context = subquery.name,
+    context_id = subquery.id
+    from (select
+        o.id,
+        o.name,
+        skus.id as sku_id
+      from object_contexts as o
+      inner join skus on (skus.form_id = o.id)
+      where skus.id = NEW.id) as subquery
+      where subquery.sku_id = sku_search_view.id;
+
+    return null;
+end;
+$$ language plpgsql;
+
+
+create trigger update_skus_view_from_object_forms
+    after update or insert on object_contexts
+    for each row
+    execute procedure update_skus_view_from_object_context_fn();
+
+
+create or replace function update_skus_view_from_object_attrs_fn() returns trigger as $$
+declare sku_ids int[];
+begin
+  case TG_TABLE_NAME
+    when 'object_shadows' then
+      select array_agg(skus.id) into strict sku_ids
+        from skus
+        inner join object_shadows as sku_shadow on (skus.shadow_id = sku_shadow.id)
+        where sku_shadow.id = NEW.id;
+    when 'object_forms' then
+     select array_agg(skus.id) into strict sku_ids
+       from skus
+       inner join object_forms as sku_form on (skus.form_id = sku_form.id)
+       where sku_form.id = NEW.id;
+  end case;
+
+  update sku_search_view set
+    title = subquery.title,
+    image = subquery.image,
+    price = subquery.price,
+    currency = subquery.currency
+    from (select
+        sku.id,
+        sku_form.attributes->>(sku_shadow.attributes->'title'->>'ref') as title,
+        sku_form.attributes->(sku_shadow.attributes->'images'->>'ref')->>0 as image,
+        sku_form.attributes->(sku_shadow.attributes->'salePrice'->>'ref')->>'value' as price,
+        sku_form.attributes->(sku_shadow.attributes->'salePrice'->>'ref')->>'currency' as currency
+      from skus as sku
+      inner join object_forms as sku_form on (sku_form.id = sku.form_id)
+      inner join object_shadows as sku_shadow on (sku_shadow.id = sku.shadow_id)
+      where sku.id = ANY(sku_ids)) as subquery
+      where subquery.id = sku_search_view.id;
+
+    return null;
+end;
+$$ language plpgsql;
+
+
+create trigger update_skus_view_from_object_shadows
+    after update or insert on object_shadows
+    for each row
+    execute procedure update_skus_view_from_object_attrs_fn();
+
+create trigger update_skus_view_from_object_forms
+  after update or insert on object_forms
+  for each row
+  execute procedure update_skus_view_from_object_attrs_fn();
+
+
+--- old sku view TODO: remove it
+create materialized view sku_search_view_test as
 select
     sku.id, 
     sku.code as code,
@@ -8,7 +114,14 @@ select
     sku_form.attributes->(sku_shadow.attributes->'images'->>'ref')->>0 as image,
     sku_form.attributes->(sku_shadow.attributes->'salePrice'->>'ref')->>'value' as price,
     sku_form.attributes->(sku_shadow.attributes->'salePrice'->>'ref')->>'currency' as currency
-from skus as sku, object_forms as sku_form, object_shadows as sku_shadow, object_contexts as context 
-where sku_shadow.id = sku.shadow_id and sku_form.id = sku.form_id and sku.context_id = context.id;
+from
+    skus as sku,
+    object_forms as sku_form,
+    object_shadows as sku_shadow,
+    object_contexts as context
+where
+    sku_shadow.id = sku.shadow_id
+    and sku_form.id = sku.form_id
+    and sku.context_id = context.id;
 
-create unique index sku_search_view_idx on sku_search_view (id, context);
+create unique index sku_search_view_test_idx on sku_search_view_test (id, context);
