5e4a156037f1a81bad5225f77e818a70441f172c
Sergey Rublev <sergey@foxcommerce.com>
2016-06-01 03:43:30 +0600

    Finish product_search_view triggers

--- sql/V52.01.02__products_search_view_triggers.sql
+++ sql/V52.01.02__products_search_view_triggers.sql
@@ -1,3 +1,4 @@
+-- object forms
 create or replace function update_products_search_view_from_forms_fn() returns trigger as $$
 begin
 
@@ -35,3 +36,94 @@ create trigger update_products_search_view_from_forms
     for each row
     when (NEW.kind = 'product')
     execute procedure update_products_search_view_from_forms_fn();
+
+-- object shadows
+
+create or replace function update_products_search_view_from_shadows_fn() returns trigger as $$
+begin
+
+  update products_search_view set
+    title = subquery.title,
+    images = subquery.images,
+    description = subquery.description,
+    active_from = subquery.active_from,
+    active_to = subquery.active_to,
+    tags = subquery.tags
+    from (select
+            p.id,
+            f.attributes->>(s.attributes->'title'->>'ref') as title,
+            f.attributes->(s.attributes->'images'->>'ref') as images,
+            f.attributes->>(s.attributes->'description'->>'ref') as description,
+            f.attributes->>(s.attributes->'activeFrom'->>'ref') as active_from,
+            f.attributes->>(s.attributes->'activeTo'->>'ref') as active_to,
+            f.attributes->>(s.attributes->'tags'->>'ref') as tags
+        from products as p
+        inner join object_forms as f on (f.id = p.form_id)
+        inner join object_shadows as s on (s.id = p.shadow_id)
+        where s.id = NEW.id) as subquery
+    where subquery.id = products_search_view.id;
+
+
+    return null;
+end;
+$$ language plpgsql;
+
+
+create trigger update_products_search_view_from_shadows
+    after insert or update on object_shadows
+    for each row
+    execute procedure update_products_search_view_from_shadows_fn();
+
+
+--- object contexts
+
+create or replace function update_products_search_view_from_context_fn() returns trigger as $$
+begin
+
+ update products_search_view set
+    context = subquery.name
+    from (select
+            p.id,
+            context.name
+        from products as p
+        inner join object_contexts as context on (p.context_id = context.id)
+        where context.id = NEW.id) as subquery
+    where subquery.id = products_search_view.id;
+
+
+    return null;
+
+end;
+$$ language plpgsql;
+
+create trigger update_products_search_view_from_context
+    after insert or update on object_contexts
+    for each row
+    when (OLD.name is DISTINCT FROM NEW.name)
+    execute procedure update_products_search_view_from_context_fn();
+
+-- product sku links
+create or replace function update_products_search_view_from_links_fn() returns trigger as $$
+begin
+
+ update products_search_view set
+    skus = subquery.skus
+    from (select
+            p.id,
+            link.skus
+        from products as p
+        inner join product_sku_links_view as link on (link.product_id = p.id)
+        where link.product_id = NEW.product_id OR link.product_id = OLD.product_id) as subquery
+    where subquery.id = products_search_view.id;
+
+
+    return null;
+
+end;
+$$ language plpgsql;
+
+create trigger update_products_search_view_from_links
+    after insert or update on product_sku_links_view
+    for each row
+    when (OLD.skus is DISTINCT FROM NEW.skus OR OLD.product_id is distinct from NEW.product_id)
+    execute procedure update_products_search_view_from_links_fn();
