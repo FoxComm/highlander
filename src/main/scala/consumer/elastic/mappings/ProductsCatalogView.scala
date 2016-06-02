51914a7496792a40dc8b0a8b7f6f7670f241d592
Jeff Mataya <jeff@foxcommerce.com>
2016-06-01 22:23:48 -0700

    Index albums into the product catalog view

--- src/main/scala/consumer/elastic/mappings/ProductsCatalogView.scala
+++ src/main/scala/consumer/elastic/mappings/ProductsCatalogView.scala
@@ -12,11 +12,18 @@ final case class ProductsCatalogView()(implicit ec: EC) extends AvroTransformer 
       field("id", IntegerType),
       field("context", StringType) index "not_analyzed",
       field("title", StringType).analyzer("autocomplete"),
-      field("images", StringType) index "not_analyzed",
       field("description", StringType).analyzer("autocomplete"),
       field("salePrice", IntegerType).analyzer("autocomplete"),
-      field("tags", StringType) index "not_analyzed"
+      field("tags", StringType) index "not_analyzed",
+      field("albums").nested(
+          field("name", StringType) index "not_analyzed",
+          field("images").nested(
+              field("alt", StringType) index "not_analyzed",
+              field("title", StringType) index "not_analyzed",
+              field("url", StringType) index "not_analyzed"
+          )
+      )
   )
 
-  override def nestedFields() = List("images", "tags")
+  override def nestedFields() = List("albums", "tags")
 }
