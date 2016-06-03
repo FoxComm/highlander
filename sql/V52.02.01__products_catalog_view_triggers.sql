f0dc1824828acd0e303feab75afdf301254fa2ea
Sergey Rublev <sergey@foxcommerce.com>
2016-06-03 23:25:55 +0600

    Revert "Fix test issue - by upsert for now"
    This reverts commit 56ca65a8931abd277554b90b7a3469b3bcea196f.

--- sql/V52.02.01__products_catalog_view_triggers.sql
+++ sql/V52.02.01__products_catalog_view_triggers.sql
@@ -1,6 +1,8 @@
 create or replace function refresh_products_cat_search_view_fn() returns trigger as $$
 declare
   product_ids int[];
+  insert_ids int[];
+  update_ids int[];
 begin
 
   case TG_TABLE_NAME
@@ -32,8 +34,22 @@ begin
         where sku.id = NEW.id;
   end case;
 
+  select array_agg(elements) into strict insert_ids
+    from (
+      select unnest(product_ids)
+        except
+      select id from products_catalog_view
+    ) t (elements);
 
-  insert into products_catalog_view select
+  select array_agg(elements) into strict update_ids
+    from (
+      select unnest(product_ids)
+        except
+      select unnest(insert_ids)
+    ) t (elements);
+
+  if array_length(insert_ids, 1) > 0 then
+    insert into products_catalog_view select
       p.id,
       f.id as product_id,
       context.name as context,
@@ -44,27 +60,51 @@ begin
       sku.currency as currency,
       f.attributes->>(s.attributes->'tags'->>'ref') as tags
       from products as p
-        left join products_catalog_view as pv on (pv.id = p.id)
         inner join object_contexts as context on (p.context_id = context.id)
         inner join object_forms as f on (f.id = p.form_id)
         inner join object_shadows as s on (s.id = p.shadow_id)
         inner join product_sku_links_view as sv on (sv.product_id = p.id)--get list of sku codes for the product
         inner join sku_search_view as sku on (sku.context_id = context.id and sku.code = sv.skus->>0)
-      where p.id = ANY(product_ids) and
+      where p.id = ANY(insert_ids) and
             (f.attributes->>(s.attributes->'activeFrom'->>'ref'))::timestamp < CURRENT_TIMESTAMP and
             (((f.attributes->>(s.attributes->'activeTo'->>'ref')) = '') IS NOT FALSE or
-            ((f.attributes->>(s.attributes->'activeTo'->>'ref'))::timestamp >= CURRENT_TIMESTAMP))
-      on conflict (id, context) do
-        update set
-          product_id = excluded.product_id,
-          context = excluded.context,
-          title = excluded.title,
-          images = excluded.images,
-          description = excluded.description,
-          sale_price = excluded.sale_price,
-          currency = excluded.currency,
-          tags = excluded.tags
-      where products_catalog_view.id = excluded.id;
+            ((f.attributes->>(s.attributes->'activeTo'->>'ref'))::timestamp >= CURRENT_TIMESTAMP));
+    end if;
+
+  if array_length(update_ids, 1) > 0 then
+    update products_catalog_view set
+      product_id = subquery.product_id,
+      context = subquery.context,
+      title = subquery.title,
+      images = subquery.images,
+      description = subquery.description,
+      sale_price = subquery.sale_price,
+      currency = subquery.currency,
+      tags = subquery.tags
+    from (select
+            p.id,
+            f.id as product_id,
+            context.name as context,
+            f.attributes->>(s.attributes->'title'->>'ref') as title,
+            f.attributes->(s.attributes->'images'->>'ref') as images,
+            f.attributes->>(s.attributes->'description'->>'ref') as description,
+            sku.price as sale_price,
+            sku.currency as currency,
+            f.attributes->>(s.attributes->'tags'->>'ref') as tags
+      from products as p
+        inner join object_contexts as context on (p.context_id = context.id)
+        inner join object_forms as f on (f.id = p.form_id)
+        inner join object_shadows as s on (s.id = p.shadow_id)
+        inner join product_sku_links_view as sv on (sv.product_id = p.id)--get list of sku codes for the product
+        inner join sku_search_view as sku on (sku.context_id = context.id and sku.code = sv.skus->>0)
+      where p.id = ANY(update_ids) and
+            (f.attributes->>(s.attributes->'activeFrom'->>'ref'))::timestamp < CURRENT_TIMESTAMP and
+            (((f.attributes->>(s.attributes->'activeTo'->>'ref')) = '') IS NOT FALSE or
+            ((f.attributes->>(s.attributes->'activeTo'->>'ref'))::timestamp >= CURRENT_TIMESTAMP)))
+      as subquery
+  where products_catalog_view.id = subquery.id;
+  end if;
+
 
   return null;
 end;
@@ -131,7 +171,7 @@ begin
           inner join product_sku_links_view as sv on (sv.product_id = p.id)--get list of sku codes for the product
           inner join sku_search_view as sku on (sku.context_id = context.id and sku.code = sv.skus->>0)
         where sku.id = NEW.id;
-  end case;
+  end case; -- 21
 
   select array_agg(p.id) into current_ids
     from products as p
@@ -157,7 +197,6 @@ begin
         ) t (elements);
 
       if array_length(del_ids, 1) > 0 then
-        raise notice 'delete from catalog by trigger: %', del_ids;
         delete from products_catalog_view where id = ANY(del_ids);
       end if;
   end if;
