02e11e57baef9415f7666950d4a61050b1e3c719
Eugene Sypachev (Axblade) <eugene.sypachev@gmail.com>
2016-06-03 21:08:22 +0300

    burn actions and old line-items

--- src/components/rmas/details.jsx
+++ src/components/rmas/details.jsx
@@ -1,7 +1,6 @@
 import React, { PropTypes } from 'react';
 import { CustomerInfo } from './helpers';
 import TotalsSummary from '../common/totals';
-import LineItems from '../line-items/line-items';
 import Payment from '../payment/payment';
 import RmaStore from '../../stores/rmas';
 
@@ -42,12 +41,12 @@ export default class RmaDetails extends React.Component {
           <CustomerInfo rma={rma} isEditing={isEditing}/>
         </div>
         <div className="fc-col-md-1-1">
-          <LineItems
-            entity={rma}
-            isEditing={isEditing}
-            tableColumns={this.lineItemColumns}
-            model={'rma'}
-            />
+          {/*<LineItems
+             entity={rma}
+             isEditing={isEditing}
+             tableColumns={this.lineItemColumns}
+             model={'rma'}
+             />*/}
         </div>
         <div className="fc-col-md-1-1">
         </div>
