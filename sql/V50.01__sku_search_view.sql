d68b98fda9b0fde5a3f3b0d46f304fb68495cb0b
Sergey Rublev <sergey@foxcommerce.com>
2016-06-04 00:25:07 +0600

    Simplify sku_search_view triggers

--- sql/V50.01__sku_search_view.sql
+++ sql/V50.01__sku_search_view.sql
@@ -9,7 +9,8 @@ create table sku_search_view
     price text,
     currency text
 );
-create unique index sku_search_view_idx on sku_search_view (id, context);
+create unique index sku_search_view_idx on sku_search_view (id, lower(context));
+
 
 create or replace function insert_skus_view_from_skus_fn() returns trigger as $$
 begin
@@ -24,8 +25,8 @@ begin
     sku_form.attributes->(sku_shadow.attributes->'salePrice'->>'ref')->>'value' as price,
     sku_form.attributes->(sku_shadow.attributes->'salePrice'->>'ref')->>'currency' as currency
     from object_contexts as context
-       left join  object_shadows as sku_shadow on (sku_shadow.id = NEW.shadow_id)
-       left join object_forms as sku_form on (sku_form.id = NEW.form_id)
+       inner join object_shadows as sku_shadow on (sku_shadow.id = NEW.shadow_id)
+       inner join object_forms as sku_form on (sku_form.id = NEW.form_id)
     where context.id = NEW.context_id;
 
 
@@ -37,23 +38,6 @@ create trigger insert_skus_view_from_skus
     after insert on skus
     for each row
     execute procedure insert_skus_view_from_skus_fn();
-
-create or replace function update_skus_view_from_skus_fn() returns trigger as $$
-begin
-
-  update sku_search_view set
-    code = NEW.code
-    where id = NEW.id;
-
-  return null;
-end;
-$$ language plpgsql;
-
-
-create trigger update_skus_view_from_skus
-    after update on skus
-    for each row
-    execute procedure update_skus_view_from_skus_fn();
 
 create or replace function update_skus_view_from_object_context_fn() returns trigger as $$
 begin
@@ -82,28 +66,17 @@ create trigger update_skus_view_from_object_forms
 
 
 create or replace function update_skus_view_from_object_attrs_fn() returns trigger as $$
-declare sku_ids int[];
 begin
-  case TG_TABLE_NAME
-    when 'object_shadows' then
-      select array_agg(skus.id) into strict sku_ids
-        from skus
-        inner join object_shadows as sku_shadow on (skus.shadow_id = sku_shadow.id)
-        where sku_shadow.id = NEW.id;
-    when 'object_forms' then
-     select array_agg(skus.id) into strict sku_ids
-       from skus
-       inner join object_forms as sku_form on (skus.form_id = sku_form.id)
-       where sku_form.id = NEW.id;
-  end case;
 
   update sku_search_view set
+    code = subquery.code,
     title = subquery.title,
     image = subquery.image,
     price = subquery.price,
     currency = subquery.currency
     from (select
         sku.id,
+        sku.code,
         sku_form.attributes->>(sku_shadow.attributes->'title'->>'ref') as title,
         sku_form.attributes->(sku_shadow.attributes->'images'->>'ref')->>0 as image,
         sku_form.attributes->(sku_shadow.attributes->'salePrice'->>'ref')->>'value' as price,
@@ -111,7 +84,7 @@ begin
       from skus as sku
       inner join object_forms as sku_form on (sku_form.id = sku.form_id)
       inner join object_shadows as sku_shadow on (sku_shadow.id = sku.shadow_id)
-      where sku.id = ANY(sku_ids)) as subquery
+      where sku.id = NEW.id) as subquery
       where subquery.id = sku_search_view.id;
 
     return null;
@@ -120,11 +93,9 @@ $$ language plpgsql;
 
 
 create trigger update_skus_view_from_object_shadows
-    after update or insert on object_shadows
+    after update on skus
     for each row
+    when (OLD.form_id is distinct from NEW.form_id or
+          OLD.shadow_id is distinct from NEW.shadow_id or
+          OLD.code is distinct from NEW.code)
     execute procedure update_skus_view_from_object_attrs_fn();
-
-create trigger update_skus_view_from_object_forms
-  after update or insert on object_forms
-  for each row
-  execute procedure update_skus_view_from_object_attrs_fn();
