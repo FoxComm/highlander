7a50c0b7bbfac7fa9c7ddec4ffd7b140f361bc70
Pavel Kiselev <kpashka@gmail.com>
2016-06-17 20:22:57 +0600

    Watch events & add to packer server

--- ansible/roles/base/consul_alerts/templates/run_consul_alerts.sh
+++ ansible/roles/base/consul_alerts/templates/run_consul_alerts.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-/usr/local/bin/consul-alerts start --alert-addr=localhost:9000 --consul-addr=${CONSUL_SERVER} --consul-dc={CONSUL_DC} --consul-acl-token=""
+/usr/local/bin/consul-alerts start --watch-checks --consul-addr=${CONSUL_SERVER} --consul-dc={CONSUL_DC} --consul-acl-token=""
