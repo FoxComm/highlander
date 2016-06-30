f00a1e79729a16916e611f4222988ea27651b00b
Maxim Khailo <max@foxcommerce.com>
2016-06-30 16:46:41 -0700

    Adding program options.

--- src/isaac/isaac.cpp
+++ src/isaac/isaac.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
 #include <fstream>
+#include <thread>
 
 #include "util/dbc.hpp"
 #include "service/query.hpp"
-#include <unistd.h>
 
 //for checking sig
 #include <botan/botan.h>
@@ -11,13 +11,47 @@
 #include <botan/rsa.h>
 #include <botan/x509_key.h>
 
+//for main
+#include <boost/asio/ip/host_name.hpp>
+#include <boost/program_options.hpp>
+
 using folly::EventBase;
 using folly::EventBaseManager;
 using folly::SocketAddress;
 
 using Protocol = proxygen::HTTPServer::Protocol;
+namespace po = boost::program_options;
+namespace ip = boost::asio::ip;
 
-const std::string RS256_EMSA = "EMSA3(SHA-256)";
+namespace 
+{
+    const std::string RS256_EMSA = "EMSA3(SHA-256)";
+}
+
+po::options_description create_descriptions()
+{
+    po::options_description d{"Options"};
+    const auto workers = std::thread::hardware_concurrency();
+
+    d.add_options()
+        ("help", "prints help")
+        ("ip", po::value<std::string>()->default_value("0.0.0.0"), "ip to bind")
+        ("http_port", po::value<std::uint16_t>()->default_value(7070), "http port")
+        ("http2_port", po::value<std::uint16_t>()->default_value(7071), "http 2.0 port")
+        ("public_key", po::value<std::string>()->default_value("public_key.pem"), "public key file")
+        ("workers", po::value<std::size_t>()->default_value(workers), "worker threads");
+
+    return d;
+}
+
+po::variables_map parse_options(int argc, char* argv[], po::options_description& desc)
+{
+    po::variables_map v;
+    po::store(po::parse_command_line(argc, argv, desc), v);
+    po::notify(v);
+
+    return v;
+}
 
 bool load_key(isaac::net::context& c, const std::string& key_file)
 {
@@ -34,12 +68,20 @@ bool load_key(isaac::net::context& c, const std::string& key_file)
 int main(int argc, char** argv)
 try
 {
-    const std::size_t WORKERS = 4 * sysconf(_SC_NPROCESSORS_ONLN);
-    const std::string ip = "0.0.0.0";
-    const std::uint16_t http_port = 7070;
-    const std::uint16_t http2_port = 7071;
+    auto description = create_descriptions();
+    auto opt = parse_options(argc, argv, description);
 
-    const std::string key_file = "pk.pem";
+    if(opt.count("help"))
+    {
+        std::cout << description << std::endl;
+        return 0;
+    }
+
+    const auto ip = opt["ip"].as<std::string>();
+    const auto http_port = opt["http_port"].as<std::uint16_t>();
+    const auto http2_port = opt["http2_port"].as<std::uint16_t>();
+    const auto key_file = opt["public_key"].as<std::string>();
+    const auto workers = opt["workers"].as<std::size_t>();
 
     isaac::net::context ctx;
     if(!load_key(ctx, key_file))
@@ -54,7 +96,7 @@ try
     };
 
     proxygen::HTTPServerOptions options;
-    options.threads = WORKERS;
+    options.threads = workers;
     options.listenBacklog = 2048;
     options.idleTimeout = std::chrono::milliseconds(60000);
     options.shutdownOn = {SIGINT, SIGTERM};
