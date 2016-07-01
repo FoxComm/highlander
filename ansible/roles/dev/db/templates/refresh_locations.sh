188eb32655ae5991c831b5b00266ee71eaf519d9
Pavel Kiselev <kpashka@gmail.com>
2016-07-01 16:20:33 -0700

    Fix hack for locations

--- ansible/roles/dev/db/templates/refresh_locations.sh
+++ ansible/roles/dev/db/templates/refresh_locations.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-psql -U{{db_user}} -h{{db_host}} -c'update countries set name = name where 1 = 1' {{db_name}}
-psql -U{{db_user}} -h{{db_host}} -c'update regions set name = name where 1 = 1' {{db_name}}
+psql -U{{db_user}} -h{{db_host}} -c'update countries set is_billable = NOT is_billable where 1 = 1' {{db_name}}
