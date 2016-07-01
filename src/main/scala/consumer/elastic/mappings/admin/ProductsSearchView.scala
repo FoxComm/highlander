7263dd73ec89c8eddeae0434da02d878fe95c51b
Pavel Kiselev <kpashka@gmail.com>
2016-07-01 13:01:44 -0700

    Compilation fix

--- src/main/scala/consumer/elastic/mappings/admin/ProductsSearchView.scala
+++ src/main/scala/consumer/elastic/mappings/admin/ProductsSearchView.scala
@@ -4,6 +4,7 @@ import com.sksamuel.elastic4s.ElasticDsl.{mapping ⇒ esMapping, _}
 import com.sksamuel.elastic4s.mappings.FieldType._
 import consumer.aliases._
 import consumer.elastic.AvroTransformer
+import consumer.elastic.mappings.dateFormat
 
 final case class ProductsSearchView()(implicit ec: EC) extends AvroTransformer {
   def mapping() = esMapping("products_search_view").fields(
