42b44aa45c784ace23b58cdc7fb7d65e5e8b2d12
Alex Kreskiyan <a.kreskiyan@gmail.com>
2016-07-01 15:58:01 +0300

    Added AsyncAction* aliases group

--- src/paragons/types.js
+++ src/paragons/types.js
@@ -4,7 +4,7 @@ export type Dictionary<T> = {
   [key: string]: T;
 };
 
-//actions
+// actions
 export type Action
   = () => void;
 export type Action1<T1>
@@ -24,7 +24,7 @@ export type Action7<T1, T2, T3, T4, T5, T6, T7>
 export type Action8<T1, T2, T3, T4, T5, T6, T7, T8>
   = (x1: T1, x2: T2, x3: T3, x4: T4, x5: T5, x6: T6, x7: T7, x8: T8) => void;
 
-//functions
+// functions
 export type Func<R>
   = () => R;
 export type Func1<T1, R>
@@ -43,3 +43,24 @@ export type Func7<T1, T2, T3, T4, T5, T6, T7, R>
   = (x1: T1, x2: T2, x3: T3, x4: T4, x5: T5, x6: T6, x7: T7) => R;
 export type Func8<T1, T2, T3, T4, T5, T6, T7, T8, R>
   = (x1: T1, x2: T2, x3: T3, x4: T4, x5: T5, x6: T6, x7: T7, x8: T8) => R;
+
+
+// async actions
+export type AsyncAction
+  = Func<Promise>;
+export type AsyncAction1<T1>
+  = Func1<T1, Promise>;
+export type AsyncAction2<T1, T2>
+  = Func2<T1, T2, Promise>;
+export type AsyncAction3<T1, T2, T3>
+  = Func3<T1, T2, T3, Promise>;
+export type AsyncAction4<T1, T2, T3, T4>
+  = Func4<T1, T2, T3, T4, Promise>;
+export type AsyncAction5<T1, T2, T3, T4, T5>
+  = Func5<T1, T2, T3, T4, T5, Promise>;
+export type AsyncAction6<T1, T2, T3, T4, T5, T6>
+  = Func6<T1, T2, T3, T4, T5, T6, Promise>;
+export type AsyncAction7<T1, T2, T3, T4, T5, T6, T7>
+  = Func7<T1, T2, T3, T4, T5, T6, T7, Promise>;
+export type AsyncAction8<T1, T2, T3, T4, T5, T6, T7, T8>
+  = Func8<T1, T2, T3, T4, T5, T6, T7, T8, Promise>;
