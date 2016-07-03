5a523327ad8ffbb087f92f2902df659f48f3dcc6
Alexandra Rodionova <alexa.rodionova@gmail.com>
2016-07-04 01:29:27 +0300

    update user list

--- src/components/user-initials/initials.jsx
+++ src/components/user-initials/initials.jsx
@@ -6,9 +6,28 @@ import React, { Component, PropTypes } from 'react';
  */
 const UserInitials = props => {
   return (
-    <div className="initials">{getInitials(props)}</div>
+    <div className="initials" style={getColor(props)}>{getInitials(props)}</div>
   );
 };
+
+function getColor(props) {
+  const initials = getInitials(props) || '';
+  const code = initials.charCodeAt(1);
+
+  console.log(code);
+  const color = (() => {
+    if (code <= 70) return '#B989DE';
+    if (code > 70 && code <= 75) return '#7DA0F3';
+    if (code > 75 && code <= 80) return '#F45758';
+    if (code > 80 && code <= 85) return '#DD63B9';
+    if (code > 85 && code <= 90) return '#47628D';
+    return '#FFBB3C';
+  })();
+
+  return {
+    background: color
+  };
+}
 
 function getInitials({ name, firstName = '', lastName = '' }) {
 
