b60fc6d52a2c7915df7c93700adb725752342d33
Pavel Kiselev <kpashka@gmail.com>
2016-07-05 10:11:51 -0700

    Working parametrized bootstrap mechanism

--- ansible/roles/dev/consul_alerts/templates/run_consul_alerts.sh
+++ ansible/roles/dev/consul_alerts/templates/run_consul_alerts.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 . /etc/consul.d/env
-/usr/local/bin/consul-alerts start --watch-checks --consul-addr=${CONSUL_SERVER} --consul-dc={CONSUL_DC} --consul-acl-token=""
+/usr/local/bin/consul-alerts start --watch-checks --consul-addr=localhost:8500 --consul-dc=${consul_dc} --consul-acl-token=""
