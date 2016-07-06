6387d88a66f193bcf54eeed250a8b13d0f184d40
Maxim Khailo <max@foxcommerce.com>
2016-07-06 09:05:25 -0700

    Better ratchet logic using optional

--- src/db/user.cpp
+++ src/db/user.cpp
@@ -2,46 +2,48 @@
 #include "db/user.hpp"
 #include "util/dbc.hpp"
 
+#include <boost/optional.hpp>
+
 #include <sstream>
 
 namespace isaac
 {
     namespace db
     {
-        std::string query(const char* table, std::size_t id)
+        std::string query_str(const char* table, const std::size_t id)
         {
             std::stringstream q;
             q << "select id from " << table << " where id=" << id << " limit 1";
             return q.str();
         }
 
-        inline bool same_ratchet(const pqxx::result& result, const int ratchet)
+
+        boost::optional<int> get_db_ratchet(pqxx::connection& c, const char* table, const std::size_t id)
         {
-            REQUIRE_GREATER_EQUAL(ratchet, 0);
-            if(result.empty()) return false;
+            pqxx::read_transaction w{c};
+            auto r = w.exec(query_str(table, id));
+            if(r.empty()) return boost::none;
 
             int db_ratchet = 0;
-            result[0][0].to(db_ratchet);
+            r[0][0].to(db_ratchet);
 
-            return ratchet == db_ratchet;
+            return db_ratchet;
         }
 
         bool user::valid_customer(std::size_t id, int ratchet)
         {
             REQUIRE_GREATER_EQUAL(ratchet, 0);
 
-            pqxx::read_transaction w{_c};
-            auto r = w.exec(query("customers", id));
-            return same_ratchet(r, ratchet);
+            const auto db_ratchet = get_db_ratchet(_c, "customers", id);
+            return db_ratchet && ratchet == db_ratchet.value();
         }
 
         bool user::valid_admin(std::size_t id, int ratchet)
         {
             REQUIRE_GREATER_EQUAL(ratchet, 0);
 
-            pqxx::read_transaction w{_c};
-            auto r = w.exec(query("store_admins", id));
-            return same_ratchet(r, ratchet);
+            const auto db_ratchet = get_db_ratchet(_c, "store_admins", id);
+            return db_ratchet && ratchet == db_ratchet.value();
         }
     }
 }
