99d724e796edd4d4dd6dfcde7b093be593f29d5e
Maxim Khailo <max@foxcommerce.com>
2016-07-06 08:54:06 -0700

    Actually get the ratchet from the db.

--- src/db/user.cpp
+++ src/db/user.cpp
@@ -8,14 +8,22 @@ namespace isaac
 {
     namespace db
     {
-        //This is temporary, the ratchet will come from query
-        const int FAKE_RATCHET = 0;
-
         std::string query(const char* table, std::size_t id)
         {
             std::stringstream q;
-            q << "select id from " << table << " where id=" << id;
+            q << "select id from " << table << " where id=" << id << " limit 1";
             return q.str();
+        }
+
+        inline bool same_ratchet(const pqxx::result& result, const int ratchet)
+        {
+            REQUIRE_GREATER_EQUAL(ratchet, 0);
+            if(result.empty()) return false;
+
+            int db_ratchet = 0;
+            result[0][0].to(db_ratchet);
+
+            return ratchet == db_ratchet;
         }
 
         bool user::valid_customer(std::size_t id, int ratchet)
@@ -24,7 +32,7 @@ namespace isaac
 
             pqxx::read_transaction w{_c};
             auto r = w.exec(query("customers", id));
-            return !r.empty() && ratchet == FAKE_RATCHET;
+            return same_ratchet(r, ratchet);
         }
 
         bool user::valid_admin(std::size_t id, int ratchet)
@@ -33,7 +41,7 @@ namespace isaac
 
             pqxx::read_transaction w{_c};
             auto r = w.exec(query("store_admins", id));
-            return !r.empty() && ratchet == FAKE_RATCHET;
+            return same_ratchet(r, ratchet);
         }
     }
 }
