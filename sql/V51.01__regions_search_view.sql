66b942b9fcc7c10108bd829f817316bc4fb8792b
Pavel Kiselev <kpashka@gmail.com>
2016-07-11 10:12:29 -0700

    Get rid of country object in regions_search_view

--- sql/V51.01__regions_search_view.sql
+++ sql/V51.01__regions_search_view.sql
@@ -3,20 +3,7 @@ select
 	r.id,
 	r.name,
 	r.abbreviation,
-	-- Country
-	to_json((
-		c.id,
-		c.name,
-		c.alpha2,
-		c.alpha3,
-		c.code,
-		c.continent,
-		c.currency,
-		c.uses_postal_code,
-		c.is_billable,
-		c.is_shippable
-	)::export_countries) as country
-from regions as r
-inner join countries as c on (r.country_id = c.id);
+    r.country_id
+from regions as r;
 
 create unique index regions_search_view_idx on regions_search_view (id);
