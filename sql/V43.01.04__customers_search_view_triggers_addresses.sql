38ce6757bc9aa5c24852c18aefa0952b9690edc2
Pavel Kiselev <kpashka@gmail.com>
2016-07-13 11:49:47 -0700

    Order shipping addresses

--- sql/V43.01.04__customers_search_view_triggers_addresses.sql
+++ sql/V43.01.04__customers_search_view_triggers_addresses.sql
@@ -1,27 +1,32 @@
-create or replace function update_orders_view_from_addresses_fn() returns trigger as $$
-declare cord_refs text[];
+create or replace function update_customers_view_from_addresses_fn() returns trigger as $$
+declare customer_ids integer[];
 begin
   case TG_TABLE_NAME
     when 'order_shipping_addresses' then
-      cord_refs := array_agg(new.cord_ref);
-    when 'regions' then
-      select array_agg(osa.cord_ref) into strict cord_refs
+      select array_agg(o.customer_id) into strict customer_ids
       from order_shipping_addresses as osa
+      inner join orders as o on (o.reference_number = osa.cord_ref)
+      where osa.id = new.id;
+    when 'regions' then
+      select array_agg(o.customer_id) into strict customer_ids
+      from order_shipping_addresses as osa
+      inner join orders as o on (o.reference_number = osa.cord_ref)
       inner join regions as r on (r.id = osa.region_id)
       where r.id = new.id;
     when 'countries' THEN
-      select array_agg(osa.cord_ref) into strict cord_refs
+      select array_agg(o.customer_id) into strict customer_ids
       from order_shipping_addresses as osa
+      inner join orders as o on (o.reference_number = osa.cord_ref)
       inner join regions as r1 on (r1.id = osa.region_id)
       inner join countries as c1 on (r1.country_id = c1.id)
       where c1.id = new.id;
   end case;
 
-  update orders_search_view set
+  update customers_search_view set
     shipping_addresses_count = subquery.count,
     shipping_addresses = subquery.addresses
     from (select
-            o.id,
+            c.id,
             count(osa) as count,
             case when count(osa) = 0
             then
@@ -37,30 +42,30 @@ begin
                   c1.currency
                 )::export_addresses)::jsonb
             end as addresses
-        from orders as o
+        from customers as c
+        left join orders as o on (c.id = o.customer_id)
         left join order_shipping_addresses as osa on (o.reference_number = osa.cord_ref)
         left join regions as r1 on (osa.region_id = r1.id)
         left join countries as c1 on (r1.country_id = c1.id)
-        where o.reference_number = any(cord_refs)
-        group by o.id) AS subquery
-    where orders_search_view.id = subquery.id;
+        where c.id = ANY(customer_ids)
+        group by c.id) AS subquery
+    where customers_search_view.id = subquery.id;
 
     return null;
 end;
 $$ language plpgsql;
 
-create trigger update_orders_view_from_order_shipping_addresses
+create trigger update_customers_view_from_order_shipping_addresses
     after update or insert on order_shipping_addresses
     for each row
-    execute procedure update_orders_view_from_addresses_fn();
+    execute procedure update_customers_view_from_addresses_fn();
 
-
-create trigger update_orders_view_from_regions
+create trigger update_customers_view_from_regions
     after update or insert on regions
     for each row
-    execute procedure update_orders_view_from_addresses_fn();
+    execute procedure update_customers_view_from_addresses_fn();
 
-create trigger update_orders_view_from_countries
+create trigger update_customers_view_from_countries
     after update or insert on countries
     for each row
-    execute procedure update_orders_view_from_addresses_fn();
+    execute procedure update_customers_view_from_addresses_fn();
