a711233f754aaf62ca804ddcad3e295ac2c920a3
Jeff Mataya <jeff.mataya@gmail.com>
2016-07-15 13:30:13 -0400

    Fix up order coupons and shipping method

--- src/components/orders/order-shipping-method.jsx
+++ src/components/orders/order-shipping-method.jsx
@@ -1,77 +1,27 @@
-import React, { PropTypes } from 'react';
-import { connect } from 'react-redux';
-import _ from 'lodash';
-import { trackEvent } from 'lib/analytics';
+import React, { Element } from 'react';
 
-import * as shippingMethodActions from '../../modules/orders/shipping-methods';
+import ShippingMethod from 'components/shipping/shipping-method';
 
-import PanelHeader from './panel-header';
-import ShippingMethod from '../shipping/shipping-method';
-
-const mapStateToProps = state => {
-  return { shippingMethods: state.orders.shippingMethods };
+type Props = {
+  order: {
+    currentOrder: {
+      shippingMethod: Object,
+    },
+  },
 };
 
-export class OrderShippingMethod extends React.Component {
-  static propTypes = {
-    isCart: PropTypes.bool,
-    order: PropTypes.shape({
-      currentOrder: PropTypes.shape({
-        shippingMethod: PropTypes.object
-      })
-    }).isRequired,
-    shippingMethods: PropTypes.shape({
-      availableMethods: PropTypes.array,
-      isEditing: PropTypes.bool.isRequired,
-      isEditingPrice: PropTypes.bool
-    }),
-    orderShippingMethodCancelEdit: PropTypes.func,
-    orderShippingMethodStartEditPrice: PropTypes.func,
-    orderShippingMethodCancelEditPrice: PropTypes.func,
-    status: PropTypes.string,
-    updateShippingMethod: PropTypes.func,
-    readOnly: PropTypes.bool,
-  };
+const OrderShippingMethod = (props: Props): Element => {
+  const { currentOrder } = props.order
+  const { shippingMethod } = currentOrder;
 
-  static defaultProps = {
-    isCart: false,
-    status: 'success',
-    readOnly: false,
-  };
+  return (
+    <ShippingMethod
+      currentOrder={currentOrder}
+      title="Shipping Method"
+      readOnly={true}
+      availableShippingMethods={[]}
+      shippingMethods={[shippingMethod]} />
+  );
+};
 
-  render() {
-    const props = this.props;
-
-    const shippingMethod = props.order.currentOrder.shippingMethod;
-    const title = <PanelHeader isCart={props.isCart} status={props.status} text="Shipping Method" />;
-
-    const isCheckingOut = _.get(props, 'order.isCheckingOut', false);
-    const editAction = isCheckingOut ? null : () => {
-      trackEvent('Orders', 'edit_shipping_method');
-      props.fetchShippingMethods(props.order.currentOrder);
-    };
-
-    const doneAction = () => {
-      trackEvent('Orders', 'edit_shipping_method_done');
-      props.orderShippingMethodCancelEdit();
-    };
-
-    return (
-      <ShippingMethod
-        currentOrder={props.order.currentOrder}
-        title={title}
-        readOnly={props.readOnly || !props.isCart}
-        availableShippingMethods={props.shippingMethods.availableMethods}
-        shippingMethods={[shippingMethod]}
-        isEditing={props.shippingMethods.isEditing}
-        editAction={editAction}
-        doneAction={doneAction}
-        updateAction={props.updateShippingMethod}
-        isEditingPrice={props.shippingMethods.isEditingPrice}
-        editPriceAction={props.orderShippingMethodStartEditPrice}
-        cancelPriceAction={props.orderShippingMethodCancelEditPrice} />
-    );
-  }
-}
-
-export default connect(mapStateToProps, shippingMethodActions)(OrderShippingMethod);
+export default OrderShippingMethod;
