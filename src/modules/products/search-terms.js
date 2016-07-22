239d034ee55e34737ebad58101c154344c02e2ef
Alexandra Rodionova <alexa.rodionova@gmail.com>
2016-07-22 20:24:32 +0300

    fix terms for product active search

--- src/modules/products/search-terms.js
+++ src/modules/products/search-terms.js
@@ -16,12 +16,12 @@ const searchTerms = [
   {
     title: 'Product : Active From',
     type: 'date',
-    term: 'activefrom',
+    term: 'activeFrom',
   },
   {
     title: 'Product : Active To',
     type: 'date',
-    term: 'activeto',
+    term: 'activeTo',
   },
 ];
 
