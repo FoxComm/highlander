86474fbc2c8a7f9d1c21b1b53a920b48380aaa52
Adil Wali <adil@adil.io>
2016-07-22 17:07:02 -0700

    Making the pre-commit hooks work in highlander structure.

--- firebird/tasks/hooks.js
+++ firebird/tasks/hooks.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const fs = require('fs');
-const PRE_PUSH = '.git/hooks/pre-push';
+const PRE_PUSH = '../.git/hooks/pre-push';
 
 const hookScript = `#!/bin/sh
 npm run lint && npm run flow
