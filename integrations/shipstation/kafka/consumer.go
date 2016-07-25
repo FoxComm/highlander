7b70ba276d4393a17235270a0a259eb9c2ac05db
Jeff Mataya <jeff@foxcommerce.com>
2016-07-24 21:22:47 -0700

    Create consumer to easily interact with Kafka

---
+++ integrations/shipstation/kafka/consumer.go
@@ -0,0 +1,92 @@
+package kafka
+
+import (
+	"errors"
+	"fmt"
+	"os"
+	"os/signal"
+
+	goavro "github.com/elodina/go-avro"
+	"github.com/elodina/go-kafka-avro"
+	"github.com/elodina/go_kafka_client"
+)
+
+type MessageHandler func(message *go_kafka_client.Message) error
+
+type Consumer interface {
+	RunTopic(string)
+}
+
+type consumer struct {
+	kafkaConsumer *go_kafka_client.Consumer
+}
+
+func NewConsumer(zookeeper, schemaRepo string) (Consumer, error) {
+	zConfig := go_kafka_client.NewZookeeperConfig()
+	zConfig.ZookeeperConnect = []string{zookeeper}
+
+	//Actual consumer settings
+	consumerConfig := go_kafka_client.DefaultConsumerConfig()
+	consumerConfig.AutoOffsetReset = go_kafka_client.SmallestOffset
+	consumerConfig.Coordinator = go_kafka_client.NewZookeeperCoordinator(zConfig)
+	consumerConfig.NumWorkers = 1
+	consumerConfig.NumConsumerFetchers = 1
+	consumerConfig.KeyDecoder = avro.NewKafkaAvroDecoder(schemaRepo)
+	consumerConfig.ValueDecoder = consumerConfig.KeyDecoder
+
+	consumerConfig.Strategy = createDefaultStrategy(defaultMessageHandler)
+
+	consumerConfig.WorkerFailureCallback = defaultFailureCallback
+	consumerConfig.WorkerFailedAttemptCallback = defaultFailedAttemptCallback
+
+	kafkaConsumer := go_kafka_client.NewConsumer(consumerConfig)
+	return &consumer{kafkaConsumer: kafkaConsumer}, nil
+}
+
+func (con *consumer) RunTopic(topic string) {
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt)
+
+	go func() {
+		<-c
+		fmt.Println("Closing ShipStation consumer")
+		con.kafkaConsumer.Close()
+	}()
+
+	fmt.Println("ShipStation consumer started!")
+	con.kafkaConsumer.StartStatic(map[string]int{
+		topic: 1,
+	})
+}
+
+func defaultMessageHandler(message *go_kafka_client.Message) error {
+	record, ok := message.DecodedValue.(*goavro.GenericRecord)
+	if !ok {
+		return errors.New("Error decoding Avro message.")
+	}
+
+	fmt.Printf("%v\n", record)
+	return nil
+}
+
+func createDefaultStrategy(fn MessageHandler) go_kafka_client.WorkerStrategy {
+	return func(
+		worker *go_kafka_client.Worker,
+		message *go_kafka_client.Message,
+		taskId go_kafka_client.TaskId) go_kafka_client.WorkerResult {
+
+		if err := fn(message); err != nil {
+			panic(err)
+		}
+
+		return go_kafka_client.NewSuccessfulResult(taskId)
+	}
+}
+
+func defaultFailureCallback(_ *go_kafka_client.WorkerManager) go_kafka_client.FailedDecision {
+	return go_kafka_client.CommitOffsetAndContinue
+}
+
+func defaultFailedAttemptCallback(_ *go_kafka_client.Task, _ go_kafka_client.WorkerResult) go_kafka_client.FailedDecision {
+	return go_kafka_client.CommitOffsetAndContinue
+}
--- integrations/shipstation/main.go
+++ integrations/shipstation/main.go
@@ -1,67 +1,17 @@
 package main
 
 import (
-	"fmt"
-	"os"
-	"os/signal"
-
-	goavro "github.com/elodina/go-avro"
-	"github.com/elodina/go-kafka-avro"
-	"github.com/elodina/go_kafka_client"
+	"github.com/FoxComm/shipstation/kafka"
 )
 
 func main() {
 	zookeeper := "localhost:2181"
 	schemaRepo := "http://localhost:8081"
 
-	//Coordinator settings
-	zookeeperConfig := go_kafka_client.NewZookeeperConfig()
-	zookeeperConfig.ZookeeperConnect = []string{zookeeper}
-
-	//Actual consumer settings
-	consumerConfig := go_kafka_client.DefaultConsumerConfig()
-	consumerConfig.AutoOffsetReset = go_kafka_client.SmallestOffset
-	consumerConfig.Coordinator = go_kafka_client.NewZookeeperCoordinator(zookeeperConfig)
-	consumerConfig.NumWorkers = 1
-	consumerConfig.NumConsumerFetchers = 1
-	consumerConfig.KeyDecoder = avro.NewKafkaAvroDecoder(schemaRepo)
-	consumerConfig.ValueDecoder = consumerConfig.KeyDecoder
-
-	consumerConfig.Strategy = func(worker *go_kafka_client.Worker, message *go_kafka_client.Message, taskId go_kafka_client.TaskId) go_kafka_client.WorkerResult {
-		// time.Sleep(2 * time.Second)
-		record, ok := message.DecodedValue.(*goavro.GenericRecord)
-		if !ok {
-			panic("Not a *GenericError, but expected one")
-		}
-
-		fmt.Println("SUCCESS")
-		fmt.Printf("%v\n", record)
-
-		return go_kafka_client.NewSuccessfulResult(taskId)
+	consumer, err := kafka.NewConsumer(zookeeper, schemaRepo)
+	if err != nil {
+		panic(err)
 	}
 
-	consumerConfig.WorkerFailureCallback = func(_ *go_kafka_client.WorkerManager) go_kafka_client.FailedDecision {
-		return go_kafka_client.CommitOffsetAndContinue
-	}
-	consumerConfig.WorkerFailedAttemptCallback = func(_ *go_kafka_client.Task, _ go_kafka_client.WorkerResult) go_kafka_client.FailedDecision {
-		return go_kafka_client.CommitOffsetAndContinue
-	}
-
-	kafkaConsumer := go_kafka_client.NewConsumer(consumerConfig)
-	pingPongLoop(kafkaConsumer)
-}
-
-func pingPongLoop(kafkaConsumer *go_kafka_client.Consumer) {
-	c := make(chan os.Signal, 1)
-	signal.Notify(c, os.Interrupt)
-	go func() {
-		<-c
-		fmt.Println("\ngolang > Closing consumer")
-		kafkaConsumer.Close()
-	}()
-
-	fmt.Println("golang > Started!")
-	kafkaConsumer.StartStatic(map[string]int{
-		"orders_search_view": 1,
-	})
+	consumer.RunTopic("orders_search_view")
 }
