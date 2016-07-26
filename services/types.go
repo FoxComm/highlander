f237ee839d78258c9e399fe4f166b5f9d3e4fd59
Alex Kreskiyan <a.kreskiyan@gmail.com>
2016-07-26 11:59:46 +0300

    Split services/helpers to helpers (functions) and types

---
+++ services/types.go
@@ -0,0 +1,15 @@
+package services
+
+import (
+	"github.com/DATA-DOG/go-sqlmock"
+	"github.com/jinzhu/gorm"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/suite"
+)
+
+type GeneralServiceTestSuite struct {
+	suite.Suite
+	assert *assert.Assertions
+	db     *gorm.DB
+	mock   sqlmock.Sqlmock
+}
