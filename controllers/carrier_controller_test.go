45f0038eef4cbd7787a2163ddd419c61dd95e652
Alex Kreskiyan <a.kreskiyan@gmail.com>
2016-07-26 16:33:38 +0300

    Finished CarrierController test

--- controllers/carrier_controller_test.go
+++ controllers/carrier_controller_test.go
@@ -1,6 +1,7 @@
 package controllers
 
 import (
+	"net/http"
 	"testing"
 
 	"github.com/FoxComm/middlewarehouse/api/responses"
@@ -22,8 +23,8 @@ func TestCarrierControllerSuite(t *testing.T) {
 	suite.Run(t, new(CarrierControllerTestSuite))
 }
 
-func (suite *CarrierControllerTestSuite) SetupSuite() {
-	suite.router = gin.Default()
+func (suite *CarrierControllerTestSuite) SetupTest() {
+	suite.router = gin.New()
 
 	suite.service = mocks.NewCarrierServiceMock()
 
@@ -36,9 +37,10 @@ func (suite *CarrierControllerTestSuite) SetupSuite() {
 func (suite *CarrierControllerTestSuite) Test_GetCarriers_EmptyData() {
 	//act
 	result := []responses.Carrier{}
-	err := suite.Get("/carriers/", &result)
+	code, err := suite.Get("/carriers/", &result)
 
 	//assert
+	suite.assert.Equal(http.StatusOK, code)
 	suite.assert.Nil(err)
 	suite.assert.Equal(0, len(result))
 }
@@ -52,13 +54,111 @@ func (suite *CarrierControllerTestSuite) Test_GetCarriers_NonEmptyData() {
 
 	//act
 	result := []responses.Carrier{}
-	err := suite.Get("/carriers/", &result)
+	code, err := suite.Get("/carriers/", &result)
 
 	//assert
+	suite.assert.Equal(http.StatusOK, code)
 	suite.assert.Nil(err)
 	suite.assert.Equal(2, len(result))
 	suite.assert.Equal(carrier1.Name, result[0].Name)
 	suite.assert.Equal(carrier1.TrackingTemplate, result[0].TrackingTemplate)
 	suite.assert.Equal(carrier2.Name, result[1].Name)
 	suite.assert.Equal(carrier2.TrackingTemplate, result[1].TrackingTemplate)
+}
+
+func (suite *CarrierControllerTestSuite) Test_GetCarrierById_NotFound() {
+	//act
+	result := responses.Error{}
+	code, err := suite.Get("/carriers/1", &result)
+
+	//assert
+	suite.assert.Equal(http.StatusNotFound, code)
+	suite.assert.Nil(err)
+	suite.assert.Equal(1, len(result.Errors))
+}
+
+func (suite *CarrierControllerTestSuite) Test_GetCarrierById_Found() {
+	//arrange
+	carrier := &models.Carrier{Name: "UPS", TrackingTemplate: "http://ups.com"}
+	suite.service.CreateCarrier(carrier)
+
+	//act
+	result := responses.Carrier{}
+	code, err := suite.Get("/carriers/1", &result)
+
+	//assert
+	suite.assert.Equal(http.StatusOK, code)
+	suite.assert.Nil(err)
+	suite.assert.Equal(carrier.Name, result.Name)
+	suite.assert.Equal(carrier.TrackingTemplate, result.TrackingTemplate)
+}
+
+func (suite *CarrierControllerTestSuite) Test_CreateCarrier() {
+	//arrange
+	carrier := &models.Carrier{Name: "UPS", TrackingTemplate: "http://ups.com"}
+
+	//act
+	var result uint
+	code, err := suite.Post("/carriers/", carrier, &result)
+
+	//assert
+	suite.assert.Equal(http.StatusCreated, code)
+	suite.assert.Nil(err)
+	suite.assert.Equal(uint(1), result)
+}
+
+func (suite *CarrierControllerTestSuite) Test_UpdateCarrier_NotFound() {
+	//arrange
+	updated := &models.Carrier{Name: "DHL", TrackingTemplate: "http://dhl.com"}
+
+	//act
+	result := responses.Error{}
+	code, err := suite.Put("/carriers/1", updated, &result)
+
+	//assert
+	suite.assert.Equal(http.StatusNotFound, code)
+	suite.assert.Nil(err)
+	suite.assert.Equal(1, len(result.Errors))
+}
+
+func (suite *CarrierControllerTestSuite) Test_UpdateCarrier_Found() {
+	//arrange
+	carrier := &models.Carrier{Name: "UPS", TrackingTemplate: "http://ups.com"}
+	suite.service.CreateCarrier(carrier)
+	updated := &models.Carrier{Name: "DHL", TrackingTemplate: "http://dhl.com"}
+
+	//act
+	var result string
+	code, err := suite.Put("/carriers/1", updated, &result)
+
+	//assert
+	suite.assert.Equal(http.StatusNoContent, code)
+	suite.assert.Nil(err)
+	suite.assert.Equal("", result)
+}
+
+func (suite *CarrierControllerTestSuite) Test_DeleteCarrier_NotFound() {
+	//act
+	result := responses.Error{}
+	code, err := suite.Delete("/carriers/1", &result)
+
+	//assert
+	suite.assert.Equal(http.StatusNotFound, code)
+	suite.assert.Nil(err)
+	suite.assert.Equal(1, len(result.Errors))
+}
+
+func (suite *CarrierControllerTestSuite) Test_DeleteCarrier_Found() {
+	//arrange
+	carrier := &models.Carrier{Name: "UPS", TrackingTemplate: "http://ups.com"}
+	suite.service.CreateCarrier(carrier)
+
+	//act
+	var result string
+	code, err := suite.Delete("/carriers/1", &result)
+
+	//assert
+	suite.assert.Equal(http.StatusNoContent, code)
+	suite.assert.Nil(err)
+	suite.assert.Equal("", result)
 }
--- controllers/types.go
+++ controllers/types.go
@@ -3,7 +3,7 @@ package controllers
 import (
 	"bytes"
 	"encoding/json"
-	"io/ioutil"
+	"io"
 	"net/http"
 	"net/http/httptest"
 
@@ -22,27 +22,55 @@ type GeneralControllerTestSuite struct {
 	router *gin.Engine
 }
 
-func (suite *GeneralControllerTestSuite) Get(url string, target interface{}) error {
+func (suite *GeneralControllerTestSuite) Get(url string, target interface{}) (int, error) {
 	request, _ := http.NewRequest("GET", url, nil)
 
 	return suite.getResponse(request, target)
 }
 
-func (suite *GeneralControllerTestSuite) getResponse(request *http.Request, target interface{}) error {
+func (suite *GeneralControllerTestSuite) Post(url string, body interface{}, target interface{}) (int, error) {
+	request, _ := http.NewRequest("POST", url, prepareBody(body))
+
+	return suite.getResponse(request, target)
+}
+
+func (suite *GeneralControllerTestSuite) Put(url string, body interface{}, target interface{}) (int, error) {
+	request, _ := http.NewRequest("PUT", url, prepareBody(body))
+
+	return suite.getResponse(request, target)
+}
+
+func (suite *GeneralControllerTestSuite) Delete(url string, target interface{}) (int, error) {
+	request, _ := http.NewRequest("DELETE", url, nil)
+
+	return suite.getResponse(request, target)
+}
+
+func (suite *GeneralControllerTestSuite) getResponse(request *http.Request, target interface{}) (int, error) {
 	//record response
 	response := httptest.NewRecorder()
 
 	//serve request with router, writing to response
 	suite.router.ServeHTTP(response, request)
 
-	//read body to []byte
-	raw, err := ioutil.ReadAll(response.Body)
-	if err != nil {
-		return err
+	return response.Code, parseBody(response.Body, target)
+}
+
+func parseBody(body *bytes.Buffer, target interface{}) error {
+	//nothing to read, if empty buffer
+	if body.Len() == 0 {
+		return nil
 	}
 
 	//decode body to target object and return it
-	decoder := json.NewDecoder(bytes.NewReader(raw))
+	decoder := json.NewDecoder(body)
 
 	return decoder.Decode(target)
+}
+
+func prepareBody(raw interface{}) io.Reader {
+	buffer := new(bytes.Buffer)
+	json.NewEncoder(buffer).Encode(raw)
+
+	return buffer
 }
