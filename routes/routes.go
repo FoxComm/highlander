b3f32d6c8d3f1e59197e6ed8c05bce1f22e52ad6
Alex Kreskiyan <a.kreskiyan@gmail.com>
2016-07-27 19:45:31 +0300

    Wired shipping methods CRUD to router

--- routes/routes.go
+++ routes/routes.go
@@ -11,19 +11,22 @@ import (
 func GetRoutes(db *gorm.DB) map[string]controllers.IController {
 	//repositories
 	carrierRepository := repositories.NewCarrierRepository(db)
+	shippingMethodRepository := repositories.NewShippingMethodRepository(db)
 
 	//services
 	summaryService := services.NewSummaryService(db)
 	inventoryService := services.NewInventoryService(db, summaryService)
 	carrierService := services.NewCarrierService(carrierRepository)
+	shippingMethodService := services.NewShippingMethodService(shippingMethodRepository)
 	// shipmentService := services.NewShipmentService(db)
 
 	return map[string]controllers.IController{
-		"/ping":      	 controllers.NewPingController(),
-		"/summary":      controllers.NewSummaryController(summaryService),
-		"/stock-items":  controllers.NewStockItemController(inventoryService),
-		"/reservations": controllers.NewReservationController(inventoryService),
-		"/carriers":     controllers.NewCarrierController(carrierService),
+		"/ping":            controllers.NewPingController(),
+		"/summary":         controllers.NewSummaryController(summaryService),
+		"/stock-items":     controllers.NewStockItemController(inventoryService),
+		"/reservations":    controllers.NewReservationController(inventoryService),
+		"/carriers":        controllers.NewCarrierController(carrierService),
+		"/shippingMethods": controllers.NewShippingMethodController(shippingMethodService),
 		// "/shipments":    controllers.NewShipments(shipmentService),
 	}
 }
