2ed433e6c585404bbc2868ab21f3c57e639779ec
Alex Kreskiyan <a.kreskiyan@gmail.com>
2016-07-27 13:10:56 +0300

    Updated shipment methods

--- sql/V000008__create_shipments_items.sql
+++
@@ -1,8 +0,0 @@
-create table shipments_items (
-  id serial primary key,
-  shipment_id integer references shipments(id) on update restrict on delete restrict,
-  stock_item_id integer not null references stock_items(id) on update restrict on delete restrict,
-  quantity integer not null,
-  state shipment_state,
-  reason shipment_failure_reason
-);
