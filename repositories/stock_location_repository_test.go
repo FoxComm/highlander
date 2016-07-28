0df6d63e0cb6f356fb72787acf902c9a86efd8a7
tony.pizzicato <tony.grebnev@gmail.com>
2016-07-28 16:05:52 +0300

    stock locations repo

---
+++ repositories/stock_location_repository_test.go
@@ -0,0 +1,121 @@
+package repositories
+
+import (
+	"testing"
+
+	"github.com/FoxComm/middlewarehouse/common/db/config"
+	"github.com/FoxComm/middlewarehouse/common/db/tasks"
+	"github.com/FoxComm/middlewarehouse/models"
+	"github.com/jinzhu/gorm"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/suite"
+)
+
+type stockLocationRepositoryTestSuite struct {
+	GeneralRepositoryTestSuite
+	repository IStockLocationRepository
+	location   *models.StockLocation
+}
+
+func TestStockLocationRepositorySuite(t *testing.T) {
+	suite.Run(t, new(stockLocationRepositoryTestSuite))
+}
+
+func (suite *stockLocationRepositoryTestSuite) SetupSuite() {
+	suite.db, _ = config.DefaultConnection()
+	suite.repository = NewStockLocationRepository(suite.db)
+	suite.assert = assert.New(suite.T())
+}
+func (suite *stockLocationRepositoryTestSuite) SetupTest() {
+	tasks.TruncateTables([]string{
+		"stock_locations",
+	})
+
+	// create one stock location for test cases
+	suite.location = &models.StockLocation{
+		Name:    "Test Location",
+		Type:    "Warehouse",
+		Address: &models.Address{Name: "WH Address"},
+	}
+	suite.repository.CreateLocation(suite.location)
+}
+
+func (suite *stockLocationRepositoryTestSuite) TearDownSuite() {
+	suite.db.Close()
+}
+
+func (suite *stockLocationRepositoryTestSuite) Test_GetLocations() {
+	locations, err := suite.repository.GetLocations()
+
+	suite.assert.Nil(err)
+	suite.assert.Equal(1, len(locations))
+}
+
+func (suite *stockLocationRepositoryTestSuite) Test_GetLocationById() {
+	location, err := suite.repository.GetLocationByID(suite.location.ID)
+
+	suite.assert.Nil(err)
+	suite.assert.Equal(suite.location.Name, location.Name)
+}
+
+func (suite *stockLocationRepositoryTestSuite) Test_GetLocationById_NotFound() {
+	location, err := suite.repository.GetLocationByID(suite.location.ID + 1)
+
+	suite.assert.Nil(location)
+	suite.assert.NotNil(err)
+	suite.assert.IsType(gorm.ErrRecordNotFound, err)
+}
+
+func (suite *stockLocationRepositoryTestSuite) Test_CreateLocation() {
+	model := &models.StockLocation{
+		Name:    "Some Name",
+		Type:    "Warehouse",
+		Address: &models.Address{Name: "Warehouse Address"},
+	}
+
+	location, err := suite.repository.CreateLocation(model)
+
+	suite.assert.NotNil(location)
+	suite.assert.Nil(err)
+	suite.assert.NotNil(location.ID)
+}
+
+func (suite *stockLocationRepositoryTestSuite) Test_UpdateLocation() {
+	model := *suite.location
+	model.Name = "Updated Name"
+
+	location, err := suite.repository.UpdateLocation(&model)
+
+	suite.assert.NotNil(location)
+	suite.assert.Nil(err)
+	suite.assert.Equal(model.Name, location.Name)
+	suite.assert.NotEqual(suite.location.Name, location.Name)
+}
+
+func (suite *stockLocationRepositoryTestSuite) Test_UpdateLocation_NotFound() {
+	model := &models.StockLocation{
+		Name:    "Some Name",
+		Type:    "Warehouse",
+		Address: &models.Address{Name: "Warehouse Address"},
+	}
+	model.ID = 1e9
+
+	location, err := suite.repository.UpdateLocation(model)
+
+	suite.assert.Nil(location)
+	suite.assert.NotNil(err)
+	suite.assert.IsType(gorm.ErrRecordNotFound, err)
+}
+
+func (suite *stockLocationRepositoryTestSuite) Test_DeleteLocation() {
+	err := suite.repository.DeleteLocation(suite.location.ID)
+
+	suite.assert.Nil(err)
+}
+
+func (suite *stockLocationRepositoryTestSuite) Test_DeleteLocation_NotFound() {
+	err := suite.repository.DeleteLocation(uint(1e9))
+
+	suite.assert.NotNil(err)
+	suite.assert.IsType(gorm.ErrRecordNotFound, err)
+}
--- sql/V000010__create_stock_locations.sql
+++ sql/V000010__create_stock_locations.sql
@@ -2,5 +2,9 @@ create table stock_locations (
     id serial primary key,
     type stock_location_type,
     name generic_string not null,
-    address jsonb
+    address jsonb,
+
+    created_at generic_timestamp_now,
+    updated_at generic_timestamp_now,
+    deleted_at generic_timestamp_null
 );
