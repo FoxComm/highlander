51475aebd8ec3aef32f4826648e85f0c9557f9ac
Jeff Mataya <jeff@foxcommerce.com>
2016-07-31 19:14:05 -0700

    Delete some dead code

--- src/modules/carts/index.js
+++ src/modules/carts/index.js
@@ -1,32 +1,16 @@
 import { combineReducers } from 'redux';
 import details from './details';
-// import bulk from './bulk';
-// import watchers from './watchers';
-// import list from './list';
-// import newOrder from './new-order';
 import skuSearch from './sku-search';
 import shippingMethods from './shipping-methods';
 import paymentMethods from './payment-methods';
 import coupons from './coupons';
-// import discounts from './discounts';
-// import shipments from './shipments';
-// import carriers from './carriers';
-// import shipmentMethods from './shipment-methods';
 
 const cartReducer = combineReducers({
   details,
-  // bulk,
-  // watchers,
-  // list,
   skuSearch,
   shippingMethods,
   paymentMethods,
-  // newOrder,
   coupons,
-  // discounts,
-  // shipments,
-  // carriers,
-  // shipmentMethods,
 });
 
 export default cartReducer;
