1277bdbeef8aad127ec9637e0e3664add1930d51
Alex Kreskiyan <a.kreskiyan@gmail.com>
2016-08-03 17:02:42 +0300

    Simplified AddressRepository test

--- repositories/address_repository_test.go
+++ repositories/address_repository_test.go
@@ -2,6 +2,7 @@ package repositories
 
 import (
 	"database/sql"
+	"database/sql/driver"
 	"testing"
 
 	"github.com/FoxComm/middlewarehouse/common/gormfox"
@@ -37,7 +38,7 @@ func (suite *AddressRepositoryTestSuite) TearDownTest() {
 func (suite *AddressRepositoryTestSuite) Test_GetAddressByID_NotFound_ReturnsNotFoundError() {
 	//arrange
 	rows := sqlmock.
-		NewRows([]string{"id", "name", "region_id", "city", "zip", "address1", "address2", "phone_number", "created_at", "updated_at", "deleted_at"})
+		NewRows(suite.getAddressColumns())
 	suite.mock.
 		ExpectQuery(`SELECT (.+) FROM "addresses" WHERE (.+) \(\("id" = \?\)\) (.+)`).
 		WithArgs(1).
@@ -55,11 +56,10 @@ func (suite *AddressRepositoryTestSuite) Test_GetAddressByID_NotFound_ReturnsNot
 
 func (suite *AddressRepositoryTestSuite) Test_GetAddressByID_Found_ReturnsAddressModel() {
 	//arrange
-	address1 := &models.Address{gormfox.Base{ID: uint(1)}, "Home address", uint(1), "Texas", "75231", "Some st, 335", sql.NullString{String: "", Valid: false}, "19527352893"}
+	address1 := suite.getTestAddress1()
 	rows := sqlmock.
-		NewRows([]string{"id", "name", "region_id", "city", "zip", "address1", "address2", "phone_number", "created_at", "updated_at", "deleted_at"}).
-		AddRow(address1.ID, address1.Name, address1.RegionID, address1.City, address1.Zip, address1.Address1,
-			nil, address1.PhoneNumber, address1.CreatedAt, address1.UpdatedAt, address1.DeletedAt)
+		NewRows(suite.getAddressColumns()).
+		AddRow(suite.getAddressRow(address1)...)
 	suite.mock.
 		ExpectQuery(`SELECT (.+) FROM "addresses" WHERE (.+) \(\("id" = \?\)\) (.+)`).
 		WithArgs(1).
@@ -78,14 +78,13 @@ func (suite *AddressRepositoryTestSuite) Test_GetAddressByID_Found_ReturnsAddres
 
 func (suite *AddressRepositoryTestSuite) Test_CreateAddress_ReturnsCreatedRecord() {
 	//arrange
-	address1 := &models.Address{gormfox.Base{ID: uint(1)}, "Home address", uint(1), "Texas", "75231", "Some st, 335", sql.NullString{String: "", Valid: false}, "19527352893"}
+	address1 := suite.getTestAddress1()
 	suite.mock.
 		ExpectExec(`INSERT INTO "addresses"`).
 		WillReturnResult(sqlmock.NewResult(1, 1))
 	rows := sqlmock.
-		NewRows([]string{"id", "name", "region_id", "city", "zip", "address1", "address2", "phone_number", "created_at", "updated_at", "deleted_at"}).
-		AddRow(address1.ID, address1.Name, address1.RegionID, address1.City, address1.Zip, address1.Address1,
-			nil, address1.PhoneNumber, address1.CreatedAt, address1.UpdatedAt, address1.DeletedAt)
+		NewRows(suite.getAddressColumns()).
+		AddRow(suite.getAddressRow(address1)...)
 	suite.mock.
 		ExpectQuery(`SELECT (.+) FROM "addresses" WHERE (.+) \(\("id" = \?\)\) (.+)`).
 		WithArgs(1).
@@ -103,4 +102,19 @@ func (suite *AddressRepositoryTestSuite) Test_CreateAddress_ReturnsCreatedRecord
 
 	//make sure that all expectations were met
 	suite.assert.Nil(suite.mock.ExpectationsWereMet())
+}
+
+func (suite *AddressRepositoryTestSuite) getTestAddress1() *models.Address {
+	return &models.Address{gormfox.Base{ID: uint(1)}, "Home address", uint(1), "Texas", "75231",
+		"Some st, 335", sql.NullString{String: "", Valid: false}, "19527352893"}
+}
+
+func (suite *AddressRepositoryTestSuite) getAddressColumns() []string {
+	return []string{"id", "name", "region_id", "city", "zip", "address1", "address2",
+		"phone_number", "created_at", "updated_at", "deleted_at"}
+}
+
+func (suite *AddressRepositoryTestSuite) getAddressRow(address *models.Address) []driver.Value {
+	return []driver.Value{address.ID, address.Name, address.RegionID, address.City, address.Zip, address.Address1,
+		nil, address.PhoneNumber, address.CreatedAt, address.UpdatedAt, address.DeletedAt}
 }
