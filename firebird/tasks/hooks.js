8d0ce9ae48542a728efdcf6b5d3e720625ed8968
Maxim Khailo <max@foxcommerce.com>
2016-08-05 05:40:15 -0700

    No hooks

--- firebird/tasks/hooks.js
+++ firebird/tasks/hooks.js
@@ -1,18 +1,4 @@
 'use strict';
 
-const fs = require('fs');
-const PRE_PUSH = '../.git/hooks/pre-push';
-
-const hookScript = `#!/bin/sh
-npm run lint && npm run flow
-`;
-
-function installHooks() {
-}
-
 module.exports = function(gulp, $, opts) {
-  if (opts.autoInstallHooks) {
-    installHooks();
-  }
-  gulp.task('hooks', installHooks);
 };
--- firebrand/tasks/hooks.js
+++ firebrand/tasks/hooks.js
@@ -1,18 +1,5 @@
 'use strict';
 
-const fs = require('fs');
-const PRE_PUSH = '../.git/hooks/pre-push';
-
-const hookScript = `#!/bin/sh
-cd firebrand && npm run lint && npm run flow
-`;
-
-function installHooks() {
-}
 
 module.exports = function(gulp, $, opts) {
-  if (opts.autoInstallHooks) {
-    installHooks();
-  }
-  gulp.task('hooks', installHooks);
 };
