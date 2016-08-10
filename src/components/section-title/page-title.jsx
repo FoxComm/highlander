d7fb9e7ed7fe57bd4b01499b579fb8661e433b95
Andrey Rublev <andrey@foxcommerce.com>
2016-08-10 19:17:19 +0700

    set document title corresponding to the current page

--- src/components/section-title/page-title.jsx
+++ src/components/section-title/page-title.jsx
@@ -1,22 +1,38 @@
-import React, { PropTypes } from 'react';
+// @flow
+import React, { Element, Component } from 'react';
+import { setPageTitle } from 'lib/navigation';
 import SectionTitle from './section-title';
 
 /**
  * PageTitle is formally SectionTitle for defined parent context - big page headers.
  */
-const PageTitle = props => {
-  return (
-    <SectionTitle {...props} titleTag={ React.DOM.h1 } className="_page-context">
-      {props.children}
-    </SectionTitle>
-  );
-};
 
-PageTitle.propTypes = {
-  ...SectionTitle.propTypes,
-  pageContext: PropTypes.bool,
-  children: PropTypes.node,
-};
+type Props = {
+  children: Element,
+  title: string|Element,
+  documentTitle?: string|boolean;
+}
 
+export default class PageTitle extends Component {
+  props: Props;
 
-export default PageTitle;
+  componentDidMount() {
+    const { props } = this;
+
+    if (props.documentTitle !== false) {
+      const documentTitle = props.documentTitle || props.title;
+      if (typeof documentTitle == 'string') {
+        setPageTitle(documentTitle);
+      }
+    }
+  }
+
+  render() {
+    const { props } = this;
+    return (
+      <SectionTitle {...props} titleTag={ React.DOM.h1 } className="_page-context">
+        {props.children}
+      </SectionTitle>
+    );
+  }
+}
