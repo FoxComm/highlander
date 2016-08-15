76fdfef4398600d3466d4418709a449e9438f14b
Alex Kreskiyan <a.kreskiyan@gmail.com>
2016-08-15 17:01:16 +0300

    Added flow types for shipment paragons

--- src/paragons/shipment.js
+++ src/paragons/shipment.js
@@ -19,3 +19,44 @@ export const itemReasons = {
 export const itemReasonsTitles = {
   [itemReasons.outOfStock]: 'Out Of Stock',
 };
+
+export type Shipment = {
+  id: number;
+  shippingMethodId: number;
+  referenceNumber: string;
+  state: string;
+  shipmentDate: ?string;
+  deliveredDate: ?string;
+  estimatedArrival: ?string;
+  address: Object;
+  lineItems: Array<ShipmentLineItem>;
+  trackingNumber: ?string;
+};
+
+export type ShipmentLineItem = {
+  id: number;
+  referenceNumber: string;
+  sku: string;
+  name: string;
+  price: number;
+  imagePath: string;
+  state: string;
+  quantity: number;
+};
+
+export type UnshippedLineItem = ShipmentLineItem & {
+  reason: ?string;
+};
+
+export type Carrier = {
+  id: number;
+  name: string;
+  trackingTemplate: string;
+};
+
+export type ShippingMethod = {
+  id: number;
+  carrierId: ?number;
+  carrier: ?Carrier;
+  name: string;
+};
