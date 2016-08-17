bd2954f85c86ac6b70e6f77efb05e3088f3e6568
Alex Kreskiyan <a.kreskiyan@gmail.com>
2016-08-17 13:56:56 +0300

    refactored slightly ShipmentService

--- services/shipment_service.go
+++ services/shipment_service.go
@@ -6,7 +6,7 @@ import (
 )
 
 type shipmentService struct {
-	repository              repositories.IShipmentRepository
+	shipmentRepository      repositories.IShipmentRepository
 	addressService          IAddressService
 	shipmentLineItemService IShipmentLineItemService
 }
@@ -26,7 +26,7 @@ func NewShipmentService(
 }
 
 func (service *shipmentService) GetShipmentsByReferenceNumber(referenceNumber string) ([]*models.Shipment, error) {
-	return service.repository.GetShipmentsByReferenceNumber(referenceNumber)
+	return service.shipmentRepository.GetShipmentsByReferenceNumber(referenceNumber)
 }
 
 func (service *shipmentService) CreateShipment(shipment *models.Shipment) (*models.Shipment, error) {
@@ -36,7 +36,7 @@ func (service *shipmentService) CreateShipment(shipment *models.Shipment) (*mode
 	}
 
 	shipment.AddressID = address.ID
-	result, err := service.repository.CreateShipment(shipment)
+	result, err := service.shipmentRepository.CreateShipment(shipment)
 	if err != nil {
 		service.addressService.DeleteAddress(address.ID)
 		return nil, err
@@ -44,10 +44,11 @@ func (service *shipmentService) CreateShipment(shipment *models.Shipment) (*mode
 
 	createdLineItems := []models.ShipmentLineItem{}
 	for i, _ := range shipment.ShipmentLineItems {
-		shipment.ShipmentLineItems[i].ShipmentID = result.ID
-		createdLineItem, err := service.shipmentLineItemService.CreateShipmentLineItem(&shipment.ShipmentLineItems[i])
+		lineItem := &shipment.ShipmentLineItems[i]
+		lineItem.ShipmentID = result.ID
+		createdLineItem, err := service.shipmentLineItemService.CreateShipmentLineItem(lineItem)
 		if err != nil {
-			service.repository.DeleteShipment(result.ID)
+			service.shipmentRepository.DeleteShipment(result.ID)
 			service.addressService.DeleteAddress(address.ID)
 			for _, lineItem := range createdLineItems {
 				service.shipmentLineItemService.DeleteShipmentLineItem(lineItem.ID)
@@ -59,11 +60,11 @@ func (service *shipmentService) CreateShipment(shipment *models.Shipment) (*mode
 		createdLineItems = append(createdLineItems, *createdLineItem)
 	}
 
-	return service.repository.GetShipmentByID(result.ID)
+	return service.shipmentRepository.GetShipmentByID(result.ID)
 }
 
 func (service *shipmentService) UpdateShipment(shipment *models.Shipment) (*models.Shipment, error) {
-	_, err := service.repository.UpdateShipment(shipment)
+	_, err := service.shipmentRepository.UpdateShipment(shipment)
 	if err != nil {
 		return nil, err
 	}
@@ -72,5 +73,5 @@ func (service *shipmentService) UpdateShipment(shipment *models.Shipment) (*mode
 		service.shipmentLineItemService.UpdateShipmentLineItem(&shipment.ShipmentLineItems[i])
 	}
 
-	return service.repository.GetShipmentByID(shipment.ID)
+	return service.shipmentRepository.GetShipmentByID(shipment.ID)
 }
