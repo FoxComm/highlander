a4ea6998e8346ab123545b39a598b4686511a8e4
Jeff Mataya <jeff@foxcommerce.com>
2016-08-22 12:13:52 -0700

    Aaaaand version the API

--- routes/routes.go
+++ routes/routes.go
@@ -29,14 +29,14 @@ func GetRoutes(db *gorm.DB) map[string]controllers.IController {
 	shipmentService := services.NewShipmentService(db, shipmentRepository, shipmentLineItemService, unitRepository)
 
 	return map[string]controllers.IController{
-		"/public/ping":             controllers.NewPingController(),
-		"/public/summary":          controllers.NewSummaryController(summaryService),
-		"/public/stock-items":      controllers.NewStockItemController(inventoryService),
-		"/public/stock-locations":  controllers.NewStockLocationController(stockLocationService),
-		"/public/carriers":         controllers.NewCarrierController(carrierService),
-		"/public/shipping-methods": controllers.NewShippingMethodController(shippingMethodService),
-		"/public/shipments":        controllers.NewShipmentController(shipmentService, shipmentLineItemService),
+		"v1/public/ping":             controllers.NewPingController(),
+		"v1/public/summary":          controllers.NewSummaryController(summaryService),
+		"v1/public/stock-items":      controllers.NewStockItemController(inventoryService),
+		"v1/public/stock-locations":  controllers.NewStockLocationController(stockLocationService),
+		"v1/public/carriers":         controllers.NewCarrierController(carrierService),
+		"v1/public/shipping-methods": controllers.NewShippingMethodController(shippingMethodService),
+		"v1/public/shipments":        controllers.NewShipmentController(shipmentService, shipmentLineItemService),
 
-		"/private/reservations": controllers.NewReservationController(inventoryService),
+		"v1/private/reservations": controllers.NewReservationController(inventoryService),
 	}
 }
