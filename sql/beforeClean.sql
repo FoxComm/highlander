aa22d617481c34ced0d20164046805b8b359b1d1
tony.pizzicato <tony.grebnev@gmail.com>
2016-08-23 00:24:40 +0300

    fix pg_replication_slots name

--- sql/beforeClean.sql
+++ sql/beforeClean.sql
@@ -1,4 +1,4 @@
-select pg_drop_replication_slot('bottledwater') where exists (
-  select * from pg_replication_slots where slot_name = 'bottledwater' and active = false);
+select pg_drop_replication_slot('bottledwater_phoenix') where exists (
+  select * from pg_replication_slots where slot_name = 'bottledwater_phoenix' and active = false);
 
 drop extension if exists bottledwater;
