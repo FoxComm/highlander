65557da1fb4f13e20eb78bee8883bf6edc48a89f
Adil Wali <adil@adil.io>
2016-08-22 18:24:46 -0700

    Properly setting up view for index method.

--- marketplace/web/views/vendor_view.ex
+++ marketplace/web/views/vendor_view.ex
@@ -2,6 +2,13 @@ defmodule Marketplace.VendorView do
   use Marketplace.Web, :view
 
   def render("index.json", %{vendors: vendors}) do
-    vendors
+    %{vendors: render_many(vendors, Marketplace.VendorView, "vendor.json")}
+  end
+
+  def render("vendor.json", %{vendor: vendor}) do
+    %{id: vendor.id,
+      name: vendor.name,
+      descripiton: vendor.description,
+      state: vendor.state}
   end
 end
