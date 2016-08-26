6970746411fa2e510773aaa82a8042635c35cdd8
Alex Kreskiyan <a.kreskiyan@gmail.com>
2016-08-26 16:33:44 +0300

    Fixed AddressRepository and its tests

--- repositories/address_repository_test.go
+++ repositories/address_repository_test.go
@@ -1,13 +1,17 @@
 package repositories
 
 import (
+	"fmt"
 	"testing"
 
+	//"github.com/FoxComm/middlewarehouse/fixtures"
+	//"github.com/FoxComm/middlewarehouse/models"
+	"github.com/FoxComm/middlewarehouse/common/db/config"
+	"github.com/FoxComm/middlewarehouse/common/db/tasks"
 	"github.com/FoxComm/middlewarehouse/fixtures"
-	"github.com/FoxComm/middlewarehouse/models"
 
-	"github.com/DATA-DOG/go-sqlmock"
-	"github.com/jinzhu/gorm"
+	"github.com/FoxComm/middlewarehouse/common/tests"
+	"github.com/FoxComm/middlewarehouse/models"
 	"github.com/stretchr/testify/suite"
 )
 
@@ -20,116 +24,79 @@ func TestAddressRepositorySuite(t *testing.T) {
 	suite.Run(t, new(AddressRepositoryTestSuite))
 }
 
-func (suite *AddressRepositoryTestSuite) SetupTest() {
-	suite.db, suite.mock = CreateDbMock()
+func (suite *AddressRepositoryTestSuite) SetupSuite() {
+	suite.db, _ = config.Connect(config.NewPGConfig())
 
 	suite.repository = NewAddressRepository(suite.db)
 }
 
-func (suite *AddressRepositoryTestSuite) TearDownTest() {
-	//make sure that all expectations were met
-	suite.Nil(suite.mock.ExpectationsWereMet())
+func (suite *AddressRepositoryTestSuite) SetupTest() {
+	tasks.TruncateTables([]string{
+		"addresses",
+	})
+}
 
+func (suite *AddressRepositoryTestSuite) TearDownSuite() {
 	suite.db.Close()
 }
 
 func (suite *AddressRepositoryTestSuite) Test_GetAddressByID_NotFound_ReturnsNotFoundError() {
-	//arrange
-	rows := sqlmock.
-		NewRows(fixtures.GetAddressColumns())
-	suite.mock.
-		ExpectQuery(`SELECT .+ FROM "addresses" WHERE .+ \(\("id" = \?\)\) .+`).
-		WithArgs(1).
-		WillReturnRows(rows)
-
 	//act
 	_, err := suite.repository.GetAddressByID(1)
 
 	//assert
-	suite.Equal(gorm.ErrRecordNotFound, err)
+	suite.Equal(fmt.Errorf(ErrorAddressNotFound, 1), err)
 }
 
 func (suite *AddressRepositoryTestSuite) Test_GetAddressByID_Found_ReturnsAddressModel() {
 	//arrange
-	country1 := fixtures.GetCountry(uint(1))
-	region1 := fixtures.GetRegion(uint(1), uint(1), country1)
-	address1 := fixtures.GetAddress(uint(1), uint(1), region1)
-	suite.expectSelectByID(address1)
+	country1 := fixtures.GetCountry(1)
+	region1 := fixtures.GetRegion(1, 1, country1)
+	address1 := fixtures.GetAddress(1, 1, region1)
+	suite.db.Create(address1)
 
 	//act
-	address, err := suite.repository.GetAddressByID(1)
+	address, err := suite.repository.GetAddressByID(address1.ID)
 
 	//assert
 	suite.Nil(err)
+	tests.SyncDates(address, address1)
 	suite.Equal(address1, address)
 }
 
 func (suite *AddressRepositoryTestSuite) Test_CreateAddress_ReturnsCreatedRecord() {
 	//arrange
-	country1 := fixtures.GetCountry(uint(1))
-	region1 := fixtures.GetRegion(uint(1), uint(1), country1)
-	address1 := fixtures.GetAddress(uint(1), uint(1), region1)
-	suite.mock.
-		ExpectExec(`INSERT INTO "addresses"`).
-		WillReturnResult(sqlmock.NewResult(1, 1))
-	suite.expectSelectByID(address1)
+	country1 := fixtures.GetCountry(1)
+	region1 := fixtures.GetRegion(1, 1, country1)
+	address1 := fixtures.GetAddress(1, 1, region1)
 
 	//act
-	address, err := suite.repository.CreateAddress(fixtures.GetAddress(uint(0), uint(1), &models.Region{}))
+	address, err := suite.repository.CreateAddress(fixtures.GetAddress(0, 1, &models.Region{}))
 
 	//assert
 	suite.Nil(err)
-	address1.CreatedAt = address.CreatedAt
-	address1.UpdatedAt = address.UpdatedAt
+	tests.SyncDates(address, address1)
 	suite.Equal(address1, address)
 }
 
 func (suite *AddressRepositoryTestSuite) Test_DeleteAddress_NotFound_ReturnsNotFoundError() {
-	//arrange
-	suite.mock.
-		ExpectExec(`UPDATE "addresses" SET deleted_at=\? .+ \(\("id" = \?\)\)`).
-		WillReturnResult(sqlmock.NewResult(1, 0))
-
 	//act
 	err := suite.repository.DeleteAddress(1)
 
 	//assert
-	suite.Equal(gorm.ErrRecordNotFound, err)
+	suite.Equal(fmt.Errorf(ErrorAddressNotFound, 1), err)
 }
 
 func (suite *AddressRepositoryTestSuite) Test_DeleteAddress_Found_ReturnsNoError() {
 	//arrange
-	suite.mock.
-		ExpectExec(`UPDATE "addresses" SET deleted_at=\? .+ \(\("id" = \?\)\)`).
-		WillReturnResult(sqlmock.NewResult(1, 1))
+	country1 := fixtures.GetCountry(1)
+	region1 := fixtures.GetRegion(1, 1, country1)
+	address1 := fixtures.GetAddress(1, 1, region1)
+	suite.db.Create(address1)
 
 	//act
 	err := suite.repository.DeleteAddress(1)
 
 	//assert
 	suite.Nil(err)
-}
-
-func (suite *AddressRepositoryTestSuite) expectSelectByID(address *models.Address) {
-	addressRows := sqlmock.
-		NewRows(fixtures.GetAddressColumns()).
-		AddRow(fixtures.GetAddressRow(address)...)
-	suite.mock.
-		ExpectQuery(`SELECT .+ FROM "addresses" WHERE .+ \(\("id" = \?\)\) .+`).
-		WithArgs(address.ID).
-		WillReturnRows(addressRows)
-	regionRows := sqlmock.
-		NewRows(fixtures.GetRegionColumns()).
-		AddRow(fixtures.GetRegionRow(&address.Region)...)
-	suite.mock.
-		ExpectQuery(`SELECT .+ FROM "regions" WHERE \("id" = \?\)`).
-		WithArgs(address.Region.ID).
-		WillReturnRows(regionRows)
-	countryRows := sqlmock.
-		NewRows(fixtures.GetCountryColumns()).
-		AddRow(fixtures.GetCountryRow(&address.Region.Country)...)
-	suite.mock.
-		ExpectQuery(`SELECT .+ FROM "countries" WHERE \("id" = \?\)`).
-		WithArgs(address.Region.Country.ID).
-		WillReturnRows(countryRows)
 }
