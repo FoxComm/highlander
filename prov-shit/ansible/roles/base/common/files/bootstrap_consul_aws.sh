06c18280d36159c3bc231ab0957feaab7ce4a05c
Pavel Kiselev <kpashka@gmail.com>
2016-08-27 12:09:59 +0700

    Add consul bootstrap for AWS

---
+++ prov-shit/ansible/roles/base/common/files/bootstrap_consul_aws.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+CONSUL_DC=$1
+CONSUL_SERVER=$2
+PRIVATE_IP=$(curl -sf http://169.254.169.254/latest/meta-data/local-ipv4 | tr -d '\n')
+
+sudo bash -c "echo 'CONSUL_DC=$CONSUL_DC' >> /etc/consul.d/env"
+sudo bash -c "echo 'CONSUL_SERVER=$CONSUL_SERVER' >> /etc/consul.d/env"
+sudo bash -c "echo 'CONSUL_BIND=$PRIVATE_IP' >> /etc/consul.d/env"
