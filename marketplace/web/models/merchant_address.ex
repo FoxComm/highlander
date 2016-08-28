ad13397b2b48c9ab80b436419861ce73e8d083f4
Adil Wali <adil@adil.io>
2016-08-28 07:11:55 -0700

    Initial data model for merchant address.

--- marketplace/web/models/merchant_address.ex
+++ marketplace/web/models/merchant_address.ex
@@ -2,5 +2,17 @@ defmodule Marketplace.MerchantAddress do
   use Marketplace.Web, :model
 
   schema "merchant_addresses" do 
+    field :name, :string
+    field :address1, :string
+    field :address2, :string
+    field :city, :string
+    field :state, :string
+    field :zip, :string
+    field :is_headquarters, :boolean
+    field :phone_number, :string
+
+    timestamps
+
+    belongs_to :merchant, Marketplace.Merchant
   end
 end
