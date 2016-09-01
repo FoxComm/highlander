a0f50af4344812d474e9c544687b2b25bcf5c91d
Adil Wali <adil@adil.io>
2016-09-01 07:45:40 -0700

    Cleaning up user SQL.  Now migrates.

--- phoenix-scala/sql/V4.023__create_unified_user.sql
+++ phoenix-scala/sql/V4.023__create_unified_user.sql
@@ -4,14 +4,6 @@ create table organization_type
     name varchar(255)
 );
 
-create table organization_domains
-(
-    id integer primary key not null,
-    organization_id integer not null references organizations(id) on update restrict on delete restrict
-    domain generic_string not null, --used to tie users to an organization if the registration
-                                    --page requires an organization
-);
-
 create table organizations
 (
     id integer primary key not null,
@@ -21,41 +13,69 @@ create table organizations
     constraint organizations_organization_type_id_fk foreign key (organization_type_id) references organization_type (id)
 );
 
+create table organization_domains
+(
+    id integer primary key not null,
+    organization_id integer not null references organizations(id) on update restrict on delete restrict,
+    domain generic_string not null --used to tie users to an organization if the registration
+                                    --page requires an organization
+);
+
+CREATE TABLE resources (
+    id serial primary key not null,
+    name generic_string NOT NULL,
+    uri text not null, --used to inject into the claim
+    description TEXT
+);
+CREATE UNIQUE INDEX resources_id_uindex ON resources USING BTREE (id);
+
+CREATE TABLE actions (
+  id INTEGER PRIMARY KEY NOT NULL,
+  name CHARACTER VARYING(255) NOT NULL
+);
+CREATE UNIQUE INDEX actions_id_uindex ON actions USING BTREE (id);
+
+--These are the legal scopes in the system
+--Scopes are used to create claims for roles.
+create table scopes
+(
+    id integer primary key not null,
+    path exts.ltree not null,
+    parent_id integer references scopes(id) on update restrict on delete restrict
+);
+
 create table permissions
 (
     id integer primary key not null,
     resource_id integer not null,
     action_id integer not null,
-    scope_id varchar(255),
+    scope_id integer not null,
     constraint permissions_resources_id_fk foreign key (resource_id) references resources (id),
     constraint permissions_actions_id_fk foreign key (action_id) references actions (id),
     constraint permissions_scopes_id_fk foreign key (scope_id) references scopes (id)
 );
 
+create table roles
+(
+    id integer primary key not null,
+    name varchar(255) not null,
+    scope_id integer references scopes(id) on update restrict on delete restrict
+);
+
 create table role_permissions
 (
-    id integer default nextval('role_permissions_id_seq'::regclass) not null,
-    permission_id integer not null,
-    role_id integer not null,
-    constraint role_permissions_permissions_id_fk foreign key (permission_id) references permissions (id),
-    constraint role_permissions_roles_id_fk foreign key (role_id) references 
+    id serial primary key not null,
+    permission_id integer not null references permissions(id) on update restrict on delete restrict,
+    role_id integer not null references roles(id) on update restrict on delete restrict
 );
 
 create unique index role_permissions_id_permission_id_role_id_uindex on role_permissions (id, permission_id, role_id);
 
---These are the legal scopes in the system
---Scopes are used to create claims for roles. 
-create table scopes
+create table accounts
 (
     id integer primary key not null,
-    path exts.ltree not null
-);
-
-create table roles
-(
-    id integer primary key not null
-    name varchar(255) not null,
-    scope_id integer references scopes(id) on update restrict on delete restrict
+    name generic_string,
+    ratchet integer not null
 );
 
 create table account_roles
@@ -65,14 +85,7 @@ create table account_roles
     role_id integer not null references roles(id) on update restrict on delete restrict
 );
 
-create table accounts
-(
-    id integer primary key not null,
-    name generic_string,
-    ratchet integer not null,
-);
-
--- Accounts may have many access methods which includes a hashed password. 
+-- Accounts may have many access methods which includes a hashed password.
 -- normal users will have passwords while service accounts will have api keys.
 -- API keys have a key id and key secret.
 create table account_access_methods
@@ -91,6 +104,7 @@ create table account_organizations
     organization_id integer not null references organizations(id) on update restrict on delete restrict
 );
 
+
 create table users
 (
     id integer primary key,
@@ -105,20 +119,7 @@ create table users
     blacklisted_reason generic_string,
     created_at generic_timestamp,
     updated_at generic_timestamp,
-    deleted_at timestamp without time zone null,
-    foreign key (disabled_by) references store_admins(id) on update restrict on delete restrict
-);
-
-create table service
-(
-    id integer primary key,
---find and assign correct sku_id and shadow_id
-update order_line_items set sku_id = ols.sku_id, sku_shadow_id = ols.sku_shadow_id
-    from (select id, sku_id, sku_shadow_id from order_line_item_skus) as ols
-    where ols.id = origin_id;
-    account_id integer not null references accounts(id) on update restrict on delete restrict,
-    client_id generic_string,
-    description generic_string
+    deleted_at timestamp without time zone null
 );
 
 create table customers
@@ -138,7 +139,21 @@ create table store_admins
     id integer primary key,
     user_id integer not null references users(id) on update restrict on delete restrict,
     account_id integer not null references accounts(id) on update restrict on delete restrict,
-    state generic_string,
+    state generic_string
 );
 
+alter table users add foreign key (disabled_by) references store_admins(id) on update restrict on delete restrict;
+
+/* create table service
+(
+    id integer primary key,
+--find and assign correct sku_id and shadow_id
+update order_line_items set sku_id = ols.sku_id, sku_shadow_id = ols.sku_shadow_id
+    from (select id, sku_id, sku_shadow_id from order_line_item_skus) as ols
+    where ols.id = origin_id;
+    account_id integer not null references accounts(id) on update restrict on delete restrict,
+    client_id generic_string,
+    description generic_string
+);
+*/
 
