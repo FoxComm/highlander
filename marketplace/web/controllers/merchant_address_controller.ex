aeff4a838ba747402e335ed3b42dffc6fbd75b9f
Adil Wali <adil@adil.io>
2016-09-01 22:09:50 -0700

    Adding working create endpoint for merchant addresses.

--- marketplace/web/controllers/merchant_address_controller.ex
+++ marketplace/web/controllers/merchant_address_controller.ex
@@ -9,6 +9,23 @@ defmodule Marketplace.MerchantAddressController do
     render(conn, "index.json", merchant_addresses: merchant_addresses)
   end
 
+  def create(conn, %{"merchant_id" => merchant_id, "merchant_address" => merchant_address_params}) do
+    changeset = MerchantAddress.changeset(%MerchantAddress{merchant_id: String.to_integer(merchant_id)}, merchant_address_params)
+
+    case Repo.insert(changeset) do 
+      {:ok, merchant_address} -> 
+        conn
+        |> put_status(:created)
+        |> put_resp_header("location", merchant_merchant_address_path(conn, :show, merchant_id, merchant_address))
+        |> render("merchant_address.json", merchant_address: merchant_address)
+      {:error, changeset} -> 
+        conn
+        |> put_status(:unprocessable_entity)
+        |> render(Marketplace.ChangesetView, "errors.json", changeset: changeset)
+
+    end
+  end
+
   defp merchant_addresses(merchant_id) do
     merchant = Repo.get!(Merchant, merchant_id)
     assoc(merchant, :merchant_addresses)
--- marketplace/web/models/merchant_address.ex
+++ marketplace/web/models/merchant_address.ex
@@ -8,11 +8,16 @@ defmodule Marketplace.MerchantAddress do
     field :city, :string
     field :state, :string
     field :zip, :string
-    field :is_headquarters, :boolean
+    field :is_headquarters, :boolean, default: false
     field :phone_number, :string
 
     timestamps
 
     belongs_to :merchant, Marketplace.Merchant
+  end
+
+  def changeset(model, params \\ :empty) do
+    model 
+    |> cast(params, ~w(name address1 city state zip), ~w(address2 is_headquarters phone_number))
   end
 end
