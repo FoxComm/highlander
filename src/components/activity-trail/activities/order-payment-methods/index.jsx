60e83a072a389ac10036e030084c5db708f5c8f4
Sergey Rublev <sergey@foxcommerce.com>
2016-09-02 18:55:30 +0700

    Fix activities handling after splitting order to order|cart

--- src/components/activity-trail/activities/order-payment-methods/index.jsx
+++ src/components/activity-trail/activities/order-payment-methods/index.jsx
@@ -17,50 +17,55 @@ const paymentMethodTitles = {
 const representatives = {
   [types.CART_PAYMENT_METHOD_ADDED_CREDIT_CARD]: {
     title: (data, activity) => {
+      const order = data.order || data.cart;
       return (
         <Title activity={activity}>
           <strong>added credit card</strong> ending in {data.creditCard.lastFour}
-          &nbsp;as payment method for <OrderTarget order={data.order} />
+          &nbsp;as payment method for <OrderTarget order={order} />
         </Title>
       );
     },
   },
   [types.CART_PAYMENT_METHOD_ADDED_GIFT_CARD]: {
     title: (data, activity) => {
+      const order = data.order || data.cart;
       return (
         <Title activity={activity}>
           <strong>added gift card</strong> with code <GiftCardLink {...data.giftCard} />
-          &nbsp;as payment method for <OrderTarget order={data.order} />
+          &nbsp;as payment method for <OrderTarget order={order} />
         </Title>
       );
     },
   },
   [types.CART_PAYMENT_METHOD_DELETED_GIFT_CARD]: {
     title: (data, activity) => {
+      const order = data.order || data.cart;
       return (
         <Title activity={activity}>
           <strong>removed gift card</strong> with code <GiftCardLink {...data.giftCard} />
-          &nbsp;from payment method for <OrderTarget order={data.order} />
+          &nbsp;from payment method for <OrderTarget order={order} />
         </Title>
       );
     },
   },
   [types.CART_PAYMENT_METHOD_ADDED_STORE_CREDIT]: {
     title: (data, activity) => {
+      const order = data.order || data.cart;
       return (
         <Title activity={activity}>
           <strong>added store credit</strong> with amount <Currency value={data.amount} />
-          &nbsp;as payment method for <OrderTarget order={data.order} />
+          &nbsp;as payment method for <OrderTarget order={order} />
         </Title>
       );
     },
   },
   [types.CART_PAYMENT_METHOD_DELETED]: {
     title: (data, activity) => {
+      const order = data.order || data.cart;
       return (
         <Title activity={activity}>
           <strong>removed payment method</strong> {paymentMethodTitles[data.pmt]}
-          &nbsp;from <OrderTarget order={data.order} />
+          &nbsp;from <OrderTarget order={order} />
         </Title>
       );
     },
