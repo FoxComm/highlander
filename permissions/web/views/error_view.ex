d87b281779ca2ae4207e5598f1a3fcd6714984ca
Adil Wali <adil@adil.io>
2016-09-02 08:35:13 -0700

    Clarifying 404 error.

--- permissions/web/views/error_view.ex
+++ permissions/web/views/error_view.ex
@@ -2,7 +2,7 @@ defmodule Permissions.ErrorView do
   use Permissions.Web, :view
 
   def render("404.json", _assigns) do
-    %{errors: %{detail: "Page not found"}}
+    %{errors: %{detail: "Resource not found"}}
   end
 
   def render("500.json", _assigns) do
