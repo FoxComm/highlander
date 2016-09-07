6d46a11f4f87966ef6ffe2db826ce918f13e5633
Sergey Rublev <sergey@foxcommerce.com>
2016-09-08 06:25:36 +0700

    Drop mat. views for notes_search_view

--- prov-shit/ansible/roles/dev/db/files/update_materialized_views.sql
+++ prov-shit/ansible/roles/dev/db/files/update_materialized_views.sql
@@ -1,6 +1,3 @@
--- orders
-refresh materialized view concurrently order_stats_view;
-
 -- gift_card_transactions
 refresh materialized view concurrently gift_card_transactions_admins_view;
 refresh materialized view concurrently gift_card_transactions_payments_view;
@@ -13,14 +10,3 @@ refresh materialized view concurrently store_credit_transactions_view;
 
 -- failed authorizations
 refresh materialized view concurrently failed_authorizations_search_view;
-
--- notes
-refresh materialized view concurrently notes_orders_view;
-refresh materialized view concurrently notes_admins_view;
-refresh materialized view concurrently notes_customers_view;
-refresh materialized view concurrently notes_gift_cards_view;
-refresh materialized view concurrently notes_skus_view;
-refresh materialized view concurrently notes_products_view;
-refresh materialized view concurrently notes_promotions_view;
-refresh materialized view concurrently notes_coupons_view;
-refresh materialized view concurrently notes_search_view;
