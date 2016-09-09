171376c92174de217b207c6807e28c4efda4cdf9
Adil Wali <adil@adil.io>
2016-09-09 13:56:57 -0700

    Rendering only role upon creation.  No nested permissions yet.

--- permissions/web/controllers/role_controller.ex
+++ permissions/web/controllers/role_controller.ex
@@ -38,7 +38,7 @@ defmodule Permissions.RoleController do
     case Repo.update(changeset) do
       {:ok, role} -> 
         conn
-        |> render("show.json", role: role)
+        |> render("role.json", role: role)
       {:error, changeset} -> 
         conn
         |> put_status(:unprocessable_entity)
--- permissions/web/views/role_view.ex
+++ permissions/web/views/role_view.ex
@@ -4,18 +4,25 @@ defmodule Permissions.RoleView do
   alias Permissions.PermissionView
 
   def render("index.json", %{roles: roles}) do
-    %{roles: render_many(roles, RoleView, "role.json")}
+    %{roles: render_many(roles, RoleView, "role_with_permissions.json")}
   end
 
   def render("show.json", %{role: role}) do
-    %{role: render_one(role, RoleView, "role.json")}
+    %{role: render_one(role, RoleView, "role_with_permissions.json")}
   end
 
-  def render("role.json", %{role: role}) do
+  def render("role_with_permissions.json", %{role: role}) do
     %{id: role.id,
       name: role.name,
       scope_id: role.scope_id, 
       granted_permissions: render_many(role.permissions, PermissionView, "permission.json")
     }
   end
+
+  def render("role.json", %{role: role}) do
+    %{id: role.id,
+      name: role.name,
+      scope_id: role.scope_id
+    }
+  end 
 end
