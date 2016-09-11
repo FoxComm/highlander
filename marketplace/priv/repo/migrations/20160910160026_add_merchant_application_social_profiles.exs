b72c5e97ba4bff9c951b60d8c8a67fbd089e5891
Adil Wali <adil@adil.io>
2016-09-10 17:07:37 -0700

    Adding unique constraint ma_sp to enforce has_one.

--- marketplace/priv/repo/migrations/20160910160026_add_merchant_application_social_profiles.exs
+++ marketplace/priv/repo/migrations/20160910160026_add_merchant_application_social_profiles.exs
@@ -7,5 +7,6 @@ defmodule Marketplace.Repo.Migrations.AddMerchantApplicationSocialProfiles do
       add :social_profile_id, references(:social_profiles)
     end
 
+    create unique_index(:merchant_application_social_profiles, [:merchant_application_id])
   end
 end
