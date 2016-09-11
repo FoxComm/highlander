6edbd2de78ada8ff449b8162027fdfca011c4091
Adil Wali <adil@adil.io>
2016-09-10 18:27:25 -0700

    Adding unique constraint for mapping to business profile.

--- marketplace/priv/repo/migrations/20160910160019_add_merchant_application_business_profiles.exs
+++ marketplace/priv/repo/migrations/20160910160019_add_merchant_application_business_profiles.exs
@@ -6,5 +6,7 @@ defmodule Marketplace.Repo.Migrations.AddMerchantApplicationBusinessProfiles do
       add :merchant_application_id, references(:merchant_applications)
       add :business_profile_id, references(:business_profiles)
     end
+
+    create unique_index(:merchant_application_business_profiles, [:merchant_application_id])
   end
 end
