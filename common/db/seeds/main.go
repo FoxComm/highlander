6c5fd502dc00eca4aa07fd23003e7f4ade3255f6
Jeff Mataya <jeff@foxcommerce.com>
2016-09-12 11:45:12 -0700

    Clean up seeder

--- common/db/seeds/main.go
+++ common/db/seeds/main.go
@@ -27,68 +27,55 @@ func createShippingMethods(db *gorm.DB) error {
 	log.Printf("Seeding carriers...")
 	carrierRepo := repositories.NewCarrierRepository(db)
 
-	carrierUSPS := &models.Carrier{
-		Name:             "USPS",
-		TrackingTemplate: "http://www.stamps.com/shipstatus/submit/?confirmation=",
-	}
-
-	carrierFedEx := &models.Carrier{
-		Name:             "FedEx",
-		TrackingTemplate: "http://www.fedex.com/Tracking?action=track&tracknumbers=",
+	carriers := []*models.Carrier{
+		&models.Carrier{
+			Name:             "USPS",
+			TrackingTemplate: "http://www.stamps.com/shipstatus/submit/?confirmation=",
+		},
+		&models.Carrier{
+			Name:             "FedEx",
+			TrackingTemplate: "http://www.fedex.com/Tracking?action=track&tracknumbers=",
+		},
 	}
 
 	var err error
-	carrierUSPS, err = carrierRepo.CreateCarrier(carrierUSPS)
-	if err != nil {
-		return err
-	}
-
-	carrierFedEx, err = carrierRepo.CreateCarrier(carrierFedEx)
-	if err != nil {
-		return err
+	for idx, carrier := range carriers {
+		carriers[idx], err = carrierRepo.CreateCarrier(carrier)
+		if err != nil {
+			return err
+		}
 	}
 
 	log.Printf("Seeding shipping methods...")
 	shippingRepo := repositories.NewShippingMethodRepository(db)
 
-	standardShipping := &models.ShippingMethod{
-		CarrierID: carrierUSPS.ID,
-		Name:      "Standard shipping",
-		Code:      "STANDARD",
+	shippingMethods := []*models.ShippingMethod{
+		&models.ShippingMethod{
+			CarrierID: carriers[0].ID,
+			Name:      "Standard shipping",
+			Code:      "STANDARD",
+		},
+		&models.ShippingMethod{
+			CarrierID: carriers[0].ID,
+			Name:      "Standard shipping",
+			Code:      "STANDARD-FREE",
+		},
+		&models.ShippingMethod{
+			CarrierID: carriers[1].ID,
+			Name:      "2-3 day express",
+			Code:      "EXPRESS",
+		},
+		&models.ShippingMethod{
+			CarrierID: carriers[1].ID,
+			Name:      "Overnight shipping",
+			Code:      "OVERNIGHT",
+		},
 	}
 
-	if _, err = shippingRepo.CreateShippingMethod(standardShipping); err != nil {
-		return err
-	}
-
-	standardShippingFree := &models.ShippingMethod{
-		CarrierID: carrierUSPS.ID,
-		Name:      "Standard shipping",
-		Code:      "STANDARD-FREE",
-	}
-
-	if _, err = shippingRepo.CreateShippingMethod(standardShippingFree); err != nil {
-		return err
-	}
-
-	expressShipping := &models.ShippingMethod{
-		CarrierID: carrierFedEx.ID,
-		Name:      "2-3 day express",
-		Code:      "EXPRESS",
-	}
-
-	if _, err = shippingRepo.CreateShippingMethod(expressShipping); err != nil {
-		return err
-	}
-
-	overnightShipping := &models.ShippingMethod{
-		CarrierID: carrierFedEx.ID,
-		Name:      "Overnight shipping",
-		Code:      "OVERNIGHT",
-	}
-
-	if _, err = shippingRepo.CreateShippingMethod(overnightShipping); err != nil {
-		return err
+	for _, shippingMethod := range shippingMethods {
+		if _, err = shippingRepo.CreateShippingMethod(shippingMethod); err != nil {
+			return err
+		}
 	}
 
 	return nil
