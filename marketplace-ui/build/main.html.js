8ccf76a3e381a3dff8cbec942c26254bbaaae20a
tony.pizzicato <tony.grebnev@gmail.com>
2016-09-13 18:59:08 +0300

    build dir removed

--- marketplace-ui/build/main.html.js
+++
@@ -1,4 +0,0 @@
-module.exports = function anonymous(it
-/**/) {
-var out='<!doctype html><html> <head> <title>Marketplace UI</title> <meta name="viewport" content="width=device-width"> <link rel="stylesheet" href="/app.css" /> </head> <body> <div id="app">'+( it.html )+'</div> <script charset="UTF-8"> window.initialState='+( it.state )+'; </script> <script type="text/javascript" src="/app.js"></script> <script type="text/javascript"> App.renderApp(); </script> </body></html>';return out;
-}
