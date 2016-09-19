df37658ab1a63158861f0b4cf8859ae23f889f14
Alex Kreskiyan <a.kreskiyan@gmail.com>
2016-09-19 17:10:05 +0300

    Added libssl-dev test

--- prov-shit/tools/caniuse/provision/__init__.py
+++ prov-shit/tools/caniuse/provision/__init__.py
@@ -1,6 +1,7 @@
 import ansible
 import ansible_lint
 import build_essential
+import libssl_dev
 import pip
 from caniuse.common import run_tests
 
@@ -8,6 +9,7 @@ from caniuse.common import run_tests
 def caniuse():
     run_tests("provisioning", [
         build_essential.check(),
+        libssl_dev.check(),
         pip.check(),
         ansible.check(),
         ansible_lint.check(),
