c54c11723681d475b50337259598451dad683be5
Pavel Kiselev <kpashka@gmail.com>
2016-09-20 17:49:03 +0300

    Fix highlander gatling SSL (#61)
    * Fix highlander gatling SSL
    
    * Remove useless sleep

--- projects.sh
+++ projects.sh
@@ -10,14 +10,6 @@ DEFAULT="ashes firebrand fox-notifications green-river integration-tests isaac m
 BASE_URL="https://api.github.com/repos/FoxComm/highlander"
 FULL_URL="$BASE_URL/pulls?head=FoxComm:$BUILDKITE_BRANCH&access_token=$GITHUB_API_TOKEN"
 
-# Sleep before Github API returns proper description
-if [ $BUILDKITE_BRANCH = "master" ]
-then
-  sleep 0
-else
-  sleep 3
-fi
-
 # Call the API
 OUTPUT=$(curl -sf $FULL_URL | grep -o "PROJECTS=(.*)" | tr -d '(' | tr -d 'PROJECTS=' | tr -d ')')
 
