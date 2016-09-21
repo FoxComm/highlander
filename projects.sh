da8a592964edd7688585dbd9c1e978de87c845b7
Alex Kreskiyan <a.kreskiyan@gmail.com>
2016-09-21 18:20:44 +0300

    Fixed projects.sh

--- projects.sh
+++ projects.sh
@@ -16,7 +16,7 @@ PROJECTS=(
     'prov-shit'
 )
 
-BASE_URL="https://api.github.com/repos/"
+BASE_URL="https://api.github.com/repos"
 
 # get ORG/REPO string from url and split it
 FULL_REPO=$(git config --get remote.origin.url | cut -d ':' -f2 | cut -d '.' -f1)
@@ -24,21 +24,25 @@ ORG=$(echo $FULL_REPO | cut -d '/' -f1)
 REPO=$(echo $FULL_REPO | cut -d '/' -f2)
 
 # get base branch through PR info
-FULL_URL="$BASE_URL/pulls?head=$ORG:$BUILDKITE_BRANCH&access_token=$GITHUB_API_TOKEN"
-BASE_BRANCH=$(curl -sf $FULL_URL | jq -r '.[0] | .base.ref')
+FULL_URL="$BASE_URL/$FULL_REPO/pulls?head=$ORG:$BUILDKITE_BRANCH&access_token=$GITHUB_API_TOKEN"
+
+#BASE_BRANCH=$(curl -sf $FULL_URL | jq -r '.[0] | .base.ref')
+BASE_BRANCH=master
 
 # get changed base paths in HEAD realtively to base path
 ALL_CHANGED=$(git diff --name-only $BASE_BRANCH..HEAD | cut -d'/' -f1 | uniq)
 
 # make newlines the only separator 
-IFS=$'\n'       
+IFS=$'\n'
+ALL_CHANGED=($ALL_CHANGED)
+unset IFS
+
+# get changed projects
 CHANGED=()
-for $CHANGE in $ALL_CHANGED
-do
+for CHANGE in ${ALL_CHANGED[@]}; do
     if [[ ${PROJECTS[@]} =~ $CHANGE ]]; then
         CHANGED+=($CHANGE)
     fi
 done
-unset IFS
 
-echo $CHANGED
+echo ${CHANGED[@]}
