cbdd5828e43d4c0cba1a3a4c006ddbcaef5f2952
Jeff Mataya <jeff@foxcommerce.com>
2016-09-25 16:16:23 -0700

    Move decoding of the payload

--- integrations/goldfish/main.go
+++ integrations/goldfish/main.go
@@ -8,8 +8,6 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-const searchQuery = `{"query":{"bool":{"filter":[{"missing":{"field":"archivedAt"}},{"term":{"externalId":"%s"}}]}}}`
-
 type ProductQuery struct {
 	Query string `json:"query"`
 }
@@ -18,6 +16,43 @@ func createError(msg string) map[string][]string {
 	return map[string][]string{
 		"errors": []string{msg},
 	}
+}
+
+func createQueryFilter(context, field, value string) map[string]interface{} {
+	return map[string]interface{}{
+		"query": map[string]interface{}{
+			"bool": map[string]interface{}{
+				"filter": []map[string]interface{}{
+					map[string]interface{}{
+						"missing": map[string]string{
+							"field": "archivedAt",
+						},
+					},
+					map[string]interface{}{
+						"term": map[string]string{
+							field: value,
+						},
+					},
+					map[string]interface{}{
+						"term": map[string]string{
+							"context": context,
+						},
+					},
+				},
+			},
+		},
+	}
+}
+
+type Product map[string]interface{}
+
+type ElasticResult struct {
+	Pagination ElasticPagination
+	Result     []Product
+}
+
+type ElasticPagination struct {
+	Total int
 }
 
 func main() {
@@ -37,43 +72,9 @@ func main() {
 			return
 		}
 
-		// var payload map[string]interface{}
-		// if err := c.Bind(&payload); err != nil {
-		// 	c.JSON(500, gin.H{
-		// 		"errors": []string{"Unexpected error parsing payload"},
-		// 	})
-		// 	return
-		// }
-
-		// query := fmt.Sprintf(searchQuery, externalID)
-		// query := make(map[string]interface{})
-
-		query := map[string]interface{}{
-			"query": map[string]interface{}{
-				"bool": map[string]interface{}{
-					"filter": []map[string]interface{}{
-						map[string]interface{}{
-							"missing": map[string]string{
-								"field": "archivedAt",
-							},
-						},
-						map[string]interface{}{
-							"term": map[string]string{
-								"externalId": externalID,
-							},
-						},
-						map[string]interface{}{
-							"term": map[string]string{
-								"context": context,
-							},
-						},
-					},
-				},
-			},
-		}
-
-		// const searchQuery = `{"query":{"bool":{"filter":[{"missing":{"field":"archivedAt"}},{"term":{"externalId":"%s"}}]}}}`
+		query := createQueryFilter(context, "externalId", externalID)
 		url := "https://admin-tgt-0916.foxcommerce.com/api/search/admin/products_search_view/_search?size=50"
+
 		resp, err := consumers.Post(url, headers, &query)
 		if err != nil {
 			c.JSON(500, createError("Unexpected error querying for products"))
@@ -81,20 +82,48 @@ func main() {
 		}
 
 		defer resp.Body.Close()
-		var payload map[string]interface{}
 
-		if err := json.NewDecoder(resp.Body).Decode(&payload); err != nil {
+		if resp.StatusCode < 200 || resp.StatusCode > 299 {
+			var payload map[string]interface{}
+
+			if err := json.NewDecoder(resp.Body).Decode(&payload); err != nil {
+				msg := fmt.Sprintf("Error reading the product with %s", err.Error())
+				c.JSON(500, createError(msg))
+				return
+			}
+			c.JSON(resp.StatusCode, payload)
+			return
+		}
+
+		esResult := new(ElasticResult)
+		if err := json.NewDecoder(resp.Body).Decode(esResult); err != nil {
 			msg := fmt.Sprintf("Error reading the product with %s", err.Error())
 			c.JSON(500, createError(msg))
 			return
 		}
 
-		if resp.StatusCode < 200 || resp.StatusCode > 299 {
-			c.JSON(resp.StatusCode, payload)
+		if esResult.Pagination.Total == 0 {
+			c.JSON(404, createError("Product not found"))
 			return
 		}
 
-		c.JSON(200, payload)
+		if esResult.Pagination.Total > 1 {
+			msg := fmt.Sprintf(
+				"Error selecting product - expected 1 found %d",
+				esResult.Pagination.Total,
+			)
+			c.JSON(400, createError(msg))
+			return
+		}
+
+		product := esResult.Result[0]
+		productID, ok := product["productId"]
+		if !ok {
+			c.JSON(500, createError("Unable to get productId from product"))
+			return
+		}
+
+		c.JSON(200, gin.H{"productID": productID})
 	})
 
 	r.Run(":5492")
