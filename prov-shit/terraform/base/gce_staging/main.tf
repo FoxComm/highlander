ab4c1e764f7da683cfb34d732d39d2dc7303b644
Jeff Mataya <jeff@foxcommerce.com>
2016-09-24 17:32:57 -0700

    Fix typo that prevents consul from starting
    Fixed a typo when calling bootstrap_consul that prevented consul from
    getting set up correctly when bootstrapping the amigos on stage.

--- prov-shit/terraform/base/gce_staging/main.tf
+++ prov-shit/terraform/base/gce_staging/main.tf
@@ -23,7 +23,7 @@ provider "google"
 ##############################################
 module "consul_cluster" {
     source = "../../modules/gce/amigos"
-    datacenter = "foxcomm-staging"
+    datacenter = "foxcomm-stage"
     network = "default"
     servers = 3
     image = "${var.consul_cluster_image}"
@@ -34,46 +34,46 @@ module "consul_cluster" {
 ##############################################
 # Setup Gatling Machines
 ##############################################
-module "foxcomm-gatling" {
-    source = "../../modules/gce/tinystack"
-    datacenter = "foxcomm-gatling"
-    backend_image = "${var.tiny_backend_image}"
-    frontend_image = "${var.tiny_frontend_image}"
-    ssh_user = "${var.ssh_user}"
-    ssh_private_key = "${var.ssh_private_key}"
-    consul_leader = "${module.consul_cluster.leader}"
-    consul_server_image = "${var.consul_server_image}"
-}
+# module "foxcomm-gatling" {
+#     source = "../../modules/gce/tinystack"
+#     datacenter = "foxcomm-gatling"
+#     backend_image = "${var.tiny_backend_image}"
+#     frontend_image = "${var.tiny_frontend_image}"
+#     ssh_user = "${var.ssh_user}"
+#     ssh_private_key = "${var.ssh_private_key}"
+#     consul_leader = "${module.consul_cluster.leader}"
+#     consul_server_image = "${var.consul_server_image}"
+# }
 
-resource "google_compute_instance" "foxcomm-gatling-gun" {
-    name = "foxcomm-gatling-gun"
-    machine_type = "n1-standard-2"
-    tags = ["no-ip", "foxcomm-gatling-gun"]
-    zone = "us-central1-a"
+# resource "google_compute_instance" "foxcomm-gatling-gun" {
+#     name = "foxcomm-gatling-gun"
+#     machine_type = "n1-standard-2"
+#     tags = ["no-ip", "foxcomm-gatling-gun"]
+#     zone = "us-central1-a"
 
-    disk {
-        image = "${var.gatling_image}"
-        type = "pd-ssd"
-        size = "30"
-    }
+#     disk {
+#         image = "${var.gatling_image}"
+#         type = "pd-ssd"
+#         size = "30"
+#     }
 
-    network_interface {
-        network = "default"
-    }
+#     network_interface {
+#         network = "default"
+#     }
 
-    connection {
-        type = "ssh"
-        user = "${var.ssh_user}"
-        private_key = "${file(var.ssh_private_key)}"
-    }
+#     connection {
+#         type = "ssh"
+#         user = "${var.ssh_user}"
+#         private_key = "${file(var.ssh_private_key)}"
+#     }
 
-    provisioner "remote-exec" {
-        inline = [
-          "/usr/local/bin/bootstrap.sh",
-          "/usr/local/bin/bootstrap_consul.sh foxcomm-gatling ${module.foxcomm-gatling.consul_address}"
-        ]
-    }
-}
+#     provisioner "remote-exec" {
+#         inline = [
+#           "/usr/local/bin/bootstrap.sh",
+#           "/usr/local/bin/bootstrap_consul.sh foxcomm-gatling ${module.foxcomm-gatling.consul_address}"
+#         ]
+#     }
+# }
 
 ##############################################
 # Setup Staging
