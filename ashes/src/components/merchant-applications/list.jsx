e5f04d72e2ad516bd8db51287d96a0179df70d21
Jeff Mataya <jeff@foxcommerce.com>
2016-09-26 01:55:28 -0700

    Base pages with API calls for merchant application

---
+++ ashes/src/components/merchant-applications/list.jsx
@@ -0,0 +1,9 @@
+/* @flow */
+
+import React, { Component, Element } from 'react';
+
+export default class MerchantApplicationsList extends Component {
+  render(): Element {
+    return <div>List!</div>;
+  }
+}
