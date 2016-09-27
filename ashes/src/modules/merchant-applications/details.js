8e3be3f997e876ad642839f506052d4f6399a841
Jeff Mataya <jeff@foxcommerce.com>
2016-09-26 21:30:04 -0700

    Add the PUT to approve the application

---
+++ ashes/src/modules/merchant-applications/details.js
@@ -0,0 +1,40 @@
+/* @flow */
+
+import { createReducer } from 'redux-act';
+import Api from 'lib/api';
+import createAsyncActions from 'modules/async-utils';
+
+import type { MerchantApplication } from 'paragons/merchant-application';
+
+type State = {
+  application: ?MerchantApplication,
+};
+
+const initialState = {
+  application: null,
+}
+
+const _getApplication = createAsyncActions(
+  'getApplication',
+  (id: number) => Api.get(`/mkt/merchant_applications/${id}`)
+);
+
+const _updateApplication = createAsyncActions(
+  'updateApplication',
+  (id: number, data: MerchantApplication) => Api.put(`/mkt/merchant_applications/${id}`, data)
+);
+
+export const fetchApplication = _getApplication.perform;
+export const updateApplication = _updateApplication.perform;
+
+function applicationSucceeded(state: State, payload: Object): State {
+  const application = payload.merchant_application || payload;
+  return { ...state, application };
+}
+
+const reducer = createReducer({
+  [_getApplication.succeeded]: applicationSucceeded,
+  [_updateApplication.succeeded]: applicationSucceeded,
+}, initialState);
+
+export default reducer;
