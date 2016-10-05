89e5400c99bf3d95283e29623b94cc532c7ba80b
Josh Ferrerra <josh@foxcommerce.com>
2016-10-05 15:17:45 -0400

    Add function to grant role from marketplace

--- marketplace/web/services/permissions_manager.ex
+++ marketplace/web/services/permissions_manager.ex
@@ -38,10 +38,39 @@ defmodule Marketplace.PermissionManager do
 
     case HTTPoison.post("#{full_perm_path}/scopes/#{scope_id}/admin_role", post_body, post_headers) do
       {:ok, %HTTPoison.Response{status_code: 201, body: body}} ->
-        case Poison.decode(body) do 
-        {:ok, decoded_body} -> 
+        case Poison.decode(body) do
+        {:ok, decoded_body} ->
           Map.fetch!(decoded_body, "role")
           |> Map.fetch!("id")
+        {:error, decoded_body} ->
+          # TODO: Probably a good idea to write this to a queue.
+          # To retry when solomon is back up.
+          nil
+        end
+      {:error, %HTTPoison.Error{reason: reason}} ->
+        IO.inspect("ERROR FROM HTTP CLIENT!")
+        IO.inspect(reason)
+        nil
+    end
+  end
+
+  # grants account with a role over HTTP then returns the role_id
+  def grant_account_id_role_id(account_id, role_id) do
+    HTTPoison.start
+    post_body = %{
+      granted_role: %{
+        role_id: role_id
+      }
+    }
+    |> Poison.encode!
+    post_headers = [{'content-type', 'application/json'}]
+
+    case HTTPoison.post("#{full_perm_path}/accounts/#{account_id}/granted_roles", post_body, post_headers) do
+      {:ok, %HTTPoison.Response{status_code: 201, body: body}} ->
+        case Poison.decode(body) do 
+        {:ok, decoded_body} -> 
+          Map.fetch!(decoded_body, "granted_role")
+          |> Map.fetch!("role_id")
         {:error, decoded_body} -> 
           # TODO: Probably a good idea to write this to a queue.  
           # To retry when solomon is back up.
