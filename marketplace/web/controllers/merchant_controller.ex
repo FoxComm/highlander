988cf36fdb4d9037c0297e1d7f94584ca50d917a
Josh Ferrerra <josh@foxcommerce.com>
2016-10-05 15:19:16 -0400

    Creates an admin user in solomon when application is activated

--- marketplace/web/controllers/merchant_controller.ex
+++ marketplace/web/controllers/merchant_controller.ex
@@ -3,6 +3,7 @@ defmodule Marketplace.MerchantController do
   alias Ecto.Multi
   alias Marketplace.Repo
   alias Marketplace.Merchant
+  alias Marketplace.MerchantAccount
   alias Marketplace.MerchantApplication
   alias Marketplace.MerchantApplicationBusinessProfile
   alias Marketplace.MerchantApplicationSocialProfile
@@ -65,6 +66,20 @@ defmodule Marketplace.MerchantController do
         scope_id = PermissionManager.create_scope
         organization_id = PermissionManager.create_organization_from_merchant_application(ma, scope_id)
 
+        merchant_account_params = %{
+          first_name: ma.business_name,
+          last_name: "admin",
+          email_address: ma.email_address,
+          password: "password" # probably not
+        }
+
+        solomon_id = PermissionManager.create_user_from_merchant_account(merchant_account_params)
+        role_id = PermissionManager.create_admin_role_from_scope_id(scope_id)
+        PermissionManager.grant_account_id_role_id(solomon_id, role_id)
+        merchant_account_cs = MerchantAccount.changeset(
+          %MerchantAccount{solomon_id: solomon_id}, merchant_account_params
+        )
+
         merchant = %{
           name: ma.name,
           business_name: ma.name, 
@@ -75,8 +90,7 @@ defmodule Marketplace.MerchantController do
           organization_id: organization_id
         } 
         merchant_cs = Merchant.changeset(%Merchant{}, merchant)
-    
-        
+
         multi_txn = Multi.new
         |> Multi.insert(:merchant, merchant_cs)
         |> Multi.run(:merchant_business_profile, fn %{merchant: merchant} -> 
@@ -85,10 +99,10 @@ defmodule Marketplace.MerchantController do
           copy_social_profile_from_merchant_application(application_id, merchant) end)
         |> Multi.run(:merchant_application, fn %{merchant: merchant} ->
           update_merchant_application(application_id, merchant) end)
-
+        |> Multi.insert(:merchant_account, merchant_account_cs)
 
         case Repo.transaction(multi_txn) do
-          {:ok, %{merchant: inserted_merchant, merchant_business_profile: m_bp, merchant_social_profile: m_sp}} -> 
+          {:ok, %{merchant: inserted_merchant, merchant_business_profile: m_bp, merchant_social_profile: m_sp, merchant_account: _}} -> 
             conn 
             |> put_status(:created)
             |> put_resp_header("location", merchant_path(conn, :show, inserted_merchant))
