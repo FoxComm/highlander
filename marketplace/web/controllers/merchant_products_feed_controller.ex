0997c427cb0e2a22c04b553359d27ba9f272b6e1
Josh Ferrerra <josh@foxcommerce.com>
2016-10-06 16:33:28 -0400

    Control products_feed through merchant_products_feed

--- marketplace/web/controllers/merchant_products_feed_controller.ex
+++ marketplace/web/controllers/merchant_products_feed_controller.ex
@@ -1,6 +1,8 @@
 defmodule Marketplace.MerchantProductsFeedController do
   use Marketplace.Web, :controller
 
+  alias Ecto.Multi
+  alias Marketplace.ProductsFeed
   alias Marketplace.MerchantProductsFeed
 
   def index(conn, _params) do
@@ -8,48 +10,59 @@ defmodule Marketplace.MerchantProductsFeedController do
     render(conn, "index.json", merchant_products_feeds: merchant_products_feeds)
   end
 
-  def create(conn, %{"merchant_products_feed" => merchant_products_feed_params}) do
-    changeset = MerchantProductsFeed.changeset(%MerchantProductsFeed{}, merchant_products_feed_params)
-
-    case Repo.insert(changeset) do
-      {:ok, merchant_products_feed} ->
+  def create(conn, %{"products_feed" => products_feed_params, "merchant_id" => merchant_id}) do
+    case Repo.transaction(insert_and_relate(products_feed_params, merchant_id)) do
+      {:ok, %{products_feed: products_feed, merchant_products_feed: m_pf}} ->
         conn
         |> put_status(:created)
-        |> put_resp_header("location", merchant_products_feed_path(conn, :show, merchant_products_feed))
-        |> render("show.json", merchant_products_feed: merchant_products_feed)
-      {:error, changeset} ->
+        |> put_resp_header("location", merchant_products_feed_path(conn, :show, merchant_id))
+        |> render(ProductsFeedView, "products_feed.json", products_feed: products_feed)
+      {:error, failed_operation, failed_value, changes_completed} ->
         conn
         |> put_status(:unprocessable_entity)
-        |> render(Marketplace.ChangesetView, "error.json", changeset: changeset)
+        |> render(Marketplace.ChangesetView, "errors.json", changeset: failed_value)
     end
   end
 
-  def show(conn, %{"id" => id}) do
-    merchant_products_feed = Repo.get!(MerchantProductsFeed, id)
-    render(conn, "show.json", merchant_products_feed: merchant_products_feed)
+  def show(conn, %{"merchant_id" => m_id}) do
+    m_pf = Repo.get_by!(MerchantProductsFeed, merchant_id: m_id)
+    |> Repo.preload(:products_feed)
+
+    conn
+    |> render(ProductsFeedView, "show.json", products_feed: m_pf.products_feed)
   end
 
-  def update(conn, %{"id" => id, "merchant_products_feed" => merchant_products_feed_params}) do
-    merchant_products_feed = Repo.get!(MerchantProductsFeed, id)
-    changeset = MerchantProductsFeed.changeset(merchant_products_feed, merchant_products_feed_params)
+  def update(conn, %{"merchant_id" => m_id, "products_feed" => products_feed_params}) do
+    m_pf = Repo.get_by!(MerchantProductsFeed, merchant_id: m_id)
+    |> Repo.preload(:products_feed)
 
+    changeset = ProductsFeed.update_changeset(m_pf.products_feed, products_feed_params)
     case Repo.update(changeset) do
-      {:ok, merchant_products_feed} ->
-        render(conn, "show.json", merchant_products_feed: merchant_products_feed)
+      {:ok, products_feed} ->
+        conn
+        |> render(ProductsFeedView, "products_feed.json", products_feed: products_feed)
       {:error, changeset} ->
         conn
         |> put_status(:unprocessable_entity)
-        |> render(Marketplace.ChangesetView, "error.json", changeset: changeset)
+        |> render(Marketplace.ChangesetView, "errors.json", changeset: changeset)
     end
   end
 
-  def delete(conn, %{"id" => id}) do
-    merchant_products_feed = Repo.get!(MerchantProductsFeed, id)
+  defp insert_and_relate(products_feed_params, merchant_id) do
+    pf_cs = ProductsFeed.changeset(%ProductsFeed{}, products_feed_params)
+    Multi.new
+    |> Multi.insert(:products_feed, pf_cs)
+    |> Multi.run(:merchant_products_feed, fn %{products_feed: products_feed} ->
+      map_products_feed_to_merchant(products_feed, merchant_id) end
+    )
+  end
 
-    # Here we use delete! (with a bang) because we expect
-    # it to always work (and if it does not, it will raise).
-    Repo.delete!(merchant_products_feed)
+  defp map_products_feed_to_merchant(products_feed, merchant_id) do
+    mpf_cs = MerchantProductsFeed.changeset(%MerchantProductsFeed{}, %{
+        "merchant_id" => merchant_id,
+        "products_feed_id" => products_feed.id
+      })
 
-    send_resp(conn, :no_content, "")
+    Repo.insert(mpf_cs)
   end
 end
