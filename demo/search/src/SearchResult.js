2ffbeebf3a641a164d377efb9f75575f9c52164b
Jeff Mataya <jeff@foxcommerce.com>
2016-10-08 14:52:46 -0700

    Mock search results and styling

--- demo/search/src/SearchResult.js
+++ demo/search/src/SearchResult.js
@@ -1,14 +1,21 @@
 import React, { Component } from 'react';
+import './SearchResult.css';
 
 class SearchResult extends Component {
   render() {
-    const { image, price, title } = this.props;
+    const { image, price, title } = this.props.result;
+    const formattedPrice = `$${price / 100}`;
+
     return (
-      <div>
-        <div>{image}</div>
+      <div className="search-result">
+        <div>
+          <img className="search-image" src={image} alt={title} />
+        </div>
         <div>{title}</div>
-        <div>{price}</div>
+        <div>{formattedPrice}</div>
       </div>
     );
   }
 }
+
+export default SearchResult;
--- demo/search/src/SearchResults.js
+++ demo/search/src/SearchResults.js
@@ -1,17 +1,17 @@
 import React, { Component } from 'react';
+import SearchResult from './SearchResult';
+
+import './SearchResults.css';
 
 class SearchResults extends Component {
   render() {
     const { results } = this.props;
-    if (results.length == 0) {
-      return <div>No results returned.</div>;
-    }
+    
+    const content = results.length === 0
+      ? <div className="empty-message">No results returned.</div>
+      : results.map(r => <SearchResult result={r} />);
 
-    return (
-      <div>
-        Some results returned.
-      </div>
-    );
+    return <div className="search-results">{content}</div>;
   }
 }
 
