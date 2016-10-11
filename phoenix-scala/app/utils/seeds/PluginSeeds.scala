d4602a17033a22690eb5d77b1e401c0adfeab9d7
Eugene Sypachev (Axblade) <eugene.sypachev@gmail.com>
2016-10-11 20:48:15 +0300

    schema

--- phoenix-scala/app/utils/seeds/PluginSeeds.scala
+++ phoenix-scala/app/utils/seeds/PluginSeeds.scala
@@ -1,5 +1,6 @@
 package utils.seeds
 
+import models.plugins.PluginSettings._
 import models.plugins._
 import payloads.PluginPayloads.RegisterPluginPayload
 import utils.aliases._
@@ -7,13 +8,40 @@ import utils.db._
 
 trait PluginSeeds {
 
+  val accountNumber = SettingDef(name = "avatax_account_number",
+                                 title = "AvaTax Account Number",
+                                 `type` = PluginSettings.String)
+  val companyCode = SettingDef(name = "avatax_company_code",
+                               title = "AvaTax Company Code",
+                               `type` = PluginSettings.String)
+  val licenseKey = SettingDef(name = "avatax_license_key",
+                              title = "AvaTax License Key",
+                              `type` = PluginSettings.String)
+  val serviceUrl = SettingDef(name = "avatax_service_url",
+                              title = "AvaTax Service URL",
+                              `type` = PluginSettings.String)
+  val commitDocs = SettingDef(name = "avatax_commit_documents",
+                              title = "Save and commit documents to AvaTax",
+                              `type` = PluginSettings.Bool)
+  val logTransactions = SettingDef(name = "avatax_log_transactions",
+                                   title = "Log AvaTax transactions in FoxCommerce",
+                                   `type` = PluginSettings.Bool)
+
+  val schema = Seq[SettingDef](accountNumber,
+                               companyCode,
+                               licenseKey,
+                               serviceUrl,
+                               commitDocs,
+                               logTransactions)
+  val avalara = RegisterPluginPayload(name = "Avalara AvaTax",
+                                      version = "1.0.0",
+                                      description = "Sales tax stuff",
+                                      apiHost = "0.0.0.0",
+                                      apiPort = 9090,
+                                      schemaSettings = None)
+
   def createAvalaraSettings()(implicit db: DB, ac: AC, ec: EC): DbResultT[Plugin] = {
-    val avalara = RegisterPluginPayload(name = "Avalara AvaTax",
-                                        version = "1.0.0",
-                                        description = "Sales tax stuff",
-                                        apiHost = "0.0.0.0",
-                                        apiPort = 9090,
-                                        schemaSettings = None)
+
     for {
       _      ← * <~ DbResultT.good(println("Inserting Avalara AvaTax settings"))
       plugin ← * <~ Plugins.create(Plugin.fromPayload(avalara))
