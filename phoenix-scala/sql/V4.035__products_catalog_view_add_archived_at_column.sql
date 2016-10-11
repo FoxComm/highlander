cfdb205f68d0a8badec624495679dac90afadc3d
Sergey Rublev <sergey@foxcommerce.com>
2016-10-12 05:13:12 +0700

    Set archived_at for products_catalog_view in one place
    Get rid of unnecessary parts where we evict or update products

--- phoenix-scala/sql/V4.035__products_catalog_view_add_archived_at_column.sql
+++ phoenix-scala/sql/V4.035__products_catalog_view_add_archived_at_column.sql
@@ -1,1 +1,6 @@
 alter table products_catalog_view add column archived_at generic_string;
+
+drop trigger evict_archived_products_cat_search_view_from_object_forms on products;
+drop trigger evict_products_cat_search_view_from_object_forms on products;
+drop function delete_inactive_products_cat_search_view_fn();
+drop function toggle_products_catalog_from_to_active();
