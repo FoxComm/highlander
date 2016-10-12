928c21a7cacc27c86bf0e80d53a32777d46495f2
Andrey Rublev <version.ru@gmail.com>
2016-10-12 10:22:23 +0700

    revert back preventSave functionality

--- ashes/src/components/products/page.jsx
+++ ashes/src/components/products/page.jsx
@@ -88,14 +88,12 @@ class ProductPage extends ObjectPage {
   }
 
   get preventSave(): boolean {
-    // const product = this.state.entity;
-    // if (product) {
-    //   return !isProductValid(product);
-    // }
-    //
-    // return true;
+    const product = this.state.entity;
+    if (product) {
+      return !isProductValid(product);
+    }
 
-    return false;
+    return true;
   }
 
   @autobind
