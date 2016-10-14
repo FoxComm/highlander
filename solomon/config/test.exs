bebdfb1c6ebf7999f61ce8cc87ccaf5b2e6486a0
Josh Ferrerra <josh@foxcommerce.com>
2016-10-14 17:35:53 -0400

    references to permissions app renamed to solomon

--- solomon/config/test.exs
+++ solomon/config/test.exs
@@ -2,7 +2,7 @@ use Mix.Config
 
 # We don't run a server during test. If one is required,
 # you can enable the server option below.
-config :permissions, Solomon.Endpoint,
+config :solomon, Solomon.Endpoint,
   http: [port: 4001],
   server: false
 
@@ -10,10 +10,10 @@ config :permissions, Solomon.Endpoint,
 config :logger, level: :warn
 
 # Configure your database
-config :permissions, Solomon.Repo,
+config :solomon, Solomon.Repo,
   adapter: Ecto.Adapters.Postgres,
   username: "postgres",
   password: "postgres",
-  database: "permissions_test",
+  database: "solomon_test",
   hostname: "localhost",
   pool: Ecto.Adapters.SQL.Sandbox
