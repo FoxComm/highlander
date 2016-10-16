684ae7e9ec1bef4f3d53e366630b63710780eb00
Jeff Mataya <jeff@foxcommerce.com>
2016-10-15 21:19:50 -0700

    Create a managed account when the merchant account is created

---
+++ marketplace/web/services/stripe.ex
@@ -0,0 +1,36 @@
+defmodule Marketplace.Stripe do
+  import Ecto
+  import Ecto.Query
+  import HTTPoison
+
+  # This function will create a new managed account in Stripe Connect and
+  # return the account ID for the new account, if successful.
+  def create_account do
+    HTTPoison.start
+    post_headers = [{'Authorization',  "Basic #{stripe_key()}"}]
+    post_url = 'https://api.stripe.com/v1/accounts'
+    post_body = {:form, [managed: true, country: 'US']}
+
+    case HTTPoison.post(post_url, post_body, post_headers) do
+      {:ok, %HTTPoison.Response{status_code: 200, body: body}} ->
+        case Poison.decode(body) do
+        {:ok, decoded_body} ->
+          account_id = Map.fetch!(decoded_body, "id")
+          IO.inspect("Received account from stripe")
+          IO.inspect(account_id)
+          account_id
+        {:error, decoded_body} ->
+          nil
+        end
+      {:error, %HTTPoison.Error{reason: reason}} ->
+        IO.inspect("ERROR FROM HTTP CLIENT CONNECTING TO STRIPE!")
+        IO.inspect(reason)
+        nil
+    end
+  end
+
+  defp stripe_key() do
+    raw_key = Application.get_env(:marketplace, Marketplace.MerchantAccount)[:stripe_private_key]
+    Base.encode64(raw_key)
+  end
+end
