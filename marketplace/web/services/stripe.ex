d5717abf3923198dc252a192f80909ba1fe25617
Jeff Mataya <jeff@foxcommerce.com>
2016-10-16 12:53:53 -0700

    Clean up submission form

--- marketplace/web/services/stripe.ex
+++ marketplace/web/services/stripe.ex
@@ -12,25 +12,32 @@ defmodule Marketplace.Stripe do
     post_body = {:form, [
       'managed': true,
       'country': 'US',
-      'legal_entity[dob][day]': String.to_integer(legal_profile_params["business_founded_day"]),
-      'legal_entity[dob][month]': String.to_integer(legal_profile_params["business_founded_month"]),
-      'legal_entity[dob][year]': String.to_integer(legal_profile_params["business_founded_year"]),
-      'legal_entity[first_name]': merchant_account.first_name,
-      'legal_entity[last_name]': merchant_account.last_name,
-      'legal_entity[business_name]': legal_profile_params["legal_entity_name"],
-      'legal_entity[type]': legal_profile_params["legal_entity_type"],
+
       'external_account[object]': 'bank_account',
       'external_account[country]': 'US',
       'external_account[currency]': 'usd',
       'external_account[routing_number]': legal_profile_params["bank_routing_number"],
       'external_account[account_number]': legal_profile_params["bank_account_number"],
+
+      'legal_entity[dob][day]': String.to_integer(legal_profile_params["business_founded_day"]),
+      'legal_entity[dob][month]': String.to_integer(legal_profile_params["business_founded_month"]),
+      'legal_entity[dob][year]': String.to_integer(legal_profile_params["business_founded_year"]),
+
+      'legal_entity[first_name]': merchant_account.first_name,
+      'legal_entity[last_name]': merchant_account.last_name,
+      'legal_entity[business_name]': legal_profile_params["legal_entity_name'"],
+      'legal_entity[type]': 'company',
+      'legal_entity[ssn_last_4]': legal_profile_params["representative_ssn_trailing_four"],
+
       'legal_entity[address][city]': legal_profile_params["city"],
       'legal_entity[address][line1]': legal_profile_params["address1"],
       'legal_entity[address][postal_code]': legal_profile_params["zip"],
       'legal_entity[address][state]': legal_profile_params["state"],
       'legal_entity[business_tax_id]': legal_profile_params["legal_entity_tax_id"],
-      'legal_entity[ssn_last_4]': legal_profile_params["representative_ssn_trailing_four"],
-      'tos_acceptance[date]': 1476587489,
+
+      'tos_acceptance[date]': :os.system_time(:seconds),
+
+      # TODO: Eventually grab this from the request.
       'tos_acceptance[ip]': '67.170.86.209'
     ]} 
 
