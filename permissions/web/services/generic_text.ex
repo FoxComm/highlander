fbb3ad8aebb0d4eb3efd45a4e037d1ec5e1e294d
Josh Ferrerra <josh@foxcommerce.com>
2016-10-16 14:22:00 -0400

    Add Postgrex extension for generic text fields

---
+++ permissions/web/services/generic_text.ex
@@ -0,0 +1,12 @@
+defmodule Permissions.GenericText do
+
+  @behavior Postgrex.Extension
+
+  def init(_parameters, opts) when opts in [:reference, :copy], do: opts
+
+  def matching(_opts), do: [output: "textout"] # base type is text
+  def format(_opts), do: :text
+  def encode(_type_info, bin, _types, _opts) when is_binary(bin), do: bin
+  def decode(_type_info, bin, _types, :reference), do: bin
+  def decode(_type_info, bin, _types, :copy),      do: :binary.copy(bin)
+end
