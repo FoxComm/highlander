788f56a76c05c0258ca5fe1299a011ceab62092f
Josh Ferrerra <josh@foxcommerce.com>
2016-10-16 14:20:46 -0700

    fixes in Postgrex extension

--- permissions/web/services/generic_text.ex
+++ permissions/web/services/generic_text.ex
@@ -1,10 +1,10 @@
 defmodule Permissions.GenericText do
 
-  @behavior Postgrex.Extension
+  @behaviour Postgrex.Extension
 
   def init(_parameters, opts) when opts in [:reference, :copy], do: opts
 
-  def matching(_opts), do: [output: "textout"] # base type is text
+  def matching(_opts), do: [input: "domain_in", output: "textout"] # base type is text
   def format(_opts), do: :text
   def encode(_type_info, bin, _types, _opts) when is_binary(bin), do: bin
   def decode(_type_info, bin, _types, :reference), do: bin
