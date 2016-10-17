cb8d1e51dd5f2a0b015fb84dc79fc4ebeb684a54
Jeff Mataya <jeff@foxcommerce.com>
2016-10-16 17:44:30 -0700

    Set the payout schedule

--- marketplace/web/services/stripe.ex
+++ marketplace/web/services/stripe.ex
@@ -19,13 +19,17 @@ defmodule Marketplace.Stripe do
       'external_account[routing_number]': legal_profile_params["bank_routing_number"],
       'external_account[account_number]': legal_profile_params["bank_account_number"],
 
+      'transfer_schedule[delay_days]': 14,
+      'transfer_schedule[interval]': 'weekly',
+      'transfer_schedule[weekly_anchor]': 'friday',
+
       'legal_entity[dob][day]': String.to_integer(legal_profile_params["business_founded_day"]),
       'legal_entity[dob][month]': String.to_integer(legal_profile_params["business_founded_month"]),
       'legal_entity[dob][year]': String.to_integer(legal_profile_params["business_founded_year"]),
 
       'legal_entity[first_name]': merchant_account.first_name,
       'legal_entity[last_name]': merchant_account.last_name,
-      'legal_entity[business_name]': legal_profile_params["legal_entity_name'"],
+      'legal_entity[business_name]': legal_profile_params["legal_entity_name"],
       'legal_entity[type]': 'company',
       'legal_entity[ssn_last_4]': legal_profile_params["representative_ssn_trailing_four"],
 
@@ -39,7 +43,7 @@ defmodule Marketplace.Stripe do
 
       # TODO: Eventually grab this from the request.
       'tos_acceptance[ip]': '67.170.86.209'
-    ]} 
+    ]}
 
     case HTTPoison.post(post_url, post_body, post_headers) do
       {:ok, %HTTPoison.Response{status_code: 200, body: body}} ->
