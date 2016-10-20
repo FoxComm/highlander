58beadbecf81a5fe467737d953f11770e0c40166
Sergey Rublev <sergey@foxcommerce.com>
2016-10-20 10:12:52 +0700

    Delete instead of archivation

--- phoenix-scala/sql/R__products_catalog_view_functions.sql
+++ phoenix-scala/sql/R__products_catalog_view_functions.sql
@@ -3,6 +3,7 @@ declare
   product_ids int[];
   insert_ids int[];
   update_ids int[];
+  delete_ids int[];
 begin
 
   case tg_table_name
@@ -16,23 +17,42 @@ begin
       select array_agg(p.id) into strict product_ids
         from products as p
           inner join object_contexts as context on (p.context_id = context.id)
-          inner join product_sku_links_view as sv on (sv.product_id = p.id)--get list of sku codes for the product
+          inner join product_sku_links_view as sv on (sv.product_id = p.id) --get list of sku codes for the product
           inner join sku_search_view as sku on (sku.context_id = context.id and sku.sku_code = sv.skus->>0)
         where sku.id = new.id;
   end case;
 
- select array_agg(p.id) into update_ids
-    from products as p
-      inner join object_contexts as context on (p.context_id = context.id)
-      inner join products_catalog_view as pv on (pv.id = p.id and context.name = pv.context)
-    where p.id = any(product_ids);
+  with temp_table(id, alive, catalog_id) as (
+    select
+         q.id,
+         q.alive,
+         q.catalog_id
+       from (select
+               p.id,
+               ((p.archived_at is null or (p.archived_at) :: timestamp > current_timestamp) and
+                (f.attributes ->> (s.attributes -> 'activeFrom' ->> 'ref')) :: timestamp <
+                current_timestamp and
+                (((f.attributes ->> (s.attributes -> 'activeTo' ->> 'ref')) = '') is not false or
+                 ((f.attributes ->> (s.attributes -> 'activeTo' ->> 'ref')) :: timestamp >=
+                  current_timestamp)))
+                 as alive,
+                pv.id as catalog_id
 
-  select array_agg(elements) into insert_ids
-    from (
-      select unnest(product_ids)
-        except
-      select unnest(update_ids)
-    ) t (elements);
+             from products as p
+               inner join object_contexts as context on (p.context_id = context.id)
+               left join products_catalog_view as pv on (pv.id = p.id and context.name = pv.context)
+               inner join object_forms as f on (f.id = p.form_id)
+               inner join object_shadows as s on (s.id = p.shadow_id)) as q
+) select
+    array_agg(id) filter (where alive = true and catalog_id is not null) as upd_ids,
+    array_agg(id) filter (where alive = false and catalog_id is not null) as del_ids,
+    array_agg(id) filter (where catalog_id is null and alive = true) as ins_ids
+      into update_ids, delete_ids, insert_ids
+  from temp_table;
+
+  if array_length(delete_ids, 1) > 0 then
+    delete from products_catalog_view where id = any(delete_ids);
+  end if;
 
   if array_length(insert_ids, 1) > 0 then
     insert into products_catalog_view select
@@ -44,61 +64,55 @@ begin
       sku.sale_price as sale_price,
       sku.sale_price_currency as currency,
       f.attributes->>(s.attributes->'tags'->>'ref') as tags,
-      albumLink.albums as albums
+      albumLink.albums as albums,
+      p.scope as scope
       from products as p
         inner join object_contexts as context on (p.context_id = context.id)
         inner join object_forms as f on (f.id = p.form_id)
         inner join object_shadows as s on (s.id = p.shadow_id)
-        inner join product_sku_links_view as sv on (sv.product_id = p.id)--get list of sku codes for the product
+        inner join product_sku_links_view as sv on (sv.product_id = p.id) --get list of sku codes for the product
         inner join sku_search_view as sku on (sku.context_id = context.id and sku.sku_code = sv.skus->>0)
         left join product_album_links_view as albumLink on (albumLink.product_id = p.id)
-      where p.id = any(insert_ids) and
-            (p.archived_at is null or (p.archived_at)::timestamp > current_timestamp) and
-            (f.attributes->>(s.attributes->'activeFrom'->>'ref'))::timestamp < current_timestamp and
-            (((f.attributes->>(s.attributes->'activeTo'->>'ref')) = '') is not false or
-            ((f.attributes->>(s.attributes->'activeTo'->>'ref'))::timestamp >= current_timestamp));
+      where p.id = any(insert_ids);
     end if;
 
   if array_length(update_ids, 1) > 0 then
     update products_catalog_view set
-      product_id = subquery.product_id,
-      context = subquery.context,
-      title = subquery.title,
-      description = subquery.description,
-      sale_price = subquery.sale_price,
-      currency = subquery.currency,
-      tags = subquery.tags,
-      albums = subquery.albums,
-      archived_at = subquery.archived_at
-    from (select
-            p.id,
-            f.id as product_id,
-            context.name as context,
-            f.attributes->>(s.attributes->'title'->>'ref') as title,
-            f.attributes->>(s.attributes->'description'->>'ref') as description,
-            sku.sale_price as sale_price,
-            sku.sale_price_currency as currency,
-            f.attributes->>(s.attributes->'tags'->>'ref') as tags,
-            albumLink.albums as albums,
-            case ((p.archived_at is null or (p.archived_at)::timestamp > current_timestamp) and
-                (f.attributes->>(s.attributes->'activeFrom'->>'ref'))::timestamp < current_timestamp and
-                (((f.attributes->>(s.attributes->'activeTo'->>'ref')) = '') is not false or
-                ((f.attributes->>(s.attributes->'activeTo'->>'ref'))::timestamp >= current_timestamp)))
-            when true
-              then null
-            else
-              to_json_timestamp(now()::timestamp)
-            end as archived_at
-      from products as p
-        inner join object_contexts as context on (p.context_id = context.id)
-        inner join object_forms as f on (f.id = p.form_id)
-        inner join object_shadows as s on (s.id = p.shadow_id)
-        inner join product_sku_links_view as sv on (sv.product_id = p.id)--get list of sku codes for the product
-        inner join sku_search_view as sku on (sku.context_id = context.id and sku.sku_code = sv.skus->>0)
-        left join product_album_links_view as albumLink on (albumLink.product_id = p.id)
-      where p.id = any(update_ids))
-      as subquery
-  where products_catalog_view.id = subquery.id;
+        product_id = subquery.product_id,
+        context = subquery.context,
+        title = subquery.title,
+        description = subquery.description,
+        sale_price = subquery.sale_price,
+        currency = subquery.currency,
+        tags = subquery.tags,
+        albums = subquery.albums,
+        scope = subquery.scope
+      from (select
+                p.id,
+                f.id as product_id,
+                context.name as context,
+                f.attributes->>(s.attributes->'title'->>'ref') as title,
+                f.attributes->>(s.attributes->'description'->>'ref') as description,
+                sku.sale_price as sale_price,
+                sku.sale_price_currency as currency,
+                f.attributes->>(s.attributes->'tags'->>'ref') as tags,
+                albumLink.albums as albums,
+                ((p.archived_at is null or (p.archived_at)::timestamp > current_timestamp) and
+                    (f.attributes->>(s.attributes->'activeFrom'->>'ref'))::timestamp < current_timestamp and
+                    (((f.attributes->>(s.attributes->'activeTo'->>'ref')) = '') is not false or
+                    ((f.attributes->>(s.attributes->'activeTo'->>'ref'))::timestamp >= current_timestamp)))
+                as alive,
+                p.scope as scope
+              from products as p
+                inner join object_contexts as context on (p.context_id = context.id)
+                inner join object_forms as f on (f.id = p.form_id)
+                inner join object_shadows as s on (s.id = p.shadow_id)
+                inner join product_sku_links_view as sv on (sv.product_id = p.id)--get list of sku codes for the product
+                inner join sku_search_view as sku on (sku.context_id = context.id and sku.sku_code = sv.skus->>0)
+                left join product_album_links_view as albumLink on (albumLink.product_id = p.id)
+              where p.id = any(update_ids)
+           ) as subquery
+    where products_catalog_view.id = subquery.id;
   end if;
 
   return null;
