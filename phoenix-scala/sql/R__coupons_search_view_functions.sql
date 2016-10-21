a9f66d0e552fa01df413b3d7d4cd0e17cfaba0ad
Sergey Rublev <sergey@foxcommerce.com>
2016-10-21 15:16:58 +0700

    Fix coupons_*_view
    1) Add missing scope to coupon_codes_search_view table
    2) Add codes to coupons_search_view

---
+++ phoenix-scala/sql/R__coupons_search_view_functions.sql
@@ -0,0 +1,66 @@
+create or replace function update_coupons_view_insert_fn() returns trigger as $$
+  begin
+    insert into coupons_search_view select distinct on (new.id)
+      f.id,
+      cp.promotion_id,
+      context.name as context,
+      illuminate_text(f, s, 'name') as name,
+      illuminate_text(f, s, 'storefrontName') as storefront_name,
+      illuminate_text(f, s, 'description') as description,
+      illuminate_text(f, s, 'activeFrom') as active_from,
+      illuminate_text(f, s, 'activeTo') as active_to,
+      0 as total_used,
+      to_char(f.created_at, 'YYYY-MM-DD"T"HH24:MI:SS.MS"Z"') as created_at,
+      to_char(cp.archived_at, 'YYYY-MM-DD"T"HH24:MI:SS.MS"Z"') as archived_at,
+      cp.scope as scope,
+      jsonb_agg(c.code) filter (where c.code is not null) over (partition by f.id)  as codes
+      from coupons as cp
+      left join coupon_codes as c on (cp.form_id = c.coupon_form_id)
+      inner join object_contexts as context on (cp.context_id = context.id)
+      inner join object_forms as f on (f.id = cp.form_id)
+      inner join object_shadows as s on (s.id = cp.shadow_id)
+      where cp.id = new.id;
+
+      return null;
+  end;
+$$ language plpgsql;
+
+create or replace function update_coupons_view_update_fn() returns trigger as $$
+begin
+    update coupons_search_view set
+      id = q.id,
+      promotion_id = q.promotion_id,
+      context = q.context,
+      name = q.name,
+      storefront_name = q.storefront_name,
+      description = q.description,
+      active_from = q.active_from,
+      active_to = q.active_to,
+      created_at = q.created_at,
+      archived_at = q.archived_at,
+      codes = q.codes
+      from (select
+              f.id,
+              cp.promotion_id,
+              cp.form_id,
+              context.name as context,
+              illuminate_text(f, s, 'name') as name,
+              illuminate_text(f, s, 'storefrontName') as storefront_name,
+              illuminate_text(f, s, 'description') as description,
+              illuminate_text(f, s, 'activeFrom') as active_from,
+              illuminate_text(f, s, 'activeTo') as active_to,
+              to_char(f.created_at, 'YYYY-MM-DD"T"HH24:MI:SS.MS"Z"') as created_at,
+              to_char(cp.archived_at, 'YYYY-MM-DD"T"HH24:MI:SS.MS"Z"') as archived_at,
+              cp.scope as scope,
+              jsonb_agg(c.code) filter (where c.code is not null) over (partition by f.id)  as codes
+            from coupons as cp
+            inner join object_contexts as context on (cp.context_id = context.id)
+            left join coupon_codes as c on (cp.form_id = c.coupon_form_id)
+            inner join object_forms as f on (f.id = cp.form_id)
+            inner join object_shadows as s on (s.id = cp.shadow_id)
+            where cp.id = new.id) as q
+    where coupons_search_view.id = q.form_id;
+    return null;
+    end;
+$$ language plpgsql;
+
---
+++ phoenix-scala/sql/V4.061__fix_coupons_views.sql
@@ -0,0 +1,18 @@
+alter table coupon_codes_search_view add column scope exts.ltree;
+
+update coupon_codes_search_view
+  set scope = text2ltree(get_scope_path((select scope_id from organizations where name = 'merchant'))::text);
+
+alter table coupon_codes_search_view alter column scope set not null;
+
+alter table coupons_search_view add column codes jsonb;
+
+update coupons_search_view set
+  codes = q.codes from (select
+    cp.form_id,
+    jsonb_agg(c.code) filter (where c.code is not null) over (partition by cp.form_id) as codes
+  from coupons as cp
+  left join coupon_codes as c on (cp.form_id = c.coupon_form_id)) as q
+  where coupons_search_view.id = q.form_id;
+
+
