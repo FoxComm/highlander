c08cf0dd90a53f1adae68fac63c5e492c2fd7ef5
Eugene Sypachev (Axblade) <eugene.sypachev@gmail.com>
2016-10-24 14:45:58 +0300

    show-hide passwords

---
+++ src/core/ui/forms/show-hide-password.jsx
@@ -0,0 +1,65 @@
+/* @flow */
+
+import React, { Component } from 'react';
+import { autobind } from 'core-decorators';
+
+import localized from 'lib/i18n';
+
+import { TextInputWithLabel } from 'ui/inputs';
+
+type State = {
+  isShown: boolean
+};
+
+type Props = {
+  name: string,
+  value: string,
+  className?: string,
+  onChange: Function,
+  placeholder?: string,
+  linkClassName?: string,
+  t: Function,
+};
+
+class ShowHidePassword extends Component {
+  props: Props;
+  state: State = {
+    isShown: false,
+  };
+
+  get showLink() {
+    const { t, linkClassName } = this.props;
+    const message = this.state.isShown ? t('HIDE') : t('SHOW');
+    return (
+      <a href="#" className={linkClassName} onClick={this.toggleState}>
+        {message}
+      </a>
+    );
+  }
+
+  @autobind
+  toggleState() {
+    this.setState({
+      isShown: !this.state.isShown,
+    });
+  }
+
+  render() {
+    const { name, className, onChange, placeholder, value } = this.props;
+    const inputType = this.state.isShown ? 'text' : 'password';
+
+    return (
+      <TextInputWithLabel
+        className={className}
+        placeholder={placeholder}
+        name={name}
+        value={value}
+        onChange={onChange}
+        type={inputType}
+        label={this.showLink}
+      />
+    );
+  }
+}
+
+export default localized(ShowHidePassword);
