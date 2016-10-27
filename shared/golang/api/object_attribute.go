788069812849cbcd4e0f68ffeaafa9efc1b450d7
Jeff Mataya <jeff@foxcommerce.com>
2016-10-27 16:50:21 -0700

    Algorithm for extracting partial SKU

--- shared/golang/api/object_attribute.go
+++ shared/golang/api/object_attribute.go
@@ -1,6 +1,23 @@
 package api
 
+import "fmt"
+
 type ObjectAttribute struct {
 	Type  string      `json:"t"`
 	Value interface{} `json:"v"`
+}
+
+type ObjectAttributes map[string]ObjectAttribute
+
+func (o ObjectAttributes) LookupValue(name, expectedType string) (interface{}, error) {
+	attr, ok := o[name]
+	if !ok {
+		return "", fmt.Errorf("Variant %s not found", name)
+	}
+
+	if attr.Type != expectedType {
+		return "", fmt.Errorf("Expected %s type for variant value, got %s", expectedType, attr.Type)
+	}
+
+	return attr.Value, nil
 }
--- shared/golang/api/variant.go
+++ shared/golang/api/variant.go
@@ -1,6 +1,16 @@
 package api
 
 type Variant struct {
-	Attributes map[string]ObjectAttribute `json:"attributes"`
-	Values     []VariantValue             `json:"values"`
+	Attributes ObjectAttributes `json:"attributes"`
+	Values     []VariantValue   `json:"values"`
+}
+
+func (v Variant) Name() (string, error) {
+	value, err := v.Attributes.LookupValue("name", "string")
+	return value.(string), err
+}
+
+func (v Variant) Type() (string, error) {
+	value, err := v.Attributes.LookupValue("type", "string")
+	return value.(string), err
 }
