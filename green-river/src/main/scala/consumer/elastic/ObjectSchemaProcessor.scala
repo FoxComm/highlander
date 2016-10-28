8058f55201f3239057efb1f3a8fce5fe21661b25
Sergey Rublev <sergey@foxcommerce.com>
2016-10-28 23:55:56 +0700

    Apply new mapping from object schemas and es definitions in psql

--- green-river/src/main/scala/consumer/elastic/ObjectSchemaProcessor.scala
+++ green-river/src/main/scala/consumer/elastic/ObjectSchemaProcessor.scala
@@ -1,22 +1,16 @@
 package consumer.elastic
 
-import consumer.AvroJsonHelper
+import scala.collection.mutable.Buffer
+import scala.concurrent.{ExecutionContext, Future}
 
-import scala.concurrent.ExecutionContext
-import scala.concurrent.Future
-import scala.util.control.NonFatal
-
-import consumer.JsonProcessor
-import consumer.PassthroughSource
-import consumer.elastic.mappings._
-import com.sksamuel.elastic4s.ElasticDsl.{mapping ⇒ esMapping, _}
-import com.sksamuel.elastic4s.mappings.FieldType._
-import com.sksamuel.elastic4s.{ElasticClient, ElasticsearchClientUri}
 import com.sksamuel.elastic4s.ElasticDsl._
+import com.sksamuel.elastic4s.mappings._
+import com.sksamuel.elastic4s.mappings.attributes._
+import com.sksamuel.elastic4s.{ElasticClient, ElasticsearchClientUri}
+import consumer.{AvroJsonHelper, JsonProcessor}
 import org.elasticsearch.common.settings.Settings
-import org.elasticsearch.client.transport.NoNodeAvailableException
-import org.elasticsearch.transport.RemoteTransportException
-import org.json4s.JsonAST.JInt
+import org.json4s.DefaultFormats
+import org.json4s.JsonAST._
 import org.json4s.jackson.JsonMethods.parse
 
 /**
@@ -27,6 +21,44 @@ import org.json4s.jackson.JsonMethods.parse
   * id and uses it as the _id in elasticsearch for that item. This is important so that
   * we don't duplicate entries in ES.
   */
+case class EsOptions(typed: String,
+                     name: Option[String],
+                     index: Option[String],
+                     analyzer: Option[String],
+                     format: Option[String]) {
+
+  val applyToField = applyIndex _ compose applyFormat compose applyAnalyzer
+
+  def applyIndex(field: TypedFieldDefinition): TypedFieldDefinition =
+    index.fold(field) { v ⇒
+      field match {
+        case f: AttributeIndex ⇒
+          f.index(v)
+        case _ ⇒ field
+      }
+    }
+
+  def applyFormat(field: TypedFieldDefinition): TypedFieldDefinition =
+    format.fold(field) { v ⇒
+      field match {
+        case f: AttributeFormat ⇒
+          f.format(v)
+        case _ ⇒ field
+      }
+    }
+
+  def applyAnalyzer(field: TypedFieldDefinition): TypedFieldDefinition =
+    analyzer.fold(field) { v ⇒
+      field match {
+        case f: AttributeAnalyzer ⇒
+          f.analyzer(v)
+        case _ ⇒ field
+      }
+    }
+}
+
+case class EsAttribute(path: Seq[String], es_opts: EsOptions)
+
 class ObjectSchemaProcessor(uri: String, cluster: String, schemasTopic: String)(
     implicit ec: ExecutionContext)
     extends JsonProcessor {
@@ -34,21 +66,79 @@ class ObjectSchemaProcessor(uri: String, cluster: String, schemasTopic: String)(
   val settings = Settings.settingsBuilder().put("cluster.name", cluster).build()
   val client   = ElasticClient.transport(settings, ElasticsearchClientUri(uri))
 
+  implicit val formats: DefaultFormats.type = DefaultFormats
+
   private val futureUnit: Future[Unit] = Future { () }
 
+  val indexPrefix = "admin" // FIXME: move to settings?
+
+
   def process(offset: Long, topic: String, key: String, inputJson: String): Future[Unit] = {
-//    val parsed = AvroJsonHelper.transformJson(inputJson)
-//    val parsedRaw = AvroJsonHelper.transformJsonRaw(inputJson)
-//    Console.out.println(s"SCHEMAS: topic = $topic, document = $inputJson")
-//    Console.out.println(s"SCHEMAS: parsed = $parsed")
+    var createFutures = Buffer[Future[Unit]]()
 
-//    client.execute {
-//      put mapping "admin" / "products_search_view" fields(
-//        field("external_id").typed(IntegerType)
-//      )
-//    }
+    val parsed   = AvroJsonHelper.transformJson(inputJson)
+    val document = AvroJsonHelper.transformJsonRaw(inputJson)
 
+    Console.out.println(s"SCHEMAS: parsed = $parsed")
+    val esIndex          = (document \ "esIndex").extract[String]
+    val schemaAttributes = parse((document \ "schemaAttributes").extract[String])
+    val esAttributes     = parse((document \ "esAttributes").extract[String])
 
-    futureUnit
+    val mapping          = makeMappingFromJsonSchema(schemaAttributes, esAttributes)
+    val fieldsDefinition = makeMappingFromJsonSchema(schemaAttributes, esAttributes)
+
+    val scopes = parse((document \ "scopes").extract[String]).extract[List[String]]
+
+    scopes.foreach { scope ⇒
+      Console.out.println(s"SCHEMAS: UPDATE MAPPING admin_$scope with $fieldsDefinition")
+      createFutures += client.execute {
+        put mapping s"${indexPrefix}_$scope" / esIndex fields fieldsDefinition
+      }.map { _ ⇒
+        ()
+      }
+    }
+
+    Future.sequence(createFutures).map(_ ⇒ ())
+  }
+
+  private def makeMappingFromJsonSchema(
+      schemaAttributes: JValue, esAttributes: JValue): Seq[TypedFieldDefinition] = {
+    val esAttrs = esAttributes.extract[Seq[EsAttribute]]
+    val props   = schemaAttributes \ "properties"
+    esAttrs.map { attr ⇒
+      val schemaDefinition = attr.path.foldLeft(props) {
+        case (acc, el) ⇒
+          acc \ el
+      }
+      schemaToEsDefinition(attr, schemaDefinition)
+    }
+  }
+
+  private def schemaToEsDefinition(attr: EsAttribute, schema: JValue): TypedFieldDefinition = {
+    val name = attr.es_opts.name.getOrElse(attr.path.last)
+    val esField: TypedFieldDefinition = attr.es_opts.typed match {
+      case "attachment"  ⇒ new AttachmentFieldDefinition(name)
+      case "binary"      ⇒ new BinaryFieldDefinition(name)
+      case "boolean"     ⇒ new BooleanFieldDefinition(name)
+      case "byte"        ⇒ new ByteFieldDefinition(name)
+      case "completion"  ⇒ new CompletionFieldDefinition(name)
+      case "date"        ⇒ new DateFieldDefinition(name)
+      case "double"      ⇒ new DoubleFieldDefinition(name)
+      case "float"       ⇒ new FloatFieldDefinition(name)
+      case "integer"     ⇒ new IntegerFieldDefinition(name)
+      case "ip"          ⇒ new IpFieldDefinition(name)
+      case "geo_point"   ⇒ new GeoPointFieldDefinition(name)
+      case "geo_shape"   ⇒ new GeoShapeFieldDefinition(name)
+      case "long"        ⇒ new LongFieldDefinition(name)
+      case "multi_field" ⇒ new MultiFieldDefinition(name)
+      case "nested"      ⇒ new NestedFieldDefinition(name)
+      case "object"      ⇒ new ObjectFieldDefinition(name)
+      case "short"       ⇒ new ShortFieldDefinition(name)
+      case "string"      ⇒ new StringFieldDefinition(name)
+      case "token_count" ⇒ new TokenCountDefinition(name)
+      case x             ⇒ throw new IllegalArgumentException(s"Unknown ES type $x")
+    }
+
+    attr.es_opts.applyToField(esField)
   }
 }
