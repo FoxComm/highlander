c96c9b122889bef1c1b852c94b733803f07cb8ec
Vladimir Korbut <v.korbut@svitla.com>
2016-10-28 12:44:30 +0300

    Remove scopes from carts_search_view

--- phoenix-scala/sql/V4.054__scope_carts.sql
+++ phoenix-scala/sql/V4.054__scope_carts.sql
@@ -1,6 +1,5 @@
 alter table carts add column scope exts.ltree;
 alter table carts_search_view add column scope exts.ltree;
-alter table carts_search_view add column scopes text[];
 
 update carts set scope = exts.text2ltree(get_scope_path((select scope_id from organizations where name = 'merchant'))::text);
 update carts_search_view set scope = text2ltree(get_scope_path((select scope_id from organizations where name = 'merchant'))::text);
@@ -70,7 +69,6 @@ begin
 
   update carts_search_view set
     line_item_count = subquery.count,
-    scopes = subquery.scopes,
     line_items = subquery.items from (select
           c.id,
           count(sku.id) as count,
@@ -87,8 +85,7 @@ begin
                     sku_form.attributes->(sku_shadow.attributes->'salePrice'->>'ref')->>'value',
                     sku.scope)::export_line_items)
                     ::jsonb
-          end as items,
-          array_agg(sku.scope) as scopes
+          end as items
           from carts as c
           left join cart_line_items as cli_skus on (c.reference_number = cli_skus.cord_ref)
           left join skus as sku on (cli_skus.sku_id = sku.id)
