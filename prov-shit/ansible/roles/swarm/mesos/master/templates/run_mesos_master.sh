eb31dc5d706d315e2d45e534602e25195ce6edf4
Alex Kreskiyan <a.kreskiyan@gmail.com>
2016-10-28 15:49:46 +0300

    Cleaned up roles

--- prov-shit/ansible/roles/swarm/mesos/master/templates/run_mesos_master.sh
+++ prov-shit/ansible/roles/swarm/mesos/master/templates/run_mesos_master.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
-export MESOS_WEBUI_DIR=/usr/local/share/mesos/webui
+QUORUM={{mesos_quorum}}
 WORK_DIR=/var/lib/mesos
 IP=0.0.0.0
-QUORUM={{mesos_quorum}}
 
-mesos-master --hostname=$IP --quorum=$QUORUM --ip=$IP --work_dir=$WORK_DIR --zk={{zookeepers}}/mesos --cluster=fox
-
+mesos-master \
+    --quorum=$QUORUM \
+    --zk={{zookeepers}}/mesos \
+    --work_dir=$WORK_DIR \
+    --cluster=fox \
+    --ip=$IP \
+    --hostname=$IP
--- prov-shit/ansible/roles/swarm/mesos/worker/templates/run_mesos_worker.sh
+++ prov-shit/ansible/roles/swarm/mesos/worker/templates/run_mesos_worker.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
+CONTAINERIZERS=docker,mesos
 WORK_DIR=/var/lib/mesos
+SANDBOX_DIR=/var/lib/sandbox
 IP=0.0.0.0
+
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib
 export MESOS_EXECUTOR_ENVIRONMENT_VARIABLES="{\"LD_LIBRARY_PATH\": \"$LD_LIBRARY_PATH\"}"
 
-mesos-slave --containerizers=docker,mesos \
+mesos-slave --containerizers=$CONTAINERIZERS \
             --docker_registry={{docker_registry}} \
-            --ip=$IP \
-            --work_dir=$WORK_DIR \
             --master={{zookeepers}}/mesos \
+            --work_dir=$WORK_DIR \
             --sandbox_directory=/var/lib/sandbox \
+            --ip=$IP \
             --hostname=$IP
