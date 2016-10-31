aa8db1d6256bd1b65fc091f9cd09cca1c14c3d30
Eugene Sypachev (Axblade) <eugene.sypachev@gmail.com>
2016-10-31 20:37:48 +0300

    afterrebase fixes

--- src/pages/catalog/pdp.jsx
+++ src/pages/catalog/pdp.jsx
@@ -6,8 +6,7 @@ import React, { Component } from 'react';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import { autobind } from 'core-decorators';
-import { browserHistory } from 'react-router';
-import cx from 'classnames';
+import classNames from 'classnames';
 
 // i18n
 import localized from 'lib/i18n';
@@ -247,7 +246,7 @@ class Pdp extends Component {
     } = product;
 
     const attributeTitles = additionalInfoAttributesMap.map(({ title: attrTitle }) => {
-      const cls = cx(styles['item-title'], {
+      const cls = classNames(styles['item-title'], {
         [styles.active]: attrTitle === this.state.currentAdditionalTitle,
       });
       const onClick = this.setCurrentAdditionalAttr.bind(this, attrTitle);
