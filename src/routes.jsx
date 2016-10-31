605575c22546e55e92a3ea2360c4d67166822c86
Eugene Sypachev (Axblade) <eugene.sypachev@gmail.com>
2016-10-31 23:12:30 +0300

    terms of use page

--- src/routes.jsx
+++ src/routes.jsx
@@ -7,6 +7,7 @@ import Pdp from './pages/catalog/pdp';
 import Search from './pages/search/search';
 import ShippingAndReturns from './pages/static/shipping-and-returns';
 import PrivacyPolicy from './pages/static/privacy-policy';
+import TermsOfUse from './pages/static/terms-of-use';
 
 import Checkout from './pages/checkout/checkout';
 import OrderPlaced from './pages/checkout/04-order-placed/order-placed';
@@ -18,6 +19,7 @@ const routes = (
       <IndexRoute component={Products} />
       <Route path="/shipping-and-returns" component={ShippingAndReturns} name="shipping-and-returns" />
       <Route path="/privacy-policy" component={PrivacyPolicy} name="privacy-policy" />
+      <Route path="/terms-of-use" component={TermsOfUse} name="terms-of-use" />
       <Route path="/checkout/done" component={OrderPlaced} />
       <Route path="/products/:productId" component={Pdp} name="product" />
       <Route path=":categoryName(/:productType)" component={Products} name="category" />
