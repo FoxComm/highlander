bf68383ec516505b8f2075700a6312c671940818
Pavel Kiselev <kpashka@gmail.com>
2016-10-31 18:29:30 +0700

    Cleanup & Mesos-related fixes

--- prov-shit/ansible/roles/base/mesos_worker/templates/run_mesos_worker.sh
+++ prov-shit/ansible/roles/base/mesos_worker/templates/run_mesos_worker.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 
+set -ue
+
+SANDBOX_DIR=/var/lib/sandbox
 WORK_DIR=/var/lib/mesos
 IP=`hostname -I | awk '{print $1}'`
+
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib
 export MESOS_EXECUTOR_ENVIRONMENT_VARIABLES="{\"LD_LIBRARY_PATH\": \"$LD_LIBRARY_PATH\"}"
 
-mesos-slave --containerizers=docker,mesos --docker_registry={{docker_registry}} --ip=$IP --work_dir=$WORK_DIR --master={{zookeepers}}/mesos --sandbox_directory=/var/lib/sandbox --hostname=$IP 
-
+mesos-slave --ip=$IP \
+    --master={{zookeepers}}/mesos \
+    --docker_registry={{docker_registry}} \
+    --containerizers=docker,mesos \
+    --work_dir=$WORK_DIR \
+    --sandbox_directory=$SANDBOX_DIR \
+    --hostname=$IP
--- prov-shit/ansible/roles/hotfix/mesos_worker/templates/run_mesos_worker.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-WORK_DIR=/var/lib/mesos
-IP=`hostname -I | awk '{print $1}'`
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib
-export MESOS_EXECUTOR_ENVIRONMENT_VARIABLES="{\"LD_LIBRARY_PATH\": \"$LD_LIBRARY_PATH\"}"
-
-mesos-slave --containerizers=docker,mesos --docker_registry={{docker_registry}} --ip=$IP --work_dir=$WORK_DIR --master={{zookeepers}}/mesos --sandbox_directory=/var/lib/sandbox --hostname=$IP
