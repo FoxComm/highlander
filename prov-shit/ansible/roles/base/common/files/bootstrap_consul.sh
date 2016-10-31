7a29ebdb05632b913c5cd690e3273e0836e7181e
Pavel Kiselev <kpashka@gmail.com>
2016-10-31 15:07:39 +0700

    Make CONSUL_JOIN_TYPE configurable through Terraform

--- prov-shit/ansible/roles/base/common/files/bootstrap_consul.sh
+++ prov-shit/ansible/roles/base/common/files/bootstrap_consul.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 CONSUL_DC=$1
 CONSUL_SERVER=$2
+CONSUL_JOIN_TYPE=$3
 PRIVATE_IP=$(curl -sf -H 'Metadata-Flavor:Google' http://metadata/computeMetadata/v1/instance/network-interfaces/0/ip | tr -d '\n')
 
 sudo bash -c "echo 'CONSUL_DC=$CONSUL_DC' >> /etc/consul.d/env"
 sudo bash -c "echo 'CONSUL_SERVER=$CONSUL_SERVER' >> /etc/consul.d/env"
 sudo bash -c "echo 'CONSUL_BIND=$PRIVATE_IP' >> /etc/consul.d/env"
+sudo bash -c "echo 'CONSUL_JOIN_TYPE=$CONSUL_JOIN_TYPE' >> /etc/consul.d/env"
