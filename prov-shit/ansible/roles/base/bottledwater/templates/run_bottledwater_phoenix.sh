fd8ea95b57ba6038bcd158edb888797a7b904a99
Pavel Kiselev <kpashka@gmail.com>
2016-11-01 08:33:08 +0700

    Init

---
+++ prov-shit/ansible/roles/base/bottledwater/templates/run_bottledwater_phoenix.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+/usr/local/bin/bottledwater \
+    --allow-unkeyed \
+    --slot {{bottledwater_slot_phoenix}} \
+    --postgres=postgres://phoenix@{{db_host}}/{{db_name}} \
+    --broker={{kafka_server}} \
+    --schema-registry="http://{{schema_server}}"
