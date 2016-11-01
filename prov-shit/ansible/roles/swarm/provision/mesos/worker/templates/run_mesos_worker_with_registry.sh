29c70437d287a006f3a159b60993cb36250d09f1
Alex Kreskiyan <a.kreskiyan@gmail.com>
2016-11-01 16:48:57 +0300

    Backuped run_mesos_worker script

--- prov-shit/ansible/roles/swarm/provision/mesos/worker/templates/run_mesos_worker_with_registry.sh
+++ prov-shit/ansible/roles/swarm/provision/mesos/worker/templates/run_mesos_worker_with_registry.sh
@@ -9,6 +9,7 @@ export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib
 export MESOS_EXECUTOR_ENVIRONMENT_VARIABLES="{\"LD_LIBRARY_PATH\": \"$LD_LIBRARY_PATH\"}"
 
 mesos-slave --containerizers=$CONTAINERIZERS \
+            --docker_registry={{docker_registry}} \
             --master={{zookeepers}}/mesos \
             --work_dir=$WORK_DIR \
             --sandbox_directory=/var/lib/sandbox \
