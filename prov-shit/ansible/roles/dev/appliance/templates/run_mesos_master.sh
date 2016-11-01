eb11069693dd4371ee429fc48b0e8fa81069a79c
Maxim Khailo <mempko@gmail.com>
2016-11-01 05:48:32 -0700

    Fix mesos webui in appliance (#281)

--- prov-shit/ansible/roles/dev/appliance/templates/run_mesos_master.sh
+++ prov-shit/ansible/roles/dev/appliance/templates/run_mesos_master.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
-export MESOS_WEBUI_DIR=/usr/local/share/mesos/webui
+export MESOS_WEBUI_DIR=/usr/share/mesos/webui
 WORK_DIR=/var/lib/mesos
 IP=0.0.0.0
 QUORUM={{mesos_quorum}}
 
-mesos-master --hostname=$IP --quorum=$QUORUM --ip=$IP --work_dir=$WORK_DIR --zk={{zookeepers}}/mesos --cluster=fox
+mesos-master --hostname=$IP \
+    --quorum=$QUORUM \
+    --ip=$IP \
+    --work_dir=$WORK_DIR \
+    --zk={{zookeepers}}/mesos \
+    --cluster=fox
 
