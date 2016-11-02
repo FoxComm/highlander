fc61cd8a09b8085147f483366cf9d425a02d1f08
Pavel Kiselev <kpashka@gmail.com>
2016-11-02 17:22:32 +0700

    Make Docker build for Shipstation consumer

--- integrations/shipstation/lib/shipstation/payloads/product.go
+++ integrations/shipstation/lib/shipstation/payloads/product.go
@@ -1,6 +1,6 @@
 package payloads
 
-import "github.com/FoxComm/shipstation/lib/shipstation/responses"
+import "github.com/FoxComm/highlander/integrations/shipstation/lib/shipstation/responses"
 
 // Product is the payload taking by the update product endpoint.
 type Product struct {
