81a3240ba83566d0cc9d8c514f3285f07e174327
Josh Ferrerra <josh@foxcommerce.com>
2016-11-02 17:00:49 -0400

    [wip] add module to create token claim

---
+++ solomon/web/services/jwt_claims.ex
@@ -0,0 +1,56 @@
+defmodule Solomon.JWTClaims do
+  import Ecto.Query
+  alias Solomon.Repo
+  alias Solomon.Account
+  alias Solomon.AccountRole
+  alias Solomon.Role
+  alias Solomon.RolePermission
+  alias Solomon.Permission
+  alias Solomon.User
+
+  def token_claim(account_id) do
+    account = Repo.get!(Account, account_id)
+    {claims, roles, scope} = get_claims_roles_scope(account_id)
+    user = Repo.get_by!(User, account_id: account_id)
+    %{
+      "aud" => "user",
+      "email" => user.email,
+      "ratchet" => account.ratchet,
+      "id" => account_id,
+      "scope" => scope, # TODO : this should be scope_path
+      "roles" => roles,
+      "name" => user.name,
+      "claims" => claims,
+      "iss" => "FC",
+      "exp" => 0 # TODO : get an exp
+    }
+  end
+
+  defp get_claims_roles_scope(account_id) do
+    Repo.all(
+      from ar in Solomon.AccountRole,
+      where: ar.account_id == ^account_id,
+      join: r in Role,
+      on: ar.role_id == r.id,
+      join: rp in RolePermission,
+      on: rp.role_id == r.id,
+      join: p in Permission,
+      on: p.id == rp.permission_id,
+      select: {p, r.name}
+    )
+    |> Enum.unzip
+    |> process_perms_and_roles
+
+  end
+
+  defp process_perms_and_roles({permissions, role_names}) do
+    {
+      Enum.map(permissions, fn p -> %{p.frn => p.actions} end)
+      |> Enum.reduce(fn (x, y) -> Map.merge(x, y) end),
+      Enum.uniq(role_names),
+      Enum.map(permissions, fn p -> p.scope_id end)
+      |> Enum.min
+    }
+  end
+
+end
