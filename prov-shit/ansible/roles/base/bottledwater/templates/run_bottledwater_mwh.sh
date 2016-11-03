fd8fe3a35d39a4bf9520bbd36263cb4b59d89750
tony.pizzicato <tony.grebnev@gmail.com>
2016-11-03 16:37:49 +0300

    bottledwater cleanup

--- prov-shit/ansible/roles/base/bottledwater/templates/run_bottledwater_mwh.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-/usr/local/bin/bottledwater \
-    --allow-unkeyed \
-    --slot {{bottledwater_slot_middlewarehouse}} \
-    --postgres postgres://{{middlewarehouse_db_user}}@{{db_host}}/{{middlewarehouse_db_name}} \
-    --broker {{kafka_server}} \
-    --schema-registry "http://{{schema_server}}"
