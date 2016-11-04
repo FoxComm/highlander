01954c6886db507d7ee4bbb4a253e3b791bf9442
Maxim Khailo <mempko@gmail.com>
2016-11-03 17:31:14 -0700

    Revert "Taxon-product links"

--- phoenix-scala/sql/V4.062__product_taxon_links.sql
+++
@@ -1,6 +0,0 @@
-
-alter table product_taxonomy_links rename to product_taxon_links;
-
-alter index product_taxonomy_link_left_idx rename to product_taxon_link_left_idx; 
-alter index product_taxonomy_link_right_idx rename to product_taxon_link_right_idx; 
-
