dc3f07c1a28309b23a33b1628f67bba0ec16e13e
Maxim Khailo <max@foxcommerce.com>
2016-11-07 15:16:59 -0800

    Removing old shipstation consumer.
    We will move the new one here some other time.

--- integrations/shipstation/lib/shipstation/payloads/insurance_options.go
+++
@@ -1,8 +0,0 @@
-package payloads
-
-// InsuranceOptions represent how a package can be insured.
-type InsuranceOptions struct {
-	Provider       string
-	InsureShipment bool
-	InsuredValue   float64
-}
--- integrations/shipstation/lib/shipstation/responses/product_category.go
+++
@@ -1,7 +0,0 @@
-package responses
-
-// ProductCategory represents the API response for a product category.
-type ProductCategory struct {
-	ID   int `json:"categoryId"`
-	Name string
-}
