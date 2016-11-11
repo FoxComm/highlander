f87256e1e276cf7449f637c31ba3d8d198acd3ac
Aelejandro Corredor <nautiluz92@Aelejandros-MacBook-Pro.local>
2016-11-11 14:37:42 -0500

    fix broken build of shipment consumer

--- middlewarehouse/consumers/capture/main.go
+++ middlewarehouse/consumers/capture/main.go
@@ -3,9 +3,10 @@ package main
 import (
 	"log"
 
-	"github.com/FoxComm/metamorphosis"
 	"github.com/FoxComm/highlander/middlewarehouse/consumers"
 	"github.com/FoxComm/highlander/middlewarehouse/consumers/capture/lib"
+	"github.com/FoxComm/highlander/middlewarehouse/shared"
+	"github.com/FoxComm/metamorphosis"
 
 	_ "github.com/jpfuentes2/go-env/autoload"
 )
@@ -21,7 +22,7 @@ func main() {
 		log.Fatalf("Unable to initialize consumer with error: %s", err.Error())
 	}
 
-	capConf, err := MakeCaptureConsumerConfig()
+	capConf, err := shared.MakeCaptureConsumerConfig()
 	if err != nil {
 		log.Fatalf("Unable to initialize consumer with error: %s", err.Error())
 	}
