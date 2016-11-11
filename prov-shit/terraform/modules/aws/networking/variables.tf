c5a969327a1e1627c9aec5dc6c74d7115b0615f5
Alex Kreskiyan <a.kreskiyan@gmail.com>
2016-11-11 15:43:56 +0300

    Revert "Formatted aws networking"
    This reverts commit ccc56c3988203bc31e3e2a7eb4f462d29613afb8.

--- prov-shit/terraform/modules/aws/networking/variables.tf
+++ prov-shit/terraform/modules/aws/networking/variables.tf
@@ -1,33 +1,26 @@
 variable "ip_range" {
-    default = "0.0.0.0/0" # Allow from any ip. We are remote.
+  default = "0.0.0.0/0" # Allow from any ip. We are remote.
 }
 
 variable "availability_zones" {
-    type    = "list"
-    # No spaces allowed between az names!
-    default = [
-        "us-west-2a",
-        "us-west-2b",
-        "us-west-2c"
-    ]
+  # No spaces allowed between az names!
+  default = ["us-west-2a", "us-west-2b", "us-west-2c"]
 }
 
 variable "vpc_cidr" {
-    description = "CIDR for the whole VPC"
-    default     = "10.0.0.0/16"
+  description = "CIDR for the whole VPC"
+  default = "10.0.0.0/16"
 }
 
 variable "public_subnet_cidr" {
-    description = "CIDR for the Public Subnet"
-    default     = "10.0.0.0/24"
+  description = "CIDR for the Public Subnet"
+  default = "10.0.0.0/24"
 }
 
 variable "private_subnet_cidr" {
-    description = "CIDR for the Private Subnet"
-    default     = "10.0.1.0/24"
+  description = "CIDR for the Private Subnet"
+  default = "10.0.1.0/24"
 }
 
-variable "name" {
-}
-variable "key_name" {
-}
+variable "name" {}
+variable "key_name" {}
--- prov-shit/terraform/modules/aws/networking/vpc.tf
+++ prov-shit/terraform/modules/aws/networking/vpc.tf
@@ -1,26 +1,27 @@
+
 ###############################
 # Setup VPC
 ###############################
 
 resource "aws_vpc" "default" {
-    cidr_block           = "${var.vpc_cidr}"
-    enable_dns_hostnames = true
+  cidr_block = "${var.vpc_cidr}"
+  enable_dns_hostnames = true
 
-    tags {
-        name = "${var.name}"
-    }
+  tags {
+      name = "${var.name}"
+  }
 }
 
 resource "aws_internet_gateway" "default" {
-    vpc_id = "${aws_vpc.default.id}"
+  vpc_id = "${aws_vpc.default.id}"
 
-    tags {
-        Name = "terraform_igw"
-    }
+  tags {
+      Name = "terraform_igw"
+  }
 }
 
 output "vpc_id" {
-    value = "${aws_vpc.default.id}"
+  value = "${aws_vpc.default.id}"
 }
 
 ###############################
@@ -28,35 +29,35 @@ output "vpc_id" {
 ###############################
 
 resource "aws_subnet" "public" {
-    vpc_id            = "${aws_vpc.default.id}"
-    cidr_block        = "${var.public_subnet_cidr}"
-    availability_zone = "${element(var.availability_zones, 0)}"
+  vpc_id = "${aws_vpc.default.id}"
+  cidr_block = "${var.public_subnet_cidr}"
+  availability_zone = "${element(var.availability_zones, 0)}"
 
-    tags {
-        Name = "terraform_public_subnet"
-    }
+  tags {
+      Name = "terraform_public_subnet"
+  }
 }
 
 output "public_subnet_id" {
-    value = "${aws_subnet.public.id}"
+  value = "${aws_subnet.public.id}"
 }
 
 resource "aws_route_table" "public" {
-    vpc_id = "${aws_vpc.default.id}"
+  vpc_id = "${aws_vpc.default.id}"
 
-    route {
-        cidr_block = "0.0.0.0/0"
-        gateway_id = "${aws_internet_gateway.default.id}"
-    }
+  route {
+      cidr_block = "0.0.0.0/0"
+      gateway_id = "${aws_internet_gateway.default.id}"
+  }
 
-    tags {
-        Name = "terraform_public_subnet_route_table"
-    }
+  tags {
+      Name = "terraform_public_subnet_route_table"
+  }
 }
 
 resource "aws_route_table_association" "public" {
-    subnet_id      = "${aws_subnet.public.id}"
-    route_table_id = "${aws_route_table.public.id}"
+  subnet_id = "${aws_subnet.public.id}"
+  route_table_id = "${aws_route_table.public.id}"
 }
 
 ###############################
@@ -64,32 +65,32 @@ resource "aws_route_table_association" "public" {
 ###############################
 
 resource "aws_subnet" "private" {
-    vpc_id            = "${aws_vpc.default.id}"
-    cidr_block        = "${var.private_subnet_cidr}"
-    availability_zone = "${element(var.availability_zones, 0)}"
-    tags {
-        Name = "terraform_private_subnet"
-    }
+  vpc_id = "${aws_vpc.default.id}"
+  cidr_block = "${var.private_subnet_cidr}"
+  availability_zone = "${element(var.availability_zones, 0)}"
+  tags {
+      Name = "terraform_private_subnet"
+  }
 }
 
 output "private_subnet_id" {
-    value = "${aws_subnet.private.id}"
+  value = "${aws_subnet.private.id}"
 }
 
 resource "aws_route_table" "private" {
-    vpc_id = "${aws_vpc.default.id}"
+  vpc_id = "${aws_vpc.default.id}"
 
-    route {
-        cidr_block  = "0.0.0.0/0"
-        instance_id = "${aws_instance.nat.id}"
-    }
+  route {
+      cidr_block = "0.0.0.0/0"
+      instance_id = "${aws_instance.nat.id}"
+  }
 
-    tags {
-        Name = "terraform_private_subnet_route_table"
-    }
+  tags {
+      Name = "terraform_private_subnet_route_table"
+  }
 }
 
 resource "aws_route_table_association" "private" {
-    subnet_id      = "${aws_subnet.private.id}"
-    route_table_id = "${aws_route_table.private.id}"
+  subnet_id = "${aws_subnet.private.id}"
+  route_table_id = "${aws_route_table.private.id}"
 }
