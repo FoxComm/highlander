3fabc121dbf9d48fd161c0311a228c3a671f922c
Jeff Mataya <jeff.mataya@gmail.com>
2016-11-15 18:21:06 -0800

    Fix reference to green river jar

--- prov-shit/ansible/roles/dev/greenriver/templates/create_es_mappings.sh
+++ prov-shit/ansible/roles/dev/greenriver/templates/create_es_mappings.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run --network host -t {{docker_registry}}:5000/greenriver:{{docker_branch}} java -Denv={{greenriver_env}} -D{{greenriver_env}}.kafka.broker={{kafka_server}} -D{{greenriver_env}}.elastic.host=elasticsearch://{{search_server}} -D{{greenriver_env}}.activity.phoenix.url=http://{{phoenix_server}}/v1 -D{{greenriver_env}}.avro.schemaRegistryUrl=http://{{schema_server}} -D{{greenriver_env}}.elastic.setup=true -jar {{greenriver_jar}}
+docker run --network host -t {{docker_registry}}:5000/greenriver:{{docker_branch}} java -Denv={{greenriver_env}} -D{{greenriver_env}}.kafka.broker={{kafka_server}} -D{{greenriver_env}}.elastic.host=elasticsearch://{{search_server}} -D{{greenriver_env}}.activity.phoenix.url=http://{{phoenix_server}}/v1 -D{{greenriver_env}}.avro.schemaRegistryUrl=http://{{schema_server}} -D{{greenriver_env}}.elastic.setup=true -jar /green-river/green-river.jar
