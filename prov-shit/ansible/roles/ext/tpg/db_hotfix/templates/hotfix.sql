4b47a268a4254e8b041bc3da48a4bb8aa4d8dbe6
Pavel Kiselev <kpashka@gmail.com>
2016-11-17 19:46:55 +0700

    Additional hotfix

--- prov-shit/ansible/roles/ext/tpg/db_hotfix/templates/hotfix.sql
+++ prov-shit/ansible/roles/ext/tpg/db_hotfix/templates/hotfix.sql
@@ -1,10 +1,22 @@
+-- Reasons
 INSERT INTO reasons ("reason_type", "store_admin_id", "body") VALUES ('giftCardCreation', 1, 'Generic Gift Card Creation Reason');
 INSERT INTO reasons ("reason_type", "store_admin_id", "body") VALUES ('storeCreditCreation', 1, 'Generic Store Credit Creation Reason');
 INSERT INTO reasons ("reason_type", "store_admin_id", "body") VALUES ('cancellation', 1, 'Generic Cancellation Reason');
 
-DELETE FROM scopes WHERE id = 2 AND id = 3;
-DELETE FROM scope_domains WHERE id = 2 AND id = 3;
+-- Scopes
+DELETE FROM scopes WHERE id IN (2, 3);
+DELETE FROM scope_domains WHERE id IN (2, 3);
 
 INSERT INTO scopes ("source", "parent_id", "parent_path") VALUES ('org', 1, 1)
 INSERT INTO scope_domains ("scope_id", "domain")
     SELECT max(id), 'theperfectgourmet.com' FROM scopes;
+
+-- Remove useless admins
+DELETE FROM admin_data WHERE id IN (2, 3);
+DELETE FROM users WHERE id IN (2, 3);
+
+DELETE FROM account_access_methods WHERE id IN (2, 3);
+DELETE FROM account_organizations WHERE id IN (2, 3);
+DELETE FROM account_roles WHERE id IN (2, 3);
+DELETE FROM accounts WHERE id IN (2, 3);
+DELETE FROM store_admins_search_view WHERE id IN (2, 3);
