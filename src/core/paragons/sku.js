6126b59740ee46942700bfc338831719b7b637a4
Andrey Rublev <version.ru@gmail.com>
2016-11-18 17:51:00 +0700

    better check for gift card type

--- src/core/paragons/sku.js
+++ src/core/paragons/sku.js
@@ -1,4 +1,7 @@
+import _ from 'lodash';
+
+const giftCardAttrs = ['message', 'senderName', 'recipientName', 'recipientEmail'];
 
 export function isGiftCard(sku) {
-  return sku.name === 'GiftCard';
+  return _.every(giftCardAttrs, name => name in sku.attributes);
 }
