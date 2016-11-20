32735f3cfd34ab6ed7271067cd6f7d20f3811d4f
Alexandra Rodionova <alexa.rodionova@gmail.com>
2016-11-20 18:00:42 +0300

    move product filter to products component

--- src/components/products-list/products-list.jsx
+++ src/components/products-list/products-list.jsx
@@ -3,89 +3,24 @@
 // libs
 import _ from 'lodash';
 import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { browserHistory } from 'react-router';
-import { autobind } from 'core-decorators';
 
 // styles
 import styles from './products-list.css';
 
 // components
 import ListItem from '../products-item/list-item';
-import ProductTypeSelector from 'ui/product-type-selector';
 import Loader from 'ui/loader';
 
 // types
 import type { HTMLElement } from 'types';
-// import type { Product } from 'modules/products';
-
-type Category = {
-  name: string;
-  id: number;
-  description: string;
-};
 
 type Props = {
   list: ?Array<Object>,
-  categories: ?Array<Category>,
-  categoryName: ?string,
-  productType: ?string,
   isLoading: ?boolean,
 };
 
-const mapStateToProps = state => ({
-  categories: state.categories.list,
-});
-
-const productTypes = [
-  'All',
-  'Poultry',
-  'Seafood',
-  'Beef',
-  'Vegetarian',
-];
-
-const defaultProductType = productTypes[0];
-
 class ProductsList extends Component {
   props: Props;
-
-  renderHeader() {
-    const props = this.props;
-    const { categoryName, categories } = props;
-
-    const realCategoryName =
-      decodeURIComponent(categoryName || '').toUpperCase().replace(/-/g, ' ');
-
-    const category = _.find(categories, {
-      name: realCategoryName,
-    });
-
-    if (!category || !categoryName ||
-        (categoryName.toLowerCase() === defaultProductType.toLowerCase())) {
-      return;
-    }
-
-
-    const description = (category && category.description)
-      ? <p styleName="description">{category.description}</p>
-      : '';
-    const bgImageStyle = category.imageUrl ?
-      { backgroundImage: `url(${category.imageUrl})` } : {};
-
-    const className = `header-${categoryName}`;
-
-    return (
-      <header styleName={className}>
-        <div styleName="header-wrap" style={bgImageStyle}>
-          <div styleName="text-wrap">
-            <span styleName="description">{description}</span>
-            <h1 styleName="title">{category.name}</h1>
-          </div>
-        </div>
-      </header>
-    );
-  }
 
   getItemList() {
     return _.map(this.props.list, (item) => {
@@ -99,47 +34,21 @@ class ProductsList extends Component {
     });
   }
 
-  @autobind
-  onDropDownItemClick (productType = '') {
-    const { categoryName = defaultProductType.toUpperCase() } = this.props;
-
-    if (productType.toLowerCase() !== defaultProductType.toLowerCase()) {
-      browserHistory.push(`/${categoryName}/${productType.toUpperCase()}`);
-    } else {
-      browserHistory.push(`/${categoryName}`);
-    }
-  }
-
   render() : HTMLElement {
     const props = this.props;
-    const { productType, isLoading } = props;
+    const { isLoading } = props;
     const items = props.list && props.list.length > 0
       ? this.getItemList()
       : <div styleName="not-found">No products found.</div>;
 
-    const type = (productType && !_.isEmpty(productType))
-      ? productType.toUpperCase()
-      : productTypes[0];
+    if (isLoading) return <Loader />;
 
     return (
-      <section styleName="catalog">
-        {this.renderHeader()}
-        <div styleName="dropdown">
-          <ProductTypeSelector
-            items={productTypes}
-            activeItem={type}
-            onItemClick={this.onDropDownItemClick}
-          />
-        </div>
-
-        {isLoading ?
-          <Loader /> :
-          <div styleName="list">
-            {items}
-          </div>}
-      </section>
+      <div styleName="list">
+        {items}
+      </div>
     );
   }
 }
 
-export default connect(mapStateToProps, {})(ProductsList);
+export default ProductsList;
--- src/pages/catalog/products.jsx
+++ src/pages/catalog/products.jsx
@@ -1,28 +1,51 @@
 /* @flow */
 
+// libs
+import _ from 'lodash';
 import React, { Component } from 'react';
 import type { HTMLElement } from 'types';
-// import type { Product } from 'modules/products';
+import { browserHistory } from 'react-router';
+import { autobind } from 'core-decorators';
 import { connect } from 'react-redux';
 
+// components
 import ProductsList from '../../components/products-list/products-list';
+import ProductTypeSelector from 'ui/product-type-selector';
 
+// styles
 import styles from './products.css';
 
 import * as actions from 'modules/products';
 
 type Params = {
   categoryName: ?string,
-  productType: ?string
+  productType: ?string,
+};
+
+type Category = {
+  name: string,
+  id: number,
+  description: string,
 };
 
 type Props = {
   params: Params,
   list: Array<Object>,
+  categories: ?Array<Category>,
   isLoading: boolean,
   fetch: Function,
   location: any,
 };
+
+const productTypes = [
+  'All',
+  'Poultry',
+  'Seafood',
+  'Beef',
+  'Vegetarian',
+];
+
+const defaultProductType = productTypes[0];
 
 const mapStateToProps = state => {
   const async = state.asyncActions.products;
@@ -30,6 +53,7 @@ const mapStateToProps = state => {
   return {
     ...state.products,
     isLoading: !!async ? async.inProgress : true,
+    categories: state.categories.list,
   };
 };
 
@@ -58,14 +82,77 @@ class Products extends Component {
     return isNaN(id) ? null : id;
   }
 
-  render(): HTMLElement {
+  @autobind
+  onDropDownItemClick (productType = '') {
+    const { categoryName = defaultProductType.toUpperCase() } = this.props.params;
+
+    if (productType.toLowerCase() !== defaultProductType.toLowerCase()) {
+      browserHistory.push(`/${categoryName}/${productType.toUpperCase()}`);
+    } else {
+      browserHistory.push(`/${categoryName}`);
+    }
+  }
+
+  renderHeader() {
+    const props = this.props;
+    const { categories } = props;
+    const { categoryName } = props.params;
+
+    const realCategoryName =
+      decodeURIComponent(categoryName || '').toUpperCase().replace(/-/g, ' ');
+
+    const category = _.find(categories, {
+      name: realCategoryName,
+    });
+
+    if (!category || !categoryName ||
+      (categoryName.toLowerCase() === defaultProductType.toLowerCase())) {
+      return;
+    }
+
+    const description = (category && category.description)
+      ? <p styleName="description">{category.description}</p>
+      : '';
+    const bgImageStyle = category.imageUrl ?
+    { backgroundImage: `url(${category.imageUrl})` } : {};
+
+    const className = `header-${categoryName}`;
+
     return (
-      <ProductsList
-        list={this.props.list}
-        categoryName={this.props.params.categoryName}
-        productType={this.props.params.productType}
-        isLoading={this.props.isLoading}
-      />
+      <header styleName={className}>
+        <div styleName="header-wrap" style={bgImageStyle}>
+          <div styleName="text-wrap">
+            <span styleName="description">{description}</span>
+            <h1 styleName="title">{category.name}</h1>
+          </div>
+        </div>
+      </header>
+    );
+  }
+
+  render(): HTMLElement {
+    const { productType } = this.props.params;
+
+    const type = (productType && !_.isEmpty(productType))
+      ? productType.toUpperCase()
+      : productTypes[0];
+
+    return (
+      <section styleName="catalog">
+        {this.renderHeader()}
+        <div styleName="dropdown">
+          <ProductTypeSelector
+            items={productTypes}
+            activeItem={type}
+            onItemClick={this.onDropDownItemClick}
+          />
+        </div>
+
+        <ProductsList
+          list={this.props.list}
+          isLoading={this.props.isLoading}
+        />
+      </section>
     );
   }
 }
