1727ce50d0a9ca90adf156d5537f5f5210f1fca2
Jeff Mataya <jeff.mataya@gmail.com>
2016-11-19 18:16:47 -0800

    Provision base consul with Terraform

---
+++ prov-shit/terraform/base/aws_dev/main.tf
@@ -0,0 +1,66 @@
+variable "aws_access_key" {}
+
+variable "aws_secret_key" {}
+
+variable "aws_ssh_user" {}
+
+variable "aws_ssh_private_key" {}
+
+variable "key_name" {}
+
+variable "region" {}
+
+variable "subnet_id" {}
+
+variable "amigo_image" {}
+
+variable "amigo_instance_type" {}
+
+variable "amigo_leader_datacenter" {}
+
+variable "amigo_leader_image" {}
+
+variable "amigo_security_groups" {
+  type = "list"
+}
+
+variable "amigo_volume_size" {}
+
+provider "aws" {
+  access_key = "${var.aws_access_key}"
+  secret_key = "${var.aws_secret_key}"
+  region     = "${var.region}"
+}
+
+resource "aws_instance" "amigo_leader" {
+  ami                    = "${var.amigo_leader_image}"
+  instance_type          = "${var.amigo_instance_type}"
+  key_name               = "${var.key_name}"
+  subnet_id              = "${var.subnet_id}"
+  vpc_security_group_ids = "${var.amigo_security_groups}"
+
+  associate_public_ip_address = false
+
+  root_block_device {
+    volume_type = "standard"
+    volume_size = "${var.amigo_volume_size}"
+  }
+
+  tags = {
+    Name = "${var.amigo_leader_datacenter} amigo leader"
+  }
+
+  connection {
+    type        = "ssh"
+    user        = "${var.aws_ssh_user}"
+    private_key = "${file(var.aws_ssh_private_key)}"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "/usr/local/bin/bootstrap_consul_aws.sh ${var.amigo_leader_datacenter} ${aws_instance.amigo_leader.0.private_ip}",
+      "sudo su -c 'echo 1 > /var/lib/zookeeper/myid'",
+      "sudo rm -rf /var/consul/* && sudo systemctl restart consul_server.service"
+    ]
+  }
+}
---
+++ prov-shit/terraform/envs/aws_dev/dev.tfvars
@@ -0,0 +1,11 @@
+region = "us-west-2"
+key_name = "goldfish"
+
+subnet_id = "subnet-9794cdf3"
+amigo_image = "ami-e9a10d89"
+amigo_instance_type = "m4.large"
+amigo_security_groups = ["sg-60051106", "sg-7905111f", "sg-a021d3d9", "sg-0f38ca76"]
+amigo_volume_size = "20"
+
+amigo_leader_datacenter = "dev"
+amigo_leader_image = "ami-84bd11e4"
