6c5a58fadac6c169ae3aeac8e3bd0d299fd169c1
Alex Kreskiyan <a.kreskiyan@gmail.com>
2016-11-20 12:17:14 +0300

    Added keys per env

---
+++ prov-shit/ansible/roles/base/secret_keys/keygen.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -ue
+
+DIR=ansible/roles/base/secret_keys/files/secret_keys/$1
+if [[ ! -d $DIR ]]; then
+  mkdir $DIR
+fi
+
+openssl genrsa -out $DIR/private_key.pem 4096
+openssl pkcs8 -topk8 -inform PEM -outform DER -in $DIR/private_key.pem -out $DIR/private_key.der -nocrypt
+openssl rsa -in $DIR/private_key.pem -pubout -outform DER -out $DIR/public_key.der
+openssl rsa -in $DIR/private_key.pem -pubout -outform PEM -out $DIR/public_key.pem
+
+ansible-vault encrypt $DIR/private_key.pem
+ansible-vault encrypt $DIR/public_key.pem
+ansible-vault encrypt $DIR/private_key.der
+ansible-vault encrypt $DIR/public_key.der
