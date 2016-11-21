6cc7fea135a31ea06e0ec5bc8a8f7b322d26388d
Alex Kreskiyan <a.kreskiyan@gmail.com>
2016-11-21 23:04:05 +0300

    Using IException for summary_repository

--- middlewarehouse/repositories/summary_repository.go
+++ middlewarehouse/repositories/summary_repository.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/FoxComm/highlander/middlewarehouse/models"
 
+	"github.com/FoxComm/highlander/middlewarehouse/common/exceptions"
 	"github.com/jinzhu/gorm"
 )
 
@@ -12,6 +13,7 @@ const (
 	ErrorSummaryNotFound              = "Summary with id=%d not found"
 	ErrorSummaryForSKUNotFound        = "Summary for sku=%s not found"
 	ErrorSummaryForItemByTypeNotFound = "Summary for stock item with id=%d and type=%s not found"
+	summaryEntity                     = "summary"
 )
 
 type summaryRepository struct {
@@ -19,22 +21,22 @@ type summaryRepository struct {
 }
 
 type ISummaryRepository interface {
-	GetSummary() ([]*models.StockItemSummary, error)
-	GetSummaryBySKU(sku string) ([]*models.StockItemSummary, error)
+	GetSummary() ([]*models.StockItemSummary, exceptions.IException)
+	GetSummaryBySKU(sku string) ([]*models.StockItemSummary, exceptions.IException)
 
-	GetSummaryItemByType(stockItemId uint, unitType models.UnitType) (*models.StockItemSummary, error)
+	GetSummaryItemByType(stockItemId uint, unitType models.UnitType) (*models.StockItemSummary, exceptions.IException)
 
-	CreateStockItemSummary(summary []*models.StockItemSummary) error
-	UpdateStockItemSummary(summary *models.StockItemSummary) error
+	CreateStockItemSummary(summary []*models.StockItemSummary) exceptions.IException
+	UpdateStockItemSummary(summary *models.StockItemSummary) exceptions.IException
 
-	CreateStockItemTransaction(transaction *models.StockItemTransaction) error
+	CreateStockItemTransaction(transaction *models.StockItemTransaction) exceptions.IException
 }
 
 func NewSummaryRepository(db *gorm.DB) ISummaryRepository {
 	return &summaryRepository{db}
 }
 
-func (repository *summaryRepository) GetSummary() ([]*models.StockItemSummary, error) {
+func (repository *summaryRepository) GetSummary() ([]*models.StockItemSummary, exceptions.IException) {
 	summary := []*models.StockItemSummary{}
 	err := repository.db.
 		Preload("StockItem").
@@ -43,10 +45,10 @@ func (repository *summaryRepository) GetSummary() ([]*models.StockItemSummary, e
 		Find(&summary).
 		Error
 
-	return summary, err
+	return summary, NewDatabaseException(err)
 }
 
-func (repository *summaryRepository) GetSummaryBySKU(sku string) ([]*models.StockItemSummary, error) {
+func (repository *summaryRepository) GetSummaryBySKU(sku string) ([]*models.StockItemSummary, exceptions.IException) {
 	summary := []*models.StockItemSummary{}
 	err := repository.db.
 		Preload("StockItem").
@@ -58,33 +60,33 @@ func (repository *summaryRepository) GetSummaryBySKU(sku string) ([]*models.Stoc
 		Error
 
 	if len(summary) == 0 {
-		return nil, fmt.Errorf(ErrorSummaryForSKUNotFound, sku)
+		return nil, NewSummaryForSKUNotFoundException(sku, fmt.Errorf(ErrorSummaryForSKUNotFound, sku))
 	}
 
-	return summary, err
+	return summary, NewDatabaseException(err)
 }
 
-func (repository *summaryRepository) GetSummaryItemByType(stockItemId uint, unitType models.UnitType) (*models.StockItemSummary, error) {
+func (repository *summaryRepository) GetSummaryItemByType(stockItemId uint, unitType models.UnitType) (*models.StockItemSummary, exceptions.IException) {
 	summary := &models.StockItemSummary{}
 	result := repository.db.Where("stock_item_id = ? AND type = ?", stockItemId, unitType).First(summary)
 
 	if result.Error != nil {
 		if result.Error == gorm.ErrRecordNotFound {
-			return nil, fmt.Errorf(ErrorSummaryForItemByTypeNotFound, stockItemId, unitType)
+			return nil, NewSummaryForItemByTypeNotFoundException(stockItemId, unitType, fmt.Errorf(ErrorSummaryForItemByTypeNotFound, stockItemId, unitType))
 		}
 
-		return nil, result.Error
+		return nil, NewDatabaseException(result.Error)
 	}
 
 	return summary, nil
 }
 
-func (repository *summaryRepository) CreateStockItemSummary(summary []*models.StockItemSummary) error {
+func (repository *summaryRepository) CreateStockItemSummary(summary []*models.StockItemSummary) exceptions.IException {
 	txn := repository.db.Begin()
 
 	for _, item := range summary {
 		// use `UPDATE SET stock_item_id = '%d'` as postgres driver does not return anything on `DO NOTHING`
-		// resulting in 'no rows in result set' sql error
+		// resulting in 'no rows in result set' sql exceptions.IException
 		onConflict := fmt.Sprintf(
 			"ON CONFLICT (stock_item_id, type) DO UPDATE SET stock_item_id = '%d'",
 			item.StockItemID,
@@ -92,23 +94,61 @@ func (repository *summaryRepository) CreateStockItemSummary(summary []*models.St
 
 		if err := txn.Set("gorm:insert_option", onConflict).Create(item).Error; err != nil {
 			txn.Rollback()
-			return err
+			return NewDatabaseException(err)
 		}
 	}
 
-	return txn.Commit().Error
+	return NewDatabaseException(txn.Commit().Error)
 }
 
-func (repository *summaryRepository) UpdateStockItemSummary(summary *models.StockItemSummary) error {
+func (repository *summaryRepository) UpdateStockItemSummary(summary *models.StockItemSummary) exceptions.IException {
 	err := repository.db.Save(summary).Error
 
 	if err == gorm.ErrRecordNotFound {
-		return fmt.Errorf(ErrorSummaryNotFound, summary.ID)
+		return NewEntityNotFoundException(summaryEntity, string(summary.ID), fmt.Errorf(ErrorSummaryNotFound, summary.ID))
 	}
 
-	return err
+	return NewDatabaseException(err)
 }
 
-func (repository *summaryRepository) CreateStockItemTransaction(transaction *models.StockItemTransaction) error {
-	return repository.db.Create(transaction).Error
+func (repository *summaryRepository) CreateStockItemTransaction(transaction *models.StockItemTransaction) exceptions.IException {
+	return NewDatabaseException(repository.db.Create(transaction).Error)
+}
+
+type summaryForSkuNotFoundException struct {
+	cls string `json:"type"`
+	sku string
+	exceptions.Exception
+}
+
+func NewSummaryForSKUNotFoundException(sku string, error error) exceptions.IException {
+	if error == nil {
+		return nil
+	}
+
+	return summaryForSkuNotFoundException{
+		cls:       "summaryForSkuNotFound",
+		sku:       sku,
+		Exception: exceptions.Exception{error},
+	}
+}
+
+type summaryForItemByTypeNotFoundException struct {
+	cls      string `json:"type"`
+	item     uint
+	unitType models.UnitType
+	exceptions.Exception
+}
+
+func NewSummaryForItemByTypeNotFoundException(item uint, unitType models.UnitType, error error) exceptions.IException {
+	if error == nil {
+		return nil
+	}
+
+	return summaryForItemByTypeNotFoundException{
+		cls:       "summaryForSkuNotFound",
+		item:      item,
+		unitType:  unitType,
+		Exception: exceptions.Exception{error},
+	}
 }
