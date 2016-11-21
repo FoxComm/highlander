ffffe75b559074cd649c1863de2566e913267e6d
Alex Kreskiyan <a.kreskiyan@gmail.com>
2016-11-22 00:07:01 +0300

    Fixed common/async to work with IException

--- middlewarehouse/common/async/async.go
+++ middlewarehouse/common/async/async.go
@@ -1,17 +1,21 @@
 package async
 
-import "log"
+import (
+	"log"
+
+	"github.com/FoxComm/highlander/middlewarehouse/common/exceptions"
+)
 
 // AsyncFn is a function that may be executed asynchronously (through a GoRoutine).
 // It returns an error, with is always nil if run async, but used if run synchronously.
-type AsyncFn func() error
+type AsyncFn func() exceptions.IException
 
 // MaybeExecAsync will conditionally run a function synchronously or asynchronously.
-func MaybeExecAsync(fn AsyncFn, isAsync bool, logMsg string) error {
+func MaybeExecAsync(fn AsyncFn, isAsync bool, logMsg string) exceptions.IException {
 	if isAsync {
 		go func() {
 			if err := fn(); err != nil {
-				log.Printf("%s: %s", logMsg, err.Error())
+				log.Printf("%s: %s", logMsg, err.ToString())
 			}
 		}()
 		return nil
