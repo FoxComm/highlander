dde43ff98284df564dfb9f2838bf00a0704039ec
Eugene Sypachev (Axblade) <eugene.sypachev@gmail.com>
2016-11-22 19:52:02 +0300

    remove extra categories

--- src/core/modules/categories.js
+++ src/core/modules/categories.js
@@ -35,18 +35,6 @@ function apiCall(): Promise {
       description: 'Gift cards will be here',
       imageUrl: '',
     },
-    {
-      id: 5,
-      name: 'FAVORITES',
-      description: '',
-      imageUrl: '',
-    },
-    {
-      id: 6,
-      name: 'HOLIDAY',
-      description: '',
-      imageUrl: '',
-    },
   ];
   return Promise.resolve(result);
 }
