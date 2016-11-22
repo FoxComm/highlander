142a4e36c85d4e217bad506539e5066932f1844e
Jeff Mataya <jeff@foxcommerce.com>
2016-11-22 01:13:03 -0800

    Product and SKU endpoints should update albums (#431)
    * Merge CreateAlbumPayload and UpdateAlbumPayload
    
    Brings the format in line with variants and SKUs and makes it possible
    for us to have an updateOrCreate action in the future.
    
    * Add image creation product creation
    
    * Add image creation to product update
    
    * Update albums on SKU payloads, too
    
    * Album payload fixes in product/SKU tests
    
    * Allow setting the album on dependent SKUs in product creation
    
    * Add tests for updating albums on SKUs
    
    * Fix up Anna's comments
    
    * Fix test failure

--- phoenix-scala/app/payloads/SkuPayloads.scala
+++ phoenix-scala/app/payloads/SkuPayloads.scala
@@ -1,7 +1,10 @@
 package payloads
 
+import payloads.ImagePayloads.AlbumPayload
 import utils.aliases._
 
 object SkuPayloads {
-  case class SkuPayload(attributes: Map[String, Json], schema: Option[String] = None)
+  case class SkuPayload(attributes: Map[String, Json],
+                        schema: Option[String] = None,
+                        albums: Option[Seq[AlbumPayload]] = None)
 }
