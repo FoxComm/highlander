2273a654eef9a513a8a4b1b26de9f80111c64646
Andrey Rublev <version.ru@gmail.com>
2016-11-24 15:15:18 +0700

    tc

--- src/components/profile/blocks/address-form.jsx
+++ src/components/profile/blocks/address-form.jsx
@@ -2,13 +2,23 @@
 import _ from 'lodash';
 import React, { Component } from 'react';
 import { autobind } from 'core-decorators';
-
 import styles from '../profile.css';
+import { connect } from 'react-redux';
 
+import { Link } from 'react-router';
+import Button from 'ui/buttons';
 import EditAddress from 'ui/address/edit-address';
 
 class AddressForm extends Component {
   static title = 'Add Address';
+
+  state = {
+    address: {},
+  };
+
+  get addressId() {
+    return _.get(this.props.routeParams, 'referenceNumber');
+  }
 
   @autobind
   handleSave() {
@@ -18,7 +28,7 @@ class AddressForm extends Component {
   render() {
     return (
       <div>
-        <EditAddress/>
+        <EditAddress address={}/>
         <div styleName="buttons-footer">
           <Button
             styleName="save-button"
