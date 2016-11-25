32316e2b2d842c5b9511ceeeac3d36a14c062c70
Eugene Sypachev (Axblade) <eugene.sypachev@gmail.com>
2016-11-25 12:50:38 +0300

    close button

--- src/components/top-banner/top-banner.jsx
+++ src/components/top-banner/top-banner.jsx
@@ -2,14 +2,23 @@
 
 import React from 'react';
 
+import Icon from 'ui/icon';
+
 import styles from './top-banner.css';
 
 const TopBanner = () => {
   return (
     <div styleName="banner">
-      To celebrate the launch of our new site, use code
-      <strong styleName="strong"> PERFECT20 </strong>
-      and enjoy 20% off sitewide!
+      <div styleName="content">
+        To celebrate the launch of our new site, use code
+        <strong styleName="strong"> PERFECT20 </strong>
+        and enjoy 20% off sitewide!
+      </div>
+      <div styleName="button">
+        <a styleName="close" onClick={() => console.log('click!')}>
+          <Icon name="fc-close" className="close-icon"/>
+        </a>
+      </div>
     </div>
   );
 };
