0b16906f47b61984723c2ba78e21c3e61ee91c3d
Pavel Kiselev <kpashka@gmail.com>
2016-11-30 12:47:23 +0700

    Final fix

--- middlewarehouse/consumers/shipstation/phoenix/full_order.go
+++ middlewarehouse/consumers/shipstation/phoenix/full_order.go
@@ -21,7 +21,7 @@ func NewFullOrderFromHttpResponse(response *http.Response) (*FullOrder, error) {
 	defer response.Body.Close()
 
 	orderResult := new(OrderResult)
-	if err := json.NewDecoder(response.Body).Decode(orderResp); err != nil {
+	if err := json.NewDecoder(response.Body).Decode(orderResult); err != nil {
 		log.Printf("Unable to read order response from Phoenix with error: %s", err.Error())
 		return nil, err
 	}
