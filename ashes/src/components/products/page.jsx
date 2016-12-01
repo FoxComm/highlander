d407b053b4f442bb6c54098faa6ee5c2eedafa24
tony.pizzicato <tony.grebnev@gmail.com>
2016-12-02 02:13:39 +0300

    create new albums/images on product duplication

--- ashes/src/components/products/page.jsx
+++ ashes/src/components/products/page.jsx
@@ -26,6 +26,7 @@ type Props = {
     createProduct: (product: Product) => void,
     fetchProduct: (productId: string, context: string) => Promise,
     productNew: () => void,
+    productDuplicate: () => void,
     updateProduct: (product: Product, context: string) => void,
     archiveProduct: (id: string|number, context: ?string) => Promise,
   },
@@ -162,6 +163,12 @@ class ProductPage extends ObjectPage {
       }, object);
       this.setState({object: newProduct});
     }
+  }
+
+  handleDuplicate() {
+    super.handleDuplicate();
+
+    this.props.actions.productDuplicate();
   }
 
   createEntity(entity) {
