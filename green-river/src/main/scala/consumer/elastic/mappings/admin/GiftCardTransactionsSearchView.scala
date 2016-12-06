2fd852d30dc995f5cabae435af1f440ed8859c27
Jeff Mataya <jeff@foxcommerce.com>
2016-12-07 02:08:18 +0300

    Hotfix mapping to cord ref that is preventing indexing in ES (#541)
    * Fix mapping to cord ref that is preventing indexing in ES
    
    * Ahh, typo!

--- green-river/src/main/scala/consumer/elastic/mappings/admin/GiftCardTransactionsSearchView.scala
+++ green-river/src/main/scala/consumer/elastic/mappings/admin/GiftCardTransactionsSearchView.scala
@@ -22,7 +22,7 @@ final case class GiftCardTransactionsSearchView()(implicit ec: EC) extends AvroT
       field("giftCardCreatedAt", DateType).format(dateFormat),
       // Order Payment
       field("orderPayment").nested(
-          field("orderReferenceNumber", StringType).analyzer("upper_cased"),
+          field("cordReferenceNumber", StringType).analyzer("upper_cased"), 
           field("orderCreatedAt", DateType).format(dateFormat),
           field("orderPaymentCreatedAt", DateType).format(dateFormat)
       ),
--- green-river/src/main/scala/consumer/elastic/mappings/admin/StoreCreditTransactionsSearchView.scala
+++ green-river/src/main/scala/consumer/elastic/mappings/admin/StoreCreditTransactionsSearchView.scala
@@ -22,7 +22,7 @@ final case class StoreCreditTransactionsSearchView()(implicit ec: EC) extends Av
       field("storeCreditCreatedAt", DateType).format(dateFormat),
       // Order Payment
       field("orderPayment").nested(
-          field("orderReferenceNumber", StringType).analyzer("upper_cased"),
+          field("cordReferenceNumber", StringType).analyzer("upper_cased"),
           field("orderCreatedAt", DateType).format(dateFormat),
           field("orderPaymentCreatedAt", DateType).format(dateFormat)
       ),
