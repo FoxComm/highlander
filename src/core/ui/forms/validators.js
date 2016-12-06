f5940b92909fab15b53dadb735950c2b5062d6c7
Steven Novick <steven@foxcommerce.com>
2016-12-06 12:41:34 -0800

    JS Formatting & Var Updates
    Changed Var to const
    Removed trailing spaces, added single quotes, and added semicolons

--- src/core/ui/forms/validators.js
+++ src/core/ui/forms/validators.js
@@ -13,10 +13,10 @@ export function phoneNumber(value) {
 }
 
 export function email(address) {
-  var strLocalRegEx = "^(([^<>()\\[\\]\\\\.,;:\\s@\"]+(\\.[^<>()\\[\\]\\\\.,;:\\s@\"]+)*)|(\".+\"))"
-  var strDomainRegEx = "@((\\[[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}])|(([a-zA-Z\\-0-9]+\\.)+[a-zA-Z]{2,}))$"
+  const strLocalRegEx = '^(([^<>()\\[\\]\\\\.,;:\\s@\"]+(\\.[^<>()\\[\\]\\\\.,;:\\s@\"]+)*)|(\".+\"))';
+  const strDomainRegEx = '@((\\[[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}])|(([a-zA-Z\\-0-9]+\\.)+[a-zA-Z]{2,}))$';
   
-  return RegExp(strLocalRegEx + strDomainRegEx).test(address)                                  
+  return RegExp(strLocalRegEx + strDomainRegEx).test(address)
     ? null
     : 'Invalid email';
 }
