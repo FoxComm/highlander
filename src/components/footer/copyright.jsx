92d42730afb07879743755b248b91d70c11d4e0c
Josh Ferrerra <josh@foxcommerce.com>
2016-12-07 15:13:39 -0500

    checkout auth improvements

---
+++ src/components/footer/copyright.jsx
@@ -0,0 +1,23 @@
+/* @flow */
+
+import React from 'react';
+import type { HTMLElement } from 'types';
+
+import styles from './footer.css';
+
+import { Link } from 'react-router';
+
+const Copyright = () : HTMLElement => {
+  return (
+    <div styleName="copyright">
+      <p>&copy; COPYRIGHT 2016. THE PERFECT GOURMET</p>
+      <ul>
+        <li><Link to="terms-of-use">Terms</Link></li>
+        <li><Link to="privacy-policy">Privacy</Link></li>
+      </ul>
+      <p><a href="http://foxcommerce.com/" target="_blank">POWERED BY FOXCOMMERCE</a>.</p>
+    </div>
+  );
+};
+
+export default Copyright;
