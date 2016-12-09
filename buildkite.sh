b4c98cdd732cf069cd4acb4d4bda8eb8ffe8e34e
Pavel Kiselev <kpashka@gmail.com>
2016-12-09 12:37:25 +0300

    One more polishing thing

--- buildkite.sh
+++ buildkite.sh
@@ -9,12 +9,12 @@ VAGRANT_UP_EXIT_CODE=$?
 echo -e "--- Executing \033[33mvagrant destroy\033[0m"
 vagrant destroy appliance --force
 
-echo "--- Final report"
-
 if [ $VAGRANT_UP_EXIT_CODE -eq 0 ]; then
-    echo -e "Developer appliance creation \033[33msuccess\033[0m"
+    echo -e "--- Appliance creation \033[33msuccess\033[0m"
+    echo "vagrant up exit code: $VAGRANT_UP_EXIT_CODE"
     exit 0
 else
-    echo -e "Developer appliance creation \033[33mfailure\033[0m"
+    echo -e "--- Appliance creation \033[33mfailure\033[0m"
+    echo "vagrant up exit code: $VAGRANT_UP_EXIT_CODE"
     exit 1
 fi
