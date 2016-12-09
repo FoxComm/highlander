946685daf020c18696d2fbdb82d2cfb2c426596e
Jeff Mataya <jeff.mataya@gmail.com>
2016-12-09 06:43:20 +0300

    Mock out the rest of the API

--- middlewarehouse/controllers/sku_controller.go
+++ middlewarehouse/controllers/sku_controller.go
@@ -16,65 +16,93 @@ func NewSKUController() IController {
 
 func (controller *skuController) SetUp(router gin.IRouter) {
 	router.Use(FetchJWT)
+	router.POST("", controller.CreateSKU())
 	router.GET(":id", controller.GetSKUByID())
+	router.PATCH(":id", controller.UpdateSKU())
+	router.GET(":id/afs", controller.GetAFS())
+}
+
+func (controller *skuController) CreateSKU() gin.HandlerFunc {
+	return func(context *gin.Context) {
+		resp := dummyResponse()
+		context.JSON(http.StatusCreated, resp)
+	}
 }
 
 func (controller *skuController) GetSKUByID() gin.HandlerFunc {
 	return func(context *gin.Context) {
-		resp := responses.SKU{
-			ID:               1,
-			Code:             "SKU-TEST",
-			UPC:              "12312342131",
-			Title:            "Some test SKU",
-			UnitCost:         299,
-			TaxClass:         "default",
-			RequiresShipping: true,
-			ShippingClass:    "default",
-			IsReturnable:     true,
-			ReturnWindow: responses.Dimension{
-				Value: 30.0,
-				Units: "days",
-			},
-			Height: responses.Dimension{
-				Value: 15.0,
-				Units: "cm",
-			},
-			Length: responses.Dimension{
-				Value: 10.0,
-				Units: "cm",
-			},
-			Width: responses.Dimension{
-				Value: 5.0,
-				Units: "cm",
-			},
-			Weight: responses.Dimension{
-				Value: 50.0,
-				Units: "g",
-			},
-			RequiresInventoryTracking: true,
-			InventoryWarningLevel: responses.QuantityLevel{
-				IsEnabled: true,
-				Level:     3,
-			},
-			MaximumQuantityInCart: responses.QuantityLevel{
-				IsEnabled: true,
-				Level:     6,
-			},
-			MinimumQuantityInCart: responses.QuantityLevel{
-				IsEnabled: false,
-			},
-			AllowPreorder:       false,
-			AllowBackorder:      false,
-			RequiresLotTracking: true,
-			LotExpirationThreshold: responses.Dimension{
-				Value: 3.0,
-				Units: "months",
-			},
-			LotExpirationWarningThreshold: responses.Dimension{
-				Value: 15.0,
-				Units: "days",
-			},
-		}
+		resp := dummyResponse()
 		context.JSON(http.StatusOK, resp)
+	}
+}
+
+func (controller *skuController) UpdateSKU() gin.HandlerFunc {
+	return func(context *gin.Context) {
+		resp := dummyResponse()
+		context.JSON(http.StatusOK, resp)
+	}
+}
+
+func (controller *skuController) GetAFS() gin.HandlerFunc {
+	return func(context *gin.Context) {
+		resp := map[string]int{"afs": 10}
+		context.JSON(http.StatusOK, resp)
+	}
+}
+
+func dummyResponse() responses.SKU {
+	return responses.SKU{
+		ID:               1,
+		Code:             "SKU-TEST",
+		UPC:              "12312342131",
+		Title:            "Some test SKU",
+		UnitCost:         299,
+		TaxClass:         "default",
+		RequiresShipping: true,
+		ShippingClass:    "default",
+		IsReturnable:     true,
+		ReturnWindow: responses.Dimension{
+			Value: 30.0,
+			Units: "days",
+		},
+		Height: responses.Dimension{
+			Value: 15.0,
+			Units: "cm",
+		},
+		Length: responses.Dimension{
+			Value: 10.0,
+			Units: "cm",
+		},
+		Width: responses.Dimension{
+			Value: 5.0,
+			Units: "cm",
+		},
+		Weight: responses.Dimension{
+			Value: 50.0,
+			Units: "g",
+		},
+		RequiresInventoryTracking: true,
+		InventoryWarningLevel: responses.QuantityLevel{
+			IsEnabled: true,
+			Level:     3,
+		},
+		MaximumQuantityInCart: responses.QuantityLevel{
+			IsEnabled: true,
+			Level:     6,
+		},
+		MinimumQuantityInCart: responses.QuantityLevel{
+			IsEnabled: false,
+		},
+		AllowPreorder:       false,
+		AllowBackorder:      false,
+		RequiresLotTracking: true,
+		LotExpirationThreshold: responses.Dimension{
+			Value: 3.0,
+			Units: "months",
+		},
+		LotExpirationWarningThreshold: responses.Dimension{
+			Value: 15.0,
+			Units: "days",
+		},
 	}
 }
