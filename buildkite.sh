eda409366b69a7779c0c3bfa8b71809fe40c26a7
Pavel Kiselev <kpashka@gmail.com>
2016-12-09 11:36:54 +0300

    Debug messages

--- buildkite.sh
+++ buildkite.sh
@@ -3,8 +3,12 @@ make up
 VAGRANT_UP_EXIT_CODE=${PIPESTATUS[0]}
 make destroy
 
+set -ue
+
 if [ "$VAGRANT_UP_EXIT_CODE" = "1" ]; then
+    echo "Failed to setup/provision GCE machine"
     exit 1
 else
+    echo "Developer appliance provision succeeded"
     exit 0
 fi
