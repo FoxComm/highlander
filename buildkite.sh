eb6c8411c2d18db3fa9ebea7f743e8e367ca4217
Pavel Kiselev <kpashka@gmail.com>
2016-12-09 12:23:25 +0300

    More polishing

--- buildkite.sh
+++ buildkite.sh
@@ -9,10 +9,12 @@ VAGRANT_UP_EXIT_CODE=$?
 echo -e "--- Executing \033[33mvagrant destroy\033[0m"
 vagrant destroy appliance --force
 
-if [ $VAGRANT_UP_EXIT_CODE -eq 1 ]; then
-    echo "Failed to setup/provision GCE machine"
-    exit 1
-else
-    echo "Developer appliance provision succeeded"
+echo "--- Final report"
+
+if [ $VAGRANT_UP_EXIT_CODE -ne 0 ]; then
+    echo -e "Developer appliance creation \033[33msuccess\033[0m"
     exit 0
+else
+    echo -e "Developer appliance creation \033[33mfailure\033[0m"
+    exit 1
 fi
