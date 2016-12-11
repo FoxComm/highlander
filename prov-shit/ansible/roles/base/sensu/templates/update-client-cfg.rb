6aa56ffc2a7a8ae9d9b84ba32ebec5cba01622d9
Valerii Shyian <valerii@foxcommerce.com>
2016-12-11 15:03:11 +0200

    update conf

--- prov-shit/ansible/roles/base/sensu/templates/update-client-cfg.rb
+++ prov-shit/ansible/roles/base/sensu/templates/update-client-cfg.rb
@@ -1,11 +1,11 @@
 #!/opt/sensu/embedded/bin/ruby
 
 # Update client config with cli args
-
 require 'json'
 
-file = File.read("{{ sensu_config_path }}/client.json")
-hash = JSON.load(file)
+conf = "{{ sensu_config_path }}/client.json"
+f = File.read(conf)
+hash = JSON.load(f)
 hash["client"]["name"] = ARGV[0]
 hash["client"]["address"] = ARGV[1]
-File.write("test.json", JSON.pretty_generate(hash))
+File.write(conf, JSON.pretty_generate(hash))
