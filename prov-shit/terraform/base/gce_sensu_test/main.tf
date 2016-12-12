2195b7d6788014133a2400c5033936ab60068925
Valerii <valerii@foxcommerce.com>
2016-12-12 05:51:43 +0000

    terraform test ts sensu

--- prov-shit/terraform/base/gce_sensu_test/main.tf
+++ prov-shit/terraform/base/gce_sensu_test/main.tf
@@ -7,8 +7,9 @@ variable "base_image" {}
 variable "tiny_backend_image" {}
 variable "tiny_frontend_image" {}
 variable "consul_server_image" {}
-variable "gatling_image" {}
+#variable "gatling_image" {}
 variable "consul_leader" {}
+variable "sensu_api_host" {}
 
 provider "google"
 {
@@ -23,11 +24,12 @@ provider "google"
 module "foxcomm-govale" {
     source = "../../modules/gce/tinystack"
     datacenter = "foxcomm-govale"
-#    backend_image = "${var.tiny_backend_image}"
-#    frontend_image = "${var.tiny_frontend_image}"
+    backend_image = "${var.tiny_backend_image}"
+    frontend_image = "${var.tiny_frontend_image}"
     ssh_user = "${var.ssh_user}"
     ssh_private_key = "${var.ssh_private_key}"
     consul_leader = "${var.consul_leader}"
     consul_server_image = "${var.consul_server_image}"
     frontend_machine_type = "n1-highmem-8"
+    sensu_api_host = "${var.sensu_api_host}"
 }
