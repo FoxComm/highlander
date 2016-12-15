a0203620f1e51fbcb54d4b1f26dd16c6163f5242
Maxim Khailo <max@foxcommerce.com>
2016-12-15 11:36:16 -0800

    Updated firebrand to use subject-verb-object tracking format.

--- firebrand/src/components/tracking-pixel/tracking-pixel.jsx
+++ firebrand/src/components/tracking-pixel/tracking-pixel.jsx
@@ -3,30 +3,26 @@
 // libs
 import React from 'react';
 import type { HTMLElement } from 'types';
-import querystring from 'querystring';
 
 // styles
 import styles from './tracking-pixel.css';
 
 type Props = {
-  prodUrl?: string;
-  queryParams: Object;
-  devUrl?: string;
+  url: string;
+  channel: number;
+  subject: number;
+  obj: string;
+  verb: string;
+  objId: number;
 }
 
 const TrackingPixel = (props: Props): HTMLElement => {
-  const urlParams = querystring.stringify(props.queryParams);
-  const devSrcUrl = (props.devUrl) ? `${props.devUrl}?${urlParams}` : null;
-  const prodUrl = (props.prodUrl) ? props.prodUrl : props.devUrl;
-
-  const srcUrl = (process.env.NODE_ENV === 'production')
-    ? `${prodUrl}?${urlParams}`
-    : devSrcUrl;
+  const salt = Math.floor(Math.random()*100000)
+  const fullUrl = `${props.url}?ch=${props.channel}&sub=${props.subject}&v=${props.verb}&ob=${props.obj}&id=${props.objId}&salt=${salt}`;
 
   return (
-    <img styleName="tracking-style" alt="" src={srcUrl} />
+    <img styleName="tracking-style" alt="" src={fullUrl} />
   );
 };
 
 export default TrackingPixel;
-
