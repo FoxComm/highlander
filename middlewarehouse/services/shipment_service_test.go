3072e0d1690173f621a4643aef5ceb7d913ff0f2
tony.pizzicato <tony.grebnev@gmail.com>
2016-12-25 14:34:01 +0300

    fix shipment service

--- middlewarehouse/services/shipment_service_test.go
+++ middlewarehouse/services/shipment_service_test.go
@@ -26,7 +26,7 @@ func TestShipmentServiceSuite(t *testing.T) {
 func (suite *ShipmentServiceTestSuite) SetupSuite() {
 	suite.db = config.TestConnection()
 
-	suite.service = NewShipmentService(suite.db, &InventoryServiceStub{}, &SummaryServiceStub{}, &mocks.ActivityLoggerMock{})
+	suite.service = NewShipmentService(suite.db, &InventoryServiceStub{}, &mocks.ActivityLoggerMock{})
 }
 
 func (suite *ShipmentServiceTestSuite) SetupTest() {
