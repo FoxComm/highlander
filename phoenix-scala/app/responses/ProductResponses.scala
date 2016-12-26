85108d980ea30a9ace3153a4974c52839d1414b2
Vladimir Korbut <v.e.korbut@gmail.com>
2016-12-26 23:59:08 +0200

    Product slugs (#493)
    * Product slugs
    
    * ProductRef path matcher
    
    * Polishing
    
    * Less limits for slug: only integers are not allowed
    
    * Code review fixes
    
    * Slug validation. Allow slug with at least one letter.
    
    * SlugDuplicates failure instead of database error message
    
    * Case insensetive index for product slug
    
    * Small naming changes
    
    * Product slugs for ES
    
    * Match ProductReference by presence of letter symbol
    
    * Code review fixes
    
    * Move product_search_view changes to to regular migrations
    
    * Sql version
    
    * Make slug required field. Generate default value for empty slug.
    
    * Test for slug generating code.

--- phoenix-scala/app/responses/ProductResponses.scala
+++ phoenix-scala/app/responses/ProductResponses.scala
@@ -2,9 +2,6 @@ package responses
 
 import java.time.Instant
 
-import cats.implicits._
-import models.inventory._
-import models.objects._
 import models.product._
 import responses.AlbumResponses._
 import responses.ObjectResponses._
@@ -23,79 +20,11 @@ object ProductResponses {
     def build(p: Product): Root = Root(id = p.formId)
   }
 
-  object ProductFormResponse {
-
-    case class Root(id: Int, attributes: Json, createdAt: Instant, archivedAt: Option[Instant])
-        extends ResponseItem
-
-    //Product here is a placeholder for future. Using only form
-    def build(p: Product, f: ObjectForm): Root =
-      Root(id = f.id, attributes = f.attributes, createdAt = p.createdAt, p.archivedAt)
-  }
-
-  object ProductShadowResponse {
-
-    case class Root(id: Int, formId: Int, attributes: Json, createdAt: Instant)
-        extends ResponseItem
-
-    def build(p: ObjectShadow): Root =
-      Root(id = p.id, formId = p.formId, attributes = p.attributes, createdAt = p.createdAt)
-  }
-
-  object IlluminatedProductResponse {
-
-    case class Root(id: Int, context: Option[ObjectContextResponse.Root], attributes: Json)
-        extends ResponseItem
-
-    def build(p: IlluminatedProduct): Root =
-      Root(id = p.id,
-           context = ObjectContextResponse.build(p.context).some,
-           attributes = p.attributes)
-    def buildLite(p: IlluminatedProduct): Root =
-      Root(id = p.id, context = None, attributes = p.attributes)
-  }
-
-  object FullProductFormResponse {
-
-    case class Root(product: ProductFormResponse.Root, skus: Seq[SkuFormResponse.Root])
-        extends ResponseItem
-
-    def build(product: Product, productForm: ObjectForm, skus: Seq[(Sku, ObjectForm)]): Root =
-      Root(product = ProductFormResponse.build(product, productForm), skus = skus.map {
-        case (s, f) ⇒ SkuFormResponse.build(s, f)
-      })
-  }
-
-  object FullProductShadowResponse {
-
-    case class Root(product: ProductShadowResponse.Root, skus: Seq[SkuShadowResponse.Root])
-        extends ResponseItem
-
-    def build(shadow: ObjectShadow, skus: Seq[(Sku, ObjectShadow)]): Root =
-      Root(product = ProductShadowResponse.build(shadow), skus = skus.map {
-        case (s, ss) ⇒ SkuShadowResponse.build(s, ss)
-      })
-  }
-
-  object FullProductResponse {
-    case class Root(form: FullProductFormResponse.Root, shadow: FullProductShadowResponse.Root)
-        extends ResponseItem
-
-    def build(product: Product,
-              productForm: ObjectForm,
-              productShadow: ObjectShadow,
-              skus: Seq[FullObject[Sku]]): Root =
-      Root(form = FullProductFormResponse.build(product, productForm, skus.map { sku ⇒
-        (sku.model, sku.form)
-      }), shadow = FullProductShadowResponse.build(productShadow, skus.map { sku ⇒
-        (sku.model, sku.shadow)
-      }))
-  }
-
   // New Product Response
   object ProductResponse {
 
     case class Root(id: Int,
+                    slug: String,
                     context: ObjectContextResponse.Root,
                     attributes: Json,
                     albums: Seq[AlbumResponse.Root],
@@ -111,6 +40,7 @@ object ProductResponses {
               variants: Seq[IlluminatedVariantResponse.Root],
               taxons: Seq[SingleTaxonResponse]): Root =
       Root(id = product.id,
+           slug = product.slug,
            attributes = product.attributes,
            context = ObjectContextResponse.build(product.context),
            albums = albums,
