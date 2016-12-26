85395db41bc2388869689d6854b1d07ecf84fead
Eugene Sypachev (@Axblade) <eugene.sypachev@gmail.com>
2016-12-26 14:29:44 +0300

    solomom logs

--- solomon/config/prod.exs
+++ solomon/config/prod.exs
@@ -17,7 +17,7 @@ config :solomon, Solomon.Endpoint,
   cache_static_manifest: "priv/static/manifest.json"
 
 # Do not print debug messages in production
-config :logger, level: :info
+config :logger, level: :info, path: "logs/solomon.log"
 
 # ## SSL Support
 #
