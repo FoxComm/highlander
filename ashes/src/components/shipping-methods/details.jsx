c021ece57021f34a7837e29dbf1b98a3e9e2585d
Jeff Mataya <jeff@foxcommerce.com>
2016-12-30 14:08:24 -0800

    Shipping method page WIP

--- ashes/src/components/shipping-methods/details.jsx
+++ ashes/src/components/shipping-methods/details.jsx
@@ -5,13 +5,16 @@
 // libs
 import React, { Component, Element } from 'react';
 import { autobind } from 'core-decorators';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
 import _ from 'lodash';
 
 // actions
 import * as ShippingMethodActions from 'modules/shipping-methods/details';
 
 // components
-import { connectPage, ObjectPage } from '../object-page/object-page';
+import { PageTitle } from 'components/section-title';
+import { PrimaryButton } from 'components/common/buttons';
 import SubNav from './details-sub-nav';
 
 // type
@@ -27,62 +30,123 @@ type Props = {
   details: {
     shippingMethod: ?ShippingMethod,
   },
+  params: {
+    shippingMethodId: number,
+  },
+  status: {
+    isFetching: boolean,
+    fetchError: ?Object,
+  },
 };
 
-function cleanShippingMethod(entity) {
-  const attributes = _.get(entity, 'attributes', entity);
-  return _.reduce(attributes, (res, val, key) => {
-    return {
-      ...res,
-      [key]: _.get(val, 'v', val),
-    };
-  });
+function mapStateToProps(state) {
+  return {
+    details: state.shippingMethods.details,
+    status: {
+      isFetching: _.get(state.asyncActions, 'fetchShippingMethod.inProgress', true),
+      fetchError: _.get(state.asyncActions, 'fetchShippingMethod.err', null),
+      isCreating: _.get(state.asyncActions, 'createShippingMethod.inProgress', false),
+      createError: _.get(state.asyncActions, 'createShippingMethod.err', null),
+      isUpdating: _.get(state.asyncActions, 'updateShippingMethod.inProgress', false),
+      updateError: _.get(state.asyncActions, 'updateShippingMethod.err', null),
+    },
+  };
 }
 
-class ShippingMethodDetails extends ObjectPage {
+function mapDispatchToProps(dispatch, props) {
+  return {
+    actions: bindActionCreators(ShippingMethodActions, dispatch),
+  };
+}
+
+class ShippingMethodDetails extends Component {
   props: Props;
 
-  get pageTitle(): string {
-    if (this.isNew) {
-      return 'New Shipping Method';
+  componentDidMount() {
+    const { shippingMethodId } = this.props.params;
+    this.props.actions.fetchShippingMethod(shippingMethodId)
+  }
+
+  get renderPageTitle(): ?Element {
+    const { isFetching } = this.props.status;
+    const { shippingMethod } = this.props.details;
+
+    if (!isFetching && shippingMethod) {
+      const title = `Shipping Method ${shippingMethod.code}`;
+
+      return (
+        <PageTitle title={title}>
+        </PageTitle>
+      );
     }
-
-    return _.get(this.props.details.shippingMethod, 'attributes.code', '');
   }
 
-
-  @autobind
-  createEntity(entity) {
-    // Strip attributes out because server doesn't use them.
-    const cleaned = cleanShippingMethod(entity);
-    this.props.actions.createShippingMethod(cleaned);
-  }
-
-  @autobind
-  updateEntity(entity) {
-    // Strip attributes out because server doesn't use them.
-    const cleaned = cleanShippingMethod(entity);
-    this.props.actions.updateShippingMethod(entity.id, cleaned).then(resp => {
-      this.transitionTo(resp.payload.id);
-    });
-  }
-
-  @autobind
-  archiveEntity() {
-    this.props.actions.archiveShippingMethod(this.entityId).then(() => {
-      this.transitionToList();
-    });
-  }
-
-  @autobind
-  subNav() {
-    if (this.props.details.shippingMethod) {
-      const { id } = this.props.details.shippingMethod;
-      if (id) {
-        return <SubNav shippingMethodId={id} />;
-      }
-    }
+  render(): Element {
+    return (
+      <div>
+        {this.renderPageTitle}
+      </div>
+    );
   }
 }
 
-export default connectPage('shippingMethod', ShippingMethodActions)(ShippingMethodDetails);
+export default connect(mapStateToProps, mapDispatchToProps)(ShippingMethodDetails);
+
+//
+// function cleanShippingMethod(entity) {
+//   const attributes = _.get(entity, 'attributes', entity);
+//   return _.reduce(attributes, (res, val, key) => {
+//     return {
+//       ...res,
+//       [key]: _.get(val, 'v', val),
+//     };
+//   });
+// }
+//
+// class ShippingMethodDetails extends ObjectPage {
+//   props: Props;
+//
+//   get pageTitle(): string {
+//     if (this.isNew) {
+//       return 'New Shipping Method';
+//     }
+//
+//     return _.get(this.props.details.shippingMethod, 'attributes.code', '');
+//   }
+//
+//
+//   @autobind
+//   createEntity(entity) {
+//     // Strip attributes out because server doesn't use them.
+//     const cleaned = cleanShippingMethod(entity);
+//     this.props.actions.createShippingMethod(cleaned);
+//   }
+//
+//   @autobind
+//   updateEntity(entity) {
+//     // Strip attributes out because server doesn't use them.
+//     const cleaned = cleanShippingMethod(entity);
+//     this.props.actions.updateShippingMethod(entity.id, cleaned).then(resp => {
+//       this.transitionTo(resp.payload.id);
+//     });
+//   }
+//
+//   @autobind
+//   archiveEntity() {
+//     this.props.actions.archiveShippingMethod(this.entityId).then(() => {
+//       this.transitionToList();
+//     });
+//   }
+//
+//   @autobind
+//   subNav() {
+//     if (this.props.details.shippingMethod) {
+//       const { id } = this.props.details.shippingMethod;
+//       if (id) {
+//         return <SubNav shippingMethodId={id} />;
+//       }
+//     }
+//   }
+// }
+//
+// export default connectPage('shippingMethod', ShippingMethodActions)(ShippingMethodDetails);
