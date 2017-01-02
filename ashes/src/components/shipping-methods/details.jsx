1abc8af855c8fa4e4cc522be2d2ac0d524588226
Jeff Mataya <jeff.mataya@gmail.com>
2017-01-02 07:17:21 -0800

    Render pricing types

--- ashes/src/components/shipping-methods/details.jsx
+++ ashes/src/components/shipping-methods/details.jsx
@@ -13,6 +13,7 @@ import _ from 'lodash';
 import * as ShippingMethodActions from 'modules/shipping-methods/details';
 
 // components
+import { Dropdown } from 'components/dropdown';
 import { PageTitle } from 'components/section-title';
 import { PrimaryButton } from 'components/common/buttons';
 import ContentBox from 'components/content-box/content-box';
@@ -46,6 +47,13 @@ type Props = {
   },
 };
 
+type State = {
+  carrierId: ?number,
+  eta: ?number,
+  externalFreightId: ?number,
+  pricingType: '',
+};
+
 function mapStateToProps(state) {
   return {
     details: state.shippingMethods.details,
@@ -68,10 +76,130 @@ function mapDispatchToProps(dispatch, props) {
 
 class ShippingMethodDetails extends Component {
   props: Props;
+  state: State = { carrierId: null, eta: null, pricingType: '' };
 
   componentDidMount() {
     const { shippingMethodId } = this.props.params;
     this.props.actions.fetchShippingMethod(shippingMethodId)
+  }
+
+  get carriers() {
+    return [
+      [2, 'FedEx'],
+      [3, 'UPS'],
+      [1, 'USPS'],
+    ]
+  }
+
+  get eta() {
+    return [
+      [7, '5-7 Days'],
+      [3, '2-3 Days'],
+      [1, 'Overnight'],
+    ];
+  }
+
+  get pricingTypes() {
+    return [
+      ['flat', 'Flat'],
+      ['variable', 'Variable'],
+    ];
+  }
+
+  get freightOptions() {
+    if (this.state.carrierId == 3) {
+      return [
+        [1, 'Standard'],
+        [2, 'Ground'],
+        [3, '3 Day Select'],
+        [4, '2nd Day Air'],
+        [5, '2nd Day Air AM'],
+        [6, 'Next Day Air Saver'],
+        [7, 'Next Day Air'],
+        [8, 'Next Day Air A.M.'],
+        [9, 'Worldwide Express'],
+        [10, 'Worldwide Express Plus'],
+        [11, 'Worldwide Expedited'],
+        [12, 'Worldwide Saver'],
+      ];
+    } else if (this.state.carrierId == 1) {
+      return [
+        [13, 'First Class'],
+        [14, 'Priority'],
+        [15, 'Express'],
+        [16, 'Parcel'],
+      ];
+    } else if (this.state.carrierId == 2) {
+      return [
+        [17, 'Same Day'],
+        [18, 'First Overnight'],
+        [19, 'Priority Overnight'],
+        [20, 'Standard Overnight'],
+        [21, '2 Day AM'],
+        [22, '2 Day'],
+        [23, 'Express Saver'],
+        [24, 'Ground'],
+      ];
+    } else {
+      return [];
+    }
+  }
+
+  get renderContent() {
+    const carrierDropdown = (
+      <FormField label="Carrier" validator="ascii" maxLength={255} required>
+        <Dropdown
+          name="carrier"
+          placeholder="Select carrier"
+          value={this.state.carrierId}
+          onChange={value => this.setState({ carrierId: Number(value) })}
+          items={this.carriers} />
+      </FormField>
+    );
+
+    if (this.state.pricingType == 'flat') {
+      return (
+        <div>
+          {carrierDropdown}
+          <FormField label="Estimated Arrival" validator="ascii" maxLength={255} required>
+            <Dropdown
+              name="eta"
+              placeholder="Select expected ETA"
+              value={this.state.eta}
+              onChange={value => this.setState({ eta: Number(value) })}
+              items={this.eta} />
+          </FormField>
+        </div>
+      );
+    } else if (this.state.pricingType == 'variable') {
+      const freightPlaceholder = this.freightOptions.length == 0
+        ? 'Please choose carrier'
+        : 'Select freight option';
+
+      return (
+        <div>
+          {carrierDropdown}
+          <FormField label="Freight Option" validator="ascii" maxLength={255} required>
+            <Dropdown
+              name="externalFreightId"
+              placeholder={freightPlaceholder}
+              value={this.state.externalFreightId}
+              onChange={value => this.setState({ externalFreightId: Number(value) })}
+              items={this.freightOptions} />
+          </FormField>
+        </div>
+      );
+    }
+  }
+
+  @autobind
+  renderSubNav() {
+    if (this.props.details.shippingMethod) {
+      const { id } = this.props.details.shippingMethod;
+      if (id) {
+        return <SubNav shippingMethodId={id} />;
+      }
+    }
   }
 
   get renderPageTitle(): ?Element {
@@ -105,11 +233,24 @@ class ShippingMethodDetails extends Component {
         {this.renderPageTitle}
         <div className="fc-grid">
           <div className="fc-col-md-1-1">
+            {this.renderSubNav()}
             <ContentBox title="General">
               <FoxyForm>
                 <FormField label="Name" validator="ascii" maxLength={255} required>
                   <input type="text" defaultValue={shippingMethod.name} />
                 </FormField>
+                <FormField label="Code" validator="ascii" maxLength={255} required>
+                  <input type="text" defaultValue={shippingMethod.code} />
+                </FormField>
+                <FormField label="Pricing Type" validator="ascii" maxLength={255} required>
+                  <Dropdown
+                    name="pricingType"
+                    placeholder="Pricing Type"
+                    value={this.state.pricingType}
+                    onChange={value => this.setState({ pricingType: value })}
+                    items={this.pricingTypes} />
+                </FormField>
+                {this.renderContent}
               </FoxyForm>
             </ContentBox>
           </div>
