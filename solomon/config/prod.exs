21047c7c3c26efd107869e63d6941546242c5b0f
Eugene Sypachev (@Axblade) <eugene.sypachev@gmail.com>
2017-01-05 17:36:08 +0200

    solomon logger format

--- solomon/config/prod.exs
+++ solomon/config/prod.exs
@@ -17,7 +17,10 @@ config :solomon, Solomon.Endpoint,
   cache_static_manifest: "priv/static/manifest.json"
 
 # Do not print debug messages in production
-config :logger, level: :info
+config :logger,
+  :console,
+  level: :info,
+  format: "[$level] [$date $time] [$metadata] $levelpad$message\n"
 
 # ## SSL Support
 #
