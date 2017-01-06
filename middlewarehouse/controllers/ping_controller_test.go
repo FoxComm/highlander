5b95aa57458bf772f7eb4f54c189eab22fb94536
Eugene Sypachev (@Axblade) <eugene.sypachev@gmail.com>
2017-01-06 19:47:52 +0200

    fix tests

--- middlewarehouse/controllers/ping_controller_test.go
+++ middlewarehouse/controllers/ping_controller_test.go
@@ -1,31 +1,31 @@
 package controllers
 
 import (
-	"net/http"
-	"testing"
+    "net/http"
+    "testing"
 
-	"github.com/gin-gonic/gin"
-	"github.com/stretchr/testify/suite"
+    "github.com/gin-gonic/gin"
+    "github.com/stretchr/testify/suite"
 )
 
 type pingControllerTestSuite struct {
-	GeneralControllerTestSuite
+    GeneralControllerTestSuite
 }
 
 func TestPingControllerSuite(t *testing.T) {
-	suite.Run(t, new(pingControllerTestSuite))
+    suite.Run(t, new(pingControllerTestSuite))
 }
 
 func (suite *pingControllerTestSuite) SetupSuite() {
-	// set up test env once
-	suite.router = gin.Default()
+    // set up test env once
+    suite.router = gin.Default()
 
-	controller := NewPingController()
-	controller.SetUp(suite.router.Group("/ping"))
+    controller := NewPingController()
+    controller.SetUp(suite.router.Group("/ping"))
 }
 
 func (suite *pingControllerTestSuite) Test_Ping() {
-	res := suite.Get("/ping")
+    res := suite.Get("/ping")
 
-	suite.Equal(http.StatusNoContent, res.Code)
+    suite.Equal(http.StatusOK, res.Code)
 }
