b23676deb62b1570803f248a9ea98a552d1be82f
Maxim Khailo <max@foxcommerce.com>
2017-01-06 17:37:37 -0800

    Initial river rock proxy code.

---
+++ intelligence/river-rock/server.go
@@ -0,0 +1,77 @@
+package main
+
+import (
+    "log"
+	//"net/http"
+    //"encoding/json"
+    "net/http"
+    "net/http/httputil"
+    "net/url"
+    "database/sql"
+	"github.com/labstack/echo"  
+    _ "github.com/lib/pq"
+)
+
+const (
+	sqlGetRefs = "select refs from resource_map where cluster_id=$1 and res=$2 limit 1"
+)
+
+func getRefs(db* sql.DB, clusterId int, res string) (string, error) {
+
+    var refs string
+
+    stmt, err := db.Prepare(sqlGetRefs)
+    if err != nil {
+        return refs, err
+    }
+
+    row := stmt.QueryRow(clusterId, res)
+    if err := row.Scan(&refs); err != nil {
+        return refs, err
+    }
+
+    return refs, nil
+}
+
+
+func main() {
+   	db, err := sql.Open("postgres", "user=ic dbname=ic sslmode=disable")
+    if err != nil {
+       log.Fatal(err)
+    }
+
+    fallback, err := url.Parse("http://localhost:1323/raw")
+    if err != nil {
+       log.Fatal(err)
+    }
+
+	e := echo.New()
+
+    e.GET("/v1/*", func(c echo.Context) error {
+        req := c.Request()
+        res := c.Response()
+
+        path := req.URL.Path    
+        clusterId := 1
+        refs, err := getRefs(db, clusterId, path)
+
+        proxy := httputil.NewSingleHostReverseProxy(fallback)
+        if err != nil {
+            log.Print(err)
+            proxy.ServeHTTP(res, req)
+        } else {
+            log.Print("REFS: " + refs)
+            proxy.ServeHTTP(res, req)
+        }
+
+        return nil
+	})
+
+    e.GET("/raw/*", func(c echo.Context) error {
+        req := c.Request()
+        log.Print(req.URL.Path)
+        return c.String(http.StatusOK, req.URL.Path)
+    })
+
+	e.Logger.Fatal(e.Start(":1323"))
+}
