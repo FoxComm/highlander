b86f515929c790bddfcff8c8603b0cfd0fcb190d
Pavel Kiselev <kpashka@gmail.com>
2017-01-09 19:17:02 +0300

    Run terraform formatting check

--- prov-shit/terraform/testfmt.sh
+++ prov-shit/terraform/testfmt.sh
@@ -1,6 +1,13 @@
 #!/usr/bin/env bash
-if [[ $(terraform fmt) ]]; then
-    echo "Unformatted Terraform files found";
+set -ue
+
+unformatted_files = $(terraform fmt)
+
+if [[ $? != 0 ]]; then
+    echo "`terraform fmt` command failed."
+    exit 1;
+else if [[ $unformatted_files ]]; then
+    echo "Unformatted Terraform files found.";
     echo "Please run \`terraform fmt\`";
     exit 1;
 fi
