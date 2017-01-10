ceab3aa82cc94277aa844f3da29a7dbe9532fc67
Anna Zubenko <anna@foxcommerce.com>
2017-01-10 02:34:08 +0200

    Use `count` instead of `filter`+`size`

--- phoenix-scala/app/models/discount/qualifiers/OrderNumUnitsQualifier.scala
+++ phoenix-scala/app/models/discount/qualifiers/OrderNumUnitsQualifier.scala
@@ -9,13 +9,13 @@ case class OrderNumUnitsQualifier(numUnits: Int) extends Qualifier {
   val qualifierType: QualifierType = OrderNumUnits
 
   def check(input: DiscountInput)(implicit db: DB, ec: EC, es: ES, au: AU): Result[Unit] = {
-    val noGcLis = input.lineItems.filter { lineItem ⇒
+    val noGcLineItemCount = input.lineItems.count { lineItem ⇒
       (for {
         attrs ← lineItem.attributes
         _     ← attrs.giftCard
-      } yield {}).isEmpty
+      } yield {}).isDefined
     }
-    if (noGcLis.size >= numUnits) accept()
+    if (noGcLineItemCount >= numUnits) accept()
     else reject(input, s"Order unit count is less than $numUnits")
   }
 }
