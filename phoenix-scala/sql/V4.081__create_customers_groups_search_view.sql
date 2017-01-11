ce2e26c8f36ad7d4ea46fe9283dd4e29669c9810
Eugene Sypachev (@Axblade) <eugene.sypachev@gmail.com>
2017-01-11 23:09:13 +0300

    modify data if any DCG does exist

--- phoenix-scala/sql/V4.081__create_customers_groups_search_view.sql
+++ phoenix-scala/sql/V4.081__create_customers_groups_search_view.sql
@@ -1,4 +1,6 @@
-alter table customer_dynamic_groups add column scope exts.ltree not null;
+alter table customer_dynamic_groups add column scope exts.ltree;
+update customer_dynamic_groups set scope = text2ltree(get_scope_path((select scope_id from organizations where name = 'merchant'))::text);
+alter table customer_dynamic_groups alter column scope set not null;
 
 create table customer_groups_search_view (
   id bigint primary key,
