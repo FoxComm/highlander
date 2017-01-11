bcf5a0eb0ade4cd68bf2d1532edae15ccd54abef
Jeff Mataya <jeff@foxcommerce.com>
2017-01-10 18:09:05 -0800

    Partway through getting summaries controller updated

--- middlewarehouse/controllers/summary_controller_test.go
+++ middlewarehouse/controllers/summary_controller_test.go
@@ -5,18 +5,23 @@ import (
 	"net/http"
 	"testing"
 
-	"github.com/FoxComm/highlander/middlewarehouse/controllers/mocks"
+	"github.com/FoxComm/highlander/middlewarehouse/api/responses"
+	"github.com/FoxComm/highlander/middlewarehouse/common/db/config"
+	"github.com/FoxComm/highlander/middlewarehouse/common/db/tasks"
+	"github.com/FoxComm/highlander/middlewarehouse/fixtures"
 	"github.com/FoxComm/highlander/middlewarehouse/models"
+	"github.com/FoxComm/highlander/middlewarehouse/services"
 
 	"github.com/gin-gonic/gin"
 	"github.com/jinzhu/gorm"
-	"github.com/stretchr/testify/mock"
 	"github.com/stretchr/testify/suite"
 )
 
 type summaryControllerTestSuite struct {
 	GeneralControllerTestSuite
-	service *mocks.SummaryServiceMock
+	db        *gorm.DB
+	service   services.SummaryService
+	stockItem *models.StockItem
 }
 
 func TestSummaryControllerSuite(t *testing.T) {
@@ -24,64 +29,85 @@ func TestSummaryControllerSuite(t *testing.T) {
 }
 
 func (suite *summaryControllerTestSuite) SetupSuite() {
-	// set up test env once
-	suite.service = new(mocks.SummaryServiceMock)
-	suite.router = gin.Default()
+	suite.db = config.TestConnection()
+	suite.router = gin.New()
+
+	suite.service = services.NewSummaryService(suite.db)
 
 	controller := NewSummaryController(suite.service)
 	controller.SetUp(suite.router.Group("/summary"))
 }
 
-func (suite *summaryControllerTestSuite) TearDownTest() {
-	// clear service mock calls expectations after each test
-	suite.service.ExpectedCalls = []*mock.Call{}
-	suite.service.Calls = []mock.Call{}
+func (suite *summaryControllerTestSuite) SetupTest() {
+	tasks.TruncateTables(suite.db, []string{
+		"inventory_search_view",
+		"stock_items",
+		"stock_locations",
+		"stock_item_summaries",
+	})
+
+	stockLocation := fixtures.GetStockLocation()
+	suite.Nil(suite.db.Create(stockLocation).Error)
+
+	suite.stockItem = fixtures.GetStockItem(stockLocation.ID, "TEST-SKU")
+	suite.Nil(suite.db.Create(suite.stockItem).Error)
+}
+
+func (suite *summaryControllerTestSuite) TearDownSuite() {
+	suite.db.Close()
 }
 
 func (suite *summaryControllerTestSuite) Test_GetSummary() {
-	suite.service.On("GetSummary").Return([]*models.StockItemSummary{{
-		StockItemID: 1,
+	summary := models.StockItemSummary{
+		StockItemID: suite.stockItem.ID,
 		Type:        models.Sellable,
-	}}, nil).Once()
+		OnHand:      13,
+	}
+	suite.Nil(suite.db.Create(&summary).Error)
 
-	res := suite.Get("/summary")
+	var response struct {
+		Summary []interface{}
+	}
 
+	res := suite.Get("/summary", &response)
 	suite.Equal(http.StatusOK, res.Code)
-	suite.Contains(res.Body.String(), "summary\":[")
-	suite.service.AssertExpectations(suite.T())
+	suite.Equal(1, len(response.Summary))
 }
 
 func (suite *summaryControllerTestSuite) Test_GetSummaryBySKU() {
 	sku := "TEST-SKU"
-	suite.service.On("GetSummaryBySKU", sku).Return([]*models.StockItemSummary{{
-		StockItemID: 1,
-		StockItem:   models.StockItem{SKU: sku},
+	summary := models.StockItemSummary{
+		StockItemID: suite.stockItem.ID,
 		Type:        models.Sellable,
-	}}, nil).Once()
+		OnHand:      13,
+	}
+	suite.Nil(suite.db.Create(&summary).Error)
 
-	res := suite.Get(fmt.Sprintf("/summary/%s", sku))
+	var summaryResp responses.StockItemSummary
+	res := suite.Get(fmt.Sprintf("/summary/%s", sku), &summaryResp)
 
 	suite.Equal(http.StatusOK, res.Code)
-	suite.Contains(res.Body.String(), sku)
-	suite.service.AssertExpectations(suite.T())
+	suite.Equal(1, len(summaryResp.Summary))
+	suite.Equal(sku, summaryResp.Summary[0].SKU)
+	suite.Equal(13, summaryResp.Summary[0].OnHand)
 }
 
-func (suite *summaryControllerTestSuite) Test_GetSummaryBySKUNoSKU() {
-	suite.service.On("GetSummaryBySKU", "NO-SKU").Return(nil, gorm.ErrRecordNotFound).Once()
+// func (suite *summaryControllerTestSuite) Test_GetSummaryBySKUNoSKU() {
+// 	suite.service.On("GetSummaryBySKU", "NO-SKU").Return(nil, gorm.ErrRecordNotFound).Once()
 
-	res := suite.Get("/summary/NO-SKU")
+// 	res := suite.Get("/summary/NO-SKU")
 
-	suite.Equal(http.StatusNotFound, res.Code)
-	suite.Contains(res.Body.String(), "errors")
-	suite.service.AssertExpectations(suite.T())
-}
+// 	suite.Equal(http.StatusNotFound, res.Code)
+// 	suite.Contains(res.Body.String(), "errors")
+// 	suite.service.AssertExpectations(suite.T())
+// }
 
-func (suite *summaryControllerTestSuite) Test_GetSummaryBySKUServerError() {
-	suite.service.On("GetSummaryBySKU", "NO-SKU").Return(nil, gorm.ErrUnaddressable).Once()
+// func (suite *summaryControllerTestSuite) Test_GetSummaryBySKUServerError() {
+// 	suite.service.On("GetSummaryBySKU", "NO-SKU").Return(nil, gorm.ErrUnaddressable).Once()
 
-	res := suite.Get("/summary/NO-SKU")
+// 	res := suite.Get("/summary/NO-SKU")
 
-	suite.Equal(http.StatusBadRequest, res.Code)
-	suite.Contains(res.Body.String(), "errors")
-	suite.service.AssertExpectations(suite.T())
-}
+// 	suite.Equal(http.StatusBadRequest, res.Code)
+// 	suite.Contains(res.Body.String(), "errors")
+// 	suite.service.AssertExpectations(suite.T())
+// }
