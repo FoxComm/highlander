b66804e02597fbd89cdef54be1aee3fdc2062fa3
DemetriousWilson <DemetriousWilson@gmail.com>
2017-01-11 17:43:45 -0800

    Add sources dropdown to analytics page, introduce static(non-draggable) versions of column-selector component

--- ashes/src/components/analytics/question-box.jsx
+++ ashes/src/components/analytics/question-box.jsx
@@ -13,31 +13,51 @@ type Props = {
   title: string,
   content: any,
   footer: any,
-  onClick?: Function,
+  isActive?: boolean,
+  onClick?: ?Function,
 }
 
 type State = {
   isActive: boolean,
 }
 
-export default class QuestionBox extends React.Component {
+class QuestionBox extends React.Component {
 
   props: Props;
+
+  static defaultProps = {
+    isActive: false,
+    onClick: null,
+  };
 
   state: State = {
     isActive: false
   };
 
+  componentWillMount() {
+    const { isActive } = this.props;
+
+    if (isActive) {
+      this.toggleActiveState();
+    }
+  }
+
+  @autobind
+  toggleActiveState() {
+    const { isActive } = this.state;
+    this.setState({isActive: !isActive});
+  }
+
   @autobind
   onClickHandler() {
     const { onClick } = this.props;
-    const { isActive } = this.state;
 
-    this.setState({isActive: !isActive});
+    this.toggleActiveState();
 
     if (!_.isNull(onClick)) {
       return onClick();
     }
+
     return null;
   }
 
@@ -60,3 +80,5 @@ export default class QuestionBox extends React.Component {
     );
   }
 }
+
+export default QuestionBox;
