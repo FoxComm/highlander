43d02ddb0fd343285e49ab3387076ca7e06aa526
DemetriousWilson <DemetriousWilson@gmail.com>
2017-01-18 11:35:53 -0800

    ProductConversion analytics question

--- ashes/src/components/analytics/analytics.jsx
+++ ashes/src/components/analytics/analytics.jsx
@@ -2,15 +2,6 @@
 import React, { PropTypes } from 'react';
 import { connect } from 'react-redux';
 import { autobind } from 'core-decorators';
-import {
-  VictoryScatter,
-  VictoryLine,
-  VictoryChart,
-  VictoryAxis,
-  VictoryLabel,
-  VictoryTooltip,
-  VictoryGroup,
-} from 'victory';
 import moment from 'moment';
 import _ from 'lodash';
 
@@ -23,6 +14,7 @@ import Currency from '../common/currency';
 import TrendButton, { TrendType } from './trend-button';
 import StaticColumnSelector from './static-column-selector';
 import { Dropdown } from '../dropdown';
+import ProductConversionChart from './charts/product-conversion-chart';
 
 // styles
 import styles from './analytics.css';
@@ -35,7 +27,7 @@ type State = {
   dateRangeBegin: string, // Unix Timestamp
   dateRangeEnd: string, // Unix Timestamp
   dateDisplay: string,
-  question: any,
+  question: QuestionBoxType,
 }
 
 const verbs = {
@@ -55,29 +47,37 @@ const sourceDropdownColumns = [
   { field: 'direct', text: 'Direct' },
 ];
 
+const questionTitles = {
+  totalRevenue: 'Total Revenue',
+  totalOrders: 'Total Orders',
+  avgNumPerOrder: 'Avg. Num. Per Order',
+  totalInCarts: 'Total In Carts',
+  productConversion: 'Product Conversion',
+};
+
 const questions: Array<QuestionBoxType> = [
   {
-    title: 'Total Revenue',
+    title: questionTitles.totalRevenue,
     content: <Currency value="578657" />,
     footer: <TrendButton trendType={TrendType.gain} value={90}/>,
   },
   {
-    title: 'Total Orders',
+    title: questionTitles.totalOrders,
     content: 132,
     footer: <TrendButton trendType={TrendType.loss} value={10}/>,
   },
   {
-    title: 'Avg. Num. Per Order',
+    title: questionTitles.avgNumPerOrder,
     content: 1,
     footer: <TrendButton trendType={TrendType.steady} value={0}/>,
   },
   {
-    title: 'Total In Carts',
+    title: questionTitles.avgNumPerOrder,
     content: 132,
     footer: <TrendButton trendType={TrendType.loss} value={10}/>,
   },
   {
-    title: 'Product Conversion',
+    title: questionTitles.productConversion,
     content: '7.2%',
     footer: <TrendButton trendType={TrendType.gain} value={3}/>,
   },
@@ -135,102 +135,6 @@ export default class Analytics extends React.Component {
     question: null,
   };
 
-  componentDidMount() {
-    const channel = 1;
-    const { entityType, entityId } = this.props.entity;
-
-    const keys = _.map(verbs[entityType], (title, verb) => {
-      const key = `track.${channel}.${entityType}.${entityId}.${verb}`;
-      return { 
-          key: key,
-          title: title,
-          verb: verb,
-      };
-    });
-
-    const to = Math.floor(Date.now() / 1000) + 300;
-    const from = to - (300*20);
-    const sizeSec = 60;
-    const stepSec = 60;
-    this.props.fetchAnalytics(keys, from, to, sizeSec, stepSec);
-  }
-
-  @autobind
-  legend(label, color, x, y) {
-    return (
-      <VictoryLabel
-        x={x}
-        y={y}
-        style={{fontSize: 8, fill: `${color}`}}
-        text={label}
-      />
-    );
-  }
-
-  @autobind
-  newChart(allData) {
-    const title = { x: 25, y: 12, size: 12, text: 'Last 60 Minutes' };
-    return (
-      <VictoryChart
-        domainPadding={20}>
-        <VictoryLabel
-          x={title.x}
-          y={title.y}
-          style={{fontSize: title.size}}
-          text={title.text}
-        />
-        {_.map(allData, (dataSet, i) => {
-          return this.legend(dataSet.name, colors[i], title.x, title.y + (i + 1) * 8);
-        })}
-        <VictoryAxis
-          label="Time"
-          scale="time"
-          tickFormat={(unixTimestamp) => {
-            return moment(unixTimestamp).format('h:mm A');
-          }}/>
-        <VictoryAxis
-          dependentAxis
-          label="Count"
-          tickFormat={(count) => (count)}/>
-        {
-          _.map(allData, (dataSet, i) => {
-            const values = dataSet.values;
-            const color = colors[i];
-
-            _.map(values, (value) => {
-              value.label = value.y;
-            });
-
-            return (
-              <VictoryGroup
-                data={values}>
-                <VictoryScatter
-                  labelComponent={
-                    <VictoryTooltip
-                      cornerRadius={0}
-                      pointerLength={0}
-                      style={{fill: color}}
-                      flyoutStyle={{stroke: color}}
-                      labels={(value) => (value.y)}
-                    />
-                  }
-                  style={{
-                    data: { fill: color, opacity: (value) => value.y == 0 ? 0 : 1 }
-                  }}
-                  x="x"
-                  y="y"/>
-                <VictoryLine
-                  style={{
-                    data: { stroke: color }
-                  }}/>
-              </VictoryGroup>
-            );
-          })
-        }
-      </VictoryChart>
-    );
-  }
-
   @autobind
   onDatePickerChange(selectionIndex) {
     let displayText = '';
@@ -285,6 +189,12 @@ export default class Analytics extends React.Component {
   @autobind
   onQuestionBoxSelect(question) {
     this.setState({question: question});
+
+    switch(question.title) {
+      case questionTitles.productConversion:
+        this.props.fetchProductConversion(this.props.entity.entityId);
+        break;
+    }
   }
 
   get dateDisplay() {
@@ -293,6 +203,25 @@ export default class Analytics extends React.Component {
 
   get question() {
     return this.state.question;
+  }
+
+  get chartFromQuestion() {
+    if (_.isNull(this.question)) {
+      return false;
+    }
+
+    const { analytics } = this.props;
+
+    if (!analytics.isFetching) {
+      switch (this.question.title) {
+        case questionTitles.productConversion:
+          return <ProductConversionChart jsonData={analytics.values}/>;
+        default:
+          return false;
+      }
+    } else {
+      return false;
+    }
   }
 
   get filterHeaders() {
@@ -332,28 +261,9 @@ export default class Analytics extends React.Component {
   get content() {
     const { analytics } = this.props;
 
-    if (analytics.isFetching === false) {
+    if (!analytics.isFetching && !_.isNull(this.question)) {
       if (!analytics.err) {
-
-        const data = _.map(analytics.values, (rawValues, k) => {
-          let values = _.map(rawValues, (v) => {
-            return {
-              x: new Date(v.x * 1000),
-              y: v.y
-            };
-          });
-
-          const verb = _.find(analytics.keys, {'key': k});
-
-          return {
-            name: verb.title,
-            values: values,
-            strokeWidth: verb.strokeWidth,
-            strokeDashArray: verb.strokeDashArray,
-          };
-        });
-
-        return this.newChart(data);
+        return this.chartFromQuestion;
       } else {
         return <ErrorAlerts error={analytics.err} />;
       }
---
+++ ashes/src/components/analytics/charts/product-conversion-chart.jsx
@@ -0,0 +1,224 @@
+// @flow
+
+// libs
+import React, { Component } from 'react';
+import {
+  VictoryBar,
+  VictoryChart,
+  VictoryAxis,
+  VictoryTooltip,
+  VictoryLabel,
+} from 'victory';
+import _ from 'lodash';
+
+const axisTickColor = "#9BA3A7";
+const yAxisStyle = {
+  axis: { stroke: axisTickColor },
+  grid: { stroke: axisTickColor, strokeWidth: 0.25, strokeDasharray: 2.5, opacity: 0.6 },
+  tickLabels: { fontSize: 8, fill: axisTickColor },
+};
+
+const xAxisStyle = {
+  axis: { stroke: axisTickColor },
+  tickLabels: { fontSize: 6, fill: axisTickColor },
+};
+
+const dataTickValues = [
+  "Category/Search View",
+  "PDP View",
+  "Add to Cart",
+  "Enter Checkout",
+  "Purchased",
+];
+
+const barStyle = {
+  data: { fill: "#4FC2C9", width: 15 },
+  labels: { fontSize: 12 },
+  parent: { border: "1px solid #ccc" },
+};
+
+// Dummy response payload for UI debugging
+const dummyJsonData = {
+  "SearchViews": 876,
+  "PdpViews": 500,
+  "CartClicks": 379,
+  "CheckoutClicks": 263,
+  "Purchases": 68,
+  "SearchToPdp": 57.1,
+  "PdpToCart": 75.8,
+  "CartToCheckout": 69.4,
+  "CheckoutToPurchase": 25.6,
+};
+
+const barEvents = [
+  {
+    // Event: Always show tooltip label onMouseOut
+    target: "data",
+    eventHandlers: {
+      onMouseOut: () => {
+        return [{
+          target: "labels",
+          mutation: (props) => {
+            return props.label;
+          }
+        }];
+      }
+    }
+  }
+];
+
+class CustomToolTip extends React.Component {
+
+  static defaultEvents = VictoryTooltip.defaultEvents;
+
+  get tooltips() {
+    const { barWidth, getDelta, datum } = this.props;
+
+    const toolTipFlyoutStyle = {
+      opacity: 0,
+    };
+    const toolTipStyle = {
+      fontSize: 6,
+    };
+
+    const deltaToolTipFlyoutStyle = {
+      stroke: "#363636",
+      fill: "#3A4350",
+    };
+    const deltaToolTipStyle = {
+      fill: "white",
+      fontSize: 6,
+    };
+    const deltaValue = getDelta(datum);
+
+    const deltaToolTip = (deltaValue > 0) ?
+      <VictoryTooltip
+        {...this.props}
+        cornerRadius={0}
+        flyoutStyle={deltaToolTipFlyoutStyle}
+        style={deltaToolTipStyle}
+        pointerLength={7}
+        pointerWidth={19}
+        dx={barWidth * 1.5}
+        y={180}
+        text={`${deltaValue} %`}
+        orientation="left"
+        labelComponent = {
+          <VictoryLabel />
+        }
+        active={true}
+        renderInPortal={false}
+      />
+      : false;
+
+    return (
+      <g>
+        <VictoryTooltip
+          {...this.props}
+          flyoutStyle={toolTipFlyoutStyle}
+          style={toolTipStyle}
+          pointerLength={0}
+          dy={-10}
+          active={true}
+          renderInPortal={false}
+        />
+        {deltaToolTip}
+      </g>
+    );
+  }
+
+  render() {
+    return this.tooltips;
+  }
+}
+
+type Props = {
+  jsonData: Object,
+  debugMode?: ?boolean,
+}
+
+class ProductConversionChart extends Component {
+
+  props: Props;
+
+  static defaultProps = {
+    jsonData: {},
+    debugMode: false,
+  };
+
+  get data() {
+    const { jsonData, debugMode } = this.props;
+    
+    const jsonDisplay = (debugMode) ? dummyJsonData : jsonData;
+
+    return [
+      {
+        key: dataTickValues[0],
+        value: jsonDisplay.SearchViews,
+        label: jsonDisplay.SearchViews.toString(),
+      },
+      {
+        key: dataTickValues[1],
+        value: jsonDisplay.PdpViews,
+        delta: _.round(jsonDisplay.SearchToPdp, 2),
+        label: jsonDisplay.PdpViews.toString(),
+      },
+      {
+        key: dataTickValues[2],
+        value: jsonDisplay.CartClicks,
+        delta: _.round(jsonDisplay.PdpToCart, 2),
+        label: jsonDisplay.CartClicks.toString(),
+      },
+      {
+        key: dataTickValues[3],
+        value: jsonDisplay.CheckoutClicks,
+        delta: _.round(jsonDisplay.CartToCheckout, 2),
+        label: jsonDisplay.CheckoutClicks.toString(),
+      },
+      {
+        key: dataTickValues[4],
+        value: jsonDisplay.Purchases,
+        delta: _.round(jsonDisplay.CheckoutToPurchase, 2),
+        label: jsonDisplay.Purchases.toString(),
+      },
+    ];
+  }
+
+  get chart() {
+    return (
+      <VictoryChart
+        domainPadding={25}>
+        <VictoryAxis
+          standalone={false}
+          style={xAxisStyle}
+          orientation="bottom"
+          tickValues={dataTickValues} />
+        <VictoryAxis
+          dependentAxis
+          standalone={false}
+          style={yAxisStyle}
+          tickCount={3}
+          orientation="left" />
+        <VictoryBar
+          labelComponent={
+            <CustomToolTip
+              barWidth={barStyle.data.width}
+              getDelta={(datum) => datum.delta}
+            />
+          }
+          style={barStyle}
+          data={this.data}
+          x="key"
+          y="value"
+          events={barEvents}
+        />
+      </VictoryChart>
+    );
+  }
+
+  render() {
+    return this.chart;
+  }
+}
+
+export default ProductConversionChart;
