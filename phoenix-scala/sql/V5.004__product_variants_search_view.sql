207885fef8463555ee3d9db919bce4111d9d877a
Krzysztof Janosz <janosz.krzysztof@gmail.com>
2017-01-20 17:08:20 +0100

    Add foreign key contrains to product_variants_search_view

--- phoenix-scala/sql/V5.004__product_variants_search_view.sql
+++ phoenix-scala/sql/V5.004__product_variants_search_view.sql
@@ -10,4 +10,9 @@ set id = product_variants.form_id
 from product_variants
 where product_variants.id = product_variants_search_view.id;
 
+alter table product_variants_search_view
+  add foreign key (id) references object_forms (id) on delete restrict on update cascade,
+  add foreign key (context_id) references object_contexts (id) on delete restrict on update cascade,
+  add foreign key (mwh_sku_id) references product_variant_mwh_sku_ids (id) on delete restrict on update cascade;
+
 ----- triggers are updated in repeatable migration file: R__product_variants_search_view_triggers.sql
