1fad81853437d39aa24d916e06a403bb3c566e49
Sergey Rublev <sergey@foxcommerce.com>
2017-01-20 23:51:02 +0700

    Add type & fill new retail_price with actual values during migration

--- phoenix-scala/sql/V4.081__add_retail_price_to_products_catalog_view.sql
+++ phoenix-scala/sql/V4.081__add_retail_price_to_products_catalog_view.sql
@@ -1,1 +1,12 @@
-alter table products_catalog_view add column retail_price;
+alter table products_catalog_view add column retail_price text;
+
+update products_catalog_view
+    set retail_price = subquery.retail_price from (
+    select p.id,
+        sku.retail_price
+    from products as p
+    inner join product_sku_links_view as sv on (sv.product_id = p.id)
+        inner join object_contexts as context on (p.context_id = context.id)
+    inner join sku_search_view as sku on (sku.context_id = context.id and sku.sku_code = sv.skus->>0)
+) as subquery
+where subquery.id = products_catalog_view.id;
