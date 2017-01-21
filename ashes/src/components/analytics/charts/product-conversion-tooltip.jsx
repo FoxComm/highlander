d7d13aeb79030e4eaabebedba5ac93f2da2a76d5
DemetriousWilson <DemetriousWilson@gmail.com>
2017-01-20 18:12:11 -0800

    Add custom SVG tooltip to ProductConversion

--- ashes/src/components/analytics/charts/product-conversion-tooltip.jsx
+++ ashes/src/components/analytics/charts/product-conversion-tooltip.jsx
@@ -61,6 +61,34 @@ const ConversionLabelComponent = (props) => {
   );
 };
 
+class DeltaFlyout extends React.Component {
+
+  badgeStyle(x, y) {
+    const trnslate = `translate(${x - 54}, ${y + 4}) scale(0.425)`;
+    return (
+      <g stroke="none" strokeWidth="1" fill="none" fillRule="evenodd">
+        <g transform={trnslate}>
+          <polygon 
+            stroke="#363636" 
+            fill="#3A4350" 
+            points="0 1.17774105e-16 2.75821033e-15 45.2614326 66.25 45.2607422 80.8210449 22.6307163 66.2856445 0">
+          </polygon>
+          <path 
+            d="M2.07990973e-15,34.5 L2.75821033e-15,45.6307163 L66.25,45.6300259 L80.5832702,23.3692837 L0,23.3692837 L0,34.5 Z"
+            stroke="#3A4350" 
+            fill="#F7F7F7">
+          </path>
+        </g>
+      </g>
+    );
+  }
+
+  render() {
+    const { x, y, orientation } = this.props;
+    return this.badgeStyle(x, y);
+  }
+}
+
 export default class ProductConversionToolTip extends React.Component {
 
   static defaultEvents = VictoryTooltip.defaultEvents;
