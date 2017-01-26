587ea602ef5ea1a7a2dec44938c5e019bc0cbe97
mbektimirov <mbektimirov@gmail.com>
2017-01-26 14:17:32 +0500

    New input mask component:
    - field editing not eating chars
    - not conflicting with standard browser autocomplete

---
+++ src/core/lib/i18n/field-masks.js
@@ -0,0 +1,10 @@
+import _ from 'lodash';
+
+// Create masks related to react-text-mask component
+// https://github.com/text-mask/text-mask/blob/master/componentDocumentation.md#mask
+
+export function createNumberMask(stringPattern) {
+  const fieldMask = _.map(stringPattern, c => c.match(/\d/) ? /\d/ : c);
+
+  return fieldMask;
+}
