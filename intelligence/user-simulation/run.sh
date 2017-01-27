98bf5001bb27d46232f6f5e42f9840d88f4c6982
Maxim Khailo <max@foxcommerce.com>
2017-01-26 17:17:50 -0800

    Allow control of total jobs in parallel from cmd line

--- intelligence/user-simulation/run.sh
+++ intelligence/user-simulation/run.sh
@@ -2,6 +2,7 @@
 
 PERSONA=McShopFace
 JOBS=5000
+J=$1
 
-seq -w 0 $JOBS | parallel --ungroup node --harmony src/simulate.js $PERSONA
+seq -w 0 $JOBS | parallel -j$J --ungroup node --harmony src/simulate.js $PERSONA
 
