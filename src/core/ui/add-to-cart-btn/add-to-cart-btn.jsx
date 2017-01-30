57275c468dc36d6db5a0f7337ad87e8ab4f5ac82
Bad_Company <Ihor.peredystyi@gmail.com>
2017-01-30 14:22:52 +0200

    feat: add new prop 'pdp' for AddToCartBtn

--- src/core/ui/add-to-cart-btn/add-to-cart-btn.jsx
+++ src/core/ui/add-to-cart-btn/add-to-cart-btn.jsx
@@ -11,23 +11,26 @@ import styles from './add-to-cart-btn.css';
 import Icon from 'ui/icon';
 
 type Props = {
+  pdp?: bool,
   expanded?: bool,
   onClick?: Function,
   className?: string,
 };
 
 const AddToCartBtn = (props: Props) => {
-  const { expanded = false, onClick, className, ...restProps } = props;
+  const { pdp, expanded = false, onClick, className, ...restProps } = props;
   const classNames = cx(className, styles['add-to-cart-btn'], {
     [styles.expanded]: expanded,
   });
 
+
   return (
-    <button className={classNames} onClick={onClick} {...restProps}>
+    <button className={classNames} onClick={onClick}>
       <span styleName="add-icon-wrapper">
         <Icon name="fc-add" styleName="add-icon" />
       </span>
-      <span styleName="add-btn-title">ADD TO CART</span>
+      { pdp && <span styleName="pdp-add-btn-title">ADD TO CART</span>}
+      { !pdp && <span styleName="add-btn-title">ADD TO CART</span>}
     </button>
   );
 };
