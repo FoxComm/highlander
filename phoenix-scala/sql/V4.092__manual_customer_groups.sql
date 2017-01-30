bd9525d0b66771f2f259a2547fa49d797ac59663
Eugene Sypachev (@Axblade) <eugene.sypachev@gmail.com>
2017-01-30 17:15:23 +0300

    group types in ES

--- phoenix-scala/sql/V4.092__manual_customer_groups.sql
+++ phoenix-scala/sql/V4.092__manual_customer_groups.sql
@@ -3,3 +3,5 @@ alter table customer_dynamic_groups rename to customer_groups;
 create domain customer_group_type text check (value in ('manual', 'dynamic'));
 
 alter table customer_groups add column group_type customer_group_type not null default 'dynamic';
+
+alter table customer_groups_search_view add column group_type customer_group_type not null default 'dynamic';
