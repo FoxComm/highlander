89f00b91249a1f121c7b2fe7c9004dea4fe201b3
Pavel Kiselev <kpashka@gmail.com>
2017-02-04 04:39:17 +0700

    No vagrant in Buildkite

--- buildkite.sh
+++ buildkite.sh
@@ -1,22 +1,19 @@
 #!/usr/bin/env bash
 # A script executed on BuildKite to test developer appliance provisioning on GCE
 
-export VAGRANT_DEFAULT_PROVIDER=google
-export eval $(cat ./.env.local)
+echo -e "--- Executing \033[33mmake up\033[0m"
+make up
+MAKE_UP_EXIT_CODE=$?
 
-echo -e "--- Executing \033[33mvagrant up\033[0m"
-vagrant up --provider=google appliance
-VAGRANT_UP_EXIT_CODE=$?
+echo -e "--- Executing \033[33mmake destroy\033[0m"
+make destroy
 
-echo -e "--- Executing \033[33mvagrant destroy\033[0m"
-vagrant destroy appliance --force
-
-if [ $VAGRANT_UP_EXIT_CODE -eq 0 ]; then
+if [ $MAKE_UP_EXIT_CODE -eq 0 ]; then
     echo -e "--- Appliance creation \033[33msuccess\033[0m"
-    echo "vagrant up exit code: $VAGRANT_UP_EXIT_CODE"
+    echo "make up exit code: $MAKE_UP_EXIT_CODE"
     exit 0
 else
     echo -e "--- Appliance creation \033[33mfailure\033[0m"
-    echo "vagrant up exit code: $VAGRANT_UP_EXIT_CODE"
+    echo "make up exit code: $MAKE_UP_EXIT_CODE"
     exit 1
 fi
