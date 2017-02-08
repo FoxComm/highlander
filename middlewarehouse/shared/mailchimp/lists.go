177cdba35831cf9dccfe00dd7611bece9cb85bca
tony.pizzicato <tony.grebnev@gmail.com>
2017-02-08 17:48:13 +0300

    get segment members from mailchimp

--- middlewarehouse/shared/mailchimp/lists.go
+++ middlewarehouse/shared/mailchimp/lists.go
@@ -1,26 +1,30 @@
 package mailchimp
 
-import "fmt"
-
-const (
-	EndpointListsStaticSegments string = "/lists/%s/segments"
-	EndpointListsStaticSegment  string = "/lists/%s/segments/%d"
+import (
+	"encoding/json"
+	"fmt"
 )
 
-func (c *ChimpClient) StaticSegments(listID string) (StaticSegmentsResponse, error) {
-	var response StaticSegmentsResponse
+const (
+	EndpointListsStaticSegments       string = "/lists/%s/segments"
+	EndpointListsStaticSegment        string = "/lists/%s/segments/%d"
+	EndpointListsStaticSegmentMembers string = "/lists/%s/segments/%d/members"
+)
+
+func (c *ChimpClient) GetSegments(listID string) (SegmentsResponse, error) {
+	var response SegmentsResponse
 	err := c.get(fmt.Sprintf(EndpointListsStaticSegments+"?count=60", listID), &response)
 	return response, err
 }
 
-func (c *ChimpClient) CreateStaticSegment(listID string, payload *StaticSegmentPayload) (StaticSegmentResponse, error) {
-	var response StaticSegmentResponse
+func (c *ChimpClient) CreateSegment(listID string, payload *SegmentPayload) (SegmentResponse, error) {
+	var response SegmentResponse
 	err := c.post(fmt.Sprintf(EndpointListsStaticSegments, listID), payload, &response)
 	return response, err
 }
 
-func (c *ChimpClient) UpdateStaticSegment(listID string, segmentId int, payload *StaticSegmentPayload) (StaticSegmentResponse, error) {
-	var response StaticSegmentResponse
+func (c *ChimpClient) UpdateStaticSegment(listID string, segmentId int, payload *SegmentPayload) (SegmentResponse, error) {
+	var response SegmentResponse
 	err := c.patch(fmt.Sprintf(EndpointListsStaticSegment, listID, segmentId), payload, &response)
 	return response, err
 }
@@ -29,9 +33,21 @@ func (c *ChimpClient) DeleteStaticSegment(listID string, segmentId int) error {
 	return c.delete(fmt.Sprintf(EndpointListsStaticSegment, listID, segmentId))
 }
 
-type StaticSegmentPayload struct {
-	Name          string   `json:"name"`
-	StaticSegment []string `json:"static_segment"`
+func (c *ChimpClient) GetSegmentMembers(listID string, segmentId int) (SegmentMembersResponse, error) {
+	var response SegmentMembersResponse
+	err := c.get(fmt.Sprintf(EndpointListsStaticSegmentMembers+"?count=60", listID, segmentId), &response)
+	return response, err
+}
+
+type SegmentOptions struct {
+	Match      string            `json:"match"`
+	Conditions []json.RawMessage `json:"conditions"`
+}
+
+type SegmentPayload struct {
+	Name          string          `json:"name"`
+	StaticSegment []string        `json:"static_segment"`
+	Options       *SegmentOptions `json:"options,omitempty"`
 }
 
 type LinkResponse struct {
@@ -42,7 +58,7 @@ type LinkResponse struct {
 	Schema       string
 }
 
-type StaticSegmentResponse struct {
+type SegmentResponse struct {
 	ID           int            `json:"id"`
 	Name         string         `json:"name"`
 	MembersCount int            `json:"member_count"`
@@ -53,9 +69,20 @@ type StaticSegmentResponse struct {
 	Links        []LinkResponse `json:"_links"`
 }
 
-type StaticSegmentsResponse struct {
-	Segments []StaticSegmentResponse `json:"segments"`
-	ListID   string                  `json:"list_id"`
-	Total    int                     `json:"total_items"`
-	Links    []LinkResponse          `json:"_links"`
+type SegmentsResponse struct {
+	Segments []SegmentResponse `json:"segments"`
+	ListID   string            `json:"list_id"`
+	Total    int               `json:"total_items"`
+	Links    []LinkResponse    `json:"_links"`
+}
+
+type SegmentMemberResponse struct {
+	ID    string `json:"id"`
+	Email string `json:"email_address"`
+}
+
+type SegmentMembersResponse struct {
+	Members []SegmentMemberResponse `json:"members"`
+	Total   int                     `json:"total_items"`
+	Links   []LinkResponse          `json:"_links"`
 }
