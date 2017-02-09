48eb6400fb46f6dcf9b892669d577a591716433b
danko-m <dankomilutinovic@gmail.com>
2017-02-09 03:29:06 +0100

    updates for homepage and favorites category page. removal of valentines category page. changes should be published automaticaly on 02.15.17.

--- src/core/modules/categories.js
+++ src/core/modules/categories.js
@@ -2,85 +2,165 @@
 
 import { createReducer } from 'redux-act';
 import { createAsyncActions } from '@foxcomm/wings';
+import moment from 'moment';
+
+const timeTarget = moment('2017-02-15');
+const timeNow = moment();
 
 function apiCall(): Promise {
-  const result = [
-    {
-      id: 0,
-      name: 'APPETIZERS',
-      description: 'Starters in 10 Minutes',
-      imageUrl: '/images/categories/Cat_Appetizers_2x.jpg',
-      showNameCatPage: true,
-    },
-    {
-      id: 1,
-      name: 'ENTRÉES',
-      description: 'Dinner in 30 minutes',
-      imageUrl: '/images/categories/Cat_Entrees_2x.jpg',
-      showNameCatPage: true,
-    },
-    {
-      id: 2,
-      name: 'SIDES',
-      description: '',
-      imageUrl: '/images/categories/Cat_Sides_2x.jpg',
-      showNameCatPage: true,
-    },
-    {
-      id: 3,
-      name: 'BEST SELLERS',
-      description: 'Dinner in 30 minutes',
-      imageUrl: '/images/categories/Cat_Best_Sellers_2x.jpg',
-      showNameCatPage: true,
-    },
-    {
-      id: 4,
-      name: 'FAVORITES',
-      description: '',
-      imageUrl: '',
-      hiddenInNavigation: true,
-      showNameCatPage: true,
-    },
-    {
-      id: 5,
-      name: 'HOLIDAY',
-      description: '',
-      imageUrl: '',
-      hiddenInNavigation: true,
-      showNameCatPage: true,
-    },
-    {
-      id: 6,
-      name: 'GIFT CARDS',
-      description: 'Gift cards will be here',
-      imageUrl: '',
-      showNameCatPage: true,
-    },
-    {
-      id: 7,
-      name: 'VALENTINE',
-      description: 'Valentine\'s Day Picks',
-      imageUrl: '/images/categories/Cat_Valentine_2x.jpg',
-      hiddenInNavigation: true,
-      showNameCatPage: false,
-    },
-    {
-      id: 8,
-      name: 'WEEKNIGHT',
-      description: 'Weeknight Favorites',
-      imageUrl: '/images/categories/Cat_Weeknights_2x.jpg',
-      hiddenInNavigation: true,
-      showNameCatPage: false,
-    },
-    {
-      id: 9,
-      name: 'SPIN',
-      description: 'Classics Revisited',
-      imageUrl: '/images/categories/Cat_Spin_2x.jpg',
-      hiddenInNavigation: true,
-      showNameCatPage: false,
-    },
-  ];
+  let result = [];
+  if (timeTarget > timeNow) {
+    // before timeTarget
+    result = [
+      {
+        id: 0,
+        name: 'APPETIZERS',
+        description: 'Starters in 10 Minutes',
+        imageUrl: '/images/categories/Cat_Appetizers_2x.jpg',
+        showNameCatPage: true,
+      },
+      {
+        id: 1,
+        name: 'ENTRÉES',
+        description: 'Dinner in 30 minutes',
+        imageUrl: '/images/categories/Cat_Entrees_2x.jpg',
+        showNameCatPage: true,
+      },
+      {
+        id: 2,
+        name: 'SIDES',
+        description: '',
+        imageUrl: '/images/categories/Cat_Sides_2x.jpg',
+        showNameCatPage: true,
+      },
+      {
+        id: 3,
+        name: 'BEST SELLERS',
+        description: 'Dinner in 30 minutes',
+        imageUrl: '/images/categories/Cat_Best_Sellers_2x.jpg',
+        showNameCatPage: true,
+      },
+      {
+        id: 4,
+        name: 'FAVORITES',
+        description: '',
+        imageUrl: '',
+        hiddenInNavigation: true,
+        showNameCatPage: true,
+      },
+      {
+        id: 5,
+        name: 'HOLIDAY',
+        description: '',
+        imageUrl: '',
+        hiddenInNavigation: true,
+        showNameCatPage: true,
+      },
+      {
+        id: 6,
+        name: 'GIFT CARDS',
+        description: 'Gift cards will be here',
+        imageUrl: '',
+        showNameCatPage: true,
+      },
+      {
+        id: 7,
+        name: 'VALENTINE',
+        description: 'Valentine\'s Day Picks',
+        imageUrl: '/images/categories/Cat_Valentine_2x.jpg',
+        hiddenInNavigation: true,
+        showNameCatPage: false,
+      },
+      {
+        id: 8,
+        name: 'WEEKNIGHT',
+        description: 'Weeknight Favorites',
+        imageUrl: '/images/categories/Cat_Weeknights_2x.jpg',
+        hiddenInNavigation: true,
+        showNameCatPage: false,
+      },
+      {
+        id: 9,
+        name: 'SPIN',
+        description: 'Classics Revisited',
+        imageUrl: '/images/categories/Cat_Spin_2x.jpg',
+        hiddenInNavigation: true,
+        showNameCatPage: false,
+      },
+    ];
+  } else {
+    // on and after timeTarget
+    result = [
+      {
+        id: 0,
+        name: 'APPETIZERS',
+        description: 'Starters in 10 Minutes NOW!',
+        imageUrl: '/images/categories/Cat_Appetizers_2x.jpg',
+        showNameCatPage: true,
+      },
+      {
+        id: 1,
+        name: 'ENTRÉES',
+        description: 'Dinner in 30 minutes',
+        imageUrl: '/images/categories/Cat_Entrees_2x.jpg',
+        showNameCatPage: true,
+      },
+      {
+        id: 2,
+        name: 'SIDES',
+        description: '',
+        imageUrl: '/images/categories/Cat_Sides_2x.jpg',
+        showNameCatPage: true,
+      },
+      {
+        id: 3,
+        name: 'BEST SELLERS',
+        description: 'Dinner in 30 minutes',
+        imageUrl: '/images/categories/Cat_Best_Sellers_2x.jpg',
+        showNameCatPage: true,
+      },
+      {
+        id: 4,
+        name: 'FAVORITES',
+        description: 'Tried and True Favorites',
+        imageUrl: '/images/categories/Cat_TriedTrue_2x.jpg',
+        hiddenInNavigation: true,
+        showNameCatPage: false,
+      },
+      {
+        id: 5,
+        name: 'HOLIDAY',
+        description: '',
+        imageUrl: '',
+        hiddenInNavigation: true,
+        showNameCatPage: true,
+      },
+      {
+        id: 6,
+        name: 'GIFT CARDS',
+        description: 'Gift cards will be here',
+        imageUrl: '',
+        showNameCatPage: true,
+      },
+      {
+        id: 7,
+        name: 'WEEKNIGHT',
+        description: 'Weeknight Favorites',
+        imageUrl: '/images/categories/Cat_Weeknights_2x.jpg',
+        hiddenInNavigation: true,
+        showNameCatPage: false,
+      },
+      {
+        id: 8,
+        name: 'SPIN',
+        description: 'Classics Revisited',
+        imageUrl: '/images/categories/Cat_Spin_2x.jpg',
+        hiddenInNavigation: true,
+        showNameCatPage: false,
+      },
+    ];
+  }
+
   return Promise.resolve(result);
 }
 
--- src/pages/home/home-page.jsx
+++ src/pages/home/home-page.jsx
@@ -3,6 +3,7 @@
 // libs
 import React from 'react';
 import _ from 'lodash';
+import moment from 'moment';
 
 // components
 import ActionBlock from './action-block';
@@ -11,26 +12,56 @@ import Icon from 'ui/icon';
 // styles
 import styles from './home-page.css';
 
-const mainBlocks = [
-  {
-    imageUrl: '/images/home-page/Home_Page_Valentines.jpg',
-    description: '',
-    title: 'Dinners In That Feel Like Nights Out',
-    action: { title: 'Shop Now', link: '/VALENTINE' },
-  },
-  {
-    imageUrl: '/images/home-page/Home_Page_Kid_2x.jpg',
-    description: 'Under $5 Per Entrée',
-    title: 'Weeknight Favorites',
-    action: { title: 'Shop Now', link: '/WEEKNIGHT' },
-  },
-  {
-    imageUrl: '/images/home-page/Home_Page_EggRolls.jpg',
-    description: 'Dishes you know and love, revisited with unique and delicious twists',
-    title: 'A New Spin On The Classics',
-    action: { title: 'Shop Now', link: '/SPIN' },
-  },
-];
+const timeTarget = moment('2017-02-15');
+const timeNow = moment();
+
+let mainBlocks = [];
+
+if (timeTarget > timeNow) {
+  // before timeTarget
+  mainBlocks = [
+    {
+      imageUrl: '/images/home-page/Home_Page_Valentines.jpg',
+      description: '',
+      title: 'Dinners In That Feel Like Nights Out',
+      action: { title: 'Shop Now', link: '/VALENTINE' },
+    },
+    {
+      imageUrl: '/images/home-page/Home_Page_Kid_2x.jpg',
+      description: 'Under $5 Per Entrée',
+      title: 'Weeknight Favorites',
+      action: { title: 'Shop Now', link: '/WEEKNIGHT' },
+    },
+    {
+      imageUrl: '/images/home-page/Home_Page_EggRolls.jpg',
+      description: 'Dishes you know and love, revisited with unique and delicious twists',
+      title: 'A New Spin On The Classics',
+      action: { title: 'Shop Now', link: '/SPIN' },
+    },
+  ];
+} else {
+  // on and after timeTarget
+  mainBlocks = [
+    {
+      imageUrl: '/images/home-page/Home_Page_Kid_2x.jpg',
+      description: 'Under $5 Per Entrée',
+      title: 'Weeknight Favorites',
+      action: { title: 'Shop Now', link: '/WEEKNIGHT' },
+    },
+    {
+      imageUrl: '/images/home-page/Home_Page_EggRolls.jpg',
+      description: 'Dishes you know and love, revisited with unique and delicious twists',
+      title: 'A New Spin On The Classics',
+      action: { title: 'Shop Now', link: '/SPIN' },
+    },
+    {
+      imageUrl: '/images/home-page/Home_Page_TriedTrue-2x.jpg',
+      description: '',
+      title: 'Tried and True Favorites',
+      action: { title: 'Shop Now', link: '/FAVORITES' },
+    },
+  ];
+}
 
 const instagramLinks = [
   'https://www.instagram.com/p/BPvzneyhjGD',
