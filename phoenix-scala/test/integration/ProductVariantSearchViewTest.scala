08698390f5db042cc22499a7ff49ae7b9a6fd9e2
Anna Zubenko <anna@foxcommerce.com>
2017-02-09 17:52:38 +0200

    Updated create/update tests; added archive test; cleaned up code

--- phoenix-scala/test/integration/ProductVariantSearchViewTest.scala
+++ phoenix-scala/test/integration/ProductVariantSearchViewTest.scala
@@ -1,13 +1,16 @@
+import java.time.Instant
+
+import cats.implicits._
 import models.objects.ObjectContexts
 import org.json4s.jackson.parseJson
-import payloads.ProductVariantPayloads.ProductVariantPayload
+import payloads.ImagePayloads.{AlbumPayload, ImagePayload}
 import responses.ProductVariantResponses.ProductVariantResponse
-import slick.profile.SqlStreamingAction
 import testutils._
 import testutils.apis.PhoenixAdminApi
 import testutils.fixtures.api._
 import testutils.fixtures.api.products._
 import utils.MockedApis
+import utils.Money.Currency
 import utils.aliases.Json
 import utils.db.ExPostgresDriver.api._
 
@@ -17,6 +20,7 @@ import utils.db.ExPostgresDriver.api._
 case class ProductVariantSearchViewResult(id: Int,
                                           variantId: Int,
                                           skuCode: String,
+                                          // Why do we have both context name and context id in view?
                                           context: String,
                                           contextId: Int,
                                           title: String,
@@ -53,39 +57,70 @@ class ProductVariantSearchViewTest
   def priceAsString(attrs: Json, field: String): String =
     (attrs \ field \ "v" \ "value").extract[String]
 
-  "Product variant search view" - {
+  "Product variant search view must be updated" - {
 
-    "must return variant view after variant is created" in new ProductVariant_ApiFixture {
-      val variantSv = findOneInSearchView(productVariant.id)
+    "when variant is created" in new ProductVariant_ApiFixture {
+      val variantAsViewed = findOneInSearchView(productVariant.id)
+
+      // Context is not built as part of variant response when embedded in product response ⇒ need to call GET
+      val variantContextName = productVariantsApi(productVariant.id)
+        .get()
+        .as[ProductVariantResponse.Root]
+        .context
+        .value
+        .name
+      val variantContextId =
+        ObjectContexts.filterByName(variantContextName).map(_.id).gimme.onlyElement
 
       {
-        import variantSv._
+        import variantAsViewed._
 
         variantId must === (productVariant.id)
         middlewarehouseSkuId must === (productVariant.skuId)
         archivedAt must not be defined
         retailPrice must === (priceAsString(productVariant.attributes, "retailPrice"))
         salePrice must === (priceAsString(productVariant.attributes, "salePrice"))
-        retailPriceCurrency must === ("USD")
-        salePriceCurrency must === ("USD")
+        retailPriceCurrency must === (Currency.USD.getCode)
+        salePriceCurrency must === (Currency.USD.getCode)
         image must not be defined
         skuCode must === (productVariantCode)
         externalId must not be defined
         title must === (productVariant.attributes.getString("title"))
-        // scope?
-        // context is not built for variant as part of product response; can call get
+        context must === (variantContextName)
+        contextId must === (variantContextId)
+        // TODO how to check scope?
+        scope must not be empty
       }
     }
 
-    "must update variant view when variant is updated" in new ProductVariant_ApiFixture {
-      val newPrice = 123
+    "when variant is updated" in new ProductVariant_ApiFixture {
+      private val newPrice = 123
+      private val imageSrc = "by the way we need image src validation..."
+      private val albums   = Seq(AlbumPayload(images = Seq(ImagePayload(src = imageSrc)).some)).some
+      private val newTitle = "Variant McVariantFace"
+
       productVariantsApi(productVariant.id)
-        .update(buildVariantPayload(code = productVariantCode, price = newPrice))
+        .update(
+            buildVariantPayload(code = productVariantCode,
+                                price = newPrice,
+                                albums = albums,
+                                title = newTitle))
         .as[ProductVariantResponse.Root]
 
       val updated = findOneInSearchView(productVariant.id)
       updated.salePrice must === (newPrice.toString)
       updated.retailPrice must === (newPrice.toString)
+      updated.image.value must === (imageSrc)
+      updated.title must === (newTitle)
+    }
+
+    "when variant is archived" in new ProductVariant_ApiFixture {
+      productVariantsApi(productVariant.id).archive().mustBeOk()
+
+      val archivedAtAsString = findOneInSearchView(productVariant.id).archivedAt.value
+      private val archivedAt = Instant.parse(archivedAtAsString)
+      archivedAt.isBefore(Instant.now) mustBe true
+      archivedAt.isAfter(Instant.now.minusSeconds(1L)) mustBe true
     }
   }
 }
