39260d06893dcfc5f52cc229b4fe6883ac3fadb7
Josh Ferrerra <josh@foxcommerce.com>
2017-02-13 17:34:37 -0500

    cleanup debugging crap

--- intelligence/cross-sell/prod-prod/src/router.py
+++ intelligence/cross-sell/prod-prod/src/router.py
@@ -1,27 +1,21 @@
 from flask import Flask, request, jsonify
-from Recommend import Recommend
+from PPRecommend import PPRecommend
 
 app = Flask(__name__)
 
-fake_resp = {'products': [
-    {'id': 1, 'score': 0.75},
-    {'id': 2, 'score': 0.65},
-    {'id': 3, 'score': 0.55}
-]}
-
-knn_rec = Recommend()
+pprec = PPRecommend()
 
 @app.route('/ping')
-def hello_world():
+def ping():
     return 'pong'
 
 @app.route('/recommend/prod-prod/<int:prod_id>', methods=['GET'])
 def rec_prod_prod(prod_id):
-    return jsonify(knn_rec.recommend(prod_id))
+    return jsonify(pprec.recommend(prod_id))
 
-@app.route('/recommend/train', methods=['POST'])
+@app.route('/recommend/prod-prod/train', methods=['POST'])
 def train():
     json_dict = request.get_json()
     for point in json_dict['points']:
-        knn_rec.addPoint(point['custID'], point['prodID'])
+        pprec.addPoint(point['custID'], point['prodID'])
     return ""
