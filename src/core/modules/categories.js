2badea76e1457c20fdec011bcb02e62fc4f6eeb0
Steven Novick <steven@Stevens-MacBook-Pro.local>
2017-02-15 11:30:15 -0800

    Fixed Lint errors

--- src/core/modules/categories.js
+++ src/core/modules/categories.js
@@ -5,75 +5,75 @@ import { createAsyncActions } from '@foxcomm/wings';
 
 function apiCall(): Promise {
   const result = [
-      {
-        id: 0,
-        name: 'APPETIZERS',
-        description: 'Starters in 10 Minutes',
-        imageUrl: '/images/categories/Cat_Appetizers_2x.jpg',
-        showNameCatPage: true,
-      },
-      {
-        id: 1,
-        name: 'ENTRÉES',
-        description: 'Dinner in 30 minutes',
-        imageUrl: '/images/categories/Cat_Entrees_2x.jpg',
-        showNameCatPage: true,
-      },
-      {
-        id: 2,
-        name: 'SIDES',
-        description: '',
-        imageUrl: '/images/categories/Cat_Sides_2x.jpg',
-        showNameCatPage: true,
-      },
-      {
-        id: 3,
-        name: 'BEST SELLERS',
-        description: 'Dinner in 30 minutes',
-        imageUrl: '/images/categories/Cat_Best_Sellers_2x.jpg',
-        showNameCatPage: true,
-      },
-      {
-        id: 4,
-        name: 'FAVORITES',
-        description: 'Tried and True Favorites',
-        imageUrl: '/images/categories/Cat_TriedTrue_2x.jpg',
-        hiddenInNavigation: true,
-        showNameCatPage: false,
-      },
-      {
-        id: 5,
-        name: 'GIFT CARDS',
-        description: 'Gift cards will be here',
-        imageUrl: '',
-        showNameCatPage: true,
-      },
-      {
-        // TODO: REMOVE THIS CODE AFTER Feb 15
-        id: 6,
-        name: 'VALENTINE',
-        description: 'Valentine\'s Day Picks',
-        imageUrl: '/images/categories/Cat_Valentine_2x.jpg',
-        hiddenInNavigation: true,
-        showNameCatPage: false,
-      },
-      {
-        id: 7,
-        name: 'WEEKNIGHT',
-        description: 'Weeknight Favorites',
-        imageUrl: '/images/categories/Cat_Weeknights_2x.jpg',
-        hiddenInNavigation: true,
-        showNameCatPage: false,
-      },
-      {
-        id: 8,
-        name: 'SPIN',
-        description: 'Classics Revisited',
-        imageUrl: '/images/categories/Cat_Spin_2x.jpg',
-        hiddenInNavigation: true,
-        showNameCatPage: false,
-      },
-    ];
+    {
+      id: 0,
+      name: 'APPETIZERS',
+      description: 'Starters in 10 Minutes',
+      imageUrl: '/images/categories/Cat_Appetizers_2x.jpg',
+      showNameCatPage: true,
+    },
+    {
+      id: 1,
+      name: 'ENTRÉES',
+      description: 'Dinner in 30 minutes',
+      imageUrl: '/images/categories/Cat_Entrees_2x.jpg',
+      showNameCatPage: true,
+    },
+    {
+      id: 2,
+      name: 'SIDES',
+      description: '',
+      imageUrl: '/images/categories/Cat_Sides_2x.jpg',
+      showNameCatPage: true,
+    },
+    {
+      id: 3,
+      name: 'BEST SELLERS',
+      description: 'Dinner in 30 minutes',
+      imageUrl: '/images/categories/Cat_Best_Sellers_2x.jpg',
+      showNameCatPage: true,
+    },
+    {
+      id: 4,
+      name: 'FAVORITES',
+      description: 'Tried and True Favorites',
+      imageUrl: '/images/categories/Cat_TriedTrue_2x.jpg',
+      hiddenInNavigation: true,
+      showNameCatPage: false,
+    },
+    {
+      id: 5,
+      name: 'GIFT CARDS',
+      description: 'Gift cards will be here',
+      imageUrl: '',
+      showNameCatPage: true,
+    },
+    {
+      // TODO: REMOVE THIS CODE AFTER Feb 15
+      id: 6,
+      name: 'VALENTINE',
+      description: 'Valentine\'s Day Picks',
+      imageUrl: '/images/categories/Cat_Valentine_2x.jpg',
+      hiddenInNavigation: true,
+      showNameCatPage: false,
+    },
+    {
+      id: 7,
+      name: 'WEEKNIGHT',
+      description: 'Weeknight Favorites',
+      imageUrl: '/images/categories/Cat_Weeknights_2x.jpg',
+      hiddenInNavigation: true,
+      showNameCatPage: false,
+    },
+    {
+      id: 8,
+      name: 'SPIN',
+      description: 'Classics Revisited',
+      imageUrl: '/images/categories/Cat_Spin_2x.jpg',
+      hiddenInNavigation: true,
+      showNameCatPage: false,
+    },
+  ];
 
   return Promise.resolve(result);
 }
