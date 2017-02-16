709e498a32ba3632b468001a7f8fb4e3b406808a
Roman Sotnikov <roman.sotnikov@gmail.com>
2017-02-16 16:53:12 +0700

    Fix migrations, dockerfile, and mix

--- hyperion/mix.exs
+++ hyperion/mix.exs
@@ -15,7 +15,7 @@ defmodule Hyperion.Mixfile do
   # Type "mix help compile.app" for more information
   def application do
     # Specify extra applications you'll use from Erlang/Elixir
-    [extra_applications: [:ex_aws, :logger, :maru, :postgrex, :ecto, :httpoison],
+    [ extra_applications: (Mix.env == :dev && [:exsync] || []) ++ [:ex_aws, :logger, :maru, :postgrex, :ecto, :httpoison],
      mod: {Hyperion, []}]
   end
 
