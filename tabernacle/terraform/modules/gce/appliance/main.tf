84ad79c45c71d8c07ea4353f66118177dbbd8bda
Pavel Kiselev <kpashka@gmail.com>
2017-02-18 06:09:05 +0700

    Terraform feature branch instances (#896)
    * Terraform feature branch instances
    
    * terraform fmt
    
    * Update
    
    * tffmt
    
    * Add tf state
    
    * Boilerplate
    
    * Add boolean filter
    
    * Various updates
    
    * Remove static inventory records
    
    * River Rock workaround
    
    * Move boilerplate

---
+++ tabernacle/terraform/modules/gce/appliance/main.tf
@@ -0,0 +1,62 @@
+variable "ssh_user" {}
+
+variable "ssh_private_key" {}
+
+variable "dnsimple_email" {}
+
+variable "dnsimple_token" {}
+
+variable "appliance_image" {}
+
+variable "instance_name" {}
+
+variable "consul_leader" {}
+
+resource "google_compute_instance" "appliance" {
+  name         = "${var.instance_name}"
+  machine_type = "n1-standard-4"
+  tags         = ["no-ip", "${var.instance_name}"]
+  zone         = "us-central1-a"
+
+  service_account {
+    scopes = ["storage-rw"]
+  }
+
+  disk {
+    image = "${var.appliance_image}"
+    type  = "pd-ssd"
+    size  = "40"
+  }
+
+  network_interface {
+    network = "default"
+  }
+
+  connection {
+    type        = "ssh"
+    user        = "${var.ssh_user}"
+    private_key = "${file(var.ssh_private_key)}"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "/usr/local/bin/bootstrap.sh",
+    ]
+  }
+}
+
+##############################################
+# Setup DNS
+##############################################
+provider "dnsimple" {
+  token = "${var.dnsimple_token}"
+  email = "${var.dnsimple_email}"
+}
+
+resource "dnsimple_record" "frontend-dns-record" {
+  domain = "foxcommerce.com"
+  name   = "${var.instance_name}"
+  value  = "${google_compute_instance.appliance.0.network_interface.0.address}"
+  type   = "A"
+  ttl    = 3600
+}
