5bb630c83a91b84cfd4035e4d346b0b397d67e46
Pavel Kiselev <kpashka@gmail.com>
2017-02-18 04:29:01 +0700

    Terraform feature branch instances

---
+++ tabernacle/terraform/base/gce_branches/main.tf
@@ -0,0 +1,38 @@
+variable "ssh_user" {}
+
+variable "ssh_private_key" {}
+
+variable "account_file" {}
+
+variable "gce_project" {}
+
+variable "region" {}
+
+variable "appliance_image" {}
+
+variable "consul_leader" {}
+
+variable "dnsimple_token" {}
+
+variable "dnsimple_email" {}
+
+provider "google" {
+  credentials = "${file(var.account_file)}"
+  project     = "${var.gce_project}"
+  region      = "${var.region}"
+}
+
+##############################################
+# Setup Returns Feature Branch
+##############################################
+module "returns" {
+  source                = "../../modules/gce/appliance"
+  datacenter            = "feature-branch-returns"
+  dns_record            = "feature-branch-returns"
+  appliance_image       = "${var.appliance_image}"
+  consul_leader         = "${var.consul_leader}"
+  ssh_user              = "${var.ssh_user}"
+  ssh_private_key       = "${var.ssh_private_key}"
+  dnsimple_email        = "${var.dnsimple_email}"
+  dnsimple_token        = "${var.dnsimple_token}"
+}
---
+++ tabernacle/terraform/modules/gce/appliance/main.tf
@@ -0,0 +1,66 @@
+variable "ssh_user" {}
+
+variable "ssh_private_key" {}
+
+variable "dnsimple_email" {}
+
+variable "dnsimple_token" {}
+
+variable "appliance_image" {}
+
+variable "datacenter" {}
+
+variable "dns_record" {}
+
+variable "consul_leader" {}
+
+resource "google_compute_instance" "appliance" {
+  name         = "${var.datacenter}-consul-server"
+  machine_type = "n1-standard-4"
+  tags         = ["no-ip", "${var.datacenter}"]
+  zone         = "us-central1-a"
+
+  service_account {
+    scopes = ["storage-rw"]
+  }
+
+  disk {
+    image = "${var.appliance_image}"
+    type  = "pd-ssd"
+    size  = "40"
+  }
+
+  network_interface {
+    network = "default"
+  }
+
+  connection {
+    type        = "ssh"
+    user        = "${var.ssh_user}"
+    private_key = "${file(var.ssh_private_key)}"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "/usr/local/bin/bootstrap.sh",
+      "/usr/local/bin/bootstrap_consul.sh ${var.datacenter} ${var.consul_leader}",
+      "sudo rm -rf /var/consul/* && sudo systemctl restart consul_server.service",
+    ]
+  }
+}
+
+##############################################
+# Setup DNS
+##############################################
+provider "dnsimple" {
+  token = "${var.dnsimple_token}"
+  email = "${var.dnsimple_email}"
+}
+
+resource "dnsimple_record" "frontend-dns-record" {
+  domain = "foxcommerce.com"
+  name   = "feature-branch-returns"
+  value  = "${google_compute_instance.appliance.0.network_interface.0.address}"
+  type   = "A"
+  ttl    = 3600
+}
