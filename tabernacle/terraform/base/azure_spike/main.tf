ac4db152f424ba002be7da7b0189e35d2a002992
Pavel Kiselev <kpashka@gmail.com>
2017-02-21 02:03:05 +0700

    Azure spike

---
+++ tabernacle/terraform/base/azure_spike/main.tf
@@ -0,0 +1,43 @@
+variable "azure_subscription_id" {}
+
+variable "azure_tenant_id" {}
+
+variable "azure_client_id" {}
+
+variable "azure_client_secret" {}
+
+provider "azurerm" {
+    subscription_id = "${var.azure_subscription_id}"
+    client_id       = "${var.azure_client_id}"
+    client_secret   = "${var.azure_client_secret}"
+    tenant_id       = "${var.azure_tenant_id}"
+}
+
+# Create a resource group
+resource "azurerm_resource_group" "production" {
+    name     = "production"
+    location = "West US"
+}
+
+# Create a virtual network in the web_servers resource group
+resource "azurerm_virtual_network" "network" {
+    name                = "productionNetwork"
+    address_space       = ["10.0.0.0/16"]
+    location            = "West US"
+    resource_group_name = "${azurerm_resource_group.production.name}"
+
+    subnet {
+        name           = "subnet1"
+        address_prefix = "10.0.1.0/24"
+    }
+
+    subnet {
+        name           = "subnet2"
+        address_prefix = "10.0.2.0/24"
+    }
+
+    subnet {
+        name           = "subnet3"
+        address_prefix = "10.0.3.0/24"
+    }
+}
