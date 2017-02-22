1138ca09420ee2c1a4ad11474ab41d9a0c4a52be
tony.pizzicato <tony.grebnev@gmail.com>
2017-02-22 20:38:31 +0300

    better naming

--- middlewarehouse/common/utils/utils.go
+++ middlewarehouse/common/utils/utils.go
@@ -35,19 +35,23 @@ func DiffSlices(needles []string, haystack []string) []string {
 func SanitizePassword(input []byte) string {
 	r, _ := regexp.Compile("pass(?:word|wd)?\":\"(.+?)\"")
 
-	return ReplaceAll(string(input), "***", r)
+	return ReplaceAllMatchingGroup(string(input), "***", r, 1)
 }
 
-func ReplaceAll(input, repl string, r *regexp.Regexp) string {
+func ReplaceAllMatchingGroup(input, repl string, r *regexp.Regexp, group int) string {
 	matches := r.FindAllStringSubmatchIndex(input, -1)
 
 	if len(matches) > 0 {
 		for _, m := range matches {
-			ru := []byte(input)
-			toReplace := string(ru[m[2]:m[3]])
+			bt := []byte(input)
+			index := group * 2
+			from := m[index]
+			to := m[index+1]
+
+			toReplace := string(bt[from:to])
 
 			if repl != toReplace {
-				return ReplaceAll(ReplaceByIndices(input, repl, m[2], m[3]), repl, r)
+				return ReplaceAllMatchingGroup(ReplaceByIndices(input, repl, from, to), repl, r, group)
 			}
 
 		}
