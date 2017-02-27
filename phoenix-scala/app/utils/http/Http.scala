1d93cf42b5445bafe3cc6910e4857c4f06cbcac7
Michal Rus <m@michalrus.com>
2017-02-27 17:54:29 +0100

    Rename SuccessfulPayload → Response & remove .isInstanceOf
    [skip ci]

--- phoenix-scala/app/utils/http/Http.scala
+++ phoenix-scala/app/utils/http/Http.scala
@@ -22,32 +22,33 @@ object Http {
   private def renderNotFoundFailure(f: NotFoundFailure404): HttpResponse =
     notFoundResponse.copy(entity = jsonEntity("errors" → Seq(f.message)))
 
-  final case class SuccessfulPayload(result: AnyRef,
-                                     warnings: Option[List[String]],
-                                     errors: Option[List[String]],
-                                     batch: Option[BatchMetadata])
+  private final case class SuccessfulResponse(result: AnyRef,
+                                              warnings: Option[List[String]],
+                                              errors: Option[List[String]],
+                                              batch: Option[BatchMetadata])
 
-  object SuccessfulPayload {
-    def from(result: AnyRef, uiInfo: List[UIInfo]): SuccessfulPayload = {
+  private object SuccessfulResponse {
+    def from(result: AnyRef, uiInfo: List[UIInfo]): SuccessfulResponse = {
       val uiInfoWarnings = uiInfo.collect { case UIInfo.Warning(f)        ⇒ f.description }
       val uiInfoErrors   = uiInfo.collect { case UIInfo.Error(f)          ⇒ f.description }
       val uiInfoBatches  = uiInfo.collectFirst { case UIInfo.BatchInfo(b) ⇒ b }
       // FIXME: have a way of merging multiple `BatchMetadata`s, as types allow for that. @michalrus
-      def insane[A](xs: List[A]): Option[List[A]] = if (xs.nonEmpty) Some(xs) else None
+      def emptyToNoneNonemptyToSome[A](xs: List[A]): Option[List[A]] =
+        if (xs.nonEmpty) Some(xs) else None
       result match {
         // FIXME: get rid of `TheResponse` and s/AnyRef/Any/ around here. @michalrus
         case TheResponse(res, alerts, errors, warnings, batch) ⇒
-          SuccessfulPayload(
+          SuccessfulResponse(
               result = res,
-              warnings =
-                insane(uiInfoWarnings ::: alerts.toList.flatten ::: warnings.toList.flatten),
-              errors = insane(uiInfoErrors ::: errors.toList.flatten),
+              warnings = emptyToNoneNonemptyToSome(
+                  uiInfoWarnings ::: alerts.toList.flatten ::: warnings.toList.flatten),
+              errors = emptyToNoneNonemptyToSome(uiInfoErrors ::: errors.toList.flatten),
               batch = uiInfoBatches orElse batch)
         case raw ⇒
-          SuccessfulPayload(result = raw,
-                            warnings = insane(uiInfoWarnings),
-                            errors = insane(uiInfoErrors),
-                            batch = uiInfoBatches)
+          SuccessfulResponse(result = raw,
+                             warnings = emptyToNoneNonemptyToSome(uiInfoWarnings),
+                             errors = emptyToNoneNonemptyToSome(uiInfoErrors),
+                             batch = uiInfoBatches)
       }
     }
   }
@@ -56,12 +57,12 @@ object Http {
     HttpResponse(OK, entity = jsonEntity(resource))
 
   def render(result: AnyRef, uiInfo: List[UIInfo], statusCode: StatusCode = OK) = {
-    val payload = SuccessfulPayload.from(result, uiInfo)
-    val temporaryHack: AnyRef =
-      if (!result
-            .isInstanceOf[TheResponse[_]] && payload.batch.isEmpty && payload.warnings.isEmpty)
-        payload.result
-      else payload
+    val response = SuccessfulResponse.from(result, uiInfo)
+    val temporaryHack: AnyRef = result match {
+      case _: TheResponse[_]                                        ⇒ response
+      case _ if response.batch.isEmpty && response.warnings.isEmpty ⇒ response.result
+      case _                                                        ⇒ response
+    }
     HttpResponse(statusCode, entity = jsonEntity(temporaryHack))
   }
 
