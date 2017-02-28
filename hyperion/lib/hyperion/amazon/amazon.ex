612d874a9ecd26407afeb6102c1822b8bf0d720c
Roman Sotnikov <roman.sotnikov@gmail.com>
2017-02-28 14:33:36 +0300

    Add product variations for Amazon (#929)
    * Add variation data and format XML
    
    * Rescue constraint error if credentials for this client has been already added
    
    * Fix product_feed XML
    
    * Remove comments

--- hyperion/lib/hyperion/amazon/amazon.ex
+++ hyperion/lib/hyperion/amazon/amazon.ex
@@ -1,9 +1,5 @@
 defmodule Hyperion.Amazon do
-  import Ecto.Query
-  alias Hyperion.Repo
   alias Hyperion.PhoenixScala.Client
-
-  alias Hyperion.Amazon.TemplateBuilder
 
   @doc """
   Returns formatted indexed list of products gotten by it's ids
@@ -11,8 +7,9 @@ defmodule Hyperion.Amazon do
   def product_feed(product_id, jwt) do
     # FIXME: get products by may ids
     # remove hd(product_id) and update PhoenixScala.Client
-    r = Client.get_product(hd(product_id), jwt)
+    Client.get_product(hd(product_id), jwt)
     |> process_products
+    |> set_parentage
     |> Enum.with_index(1)
   end
 
@@ -22,7 +19,7 @@ defmodule Hyperion.Amazon do
   def price_feed(product_id, jwt) do
      Client.get_product(hd(product_id), jwt)
     |> process_products
-    |> Enum.map(fn(x) -> Enum.filter(x, fn({k, v}) -> k == :code || k == :retailprice  end) end)
+    |> Enum.map(fn(x) -> Enum.filter(x, fn({k, _v}) -> k == :code || k == :retailprice end) end)
     |> Enum.with_index(1)
   end
 
@@ -35,35 +32,108 @@ defmodule Hyperion.Amazon do
     |> process_images
   end
 
+  # gets the albums and images for skus,
+  # sets the album name as key and atomize it
+  # adds the SKU for matching
   defp process_images(response) do
     r = response.body
-    a = Enum.map(r["skus"], fn(s)->
-      Enum.map(s["albums"], fn(a) ->
-        {String.to_atom(a["name"]), a["images"]}
+    albums = Enum.map(r["skus"], fn(sku)->
+      Enum.map(sku["albums"], fn(album) ->
+        {String.to_atom(album["name"]), album["images"]}
       end)
-    end) |> hd
-    Enum.map(r["skus"], fn(x)-> [albums: a, code: x["attributes"]["code"]["v"]] end)
+    end) |> Enum.reject(fn(list) -> list == [] end) |> hd
+    Enum.map(r["skus"], fn(x)-> [albums: albums, code: x["attributes"]["code"]["v"]] end)
   end
 
-  @doc """
-  Construct products list from client result
-  """
-  defp process_products(response) do
-    products = response.body["attributes"] |> format_map
+  # gets product, skus, and variants info
+  # merge product with all skus, delete empty :description field
+  # add variants to spoper skus
+  # sets parent product
+  def process_products(response) do
+    raw_products = response.body["attributes"]
+               |> format_map
 
     skus = response.body["skus"]
            |> Enum.map(fn(map) -> format_map(map["attributes"]) end)
-           |> Enum.filter( fn (m) -> m[:channel] == "amazon"  end)
 
-    Enum.map(skus, fn(s) -> Enum.into(s, products) end)
-    |> Enum.map(fn list -> Keyword.delete(list, :description, "<p><br></p>") end)
+    variants = process_variants(response.body["variants"])
+
+
+    products = Enum.map(skus, fn(s) -> Enum.into(s, raw_products) end)
+               |> Enum.map(fn list -> Keyword.delete(list, :description, "<p><br></p>") end)
+
+    for product <- products do
+      Enum.into(product, Keyword.get(variants, String.to_atom(product[:code])))
+    end
   end
 
-  @doc """
-  Formats list: convert names to atoms, reduce the {type, value tuple}, and removes empty values
-  """
+  # atomizes the keys
+  # transform variants data to proper structure
+  # assign variants options to associated sku
+  def process_variants(variants) do
+    func = fn var ->
+      {String.to_atom(var["attributes"]["name"]["v"]), atomize_keys(var["values"])}
+    end
+
+    props = Enum.map(variants, func)
+            |> Enum.flat_map(fn(variant) -> transform_variants(variant) end)
+    skus = Enum.map(props, fn{sku, _l} -> sku end) |> Enum.uniq
+
+    for sku <- skus do
+      Keyword.put_new([], sku, Keyword.get_values(props, sku) |> List.flatten)
+    end |> List.flatten
+  end
+
+  # for each sku in list (not unique) gets the all available options as {key, name}
+  # groups the list by sku and flatten it
+  def transform_variants({key, var}) do
+    for v <- var do
+      name = v[:name]
+      list = Keyword.new
+      for s <- v[:skuCodes] do
+        Keyword.put(list, String.to_atom(s), Keyword.put_new([], key, name))
+      end
+    end |> List.flatten
+        |> Enum.group_by(fn({key, _lst}) -> key end)
+        |> Map.to_list
+        |> Enum.flat_map(fn{_k, data} -> data end)
+  end
+
+  # deeply atomize all keys in Keyword structure
+  defp atomize_keys(values) do
+    for val <- values do
+      atomk = fn({k, v}, acc) ->
+                Map.put_new(acc, String.to_atom(k), v)
+              end
+      Enum.reduce(val, %{}, atomk)
+    end
+  end
+
+  # duplicates first list element
+  # removes unneeded fields
+  # adds parent SKU
+  # adds duplicated element as parent to all children
+  defp set_parentage(list) do
+    parent = hd(list)
+             |> Enum.into(parentage: "parent")
+             |> Enum.reject(fn{k, _v} -> k in [:upc, :taxcode] end)
+             |> Keyword.update(:code, nil, &("PARENT#{&1}"))
+    children = Enum.map(list, fn(c) -> Enum.into(c, parentage: "child") end)
+    [parent|children]
+  end
+
+  # formats mas from `"foo" => {"t" => 'type', "v" => 'value'}`
+  # to foo: "value"
+  # removes empty and nil values
   defp format_map(map) do
-    Enum.map(map, fn({k, %{"t" => _t, "v" => v}}) -> {String.downcase(k) |> String.to_atom, v} end)
+    Enum.map(map, fn({k, %{"t" => _t, "v" => v}}) -> {format_string(k), v} end)
     |> Enum.reject(fn({_k, v}) -> v == nil || v == "" end)
+  end
+
+  def format_string(str) do
+    str
+    |> String.downcase
+    |> String.replace(~r/\s+/, "")
+    |> String.to_atom
   end
 end
