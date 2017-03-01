ecee37c522fbe760351714be7f2fcfd73bf178e0
DemetriousWilson <DemetriousWilson@gmail.com>
2017-03-01 14:35:19 -0800

    Add chanID to product recommender route

--- intelligence/cross-sell/prod-prod/src/PPRecommend.py
+++ intelligence/cross-sell/prod-prod/src/PPRecommend.py
@@ -7,14 +7,14 @@ class PPRecommend(object):
         self.events = set()
         self.upToDate = False
 
-    def add_point(self, custID, prodID):
+    def add_point(self, custID, prodID, chanID):
         """add_point
-        takes and event 'custID purchased prodID' and adds it to
+        takes and event 'custID purchased prodID, channel ID' and adds it to
         the recommender data.
         The sparse matrix needs to be recomputed before making more
         recommendations.
         """
-        self.events.add((custID, prodID))
+        self.events.add((custID, prodID, chanID))
         self.upToDate = False
 
     def make_matrix(self):
@@ -31,19 +31,19 @@ class PPRecommend(object):
         these are the values to go in the sparse matrix so that the columns are
         l2 normalized. 
         """
-        return np.array([1.0/sqrt(self.count(x)) for (_, x) in self.events])
+        return np.array([1.0/sqrt(self.count(x)) for (_, x, _) in self.events])
 
     def count(self, prodID):
         """how many customers have purchased product prodID
         """
-        return len([prod for (_, prod) in self.events if prod == prodID])
+        return len([prod for (_, prod, _) in self.events if prod == prodID])
 
     def coords(self):
         """list of pairs (custID, prodID) where custID has purchased prodID
         """
         return (
-            [custID for (custID, _) in self.events],
-            [prodID for (_, prodID) in self.events]
+            [custID for (custID, _, _) in self.events],
+            [prodID for (_, prodID, _) in self.events]
         )
 
     def recommend(self, prodID):
@@ -58,7 +58,7 @@ class PPRecommend(object):
         v = self.mat[:, prodID].toarray()
         inds = np.argsort(v.T[0])[::-1]
         out = {'products': [{'id': np.asscalar(x), 'score': np.asscalar(y)}
-                for (x, y) in zip(inds, v[inds].T[0])
-                if x != int(prodID)]}
+                            for (x, y) in zip(inds, v[inds].T[0])
+                            if x != int(prodID)]}
 
         return out
