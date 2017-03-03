b2bb486afdd232d2913f01afdf51a20b35d05300
Artem <artem@foxcommerce.com>
2017-03-03 18:45:35 +0300

    Improve on formatting even more

--- ashes/src/components/icon/icon.jsx
+++ ashes/src/components/icon/icon.jsx
@@ -5,7 +5,7 @@ import classnames from 'classnames';
 const Icon = ({name, className} : {name:string, className?:string})=>{
   return(
     <svg className={className}>
-      <use xlinkHref={'#'+ "fc-"+name+"-icon"} />
+      <use xlinkHref={`#fc-${name}-icon`} />
     </svg>
   );
 };
