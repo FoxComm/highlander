2dc031e10ddc374200a2af595bf790db756b6fb5
Josh Ferrerra <josh@foxcommerce.com>
2017-03-03 16:19:13 -0500

    fix cross-sell endpoint urls
    old: api/v1/recommend/recommend/prod-prod/train
    new: api/v1/recommend/prod-prod/train

--- intelligence/cross-sell/src/router.py
+++ intelligence/cross-sell/src/router.py
@@ -10,12 +10,12 @@ pprec = PPRecommend()
 def ping():
     return 'pong'
 
-@app.route('/recommend/prod-prod/<int:prod_id>', methods=['GET'])
+@app.route('/prod-prod/<int:prod_id>', methods=['GET'])
 def rec_prod_prod(prod_id):
     return jsonify(pprec.recommend(prod_id))
 
-@app.route('/recommend/prod-prod/train', methods=['POST'])
-def train():
+@app.route('/prod-prod/train', methods=['POST'])
+def train_prod_prod():
     json_dict = request.get_json()
     for point in json_dict['points']:
         pprec.add_point(point['custID'], point['prodID'], point['chanID'])
