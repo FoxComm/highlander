35107adab4ff1a1875ca680b0bd7b6e7f0699b5a
Roman Sotnikov <roman.sotnikov@gmail.com>
2017-03-03 16:27:04 +0700

    Update category suggester: now we may have not primary category

--- hyperion/lib/hyperion/category_suggester.ex
+++ hyperion/lib/hyperion/category_suggester.ex
@@ -8,8 +8,9 @@ defmodule CategorySuggester do
     asin = get_product_asin(product_name, cfg)
     case MWSClient.get_product_categories_for_asin(asin, cfg) do
       {:success, resp} ->
+        IO.puts(inspect(resp))
         category = case resp["GetProductCategoriesForASINResponse"]["GetProductCategoriesForASINResult"]["Self"] do
-                     x when is_list(x) -> if (Enum.count(x) > 1), do: tl(x) |> hd , else: hd(x)
+                     x when is_list(x) -> hd x#if (Enum.count(x) > 1), do: tl(x) |> hd , else: hd(x)
                      x -> x
                    end
         get_all_categories([node_id: category["ProductCategoryId"], name: category["ProductCategoryName"]])
@@ -29,15 +30,22 @@ defmodule CategorySuggester do
   end
 
 
-  defp get_all_categories([node_id: node_id, name: name]) do
+  def get_all_categories([node_id: node_id, name: name]) do
     main_category = (from c in Category, select: %{node_id: c.node_id, node_path: c.node_path,
                                                    department: c.department, item_type: c.item_type})
                     |> where([c], c.node_id in ^[node_id])
                     |> Hyperion.Repo.all
-                    |> hd
+    all_categories = Category.search(name)
+    case main_category do
+      [] -> %{primary: nil, secondary: all_categories.items, count: Enum.count(all_categories.items)}
+      _ -> merge_categories(all_categories, main_category)
+    end
+  end
 
-    filtered = Category.search(name).items
-               |> Enum.reject(fn(cats) -> cats.node_id == main_category.node_id end)
+  def merge_categories(all, main) do
+    main_category = hd(main)
+    filtered = all.items
+               |> Enum.reject(fn(category) -> category.node_id == main_category.node_id end)
 
     %{primary: main_category, secondary: filtered, count: Enum.count(filtered) + 1}
   end
