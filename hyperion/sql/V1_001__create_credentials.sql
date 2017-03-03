3a5ed2442d43fc87aa3a75bf54ae950fd706d1f6
Roman Sotnikov <roman.sotnikov@gmail.com>
2017-03-03 17:23:48 +0700

    downcase sql

--- hyperion/sql/V1_001__create_credentials.sql
+++ hyperion/sql/V1_001__create_credentials.sql
@@ -1,8 +1,8 @@
-CREATE TABLE amazon_credentials(
+create table amazon_credentials(
   id bigserial primary key,
-  client_id int4 NULL,
-  seller_id varchar(255) NULL,
-  mws_auth_token varchar(255) NULL,
-  inserted_at timestamp NOT NULL,
-  updated_at timestamp NOT NULL
+  client_id int4 null,
+  seller_id varchar(255) null,
+  mws_auth_token varchar(255) null,
+  inserted_at timestamp not null,
+  updated_at timestamp not null
 );
