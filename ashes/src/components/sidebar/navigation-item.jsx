8748e6eb5da10e1d25f5505943921517b7adbc6e
Artem <artem@foxcommerce.com>
2017-03-06 14:59:43 +0300

    Restructure navigation-item.jsx to accommodate for sidebar enhancements

--- ashes/src/components/sidebar/navigation-item.jsx
+++ ashes/src/components/sidebar/navigation-item.jsx
@@ -2,100 +2,20 @@ import _ from 'lodash';
 import React, { PropTypes } from 'react';
 import classNames from 'classnames';
 import { autobind } from 'core-decorators';
+import styles from './navigation-item.css';
 
 import { IndexLink, Link } from '../link';
+
+import Icon from '../icon/icon';
 
 export default class NavigationItem extends React.Component {
 
   static propTypes = {
-    isIndex: PropTypes.bool,
-    isExpandable: PropTypes.bool,
-    collapsed: PropTypes.bool,
     to: PropTypes.string.isRequired,
     icon: PropTypes.string.isRequired,
     title: PropTypes.string.isRequired,
-    children: PropTypes.node,
-    routes: PropTypes.array.isRequired,
-    status: PropTypes.shape({
-      isOpen: PropTypes.bool,
-      toggledManually: PropTypes.bool
-    }),
-    toggleMenuItem: PropTypes.func
+    routes: PropTypes.array.isRequired
   };
-
-  static defaultProps = {
-    isIndex: false,
-    isExpandable: false,
-    collapsed: false,
-    status: {
-      isOpen: false,
-      toggledManually: false
-    }
-  };
-
-  @autobind
-  expandItem() {
-    if (this.isSelected && !this.props.status.isOpen && !this.props.status.toggledManually) {
-      this.props.toggleMenuItem(this.props.to, true);
-    } else {
-      this.props.toggleMenuItem(this.props.to);
-    }
-  }
-
-  get isSelected() {
-    let isSelected = false;
-    if (!_.isEmpty(this.props.children)) {
-      const tos = _.compact(React.Children.map(this.props.children, c => c.props.to));
-      const routeNames = _.compact(this.props.routes.map(route => route.name));
-      isSelected = !_.isEmpty(_.intersection(tos, routeNames));
-    }
-    return isSelected;
-  }
-
-  get currentState() {
-    const mustBeOpenAutomatically = !this.props.status.toggledManually && this.isSelected;
-    return !this.props.collapsed && this.props.isExpandable &&
-      (this.props.status.isOpen || mustBeOpenAutomatically);
-  }
-
-  get expandButton() {
-    const iconClass = classNames('icon-chevron-down', { '_up': this.currentState });
-    return (
-      <div className="fc-navigation-item__expand" onClick={this.expandItem}>
-        <i className={iconClass}/>
-      </div>
-    );
-  }
-
-  get indexLink() {
-    return (
-      <div className="fc-navigation-item">
-        <IndexLink to={this.props.to} className="fc-navigation-item__link">
-          <i className={this.props.icon}/>
-          <span>{this.props.title}</span>
-        </IndexLink>
-        {this.props.isExpandable && this.expandButton}
-      </div>
-    );
-  }
-
-  get link() {
-    return (
-      <div className="fc-navigation-item">
-        <Link to={this.props.to} className="fc-navigation-item__link">
-          <i className={this.props.icon}/>
-          <span>{this.props.title}</span>
-        </Link>
-        {this.props.isExpandable && this.expandButton}
-      </div>
-    );
-  }
-
-  get childrenClass() {
-    return classNames('fc-navigation-item__children', {
-      '_open': this.currentState
-    });
-  }
 
   get containerClass() {
     const routeNames = this.props.routes.map(route => route.name);
@@ -109,9 +29,15 @@ export default class NavigationItem extends React.Component {
   render() {
     return (
       <div className={this.containerClass}>
-        { this.props.isIndex ? this.indexLink : this.link }
-        <div className={this.childrenClass}>
-          {this.props.children}
+        <div className="fc-navigation-item">
+          <IndexLink
+            to={this.props.to}
+            className="fc-navigation-item__link"
+            actualClaims={this.props.actualClaims}
+            expectedClaims={this.props.expectedClaims}>
+            <Icon name={this.props.icon} className={styles["nav-item"]} />
+            <span>{this.props.title}</span>
+          </IndexLink>
         </div>
       </div>
     );
