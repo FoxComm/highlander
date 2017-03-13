c9a61ffc6863c6f38c1d3edbc92de6d0d5a01b58
Pavel Kiselev <kpashka@gmail.com>
2017-03-13 19:25:45 +0700

    Sed deployment

--- tabernacle/ansible/roles/cloud/webhook/templates/deploy-bigbag.sh
+++ tabernacle/ansible/roles/cloud/webhook/templates/deploy-bigbag.sh
@@ -9,6 +9,11 @@ make docker
 make docker-push
 
 # Trigger Marathon deployment
+sed "s/NOW/$(date +%Y-%m-%d:%H:%M:%S)/g" storefront.json > latest.json
+
+curl --header "Content-Type: application/json" \
+    -d @latest.json \
+    -XPUT http://{{cloud_demo_server}}/v2/apps/storefront
 
 # Cleanup
 cd ../
