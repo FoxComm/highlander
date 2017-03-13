53c5c4500c57f3341b05b6e5eab43747844aab84
Pavel Kiselev <kpashka@gmail.com>
2017-03-13 20:25:39 +0700

    Latest push

--- tabernacle/ansible/roles/cloud/webhook/templates/deploy-bigbag.sh
+++ tabernacle/ansible/roles/cloud/webhook/templates/deploy-bigbag.sh
@@ -5,9 +5,15 @@ set -ue
 # Clean checkout
 rm -rf bigbag
 
-# Clone and build
+# Clone repository
 git clone https://github.com/kpashka/bigbag.git
 cd bigbag
+
+# Login to Docker Hub
+source ./dockercfg
+docker login --username=$DOCKER_LOGIN --password=$DOCKER_PASSWORD
+
+# Build and push
 make docker
 make docker-push
 
@@ -17,4 +23,3 @@ sed "s/NOW/$(date +%Y-%m-%d:%H:%M:%S)/g" storefront.json > latest.json
 curl --header "Content-Type: application/json" \
     -d @latest.json \
     -XPUT http://{{cloud_demo_server}}/v2/apps/storefront
-
