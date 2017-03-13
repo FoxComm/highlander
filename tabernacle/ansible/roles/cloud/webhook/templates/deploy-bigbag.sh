66907607fb1676d841375adc8afd16c15398e566
Pavel Kiselev <kpashka@gmail.com>
2017-03-13 21:28:44 +0700

    Final fixes

--- tabernacle/ansible/roles/cloud/webhook/templates/deploy-bigbag.sh
+++ tabernacle/ansible/roles/cloud/webhook/templates/deploy-bigbag.sh
@@ -10,7 +10,7 @@ git clone https://github.com/kpashka/bigbag.git
 cd bigbag
 
 # Login to Docker Hub
-source ./dockercfg
+source {{webhook_dir}}/.dockercfg
 docker login --username=$DOCKER_LOGIN --password=$DOCKER_PASSWORD
 
 # Build and push
@@ -18,8 +18,8 @@ make docker
 make docker-push | tee
 
 # Trigger Marathon deployment
-sed "s/NOW/$(date +%Y-%m-%d:%H:%M:%S)/g" storefront.json > latest.json
+sed "s/NOW/$(date +%Y-%m-%d:%H:%M:%S)/g" {{webhook_dir}}/storefront.json > {{webhook_dir}}/latest.json
 
 curl --header "Content-Type: application/json" \
-    -d @latest.json \
+    -d @{{webhook_dir}}/latest.json \
     -XPUT http://{{cloud_demo_server}}/v2/apps/storefront
