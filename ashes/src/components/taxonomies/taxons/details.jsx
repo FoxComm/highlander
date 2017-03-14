cf94db78782b1205c3047ac52a2b7a983a92acba
Bad_Company <ihor.peredystyi@gmail.com>
2017-03-14 21:58:27 +0200

    feat: add TaxonDetails and layout

--- ashes/src/components/taxonomies/taxons/details.jsx
+++ ashes/src/components/taxonomies/taxons/details.jsx
@@ -3,8 +3,26 @@
 // lib
 import React, { Element } from 'react';
 
+// components
+import ObjectDetailsDeux from 'components/object-page/object-details-deux';
+
+const layout = require('./layout.json');
+
 export default class TaxonDetails extends React.Component {
-  render() {
-    return <div>New taxon page</div>;
+  render () {
+    const { schema, taxon, onUpdateObject } = this.props;
+    if (!taxon) {
+      return <div></div>;
+    }
+    return (
+      <ObjectDetailsDeux
+        layout={layout}
+        title="taxon"
+        plural="taxons"
+        object={taxon}
+        schema={schema}
+        onUpdateObject={onUpdateObject}
+      />
+    );
   }
 };
