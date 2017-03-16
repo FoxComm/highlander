774ff233e873a5b5228f0846da1f072e3d45749e
Eugene Sypachev (@Axblade) <eugene.sypachev@gmail.com>
2017-03-16 16:16:16 +0300

    footer clean up

--- demo/peacock/src/components/footer/footer.jsx
+++ demo/peacock/src/components/footer/footer.jsx
@@ -13,39 +13,11 @@ import Copyright from './copyright';
 const Footer = () : HTMLElement => {
   return (
     <section styleName="footer">
-      <header styleName="heading">
-        <h2 styleName="title">JOIN OUR NEWSLETTER</h2>
-        <h3 styleName="subtitle">GET ACCESS TO SPECIAL PROMOTIONS AND OUR NEWEST CREATIONS</h3>
-      </header>
-
       <div styleName="wrap">
-        <SubscriptionForm />
-
-        <ul styleName="links">
-          <li><Link to="/about">ABOUT US</Link></li>
-          <li><Link to="/stores">STORES</Link></li>
-          <li><Link to="/gift-cards">GIFT CARDS</Link></li>
-          <li><Link to="/frequently-asked-questions">FAQ</Link></li>
-          <li><Link to="/contact-us">CONTACT US</Link></li>
-          <li><Link to="/shipping-and-returns">SHIPPING & RETURNS</Link></li>
-        </ul>
-
-        <div styleName="social-links">
-          <Link to="https://www.instagram.com/theperfectgourmet/" target="_blank">
-            <Icon name="fc-instagram" styleName="social-icon"/>
-          </Link>
-          <Link to="https://www.facebook.com/PerfectGourmet/" target="_blank">
-            <Icon name="fc-facebook" styleName="social-icon"/>
-          </Link>
-          <Link to="https://twitter.com/perfectgourmet1" target="_blank">
-            <Icon name="fc-twitter" styleName="social-icon"/>
-          </Link>
-          <Link to="https://www.pinterest.com/perfectgourmet/" target="_blank">
-            <Icon name="fc-pinterest" styleName="social-icon"/>
-          </Link>
+        <div styleName="logo">
+          PURE
         </div>
       </div>
-      <Copyright />
     </section>
   );
 };
