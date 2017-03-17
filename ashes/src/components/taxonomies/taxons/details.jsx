bee6b95f89eaf17689a4984cdcf60179add11b4a
Bad_Company <ihor.peredystyi@gmail.com>
2017-03-17 14:00:19 +0200

    feat: add TaxonListWidget renderer
    also: change TaxonDetails to class, add get renderers()

--- ashes/src/components/taxonomies/taxons/details.jsx
+++ ashes/src/components/taxonomies/taxons/details.jsx
@@ -2,10 +2,43 @@
 
 // lib
 import React, { Element } from 'react';
+import { autobind } from 'core-decorators';
 
 // components
 import ObjectDetailsDeux from 'components/object-page/object-details-deux';
+import TaxonListWidget from './taxon-list-widget';
 
-export default (props: ObjectPageChildProps<Taxon>) => (
-  <ObjectDetailsDeux {...props} />
-);
+import type { Renderers } from 'components/object-page/object-details-deux';
+
+export default class TaxonDetails extends React.Component {
+
+  props: ObjectPageChildProps<Taxon>;
+
+  @autobind
+  renderTaxonListWidget() {
+    const { taxonomyId, context, taxonId } = this.props.params;
+    return(
+      <TaxonListWidget
+        id={taxonomyId}
+        context={context}
+        currentTaxon={taxonId}
+      />
+    );
+  };
+
+  get renderers(): Renderers {
+    return {
+      taxonList: this.renderTaxonListWidget
+    };
+  }
+
+  render() {
+    return (
+      <ObjectDetailsDeux
+        {...this.props}
+        renderers={this.renderers}
+      />
+    );
+  }
+
+}
