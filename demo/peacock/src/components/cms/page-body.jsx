4cfbf48f2bef4eea04baf2f7d09e60b547a4bc9e
Eugene Sypachev (@Axblade) <eugene.sypachev@gmail.com>
2017-03-21 23:10:47 +0300

    fix build

--- demo/peacock/src/components/cms/page-body.jsx
+++ demo/peacock/src/components/cms/page-body.jsx
@@ -4,8 +4,6 @@
 import React from 'react';
 import _ from 'lodash';
 
-import FAQSection from '../faqs/faq-section';
-
 // paragons
 import { fieldTypes } from 'paragons/cms';
 
@@ -59,14 +57,6 @@ const renderBlock = (block) => {
         <a href={`tel:${block.content}`} styleName="link" key={generateKey()}>
           {block.content}
         </a>
-      );
-    case fieldTypes.FAQSECTION:
-      return (
-        <FAQSection
-          title={block.content.title}
-          faqs={block.content.faqs}
-          key={generateKey()}
-        />
       );
     default:
       return block.content;
