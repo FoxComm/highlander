480a75cc165335f38151c6dc5acfd884ec8b9de1
Bagrat Bagoyan <bagratinho@gmail.com>
2017-03-21 13:56:21 +0400

    indentation fixes

--- ashes/src/components/promotions/discounts/currency.jsx
+++ ashes/src/components/promotions/discounts/currency.jsx
@@ -6,8 +6,8 @@ import styles from '../attrs-edit.css';
 import CurrencyInput from '../../forms/currency-input';
 
 type Props = {
-	value: Number,
-	onChange: Function
+  value: Number,
+  onChange: Function
 }; 
 
 const Currency = (props: Props) => {
