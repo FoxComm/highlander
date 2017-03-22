8ca289cadaffd4a6e4117f100d0fc89d16e1ecb3
Eugene Sypachev (@Axblade) <eugene.sypachev@gmail.com>
2017-03-22 21:25:54 +0300

    drawer for menu

--- demo/peacock/src/components/navigation/navigation.jsx
+++ demo/peacock/src/components/navigation/navigation.jsx
@@ -29,14 +29,10 @@ type Props = {
   path: string,
 };
 
-const getState = state => ({...state.categories});
-
-class Navigation extends Component {
-  props: Props;
-
-  componentWillMount() {
-    this.props.fetch();
-  }
+class NavigationItem extends Component {
+  state = {
+    expanded: false,
+  };
 
   @autobind
   getNavUrl(category : ?Category) {
@@ -52,36 +48,80 @@ class Navigation extends Component {
     return url;
   }
 
+  @autobind
+  handleHoverOn() {
+    console.log('on');
+    this.setState({ expanded: true });
+  }
+
+  @autobind
+  handleHoverOff() {
+    console.log('off');
+    this.setState({ expanded: false });
+  }
+
+  render() {
+    const { item, path, t } = this.props;
+
+    if (item.hiddenInNavigation) {
+      return null;
+    }
+
+    const dashedName = item.name.replace(/\s/g, '-');
+    const key = `category-${dashedName}`;
+    const url = this.getNavUrl(item);
+    const isActive = path.match(new RegExp(dashedName, 'i'));
+    const linkClasses = classNames(styles.item, {
+      [styles.active]: isActive,
+    });
+
+    const drawerStyle = classNames(styles.submenu, {
+      [styles.open]: this.state.expanded,
+    });
+
+    return (
+      <div
+        className={linkClasses}
+        key={key}
+        onMouseEnter={this.handleHoverOn}
+        onMouseLeave={this.handleHoverOff}
+      >
+        <Link
+          styleName="item-link"
+          to={url}
+          onClick={this.props.onClick}
+        >
+          {t(item.name.toUpperCase())}
+        </Link>
+        { item.children && <div className={drawerStyle}>Sumbenu { item.name }</div> }
+      </div>
+    );
+  }
+}
+
+const getState = state => ({...state.categories});
+
+class Navigation extends Component {
+  props: Props;
+
+  componentWillMount() {
+    this.props.fetch();
+  }
+
   render(): Element<*> {
     const { t } = this.props;
     const path = decodeURIComponent(this.props.path);
 
     const categoryItems = _.map(this.props.list, (item) => {
-      if (item.hiddenInNavigation) {
-        return null;
-      }
-
-      const dashedName = item.name.replace(/\s/g, '-');
-      const key = `category-${dashedName}`;
-      const url = this.getNavUrl(item);
-      const isActive = path.match(new RegExp(dashedName, 'i'));
-      const linkClasses = classNames(styles.item, {
-        [styles.active]: isActive,
-      });
-
       return (
-        <li className={linkClasses} key={key}>
-          <Link styleName="item-link" to={url} onClick={this.props.onClick}>
-            {t(item.name.toUpperCase())}
-          </Link>
-        </li>
+        <NavigationItem item={item} path={this.props.path} t={this.props.t}/>
       );
     });
 
     return (
-      <ul styleName="list">
+      <div styleName="list">
         {categoryItems}
-      </ul>
+      </div>
     );
   }
 }
