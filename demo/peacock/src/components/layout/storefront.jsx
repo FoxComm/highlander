e7532ec03f07a65ac996056ceaea741835ba4652
Andrey Rublev <version.ru@gmail.com>
2017-03-24 19:16:21 +0700

    get rid of banner

--- demo/peacock/src/components/layout/storefront.jsx
+++ demo/peacock/src/components/layout/storefront.jsx
@@ -1,12 +1,9 @@
 /* @flow */
 
 import React, { Element } from 'react';
-import { connect } from 'react-redux';
-import classNames from 'classnames';
 
 import Header from '../header/header';
 import Footer from '../footer/footer';
-import * as actions from 'modules/banner';
 
 import styles from './storefront.css';
 
@@ -15,21 +12,9 @@ import type { RoutesParams } from 'types';
 type Props = RoutesParams & {
   children: Element<*>,
   location: any,
-  banner: {
-    isVisible: boolean,
-  },
-  closeBanner: Function,
 };
 
-const mapState = state => ({
-  banner: state.banner,
-});
-
 const StoreFront = (props: Props) => {
-  const bodyClass = classNames(styles['content-container'], {
-    [styles['_without-banner']]: !props.banner.isVisible,
-  });
-
   const childrenWithRoutes = React.Children.map(props.children,
     child => React.cloneElement(child, {
       routes: props.routes,
@@ -43,7 +28,7 @@ const StoreFront = (props: Props) => {
         path={props.location.pathname}
         query={props.location.query}
       />
-      <div className={bodyClass}>
+      <div styleName="content-container">
         {childrenWithRoutes}
       </div>
       <Footer />
@@ -51,4 +36,4 @@ const StoreFront = (props: Props) => {
   );
 };
 
-export default connect(mapState, actions)(StoreFront);
+export default StoreFront;
