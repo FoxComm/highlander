2dcf6fa9691e369ac0a1d7a5e8f82acd162c3ff6
Bad_Company <ihor.peredystyi@gmail.com>
2017-03-25 12:27:07 +0200

    chore: remove copy-pasted comment

--- ashes/src/elastic/taxonomy.js
+++ ashes/src/elastic/taxonomy.js
@@ -3,8 +3,6 @@
 import { post } from '../lib/search';
 import * as dsl from './dsl';
 
-// 1000 should be big enough to request all promotions with applyType = coupon
-// without size parameter ES responds with 10 items max
 const MAX_RESULTS = 1000;
 const taxonomiesSearchUrl = `taxonomies_search_view/_search?size=${MAX_RESULTS}`;
 
