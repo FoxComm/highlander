1c8c72b3b15332c8cba1549721823f3ca9544c42
tony.pizzicato <tony.grebnev@gmail.com>
2017-03-26 17:57:19 +0300

    remove taxons list widget from taxon page

--- ashes/src/components/taxonomies/taxons/details.jsx
+++ ashes/src/components/taxonomies/taxons/details.jsx
@@ -1,53 +1,18 @@
 // @flow
 
 // lib
-import get from 'lodash/get';
-import React, { Element, Component } from 'react';
-import { autobind } from 'core-decorators';
+import React, { Element } from 'react';
 
 // components
 import ObjectDetailsDeux from 'components/object-page/object-details-deux';
-import TaxonListWidget from './taxon-list-widget';
 
-import type { Renderers } from 'components/object-page/object-details-deux';
-
-export default class TaxonDetails extends Component {
-  /*
-   * should be
-   * props: ObjectPageChildProps<Taxon> & {
-   *  taxonomy: Taxonomy,
-   * }
-   * but flow does not understand that props are of type ObjectPageChildProps<Taxon> and throw an error in
-   * <ObjectDetailsDeux
-   *   {...this.props}
-   *   renderers={this.renderers}
-   * />
-   */
+export default class TaxonDetails extends React.Component {
   props: ObjectPageChildProps<Taxon>;
-
-  @autobind
-  renderTaxonListWidget() {
-    const params = get(this.props, 'params', {});
-    return (
-      <TaxonListWidget
-        context={params.context}
-        taxonomyId={params.taxonomyId}
-        activeTaxonId={params.taxonId}
-      />
-    );
-  };
-
-  get renderers(): Renderers {
-    return {
-      taxonList: this.renderTaxonListWidget,
-    };
-  }
 
   render() {
     return (
       <ObjectDetailsDeux
         {...this.props}
-        renderers={this.renderers}
       />
     );
   }
