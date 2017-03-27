35aa0975d8798e2af4c8564f09181109f9d12939
Eugene Sypachev (@Axblade) <eugene.sypachev@gmail.com>
2017-03-27 17:00:18 +0300

    escape quotes

--- demo/peacock/src/pages/checkout/order-placed/order-placed.jsx
+++ demo/peacock/src/pages/checkout/order-placed/order-placed.jsx
@@ -85,7 +85,7 @@ class OrderPlaced extends Component {
           <h1 styleName="title">Thank you for your order!</h1>
 
           <div styleName="desc">
-            <p>We've received your order and we'll be packaging it up to ship out soon!</p>
+            <p>We&#39;ve received your order and we&#39;ll be packaging it up to ship out soon!</p>
             <p>Keep your eye out for an email confirmation, which you should receive shortly.</p>
           </div>
           <Button styleName="to-home" onClick={this.toHome}>{t('Continue Shopping')}</Button>
