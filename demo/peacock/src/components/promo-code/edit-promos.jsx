62e28c692f2210dc23bb7a7603e9c1a881cc6c5f
Artem <artem@foxcommerce.com>
2017-03-31 15:00:41 +0300

    Create a cmp for adding promo codes

---
+++ demo/peacock/src/components/promo-code/edit-promos.jsx
@@ -0,0 +1,63 @@
+/* @flow */
+
+// libs
+import React, { Component } from 'react';
+import { autobind } from 'core-decorators';
+import _ from 'lodash';
+import classNames from 'classnames';
+
+// components
+import { TextInput } from 'ui/text-input';
+import { FormField } from 'ui/forms';
+import ErrorAlerts from '@foxcomm/wings/lib/ui/alerts/error-alerts';
+
+// styles
+import styles from './edit-promos.css';
+
+type Props = {
+  className?: string,
+  onChange: Function,
+  placeholder?: string,
+  saveCode: Function,
+};
+
+class EditPromos extends Component {
+  props: Props;
+
+  @autobind
+  changeCode({ target }: Object) {
+    this.props.onChange(target.value);
+  }
+
+  @autobind
+  onKeyPress(e: Object) {
+    if (e.key === 'Enter') {
+      e.preventDefault();
+
+      this.props.saveCode();
+    }
+  }
+
+  render() {
+    const { className } = this.props;
+    const classes = classNames(styles['edit-promos'], {
+      [className]: className,
+    });
+
+    return (
+      <div className={classes}>
+        <FormField styleName="code-field">
+          <TextInput
+            required
+            styleName="code-input"
+            placeholder={this.props.placeholder}
+            onChange={this.changeCode}
+            onKeyPress={this.onKeyPress}
+          />
+        </FormField>
+      </div>
+    );
+  }
+}
+
+export default EditPromos;
