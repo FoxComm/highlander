9caf96b4009ca012ee8a61d9115b6d1c0893fcc5
Alexey Afanasev <avafanasiev@gmail.com>
2017-03-31 13:17:05 +0300

    Bump stripe ver

--- phoenix-scala/phoenix/app/phoenix/models/payment/creditcard/CreditCardCharge.scala
+++ phoenix-scala/phoenix/app/phoenix/models/payment/creditcard/CreditCardCharge.scala
@@ -66,7 +66,7 @@ object CreditCardCharge {
                      chargeId = stripe.getId,
                      state = Auth,
                      currency = currency,
-                     amount = stripe.getAmount)
+                     amount = stripe.getAmount.toInt) // FIXME int type shrink here
 }
 
 class CreditCardCharges(tag: Tag) extends FoxTable[CreditCardCharge](tag, "credit_card_charges") {
