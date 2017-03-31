0f3092b890f5221368bed7132a034a468a337367
Bagrat Bagoyan <bagratinho@gmail.com>
2017-03-31 18:10:49 +0400

    remove max uses from coupons list

--- ashes/src/components/coupons/coupons.jsx
+++ ashes/src/components/coupons/coupons.jsx
@@ -47,8 +47,6 @@ const tableColumns: Array<Object> = [
   {field: 'codes', text: 'Codes'},
   {field: 'createdAt', text: 'Date/Time Created', type: 'datetime'},
   {field: 'totalUsed', text: 'Total Uses'},
-  {field: 'usesPerCode', text: 'Max Uses'},
-  {field: 'usesPerCustomer', text: 'Max Uses per Customer'},
   {field: 'currentCarts', text: 'Current Carts'},
 ];
 
