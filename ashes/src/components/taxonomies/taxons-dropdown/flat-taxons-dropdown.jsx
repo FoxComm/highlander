37524c35ed12026a37e2c415d92cae9174b050f9
Bad_Company <ihor.peredystyi@gmail.com>
2017-03-31 13:57:30 +0300

    feat: add search & highlight

--- ashes/src/components/taxonomies/taxons-dropdown/flat-taxons-dropdown.jsx
+++ ashes/src/components/taxonomies/taxons-dropdown/flat-taxons-dropdown.jsx
@@ -2,9 +2,10 @@
 
 // libs
 import React, { Component } from 'react';
-import Dropdown from 'components/dropdown/dropdown';
+import GenericDropdown from 'components/dropdown/generic-dropdown';
+import { DropdownItem } from 'components/dropdown';
 import { autobind } from 'core-decorators';
-import { get } from 'lodash';
+import { get, compact } from 'lodash';
 
 // style
 import styles from './flat-taxons-dropdown.css';
@@ -20,56 +21,97 @@ type EventTarget = {
   },
 };
 
+
 export default class FlatTaxonsDropdown extends Component {
 
-  props: Props
+  props: Props;
 
   state = {
     value: '',
-    searchList: [],
+    open: false,
   };
 
+  _d: GenericDropdown;
+
+  @autobind
   onInputChange({ target }: EventTarget) {
     this.setState({
       value: target.value
     });
-    this.searchTaxons(target.value);
   }
 
   @autobind
-  searchTaxons(input: string) {
+  onInputClick() {
+    this._d.toggleMenu()
+  }
+
+  @autobind
+  searchTaxons() {
     const { taxonomy: {taxons} } = this.props;
-    let search = input.toLowerCase();
-    let searchList = [];
-    taxons.map((taxon) => {
-      const name = get(taxon, 'node.attributes.name.v').toLowerCase();
-      if (name.includes(search)) {
-        searchList.push(taxon);
+    let search = this.state.value.toLowerCase();
+
+    return taxons.map((taxon) => {
+      const name = get(taxon, 'node.attributes.name.v');
+
+      if (name.toLowerCase().includes(search)) {
+        const highlighted = name.replace(new RegExp(
+          this.state.value, 'ig'),
+          x => `<span class=${styles.needle}>${x}</span>`
+        );
+
+        return (
+          <DropdownItem value={taxon.node.id} key={taxon.node.id}>
+            <span dangerouslySetInnerHTML={{ __html: highlighted }} />
+          </DropdownItem>
+        )
       }
     });
 
-    this.setState({ searchList: searchList });
+  }
+
+  @autobind
+  renderInput(value: any, title: any, props: any, handleToggleClick: (e: MouseEvent) => void): Element<*> {
+    const { taxonomy: { taxons }, taxon } = this.props;
+
+    return (
+      <input
+        value={this.state.value}
+        onChange={(event) => this.onInputChange(event)}
+        onClick={this.onInputClick}
+      />
+    );
+  }
+
+  @autobind
+  searchResults() {
+    const { taxonomy } = this.props;
+
+    if (this.state.value.length == 0) {
+      return taxonomy.taxons.map((item) => (
+        <DropdownItem value={item.node.id} key={item.node.id}>
+          <span>{item.node.attributes.name.v}</span>
+        </DropdownItem>
+      ))
+    }
+
   }
 
   render() {
-    const { taxonomy, onTaxonClick } = this.props;
-
-    const taxonList = this.state.value.length == 0 ? taxonomy.taxons : this.state.searchList;
-    const items = taxonList.map((item) => (
-      item.node.attributes.name.v, [item.node.id, item.node.attributes.name.v, false]
-    ));
+    const { onTaxonClick } = this.props;
+    const { value } = this.state;
+    const renderChild = (value == "") ? this.searchResults() : this.searchTaxons();
 
     return (
-      <div styleName="dropdown" onChange={(event) => this.onInputChange(event)}>
-        <Dropdown
-          name="flat-dropdown"
-          value=""
-          editable
-          placeholder=""
+      <div styleName="dropdown">
+        <GenericDropdown
+          open={this.state.open}
           noControls
-          items={items}
           onChange={onTaxonClick}
-        />
+          renderDropdownInput={this.renderInput}
+          ref={d => this._d = d}
+        >
+          {renderChild}
+        </GenericDropdown>
       </div>
     );
   }
