431ed00a68d74b648d3244f3b3c7b2cd315acce8
Sergey Rublev <sergey@foxcommerce.com>
2017-04-03 17:53:20 +0300

    Fix syntax in psql

--- phoenix-scala/sql/V4.116__plugin_imgix_predefined.sql
+++ phoenix-scala/sql/V4.116__plugin_imgix_predefined.sql
@@ -2,7 +2,7 @@ insert into plugins
     (name, version, description, is_disabled, api_host, api_port, settings, schema_settings, created_at, updated_at, deleted_at)
     values ('imgix', '1.0', 'Imgix CDN integration plugin', false, null, null,
     '{"s3_bucket": "", "s3_prefix": "", "cdn_prefix": ""}'::jsonb,
-    '[{"name": "cdn_prefix", "type": "string", "title": "CDN url prefix", "default": ""},' ||
+                     ('[{"name": "cdn_prefix", "type": "string", "title": "Imgix source url", "default": ""},' ||
     ' {"name": "s3_bucket", "type": "string", "title": "S3 bucket", "default": ""}, ' ||
-    '{"name": "s3_prefix", "type": "string", "title": "S3 prefix", "default": "albums"}]'::jsonb,
+    '{"name": "s3_prefix", "type": "string", "title": "S3 prefix", "default": "albums"}]')::jsonb,
     '2017-03-28 23:04:50.896000', null, null);
