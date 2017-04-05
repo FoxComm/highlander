86b0d53504e252d7c41c1dc4629da5f6c1b4a496
Diokuz <diokuz@gmail.com>
2017-04-05 17:31:30 +0300

    remove some global vars

--- ashes/test/unit/modules/orders/list.js
+++ ashes/test/unit/modules/orders/list.js
@@ -1,6 +1,6 @@
 const ordersSearchTerms = require('../../../fixtures/orders-search-terms.js');
 const ordersSavedSearches = require('../../../fixtures/orders-saved-searches.js');
-const makeLiveSearch = importDefault('modules/live-search/index.js');
+const makeLiveSearch = require('modules/live-search/index.js').default;
 const { reducer, actions } = makeLiveSearch('TEST', ordersSearchTerms, ordersSavedSearches);
 const { selectSavedSearch, submitFilters } = actions;
 
