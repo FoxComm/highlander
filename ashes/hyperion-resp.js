05ebfbd3a623642232c53c9935e32232b913bb98
Diokuz <diokuz@gmail.com>
2017-04-11 12:32:15 +0300

    temp

---
+++ ashes/hyperion-resp.js
@@ -0,0 +1,69 @@
+// Hyperion
+{
+  "result": {
+    "totals": {
+      "total": 4500,
+      "taxes": 0,
+      "subTotal": 4500,
+      "shipping": 0,
+      "adjustments": 0
+    },
+    "shippingState": "---",
+    "shippingMethod": {
+      "price": 0,
+      "name": "FreeEconomy",
+      "isEnabled": true,
+      "id": 0,
+      "code": "Econ Cont US"
+    },
+    "shippingAddress": {
+      "zip": "98121-1044",
+      "region": {
+        "name": "WA",
+        "id": 0,
+        "countryId": 234
+      },
+      "name": "Bree Swineford",
+      "isDefault": false,
+      "id": 0,
+      "city": "SEATTLE",
+      "address2": null,
+      "address1": "3131 ELLIOTT AVE STE 240"
+    },
+    "referenceNumber": "111-5296499-9653858",
+    "placedAt": "2017-03-08T19:38:36Z",
+    "paymentState": "Captured",
+    "paymentMethods": {
+      "type": "CreditCard",
+      "id": 0,
+      "currentBalance": 0,
+      "createdAt": "2017-03-08T19:38:36Z",
+      "availableBalance": 0,
+      "amount": 4500
+    },
+    "orderState": "Shipped",
+    "line_items": {
+      "skus": [
+        {
+          "trackInventory": false,
+          "totalPrice": 4500,
+          "state": "pending",
+          "sku": "FOX-2131",
+          "referenceNumbers": [],
+          "quantity": 1,
+          "productFormId": null,
+          "price": 4500,
+          "name": "---",
+          "imagePath": "https://s3-us-west-1.amazonaws.com/foxcomm-images/albums/1/296/81B6PnROB1L._UX522_.jpg"
+        }
+      ]
+    },
+    "lineItemAdjustments": [],
+    "fraudScore": 0,
+    "customer": {
+      "name": "Zakk Pershing",
+      "email": "67xlr8kb83lx0t2@marketplace.amazon.com"
+    },
+    "channel": "Amazon.com"
+  }
+}™
