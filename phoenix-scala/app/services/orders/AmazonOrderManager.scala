49b52f5654db62aa7de34eb4e1dec9fbe4f0458a
Roman Sotnikov <roman.sotnikov@gmail.com>
2017-04-11 17:30:05 +0700

    Make order creation idempotent

--- phoenix-scala/app/services/orders/AmazonOrderManager.scala
+++ phoenix-scala/app/services/orders/AmazonOrderManager.scala
@@ -4,8 +4,6 @@ import cats.implicits._
 import payloads.AmazonOrderPayloads._
 import models.cord._
 import responses.cord.AmazonOrderResponse
-import responses.cord.AmazonOrderResponse._
-import responses.cord.AmazonOrderResponse.AmazonOrderInfo._
 import utils.aliases._
 import utils.db._
 
@@ -13,8 +11,21 @@ object AmazonOrderManager {
   def createAmazonOrder(payload: CreateAmazonOrderPayload)(
       implicit ec: EC,
       db: DB,
-      au: AU): DbResultT[AmazonOrderResponse.Root] =
-    for {
-      amazonOrder ← * <~ AmazonOrders.create(AmazonOrder.build(payload))
-    } yield AmazonOrderResponse.build(amazonOrder)
+      au: AU): DbResultT[AmazonOrderResponse.Root] = {
+
+    val amazonOrderT = for {
+      result ← * <~ AmazonOrders
+                .findOneByAmazonOrderId(payload.amazonOrderId)
+                .findOrCreateExtended(AmazonOrders.create(AmazonOrder.build(payload)))
+      (existingOrder, foundOrCreated) = result
+    } yield result
+
+    amazonOrderT.map {
+      case (existingOrder, foundOrCreated) ⇒
+        AmazonOrderResponse.build(AmazonOrder.fromExistingAmazonOrder(existingOrder))
+    }
+  }
+//    for {
+//      amazonOrder ← * <~ AmazonOrders.create(AmazonOrder.build(payload))
+//    } yield AmazonOrderResponse.build(amazonOrder)
 }
