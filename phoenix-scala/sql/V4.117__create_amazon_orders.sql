da303c7201d9ee9f158c90cffbbde27aa3633218
Roman Sotnikov <roman.sotnikov@gmail.com>
2017-04-12 17:04:00 +0700

    Add amazon_order_search_view and stuff

--- phoenix-scala/sql/V4.117__create_amazon_orders.sql
+++ phoenix-scala/sql/V4.117__create_amazon_orders.sql
@@ -6,7 +6,20 @@ create table amazon_orders(
     order_type generic_string,
     currency currency,
     order_status generic_string,
-    purchased_date timestamp,
-    updated_at timestamp,
-    created_at timestamp
-)
+    purchase_date generic_timestamp,
+    updated_at generic_timestamp,
+    created_at generic_timestamp
+);
+
+create table amazon_orders_search_view (
+  id bigint primary key,
+    amazon_order_id generic_string,
+    order_total integer not null default 0,
+    payment_method_detail generic_string,
+    order_type generic_string,
+    currency currency,
+    order_status generic_string,
+    purchase_date json_timestamp,
+    updated_at json_timestamp,
+    created_at json_timestamp
+);
