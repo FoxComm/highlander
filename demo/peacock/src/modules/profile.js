e0d403d0c0415af0714fdd231908807f86eb875d
Artem <artem@foxcommerce.com>
2017-04-14 14:05:57 +0300

    Add email block

--- demo/peacock/src/modules/profile.js
+++ demo/peacock/src/modules/profile.js
@@ -6,6 +6,7 @@ import _ from 'lodash';
 import { updateUser } from './auth';
 
 export const toggleNameModal = createAction('TOGGLE_NAME_MODAL');
+export const toggleEmailModal = createAction('TOGGLE_EMAIL_MODAL');
 
 const _fetchAccount = createAsyncActions(
   'fetchAccount',
@@ -38,6 +39,7 @@ export const changePassword = _changePassword.perform;
 const initialState = {
   account: {},
   nameModalVisible: false,
+  emailModalVisible: false,
 };
 
 function updateAccountInState(state, account) {
@@ -55,6 +57,13 @@ const reducer = createReducer({
     return {
       ...state,
       nameModalVisible: !current,
+    };
+  },
+  [toggleEmailModal]: (state) => {
+    const current = _.get(state, 'emailModalVisible', false);
+    return {
+      ...state,
+      emailModalVisible: !current,
     };
   },
 }, initialState);
