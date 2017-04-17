f02d1317b83dcb4181a164a7d7c94d99feebd15b
Jeff Mataya <jeff@foxcommerce.com>
2017-04-17 11:01:38 -0700

    Main nav for the women's category page

--- demo/peacock/src/pages/category/women.jsx
+++ demo/peacock/src/pages/category/women.jsx
@@ -2,7 +2,10 @@
 
 import React from 'react';
 import Button from 'ui/buttons';
+import { Link } from 'react-router';
 import LocalNav from 'components/local-nav/local-nav';
+
+import styles from './women.css';
 
 type Props = {};
 
@@ -17,6 +20,57 @@ const WomensCatPage = (props: Props) => {
   return (
     <div>
       <LocalNav categoryName="Women" links={navItems} />
+      <div styleName="wrap">
+        <div styleName="header-wrap">
+          <div styleName="header-content">
+            <div styleName="header-title">
+              Karlie Is Never Done
+            </div>
+            <div styleName="header-body">
+              <p>
+                The latest Training Collection is all about breaking boundaries.
+              </p>
+              <div styleName="header-button">
+                <Button>
+                  Shop now
+                </Button>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div styleName="featured">
+          <div styleName="featured-card">
+            <div styleName="climachill">
+              <div>Climachill</div>
+              <div>
+                <Button>
+                  Shop now
+                </Button>
+              </div>
+            </div>
+          </div>
+          <div styleName="featured-card">
+            <div styleName="champ">
+              <div>Athletics X Reigning Champ</div>
+              <div>
+                <Button>
+                  Shop now
+                </Button>
+              </div>
+            </div>
+          </div>
+          <div styleName="featured-card">
+            <div styleName="originals">
+              <div>Originals: Ocean Elements</div>
+              <div>
+                <Button>
+                  Shop now
+                </Button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
