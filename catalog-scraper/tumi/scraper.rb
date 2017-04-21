1848de29dd49b26c85b5ef27f7c68ff9c7864d0f
Jeff Mataya <jeff.mataya@gmail.com>
2017-04-21 19:28:44 -0400

    Get sizes and tie SKUs together to be products

--- catalog-scraper/tumi/scraper.rb
+++ catalog-scraper/tumi/scraper.rb
@@ -3,11 +3,10 @@ require "open-uri"
 require "json"
 
 def sanitize_content(str)
-  str.tr("\n", "").tr("\t", "").gsub("\r", "")
+  str.tr("\n", "").tr("\t", "").gsub("\r", "").strip.gsub(/[\u0080-\u00ff]/, "")
 end
 
 def collect_pdps()
-
   files = ['luggage.html', 'backpacks.html', 'accessories.html', 'bags.html']
   pdp_links = []
   files.each do |file|
@@ -17,17 +16,7 @@ def collect_pdps()
     pdp_links = pdp_links + links
   end
 
-  pdp_links = pdp_links.sort { |x, y| x <=> y }.uniq
-
-=begin
-  output = File.open('pdp_links.txt', 'w')
-  pdp_links.each do |link|
-    output << link << "\n"
-  end
-
-  output.close
-=end
-  pdp_links
+  pdp_links.sort { |x, y| x <=> y }.uniq
 end
 
 def read_images(doc)
@@ -53,34 +42,41 @@ def parse_color_option(li_content)
   {url:url, name:name, img:img_src, variant_name:title}
 end
 
-def read_pdp_colors(doc)
-  options = []
-  doc.css("ul.choose-colors").css("li").each do |li|
+def read_current_pdp_color(doc)
+  doc.css("ul.choose-colors li").each do |li|
     current = (!li.attribute('class').nil? && (li['class'].include? 'select'))
     parsed = parse_color_option(li)
     if current
-      options = options.insert(0, parsed)
-    else
-      options = options.insert(-1, parsed)
+      return parsed
     end
   end
-  options
+
+  nil
 end
 
-# Start by grabbing all of the products.
-pdp_links = collect_pdps
+def read_current_pdp_size(doc, url)
+  doc.css("select.select-size option").each do |size|
+    value = size.attribute('value').to_s
+    if value == url
+      variant_name = sanitize_content(size.content.split(",")[0])[4..-1]
+      return {
+        url: value,
+        name: value,
+        variant_name: variant_name
+      }
+    end
+  end
 
-# What Tumi calls a product, we call a SKU
-skus = pdp_links.map do |link|
-  sku = {
-    albums: [],
-    attributes: {},
-    taxons: {}
-  }
+  return nil
+end
 
-  url = "https://www.tumi.com#{link}"
-  doc = Nokogiri::HTML(open(url, "Cookie"=>'tumi-newTest=geoNo;tumi-geo=no'))
+def read_innovation_features(doc)
+  doc.css(".innovation-details .innovation-slider-clc").map do |innov|
+    title = sanitize_content(innov.css('h3').text)
+  end
+end
 
+def update_sku_from_scopes(doc, sku)
   script_tags = doc.css("script")
 
   # Get the lpvars contents
@@ -105,22 +101,30 @@ skus = pdp_links.map do |link|
         }
       }
     elsif s[0] == "ProductCategories"
-      sku[:taxons][:productType] = {
-        name: s[1]
-      }
+      sku[:taxons][:productType] = [ s[1] ]
     end
   end
+
+  sku
+end
+
+def update_sku_from_data_properties(doc, sku)
+  script_tags = doc.css("script")
 
   data_scripts = script_tags.select do |st|
     st.text.include? "dataLayer = [{"
   end
 
-  data_properties = data_scripts[0].text.scan(/'([a-zA-Z0-9_]+)':( )?('(.)*'|\[\]|[0-9\.\-]+|true|false)/)
-  data_properties.each do |d|
-    if d[0] == "productSubCat"
-      sku[:taxons][:category] = {
-        name: d[2].gsub("'", "")
+  props = data_scripts[0].text.scan(/'([a-zA-Z0-9_]+)':( )?('(.)*'|\[\]|[0-9\.\-]+|true|false)/)
+
+  props.each do |d|
+    if d[0] == "productName"
+      sku[:attributes][:title] = {
+        t: "string",
+        v: d[2].gsub("'", "")
       }
+    elsif d[0] == "productSubCat"
+      sku[:taxons][:category] = [ d[2].gsub("'", "") ]
     elsif d[0] == "productDescription"
       sku[:attributes][:description] = {
         t: "richtext",
@@ -158,18 +162,21 @@ skus = pdp_links.map do |link|
         }
       end
     elsif d[0] == "productCollection"
-      sku[:taxons][:collection] = {
-        name: d[2].gsub("'", "")
-      }
+      sku[:taxons][:collection] = [ d[2].gsub("'", "") ]
     end
   end
 
+  sku
+end
+
+def update_sku_from_dimensions(doc, sku)
   dimensions = doc.css("ul.full-row .dimen-title").select do |d|
     !d.attribute('class').to_s.include? "hidden"
   end
 
   dimensions.each do |d|
     dimension_title = d.css(".title-name").first.text
+
     if dimension_title == "Laptop Dimensions"
       measures = doc.css('.prod-measurements .dimen-prop .dimen-attr span')
       measurements = {}
@@ -195,23 +202,134 @@ skus = pdp_links.map do |link|
       end
     else
       value_tag = d.parent.css('> span').first
+      value_text = sanitize_content(value_tag.text)
+
       unless value_tag == nil || value_tag.text == ""
-        sku[:attributes][dimension_title] = {
-          t: "string",
-          v: sanitize_content(value_tag.text)
-        }
+        if dimension_title == "Primary Material"
+          sku[:taxons][:material] = [ sanitize_content(value_text) ]
+        elsif dimension_title == "Weight"
+          sku[:attributes][:weight] = {
+            t: "string",
+            v: value_text
+          }
+        else
+          sku[:attributes][dimension_title] = {
+            t: "string",
+            v: value_text
+          }
+        end
       end
     end
   end
 
+  sku
+end
+
+
+# Start by grabbing all of the products.
+pdp_links = collect_pdps
+
+# What Tumi calls a product, we call a SKU
+products = {}
+skus = pdp_links.map.with_index do |link, idx|
+  puts "Processing PDP #{idx + 1} of #{pdp_links.count}"
+  sku = {
+    albums: [],
+    attributes: {},
+    taxons: {}
+  }
+
+  url = "https://www.tumi.com#{link}"
+  doc = Nokogiri::HTML(open(url, "Cookie"=>'tumi-newTest=geoNo;tumi-geo=no'))
+
+  sku = update_sku_from_scopes(doc, sku)
+  sku = update_sku_from_data_properties(doc, sku)
+  sku = update_sku_from_dimensions(doc, sku)
+
+
   sku[:albums]=[{'name':'default'}]
   sku[:albums][0][:images] = read_images(doc)
+  sku[:taxons][:features] = read_innovation_features(doc)
 
-  colors = read_pdp_colors(doc)
+  color = read_current_pdp_color(doc)
 
-  puts "Object #{sku}"
-  puts "Colors: #{colors}"
+  # Get the ID that ties everything together.
+  sku_code = sku[:attributes][:code][:v]
+  product_id = /^[0-9]+/.match(sku_code).to_s
+  product = products[product_id]
+
+  if product == nil
+    product = { 
+      albums: sku[:albums], 
+      attributes: {
+        title: {
+          t: "string",
+          v: sku[:attributes][:title][:v]
+        },
+        description: {
+          t: "string",
+          v: sku[:attributes][:description][:v]
+        }
+      },
+      skus: [sku],
+      variants: {
+        color: {},
+        size: {}
+      },
+      taxons: sku[:taxons]
+    }
+  else
+    product[:skus] = product[:skus] << sku
+    product[:albums] = product[:albums] << sku[:albums][0]
+
+    sku[:taxons].each do |taxon_name, taxon_value|
+      taxon_list = product[:taxons][taxon_name]
+      taxon_list = [] if taxon_list == nil
+
+      taxon_list = (taxon_list << taxon_value).uniq
+      product[:taxons][taxon_name] = taxon_list
+    end
+  end
+
+  unless color == nil
+    variant_name = color[:variant_name]
+    variant = product[:variants][:color][variant_name]
+
+    if variant == nil
+      product[:variants][:color][variant_name] = color
+      product[:variants][:color][variant_name][:sku_codes] = []
+    end
+
+    sku_codes = product[:variants][:color][variant_name][:sku_codes] << sku_code
+    product[:variants][:color][variant_name][:sku_codes] = sku_codes
+  end
+
+  size = read_current_pdp_size(doc, link)
+
+  unless size == nil
+    variant_name = size[:variant_name]
+    variant = product[:variants][:size][variant_name]
+
+    if variant == nil
+      product[:variants][:size][variant_name] = size
+      product[:variants][:size][variant_name][:sku_codes] = []
+    end
+
+    sku_codes = product[:variants][:size][variant_name][:sku_codes] << sku_code
+    product[:variants][:size][variant_name][:sku_codes] = sku_codes
+  end
+
+  products[product_id] = product
 end
+
+puts "Writing to json"
+
+File.open('products_tumi.json', 'w') do |f|
+  f.puts products.to_json
+end
+
+puts "Complete!"
+
 # TODO: This gets the color variants
 # color_tags = doc.css("ul.choose-colors a")
 #
