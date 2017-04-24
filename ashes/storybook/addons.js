9247cc532834df36f09daeffc4a43c7fbd92a142
tony.pizzicato <tony.grebnev@gmail.com>
2017-04-24 17:21:46 +0300

    get rid of storybook

--- ashes/storybook/addons.js
+++
@@ -1,3 +0,0 @@
-import '@kadira/storybook/addons';
-import '@kadira/storybook-addon-options/register';
-import '@kadira/storybook-addon-notes/register';
--- ashes/styleguide/webpack.styleguide.js
+++ ashes/styleguide/webpack.styleguide.js
@@ -5,7 +5,7 @@ module.exports = {
   resolve: {
     modules: [
       path.resolve('./src'),
-      path.resolve('./node_modules')
+      path.resolve('./node_modules'),
     ],
   },
   module: {
