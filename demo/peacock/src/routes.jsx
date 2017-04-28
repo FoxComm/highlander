7aac4e650db2065f948e41750910b74c8527833c
Eugene Glova <glova.eugene@foxcommerce.com>
2017-04-28 11:02:05 +0300

    Copy kids page from men

--- demo/peacock/src/routes.jsx
+++ demo/peacock/src/routes.jsx
@@ -17,6 +17,7 @@ import AddressForm from './components/profile/blocks/address-form';
 
 import MensCatPage from './pages/category/men';
 import WomensCatPage from './pages/category/women';
+import KidsCatPage from './pages/category/kids';
 
 import HomePage from './pages/home/home-page';
 
@@ -53,6 +54,7 @@ export default function makeRoutes(getStore) {
         </Route>
         <Route path="men" component={MensCatPage} />
         <Route path="women" component={WomensCatPage} />
+        <Route path="kids" component={KidsCatPage} />
         <Route path="/checkout/done" component={OrderPlaced} />
         <Route name="product" path="/products/:productSlug" component={Pdp} />
         <Route name="gift-cards" path="/gift-cards" component={Pdp} />
