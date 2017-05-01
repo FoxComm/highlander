960b5f208351378d52cd14ded149b0139a57a813
Artem <artem@foxcommerce.com>
2017-05-01 21:39:22 +0300

    Add the modal for exporting the selected orders

---
+++ ashes/src/components/bulk-actions/modal/bulk-export-modal.jsx
@@ -0,0 +1,93 @@
+/* @flow */
+
+import React, { Component } from 'react';
+// libs
+import { numberize } from 'lib/text-utils';
+import { autobind } from 'core-decorators';
+import _ from 'lodash';
+
+// components
+import modal from 'components/modal/wrapper';
+import ContentBox from 'components/content-box/content-box';
+import SaveCancel from 'components/core/save-cancel';
+import TextInput from 'components/forms/text-input';
+
+type Props = {
+  entity: string,
+  onCancel: () => void,
+  count: number,
+  onConfirm: Function, // signature here
+};
+
+type State = {
+  value: string,
+};
+class BulkExportModal extends Component {
+  props: Props;
+
+  state: State = {
+    value: '',
+  };
+
+  @autobind
+  onChange(value) {
+    this.setState({ value });
+  }
+
+  @autobind
+  handleSave() {
+    const description = !_.isEmpty(this.state.value) ? this.state.value : null;
+    this.props.onConfirm(description);
+  }
+
+  get footer() {
+    const { onCancel } = this.props;
+    return (
+      <SaveCancel
+        className="fc-modal-footer fc-bulk-export-modal-footer"
+        cancelTabIndex="2"
+        cancelText="Cancel"
+        onCancel={onCancel}
+        saveTabIndex="1"
+        onSave={this.handleSave}
+        saveText="Yes, export"
+      />
+    );
+  }
+
+  get actionBlock() {
+    return (
+      <i onClick={this.props.onCancel} className="fc-btn-close icon-close" title="Close" />
+    );
+  }
+
+  get label() {
+    const { entity, count } = this.props;
+    const entityForm = numberize(entity, count);
+
+    return (
+      <span>Are you sure you want to export <b>{count} {entityForm}</b>?</span>
+    );
+  }
+
+  render() {
+    return (
+      <ContentBox
+        title="Export Selected Orders"
+        actionBlock={this.actionBlock}
+        footer={this.footer}
+        className="fc-bulk-action-modal"
+      >
+        <div className="fc-modal-body">{this.label}</div>
+        <TextInput
+          onChange={this.onChange}
+          placeholder="Short description (optional)"
+          value={this.state.value}
+          className="fc-bulk-export-modal-description"
+        />
+      </ContentBox>
+    );
+  }
+}
+
+export default modal(BulkExportModal);
