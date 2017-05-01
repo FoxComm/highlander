0cd04f870fcbed5a3208a024373badfa2fea8a94
Vladimir Korbut <v.korbut@svitla.com>
2017-05-01 07:27:09 +0300

    Update product_search_view.taxonomies on removing of product-taxon link.

--- phoenix-scala/sql/R__products_search_view_triggers.sql
+++ phoenix-scala/sql/R__products_search_view_triggers.sql
@@ -1,43 +1,50 @@
 create or replace function update_products_search_view_taxonomies_fn() returns trigger as $$
 declare product_ids bigint [];
 begin
-  case tg_table_name
-    when 'product_taxon_links'
+  if tg_op = 'DELETE'
     then
-      product_ids := array_agg(new.left_id);
-    when 'taxonomy_taxon_links'
-    then
-      product_ids := (select array_agg(left_id)
-                      from product_taxon_links
-                      where product_taxon_links.right_id = new.taxon_id);
-  end case;
+      product_ids := array_agg(old.left_id);
+  else
+    case tg_table_name
+      when 'product_taxon_links'
+      then
+        product_ids := array_agg(new.left_id);
+      when 'taxonomy_taxon_links'
+      then
+        product_ids := (select array_agg(left_id)
+                        from product_taxon_links
+                        where product_taxon_links.right_id = new.taxon_id);
+    end case;
+  end if;
 
   update products_search_view
   set
     taxonomies = productTaxonomies.taxonomies
   from (select
-          product_id,
-          case when count(taxonomy_taxons) = 0
-            then
-              '[]' :: jsonb
-          else
-            jsonb_agg(taxonomy_taxons)
-          end as taxonomies
-        from
+          psv.id                                as product_id,
+          coalesce(t.taxonomies, '[]' :: jsonb) as taxonomies
+        from products_search_view as psv
+          left join
           (select
              product_id,
-             json_build_object('taxonomy', taxonomy, 'taxons', taxons) as taxonomy_taxons
+             case when count(taxons) = 0
+               then
+                 '[]' :: jsonb
+             else
+               jsonb_agg(taxons)
+             end as taxonomies
            from
              (select
-                product_taxon_links.left_id                                    as product_id,
-                full_path [1]                                                  as taxonomy,
-                json_agg(full_path [2 :array_length(full_path, 1)] :: text []) as taxons
-
+                product_taxon_links.left_id as product_id,
+                full_path [1]               as taxonomy,
+                jsonb_build_object('taxonomy', full_path [1],
+                                   'taxons', json_agg(full_path [2 :array_length(full_path, 1)] :: text[]))
+                  as taxons
               from
                 taxonomy_taxon_links
                 inner join product_taxon_links on taxonomy_taxon_links.taxon_id = product_taxon_links.right_id
-              group by product_taxon_links.left_id, taxonomy) as x) as y
-        group by product_id) as productTaxonomies
+              group by product_taxon_links.left_id, taxonomy) as y
+           group by y.product_id) as t on t.product_id = psv.id) as productTaxonomies
   where productTaxonomies.product_id = products_search_view.id and products_search_view.id = any (product_ids);
 
   return null;
@@ -46,7 +53,7 @@ $$ language plpgsql;
 
 drop trigger if exists update_products_search_view_taxonomies on product_taxon_links;
 create trigger update_products_search_view_taxonomies
-after insert or update on product_taxon_links
+after insert or update or delete on product_taxon_links
   for each row
   execute procedure update_products_search_view_taxonomies_fn();
 
