cf9bfc3f20e7eb8792cdf2d7c2e28e4cb47c9f4b
Diokuz <diokuz@gmail.com>
2017-05-10 21:05:15 +0300

    add production config

--- ashes/webpack/dev.js
+++ ashes/webpack/dev.js
@@ -1,22 +1,13 @@
 const path = require('path');
 const webpack = require('webpack');
-const merge = require('webpack-merge');
+// const WriteFilePlugin = require('write-file-webpack-plugin');
+// const failPlugin = require('webpack-fail-plugin');
+// const StatsPlugin = require('stats-webpack-plugin');
+// const StatsWriterPlugin = require('webpack-stats-plugin').StatsWriterPlugin;
 
-const isProduction = process.env.NODE_ENV === 'production';
+// const SvgStore = require('webpack-svgstore-plugin');
 
-const devConfig = require('./webpack/dev');
-const prodConfig = require('./webpack/prod');
-
-const cssLoader = {
-  loader: 'css-loader',
-  query: {
-    modules: true,
-    importLoaders: 1,
-    localIdentName: '[name]__[local]'
-  }
-};
-
-const baseConfig = {
+module.exports = {
   entry: [ 'babel-polyfill', './src/client.js' ],
 
   output: {
@@ -74,7 +65,3 @@ const baseConfig = {
     extensions: ['.js', '.jsx']
   },
 };
-
-module.exports = isProduction
-  ? merge(baseConfig, prodConfig)
-  : merge(baseConfig, devConfig);
