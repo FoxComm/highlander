93413f1199babff5b4fdb13554a40b429ba1c21c
Artem <artem@foxcommerce.com>
2017-05-12 00:07:59 +0300

    Enable the use of export modal universally

--- ashes/src/modules/bulk-export/helpers.js
+++ ashes/src/modules/bulk-export/helpers.js
@@ -84,6 +84,8 @@ export const renderExportModal = (
 ): Element<*> => {
   const fields = columnsToPayload(tableColumns);
   const identifier = _.map(tableColumns, item => item.text).toString();
+  const exportByIds = (description) => performExport(toggledIds, description, fields, entity, identifier);
+  const exportByQuery = (description) => performExport(fields, entity, identifier, description);
 
   const exportByIds = (description) => performExport(toggledIds, description, fields, entity, identifier);
   const exportByQuery = (description) => performExport(fields, entity, identifier, description);
