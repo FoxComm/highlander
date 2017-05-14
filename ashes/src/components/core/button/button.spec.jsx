f81840c46d58bd7705817552bdff02745ebc932e
tony.pizzicato <tony.grebnev@gmail.com>
2017-05-14 13:47:28 +0300

    update button tests with sinon

--- ashes/src/components/core/button/button.spec.jsx
+++ ashes/src/components/core/button/button.spec.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import sinon from 'sinon';
+import { shallow, mount } from 'enzyme';
 
 import * as Buttons from './button';
 
@@ -22,17 +23,15 @@ describe('Buttons', function () {
     expect(button.find('i').hasClass('icon-fake')).to.be.true;
   });
 
-  it('should set onClick handler to Button', function () {
-    let counter = 0;
-
-    const onClick = () => counter += 1;
+  it('should handle click', function () {
+    const onClick = sinon.spy();
 
     const button = shallow(
       <Buttons.Button onClick={onClick}>Done</Buttons.Button>
     );
 
     button.simulate('click');
-    expect(counter).to.equal(1);
+    expect(onClick.called).to.be.true;
   });
 
   it('should render className in PrimaryButton', function () {
@@ -45,79 +44,101 @@ describe('Buttons', function () {
     expect(button.hasClass('super-button')).to.be.true;
   });
 
+  it('should not handle click when disabled', function () {
+    const onClick = sinon.spy();
+
+    const button = mount(
+      <Buttons.Button onClick={onClick} disabled>Done</Buttons.Button>
+    );
+
+    button.simulate('click');
+    expect(onClick.called).to.be.false;
+  });
+
+  it('should not handle click when loading', function () {
+    const onClick = sinon.spy();
+
+    const button = mount(
+      <Buttons.Button onClick={onClick} isLoading>Done</Buttons.Button>
+    );
+
+    button.simulate('click');
+    expect(onClick.called).to.be.false;
+  });
+
   it('should render icon in LeftButton', function () {
-    const button = shallow(
+    const button = mount(
       <Buttons.LeftButton />
     );
 
-    expect(button.dive().find('i').hasClass('icon-chevron-left')).to.be.true;
+    expect(button.find('i').hasClass('icon-chevron-left')).to.be.true;
   });
 
   it('should render icon in RightButton', function () {
-    const button = shallow(
+    const button = mount(
       <Buttons.RightButton />
     );
 
-    expect(button.dive().find('i').hasClass('icon-chevron-right')).to.be.true;
+    expect(button.find('i').hasClass('icon-chevron-right')).to.be.true;
   });
 
   it('should render icon in AddButton', function () {
-    const button = shallow(
+    const button = mount(
       <Buttons.AddButton />
     );
 
-    expect(button.dive().find('i').hasClass('icon-add')).to.be.true;
+    expect(button.find('i').hasClass('icon-add')).to.be.true;
   });
 
   it('should render icon in EditButton', function () {
-    const button = shallow(
+    const button = mount(
       <Buttons.EditButton />
     );
 
-    expect(button.dive().find('i').hasClass('icon-edit')).to.be.true;
+    expect(button.find('i').hasClass('icon-edit')).to.be.true;
   });
 
   it('should render icon in DeleteButton', function () {
-    const button = shallow(
+    const button = mount(
       <Buttons.DeleteButton />
     );
 
     expect(button.hasClass('delete')).to.be.true;
-    expect(button.dive().find('i').hasClass('icon-trash')).to.be.true;
+    expect(button.find('i').hasClass('icon-trash')).to.be.true;
   });
 
   it('should render icon in CloseButton', function () {
-    const button = shallow(
+    const button = mount(
       <Buttons.CloseButton />
     );
 
     expect(button.hasClass('close')).to.be.true;
-    expect(button.dive().find('i').hasClass('icon-close')).to.be.true;
+    expect(button.find('i').hasClass('icon-close')).to.be.true;
   });
 
   it('should render icon in IncrementButton', function () {
-    const button = shallow(
+    const button = mount(
       <Buttons.IncrementButton />
     );
 
-    expect(button.dive().find('i').hasClass('icon-chevron-up')).to.be.true;
+    expect(button.find('i').hasClass('icon-chevron-up')).to.be.true;
   });
 
   it('should render icon in DecrementButton', function () {
-    const button = shallow(
+    const button = mount(
       <Buttons.DecrementButton />
     );
 
-    expect(button.dive().find('i').hasClass('icon-chevron-down')).to.be.true;
+    expect(button.find('i').hasClass('icon-chevron-down')).to.be.true;
   });
 
   it('should render icon in SocialButton with google icon', function () {
-    const button = shallow(
+    const button = mount(
       <Buttons.SocialButton type="google" />
     );
 
     expect(button.hasClass('socialButton')).to.be.true;
     expect(button.hasClass('google')).to.be.true;
-    expect(button.dive().find('i').hasClass('icon-google')).to.be.true;
+    expect(button.find('i').hasClass('icon-google')).to.be.true;
   });
 });
