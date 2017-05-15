2c22cccef6c5060528355b6b0ae4dba86b5441ed
Eugene Sypachev (@Axblade) <eugene.sypachev@gmail.com>
2017-05-15 23:26:38 +0300

    some webpack

---
+++ nexus/webpack.config.js
@@ -0,0 +1,30 @@
+'use strict';
+
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: [path.join(__dirname, 'src/index.js')],
+  output: {
+    path: path.join(__dirname, './public/'),
+    filename: 'app.js',
+    publicPath: '/',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'src/templates/index.tpl.html',
+      inject: 'body',
+      filename: 'index.html',
+    }),
+  ],
+  module: {
+    loaders: [
+      {
+        test: /\.(js|jsx)?$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+      },
+    ],
+  },
+};
