850f87e7b1255f6c089e19612cfec4d801fa181e
Pavel Kiselev <kpashka@gmail.com>
2017-05-15 16:02:27 -0700

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,9 @@
 # fail on unexported vars
 set -ue
 
-for fn in `./projects.sh`; do
-    echo "Deploying application $fn"
-    # cat $fn
+cd tabernacle
+
+for app in `./projects.sh`; do
+    echo "Deploying application $app"
+    ansible-playbook -v -i inventory/static/dev ansible/goldrush_update_app.yml --extra-vars "app_name=$app branch_name=$DEPLOY_TAG_NAME auto_build=n instance_ip=$DEPLOY_INSTANCE_IP"
 done
