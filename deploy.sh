457078c12589c40d04a7b17938233888fd1fe630
Pavel Kiselev <kpashka@gmail.com>
2017-05-15 16:05:29 -0700

    Test

--- deploy.sh
+++ deploy.sh
@@ -3,9 +3,9 @@
 # fail on unexported vars
 set -ue
 
-cd tabernacle
-
 for app in `./projects.sh`; do
+    cd $BUILDKITE_BUILD_CHECKOUT_PATH
     echo "Deploying application $app"
+    cd tabernacle
     ansible-playbook -v -i inventory/static/dev ansible/goldrush_update_app.yml --extra-vars "app_name=$app branch_name=$DEPLOY_TAG_NAME auto_build=n instance_ip=$DEPLOY_INSTANCE_IP"
 done
