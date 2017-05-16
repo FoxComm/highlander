0bf6b2961aae58cf12b7f4ff7873d14960a38913
Eugene Sypachev (@Axblade) <eugene.sypachev@gmail.com>
2017-05-16 19:32:15 +0300

    eslint and prettier conflicts

--- nexus/src/store.js
+++ nexus/src/store.js
@@ -4,9 +4,7 @@ import _ from 'lodash';
 import { createStore, applyMiddleware as clientApplyMiddleware } from 'redux';
 import { createLogger } from 'redux-logger';
 import rootReducer from './modules/index';
-import {
-  routerMiddleware,
-} from 'react-router-redux';
+import { routerMiddleware } from 'react-router-redux';
 
 export default function makeStore(
   history: Object,
@@ -21,20 +19,19 @@ export default function makeStore(
 
   const middleware = routerMiddleware(history);
 
-  const store = createStore(rootReducer,
-                            initialState,
-                            _.flow(applyMiddleware(middleware), applyMiddleware(logger)));
+  const store = createStore(
+    rootReducer,
+    initialState,
+    _.flow(applyMiddleware(middleware), applyMiddleware(logger))
+  ); // eslint-disable-line spaced-comment
 
-  /*::`*/ // eslint-disable-line spaced-comment
-  if (module.onReload) {
+  /* ::`*/ if (module.onReload) {
     module.onReload(() => {
       const nextReducer = require('./modules');
       store.replaceReducer(nextReducer.default || nextReducer);
 
       return true;
     });
-  }
-  /*::`;*/ // eslint-disable-line spaced-comment
-
-  return store;
+  } // eslint-disable-line spaced-comment
+  /* ::`;*/ return store;
 }
