0b10e6a455fa4fa79f0a341c7514bb70010f1a71
Pavel Kiselev <kpashka@gmail.com>
2017-05-15 18:10:01 -0700

    Add a bit of description to deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,12 @@
 # fail on unexported vars
 set -ue
 
+# Redeploys changed projects in feature branches
+# Required variables (set in CI):
+# * BUILDKITE_BUILD_CHECKOUT_PATH - checkout path
+# * DEPLOY_TAG_NAME - Docker container tags
+# * DEPLOY_INSTANCE_IP - an instance with Marathon that will be used to swap containers
+
 for app in `./projects.sh`; do
     cd $BUILDKITE_BUILD_CHECKOUT_PATH
     echo "Deploying application $app"
