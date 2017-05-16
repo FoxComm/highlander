7fa02f9bd589b5c977218d43a53edde73591ef44
Eugene Sypachev (@Axblade) <eugene.sypachev@gmail.com>
2017-05-16 16:28:26 +0300

    react rendering

--- nexus/src/index.js
+++ nexus/src/index.js
@@ -1,1 +1,16 @@
-console.log('Nexus');
+import _ from 'lodash';
+import React from 'react';
+import { render } from 'react-dom';
+
+const DEBUG = process.env.NODE_ENV != 'production';
+
+function renderApp() {
+//  const history = browserHistory;
+
+  render((
+    <div>Nexus</div>
+  ), document.getElementById('app'));
+}
+
+
+renderApp();
