15ada174bbeebb2be3f96067b733490cff92bf5e
Krzysztof Janosz <janosz.krzysztof@gmail.com>
2017-05-17 10:01:14 +0200

    Add payment state test to bulk export

--- phoenix-scala/starfish/src/main/scala/utils/Strings.scala
+++ phoenix-scala/starfish/src/main/scala/utils/Strings.scala
@@ -14,7 +14,7 @@ object Strings {
         case c if c.isUpper ⇒ s"_${c.toLower}"
         case c              ⇒ s"$c"
       }.stripPrefix("_")
-    def prettify: String                = s.split("(?=\\p{Upper})").mkString(" ")
-    def quote(escapeChar: Char): String = "\"" + s.replace("\"", s"""$escapeChar"""") + "\""
+    def prettify: String                       = s.split("(?=\\p{Upper})").mkString(" ")
+    def quote(escapeChar: Char = '\\'): String = "\"" + s.replace("\"", s"""$escapeChar"""") + "\""
   }
 }
--- phoenix-scala/test/unit/payloads/EntityExportPayloadsTest.scala
+++ phoenix-scala/test/unit/payloads/EntityExportPayloadsTest.scala
@@ -1,11 +1,14 @@
 package payloads
 
 import java.time.Instant
+import models.payment.InStorePaymentStates
+import models.payment.creditcard.CreditCardCharge
 import org.json4s.JsonAST._
 import org.scalacheck.Gen
 import org.scalatest.prop.PropertyChecks
 import payloads.EntityExportPayloads.FieldCalculation
 import testutils.TestBase
+import utils.Strings._
 
 class EntityExportPayloadsTest extends TestBase with PropertyChecks {
   "FieldCalculation.State" - {
@@ -43,6 +46,84 @@ class EntityExportPayloadsTest extends TestBase with PropertyChecks {
       forAll(Gen.alphaStr.suchThat(_ != "state")) { field ⇒
         FieldCalculation.State.calculate.isDefinedAt(field → JObject()) must === (false)
       }
+    }
+  }
+
+  "FieldCalculation.PaymentState" - {
+    import models.cord.CordPaymentState._
+
+    "should return 'Cart' state if payments are empty" in {
+      FieldCalculation.PaymentState
+        .calculate("payment.state" → JObject("payments" → JArray(Nil))) must === (
+          Cart.toString.prettify.quote())
+    }
+
+    "should return 'Cart' if any state in payments is in cart state" in {
+      val payments = List(
+          JObject("giftCardState" → JString(
+                  InStorePaymentStates.State.show(InStorePaymentStates.Capture))),
+          JObject("creditCardState" → JString(
+                  CreditCardCharge.State.show(CreditCardCharge.FailedAuth)))
+      )
+      FieldCalculation.PaymentState.calculate(
+          "payment.state" → JObject("payments" → JArray(payments))) must === (
+          Cart.toString.prettify.quote())
+    }
+
+    "should return 'Cart' if any state in payments is unknown" in {
+      val payments = List(JObject("creditCardState" → JString("whatever")))
+      FieldCalculation.PaymentState.calculate(
+          "payment.state" → JObject("payments" → JArray(payments))) must === (
+          Cart.toString.prettify.quote())
+    }
+
+    "should return 'Full Capture' if all payments are fully captured" in {
+      val payments = List(
+          JObject("storeCreditState" → JString(
+                  InStorePaymentStates.State.show(InStorePaymentStates.Capture))),
+          JObject("creditCardState" → JString(
+                  CreditCardCharge.State.show(CreditCardCharge.FullCapture)))
+      )
+      FieldCalculation.PaymentState.calculate(
+          "payment.state" → JObject("payments" → JArray(payments))) must === (
+          FullCapture.toString.prettify.quote())
+    }
+
+    "should return 'Expired Auth' if any payment is expired" in {
+      val payments = List(
+          JObject("storeCreditState" → JString(
+                  InStorePaymentStates.State.show(InStorePaymentStates.Capture))),
+          JObject("creditCardState" → JString(
+                  CreditCardCharge.State.show(CreditCardCharge.ExpiredAuth)))
+      )
+      FieldCalculation.PaymentState.calculate(
+          "payment.state" → JObject("payments" → JArray(payments))) must === (
+          ExpiredAuth.toString.prettify.quote())
+    }
+
+    "should return 'Failed Capture' if any payment has failed during capture" in {
+      val payments = List(
+          JObject("giftCardState" → JString(
+                  InStorePaymentStates.State.show(InStorePaymentStates.Capture))),
+          JObject("creditCardState" → JString(
+                  CreditCardCharge.State.show(CreditCardCharge.FailedCapture)))
+      )
+      FieldCalculation.PaymentState.calculate(
+          "payment.state" → JObject("payments" → JArray(payments))) must === (
+          FailedCapture.toString.prettify.quote())
+    }
+
+    "should return 'Auth' if all payments are in auth state" in {
+      val payments = List(
+          JObject("giftCardState" → JString(
+                  InStorePaymentStates.State.show(InStorePaymentStates.Auth))),
+          JObject("creditCardState" → JString(CreditCardCharge.State.show(CreditCardCharge.Auth))),
+          JObject("storeCreditState" → JString(
+                  InStorePaymentStates.State.show(InStorePaymentStates.Auth)))
+      )
+      FieldCalculation.PaymentState.calculate(
+          "payment.state" → JObject("payments" → JArray(payments))) must === (
+          Auth.toString.prettify.quote())
     }
   }
 }
