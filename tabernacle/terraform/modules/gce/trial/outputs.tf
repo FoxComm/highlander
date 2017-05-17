c6bae46305782a5c00c70b492b87994ab3dea04e
Pavel Kiselev <kpashka@gmail.com>
2017-05-17 11:41:16 -0700

    Renaming & creating instances

--- tabernacle/terraform/modules/gce/trial/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "amigo_address" {
-  value = "${google_compute_instance.trial-amigo-0.network_interface.0.address}"
-}
-
-output "frontend_address" {
-  value = "${google_compute_instance.trial-frontend.network_interface.0.address}"
-}
