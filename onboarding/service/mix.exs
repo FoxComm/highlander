ae897ac01c0590046f89d913f144e347d8dfe981
Maxim Khailo <max@foxcommerce.com>
2017-05-18 11:29:17 -0700

    Adding SRV record lookup function.

--- onboarding/service/mix.exs
+++ onboarding/service/mix.exs
@@ -6,6 +6,7 @@ defmodule OnboardingService.Mixfile do
      version: "0.0.1",
      elixir: "~> 1.2",
      elixirc_paths: elixirc_paths(Mix.env),
+     erlc_paths: ["erl"],
      compilers: [:phoenix, :gettext] ++ Mix.compilers,
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
