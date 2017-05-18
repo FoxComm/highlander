3d837a34698867c337927ffb09593d9468c2d435
Diokuz <diokuz@gmail.com>
2017-05-18 20:56:22 +0300

    Dynamic code splitting added

--- ashes/webpack/prod.js
+++ ashes/webpack/prod.js
@@ -5,17 +5,12 @@ const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 const ManifestPlugin = require('webpack-manifest-plugin');
 
 module.exports = {
-  output: {
-    // What is chunkhash? Read explanation: https://github.com/webpack-contrib/extract-text-webpack-plugin/issues/153
-    filename: '[name].[chunkhash:4].js',
+  entry: {
+    app: path.resolve(__dirname, '../src/client.js')
+  },
 
-    // We set const string here because `chunkFilename` value will be injected to vendor.js.
-    // It expected to be vice versa, but for some reasons webpack thinks that `vendor.js` is the main chunk.
-    // Default of `chunkFilename` is something like `[name].[chunkhash].js`, and if `main.js` changes, its hash changes,
-    // which changes the content of `vendor.js`, which changes hash of `vendor.js`,
-    // and that breaks the point of long-term cache.
-    // If we want `true` (not hardcoded) chunks, `chunkFilename` must be changed.
-    chunkFilename: 'unused-but-const-string'
+  output: {
+    filename: '[name].[chunkhash:6].js'
   },
 
   module: {
@@ -49,12 +44,17 @@ module.exports = {
       minChunks: module => module.resource && module.resource.includes(path.resolve('node_modules')),
     }),
 
+    // Move all chunkhashes to manifest chunk https://webpack.js.org/guides/code-splitting-libraries/#manifest-file
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'manifest'
+    }),
+
     new webpack.optimize.UglifyJsPlugin({
       compress: { warnings: false },
       sourceMap: false
     }),
 
-    new ExtractTextPlugin('app.[contenthash:4].css'),
+    new ExtractTextPlugin('app.[contenthash:6].css'),
     new OptimizeCssAssetsPlugin(),
 
     new ManifestPlugin(),
