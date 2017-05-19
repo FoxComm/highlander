316b99e742cc833f90055f18d0cf70c02ad33431
mbektimirov <mbektimirov@gmail.com>
2017-05-20 00:04:20 +0500

    Use double-ended wildcard queries by default for string terms

--- ashes/src/modules/carts/list.js
+++ ashes/src/modules/carts/list.js
@@ -11,10 +11,12 @@ const { reducer, actions } = makeLiveSearch(
   {
     processQuery: (query) => {
       query = addNativeFilters(query, [dsl.existsFilter('deletedAt', 'missing')]);
-      return addShouldFilters(query, [
+      query = addShouldFilters(query, [
         dsl.rangeFilter('lineItemCount', { gt: 0 }),
         dsl.nestedExistsFilter('customer.email', 'exists'),
       ]);
+
+      return query;
     },
     initialState: { sortBy: '-createdAt' },
     rawSorts: ['customer.name', 'customer.email']
