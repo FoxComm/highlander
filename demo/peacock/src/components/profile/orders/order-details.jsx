9b3127cf07c33a99a0d881a2feaae4abee2a04a3
Artem <artem@foxcommerce.com>
2017-05-22 19:12:56 +0300

    Add payment information

--- demo/peacock/src/components/profile/orders/order-details.jsx
+++ demo/peacock/src/components/profile/orders/order-details.jsx
@@ -16,6 +16,7 @@ import AddressDetails from 'ui/address/address-details';
 import Currency from 'ui/currency';
 import Icon from 'ui/icon';
 import OrderSummary from 'components/order-summary/order-summary';
+import ViewBilling from 'pages/checkout/billing/view-billing';
 
 import styles from '../profile.css';
 
@@ -75,6 +76,26 @@ class Order extends Component {
     );
   }
 
+  get paymentMethod() {
+    const { order } = this.props;
+    const paymentMethod = _.filter(order.paymentMethods, { type: 'creditCard' })[0];
+    const methodEmpty = _.isEmpty(paymentMethod);
+
+    return (
+      <div styleName="payment-method">
+        <div styleName="title">Payment</div>
+        {
+          methodEmpty ?
+          <div>No credit card found</div>
+          :
+          <ViewBilling
+            billingData={paymentMethod}
+          />
+        }
+      </div>
+    );
+  }
+
   get orderSummary() {
     const { order } = this.props;
 
@@ -82,8 +103,8 @@ class Order extends Component {
       <OrderSummary
         embedded
         styleName="order-summary"
-        {...order}
         skus={order.lineItems.skus}
+        {...order}
       />
     );
   }
@@ -97,13 +118,13 @@ class Order extends Component {
       <div>
         {this.shippingAddress}
         {this.shippingMethod}
+        {this.paymentMethod}
         {this.orderSummary}
       </div>
     );
   }
 
   render() {
-    console.log('render() fetchOrderState -> ', this.props.fetchOrderState);
     return (
       <div>
         {this.content}
@@ -111,123 +132,6 @@ class Order extends Component {
     );
   }
 }
-
-
-  // get shippingAddress() {
-  //   const { shippingAddress } = this.props.order;
-  //
-  //   return <AddressDetails address={shippingAddress} />;
-  // }
-  //
-  // renderCreditCard(paymentMethod) {
-  //   return (
-  //     <div styleName="payment-method" key={paymentMethod.type}>
-  //       <div>
-  //         <Icon styleName="payment-icon" name={`fc-payment-${paymentMethod.brand.toLowerCase()}`} />
-  //       </div>
-  //       <div>
-  //         <strong>{paymentMethod.holderName}</strong>
-  //       </div>
-  //       <div>
-  //         XXXX-XXXX-XXXX-{paymentMethod.lastFour}
-  //       </div>
-  //       <div>
-  //         {paymentMethod.expMonth}/{paymentMethod.expYear}
-  //       </div>
-  //     </div>
-  //   );
-  // }
-  //
-  // renderGiftCard(paymentMethod) {
-  //   return (
-  //     <div styleName="payment-method" key={paymentMethod.code}>
-  //       <div>GIFT CARD {paymentMethod.code}</div>
-  //       <div>
-  //         <strong><Currency value={paymentMethod.amount} /></strong>
-  //       </div>
-  //     </div>
-  //   );
-  // }
-  //
-  // renderStoreCredit(paymentMethod) {
-  //   return (
-  //     <div styleName="payment-method" key={paymentMethod.type}>
-  //       <div>GIFT CARD BALANCE</div>
-  //       <div>
-  //         <strong><Currency value={paymentMethod.amount} /></strong>
-  //       </div>
-  //     </div>
-  //   );
-  // }
-  //
-  // @autobind
-  // renderPaymentMethod(paymentMethod) {
-  //   switch (paymentMethod.type) {
-  //     case 'creditCard':
-  //       return this.renderCreditCard(paymentMethod);
-  //     case 'giftCard':
-  //       return this.renderGiftCard(paymentMethod);
-  //     case 'storeCredit':
-  //       return this.renderStoreCredit(paymentMethod);
-  //     default:
-  //       return null;
-  //   }
-  // }
-  //
-  // render() {
-  //   const { order } = this.props;
-  //   if (!order) {
-  //     return <Loader />;
-  //   }
-  //
-  //   return (
-  //     <div>
-  //       <table styleName="simple-table">
-  //         <thead>
-  //           <tr>
-  //             <th>Date</th>
-  //             <th>Order #</th>
-  //             <th>Total</th>
-  //             <th>Status</th>
-  //             <th>Tracking</th>
-  //           </tr>
-  //         </thead>
-  //         <tbody>
-  //           <OrderRow order={order} />
-  //         </tbody>
-  //         <tbody styleName="order-service">
-  //           <tr>
-  //             <th colSpan="2">SHIPPING</th>
-  //             <th colSpan="2">DELIVERY</th>
-  //             <th>BILLING</th>
-  //           </tr>
-  //           <tr>
-  //             <td colSpan="2">
-  //               {this.shippingAddress}
-  //             </td>
-  //             <td colSpan="2">
-  //               <div styleName="delivery-name">{order.shippingMethod.name}</div>
-  //               <div>
-  //                 <strong><Currency value={order.shippingMethod.price} /></strong>
-  //               </div>
-  //             </td>
-  //             <td>
-  //               {_.map(order.paymentMethods, this.renderPaymentMethod)}
-  //             </td>
-  //           </tr>
-  //         </tbody>
-  //       </table>
-  //       <OrderSummary
-  //         isCollapsed={false}
-  //         header={null}
-  //         totalTitle="ORDER TOTAL"
-  //         embedded
-  //         styleName="order-summary"
-  //         {...order}
-  //         skus={order.lineItems.skus}
-  //       />
-  //     </div>
-  //   );
 
 const mapStateToProps = (state) => {
   return {
