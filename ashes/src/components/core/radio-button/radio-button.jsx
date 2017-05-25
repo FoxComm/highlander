880f24d589aecfd5d3092659dfa3bbd49e80caaf
Bad_Company <ihor.peredystyi@gmail.com>
2017-05-25 18:33:59 +0300

    feat: add styles to radio-button

--- ashes/src/components/core/radio-button/radio-button.jsx
+++ ashes/src/components/core/radio-button/radio-button.jsx
@@ -2,6 +2,10 @@
 
 // libs
 import React from 'react';
+import classNames from 'classnames';
+
+// styles
+import s from './radio-button.css'
 
 type Props = {
   /** Button content (label) */
@@ -12,7 +16,7 @@ const RadioButton = (props: Props) => {
   const { children, ...rest } = props;
 
   return (
-    <div className="fc-form-field fc-radio">
+    <div className={ classNames(s.radio, props.className) }>
       <input type="radio" {...rest} />
       {children}
     </div>
