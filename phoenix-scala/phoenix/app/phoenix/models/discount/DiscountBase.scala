d5dfc64a8333f20d40cce69b440bb277d7b9c231
Anna Zubenko <anna@foxcommerce.com>
2017-05-29 20:39:19 +0300

    Get rid of unused methods

--- phoenix-scala/phoenix/app/phoenix/models/discount/DiscountBase.scala
+++ phoenix-scala/phoenix/app/phoenix/models/discount/DiscountBase.scala
@@ -16,14 +16,4 @@ trait DiscountBase {
     lineItems.foldLeft(0L) { (sum, data) ⇒
       if (formIds.contains(data.productForm.id.toString)) sum + data.price else sum
     }
-
-  def unitsBySku(lineItems: Seq[LineItemProductData[_]], codes: Seq[String]): Int =
-    lineItems.foldLeft(0) { (sum, data) ⇒
-      if (codes.contains(data.sku.code)) sum + 1 else sum
-    }
-
-  def totalBySku(lineItems: Seq[LineItemProductData[_]], codes: Seq[String]): Long =
-    lineItems.foldLeft(0L) { (sum, data) ⇒
-      if (codes.contains(data.sku.code)) sum + data.price else sum
-    }
 }
