6585b38fb07630315010707e665d9aac9622fc1f
Jeff Mataya <jeff.mataya@gmail.com>
2017-06-02 15:31:19 -0700

    Protect against import cycles when updating

--- phoenix-scala/objectframework/app/objectframework/failures/Failures.scala
+++ phoenix-scala/objectframework/app/objectframework/failures/Failures.scala
@@ -9,3 +9,9 @@ case object CorruptedContentObject extends Failure {
 case class RelatedContentDoesNotExist(kind: String, commitId: Int) extends Failure {
   override def description = s"Unable to create relation $kind $commitId - object not found"
 }
+
+case class ImportCycleFound(entityId: Int, entityKind: String, relCommitId: Int, relKind: String)
+    extends Failure {
+  override def description =
+    s"Unable to create relation from $entityKind $entityId to $relKind $relCommitId - results in relations cycle"
+}
