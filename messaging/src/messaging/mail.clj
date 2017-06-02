01679639ddc9ef9fa48a3e3c21a2b2344d62fda1
Pavel Kiselev <kpashka@gmail.com>
2017-06-02 01:43:06 -0700

    Add onboarding services support to Highlander Group (#2038)
    * Add onboarding services support to Highlander Group
    
    * Finishing touches
    
    * Add dev/nginx changes
    
    * Make messaging silent on staging and appliance
    
    * Split module to avoid cyclic dependency
    
    * Add cljfmt
    
    * Deref
    
    * Use Alpine

--- messaging/src/messaging/mail.clj
+++ messaging/src/messaging/mail.clj
@@ -1,41 +1,40 @@
 (ns messaging.mail
   (:require
-    [aleph.http :as http]
-    [cheshire.core :as json]
-    [byte-streams :as bs]
-    [environ.core :refer [env]]
-    [clojure.string :as string]
-    [messaging.settings :as settings]
-    [messaging.phoenix :as phoenix]
-    [clojchimp.client :as mailchimp]
-    [gws.mandrill.client :as client]
-    [gws.mandrill.api.messages :as messages]
-    [gws.mandrill.api.templates :as templates]
-    [helpers.activities-transforms :as at]))
-
+   [aleph.http :as http]
+   [cheshire.core :as json]
+   [byte-streams :as bs]
+   [taoensso.timbre :as log]
+   [environ.core :refer [env]]
+   [clojure.string :as string]
+   [messaging.shared :as shared]
+   [messaging.settings :as settings]
+   [messaging.phoenix :as phoenix]
+   [clojchimp.client :as mailchimp]
+   [gws.mandrill.client :as client]
+   [gws.mandrill.api.messages :as messages]
+   [gws.mandrill.api.templates :as templates]
+   [helpers.activities-transforms :as at]))
 
 ;; mandrill client
 (defn client []
   (let [mkey (settings/get :mandrill_key)]
-    (when (empty? mkey)
+    (when (and (not= @shared/environment shared/staging) (empty? mkey))
       (throw (ex-info "Mandrill key is not defined" {})))
     (client/create mkey)))
 
 ;; mailchimp client
 (defn mclient []
   (let [mkey (settings/get :mailchimp_key)]
-    (when (empty? mkey)
+    (when (and (not= @shared/environment shared/staging) (empty? mkey))
       (throw (ex-info "Mailchimp key is not defined" {})))
     (mailchimp/create-client "fox-messaging" mkey)))
-
 
 (defn make-tpl-vars
   "Convert clojure map to mandtrill template vars"
   [vars]
   {:pre  [(map? vars)]}
   (for [[k v] vars]
-   {:name k :content v}))
-
+    {:name k :content v}))
 
 (defn gen-msg
   "Generate ready to send email message for Mandrill.
@@ -49,23 +48,23 @@
    {:keys [subject text html] :as opts}]
   {:pre [(every? not-empty [recipients customer-email])]}
   (let [additional-vars (some->
-                          (settings/get :additional_merge_vars)
-                          (json/parse-string true))
+                         (settings/get :additional_merge_vars)
+                         (json/parse-string true))
         base-vars {:shop_base_url (settings/get :shop_base_url)
                    :company_name (settings/get :retailer_name)
                    :email_subject subject
                    :update_profile_link (settings/get :update_customer_profile_link)
                    :customer_name customer-name}
         base-merge-vars (if additional-vars
-                           (merge base-vars additional-vars)
+                          (merge base-vars additional-vars)
                           base-vars)]
-   (merge opts {:to recipients
-                :global_merge_vars (make-tpl-vars (merge base-merge-vars vars))
-                :merge_language "handlebars"
-                :auto_text true
+    (merge opts {:to recipients
+                 :global_merge_vars (make-tpl-vars (merge base-merge-vars vars))
+                 :merge_language "handlebars"
+                 :auto_text true
 
-                :from_email (settings/get :from_email)
-                :subject subject})))
+                 :from_email (settings/get :from_email)
+                 :subject subject})))
 
 (defn send-template!
   [slug template]
@@ -74,11 +73,9 @@
                            :template_content []
                            :message template}))
 
-
 (defn dispatch-activity
   [activity]
   (keyword (:kind activity)))
-
 
 (defmulti handle-activity dispatch-activity)
 (defmethod handle-activity :default [act] nil)
@@ -92,8 +89,8 @@
         order-ref (get order "referenceNumber")
         rcpt {:email email :name customer-name}
         rcpts (if (settings/get :send_order_checkout_completed_bcc)
-                  [rcpt {:email (settings/get :from_email) :type "bcc"}]
-                  [rcpt])
+                [rcpt {:email (settings/get :from_email) :type "bcc"}]
+                [rcpt])
         msg (gen-msg rcpts
                      {:items (let [skus (get-in order ["lineItems" "skus"])]
                                (map at/sku->item skus))
@@ -107,7 +104,7 @@
 
                      {:subject (settings/get :order_checkout_subject)})]
 
-      (send-template! (settings/get :order_confirmation_template) msg)))
+    (send-template! (settings/get :order_confirmation_template) msg)))
 
 (defmethod handle-activity :shipment_shipped
   [activity]
@@ -133,7 +130,7 @@
                       :order_ref order-ref}
 
                      {:subject (settings/get :order_shipped_subject)})]
-      (send-template! (settings/get :order_shipped_template) msg)))
+    (send-template! (settings/get :order_shipped_template) msg)))
 
 (defmethod handle-activity :order_state_changed
   [activity]
@@ -145,7 +142,7 @@
         new-state (get-in order ["orderState"])
         msg (gen-msg [{:email email :name customer-name}]
                      {:items (let [skus (get-in order ["lineItems" "skus"])]
-                              (map at/sku->item skus))
+                               (map at/sku->item skus))
                       :totals (at/format-prices (get order "totals"))
                       :placed_at (at/date-simple-format (get order "placedAt"))
                       :shipping_method (get-in order ["shippingMethod" "name"])
@@ -154,23 +151,22 @@
                       :billing_info (get order "billingCreditCardInfo")
                       :order_ref order-ref}
 
-                    {:subject (settings/get :order_canceled_subject)})]
+                     {:subject (settings/get :order_canceled_subject)})]
     (when (= "canceled" new-state)
       (send-template! (settings/get :order_canceled_template) msg))))
 
 (defmethod handle-activity :user_remind_password
   [activity]
   (let [email (get-in activity [:data "user" "email"])
-              reset-code (get-in activity [:data "code"])
-              reset-pw-link (format (settings/get :reset_password_link_format) reset-code)
-              full-reset-password-link (format "%s/%s" (settings/get :shop_base_url) reset-pw-link)
-              customer-name (get-in activity [:data "user" "name"])]
-       (send-template! (settings/get :customer_remind_password_template)
-           (gen-msg [{:email email :name customer-name}]
-               {:reset_password_link full-reset-password-link
-                :reset_code reset-code}
-               {:subject (settings/get :customer_remind_password_subject)}))))
-
+        reset-code (get-in activity [:data "code"])
+        reset-pw-link (format (settings/get :reset_password_link_format) reset-code)
+        full-reset-password-link (format "%s/%s" (settings/get :shop_base_url) reset-pw-link)
+        customer-name (get-in activity [:data "user" "name"])]
+    (send-template! (settings/get :customer_remind_password_template)
+                    (gen-msg [{:email email :name customer-name}]
+                             {:reset_password_link full-reset-password-link
+                              :reset_code reset-code}
+                             {:subject (settings/get :customer_remind_password_subject)}))))
 
 (defmethod handle-activity :gift_card_created
   [activity]
@@ -182,16 +178,15 @@
         senderName (get-in data ["giftCard" "senderName"] "")
         giftCardCode (get-in data ["giftCard" "code"])]
 
-   (when (every? seq [recipientEmail giftCardCode])
-     (send-template! (settings/get :gift_card_customer_template)
-          (gen-msg [{:email recipientEmail :name recipientName}]
-              {:balance (at/format-price-int (get giftCard "availableBalance"))
-               :message message
-               :sender_name senderName
-               :recipient_name recipientName
-               :gift_card_number giftCardCode}
-              {:subject (settings/get :gift_card_customer_subject)})))))
-
+    (when (every? seq [recipientEmail giftCardCode])
+      (send-template! (settings/get :gift_card_customer_template)
+                      (gen-msg [{:email recipientEmail :name recipientName}]
+                               {:balance (at/format-price-int (get giftCard "availableBalance"))
+                                :message message
+                                :sender_name senderName
+                                :recipient_name recipientName
+                                :gift_card_number giftCardCode}
+                               {:subject (settings/get :gift_card_customer_subject)})))))
 
 (defmethod handle-activity :send_simple_mail
   [activity]
@@ -214,20 +209,16 @@
     (when (settings/add-new-customers-to-mailchimp?)
       (try
         (mailchimp/create-member-for-list
-          (mclient)
-          (settings/get :mailchimp_customers_list_id)
+         (mclient)
+         (settings/get :mailchimp_customers_list_id)
          {:email_type "html"
           :email_address email
           :merge_fields {"NAME" customer-name}
           :status "subscribed"})
-       (catch Exception e (prn "Can't add user to list" e))))
-
-
-   (send-template! (settings/get :customer_created_template)
-                   (gen-msg [{:email email :name customer-name}]
-                            {}
-                            {:subject (settings/get :customer_registration_subject)}))))
-
+        (catch Exception e (prn "Can't add user to list" e)))) (send-template! (settings/get :customer_created_template)
+                                                                               (gen-msg [{:email email :name customer-name}]
+                                                                                        {}
+                                                                                        {:subject (settings/get :customer_registration_subject)}))))
 
 (defmethod handle-activity :customer_registered
   [activity]
