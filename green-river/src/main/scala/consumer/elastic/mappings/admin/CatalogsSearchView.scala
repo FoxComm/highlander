ea1c3be4b0d5c89e469322ee369d1d1aa680a398
Jeff Mataya <jeff@foxcommerce.com>
2017-06-08 15:32:49 -0700

    Fix greenriver fmt

--- green-river/src/main/scala/consumer/elastic/mappings/admin/CatalogsSearchView.scala
+++ green-river/src/main/scala/consumer/elastic/mappings/admin/CatalogsSearchView.scala
@@ -9,22 +9,22 @@ import consumer.elastic.mappings._
 final case class CatalogsSeachView()(implicit ec: EC) extends AvroTransformer {
   def topic() = "catalogs_search_view"
   def mapping() = esMapping(topic()).fields(
-      field("id", LongType),
-      field("scope", StringType).index("not_analyzed"),
-      field("name", StringType)
-        .analyzer("autocomplete")
-        .fields(field("raw", StringType).index("not_analyzed")),
-      field("site", StringType)
-        .analyzer("autocomplete")
-        .fields(field("raw", StringType).index("not_analyzed")),
-      field("countryId", IntegerType),
-      field("countryName", StringType)
-        .analyzer("autocomplete")
-        .fields(field("raw", StringType).index("not_analyzed")),
-      field("defaultLanguage", StringType)
-        .analyzer("autocomplete")
-        .fields(field("raw", StringType).index("not_analyzed")),
-      field("createdAt", DateType).format(dateFormat),
-      field("updatedAt", DateType).format(dateFormat)
+    field("id", LongType),
+    field("scope", StringType).index("not_analyzed"),
+    field("name", StringType)
+      .analyzer("autocomplete")
+      .fields(field("raw", StringType).index("not_analyzed")),
+    field("site", StringType)
+      .analyzer("autocomplete")
+      .fields(field("raw", StringType).index("not_analyzed")),
+    field("countryId", IntegerType),
+    field("countryName", StringType)
+      .analyzer("autocomplete")
+      .fields(field("raw", StringType).index("not_analyzed")),
+    field("defaultLanguage", StringType)
+      .analyzer("autocomplete")
+      .fields(field("raw", StringType).index("not_analyzed")),
+    field("createdAt", DateType).format(dateFormat),
+    field("updatedAt", DateType).format(dateFormat)
   )
 }
