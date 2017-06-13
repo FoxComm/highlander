0d946703ef54f3d98f9d88d4aa4323ffcb709f84
Bad_Company <ihor.peredystyi@gmail.com>
2017-06-13 15:05:56 +0300

    refactor: replace less with css-modules
    & remove less file

--- ashes/src/components/utils/change/change.jsx
+++ ashes/src/components/utils/change/change.jsx
@@ -1,22 +1,30 @@
-/** Libs */
+/* @flow */
+
+// libs
 import classNames from 'classnames';
 import React from 'react';
-import PropTypes from 'prop-types';
 
-/** Change component to render colored number changes */
-const Change = props => {
+// styles
+import s from './change.css';
+
+type Props = {
+  /** value of change */
+  value: number
+}
+
+/** Change component serves to render colored number changes
+ *
+ * @function Change
+ * */
+const Change = (props: Props) => {
   const { value } = props;
 
-  const cls = classNames('fc-change', {
-    '_positive': value > 0,
-    '_negative': value < 0
+  const cls = classNames(s.change, {
+    [s.positive]: value > 0,
+    [s.negative]: value < 0
   });
 
   return <span className={cls}>{Math.abs(value)}</span>;
-};
-
-Change.propTypes = {
-  value: PropTypes.number.isRequired
 };
 
 export default Change;
