c7a6aab4762e45d442bdb12fc2e843459134415f
Bagrat Bagoyan <bagratinho@gmail.com>
2017-06-13 15:45:43 +0400

    Change delete label to archive for coupons list bulk actions

--- ashes/src/modules/coupons/list.js
+++ ashes/src/modules/coupons/list.js
@@ -12,6 +12,15 @@ const searchTerms = [
     type: 'date',
     term: 'createdAt',
   },
+  {
+    title: 'Coupon : Is Archived',
+    type: 'exists',
+    term: 'archivedAt',
+    suggestions: [
+      { display: 'Yes', operator: 'exists' },
+      { display: 'No', operator: 'missing' },
+    ],
+  },
 ];
 
 const { reducer, actions } = makeLiveSearch(
