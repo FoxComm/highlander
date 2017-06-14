3fd7cba5a32d5db34988c0b4e6e303af35bbfbbb
Jeff Mataya <jeff@foxcommerce.com>
2017-06-13 17:54:23 -0700

    Triggers for adding the catalog to the product views

--- phoenix-scala/sql/R__products_catalog_view_functions.sql
+++ phoenix-scala/sql/R__products_catalog_view_functions.sql
@@ -166,3 +166,30 @@ create trigger update_products_cat_from_product_sku_links_delete_trigger
   after delete on product_sku_links
   for each row
   execute procedure update_products_cat_from_product_sku_links_delete_fn();
+
+create or replace function update_products_catalog_view_catalogs_fn() returns trigger as $$
+begin
+  update products_catalog_view
+  set
+    catalogs = catalogProducts.catalog_names
+  from (select
+          case when count(cp.id) = 0
+            then
+              '[]' :: jsonb
+            else
+              jsonb_agg(c.name)
+          end as catalog_names
+        from catalogs as c
+          left join catalog_products as cp on (cp.catalog_id = c.id and cp.archived_at is null)
+        where cp.product_id = new.product_id) as catalogProducts
+  where products_catalog_view.product_id = new.product_id;
+
+  return null;
+end;
+$$ language plpgsql;
+
+drop trigger if exists update_products_catalog_view_catalogs on catalog_products;
+create trigger update_products_catalog_view_catalogs
+after insert or update on catalog_products
+  for each row
+  execute procedure update_products_catalog_view_catalogs_fn();
