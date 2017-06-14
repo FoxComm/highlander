f9229ac417887d200f16923a6333e825ae4d9663
Krzysztof Janosz <janosz.krzysztof@gmail.com>
2017-06-14 15:55:21 +0200

    Apply small changes
    Allow for empty fc query, that is interpreted as `match_all` query.
    Return max_score with response to keep it consistent with current search result.

--- search-service/core/app/foxcomm/search/SearchService.scala
+++ search-service/core/app/foxcomm/search/SearchService.scala
@@ -24,26 +24,28 @@ class SearchService(private val client: ElasticClient) extends AnyVal {
         // consider more principled approach
         // maybe free monad would be a good fit there?
         (_: SearchDefinition) bool {
-          query.query.foldLeft(new BoolQueryDefinition) {
-            case (bool, QueryFunction.eq(in, value)) ⇒
-              bool.filter(in.toList.map(termsQuery(_, value.toList: _*)))
-            case (bool, QueryFunction.neq(in, value)) ⇒
-              bool.not(in.toList.map(termsQuery(_, value.toList: _*)))
-            case (bool, QueryFunction.matches(in, value)) ⇒
-              val fields = in.toList
-              bool.must(value.toList.map(q ⇒ multiMatchQuery(q).fields(fields)))
-            case (bool, QueryFunction.range(in, value)) ⇒
-              val query   = rangeQuery(in.field)
-              val unified = value.unify
-              val queryWithLowerBound = unified.lower.fold(query) {
-                case (b, v) ⇒ query.from(v).includeLower(b.withBound)
-              }
-              val boundedQuery = unified.upper.fold(queryWithLowerBound) {
-                case (b, v) ⇒ queryWithLowerBound.to(v).includeUpper(b.withBound)
-              }
-              bool.filter(boundedQuery)
-            case (bool, _) ⇒ bool // TODO: implement rest of cases
-          }
+          query
+            .map(_.query.foldLeft(new BoolQueryDefinition) {
+              case (bool, QueryFunction.eq(in, value)) ⇒
+                bool.filter(in.toList.map(termsQuery(_, value.toList: _*)))
+              case (bool, QueryFunction.neq(in, value)) ⇒
+                bool.not(in.toList.map(termsQuery(_, value.toList: _*)))
+              case (bool, QueryFunction.matches(in, value)) ⇒
+                val fields = in.toList
+                bool.must(value.toList.map(q ⇒ multiMatchQuery(q).fields(fields)))
+              case (bool, QueryFunction.range(in, value)) ⇒
+                val query   = rangeQuery(in.field)
+                val unified = value.unify
+                val queryWithLowerBound = unified.lower.fold(query) {
+                  case (b, v) ⇒ query.from(v).includeLower(b.withBound)
+                }
+                val boundedQuery = unified.upper.fold(queryWithLowerBound) {
+                  case (b, v) ⇒ queryWithLowerBound.to(v).includeUpper(b.withBound)
+                }
+                bool.filter(boundedQuery)
+              case (bool, _) ⇒ bool // TODO: implement rest of cases
+            })
+            .getOrElse((new BoolQueryDefinition).must(matchAllQuery))
         }
     }
     val baseSearch = withQuery(search in searchIndex size searchSize)
@@ -52,9 +54,13 @@ class SearchService(private val client: ElasticClient) extends AnyVal {
     client
       .execute(searchFrom.fold(limitedSearch)(limitedSearch from))
       .map(response ⇒
-        SearchResult(result = response.hits.collect {
-          case ExtractJsonObject(obj) ⇒ obj
-        }(collection.breakOut), pagination = SearchPagination(total = response.totalHits)))
+        SearchResult(
+          result = response.hits.collect {
+            case ExtractJsonObject(obj) ⇒ obj
+          }(collection.breakOut),
+          pagination = SearchPagination(total = response.totalHits),
+          maxScore = response.maxScore
+      ))
   }
 }
 
