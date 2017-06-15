4d55c573af11ed224e1245c1337b512c6ae346ad
Jeff Mataya <jeff@foxcommerce.com>
2017-06-15 14:42:42 -0700

    PR feedback

--- phoenix-scala/objectframework/app/objectframework/content/ContentAttribute.scala
+++ phoenix-scala/objectframework/app/objectframework/content/ContentAttribute.scala
@@ -11,8 +11,8 @@ case class ContentAttribute(t: String, v: JValue)
 
 object ContentAttribute {
   def build(shadowAttribute: JValue, form: Form): Either[Failures, ContentAttribute] = {
-    def attributeType = shadowAttribute \ "type"
-    def attributeRef  = shadowAttribute \ "ref"
+    val attributeType = shadowAttribute \ "type"
+    val attributeRef  = shadowAttribute \ "ref"
 
     (attributeType, attributeRef) match {
       case (JString(attrType), JString(attrRef)) ⇒
