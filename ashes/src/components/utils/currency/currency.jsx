6e5faaa8f1ab99abae1c7726f3103231b94729e1
Bad_Company <ihor.peredystyi@gmail.com>
2017-06-16 01:26:23 +0300

    refactor: use `Change` component for transaction mode.

--- ashes/src/components/utils/currency/currency.jsx
+++ ashes/src/components/utils/currency/currency.jsx
@@ -1,11 +1,13 @@
 /* @flow */
 
 // libs
+import { curryRight } from 'lodash';
 import React from 'react';
 import classNames from 'classnames';
 
 // components
 import formatCurrency from '../../../lib/format-currency';
+import Change from 'components/utils/change';
 
 // styles
 import s from './currency.css';
@@ -36,14 +38,17 @@ type Props = {
 
 const Currency = (props: Props) => {
   const {isTransaction, id, className, ...rest} = props;
-  const currencyCls = classNames(s.currency, {
-    [s.transaction]: isTransaction,
-    [s.negative]: parseInt(props.value, 10) < 0
-  });
+  let value;
+
+  if (isTransaction) {
+    value = <Change value={props.value} format={curryRight(formatCurrency)(rest)} />;
+  } else {
+    value = formatCurrency(props.value, rest);
+  }
 
   return (
-    <span id={id} className={classNames(currencyCls, className)}>
-      {formatCurrency(props.value, {...rest})}
+    <span id={id} className={classNames(s.currency, className)}>
+      {value}
     </span>
   );
 };
