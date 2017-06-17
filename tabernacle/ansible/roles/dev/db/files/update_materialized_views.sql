785f24ad3196163c00a832e3202d46a7ec94be1e
Sergey Rublev <sergey@foxcommerce.com>
2017-06-17 22:27:48 +0300

    store_credit_transactions_view already doesn't exist

--- tabernacle/ansible/roles/dev/db/files/update_materialized_views.sql
+++ tabernacle/ansible/roles/dev/db/files/update_materialized_views.sql
@@ -1,7 +1,6 @@
 -- store_credit_transactions
 refresh materialized view concurrently store_credit_transactions_admins_view;
 refresh materialized view concurrently store_credit_transactions_payments_view;
-refresh materialized view concurrently store_credit_transactions_view;
 
 -- failed authorizations
 refresh materialized view concurrently failed_authorizations_search_view;
