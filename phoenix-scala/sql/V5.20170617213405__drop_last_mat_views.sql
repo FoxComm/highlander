f6752e6637fb20ee87f1ad51463af2d3e73ed316
Sergey Rublev <sergey@foxcommerce.com>
2017-06-17 23:13:35 +0300

    Migrate failed_authorizations_search_view to triggers

--- phoenix-scala/sql/V5.20170617213405__drop_last_mat_views.sql
+++ phoenix-scala/sql/V5.20170617213405__drop_last_mat_views.sql
@@ -101,3 +101,47 @@ insert into store_credit_transactions_admins_view
   left join admin_data as sa on (sa.account_id = sca.store_admin_id)
   left join users as u on (u.account_id = sa.account_id)
   group by sca.id, sc.id, sa.id, u.email, u.name;
+
+
+-- failed_authorizations_search_view
+
+alter table failed_authorizations_search_view
+    rename column charge_id to stripe_charge_id; --  ¯\_(ツ)_/¯
+
+
+insert into failed_authorizations_search_view
+    select distinct on (ccc.id)
+      -- Credit Card Charge
+      ccc.id,
+      ccc.stripe_charge_id,
+      ccc.amount,
+      ccc.currency,
+      ccc.state,
+      to_char(ccc.created_at, 'YYYY-MM-DD"T"HH24:MI:SS.MS"Z"') as created_at,
+      -- Credit Card
+      cc.holder_name,
+      cc.last_four,
+      cc.exp_month,
+      cc.exp_year,
+      cc.brand,
+      -- Billing address
+      cc.address1,
+      cc.address2,
+      cc.city,
+      cc.zip,
+      r.name as region,
+      c.name as country,
+      c.continent,
+      -- Order
+      o.reference_number as cord_reference_number,
+      -- Customer
+      o.account_id as account_id
+    from credit_card_charges as ccc
+      inner join credit_cards as cc on (ccc.credit_card_id = cc.id)
+      inner join regions as r on (cc.region_id = r.id)
+      inner join countries as c on (r.country_id = c.id)
+      inner join order_payments as op on (op.id = ccc.order_payment_id)
+      inner join orders as o on (op.cord_ref = o.reference_number)
+    where ccc.state = 'failedAuth'
+    order by ccc.id
+  on conflict (id) do nothing ; -- ignore if already exists
