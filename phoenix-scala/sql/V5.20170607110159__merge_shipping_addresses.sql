ebc03d27a153a2956702cb7738b53e2fb514d334
Alexey Afanasev <avafanasiev@gmail.com>
2017-06-19 21:11:44 +0300

    Drop testing inside migration, will implement it in a separate PR

--- phoenix-scala/sql/V5.20170607110159__merge_shipping_addresses.sql
+++ phoenix-scala/sql/V5.20170607110159__merge_shipping_addresses.sql
@@ -10,45 +10,22 @@ alter table shipments add constraint shipments_shipping_address_id_fkey
   on update restrict on delete restrict;
 
 -- move everything from order_shipping_addresses to addresses
-create or replace function shipping_addresses_migration() returns text as $$
-begin
-    -- test data for migration
-    insert into carts (account_id, reference_number, created_at, updated_at, currency, scope) values (1, 'TEST-ABC-1', now(), null, 'USD', '1');
-    insert into order_shipping_addresses(cord_ref, region_id, name, address1, address2, city, zip, phone_number, created_at, updated_at)
-    values ('TEST-ABC-1', 1, 'Test address', 'Test Case rd.', null, 'Testburg', '125438', '12345678', now(), null);
-
-    -- data migration
-    insert into addresses(account_id, region_id, name, address1, address2, city, zip, phone_number, created_at, updated_at, deleted_at, cord_ref)
-      select
-        c.account_id,
-        osa.region_id,
-        osa.name,
-        osa.address1,
-        osa.address2,
-        osa.city,
-        osa.zip,
-        osa.phone_number,
-        osa.created_at,
-        osa.updated_at,
-        null, -- no deleted_at
-        osa.cord_ref
-      from order_shipping_addresses as osa
-      inner join carts as c on osa.cord_ref = c.reference_number;
-
-    assert ((select a.cord_ref from addresses as a) = 'TEST-ABC-1'), 'Something is wrong with migration, `TEST-ABC-1` cord_ref was not found';
-
-    -- delete test data
-    delete from addresses where cord_ref = 'TEST-ABC-1';
-    delete from order_shipping_addresses where cord_ref = 'TEST-ABC-1';
-    delete from carts where reference_number = 'TEST-ABC-1';
-    delete from cords where reference_number = 'TEST-ABC-1';
-
-    return '';
-  end;
-  $$ language plpgsql;
-
-
-select shipping_addresses_migration();
+insert into addresses(account_id, region_id, name, address1, address2, city, zip, phone_number, created_at, updated_at, deleted_at, cord_ref)
+  select
+    c.account_id,
+    osa.region_id,
+    osa.name,
+    osa.address1,
+    osa.address2,
+    osa.city,
+    osa.zip,
+    osa.phone_number,
+    osa.created_at,
+    osa.updated_at,
+    null, -- no deleted_at
+    osa.cord_ref
+  from order_shipping_addresses as osa
+    inner join carts as c on osa.cord_ref = c.reference_number;
 
 -- is moved to R__orders_search_view_triggers.sql
 drop function if exists update_orders_view_from_shipping_addresses_fn() cascade;
