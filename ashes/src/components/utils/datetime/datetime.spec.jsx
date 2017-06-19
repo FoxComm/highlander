7e105ba51e3c033f6855d7777011d08b96fe1c58
Bad_Company <ihor.peredystyi@gmail.com>
2017-06-19 16:35:24 +0300

    fix: input/expected time format

--- ashes/src/components/utils/datetime/datetime.spec.jsx
+++ ashes/src/components/utils/datetime/datetime.spec.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { mount } from 'enzyme';
+import moment from 'moment';
 
 import * as DateTime from './datetime';
 
@@ -16,22 +17,24 @@ describe('DateTime', function () {
   });
 
   it('should render date/time in (L LTS) format by default', function () {
-
+    const date = '2017-12-29T15:10:30';
+    const expectedResult = moment.utc(date).local().format('L LTS');
     const datetime = mount(
-      <DateTime.Moment value={'2017-05-19T13:51:50.417Z'} />
+      <DateTime.Moment value={date} />
     );
 
     expect(datetime.find('time').hasClass('time')).to.be.true;
-    expect(datetime.text()).to.equal('05/19/2017 4:51:50 PM');
+    expect(datetime.text()).to.equal(expectedResult);
   });
 
   it('should render date/time with defined format', function () {
-
+    const date = '2017-12-29T15:10:30';
+    const expectedResult = moment.utc(date).local().format('L LT');
     const datetime = mount(
-      <DateTime.Moment value={'2017-05-19T13:51:50.417Z'} format="L LT" />
+      <DateTime.Moment value={date} format="L LT" />
     );
 
-    expect(datetime.text()).to.equal('05/19/2017 4:51 PM');
+    expect(datetime.text()).to.equal(expectedResult);
   });
 
   it('should render passed className inside Moment', function () {
@@ -44,30 +47,33 @@ describe('DateTime', function () {
   });
 
   it('should render date/time in (L LT) format', function () {
-
+    const date = '2017-12-29T15:10:30';
+    const expectedResult = moment.utc(date).local().format('L LT');
     const datetime = mount(
-      <DateTime.DateTime value={'2017-05-19T13:51:50.417Z'} />
+      <DateTime.DateTime value={date} />
     );
 
-    expect(datetime.text()).to.equal('05/19/2017 4:51 PM');
+    expect(datetime.text()).to.equal(expectedResult);
   });
 
   it('should render date in (L) format', function () {
-
+    const date = '2017-12-29T15:10:30';
+    const expectedResult = moment.utc(date).local().format('L');
     const datetime = mount(
-      <DateTime.Date value={'2017-05-19T13:51:50.417Z'} />
+      <DateTime.Date value={date} />
     );
 
-    expect(datetime.text()).to.equal('05/19/2017');
+    expect(datetime.text()).to.equal(expectedResult);
   });
 
   it('should render date/time in (LT) format', function () {
-
+    const date = '2017-12-29T15:10:30';
+    const expectedResult = moment.utc(date).local().format('LT');
     const datetime = mount(
-      <DateTime.Time value={'2017-05-19T13:51:50.417Z'} />
+      <DateTime.Time value={date} />
     );
 
-    expect(datetime.text()).to.equal('4:51 PM');
+    expect(datetime.text()).to.equal(expectedResult);
   });
 
 });
