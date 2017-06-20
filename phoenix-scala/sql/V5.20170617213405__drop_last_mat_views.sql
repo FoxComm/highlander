9c315c8c8459f852ae4f91644bba973f8197e478
Sergey Rublev <sergey@foxcommerce.com>
2017-06-20 19:17:05 +0300

    Drop not needed tables

--- phoenix-scala/sql/V5.20170617213405__drop_last_mat_views.sql
+++ phoenix-scala/sql/V5.20170617213405__drop_last_mat_views.sql
@@ -1,103 +1,51 @@
 -- gift_card_transactions_payments_view
 
 drop materialized view gift_card_transactions_payments_view;
-
-create table gift_card_transactions_payments_view(
-    id integer,
-    order_payment jsonb,
-    scope exts.ltree);
-
-create unique index gift_card_transactions_payments_view_idx on gift_card_transactions_payments_view (id);
-
-insert into gift_card_transactions_payments_view
-    select
-        gca.id,
-        -- Order Payments
-        case when op is not null
-        then
-          to_json((
-              o.reference_number,
-              to_json_timestamp(o.placed_at),
-              to_json_timestamp(op.created_at)
-          )::export_order_payments)
-        else null end,
-        o.scope
-    from gift_card_adjustments as gca
-    inner join gift_cards as gc on (gca.gift_card_id = gc.id)
-    left join order_payments as op on (op.id = gca.order_payment_id)
-    left join orders as o on (op.cord_ref = o.reference_number);
-
--- gift_card_transactions_admins_view
-
 drop materialized view gift_card_transactions_admins_view;
 
-create table gift_card_transactions_admins_view(
-    id integer unique,
-    store_admin jsonb,
-    scope exts.ltree);
-
-insert into gift_card_transactions_admins_view
-    select
-        gca.id,
-        -- Store admins
-        case when sa is not null then
-          to_json((u.email, u.name)::export_store_admins)
-        else null end,
-        sa.scope
-    from gift_card_adjustments as gca
-    inner join gift_cards as gc on (gc.id = gca.gift_card_id)
-    left join users as u on (u.account_id = gca.store_admin_id)
-    left join admin_data as sa on (sa.account_id = gca.store_admin_id);
-
---- store_credit_transactions_payments_view
+alter table gift_card_transactions_view
+  add column scope exts.ltree;
 
 drop materialized view store_credit_transactions_payments_view;
-
-create table store_credit_transactions_payments_view(
-    id integer unique,
-    order_payment jsonb,
-    scope exts.ltree
-);
-
-insert into store_credit_transactions_payments_view
-  select
-    sca.id,
-    -- Order Payments
-    case when op is not null then
-      to_json((
-        o.reference_number,
-        to_json_timestamp(o.placed_at),
-        to_json_timestamp(op.created_at)
-      )::export_order_payments)
-    else null end,
-    o.scope
-from store_credit_adjustments as sca
-inner join store_credits as sc on (sca.store_credit_id = sc.id)
-left join order_payments as op on (op.id = sca.order_payment_id)
-left join orders as o on (op.cord_ref = o.reference_number);
-
--- store_credit_transactions_admins_view
-
 drop materialized view store_credit_transactions_admins_view;
+drop trigger if exists update_store_credit_transactions_view_from_orders on orders;
 
-create table store_credit_transactions_admins_view(
-  id integer unique,
-  store_admin jsonb,
-  scope exts.ltree
-);
+-- gift_card_transaction_view was empty
+truncate gift_card_transactions_view;
 
-insert into store_credit_transactions_admins_view
-  select
-    sca.id,
-    -- Store admins
-    case when sa is not null then
-      to_json((u.email, u.name)::export_store_admins)
-    else null end,
-    sa.scope
-  from store_credit_adjustments as sca
-  inner join store_credits as sc on (sc.id = sca.store_credit_id)
-  left join admin_data as sa on (sa.account_id = sca.store_admin_id)
-  left join users as u on (u.account_id = sa.account_id);
+insert into gift_card_transactions_view
+    select
+        -- Gift Card Transaction
+        gca.id,
+        gca.debit,
+        gca.credit,
+        gca.available_balance,
+        gca.state,
+        to_json_timestamp(gca.created_at) as created_at,
+        -- Gift Card
+        gc.code,
+        gc.origin_type,
+        gc.currency,
+        to_json_timestamp(gc.created_at) as gift_card_created_at,
+        -- Order Payment
+        case when op is not null then
+          to_json((
+            o.reference_number,
+            to_json_timestamp(o.placed_at),
+            to_json_timestamp(op.created_at)
+          )::export_order_payments)
+        else null end as order_payment,
+        -- Store admin
+        case when sa is not null then
+          to_json((u.email, u.name)::export_store_admins)
+        else null end as store_admin,
+        gc.scope
+        from gift_card_adjustments as gca
+        inner join gift_cards as gc on (gc.id = gca.gift_card_id)
+          left join order_payments as op on (op.id = gca.order_payment_id)
+          left join orders as o on (op.cord_ref = o.reference_number)
+          left join users as u on (u.account_id = gca.store_admin_id)
+          left join admin_data as sa on (sa.account_id = gca.store_admin_id);
 
 
 -- failed_authorizations_search_view
@@ -116,7 +64,7 @@ insert into failed_authorizations_search_view
       ccc.amount,
       ccc.currency,
       ccc.state,
-      to_char(ccc.created_at, 'YYYY-MM-DD"T"HH24:MI:SS.MS"Z"') as created_at,
+      to_json_timestamp(ccc.created_at) as created_at,
       -- Credit Card
       cc.holder_name,
       cc.last_four,
