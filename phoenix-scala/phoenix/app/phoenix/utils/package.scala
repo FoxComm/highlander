ba139460aa33c868694c21b25e1eb9a67e84ab53
Krzysztof Janosz <janosz.krzysztof@gmail.com>
2017-06-21 16:04:28 +0200

    Fix logging condition

--- phoenix-scala/phoenix/app/phoenix/utils/package.scala
+++ phoenix-scala/phoenix/app/phoenix/utils/package.scala
@@ -18,11 +18,13 @@ package object utils {
       Future.firstCompletedOf(List(future, timeoutFuture.future)).andThen {
         case _ ⇒
           val cancelled = scheduledTimeout.cancel(true)
-          if (cancelled && timeoutFuture.isCompleted) future.onComplete {
-            case Success(v) ⇒
-              logger.info(s"Call to Stripe has timeouted and then succeeded: $v")
-            case Failure(th) ⇒
-              logger.error("Call to Stripe has timeouted and then failed", th)
+          if (!cancelled && timeoutFuture.isCompleted) {
+            future.onComplete {
+              case Success(v) ⇒
+                logger.info(s"Call to Stripe has timeouted and then succeeded: $v")
+              case Failure(th) ⇒
+                logger.error("Call to Stripe has timeouted and then failed", th)
+            }
           }
       }
     }
