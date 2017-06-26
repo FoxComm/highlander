a35861ed61b360b9d7fae80a19273b086311b203
Bad_Company <ihor.peredystyi@gmail.com>
2017-06-27 02:29:04 +0300

    feat: add steps for `cart`, `shared-search`, `gift-card`

--- build-tests/helpers/steps.js
+++ build-tests/helpers/steps.js
@@ -1,5 +1,7 @@
 import $ from '../payloads';
 
+//TODO structure steps, to make them more readable
+
 export const signup = allure.createStep('Sign Up', (api, email, name, password) => (
   api.auth.signup(email, name, password)
 ));
@@ -13,6 +15,14 @@ export const loginAsAdmin = (api) => login(api, $.adminEmail, $.adminPassword, $
 export const logout = allure.createStep('Logout', (api) => (
 	api.auth.logout()
 ));
+
+export const loginAsCustomer = async (api) => {
+	const { email, name, password } = $.randomUserCredentials();
+	const account = await signup(api, email, name, password);
+	await login(api, email, password, $.customerOrg);
+	api.account = { id: account.user.id, email, name, password };
+	return api;
+};
 
 export const getAccount = allure.createStep('Get account', (api) => (
 	api.account.get()
@@ -39,7 +49,7 @@ export const createNewCustomer = allure.createStep('Create new customer', (api, 
 ));
 
 export const getCreditCardToken = allure.createStep('Get credit card token', (api, payload) => (
-	api.dev.creditCardToken(payload)
+  api.dev.creditCardToken(payload)
 ));
 
 export const createNewPromotion = allure.createStep('Create new promotion', (api, context, payload) => (
@@ -98,8 +108,8 @@ export const getCurrentCart = allure.createStep('Get current cart', (api) => (
 	api.cart.get()
 ));
 
-export const addSkuToCart = allure.createStep('add SKU to cart', (api, skuCode, amount) => (
-	api.cart.addSku(skuCode, amount)
+export const addSkuToCart = allure.createStep('add SKU to cart', (api, skuCode, quantity, attrs) => (
+	api.cart.addSku(skuCode, quantity, attrs)
 ));
 
 export const setShippingAddress = allure.createStep('set Shipping address to cart', (api, payload) => (
@@ -110,8 +120,8 @@ export const getShippingMethods = allure.createStep('List shipping methods', (ap
 	api.cart.getShippingMethods()
 ));
 
-export const chooseShippingMethod = allure.createStep('Choose shipping method', (api, methodId) => (
-	api.cart.chooseShippingMethod(methodId)
+export const chooseShippingMethod = allure.createStep('Choose shipping method', (api, method) => (
+	api.cart.chooseShippingMethod(method.id)
 ));
 
 export const addCreditCard = allure.createStep('Add credit card', (api, cardId) => (
@@ -137,3 +147,85 @@ export const increaseRemorsePeriod = allure.createStep('Increase remorse period'
 export const getShipments = allure.createStep('List shipments', (api, referenceNumber) => (
 	api.inventories.getShipments(referenceNumber)
 ));
+
+export const createNewGiftCard = allure.createStep('Create new gift card', (api, payload) => (
+	api.giftCards.create(payload)
+));
+
+export const getGiftCard = allure.createStep('Get a single gift card', (api, giftCardCode) => (
+	api.giftCards.one(giftCardCode)
+));
+
+export const updateGiftCard = allure.createStep('Update gift card', (api, giftCardCode, payload) => (
+	api.giftCards.update(giftCardCode, payload)
+));
+
+export const deleteSharedSearch = allure.createStep('Delete shared search', (api, code) => (
+	api.sharedSearches.delete(code)
+));
+
+export const listSharedSearch = allure.createStep('List shared search', (api, scope) => (
+	api.sharedSearches.list(scope)
+));
+
+export const createNewSharedSearch = allure.createStep('Create new shared search', (api, payload) => (
+	api.sharedSearches.create(payload)
+));
+
+export const getSharedSearch = allure.createStep('Get a single shared search', (api, code) => (
+	api.sharedSearches.one(code)
+));
+
+export const getAssociates = allure.createStep('Get shared search associates', (api, code) => (
+	api.sharedSearches.getAssociates(code)
+));
+
+export const addAssociate = allure.createStep('Add shared search associates', (api, code, payload) => (
+	api.sharedSearches.addAssociate(code, payload)
+));
+
+export const removeAssociate = allure.createStep('Remove shared search associates', (api, code, adminId) => (
+	api.sharedSearches.removeAssociate(code, adminId)
+));
+
+export const updateQty = allure.createStep('Update quantity in cart', (api, skuCode, newQty) => (
+	api.cart.updateQty(skuCode, newQty)
+));
+
+export const removeSku = allure.createStep('Remove SKU', (api, skuCode) => (
+	api.cart.removeSku(skuCode)
+));
+
+export const removeCreditCards = allure.createStep('Remove credit cards', (api, skuCode) => (
+	api.cart.removeCreditCards(skuCode)
+));
+
+export const addGiftCard = allure.createStep('Add Gift card', (api, payload) => (
+	api.cart.addGiftCard(payload)
+));
+
+export const removeGiftCard = allure.createStep('Remove Gift card', (api, giftCardCode) => (
+  api.cart.removeGiftCard(giftCardCode)
+));
+
+export const issueStoreCredit = allure.createStep('Issue store credit', (api, customerApi, payload) => (
+	api.customers.issueStoreCredit(customerApi.account.id, payload)
+));
+
+export const addStoreCredit = allure.createStep('Add store credit', (api, availableBalance) => (
+	api.cart.addStoreCredit(availableBalance)
+));
+
+export const removeStoreCredits = allure.createStep('Remove store credit', (api) => (
+	api.cart.removeStoreCredits()
+));
+
+export const addCoupon = allure.createStep('Add coupon', (api, couponCode) => (
+	api.cart.addCoupon(couponCode)
+));
+
+export const removeCoupon = allure.createStep('Remove coupon', (api, couponCode) => (
+	api.cart.removeCoupon()
+));
+
+
