8b70e61e97bf9f8e01d5191f0bff9e8d544cc0ff
Eugene Sypachev (@Axblade) <eugene.sypachev@gmail.com>
2017-07-05 18:55:48 +0300

    use class instead of plain functions

--- ashes/src/lib/agni.js
+++ ashes/src/lib/agni.js
@@ -4,18 +4,22 @@ import { request as baseRequest } from './api';
 
 type TArgs = [string, Object];
 
-function searchURI(uri: string): string {
-  return `/api/advanced-search/admin/${uri}`;
+class Agni {
+  searchURI(uri: string): string {
+    return `/api/advanced-search/admin/${uri}`;
+  }
+
+  request(method: string, uri: string, data: Object): Promise<*> {
+    const payload = {
+      type: 'es',
+      query: {...data},
+    };
+    return baseRequest(method, this.searchURI(uri), payload);
+  }
+
+  search(...args: TArgs): Promise<*> {
+    return this.request('POST', ...args);
+  }
 }
 
-function request(method: string, uri: string, data: Object): Promise<*> {
-  const payload = {
-    type: 'es',
-    query: {...data},
-  };
-  return baseRequest(method, searchURI(uri), payload);
-}
-
-export function post(...args: TArgs): Promise<*> {
-  return request('POST', ...args);
-}
+export default new Agni();
